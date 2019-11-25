c75cfda50408ec3325846ca79b15cc18d86a729d
hywqc <470511113@qq.com>
2019-11-25 18:02:18 +0800

    .
    Former-commit-id: d25244e70c66ccc1e31a91c590ada45b6664d42b [formerly 1384964efe9bfaf23a864449213dc106918c8aa3]
    Former-commit-id: f6cb8b6f57bbbae4247f3a16d3be11249a0195bf

--- config/config_test.go
+++ config/config_test.go
@@ -1,1 +1,52 @@
 package config
+
+import (
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/influxdata/toml"
+	"github.com/influxdata/toml/ast"
+)
+
+func TestCfg(t *testing.T) {
+
+	// cfgstr := `
+	// uuid = '1122'
+	// ftdataway = 'http://localhost:9527'
+	// log = 'aa'
+	// log_level = 'info'
+	// config_dir = '/ect/cfg'
+
+	// `
+
+	// Cfg.GlobalTags = make(map[string]string)
+
+	// out, _ := toml.Marshal(&Cfg)
+	// fmt.Println(string(out))
+	if err := LoadConfig(`./cfg.toml`); err != nil {
+		log.Fatalln(err)
+	}
+
+	fdata, _ := ioutil.ReadFile(`./cfg.toml`)
+
+	tbl, err := toml.Parse(fdata)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if val, ok := tbl.Fields["global_tags"]; ok {
+		subTable, ok := val.(*ast.Table)
+		var tags map[string]string
+		tags = map[string]string{}
+		if ok {
+			if err := toml.UnmarshalTable(subTable, tags); err != nil {
+				log.Fatalln(err)
+			} else {
+				log.Println("global_tags:", tags)
+			}
+		}
+	}
+
+	log.Printf("%#v", Cfg)
+}
