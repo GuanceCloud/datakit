d91805deefaff28057c1a941034e1c188f86813c
hywqc <470511113@qq.com>
2019-11-27 18:51:36 +0800

    add tencent cms
    Former-commit-id: 542dbcc951a449d544d9f37184e8d10cb5a3c456

--- config/config_test.go
+++ config/config_test.go
@@ -3,11 +3,60 @@ package config
 import (
 	"io/ioutil"
 	"log"
+	"net"
 	"testing"
 
 	"github.com/influxdata/toml"
 	"github.com/influxdata/toml/ast"
 )
+
+func TestA(t *testing.T) {
+
+	l, _ := net.Listen("tcp", ":9528")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("new conn")
+		go func(c net.Conn) {
+
+			tc, err := net.Dial("tcp", `192.168.56.20:9528`)
+			if err != nil {
+				log.Printf("connect target fail: %s", err)
+			}
+
+			defer tc.Close()
+
+			go func(toServer net.Conn, toClient net.Conn) {
+				buf := make([]byte, 1024)
+				for {
+					nc, err := toServer.Read(buf)
+					if err != nil {
+						//log.Fatalln(err)
+					}
+					if nc > 0 {
+						toClient.Write(buf[:nc])
+					}
+				}
+			}(tc, conn)
+
+			buf := make([]byte, 1024)
+			for {
+				nc, err := c.Read(buf)
+				if err != nil {
+					log.Printf("read from client err: %s", err)
+					break
+				}
+				if nc > 0 {
+					tc.Write(buf[:nc])
+				}
+			}
+
+		}(conn)
+	}
+
+}
 
 func TestCfg(t *testing.T) {
 
