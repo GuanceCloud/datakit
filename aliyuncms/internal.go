f7a1229f1e1482972f2fb4d4bda5b3ddf97d2bf0
hywqc <470511113@qq.com>
2019-11-27 18:51:36 +0800

    add tencent cms
     [formerly 399bc4ab601b619963881ab7c23e0c89f47ab85a [formerly d91805deefaff28057c1a941034e1c188f86813c] [formerly d91805deefaff28057c1a941034e1c188f86813c [formerly 542dbcc951a449d544d9f37184e8d10cb5a3c456]]]
    
    
    
    Former-commit-id: 8b2808dd7107dd6d8d368bdad009fe2a6893420f
    Former-commit-id: 907283a63e015b0aa6f6c2af13786228fb249a00

--- aliyuncms/internal.go
+++ aliyuncms/internal.go
@@ -3,7 +3,6 @@ package aliyuncms
 import (
 	"bytes"
 	"strconv"
-	"sync"
 	"time"
 	"unicode"
 )
@@ -58,57 +57,4 @@ func SnakeCase(in string) string {
 	}
 
 	return string(out)
-}
-
-type rateLimiter struct {
-	C    chan bool
-	rate time.Duration
-	n    int
-
-	shutdown chan bool
-	wg       sync.WaitGroup
-}
-
-func NewRateLimiter(n int, rate time.Duration) *rateLimiter {
-	r := &rateLimiter{
-		C:        make(chan bool),
-		rate:     rate,
-		n:        n,
-		shutdown: make(chan bool),
-	}
-	r.wg.Add(1)
-	go r.limiter()
-	return r
-}
-
-func (r *rateLimiter) Stop() {
-	close(r.shutdown)
-	r.wg.Wait()
-	close(r.C)
-}
-
-func (r *rateLimiter) limiter() {
-	defer r.wg.Done()
-	ticker := time.NewTicker(r.rate)
-	defer ticker.Stop()
-	counter := 0
-	for {
-		select {
-		case <-r.shutdown:
-			return
-		case <-ticker.C:
-			counter = 0
-		default:
-			if counter < r.n {
-				select {
-				case r.C <- true:
-					counter++
-				case <-r.shutdown:
-					return
-				}
-			} else {
-				time.Sleep(time.Millisecond * 5)
-			}
-		}
-	}
 }
