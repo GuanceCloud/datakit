792a936c4ddbd60286a6b89f47a056298a060a20
wuqinchao <wuqinchao@jiagouyun.com>
2019-12-05 20:38:04 +0800

    .
     [formerly 1e3898b1df15a22c42f3a0dc2b7b9edddf0bb672] [formerly fad1bc2f2e83782c07fe01e321559a5ee3dec391 [formerly 7e8e0e63736dd0933c3a38126ee960cb9692cea1] [formerly 1e3898b1df15a22c42f3a0dc2b7b9edddf0bb672 [formerly 084234ab23db02e7487f667bd4feebb9d4b3be7e]]]
    
    
    
    
    Former-commit-id: 8f09e0ce0663609c85c18a9e516b099ea68a9b61

--- aliyunboa/aliyunboa.go
+++ aliyunboa/aliyunboa.go
@@ -2,9 +2,6 @@ package aliyunboa
 
 import (
 	"context"
-	"fmt"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -51,6 +48,11 @@ type (
 		client *bssopenapi.Client
 
 		modules []BoaModule
+
+		lmtr *utils.RateLimiter
+
+		wgSuspend sync.WaitGroup
+		mutex     sync.Mutex
 	}
 
 	AliyunBoaSvr struct {
@@ -59,24 +61,9 @@ type (
 	}
 
 	BoaModule interface {
-		fetch(context.Context, *bssopenapi.Client, log.Logger, uploader.IUploader) error
 		getInterval() time.Duration
 		getName() string
-	}
-
-	BoaAccount struct {
-		interval time.Duration
-		name     string
-	}
-
-	BoaBill struct {
-		interval time.Duration
-		name     string
-	}
-
-	BoaOrder struct {
-		interval time.Duration
-		name     string
+		run(context.Context) error
 	}
 )
 
@@ -93,19 +80,23 @@ func (s *AliyunBoaSvr) Start(ctx context.Context, up uploader.IUploader) error {
 			cfg:      c,
 			uploader: up,
 			logger:   s.logger,
-			modules: []BoaModule{
-				&BoaAccount{
-					name:     "aliyun_cost_account",
-					interval: c.AccountInterval.Duration,
-				},
-				&BoaBill{
-					name:     "aliyun_cost_bill",
-					interval: c.AccountInterval.Duration,
-				},
-				&BoaOrder{
-					name:     "aliyun_cost_order",
-					interval: c.AccountInterval.Duration,
-				},
+		}
+
+		a.modules = []BoaModule{
+			&BoaAccount{
+				name:     "aliyun_cost_account",
+				interval: c.AccountInterval.Duration,
+				boa:      a,
+			},
+			&BoaBill{
+				name:     "aliyun_cost_bill",
+				interval: c.AccountInterval.Duration,
+				boa:      a,
+			},
+			&BoaOrder{
+				name:     "aliyun_cost_order",
+				interval: c.AccountInterval.Duration,
+				boa:      a,
 			},
 		}
 
@@ -133,243 +124,26 @@ func (s *AliyunBoaSvr) Start(ctx context.Context, up uploader.IUploader) error {
 	return nil
 }
 
-func (b *BoaOrder) getInterval() time.Duration {
-	return b.interval
+func (s *RunningBoa) suspendLastyearFetch() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.wgSuspend.Add(1)
 }
 
-func (b *BoaOrder) getName() string {
-	return b.name
+func (s *RunningBoa) resumeLastyearFetch() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.wgSuspend.Done()
 }
 
-func (b *BoaOrder) fetch(ctx context.Context, client *bssopenapi.Client, l log.Logger, up uploader.IUploader) error {
-
-	reqOrder := bssopenapi.CreateQueryOrdersRequest()
-	respOrder, err := client.QueryOrders(reqOrder)
-	if err != nil {
-		return err
-	}
-
-	for _, item := range respOrder.Data.OrderList.Order {
-
-		tags := map[string]string{
-			"ProductCode":      item.ProductCode,
-			"ProductType":      item.ProductType,
-			"SubscriptionType": item.SubscriptionType,
-			"OrderType":        item.OrderType,
-			"Currency":         item.Currency,
-		}
-
-		fields := map[string]interface{}{
-			"OrderID":        item.OrderId,
-			"RelatedOrderId": item.RelatedOrderId,
-		}
-
-		fields["PretaxGrossAmount"], _ = strconv.ParseFloat(item.PretaxGrossAmount, 64)
-		fields["PretaxAmount"], _ = strconv.ParseFloat(item.PretaxAmount, 64)
-
-		t, err := time.Parse(time.RFC3339, item.PaymentTime)
-		if err != nil {
-			l.Errorf("fail to parse time %s, error:%s", item.PaymentTime, err)
-		} else {
-			addLine(b.getName(), tags, fields, t, up, l)
-		}
-
-	}
-
-	return nil
-}
-
-func (b *BoaBill) fetch(ctx context.Context, client *bssopenapi.Client, l log.Logger, up uploader.IUploader) error {
-
-	reqBill := bssopenapi.CreateQueryBillRequest()
-	today := time.Now()
-	reqBill.BillingCycle = fmt.Sprintf("%d-%d", today.Year(), today.Month())
-	respBill, err := client.QueryBill(reqBill)
-	if err != nil {
-		return err
-	}
-
-	for _, item := range respBill.Data.Items.Item {
-		tags := map[string]string{
-			"AccountID":   respBill.Data.AccountID,
-			"AccountName": respBill.Data.AccountName,
-			"OwnerID":     fmt.Sprintf("%v", respBill.Data.OwnerUid),
-		}
-
-		tags["OwnerID"] = item.OwnerID
-		tags["Item"] = item.Item
-		tags["ProductCode"] = item.ProductCode
-		tags["ProductName"] = item.ProductName
-		tags["ProductType"] = item.ProductType
-		tags["SubscriptionType"] = item.SubscriptionType
-		tags["Status"] = item.Status
-		tags[`Currency`] = item.Currency
-
-		fields := map[string]interface{}{}
-
-		fields[`RecordID`] = item.RecordID
-		fields[`PretaxGrossAmount`] = item.PretaxGrossAmount
-		fields[`DeductedByCoupons`] = item.DeductedByCoupons
-		fields[`InvoiceDiscount`] = item.InvoiceDiscount
-		fields[`RoundDownDiscount`], _ = strconv.ParseFloat(item.RoundDownDiscount, 64)
-		fields[`PretaxAmount`] = item.PretaxAmount
-		fields[`DeductedByCashCoupons`] = item.DeductedByCashCoupons
-		fields[`DeductedByPrepaidCard`] = item.DeductedByPrepaidCard
-		fields[`PaymentAmount`] = item.PaymentAmount
-		fields[`OutstandingAmount`] = item.OutstandingAmount
-
-		t, err := time.Parse(`2006-01-02 15:04:05`, item.UsageEndTime)
-		if err != nil {
-			l.Errorf("fail to parse time %s, error:%s", item.UsageEndTime, err)
-		} else {
-			addLine(b.getName(), tags, fields, t, up, l)
-		}
-	}
-
-	reqInstBill := bssopenapi.CreateQueryInstanceBillRequest()
-	reqInstBill.BillingCycle = fmt.Sprintf("%d-%d", today.Year(), today.Month())
-	respInstill, err := client.QueryInstanceBill(reqInstBill)
-	if err != nil {
-		return err
-	}
-
-	//QueryInstanceBill
-	for _, item := range respInstill.Data.Items.Item {
-		tags := map[string]string{
-			"AccountID":   respInstill.Data.AccountID,
-			"AccountName": respInstill.Data.AccountName,
-		}
-
-		tags[`OwnerID`] = item.OwnerID
-		tags[`CostUnit`] = item.CostUnit
-		tags[`SubscriptionType`] = item.SubscriptionType
-		tags[`Item`] = item.Item
-		tags[`ProductCode`] = item.ProductCode
-		tags[`ProductName`] = item.ProductName
-		tags[`ProductType`] = item.ProductType
-		tags[`InstanceID`] = item.InstanceID
-		tags[`NickName`] = item.NickName
-		tags[`InstanceSpec`] = item.InstanceSpec
-		tags[`InternetIP`] = item.InternetIP
-		tags[`IntranetIP`] = item.IntranetIP
-		tags[`Region`] = item.Region
-		tags[`Zone`] = item.Zone
-		tags[`BillingItem`] = item.BillingItem
-		tags[`Currency`] = item.Currency
-
-		if item.Tag != "" {
-			kvs := strings.Split(item.Tag, `;`)
-			for _, kv := range kvs {
-				parts := strings.Split(kv, ` `)
-				if len(parts) != 2 {
-					continue
-				}
-				k := parts[0]
-				v := parts[1]
-				var key, val string
-				pos := strings.Index(k, `key:`)
-				if pos != -1 {
-					key = k[4:]
-				}
-				pos = strings.Index(v, `value:`)
-				if pos != -1 {
-					val = v[6:]
-				}
-				if key != "" {
-					tags[key] = val
-				}
-			}
-		}
-
-		fields := map[string]interface{}{}
-
-		fields[`PretaxGrossAmount`] = item.PretaxGrossAmount
-		fields[`InvoiceDiscount`] = item.InvoiceDiscount
-		fields[`DeductedByCoupons`] = item.DeductedByCoupons
-		fields[`PretaxAmount`] = item.PretaxAmount
-		fields[`DeductedByCashCoupons`] = item.DeductedByCashCoupons
-		fields[`DeductedByPrepaidCard`] = item.DeductedByPrepaidCard
-		fields[`PaymentAmount`] = item.PaymentAmount
-		fields[`OutstandingAmount`] = item.OutstandingAmount
-
-		addLine(b.getName(), tags, fields, time.Now().UTC(), up, l)
-	}
-
-	return nil
-}
-
-func (b *BoaBill) getInterval() time.Duration {
-	return b.interval
-}
-
-func (b *BoaBill) getName() string {
-	return b.name
-}
-
-func (b *BoaAccount) fetch(ctx context.Context, client *bssopenapi.Client, l log.Logger, up uploader.IUploader) error {
-
-	reqAmount := bssopenapi.CreateQueryAccountBalanceRequest()
-	respAmount, err := client.QueryAccountBalance(reqAmount)
-	if err != nil {
-		return err
-	}
-	tags := map[string]string{}
-	fields := map[string]interface{}{}
-
-	tags[`Currency`] = respAmount.Data.Currency
-
-	fields[`AvailableAmount`], _ = strconv.ParseFloat(respAmount.Data.AvailableAmount, 64)
-	fields[`MybankCreditAmount`], _ = strconv.ParseFloat(respAmount.Data.MybankCreditAmount, 64)
-	fields[`AvailableCashAmount`], _ = strconv.ParseFloat(respAmount.Data.AvailableCashAmount, 64)
-	fields[`CreditAmount`], _ = strconv.ParseFloat(respAmount.Data.CreditAmount, 64)
-
-	addLine(b.getName(), tags, fields, time.Now().UTC(), up, l)
-
-	reqTransaction := bssopenapi.CreateQueryAccountTransactionsRequest()
-	respTransaction, err := client.QueryAccountTransactions(reqTransaction)
-	if err != nil {
-		return err
-	}
-
-	if respTransaction.Data.AccountTransactionsList.AccountTransactionsListItem != nil {
-		for _, item := range respTransaction.Data.AccountTransactionsList.AccountTransactionsListItem {
-			tags := map[string]string{}
-			fields := map[string]interface{}{}
-
-			tags[`TransactionFlow`] = item.TransactionFlow
-			tags[`TransactionType`] = item.TransactionType
-			tags[`TransactionChannel`] = item.TransactionChannel
-			tags[`FundType`] = item.FundType
-			//tags[`TransactionAccount`] = item.TransactionAccount
-
-			fields[`TransactionNumber`] = item.TransactionNumber
-			fields[`TransactionChannelSN`] = item.TransactionChannelSN
-			fields[`RecordID`] = item.RecordID
-			fields[`Remarks`] = item.Remarks
-			fields[`Amount`], _ = strconv.ParseFloat(item.Amount, 64)
-			fields[`Balance`], _ = strconv.ParseFloat(item.Balance, 64)
-
-			t, err := time.Parse(time.RFC3339, item.TransactionTime)
-			if err != nil {
-				l.Errorf("fail to parse time %s, error:%s", item.TransactionTime, err)
-			} else {
-				addLine(b.getName(), tags, fields, t, up, l)
-			}
-		}
-	}
-
-	return nil
-}
-
-func (b *BoaAccount) getInterval() time.Duration {
-	return b.interval
-}
-
-func (b *BoaAccount) getName() string {
-	return b.name
+func (s *RunningBoa) wait() {
+	s.wgSuspend.Wait()
 }
 
 func (s *RunningBoa) Run(ctx context.Context) error {
+
+	s.lmtr = utils.NewRateLimiter(10, time.Minute)
+	defer s.lmtr.Stop()
 
 	var err error
 	s.client, err = bssopenapi.NewClientWithAccessKey(s.cfg.RegionID, s.cfg.AccessKeyID, s.cfg.AccessKeySecret)
@@ -383,32 +157,12 @@ func (s *RunningBoa) Run(ctx context.Context) error {
 	default:
 	}
 
-	lmtr := utils.NewRateLimiter(rateLimit, time.Second)
-	defer lmtr.Stop()
-
 	for _, boaModule := range s.modules {
 		s.wg.Add(1)
-
 		go func(m BoaModule, ctx context.Context) {
 			defer s.wg.Done()
 
-			ticker := time.NewTicker(m.getInterval())
-
-			for {
-
-				s.logger.Debugf("start fetch")
-
-				err = m.fetch(ctx, s.client, s.logger, s.uploader)
-				if err != nil {
-					s.logger.Errorf(`fail to get metric %s: %s`, m.getName(), err)
-				}
-
-				select {
-				case <-ctx.Done():
-					return
-				case <-ticker.C:
-				}
-			}
+			m.run(ctx)
 
 		}(boaModule, ctx)
 
@@ -426,7 +180,7 @@ func addLine(metricName string, tags map[string]string, fields map[string]interf
 	m, _ := metric.New(metricName, tags, fields, tm)
 
 	output, err := serializer.Serialize(m)
-	l.Debug(string(output))
+	//l.Debug(string(output))
 	if err == nil {
 		if up != nil {
 			up.AddLog(&uploader.LogItem{
