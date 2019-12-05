2191f500abe31e47aa6b307bff694acc442b4f1d
wuqinchao <wuqinchao@jiagouyun.com>
2019-12-05 20:38:04 +0800

    .
     [formerly 1e3898b1df15a22c42f3a0dc2b7b9edddf0bb672] [formerly 119152a937d4ea9fc8a0e62efa558e05130755ed [formerly 119152a937d4ea9fc8a0e62efa558e05130755ed [formerly fad1bc2f2e83782c07fe01e321559a5ee3dec391]] [formerly 119152a937d4ea9fc8a0e62efa558e05130755ed [formerly fad1bc2f2e83782c07fe01e321559a5ee3dec391] [formerly fad1bc2f2e83782c07fe01e321559a5ee3dec391 [formerly 7e8e0e63736dd0933c3a38126ee960cb9692cea1]]] [formerly 1e3898b1df15a22c42f3a0dc2b7b9edddf0bb672 [formerly 1e3898b1df15a22c42f3a0dc2b7b9edddf0bb672 [formerly 1e3898b1df15a22c42f3a0dc2b7b9edddf0bb672 [formerly 084234ab23db02e7487f667bd4feebb9d4b3be7e]]]]]
    
    
    
    
    Former-commit-id: a9faa9d165c138cee85a915d9e7d8429fe7c408f [formerly 8f09e0ce0663609c85c18a9e516b099ea68a9b61]
    Former-commit-id: 241732da8c28ed654e93fe0bb73f0f3304db3861 [formerly 792a936c4ddbd60286a6b89f47a056298a060a20]
    Former-commit-id: 5879acf738f9fb4d98699fdd27a0926bcc891539

--- uploader/uploader.go
+++ uploader/uploader.go
@@ -30,7 +30,7 @@ func New(r string) *Uploader {
 	u := &Uploader{
 		Remote:              r,
 		numShards:           5,
-		shardCap:            1000,
+		shardCap:            2000,
 		maxSamplesPerSecond: 100,
 		Compress:            true,
 		batchSendDeadline:   time.Second * 10,
