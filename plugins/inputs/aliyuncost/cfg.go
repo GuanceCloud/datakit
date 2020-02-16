fc921b21890fc19bd36e836954caae56e417b328
hywqc <470511113@qq.com>
2020-02-16 23:34:52 +0800

    .
    Former-commit-id: 5543ecdf12ea431f4564aae1c4918e8c491a0cde [formerly d4a81f29d0d584bf154e1ae22f9dcd312bccd33c] [formerly 221498459482acb60cbe578bcbb50c80fc3e2712 [formerly ce244db442fd3d0fadf9256d4a60b11c0ae65a57]]
    Former-commit-id: 221498459482acb60cbe578bcbb50c80fc3e2712
    Former-commit-id: 79922792700e3fe798f68da9c3c3057ca92ebdcc

---
+++ plugins/inputs/aliyuncost/cfg.go
@@ -0,0 +1,39 @@
+package aliyuncost
+
+import "gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+
+const (
+	aliyuncostConfigSample = `
+#[[boa]]
+#  ## Aliyun Region (required)
+#  ## See: https://www.alibabacloud.com/help/zh/doc-detail/40654.htm
+#  region_id = 'cn-hangzhou'
+  
+#  ## Aliyun Credentials (required)
+#  access_key_id = ''
+#  access_key_secret = ''
+
+#  account_interval = "24h"
+#  bill_interval = "1h"
+#  order_interval = "1h"
+
+#  ##是否采集账单最近1年历史数据
+#  collect_history_data = false
+`
+)
+
+// var (
+// 	Cfg AliyunBoaCfg
+// )
+
+type (
+	CostCfg struct {
+		AccessKeyID        string            `toml:"access_key_id"`
+		AccessKeySecret    string            `toml:"access_key_secret"`
+		RegionID           string            `toml:"region_id"`
+		AccountInterval    internal.Duration `toml:"account_interval"`
+		BiilInterval       internal.Duration `toml:"bill_interval"`
+		OrdertInterval     internal.Duration `toml:"order_interval"`
+		CollectHistoryData bool              `toml:"collect_history_data "`
+	}
+)
