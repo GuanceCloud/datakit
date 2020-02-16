d188c78e63cd3ab06e217e43a11f4e9941997bde
hywqc <470511113@qq.com>
2020-02-16 23:34:52 +0800

    .
    Former-commit-id: 6aabe1856c372a415b64048c137d0f58d5d04e0e [formerly d4a81f29d0d584bf154e1ae22f9dcd312bccd33c] [formerly 7ff4051e9be30ec4833b37bafc3d751b1f8703ca [formerly ce244db442fd3d0fadf9256d4a60b11c0ae65a57]]
    Former-commit-id: 7ff4051e9be30ec4833b37bafc3d751b1f8703ca
    Former-commit-id: 79922792700e3fe798f68da9c3c3057ca92ebdcc

---
+++ plugins/inputs/aliyuncost/aliyuncost.go
@@ -0,0 +1,197 @@
+package aliyuncost
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/bssopenapi"
+
+	"github.com/influxdata/telegraf"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/limiter"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+var (
+	batchInterval = time.Duration(5) * time.Minute
+	metricPeriod  = time.Duration(5 * time.Minute)
+	rateLimit     = 20
+)
+
+type (
+	AliyunCost struct {
+		Costs []*CostCfg `toml:"boa"`
+
+		runningInst []*RunningInstance
+
+		tags map[string]string
+
+		ctx       context.Context
+		cancelFun context.CancelFunc
+
+		accumulator telegraf.Accumulator
+	}
+
+	RunningInstance struct {
+		cfg *CostCfg
+
+		wg sync.WaitGroup
+
+		client *bssopenapi.Client
+
+		modules []costModule
+
+		lmtr *limiter.RateLimiter
+
+		wgSuspend sync.WaitGroup
+		mutex     sync.Mutex
+
+		cost *AliyunCost
+
+		ctx context.Context
+	}
+
+	costModule interface {
+		getInterval() time.Duration
+		getName() string
+		run(context.Context) error
+	}
+)
+
+func (_ *AliyunCost) SampleConfig() string {
+	return aliyuncostConfigSample
+}
+
+func (_ *AliyunCost) Description() string {
+	return ""
+}
+
+func (_ *AliyunCost) Gather(telegraf.Accumulator) error {
+	return nil
+}
+
+func (ac *AliyunCost) Init() error {
+
+	for _, cfg := range ac.Costs {
+		if cfg.AccountInterval.Duration == 0 {
+			cfg.AccountInterval.Duration = 24 * time.Hour
+		}
+
+		if cfg.BiilInterval.Duration == 0 {
+			cfg.BiilInterval.Duration = time.Hour
+		}
+
+		if cfg.OrdertInterval.Duration == 0 {
+			cfg.OrdertInterval.Duration = time.Hour
+		}
+	}
+
+	return nil
+}
+
+func (ac *AliyunCost) Start(acc telegraf.Accumulator) error {
+
+	if len(ac.Costs) == 0 {
+		log.Printf("W! no aliyun cost configuration")
+		return nil
+	}
+
+	log.Printf("aliyun cost start")
+
+	ac.accumulator = acc
+
+	for _, cfg := range ac.Costs {
+
+		ri := &RunningInstance{
+			cfg:  cfg,
+			cost: ac,
+			ctx:  ac.ctx,
+		}
+
+		ri.modules = []costModule{
+			NewCostAccount(cfg, ri),
+			NewCostBill(cfg, ri),
+			NewCostOrder(cfg, ri),
+		}
+
+		ac.runningInst = append(ac.runningInst, ri)
+	}
+
+	for _, inst := range ac.runningInst {
+
+		go func(ri *RunningInstance) {
+
+			if err := ri.run(); err != nil && err != context.Canceled {
+				log.Printf("E! [aliyuncost] %s", err)
+			}
+
+			log.Printf("[aliyuncost] instance done")
+
+		}(inst)
+	}
+
+	return nil
+}
+
+func (ac *AliyunCost) Stop() {
+	ac.cancelFun()
+}
+
+func (s *RunningInstance) suspendLastyearFetch() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.wgSuspend.Add(1)
+}
+
+func (s *RunningInstance) resumeLastyearFetch() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.wgSuspend.Done()
+}
+
+func (s *RunningInstance) wait() {
+	s.wgSuspend.Wait()
+}
+
+func (s *RunningInstance) run() error {
+
+	s.lmtr = limiter.NewRateLimiter(10, time.Minute)
+	defer s.lmtr.Stop()
+
+	var err error
+	s.client, err = bssopenapi.NewClientWithAccessKey(s.cfg.RegionID, s.cfg.AccessKeyID, s.cfg.AccessKeySecret)
+	if err != nil {
+		return err
+	}
+
+	select {
+	case <-s.ctx.Done():
+		return context.Canceled
+	default:
+	}
+
+	for _, boaModule := range s.modules {
+		s.wg.Add(1)
+		go func(m costModule, ctx context.Context) {
+			defer s.wg.Done()
+
+			m.run(ctx)
+
+		}(boaModule, s.ctx)
+
+	}
+
+	s.wg.Wait()
+
+	return nil
+}
+
+func init() {
+	inputs.Add("aliyuncost", func() telegraf.Input {
+		ac := &AliyunCost{}
+		ac.ctx, ac.cancelFun = context.WithCancel(context.Background())
+		return ac
+	})
+}
