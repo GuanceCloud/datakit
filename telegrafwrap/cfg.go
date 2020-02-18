a02c3e195c4d227b8a8a8890d9012e5b9b2a5b63
hywqc <470511113@qq.com>
2020-02-18 23:10:01 +0800

    config agent
     [formerly 38ababa59279ecb76698f5daf943c29be6d24fb4] [formerly 38ababa59279ecb76698f5daf943c29be6d24fb4 [formerly 38ababa59279ecb76698f5daf943c29be6d24fb4 [formerly 38ababa59279ecb76698f5daf943c29be6d24fb4 [formerly 73a631b36eed954b3c31dcb9816a52c27d898bb3]]]] [formerly bd80653f82c93abaabf8b5302de822fc60024bff [formerly bd80653f82c93abaabf8b5302de822fc60024bff [formerly bd80653f82c93abaabf8b5302de822fc60024bff [formerly 87da466996b21c87104b7b99c170d63cbc884fec]]] [formerly 62dd54b5f88bb4419dadff3f93e7a4d0f4821a12 [formerly 62dd54b5f88bb4419dadff3f93e7a4d0f4821a12 [formerly c1ab66d9ce5ba597b57b8d2671c04641e19e7de9]]] [formerly 38ababa59279ecb76698f5daf943c29be6d24fb4 [formerly 38ababa59279ecb76698f5daf943c29be6d24fb4 [formerly 38ababa59279ecb76698f5daf943c29be6d24fb4 [formerly 73a631b36eed954b3c31dcb9816a52c27d898bb3]]] [formerly 62dd54b5f88bb4419dadff3f93e7a4d0f4821a12 [formerly 62dd54b5f88bb4419dadff3f93e7a4d0f4821a12 [formerly c1ab66d9ce5ba597b57b8d2671c04641e19e7de9]] [formerly 62dd54b5f88bb4419dadff3f93e7a4d0f4821a12 [formerly c1ab66d9ce5ba597b57b8d2671c04641e19e7de9] [formerly c1ab66d9ce5ba597b57b8d2671c04641e19e7de9 [formerly 05cb023cebe7f1785c83d41f88ad629470735cd5]]]]]]
     [formerly 62dd54b5f88bb4419dadff3f93e7a4d0f4821a12 [formerly 62dd54b5f88bb4419dadff3f93e7a4d0f4821a12 [formerly c1ab66d9ce5ba597b57b8d2671c04641e19e7de9]]]
    
    
    
    
    Former-commit-id: 7e27330156b396b8029ae92b2c5340c29355bc54 [formerly be4ba6a23ab7298e9a9554b3f555cbc6c3bb2324]
    Former-commit-id: ed3f0ea48de138fcbb72e564df6c636e107a03c4 [formerly ca02e898200bbfefb667cdfdb736a0e61c259d69]
    Former-commit-id: 774792b9069db011cef7692ad8456a64071893d9

--- telegrafwrap/cfg.go
+++ telegrafwrap/cfg.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"time"
+
+	"github.com/influxdata/toml"
 
 	"github.com/alecthomas/template"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
@@ -12,23 +15,7 @@ import (
 )
 
 const (
-	telegrafConfTemplate = `
-[agent]
-  interval = "10s"
-  round_interval = true
-
-  metric_batch_size = 1000
-  metric_buffer_limit = 10000
-  collection_jitter = "0s"
-  flush_interval = "10s"
-  flush_jitter = "0s"
-  precision = ""
-  logfile='{{.LogFile}}'
-  debug = {{.DebugMode}}
-  quiet = false
-  hostname = ""
-  omit_hostname = false
-
+	httpOutputTemplate = `
 [[outputs.http]]
   url = "{{.FtGateway}}"
   method = "POST"
@@ -44,35 +31,90 @@ const (
 `
 )
 
+func marshalAgentCfg(cfg *config.TelegrafAgentConfig) (string, error) {
+
+	type dummyAgentCfg struct {
+		Interval                   time.Duration
+		RoundInterval              bool
+		Precision                  time.Duration
+		CollectionJitter           time.Duration
+		FlushInterval              time.Duration
+		FlushJitter                time.Duration
+		MetricBatchSize            int
+		MetricBufferLimit          int
+		FlushBufferWhenFull        bool
+		UTC                        bool          `toml:"utc"`
+		Debug                      bool          `toml:"debug"`
+		Quiet                      bool          `toml:"quiet"`
+		LogTarget                  string        `toml:"logtarget"`
+		Logfile                    string        `toml:"logfile"`
+		LogfileRotationInterval    time.Duration `toml:"logfile_rotation_interval"`
+		LogfileRotationMaxSize     int64         `toml:"logfile_rotation_max_size"`
+		LogfileRotationMaxArchives int           `toml:"logfile_rotation_max_archives"`
+		Hostname                   string
+		OmitHostname               bool
+	}
+
+	c := &dummyAgentCfg{
+		Interval:                   cfg.Interval.Duration / time.Second,
+		RoundInterval:              cfg.RoundInterval,
+		Precision:                  cfg.Precision.Duration / time.Second,
+		CollectionJitter:           cfg.CollectionJitter.Duration / time.Second,
+		FlushInterval:              cfg.FlushInterval.Duration / time.Second,
+		FlushJitter:                cfg.FlushJitter.Duration / time.Second,
+		MetricBatchSize:            cfg.MetricBatchSize,
+		MetricBufferLimit:          cfg.MetricBufferLimit,
+		FlushBufferWhenFull:        cfg.FlushBufferWhenFull,
+		UTC:                        cfg.UTC,
+		Debug:                      cfg.Debug,
+		Quiet:                      cfg.Quiet,
+		LogTarget:                  cfg.LogTarget,
+		Logfile:                    cfg.Logfile,
+		LogfileRotationInterval:    cfg.LogfileRotationInterval.Duration / time.Second,
+		LogfileRotationMaxSize:     cfg.LogfileRotationMaxSize.Size,
+		LogfileRotationMaxArchives: cfg.LogfileRotationMaxArchives,
+		Hostname:                   cfg.Hostname,
+		OmitHostname:               cfg.OmitHostname,
+	}
+
+	agdata, err := toml.Marshal(c)
+	if err != nil {
+		return "", err
+	}
+	return string(agdata), nil
+}
+
 func (s *TelegrafSvr) GenerateTelegrafConfig() (string, error) {
 
+	agentcfg, err := marshalAgentCfg(s.Cfg.TelegrafAgentCfg)
+	if err != nil {
+		return "", nil
+	}
+	agentcfg = "\n[agent]\n" + agentcfg
+	agentcfg += "\n"
+
 	globalTags := "[global_tags]\n"
-	for k, v := range s.MainCfg.GlobalTags {
+	for k, v := range s.Cfg.MainCfg.GlobalTags {
 		tag := fmt.Sprintf("%s='%s'\n", k, v)
 		globalTags += tag
 	}
 
-	type telegrafCfg struct {
-		LogFile     string
+	type httpoutCfg struct {
 		FtGateway   string
 		DKUUID      string
 		DKVERSION   string
 		DKUserAgent string
-		DebugMode   bool
 	}
 
-	cfg := telegrafCfg{
-		LogFile:     filepath.Join(config.ExecutableDir, "agent.log"),
-		FtGateway:   s.MainCfg.FtGateway,
-		DKUUID:      s.MainCfg.UUID,
+	cfg := httpoutCfg{
+		FtGateway:   s.Cfg.MainCfg.FtGateway,
+		DKUUID:      s.Cfg.MainCfg.UUID,
 		DKVERSION:   git.Version,
 		DKUserAgent: config.UserAgent(),
-		DebugMode:   s.MainCfg.LogLevel == "debug",
 	}
 
-	var err error
 	tpl := template.New("")
-	tpl, err = tpl.Parse(telegrafConfTemplate)
+	tpl, err = tpl.Parse(httpOutputTemplate)
 	if err != nil {
 		return "", err
 	}
@@ -82,14 +124,14 @@ func (s *TelegrafSvr) GenerateTelegrafConfig() (string, error) {
 		return "", err
 	}
 
-	tlegrafConfig := globalTags + string(buf.Bytes())
+	tlegrafConfig := globalTags + agentcfg + string(buf.Bytes())
 
 	pluginCfgs := ""
 	for index, n := range config.SupportsTelegrafMetraicNames {
 		if !config.MetricsEnablesFlags[index] {
 			continue
 		}
-		cfgpath := filepath.Join(s.MainCfg.ConfigDir, n, fmt.Sprintf(`%s.conf`, n))
+		cfgpath := filepath.Join(s.Cfg.MainCfg.ConfigDir, n, fmt.Sprintf(`%s.conf`, n))
 		d, err := ioutil.ReadFile(cfgpath)
 		if err != nil {
 			return "", err
