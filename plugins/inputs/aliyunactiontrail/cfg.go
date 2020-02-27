446534598aa3627b4dcee555a5a4f1606edd7025
hywqc <470511113@qq.com>
2020-02-27 14:47:55 +0800

    aliyun actiontrail
     [formerly 8f050d4a6258439ad0b64cd722f44610b5e6faf2 [formerly 65d0e2f5d9c65b98848efab8bfc675917282b1b4] [formerly 65d0e2f5d9c65b98848efab8bfc675917282b1b4 [formerly 07598c7fb2145fb015df9085c8eede500e162727]]]
    
    
    
    Former-commit-id: 6d0492f2aba785a5f9f34c2c49cc44c6e61f2c87
    Former-commit-id: c07f8f65e322d8d731668e445260f6565b140aa8

---
+++ plugins/inputs/aliyunactiontrail/cfg.go
@@ -0,0 +1,34 @@
+package aliyunactiontrail
+
+import "gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+
+const (
+	configSample = `
+#[[actiontrail]]
+#    region = 'cn-hangzhou'
+#    access_key = ''
+#    access_id = ''
+
+#    ##if empty, use "aliyun_actiontrail"
+#    metric_name = ''
+
+#    ## ISO8601 unix time format: 2020-02-01T06:00:00Z 
+#    ## the earliest is 90 days from now.
+#    ## if empty, from now on. 
+#    from = ''
+
+#    ## default is 10m, must not be less than 10m
+#    interval = '10m'
+`
+)
+
+type (
+	ActiontrailInstance struct {
+		Region     string
+		AccessKey  string
+		AccessID   string
+		MetricName string
+		From       string
+		Interval   internal.Duration //至少10分钟
+	}
+)
