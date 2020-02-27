9a0214a81583dd4edce429253b53ca4be070d812
hywqc <470511113@qq.com>
2020-02-27 14:47:55 +0800

    aliyun actiontrail
     [formerly 5872c7409d16b928081418741a63eef7b15dad5c] [formerly 82ae0de2089f79129f8b1aecee3553aafebfe5a7 [formerly 82ae0de2089f79129f8b1aecee3553aafebfe5a7 [formerly 82ae0de2089f79129f8b1aecee3553aafebfe5a7 [formerly 1e8f148f48ec1671169b65e8e3f8d30f64dbe854]]] [formerly 5872c7409d16b928081418741a63eef7b15dad5c [formerly 5872c7409d16b928081418741a63eef7b15dad5c [formerly 5872c7409d16b928081418741a63eef7b15dad5c [formerly 4eb92d8ef3462083749e2ee8c50240097b515a40]]]]]
    
    
    
    
    Former-commit-id: 52d3f4702d80b367230ccd1378a98b041a206d49 [formerly 643cf7cee086770f6482b56f76517ba1e24d00a7]
    Former-commit-id: bffe601721dbd2b99daaaa4d0b0f49d7f3eb4aeb [formerly 75fcb68417b9ad311d30a08fdd5df596e4a9f6b0]
    Former-commit-id: 4c5738cfceab510769bfb5954645e6388605b3ce

---
+++ plugins/inputs/aliyunactiontrail/aliyunactiontrail_test.go
@@ -0,0 +1,94 @@
+package aliyunactiontrail
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/actiontrail"
+	"github.com/influxdata/toml"
+)
+
+func TestConfig(t *testing.T) {
+	var cfg AliyunActiontrail
+	cfg.Actiontrail = []*ActiontrailInstance{
+		&ActiontrailInstance{
+			Region:     "xx",
+			AccessID:   "xx",
+			AccessKey:  "xx",
+			MetricName: "aa",
+		},
+	}
+	if data, err := toml.Marshal(&cfg); err != nil {
+		t.Errorf("%s", err)
+	} else {
+		log.Printf("%s", string(data))
+	}
+}
+
+func TestActiontrail(t *testing.T) {
+	cli, err := actiontrail.NewClientWithAccessKey(`cn-hangzhou`, `LTAI4FkR2SokHHESouUMrkxV`, `ht4jybX3IrhQAUgHrUOTJRrkP8dONJ`)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
+	startTm := time.Now().Truncate(time.Hour).Add(-time.Hour * 22)
+
+	request := actiontrail.CreateLookupEventsRequest()
+	request.Scheme = "https"
+	request.StartTime = unixTimeStrISO8601(startTm)
+	request.EndTime = unixTimeStrISO8601(startTm.Add(time.Minute * 30))
+
+	log.Printf("range: %s - %s", request.StartTime, request.EndTime)
+
+	response, err := cli.LookupEvents(request)
+	if err != nil {
+		t.Errorf("LookupEvents failed, %s", err)
+	}
+
+	for _, ev := range response.Events {
+		tags := map[string]string{}
+		fields := map[string]interface{}{}
+
+		if eventType, ok := ev["eventType"].(string); ok {
+			tags["eventType"] = eventType
+		}
+
+		if acsRegion, ok := ev["acsRegion"].(string); ok {
+			tags["region"] = acsRegion
+		}
+
+		fields["eventId"] = ev["eventId"]
+		fields["eventSource"] = ev["eventSource"]
+		fields["serviceName"] = ev["serviceName"]
+		if ev["sourceIpAddress"] != nil {
+			fields["sourceIpAddress"] = ev["sourceIpAddress"]
+		}
+		fields["userAgent"] = ev["userAgent"]
+		fields["eventVersion"] = ev["eventVersion"]
+
+		if userIdentity, ok := ev["userIdentity"].(map[string]interface{}); ok {
+			//userIdentity:map[accountId:50220571 principalId:50220571 type:root-account userName:root]
+			fields["accountId"] = userIdentity["accountId"]
+			fields["accountType"] = userIdentity["type"]
+			fields["userName"] = userIdentity["userName"]
+			fields["principalId"] = userIdentity["principalId"]
+		}
+
+		if additionalEventData, ok := ev["additionalEventData"].(map[string]interface{}); ok {
+			//additionalEventData:map[isMFAChecked:false loginAccount:13626736491]
+			fields["loginAccount"] = additionalEventData["loginAccount"]
+			fields["isMFAChecked"] = additionalEventData["isMFAChecked"]
+		}
+
+		eventTime := ev["eventTime"].(string) //utc
+		evtm, err := time.Parse(`2006-01-02T15:04:05Z`, eventTime)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		fmt.Printf("%s, %s\n", fields["eventId"], evtm)
+
+	}
+}
