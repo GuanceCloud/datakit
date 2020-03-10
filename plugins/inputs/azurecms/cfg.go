5355a3ae6fbd165e60d3f3659a710bb2fdf1e2b7
wuqinchao <wuqinchao@jiagouyun.com>
2020-03-10 19:15:11 +0800

    support azure monitor
    Former-commit-id: 263e1d354091bd68184ca1a19e19bfb2a0cb9ad9 [formerly f32ce0b0912abe8075fcea6ad39df43e8b73a959] [formerly 87176ca3968106e5a9850a59347f1b689028d33f [formerly 68af196cdac65d8bd51d742d2c91608a370fcb9b]]
    Former-commit-id: e7e87bba4e7d4eea6660e505d2b98ad62f092ce2 [formerly 7a0203385adc80c9f90d887af1404d908baa28ab]
    Former-commit-id: fff4c4f4a37f9dc261b3970ce5f21c5fbec5f86a

---
+++ plugins/inputs/azurecms/cfg.go
@@ -0,0 +1,73 @@
+package azurecms
+
+import (
+	"github.com/Azure/azure-sdk-for-go/services/preview/monitor/mgmt/2019-06-01/insights"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+)
+
+const (
+	sampleConfig = `
+`
+)
+
+type (
+	azureMetric struct {
+		MetricName string
+		Interval   internal.Duration
+	}
+
+	azureResource struct {
+		ResourceID string
+		Metrics    []*azureMetric
+	}
+
+	azureInstance struct {
+		ClientID       string
+		ClientSecret   string
+		TenantID       string
+		SubscriptionID string
+		EndPoint       string //https://management.chinacloudapi.cn
+
+		Resource []*azureResource
+	}
+
+	metricMeta struct {
+		supportTimeGrain      []int64
+		supportedAggregations []string
+		unit                  string
+	}
+
+	queryListInfo struct {
+		meta *metricMeta
+
+		resourceID  string
+		timespan    string
+		interval    string
+		metricname  string
+		aggregation string
+		top         int32
+		orderby     string
+		filter      string
+		resultType  insights.ResultType
+		//apiVersion  string // "2018-01-01"
+		metricnamespace string // `Microsoft.Compute/virtualMachines`
+	}
+)
+
+func (a *azureInstance) genQueryInfo() []*queryListInfo {
+
+	var infos []*queryListInfo
+
+	for _, res := range a.Resource {
+		for _, ms := range res.Metrics {
+			info := &queryListInfo{
+				resourceID: res.ResourceID,
+				metricname: ms.MetricName,
+				interval:   "",
+			}
+			infos = append(infos, info)
+		}
+	}
+
+	return infos
+}
