5e98a5a3d76e32557844b0d2a21800d046f91bd5
wuqinchao <wuqinchao@jiagouyun.com>
2020-03-11 17:46:22 +0800

    .
    Former-commit-id: 4bd93238a0aff1f28601feaaf3a55b4f3f3c8b77 [formerly 1af02e0b6abb875f537dd3c6a4da27a12fcf3f22]
    Former-commit-id: 86a43f3a89df3e4beab02cf4a43e049f9a730480

--- plugins/inputs/azurecms/cfg.go
+++ plugins/inputs/azurecms/cfg.go
@@ -1,6 +1,8 @@
 package azurecms
 
 import (
+	"time"
+
 	"github.com/Azure/azure-sdk-for-go/services/preview/monitor/mgmt/2019-06-01/insights"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
 )
@@ -18,7 +20,7 @@ const (
 
 #[[instances.resource.metrics]]
 #metric_name = 'Percentage CPU'
-#interval = '1m'
+# #interval = '1m'
 `
 )
 
@@ -52,17 +54,20 @@ type (
 	queryListInfo struct {
 		meta *metricMeta
 
-		resourceID  string
-		timespan    string
-		interval    string
-		metricname  string
-		aggregation string
-		top         int32
-		orderby     string
-		filter      string
-		resultType  insights.ResultType
+		resourceID   string
+		timespan     string
+		intervalTime time.Duration
+		interval     string
+		metricname   string
+		aggregation  string
+		top          int32
+		orderby      string
+		filter       string
+		resultType   insights.ResultType
 		//apiVersion  string // "2018-01-01"
 		metricnamespace string // `Microsoft.Compute/virtualMachines`
+
+		lastFetchTime time.Time
 	}
 )
 
@@ -73,13 +78,46 @@ func (a *azureInstance) genQueryInfo() []*queryListInfo {
 	for _, res := range a.Resource {
 		for _, ms := range res.Metrics {
 			info := &queryListInfo{
-				resourceID: res.ResourceID,
-				metricname: ms.MetricName,
-				interval:   "",
+				resourceID:   res.ResourceID,
+				metricname:   ms.MetricName,
+				intervalTime: ms.Interval.Duration,
+				interval:     convertInterval(ms.Interval.Duration),
+			}
+			if info.intervalTime < time.Minute {
+				info.intervalTime = time.Minute
 			}
 			infos = append(infos, info)
 		}
 	}
 
 	return infos
+}
+
+func convertInterval(interval time.Duration) string {
+
+	if interval == time.Minute {
+		return "PT1M"
+	} else if interval == 5*time.Minute {
+		return "PT5M"
+	} else if interval == 15*time.Minute {
+		return "PT15M"
+	} else if interval == 30*time.Minute {
+		return "PT30M"
+	} else if interval == time.Hour {
+		return "PT1H"
+	} else if interval == 6*time.Hour {
+		return "PT6H"
+	} else if interval == 12*time.Hour {
+		return "PT12H"
+	} else if interval == 24*time.Hour {
+		return "P1D"
+	}
+	return "PT1M"
+}
+
+func unixTimeStrISO8601(t time.Time) string {
+	_, zoff := t.Zone()
+	nt := t.Add(-(time.Duration(zoff) * time.Second))
+	s := nt.Format(`2006-01-02T15:04:05Z`)
+	return s
 }
