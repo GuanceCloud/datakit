851ce49c55c9fe770e01ea0eeb404816d0ad21c9
wuqinchao <wuqinchao@jiagouyun.com>
2020-03-24 17:52:36 +0800

    .
    Former-commit-id: 7d0c6555aa0f3beb4633490536af1da7966b7647 [formerly dbd0aea653e8e65e1ae50bab93d7b063ee875c88]
    Former-commit-id: 9850ea5f653fceb0df635b2a804d3aa721fea230

--- main.go
+++ main.go
@@ -33,10 +33,13 @@ var (
 
 	flagVersion = flag.Bool("version", false, `show verison info`)
 
-	flagInit      = flag.Bool(`init`, false, `init agent`)
-	flagFtDataway = flag.String("ftdataway", ``, `address of ftdataway`)
-	flagLogFile   = flag.String(`log`, ``, `log file`)
-	flagLogLevel  = flag.String(`log-level`, ``, `log level`)
+	flagInit         = flag.Bool(`init`, false, `init agent`)
+	flagFtDataway    = flag.String("ftdataway", ``, `address of ftdataway`)
+	flagLogFile      = flag.String(`log`, ``, `log file`)
+	flagLogLevel     = flag.String(`log-level`, ``, `log level`)
+	flagAgentLogFile = flag.String(`agent-log`, ``, `agent log file`)
+
+	flagDocker = flag.Bool(`docker`, false, `run in docker`)
 
 	flagUpgrade = flag.Bool(`upgrade`, false, `upgrade agent`)
 
@@ -46,14 +49,20 @@ var (
 	flagCfgFile = flag.String("cfg", ``, `configure file`)
 	flagCfgDir  = flag.String("config-dir", ``, `sub configuration dir`)
 
+	flagCheckCfgDir = flag.String("verify-conf-dir", ``, `verify datakit conf.d`)
+
 	fRunAsConsole = flag.Bool("console", false, "run as console application (windows only)")
 
 	fService    = flag.String("service", "", "operate on the service (windows only)")
 	fInstallDir = flag.String("installdir", `C:\Program Files (x86)\Forethought\DataFlux EBA Agent`, "install directory")
+
+	fInputFilters = flag.String("input-filter", "", "filter the inputs to enable, separator is :")
 )
 
 var (
 	stop chan struct{}
+
+	inputFilters = []string{}
 
 	datakitConfig *config.Config
 )
@@ -61,8 +70,9 @@ var (
 func main() {
 
 	flag.Parse()
+	args := flag.Args()
 
-	if *flagVersion {
+	if *flagVersion || (len(args) > 0 && args[0] == "version") {
 		fmt.Printf(`Version:        %s
 Sha1:           %s
 Build At:       %s
@@ -71,16 +81,17 @@ Golang Version: %s
 		return
 	}
 
-	if *flagInstall != "" {
-		if err := doInstall(*flagInstall); err != nil {
-			os.Exit(-1)
-		}
-		return
-	}
+	if runtime.GOOS == "windows" {
 
-	if *fService != "" && runtime.GOOS == "windows" {
-		serviceCmd()
-		return
+		if *flagInstall != "" {
+			if err := doInstall(*flagInstall); err != nil {
+				os.Exit(-1)
+			}
+			return
+		} else if *fService != "" {
+			serviceCmd()
+			return
+		}
 	}
 
 	exepath, err := os.Executable()
@@ -97,21 +108,39 @@ Golang Version: %s
 	}
 	config.MainCfgPath = *flagCfgFile
 
-	if *flagInit {
-		if err := initialize(); err != nil {
+	if *flagAgentLogFile != "" {
+		config.AgentLogFile = *flagAgentLogFile
+	}
+
+	if *fInputFilters != "" {
+		inputFilters = strings.Split(":"+strings.TrimSpace(*fInputFilters)+":", ":")
+	}
+
+	if len(args) > 0 && args[0] == "input" {
+		//TODO:
+		return
+	}
+
+	switch {
+	case *flagInit:
+		if err := initialize(false); err != nil {
 			log.Fatalf("%s", err)
 		}
 		return
-	} else if *flagUpgrade {
 
-		if *flagCfgDir == "" {
-			*flagCfgDir = filepath.Join(config.ExecutableDir, "conf.d")
-		}
-
+	case *flagUpgrade:
 		config.InitTelegrafSamples()
 
 		if err = config.CreatePluginConfigs(*flagCfgDir, true); err != nil {
 			log.Fatalf("%s", err)
+		}
+		return
+
+	case *flagDocker:
+		if _, err := os.Stat(filepath.Join(config.ExecutableDir, fmt.Sprintf("%s.conf", config.ServiceName))); err != nil && os.IsNotExist(err) {
+			if err := initialize(true); err != nil {
+				log.Fatalf("%s", err)
+			}
 		}
 		return
 	}
@@ -137,7 +166,7 @@ Golang Version: %s
 
 	} else {
 		stop = make(chan struct{})
-		reloadLoop(stop)
+		reloadLoop(stop, inputFilters)
 	}
 }
 
@@ -151,7 +180,7 @@ func (p *program) Start(s winsvr.Service) error {
 
 func (p *program) run(s winsvr.Service) {
 	stop = make(chan struct{})
-	reloadLoop(stop)
+	reloadLoop(stop, inputFilters)
 }
 
 func (p *program) Stop(s winsvr.Service) error {
@@ -225,17 +254,13 @@ func serviceCmd() {
 	os.Exit(0)
 }
 
-func initialize() error {
+func initialize(reserveExist bool) error {
 	if *flagLogFile == "" {
 		*flagLogFile = filepath.Join(config.ExecutableDir, "datakit.log")
 	}
 
 	if *flagLogLevel == "" {
 		*flagLogLevel = "info"
-	}
-
-	if *flagCfgDir == "" {
-		*flagCfgDir = filepath.Join(config.ExecutableDir, "conf.d")
 	}
 
 	uid, err := uuid.NewV4()
@@ -257,10 +282,10 @@ func initialize() error {
 
 	config.InitTelegrafSamples()
 	config.CreateDataDir()
-	return config.CreatePluginConfigs(*flagCfgDir, false)
+	return config.CreatePluginConfigs(*flagCfgDir, reserveExist)
 }
 
-func reloadLoop(stop chan struct{}) {
+func reloadLoop(stop chan struct{}, inputFilters []string) {
 	reload := make(chan bool, 1)
 	reload <- true
 	for <-reload {
@@ -285,7 +310,7 @@ func reloadLoop(stop chan struct{}) {
 			}
 		}()
 
-		err := loadConfig(ctx)
+		err := loadConfig(ctx, inputFilters)
 		if err == nil {
 			err = runTelegraf(ctx)
 		}
@@ -304,18 +329,24 @@ func reloadLoop(stop chan struct{}) {
 	}
 }
 
-func loadConfig(ctx context.Context) error {
+func loadConfig(ctx context.Context, inputFilters []string) error {
 	c := config.NewConfig()
+	c.InputFilters = inputFilters
 
 	if err := c.LoadMainConfig(ctx, config.MainCfgPath); err != nil {
 		return err
+	}
+
+	logfile := c.MainCfg.Log
+	if *flagLogFile != "" {
+		logfile = *flagLogFile
 	}
 
 	logConfig := logger.LogConfig{
 		Debug:     (strings.ToLower(c.MainCfg.LogLevel) == "debug"),
 		Quiet:     false,
 		LogTarget: logger.LogTargetFile,
-		Logfile:   c.MainCfg.Log,
+		Logfile:   logfile,
 	}
 	logConfig.RotationMaxSize.Size = (20 << 10 << 10)
 	logger.SetupLogging(logConfig)
@@ -340,6 +371,9 @@ func runAgent(ctx context.Context) error {
 
 	pnames := []string{}
 	for _, ip := range datakitConfig.Inputs {
+		if ip.Config.Name == "mock" {
+			continue
+		}
 		pnames = append(pnames, ip.Config.Name)
 	}
 	log.Printf("[agent] avariable inputs: %s", strings.Join(pnames, ","))
