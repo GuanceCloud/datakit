decfb323be7f52070349c4abd7e8d0efbed775e8
leecha <22087646+leecha@users.noreply.github.com>
2020-04-16 14:02:56 +0800

    add etcd collection and fix mongodboploag bug
     [formerly f1006a636927bce399acf2ffd2cdd45e406aeeaa [formerly aae31c7231e041027d48ea653f4da8659ee3c96b]] [formerly fd62cb5fa2ceddcecc3e203907185902c5d25c64 [formerly 4cfd044a4114f34298cea4575b6f010f679a16ad] [formerly fd62cb5fa2ceddcecc3e203907185902c5d25c64 [formerly 4cfd044a4114f34298cea4575b6f010f679a16ad] [formerly fd62cb5fa2ceddcecc3e203907185902c5d25c64 [formerly 4cfd044a4114f34298cea4575b6f010f679a16ad] [formerly 056ee19d054c09737a8b5ab34adf29caa55893a3 [formerly 95f541af4f404a96157401775abc378f41f36e4b]]]] [formerly f1006a636927bce399acf2ffd2cdd45e406aeeaa [formerly aae31c7231e041027d48ea653f4da8659ee3c96b] [formerly f1006a636927bce399acf2ffd2cdd45e406aeeaa [formerly aae31c7231e041027d48ea653f4da8659ee3c96b] [formerly f1006a636927bce399acf2ffd2cdd45e406aeeaa [formerly aae31c7231e041027d48ea653f4da8659ee3c96b] [formerly 7c370531f53a129279c3cf14b653902c6946a8eb [formerly 1c7706ae523ec7f8ddb009d850d01afa56f1c5f4]]]]]]
    
    
    
    
    Former-commit-id: 061cb3e7e1af4b79e3637a8bd88691b33a268b06 [formerly c296912a169de39fee1ec6071dff33b8844b2de6]
    Former-commit-id: eca5f1ed2a97fa642386fd70894fe697bf946a91 [formerly 3e580739ad8fb94d3144371aa8c49241ab71adb4] [formerly 5d55f1e4affedf66763aca4d7c9c30618df18360 [formerly e29720d7ec9483b2b58c6b010a275f407098826b]]
    Former-commit-id: fcbf4f55a994b20df459d4c88c41b32a98fb8969 [formerly cfc29cf0cfbddd2f89eea399c7201b1848f21d97]
    Former-commit-id: 3ce99eeb00d9b9b4179183a20a7839b2cd10c499

---
+++ plugins/inputs/etcd/etcd_test.go
@@ -0,0 +1,35 @@
+package etcd
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestStart(t *testing.T) {
+
+	var e = Etcd{
+		Config: Config{
+			Subscribes: []Subscribe{
+				Subscribe{
+					EtcdHost: "10.100.64.106",
+					EtcdPort: 32379,
+					Cycle:    3,
+				},
+			},
+		},
+	}
+
+	e.wg = new(sync.WaitGroup)
+
+	for _, sub := range e.Config.Subscribes {
+		e.wg.Add(1)
+		fmt.Printf("%#v\n", sub)
+		stream := newStream(&sub, nil)
+		panic(stream.start(e.wg))
+	}
+
+	time.Sleep(10 * time.Second)
+
+}
