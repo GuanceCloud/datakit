05e721391adcd8c18cb7d454fd8d7c48529968eb
huangbo <huangbo@jiagouyun.com>
2020-04-16 17:45:49 +0800

    增加timezone变更采集器
     [formerly 9cfe8853e205a4123a11d43004584c824043c978 [formerly b1b3d7073131593eaaf59efa3726831ed269d48e] [formerly 7d13d1107b2d5dd73be59c765d15d81373dce050 [formerly 256a1dfb90dd4bbf465481b24f3d201bf07ab31e]] [formerly 7d13d1107b2d5dd73be59c765d15d81373dce050 [formerly 256a1dfb90dd4bbf465481b24f3d201bf07ab31e] [formerly 806bfa2e9453a173a9b180ed5f4bfe77a1347dab [formerly 786ac36ff87f28d20dc70a7c3628948fa34d3081]]]]
    
    
    
    Former-commit-id: 5caeae2b84f6de1e10ce1665a7be680769cb531c
    Former-commit-id: 0332db2e8b615efbd7c9f44fcfd96278126966ec [formerly 945a0c0f32a8470fa8879174d1d26b82be70f535]
    Former-commit-id: f1912eb48fac3b810bcdeea0ca289b2a55fd5c61

---
+++ plugins/inputs/timezone/timezone.go
@@ -0,0 +1,190 @@
+package timezone
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os/exec"
+	"runtime"
+	"time"
+	"strings"
+	"log"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/metric"
+	tzlog "github.com/siddontang/go-log/log"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+type Timezone struct {
+	Metricname string  `toml:"metric_name"`
+	Active     bool
+	Interval   int
+	Hostname   string
+}
+
+type TzInput struct {
+	Timezone
+}
+
+type TzOutput struct {
+	acc telegraf.Accumulator
+}
+
+type TzParams struct {
+	input  TzInput
+	output TzOutput
+}
+
+type TzLogWriter struct {
+	io.Writer
+}
+
+const (
+	defaultMetricName = "timezone"
+	defaultInterval   = 60
+)
+
+var (
+	timeZoneConfigSample = `### metric_name: the name of metric, default is "timezone".
+### active: whether to monitor timezone changes.
+### interval: monitor interval second, unit is second. The default value is 60.
+### hostname: If not specified, the UUID will be used.
+
+metric_name="timezone"
+active   = false
+interval = 60
+hostname = ""`
+)
+
+var (
+	ctx          context.Context
+	cfun         context.CancelFunc
+	stopChan     chan bool
+	prevTimezone string
+)
+
+func (t *Timezone) SampleConfig() string {
+	return timeZoneConfigSample
+}
+
+func (t *Timezone) Description() string {
+	return "Monitor timezone changes"
+}
+
+func (t *Timezone) Gather(telegraf.Accumulator) error {
+	return nil
+}
+
+func (t *Timezone) Start(acc telegraf.Accumulator) error {
+	setupLogger()
+
+	log.Printf("I! [timezone] start")
+	ctx, cfun = context.WithCancel(context.Background())
+
+	input := TzInput{*t}
+	if input.Interval <= 0 {
+		input.Interval = defaultInterval
+	}
+	if input.Metricname == "" {
+		input.Metricname = defaultMetricName
+	}
+
+	output := TzOutput{acc}
+
+	p := TzParams{input, output}
+	go p.gather(ctx)
+
+	stopChan = make(chan bool, 1)
+	return nil
+}
+
+func (t *Timezone) Stop() {
+	stopChan <- true
+	cfun()
+}
+
+func (p *TzParams) gather(ctx context.Context) {
+	for {
+		select {
+		case <-stopChan:
+			return
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		err := p.getMetrics()
+		if err != nil {
+			log.Printf("W! [timezone] %s", err.Error())
+		}
+
+		err = internal.SleepContext(ctx, time.Duration(p.input.Interval)*time.Second)
+		if err != nil {
+			log.Printf("W! [timezone] %s", err.Error())
+		}
+	}
+}
+
+func (p *TzParams) getMetrics() error {
+	tags := make(map[string]string)
+	fields := make(map[string]interface{})
+
+	timezone, err := getOsTimezone()
+	if err != nil {
+		return err
+	}
+	if timezone == prevTimezone {
+		return nil
+	}
+	prevTimezone = timezone
+
+	if p.input.Hostname != "" {
+		tags["host"] = p.input.Hostname
+	}
+	fields["tz"] = timezone
+
+	pointMetric, err := metric.New(p.input.Metricname, tags, fields, time.Now())
+	if err != nil {
+		return err
+	}
+
+	p.output.acc.AddMetric(pointMetric)
+	return nil
+}
+
+func getOsTimezone() (string, error) {
+	var outInfo bytes.Buffer
+	os := runtime.GOOS
+
+	if os == "linux" || os == "darwin" {
+		cmd := exec.Command("date", `+%Z`)
+		cmd.Stdout = &outInfo
+		cmd.Run()
+		return strings.Trim(outInfo.String(), "\n"), nil
+	} else if os == "windows" {
+		cmd := exec.Command("tzutil.exe", "/g")
+		cmd.Stdout = &outInfo
+		cmd.Run()
+		return outInfo.String(), nil
+	} else {
+		return "", fmt.Errorf("Os: %s unsuport get timezone", os)
+	}
+}
+
+func setupLogger() {
+	loghandler, _ := tzlog.NewStreamHandler(&TzLogWriter{})
+	logger := tzlog.New(loghandler, 0)
+	tzlog.SetLevel(tzlog.LevelDebug)
+	tzlog.SetDefaultLogger(logger)
+}
+
+func init() {
+	inputs.Add("timezone", func() telegraf.Input {
+		tz := &Timezone{}
+		return tz
+	})
+}
