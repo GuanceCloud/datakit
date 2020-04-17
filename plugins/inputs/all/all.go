cfb8b26185399ec4ea9c5bfa1bb6f6ef4e22c242
leecha <22087646+leecha@users.noreply.github.com>
2020-04-17 19:01:33 +0800

    fix lighttpd
     [formerly 968542304603c1304ba27ad14e619068fdf4ccbc [formerly fe45fe149adbaccbea11e83248dbf978667adf7b] [formerly 968542304603c1304ba27ad14e619068fdf4ccbc [formerly fe45fe149adbaccbea11e83248dbf978667adf7b] [formerly 55bf1fbfb8e7b73db83ae9b1be2da502f4a076df [formerly 4d0f19f03d64317912f7500500b5115cada4c9ae]]]]
    
    
    
    Former-commit-id: 42fb0d60a1212ab71d61928243da8d2b327dce11
    Former-commit-id: d8a462ed50f0b6a061b5d73f3615e52c2dbaa903 [formerly 32f190691b080084ef1688f5286ae16b29001e7f]
    Former-commit-id: 33b8647becb7940eb064afa15aed4dd89c8e5145

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -14,6 +14,7 @@ import (
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/dataclean"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/etcd"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/httpstat"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/lighttpd"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mock"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mongodboplog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/netPacket"
