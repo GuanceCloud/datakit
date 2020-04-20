059bad9dec67f95d0589d8cb66ac5f253649f231
liushaobo <liushaobo@jiagouyun.com>
2020-04-20 19:41:05 +0800

    提交代码
     [formerly 514d88738485bbb00c8cdd29acb2f7b85ec86c8d [formerly 570586262f165aaf4977404d688d41ebd0622829]] [formerly 23581cadc86a88cad3cd220a374559f414079b3d [formerly fde100d5d029d9f0b9ca3c42278fdc45289b5e7e] [formerly 23581cadc86a88cad3cd220a374559f414079b3d [formerly fde100d5d029d9f0b9ca3c42278fdc45289b5e7e] [formerly 23581cadc86a88cad3cd220a374559f414079b3d [formerly fde100d5d029d9f0b9ca3c42278fdc45289b5e7e] [formerly 4afc4d3ff96583bf8168d84f113b88f7ddb920ca [formerly 7c0aa58b656b4ed9624e2055b5eed356af14f6e0]]]] [formerly 514d88738485bbb00c8cdd29acb2f7b85ec86c8d [formerly 570586262f165aaf4977404d688d41ebd0622829] [formerly 514d88738485bbb00c8cdd29acb2f7b85ec86c8d [formerly 570586262f165aaf4977404d688d41ebd0622829] [formerly 514d88738485bbb00c8cdd29acb2f7b85ec86c8d [formerly 570586262f165aaf4977404d688d41ebd0622829] [formerly c46177fd9686c8041e24d3f37660fbc2d9712070 [formerly 955ca00205b32ad22f6d5dc5d4fe3e2337f32e65]]]]]]
    
    
    
    
    Former-commit-id: 58d03fdf0038acf992aaa876a9ad455814c3604e [formerly 7c34ac3b3d8bda99ac7018f402eceffbec331e71]
    Former-commit-id: 574f2f7a706b78a1c55901daa8bb87fd549e8f82 [formerly dbefecca81184bc16b71a1f57f9f10aab07d079e] [formerly 030f68da9ad974ffbdb68e8eb3d1d4f271991a40 [formerly f5a5835238c22e7420a0a2789afc088f7f1bf625]]
    Former-commit-id: 1b50320e67e409cca8bba41a8a905b6e16eed114 [formerly b8706a3bd4c397d704c2b3b4ca5578225d277347]
    Former-commit-id: 7a15385e33cc57b09b2e21294f256597434e30d1

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -7,6 +7,7 @@ import (
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/aliyuncost"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/aliyunlog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/aliyunprice"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/aliyunrdsslowlog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/apachelog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/binlog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/etcd"
