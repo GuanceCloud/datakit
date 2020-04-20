927c4370c62c56c8b7239b801a5b3937b9ce56d1
huangbo <huangbo@jiagouyun.com>
2020-04-20 13:14:57 +0800

    增加statsd采集器
     [formerly bd3606da4a7bfd0feabb1b37a853a79af987bfd2 [formerly a41b3e607c5be4a257db82f4b7c0e7a83de3ebea] [formerly af18435371b14e66aa993481bab9f6bf945db090 [formerly c8ba924c6dda7f74a7fb7585880cab8bb5bc1428]] [formerly af18435371b14e66aa993481bab9f6bf945db090 [formerly c8ba924c6dda7f74a7fb7585880cab8bb5bc1428] [formerly ff73bcd2ab21c4dee0d6168eec9340bbaf16b59c [formerly 1c603e7bf298346d46454aa2e0b4f387117d051f]]]]
    
    
    
    Former-commit-id: 0706a9a91054f8574d51350a92e54ab54f88f468
    Former-commit-id: fe477404d3f33dfd37c9c19fb393326d13446272 [formerly 54cd5414885e359ab6a5f343a232674764d51384]
    Former-commit-id: 5f7d6bfc5b09f15137bb490ade7fefb264feb695

---
+++ plugins/inputs/statsd/statsd.go
@@ -0,0 +1,149 @@
+package statsd
+
+import (
+	"context"
+	"io"
+	"log"
+	"regexp"
+
+	"github.com/influxdata/telegraf"
+	sdlog "github.com/siddontang/go-log/log"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+type Target struct {
+	Interval int
+	Active   bool
+	Host     string
+}
+
+type StatsD struct {
+	MetricName string `toml:"metric_name"`
+	Targets    []Target
+}
+
+type StatsdInput struct {
+	MetricName string
+	Interval   int
+	Host       string
+}
+
+type StatsdOutput struct {
+	acc telegraf.Accumulator
+}
+
+type StatsdParams struct {
+	input  StatsdInput
+	output StatsdOutput
+}
+
+type sdLogWriter struct {
+	io.Writer
+}
+
+const statsdConfigSample = `### metric_name: the name of metric, default is "statsd".
+### You need to configure an [[targets]] for each statsd service to be monitored.
+### interval: monitor interval second, unit is second. The default value is 60.
+### active: whether to monitor statsd.
+### host: statsd service ip:port, if "127.0.0.1", default port is 8126.
+
+metric_name="statsd"
+[[targets]]
+	interval = 60
+	active   = false
+	host     = "127.0.0.1:8126"
+
+#[[targets]]
+#	interval = 60
+#	active   = false
+#	host     = "127.0.0.1:8126"
+`
+
+var (
+	ctx  context.Context
+	cfun context.CancelFunc
+	activeTargets = 0
+	stopChan chan bool
+)
+
+const (
+	defaultMetricName = "statsd"
+	defaultInterval   = 60
+)
+
+func (t *StatsD) SampleConfig() string {
+	return statsdConfigSample
+}
+
+func (t *StatsD) Description() string {
+	return "Monitor StatsD Service"
+}
+
+func (t *StatsD) Gather(telegraf.Accumulator) error {
+	return nil
+}
+
+func (t *StatsD) Start(acc telegraf.Accumulator) error {
+	setupLogger()
+	reg, _ := regexp.Compile(`:\d{1,5}$`)
+	log.Printf("I! [statsd] start")
+	ctx, cfun = context.WithCancel(context.Background())
+
+	if t.MetricName == "" {
+		t.MetricName = defaultMetricName
+	}
+
+	activeCnt := 0
+	for _, target := range t.Targets {
+		if target.Active == false || target.Host == "" {
+			continue
+		}
+
+		input := StatsdInput{
+			t.MetricName,
+			target.Interval,
+			target.Host,
+		}
+
+		if input.Interval <= 0 {
+			input.Interval = defaultInterval
+		}
+
+		if !reg.MatchString(target.Host) {
+			target.Host += ":8126"
+		}
+
+		output := StatsdOutput{acc}
+
+		p := StatsdParams{input, output}
+		go p.gather(ctx)
+
+		activeCnt += 1
+	}
+
+	activeTargets = activeCnt
+	stopChan = make(chan bool, activeTargets)
+	return nil
+}
+
+func (t *StatsD) Stop() {
+	for i:=0; i<activeTargets; i++ {
+		stopChan <- true
+	}
+	cfun()
+}
+
+func setupLogger() {
+	loghandler, _ := sdlog.NewStreamHandler(&sdLogWriter{})
+	sdlogger := sdlog.New(loghandler, 0)
+	sdlog.SetLevel(sdlog.LevelDebug)
+	sdlog.SetDefaultLogger(sdlogger)
+}
+
+func init() {
+	inputs.Add("statsd", func() telegraf.Input {
+		p := &StatsD{}
+		return p
+	})
+}
