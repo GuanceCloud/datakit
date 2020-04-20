fcf5e3a9a34c7470574dc8d869b5653d5640549d
liushaobo <liushaobo@jiagouyun.com>
2020-04-20 15:31:34 +0800

    提价代码
    Former-commit-id: ec3e78ee0abfbe54203226a7985ff8d25c719ed9 [formerly 713c037b0c0b7925a21302c07799a03914ca72af] [formerly d6234b596cf6e5d5ebd8bb5442f5a3edeb22c6d7 [formerly 877853abb741abc22aa2a84cecce93ad1b88633b]]
    Former-commit-id: 882a1ecf50c476b6c7fb60a4d68321f56413188e [formerly e0d22ccb12b520c04e7d94e5a3004920ecd7b9c6]
    Former-commit-id: d72cbb71a0ff4e28375a298013028f8d466ef7d0

---
+++ plugins/inputs/aliyuncdn/cdn.go
@@ -0,0 +1,232 @@
+package aliyuncdn
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/cdn"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/selfstat"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/models"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+type AliyunCDN struct {
+	CDN              []*CDN `toml:"cdn"`
+	runningInstances []*RunningInstance
+	ctx              context.Context
+	cancelFun        context.CancelFunc
+	accumulator      telegraf.Accumulator
+	logger           *models.Logger
+}
+
+type RunningInstance struct {
+	cfg             *CDN
+	agent           *AliyunCDN
+	logger          *models.Logger
+	runningProjects []*RunningProject
+	metricName      string
+	client          *cdn.Client
+	domains         []string
+}
+
+func (_ *AliyunCDN) SampleConfig() string {
+	return aliyunCDNConfigSample
+}
+
+func (_ *AliyunCDN) Description() string {
+	return ""
+}
+
+func (_ *AliyunCDN) Gather(telegraf.Accumulator) error {
+	return nil
+}
+
+func (c *AliyunCDN) Start(acc telegraf.Accumulator) error {
+	if len(c.CDN) == 0 {
+		c.logger.Warnf("no configuration found")
+		return nil
+	}
+
+	c.logger = &models.Logger{
+		Errs: selfstat.Register("gather", "errors", nil),
+		Name: `aliyuncdn`,
+	}
+
+	c.logger.Infof("aliyun cdn start...")
+
+	c.accumulator = acc
+
+	for _, instCfg := range c.CDN {
+		r := &RunningInstance{
+			cfg:    instCfg,
+			agent:  c,
+			logger: c.logger,
+		}
+
+		c.runningInstances = append(c.runningInstances, r)
+
+		go r.run(c.ctx)
+	}
+
+	return nil
+}
+
+func (cdn *AliyunCDN) Stop() {
+	cdn.cancelFun()
+}
+
+func (r *RunningInstance) run(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return context.Canceled
+		default:
+		}
+
+		// check 配置
+		if err := CheckCfg(r.cfg); err != nil {
+			r.logger.Errorf("config error %v", err)
+			return nil
+		}
+
+		// 域名采集
+		r.domains = r.cfg.DomainName
+		cli, err := cdn.NewClientWithAccessKey(r.cfg.RegionID, r.cfg.AccessKeyID, r.cfg.AccessKeySecret)
+		if err != nil {
+			r.logger.Errorf("create client failed, %s", err)
+			return err
+		}
+
+		r.client = cli
+
+		fmt.Println("域名采集=========>", r.domains)
+		if len(r.domains) == 0 {
+			r.domains = r.getDomain(r.cfg.Summary.MetricName)
+		}
+
+		for _, action := range r.cfg.Metric.Actions {
+			fmt.Println("action =====> action", action)
+			go r.exec(ctx, action)
+		}
+
+		internal.SleepContext(ctx, 10*time.Second)
+	}
+
+	return nil
+}
+
+func (r *RunningInstance) getDomain(metricName string) []string {
+	var pageNumber = 1
+	var pageSize = 50
+	var domains = []string{}
+
+	if metricName == "" {
+		metricName = "aliyun_cdn_summary"
+	}
+
+	for {
+		request := cdn.CreateDescribeUserDomainsRequest()
+		request.RegionId = r.cfg.RegionID
+		request.Scheme = "https"
+		request.PageSize = requests.NewInteger(50)
+		request.PageNumber = requests.NewInteger(1)
+
+		response, err := r.client.DescribeUserDomains(request)
+		if err != nil {
+			r.logger.Errorf("[DescribeUserDomainsRequest] failed, %v", err.Error())
+		}
+
+		for _, item := range response.Domains.PageData {
+			if item.DomainStatus == "online" {
+				domains = append(domains, item.Cname)
+			}
+
+			for _, point := range item.Sources.Source {
+				tags := map[string]string{}
+				fields := map[string]interface{}{}
+
+				tags["cdnType"] = item.CdnType
+				tags["cname"] = item.Cname
+				tags["domainName"] = item.DomainName
+				tags["domainStatus"] = item.DomainStatus
+				tags["sslProtocol"] = item.SslProtocol
+
+				fields["gmtCreated"] = RFC3339(item.GmtCreated)
+				fields["gmtModified"] = RFC3339(item.GmtModified)
+				fields["resourceGroupId"] = item.ResourceGroupId
+				fields["description"] = item.Description
+				fields["content"] = point.Content
+				fields["port"] = point.Port
+				fields["priority"] = ConvertToNum(point.Priority)
+				fields["type"] = point.Type
+				fields["weight"] = ConvertToNum(point.Weight)
+
+				r.agent.accumulator.AddFields(metricName, fields, tags)
+			}
+		}
+
+		total := response.TotalCount
+		if int64(pageNumber*pageSize) >= total {
+			break
+		}
+	}
+
+	return domains
+}
+
+func (r *RunningInstance) exec(ctx context.Context, action string) error {
+	fmt.Println("开始执行", r.domains)
+	et := time.Now()
+	st := et.Add(-time.Minute * 10)
+
+	p := &RunningProject{
+		accumulator: r.agent.accumulator,
+		cfg:         r.cfg.Metric,
+		client:      r.client,
+		logger:      r.logger,
+		domain:      r.cfg.DomainName,
+		startTime:   unixTimeStrISO8601(st),
+		endTime:     unixTimeStrISO8601(et),
+	}
+
+	// metricname
+	p.metricName = r.cfg.Metric.MetricName
+	if p.metricName == "" {
+		p.metricName = "aliyun_cdn_metrics"
+	}
+
+	go p.commond(action)
+
+	return nil
+}
+
+func (run *RunningProject) commond(action string) {
+	switch action {
+	case "describeDomainBpsData":
+		run.DescribeDomainBpsData()
+	case "describeDomainTrafficData":
+		run.DescribeDomainTrafficData()
+	case "describeDomainHitRateData":
+		run.DescribeDomainHitRateData()
+	case "describeDomainReqHitRateData":
+		run.DescribeDomainReqHitRateData()
+	case "describeDomainSrcBpsData":
+		run.DescribeDomainSrcBpsData()
+	case "describeDomainSrcTrafficData":
+		run.DescribeDomainSrcTrafficData()
+	case "describeDomainUvData":
+		run.DescribeDomainUvData()
+	}
+}
+
+func init() {
+	inputs.Add("aliyuncdn", func() telegraf.Input {
+		ac := &AliyunCDN{}
+		ac.ctx, ac.cancelFun = context.WithCancel(context.Background())
+		return ac
+	})
+}
