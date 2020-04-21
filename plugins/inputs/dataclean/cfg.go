89d9ba541661094ffe4f69a7b94bfb00dd533999
wuqinchao <wuqinchao@jiagouyun.com>
2020-04-21 15:31:27 +0800

    .
    Former-commit-id: b5b66f92164446a9e45828f2d10fbf0100f68390 [formerly 1fef7f2fdc3c88f1e8c0f820b22d15427a88d392] [formerly d95fba66a70e0d4f6242bdb73de6f88de07ad53d [formerly d2402f36c217e68c170191673fda0b6cc6dc478a]] [formerly 971536b6a01c0885369a0718d3b8095ec1df511f [formerly c85e55bcefabdbfb91a798464432c1b5a6d8aa37] [formerly 352b9ffae10c29a9ce6185b52b2f70d53ac650b3 [formerly 9e1dea2f107a4ba4e39ef543519c84d8f550f0d9]]]
    Former-commit-id: 971536b6a01c0885369a0718d3b8095ec1df511f [formerly c85e55bcefabdbfb91a798464432c1b5a6d8aa37]
    Former-commit-id: bd5ab04515eed1f6d456814ec437b44f5613aea1 [formerly c9d7419cb06dd1244b755fd187720e6e7f3682ab]
    Former-commit-id: 2d417dd3f19b9fbf116e5c772895e69a74d8bd1b

--- plugins/inputs/dataclean/cfg.go
+++ plugins/inputs/dataclean/cfg.go
@@ -7,22 +7,18 @@ var (
 #bind_addr = '0.0.0.0:9528'
 #gin_log = 'gin.log'
 #lua_worker = 4
-#enable_config_api = true
-#cfg_api_pwd = 'xxx'
 
 #[[global_lua]]
 #path = 'a.lua'
-#circle = '* */1 * * *'
+#circle = '*/10 * * * *'
 
 #[[routes_config]]
 #name = 'demo'
 #disable_type_check = false
 #disable_lua = false
-#ak_open = false
 
 # [[routes_config.lua]]
 #  path = 'demo.lua'
-#  circle = '* */5 * * *'	
 `
 )
 
@@ -36,5 +32,4 @@ type RouteConfig struct {
 	Lua              []LuaConfig `toml:"lua,omitempty"`
 	DisableTypeCheck bool        `toml:"disable_type_check"`
 	DisableLua       bool        `toml:"disable_lua"`
-	AkOpen           bool        `toml:"ak_open"`
 }
