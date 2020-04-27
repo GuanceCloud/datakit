6fc7dcdb6a6258e6acba22930a95005371f57d4d
coanor <coanor@gmail.com>
2020-04-27 03:25:58 +0000

    update: 	[*] update install powershell
     [formerly dc3a477d5f93e946b6108f58251fa1b9344c2c22 [formerly 5740e1b3d0029ed0761c4b4ce309b1cf692b9b67] [formerly dc3a477d5f93e946b6108f58251fa1b9344c2c22 [formerly 5740e1b3d0029ed0761c4b4ce309b1cf692b9b67] [formerly 00a2a01738770a8ee185de69a9fdc7036a2d9784 [formerly 9a39794b072490d01b5dd3e4f319d0cb73fe085c]]]]
    
    
    
    Former-commit-id: bf6b3c560bb1deb2bad0ee08ef32d6436441f3d9
    Former-commit-id: a7c6abaedf615f50b89d5104ae07322bfd351001 [formerly df51dcd5ba6e083a439c8dd5bfc25209c8d2e0fb]
    Former-commit-id: 7542c608829f767ab658fd4baac66e1a1842e0dd

--- install.template.ps1
+++ install.template.ps1
@@ -11,61 +11,46 @@ if ([Environment]::Is64BitProcess -ne [Environment]::Is64BitOperatingSystem) {
 
 # get dataway host from command line env
 $dw=$env:dw
-$upgrade=$env:upgrade
 
 if ($dw -eq $null) {
-	Write-Host 'dataway not set, set it like $env:dw="<like:1.2.3.4:9528>"' -ForegroundColor Red
-	exit -1
-} else {
-	Write-Host $("* Set dataway to {0:C}" -f $dw) -ForegroundColor Green
-}
+	Write-Host -NoNewline "* Please set DataWay IP:Port > " -ForegroundColor green
+	$dw=Read-Host
+} 
+
+Write-Host $("* DataWay set to http://{0:c}/v1/write/metrics" -f $dw) -ForegroundColor Green
+
+$upgrade=$env:upgrade
 
 Write-Host $("* Downloading installer-windows-amd64-{0:C}..." -f $version) -ForegroundColor Green
 Invoke-WebRequest -Uri $download_installer_from -OutFile "dk-installer.exe"
 
 if (Test-Path datakit.tar.gz) {
-	Write-Hosta '* Skip download datakit.tar.gz' -ForegroundColor Green
+	Write-Host '* Skip download datakit.tar.gz' -ForegroundColor Green
 } else {
 	Write-Host $("* Downloading datakit-windows-amd64-{0:C}..." -f $version) -ForegroundColor Green
 	Invoke-WebRequest -Uri $download_datakit_from -OutFile "datakit.tar.gz"
 }
 
-$args = @("-dataway", $dw, "-install-dir", $install_dir, "-gzpath", "datakit.tar.gz", "-install-log", "dk-install.log")
+# stop agent if exists (BUG: sometimes the agent process will not terminate after service stopped)
+$agent=Get-Process agent -ErrorAction SilentlyContinue
+if ($agent) {
+	Write-Host $("* Terminate agent ..." -f $version) -ForegroundColor Yellow
+	$agent | Stop-Process -Force # terminate it
+}
+Remove-Variable $agent
+
 if ($upgrade -eq 1) {
 	Write-Host $("* Upgrading to datakit-windows-amd64-{0:C}..." -f $version) -ForegroundColor Green
-	$args = @("-gzpath", "datakit.tar.gz", "-upgrade", "-install-log", "dk-install.log")
+	.\dk-installer.exe -gzpath datakit.tar.gz -upgrade
 } else {
 	Write-Host $("* Installing datakit-windows-amd64-{0:C}..." -f $version) -ForegroundColor Green
+	.\dk-installer.exe -dataway $dw -install-dir $install_dir -gzpath datakit.tar.gz
 }
 
-# Start installer.exe to complete the datakit installation
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = "dk-installer.exe"
-$pinfo.RedirectStandardError = $true
-$pinfo.RedirectStandardOutput = $true
-$pinfo.UseShellExecute = $false
-$pinfo.Arguments = $args
-$p = New-Object System.Diagnostics.Process
-$p.StartInfo = $pinfo
-$p.Start() | Out-Null
-#Do Other Stuff Here....
-$p.WaitForExit()
-
-if ($p.ExitCode -eq 0) {
-	write-host $("* Install datakit({0:C}) ok" -f $version) -ForegroundColor Green
-} else {
-	write-host $("* Install datakit({0:C}) failed" -f $version) -ForegroundColor Red
-}
-
-#$proc = Start-Process -Filepath "dk-installer.exe" -windowstyle Hidden -ArgumentList $args -PassThru -Wait
-#
-#if ($proc.ExitCode -eq 1) {
-#	write-host $("install datakit({0:C}) ok" -f $version) -ForegroundColor Green
-#} else {
-#	write-host $("install datakit({0:C}) failed" -f $version) -ForegroundColor Red
-#}
-
-# Remove-Item -Force "dk-installer.exe" -ErrorAction Ignore
+Remove-Item -Force "dk-installer.exe" -ErrorAction Ignore
+Remove-Item -Force "datakit.tar.gz" -ErrorAction Ignore
 
 # install script:
 # $env:dw="http://<dataway-ip:port>/v1/write/metrics"; powershell -exec bypass -c "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('https://{{.DownloadAddr}}/install.ps1')|iex"
+# upgrade script:
+# $env:upgrade=1; powershell -exec bypass -c "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('https://{{.DownloadAddr}}/install.ps1')|iex"
