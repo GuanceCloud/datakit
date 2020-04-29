0b82cf60d9b6d2d9da68bfd9819ed4a04e0b65dd
coanor <coanor@gmail.com>
2020-04-29 14:44:36 +0800

    update: 	[+] add build for 32bit arch(with part of collectors disabled)
    Former-commit-id: 00ac52fb14d9fe7aa7c06922d9a2ebe48b9d767e [formerly a4a3abb2f792b6b39ef89dd36c1ebd5527958aa5] [formerly 6a44fb0563f5be555d642327e599f262ac58b058 [formerly f77b4101e3e5a03c34209f7051baeda441433812]] [formerly a8094ce9fcadcc5b5e110a02cd4056e927be10f4 [formerly cd9cf483674be1c15ac9361165d1738d26fea904] [formerly 04ae220e966b97546be94c4cd0c33051b0176b7b [formerly d70e6d50c551930375d40c7a3cd0c66a2a343749]]]
    Former-commit-id: a8094ce9fcadcc5b5e110a02cd4056e927be10f4 [formerly cd9cf483674be1c15ac9361165d1738d26fea904]
    Former-commit-id: be0fef2fc0abd66da94c61f737542c9d2da5d03d [formerly dd185ad0a1a615e61f2c939175db06c76343d2ec]
    Former-commit-id: 298e1acafff0226ec6d0cd10608987174dfbe1ff

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -10,9 +10,6 @@ import (
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/aliyunrdsslowlog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/apachelog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/awsbill"
-
-	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/baiduIndex"
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/binlog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/coredns"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/dataclean"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/etcd"
@@ -21,18 +18,24 @@ import (
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mock"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mongodboplog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mysqlmonitor"
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/netPacket"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/nginxlog"
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/oraclemonitor"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/prometheus"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/replication"
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/scanport"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/self"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/ssh"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/statsd"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/tencentcms"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/timezone"
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/tracerouter"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/ucmon"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/zabbix"
+
+
+	// 32bit disabled, only 64 bit available
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/binlog"
+
+	// with dll/so dependencies, and also 32bit disabled
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/oraclemonitor"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/scanport"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/tracerouter"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/netPacket"
 )
