1e86e9a7a87e95daeda2b697c4f130fc83caa4d7
coanor <coanor@gmail.com>
2020-04-30 11:34:48 +0000

    update: 	[*] refact installer.go and datakit
    Former-commit-id: 4063fa53e985f5f389248a736d1bc39486857ac9 [formerly 35355cb395c5141e0337a7bdfb8c79aea4594b1e]
    Former-commit-id: d2e8c52ef7d96144ba1494cffa45c7cd62c54d4e

--- run/run.go
+++ run/run.go
@@ -30,17 +30,7 @@ func NewAgent(config *config.Config) (*Agent, error) {
 	return a, nil
 }
 
-func (a *Agent) Run(ctx context.Context) error {
-
-	if ctx.Err() != nil {
-		return ctx.Err()
-	}
-
-	select {
-	case <-ctx.Done():
-		return context.Canceled
-	default:
-	}
+func (a *Agent) Run() error {
 
 	log.Printf("Initializing plugins")
 	err := a.initPlugins()
@@ -57,21 +47,21 @@ func (a *Agent) Run(ctx context.Context) error {
 	for _, p := range a.outputsMgr.Outputs {
 		outputsNames = append(outputsNames, p.Config.Name)
 	}
-	log.Printf("avariable outputs: %s", strings.Join(outputsNames, ","))
+
+	log.Printf("available outputs: %s", strings.Join(outputsNames, ","))
 
 	log.Printf("Connecting outputs")
 	err = a.outputsMgr.Init()
 	if err != nil {
 		return err
 	}
+
 	err = a.outputsMgr.ConnectOutputs(ctx)
 	if err != nil {
 		return err
 	}
 
 	inputC := make(chan telegraf.Metric, 100)
-	//procC := make(chan telegraf.Metric, 100)
-	//outputC := make(chan telegraf.Metric, 100)
 
 	startTime := time.Now()
 
@@ -115,8 +105,11 @@ func (a *Agent) Run(ctx context.Context) error {
 		go func(src chan telegraf.Metric) {
 			defer wg.Done()
 
-			err := a.outputsMgr.Start(src, startTime, a.Config.MainCfg.FlushInterval.Duration, a.Config.MainCfg.FlushJitter.Duration, a.Config.MainCfg.RoundInterval)
-			if err != nil {
+			if err := a.outputsMgr.Start(src,
+				startTime,
+				a.Config.MainCfg.FlushInterval.Duration,
+				a.Config.MainCfg.FlushJitter.Duration,
+				a.Config.MainCfg.RoundInterval); err != nil {
 				log.Printf("E! Error running outputs: %v", err)
 			}
 
