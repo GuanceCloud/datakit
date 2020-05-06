48d29981d00e69a28494cdad111adbe926abd92e
liushaobo <liushaobo@jiagouyun.com>
2020-05-06 17:05:11 +0800

    提交代码
     [formerly ab75fc5cbe954b0f6792fa3c3aeab5083d4e8c1b [formerly c7678fbef7b7b122666eb0dd2c358e97a9dee877]] [formerly 28c0d81016cc00fd6f59b6cf0493db864dad59a7 [formerly 521a5a84a6d38bd2fe5a4ceafc8b7680d2b44110] [formerly 28c0d81016cc00fd6f59b6cf0493db864dad59a7 [formerly 521a5a84a6d38bd2fe5a4ceafc8b7680d2b44110] [formerly 28c0d81016cc00fd6f59b6cf0493db864dad59a7 [formerly 521a5a84a6d38bd2fe5a4ceafc8b7680d2b44110] [formerly a03dec56797563039fe2e0d9c1c72072bf716d76 [formerly 31254b7a39046171ccfd915fabc84e5dcadc37aa]]]] [formerly ab75fc5cbe954b0f6792fa3c3aeab5083d4e8c1b [formerly c7678fbef7b7b122666eb0dd2c358e97a9dee877] [formerly ab75fc5cbe954b0f6792fa3c3aeab5083d4e8c1b [formerly c7678fbef7b7b122666eb0dd2c358e97a9dee877] [formerly ab75fc5cbe954b0f6792fa3c3aeab5083d4e8c1b [formerly c7678fbef7b7b122666eb0dd2c358e97a9dee877] [formerly e13502e330e236679a498ede9d346de4e6d0c0d0 [formerly 7206dc61086683f29d9200358709830abf44dcb9]]]]]]
    
    
    
    
    Former-commit-id: e7a5bf422fe158bf7560e6773d6fa41d67d38705 [formerly aa92605e0d7bea7cc50d70166dc5d7a2bde2684a]
    Former-commit-id: cdb15f1f25b35602756cdbae5ca90822bec545bf [formerly 7eedbc1f48b5fb6dec356343d1b015aa7def070a] [formerly ed31947afadb3f3460bddcf787eefc36c51d713a [formerly 935d9f18926b6d6b1d65227726fbf7172b8949cd]]
    Former-commit-id: a59e5444fed2530584c5274159205efc851c2458 [formerly db0f17a18354e0b178f7c043e51b46028c6385ed]
    Former-commit-id: 57e209a1f28e9b9e8805225bea8b63781e042c17

--- plugins/inputs/aliyunddos/ddos.go
+++ plugins/inputs/aliyunddos/ddos.go
@@ -17,6 +17,21 @@ import (
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
+var regions = []string{
+	"cn-hangzhou",
+	"ap-southeast-3",
+}
+
+var regions2 = []string{
+	"cn-hangzhou",
+	"ap-southeast-1",
+}
+
+var regions3 = []string{
+	"cn-hangzhou",
+	"cn-hongkong",
+}
+
 type AliyunDDoS struct {
 	DDoS        []*DDoS
 	ctx         context.Context
@@ -53,6 +68,7 @@ func (_ *AliyunDDoS) Init() error {
 }
 
 func (a *AliyunDDoS) Start(acc telegraf.Accumulator) error {
+	fmt.Println("======ddos采集 start")
 	a.logger = &models.Logger{
 		Name: `aliyunddos`,
 	}
@@ -120,56 +136,81 @@ func (r *runningInstance) run(ctx context.Context) error {
 		default:
 		}
 
+		fmt.Println("======ddos采集")
+
 		r.command()
 
 		internal.SleepContext(ctx, r.cfg.Interval.Duration)
 	}
 }
 
-func (r *runningInstance) getInstance() error {
-	// var pageNumber = 1
-	// var pageSize = 50
+func (r *runningInstance) getInstance(region string) error {
+	var pageNumber = 1
+	var pageSize = 50
 
-	request := requests.NewCommonRequest()
-	request.Method = "POST"
-	request.Scheme = "https" // https | http
-	request.Domain = "ddoscoo.cn-hangzhou.aliyuncs.com"
-	request.Version = "2020-01-01"
-	request.ApiName = "DescribeInstances"
+	for {
+		request := requests.NewCommonRequest()
+		request.Method = "POST"
+		request.Scheme = "https" // https | http
+		request.Domain = "ddoscoo.cn-hangzhou.aliyuncs.com"
+		request.Version = "2020-01-01"
+		request.ApiName = "DescribeInstances"
 
-	request.QueryParams["RegionId"] = "cn-hangzhou"
-	request.QueryParams["PageSize"] = "10"
-	request.QueryParams["PageNumber"] = "1"
+		request.QueryParams["RegionId"] = region
+		request.QueryParams["PageSize"] = fmt.Sprintf("%d", pageSize)
+		request.QueryParams["PageNumber"] = fmt.Sprintf("%d", pageNumber)
 
-	response, err := r.client.ProcessCommonRequest(request)
-	if err != nil {
-		r.logger.Error("instance failed")
-		return err
+		response, err := r.client.ProcessCommonRequest(request)
+		if err != nil {
+			r.logger.Error("instance failed")
+			return err
+		}
+
+		data := response.GetHttpContentString()
+		instanceArr := gjson.Parse(data).Get("InstanceIds").Array()
+
+		for _, item := range instanceArr {
+			tags := map[string]string{}
+			fields := map[string]interface{}{}
+
+			tags["edition"] = item.Get("Edition").String()
+			tags["product"] = "ddos"
+			tags["action"] = "getInstance"
+			tags["region"] = region
+
+			fields["instanceId"] = item.Get("InstanceId").String()
+			fields["remark"] = item.Get("Remark").String()
+
+			go r.describeInstanceDetails(item.Get("InstanceId").String(), region)
+			go r.describeInstanceStatistics(item.Get("InstanceId").String(), region)
+		}
+
+		total := gjson.Parse(data).Get("TotalCount").Int()
+		if int64(pageNumber*pageSize) >= total {
+			break
+		}
+
+		pageNumber = pageNumber + 1
 	}
-
-	data := response.GetHttpContentString()
-	instanceArr := gjson.Parse(data).Get("InstanceIds").Array()
-
-	for _, item := range instanceArr {
-		tags := map[string]string{}
-		fields := map[string]interface{}{}
-
-		tags["edition"] = item.Get("Edition").String()
-		tags["product"] = "ddos"
-		tags["action"] = ""
-
-		fields["instanceId"] = item.Get("InstanceId").String()
-		fields["remark"] = item.Get("Remark").String()
-	}
-
 	return nil
 }
 
 func (r *runningInstance) command() {
-	r.getInstance()
+	for _, region := range regions {
+		go r.getInstance(region)
+	}
+
+	for _, region := range regions2 {
+		go r.describeWebRules(region)
+		go r.describeNetworkRules(region)
+	}
+
+	for _, region := range regions3 {
+		go r.describePayInfo(region)
+	}
 }
 
-func (r *runningInstance) describeInstanceDetails(instanceID string) error {
+func (r *runningInstance) describeInstanceDetails(instanceID, region string) error {
 	request := requests.NewCommonRequest()
 	request.Method = "POST"
 	request.Scheme = "https"
@@ -177,7 +218,7 @@ func (r *runningInstance) describeInstanceDetails(instanceID string) error {
 	request.Version = "2020-01-01"
 	request.ApiName = "DescribeInstanceDetails"
 
-	request.QueryParams["RegionId"] = "cn-hangzhou"
+	request.QueryParams["RegionId"] = region
 	request.QueryParams["InstanceIds.1"] = instanceID
 
 	response, err := r.client.ProcessCommonRequest(request)
@@ -196,6 +237,7 @@ func (r *runningInstance) describeInstanceDetails(instanceID string) error {
 		tags["edition"] = item.Get("Edition").String()
 		tags["product"] = "ddos"
 		tags["action"] = "describeInstanceDetails"
+		tags["region"] = region
 
 		eipStatus := []string{}
 		eip := []string{}
@@ -208,12 +250,13 @@ func (r *runningInstance) describeInstanceDetails(instanceID string) error {
 		fields["eip"] = strings.Join(eip, "\\")
 		fields["line"] = item.Get("line").String()
 		fields["instanceId"] = item.Get("InstanceId").String()
+		r.agent.accumulator.AddFields(r.metricName, fields, tags)
 	}
 
 	return nil
 }
 
-func (r *runningInstance) describeInstanceStatistics(instanceID string) error {
+func (r *runningInstance) describeInstanceStatistics(instanceID, region string) error {
 	request := requests.NewCommonRequest()
 	request.Method = "POST"
 	request.Scheme = "https" // https | http
@@ -239,99 +282,148 @@ func (r *runningInstance) describeInstanceStatistics(instanceID string) error {
 
 		tags["product"] = "ddos"
 		tags["action"] = "describeInstanceStatistics"
+		tags["region"] = region
 
 		fields["defenseCountUsage"] = item.Get("DefenseCountUsage").Int()
 		fields["domainUsage"] = item.Get("DomainUsage").Int()
 		fields["instanceId"] = item.Get("InstanceId").String()
 		fields["portUsage"] = item.Get("PortUsage").Int()
 		fields["siteUsage"] = item.Get("SiteUsage").Int()
+
+		r.agent.accumulator.AddFields(r.metricName, fields, tags)
 	}
 
 	return nil
 }
 
-func (r *runningInstance) describeWebRules() error {
-	// var pageNumber = 1
-	// var pageSize = 50
+func (r *runningInstance) describeWebRules(region string) error {
+	var pageNumber = 1
+	var pageSize = 50
 
-	request := requests.NewCommonRequest()
-	request.Method = "POST"
-	request.Scheme = "https" // https | http
-	request.Domain = "ddoscoo.cn-hangzhou.aliyuncs.com"
-	request.Version = "2020-01-01"
-	request.ApiName = "DescribeWebRules"
+	for {
+		request := requests.NewCommonRequest()
+		request.Method = "POST"
+		request.Scheme = "https" // https | http
+		request.Domain = "ddoscoo.cn-hangzhou.aliyuncs.com"
+		request.Version = "2020-01-01"
+		request.ApiName = "DescribeWebRules"
 
-	request.QueryParams["RegionId"] = "cn-hangzhou"
-	request.QueryParams["PageSize"] = "10"
-	request.QueryParams["PageNumber"] = "1"
+		request.QueryParams["RegionId"] = region
+		request.QueryParams["PageSize"] = fmt.Sprintf("%d", pageSize)
+		request.QueryParams["PageNumber"] = fmt.Sprintf("%d", pageNumber)
 
-	response, err := r.client.ProcessCommonRequest(request)
-	if err != nil {
-		r.logger.Error("instance failed")
-		return err
-	}
-
-	data := response.GetHttpContentString()
-	instanceArr := gjson.Parse(data).Get("WebRules").Array()
-
-	for _, item := range instanceArr {
-		tags := map[string]string{}
-		fields := map[string]interface{}{}
-
-		tags["product"] = "ddos"
-		tags["action"] = "describeWebRules"
-
-		fields["CcEnabled"] = item.Get("CcEnabled").Bool()
-		fields["SslProtocols"] = item.Get("SslProtocols").String()
-		fields["CcRuleEnabled"] = item.Get("CcRuleEnabled").Bool()
-		fields["SslCiphers"] = item.Get("SslCiphers").String()
-		fields["CertName"] = item.Get("CertName").String()
-		fields["Domain"] = item.Get("Domain").String()
-		fields["Http2Enable"] = item.Get("Http2Enable").Bool()
-		fields["Cname"] = item.Get("Cname").String()
-		fields["CcTemplate"] = item.Get("CcTemplate").String()
-
-		for _, obj := range item.Get("ProxyTypes").Array() {
-			proxyKey := obj.Get("ProxyType").String()
-			fields[proxyKey] = obj.Get("ProxyPorts").Array()[0].Int()
+		response, err := r.client.ProcessCommonRequest(request)
+		if err != nil {
+			r.logger.Error("instance failed")
+			return err
 		}
 
-		for _, obj := range item.Get("RealServers").Array() {
-			key := fmt.Sprintf("real_server_%s", obj.Get("RsType").String())
-			fields[key] = obj.Get("RealServer").String()
+		data := response.GetHttpContentString()
+		instanceArr := gjson.Parse(data).Get("WebRules").Array()
+
+		for _, item := range instanceArr {
+			tags := map[string]string{}
+			fields := map[string]interface{}{}
+
+			tags["product"] = "ddos"
+			tags["action"] = "describeWebRules"
+			tags["region"] = region
+
+			fields["CcEnabled"] = item.Get("CcEnabled").Bool()
+			fields["SslProtocols"] = item.Get("SslProtocols").String()
+			fields["CcRuleEnabled"] = item.Get("CcRuleEnabled").Bool()
+			fields["SslCiphers"] = item.Get("SslCiphers").String()
+			fields["CertName"] = item.Get("CertName").String()
+			fields["Domain"] = item.Get("Domain").String()
+			fields["Http2Enable"] = item.Get("Http2Enable").Bool()
+			fields["Cname"] = item.Get("Cname").String()
+			fields["CcTemplate"] = item.Get("CcTemplate").String()
+
+			for _, obj := range item.Get("ProxyTypes").Array() {
+				proxyKey := obj.Get("ProxyType").String()
+				fields[proxyKey] = obj.Get("ProxyPorts").Array()[0].Int()
+			}
+
+			for _, obj := range item.Get("RealServers").Array() {
+				key := fmt.Sprintf("real_server_%s", obj.Get("RsType").String())
+				fields[key] = obj.Get("RealServer").String()
+			}
+
+			r.agent.accumulator.AddFields(r.metricName, fields, tags)
 		}
+
+		total := gjson.Parse(data).Get("TotalCount").Int()
+		if int64(pageNumber*pageSize) >= total {
+			break
+		}
+
+		pageNumber = pageNumber + 1
+	}
+	return nil
+}
+
+func (r *runningInstance) describeNetworkRules(region string) error {
+	var pageNumber = 1
+	var pageSize = 50
+
+	for {
+		request := requests.NewCommonRequest()
+		request.Method = "POST"
+		request.Scheme = "https" // https | http
+		request.Domain = "ddoscoo.cn-hangzhou.aliyuncs.com"
+		request.Version = "2020-01-01"
+		request.ApiName = "DescribeNetworkRules"
+
+		request.QueryParams["RegionId"] = region
+		request.QueryParams["PageSize"] = fmt.Sprintf("%d", pageSize)
+		request.QueryParams["PageNumber"] = fmt.Sprintf("%d", pageNumber)
+
+		response, err := r.client.ProcessCommonRequest(request)
+		if err != nil {
+			r.logger.Error("instance failed")
+			return err
+		}
+
+		data := response.GetHttpContentString()
+
+		instanceArr := gjson.Parse(data).Get("NetworkRules").Array()
+
+		for _, item := range instanceArr {
+			tags := map[string]string{}
+			fields := map[string]interface{}{}
+
+			tags["product"] = "ddos"
+			tags["action"] = "describeNetworkRules"
+			tags["region"] = region
+			tags["Protocol"] = item.Get("Protocol").String()
+
+			fields["backendPort"] = item.Get("BackendPort").Int()
+			fields["isAutoCreate"] = item.Get("IsAutoCreate").Bool()
+			fields["instanceId"] = item.Get("InstanceId").String()
+			fields["frontendPort"] = item.Get("FrontendPort").Int()
+
+			realServer := []string{}
+
+			for _, obj := range item.Get("RealServers").Array() {
+				realServer = append(realServer, obj.String())
+			}
+			fields["realServer"] = strings.Join(realServer, "\\")
+
+			r.agent.accumulator.AddFields(r.metricName, fields, tags)
+		}
+
+		total := gjson.Parse(data).Get("TotalCount").Int()
+		if int64(pageNumber*pageSize) >= total {
+			break
+		}
+
+		pageNumber = pageNumber + 1
 	}
 
 	return nil
 }
 
-func (r *runningInstance) describeNetworkRules() error {
-	// var pageNumber = 1
-	// var pageSize = 50
-
-	request := requests.NewCommonRequest()
-	request.Method = "POST"
-	request.Scheme = "https" // https | http
-	request.Domain = "ddoscoo.cn-hangzhou.aliyuncs.com"
-	request.Version = "2020-01-01"
-	request.ApiName = "DescribeNetworkRules"
-
-	request.QueryParams["RegionId"] = "cn-hangzhou"
-	request.QueryParams["PageSize"] = "10"
-	request.QueryParams["PageNumber"] = "1"
-
-	response, err := r.client.ProcessCommonRequest(request)
-	if err != nil {
-		r.logger.Error("instance failed")
-		return err
-	}
-
-	fmt.Println("========", response.GetHttpContentString())
-
-	return nil
-}
-
-func (r *runningInstance) describePayInfo() error {
+func (r *runningInstance) describePayInfo(region string) error {
 	request := requests.NewCommonRequest()
 	request.Method = "POST"
 	request.Scheme = "https" // https | http
@@ -347,7 +439,29 @@ func (r *runningInstance) describePayInfo() error {
 		return err
 	}
 
-	fmt.Println("========", response.GetHttpContentString())
+	data := response.GetHttpContentString()
+
+	instanceArr := gjson.Parse(data).Get("Result").Array()
+
+	for _, item := range instanceArr {
+		tags := map[string]string{}
+		fields := map[string]interface{}{}
+
+		tags["product"] = "waf"
+		tags["action"] = "describePayInfo"
+		tags["region"] = item.Get("Region").String()
+
+		fields["endDate"] = item.Get("EndDate").Int()
+		fields["inDebt"] = item.Get("InDebt").Int()
+		fields["instanceId"] = item.Get("InstanceId").String()
+		fields["PayType"] = item.Get("PayType").Int()
+		fields["trial"] = item.Get("Trial").Int()
+		fields["region"] = item.Get("Region").String()
+		fields["remainDay"] = item.Get("RemainDay").Int()
+		fields["status"] = item.Get("Status").Int()
+
+		r.agent.accumulator.AddFields(r.metricName, fields, tags)
+	}
 
 	return nil
 }
