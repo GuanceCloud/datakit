bfef9ea3e12336555be8af17d899d319b2d00933
leecha <22087646+leecha@users.noreply.github.com>
2020-05-08 14:48:22 +0800

    add etcd HTTPS TLS
    Former-commit-id: 067fa30292a15de1a21024793a81227adb77fc49 [formerly 5b525ed0b8a57f777729bcffdf1e8fbaac09be73] [formerly bab7b2a2f5cae71033346a0c98764cc0a09b03ab [formerly 6dbc0a0d05d5af666d964599af53e35ab7f73bac]]
    Former-commit-id: e820bad09e69f2c00928f41b84870122b11fd2ac [formerly 03ff391656906e025f9337d25d3b5d657a1cf38d]
    Former-commit-id: c26d39b333fbcade4de1775807267c4a2bf2924d

--- plugins/inputs/etcd/conf.go
+++ plugins/inputs/etcd/conf.go
@@ -12,7 +12,16 @@ const (
 #	host = "127.0.0.1"
 #
 #       ## etcd 端口
-#	port = "2379"
+#	port = 2379
+#
+#       ## CA 证书路径（例如：ca.crt）
+#       # tls_cacert_file = ""
+#
+#       ## 客户端证书文件路径（例如：peer.crt）
+#	# tls_cert_file = ""
+#
+#	## 私钥文件路径（例如：peer.key）
+#	tls_key_file = ""
 #
 #	## 采集周期，时间单位是秒
 #	collect_cycle = 60
@@ -25,6 +34,9 @@ const (
 type Subscribe struct {
 	EtcdHost    string        `toml:"host"`
 	EtcdPort    int           `toml:"port"`
+	CacertFile  string        `toml:"tls_cacert_file"`
+	CertFile    string        `toml:"tls_cert_file"`
+	KeyFile     string        `toml:"tls_key_file"`
 	Cycle       time.Duration `toml:"collect_cycle"`
 	Measurement string        `toml:"measurement"`
 }
