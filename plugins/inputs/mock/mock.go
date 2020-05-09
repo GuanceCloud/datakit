333227268b2c1fa5810d732ecc4d50ed304330aa
wuqinchao <wuqinchao@jiagouyun.com>
2020-05-09 15:34:09 +0800

    tag斜杠问题
     [formerly 7732345299889c4086ae9b55e26ff1854a65bd41 [formerly 07e9b595322cb93b61739439bb15e8f46251434a] [formerly 7732345299889c4086ae9b55e26ff1854a65bd41 [formerly 07e9b595322cb93b61739439bb15e8f46251434a] [formerly bb3acb4bfe40ab64b408d3c06519d10a758b8a2e [formerly efd114f44a68596ca08ff65355758b539421916d]]]]
    
    
    
    Former-commit-id: 5f9b53b565d1d30db5fc2582b8f0f275da7dfc38
    Former-commit-id: 04e92e9ec488a2162cb5c5b1aaf06dc3e7efacf6 [formerly 96edf6df6bdc527a689b277480fe57c184dae348]
    Former-commit-id: e5ec0b87fa147863622fc3261e944f6de23dc4dc

--- plugins/inputs/mock/mock.go
+++ plugins/inputs/mock/mock.go
@@ -1,8 +1,6 @@
 package mock
 
 import (
-	"fmt"
-	"math/rand"
 	"time"
 
 	"github.com/influxdata/telegraf"
@@ -11,6 +9,7 @@ import (
 
 type (
 	Mock struct {
+		Value string
 	}
 )
 
@@ -30,7 +29,10 @@ func (m *Mock) makeMetric(value interface{}, name ...string) telegraf.Metric {
 	if len(name) > 0 {
 		measurement = name[0]
 	}
-	tags := map[string]string{"tag1": "value1"}
+	tags := map[string]string{
+		"device": `E:\`,
+		"size":   "1",
+	}
 	pt, _ := metric.New(
 		measurement,
 		tags,
@@ -40,97 +42,14 @@ func (m *Mock) makeMetric(value interface{}, name ...string) telegraf.Metric {
 	return pt
 }
 
-type person struct {
-	name string
-	age  int
-	addr string
-	sexy int
-}
-
-var testPersons []*person
-
-func randPerson() *person {
-	namelen := rand.Intn(6)
-	if namelen < 3 {
-		namelen = 3
-	}
-
-	name := make([]byte, namelen)
-	for j := 0; j < namelen; j++ {
-		name[j] = 'a' + byte(rand.Intn(26))
-		if j == 0 {
-			name[j] -= 32
-		}
-	}
-
-	age := rand.Intn(80)
-
-	addrs := []string{
-		"上海",
-		"北京",
-		"湖北",
-		"河南",
-		"浙江",
-		"广州",
-		"意大利",
-		"日本",
-		"韩国",
-	}
-
-	return &person{
-		name: string(name),
-		age:  age,
-		addr: addrs[rand.Intn(len(addrs))],
-		sexy: rand.Intn(2),
-	}
-}
-
-func randTemp(p *person) float32 {
-	var temp float32
-	for {
-		n := rand.Intn(40)
-		if n >= 35 {
-			temp = float32(n) + rand.Float32()
-			break
-		}
-	}
-	return temp
-}
-
 func (m *Mock) Gather(acc telegraf.Accumulator) error {
 
-	if testPersons == nil {
-		for i := 0; i < 100; i++ {
-			p := randPerson()
-			testPersons = append(testPersons, p)
-		}
-	}
-
-	for _, p := range testPersons {
-		fields := map[string]interface{}{
-			"temprature": randTemp(p),
-		}
-		tags := map[string]string{
-			"name": p.name,
-			"age":  fmt.Sprintf("%d", p.age),
-			"addr": p.addr,
-		}
-		if p.sexy == 0 {
-			tags["sexy"] = "男"
-		} else {
-			tags["sexy"] = "女"
-		}
-		acc.AddFields("virus", fields, tags)
-	}
-
-	// acc.AddMetric(m.makeMetric(val))
-	// val++
+	acc.AddMetric(m.makeMetric(1.0))
 
 	return nil
 }
 
 // func init() {
-// 	rand.Seed(time.Now().Unix())
 // 	inputs.Add("mock", func() telegraf.Input {
 // 		return &Mock{}
 // 	})
