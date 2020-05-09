e988bdcecde9304453c4b31d54264c33d1940693
leecha <22087646+leecha@users.noreply.github.com>
2020-05-09 14:44:53 +0800

    coredns fix bug
    Former-commit-id: c4f459723fc1313c1a52131cac38abf891f20954 [formerly 756b286d69101e3803cf8f44510daf28dc783970] [formerly b9cbb3906635e083e3ed0895c3945f61c8bcb9a3 [formerly 2c8ff6c7782ebf78269024183ba6da71c7197281]] [formerly baf1f4b8136290af38cbe0b27054172ab6114ae4 [formerly 90edc40b0ef210126e1a17ccf6539d3a99473e05] [formerly 855073841c8ff79617d6c5a494c3591fc5dbe329 [formerly 5f9e0b8e037725b954c7d1e43546b18617c2c5bc]]]
    Former-commit-id: baf1f4b8136290af38cbe0b27054172ab6114ae4 [formerly 90edc40b0ef210126e1a17ccf6539d3a99473e05]
    Former-commit-id: 26ebb2e4f5567b336803422a5298a7bb9b9b2acd [formerly b95ab708a85fe31ee180ffec797fd7be7d61c85e]
    Former-commit-id: d39db2368ea5e59b5480a8e5b492f9c375bc0db3

--- plugins/inputs/coredns/stream.go
+++ plugins/inputs/coredns/stream.go
@@ -1,21 +1,15 @@
 package coredns
 
 import (
+	"errors"
 	"fmt"
-	"io"
-	"math"
+	"log"
 	"net/http"
 	"sync"
 	"time"
 
 	influxdb "github.com/influxdata/influxdb1-client/v2"
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/metric"
-	dto "github.com/prometheus/client_model/go"
-	"github.com/prometheus/common/expfmt"
 )
-
-const _PROMETHEUS_TO_METRIC_MEASUREMENT = "tmp"
 
 var tagsWhiteList = map[string]byte{"version": '0'}
 
@@ -44,7 +38,9 @@ func (s *stream) start(wg *sync.WaitGroup) error {
 	defer wg.Done()
 
 	if s.sub.Measurement == "" {
-		return fmt.Errorf("invalid measurement")
+		err := errors.New("invalid measurement")
+		log.Printf("E! [CoreDNS] subscribe %s:%d, error: %s\n", s.sub.CorednsHost, s.sub.CorednsPort, err.Error())
+		return err
 	}
 
 	ticker := time.NewTicker(time.Second * s.sub.Cycle)
@@ -53,7 +49,9 @@ func (s *stream) start(wg *sync.WaitGroup) error {
 	for {
 		select {
 		case <-ticker.C:
-			s.exec()
+			if err := s.exec(); err != nil {
+				log.Printf("E! [CoreDNS] subscribe %s:%d, error: %s\n", s.sub.CorednsHost, s.sub.CorednsPort, err.Error())
+			}
 		default:
 			// nil
 		}
@@ -74,7 +72,7 @@ func (s *stream) exec() error {
 	}
 
 	if len(metrics) == 0 {
-		return fmt.Errorf("The metrics is nil")
+		return errors.New("the metrics is empty")
 	}
 
 	var tags = make(map[string]string)
@@ -110,163 +108,4 @@ func (s *stream) flush() (err error) {
 	err = s.coredns.ProcessPts(s.points)
 	s.points = nil
 	return err
-}
-
-// Parse returns a slice of Metrics from a text representation of a
-// metrics
-func ParseV2(prodata io.Reader) ([]telegraf.Metric, error) {
-	var metrics []telegraf.Metric
-	var parser expfmt.TextParser
-
-	metricFamilies, err := parser.TextToMetricFamilies(prodata)
-	if err != nil {
-		return nil, fmt.Errorf("reading text format failed: %s", err)
-	}
-
-	// make sure all metrics have a consistent timestamp so that metrics don't straddle two different seconds
-	now := time.Now()
-	// read metrics
-	for metricName, mf := range metricFamilies {
-		for _, m := range mf.Metric {
-			// reading tags
-			tags := makeLabels(m)
-
-			if mf.GetType() == dto.MetricType_SUMMARY {
-				// summary metric
-				telegrafMetrics := makeQuantilesV2(m, tags, metricName, mf.GetType(), now)
-				metrics = append(metrics, telegrafMetrics...)
-			} else if mf.GetType() == dto.MetricType_HISTOGRAM {
-				// histogram metric
-				telegrafMetrics := makeBucketsV2(m, tags, metricName, mf.GetType(), now)
-				metrics = append(metrics, telegrafMetrics...)
-			} else {
-				// standard metric
-				// reading fields
-				fields := make(map[string]interface{})
-				fields = getNameAndValueV2(m, metricName)
-				// converting to telegraf metric
-				if len(fields) > 0 {
-					var t time.Time
-					if m.TimestampMs != nil && *m.TimestampMs > 0 {
-						t = time.Unix(0, *m.TimestampMs*1000000)
-					} else {
-						t = now
-					}
-					metric, err := metric.New(_PROMETHEUS_TO_METRIC_MEASUREMENT, tags, fields, t, valueType(mf.GetType()))
-					if err == nil {
-						metrics = append(metrics, metric)
-					}
-				}
-			}
-		}
-	}
-
-	return metrics, err
-}
-
-// Get Quantiles for summary metric & Buckets for histogram
-func makeQuantilesV2(m *dto.Metric, tags map[string]string, metricName string, metricType dto.MetricType, now time.Time) []telegraf.Metric {
-	var metrics []telegraf.Metric
-	fields := make(map[string]interface{})
-	var t time.Time
-	if m.TimestampMs != nil && *m.TimestampMs > 0 {
-		t = time.Unix(0, *m.TimestampMs*1000000)
-	} else {
-		t = now
-	}
-	fields[metricName+"_count"] = float64(m.GetSummary().GetSampleCount())
-	fields[metricName+"_sum"] = float64(m.GetSummary().GetSampleSum())
-	met, err := metric.New(_PROMETHEUS_TO_METRIC_MEASUREMENT, tags, fields, t, valueType(metricType))
-	if err == nil {
-		metrics = append(metrics, met)
-	}
-
-	for _, q := range m.GetSummary().Quantile {
-		newTags := tags
-		fields = make(map[string]interface{})
-
-		newTags["quantile"] = fmt.Sprint(q.GetQuantile())
-		fields[metricName] = float64(q.GetValue())
-
-		quantileMetric, err := metric.New(_PROMETHEUS_TO_METRIC_MEASUREMENT, newTags, fields, t, valueType(metricType))
-		if err == nil {
-			metrics = append(metrics, quantileMetric)
-		}
-	}
-	return metrics
-}
-
-// Get Buckets  from histogram metric
-func makeBucketsV2(m *dto.Metric, tags map[string]string, metricName string, metricType dto.MetricType, now time.Time) []telegraf.Metric {
-	var metrics []telegraf.Metric
-	fields := make(map[string]interface{})
-	var t time.Time
-	if m.TimestampMs != nil && *m.TimestampMs > 0 {
-		t = time.Unix(0, *m.TimestampMs*1000000)
-	} else {
-		t = now
-	}
-	fields[metricName+"_count"] = float64(m.GetHistogram().GetSampleCount())
-	fields[metricName+"_sum"] = float64(m.GetHistogram().GetSampleSum())
-
-	met, err := metric.New(_PROMETHEUS_TO_METRIC_MEASUREMENT, tags, fields, t, valueType(metricType))
-	if err == nil {
-		metrics = append(metrics, met)
-	}
-
-	for _, b := range m.GetHistogram().Bucket {
-		newTags := tags
-		fields = make(map[string]interface{})
-		newTags["le"] = fmt.Sprint(b.GetUpperBound())
-		fields[metricName+"_bucket"] = float64(b.GetCumulativeCount())
-
-		histogramMetric, err := metric.New(_PROMETHEUS_TO_METRIC_MEASUREMENT, newTags, fields, t, valueType(metricType))
-		if err == nil {
-			metrics = append(metrics, histogramMetric)
-		}
-	}
-	return metrics
-}
-
-func valueType(mt dto.MetricType) telegraf.ValueType {
-	switch mt {
-	case dto.MetricType_COUNTER:
-		return telegraf.Counter
-	case dto.MetricType_GAUGE:
-		return telegraf.Gauge
-	case dto.MetricType_SUMMARY:
-		return telegraf.Summary
-	case dto.MetricType_HISTOGRAM:
-		return telegraf.Histogram
-	default:
-		return telegraf.Untyped
-	}
-}
-
-// Get labels from metric
-func makeLabels(m *dto.Metric) map[string]string {
-	result := map[string]string{}
-	for _, lp := range m.Label {
-		result[lp.GetName()] = lp.GetValue()
-	}
-	return result
-}
-
-// Get name and value from metric
-func getNameAndValueV2(m *dto.Metric, metricName string) map[string]interface{} {
-	fields := make(map[string]interface{})
-	if m.Gauge != nil {
-		if !math.IsNaN(m.GetGauge().GetValue()) {
-			fields[metricName] = float64(m.GetGauge().GetValue())
-		}
-	} else if m.Counter != nil {
-		if !math.IsNaN(m.GetCounter().GetValue()) {
-			fields[metricName] = float64(m.GetCounter().GetValue())
-		}
-	} else if m.Untyped != nil {
-		if !math.IsNaN(m.GetUntyped().GetValue()) {
-			fields[metricName] = float64(m.GetUntyped().GetValue())
-		}
-	}
-	return fields
 }
