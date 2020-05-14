3bad917f03774d65b4c041159da00a2b207f95d8
coanor <coanor@gmail.com>
2020-05-14 11:17:01 +0000

    update: 	[+] add docker image build
     [formerly 38b8ddf47a6139e90ddcfaade74a491e9002ece2 [formerly 053bdc0f159a3580952c50acc6b3cc4f45ec1362] [formerly 38b8ddf47a6139e90ddcfaade74a491e9002ece2 [formerly 053bdc0f159a3580952c50acc6b3cc4f45ec1362] [formerly 3a054dc17de10cd8b009767ca08b4fd251c20adf [formerly 5d4ec167ac4e9b9789b27b786ac520fd7004ca52]]]]
    
    
    
    Former-commit-id: 2dd1c5e6bc9e455da1a37617a6ea1be3ccb7fbf9
    Former-commit-id: 6196731bc2d0d53e970321c9556d9ed18a71bc3c [formerly 33fcba078f8f39e1340c86181cfb31436b5bd2a4]
    Former-commit-id: 0a6bcaed702ed8956406082e55c96773db8efc5e

--- plugins/inputs/registry.go
+++ plugins/inputs/registry.go
@@ -4,6 +4,17 @@ import (
 	"github.com/influxdata/telegraf"
 )
 
+type DataKitInput interface {
+	telegraf.Input
+
+	CollectionName() string
+	Status() string
+
+	/* TotalBytes() int64 */
+
+	// add more...
+}
+
 type Creator func() telegraf.Input
 
 var Inputs = map[string]Creator{}
