5fb9f1f03bfcfd1dfffdcdabc8842ca56556f4fe
wuqinchao <wuqinchao@jiagouyun.com>
2020-05-17 00:45:54 +0800

    阿里云日志采集优化

--- plugins/inputs/aliyunlog/aliyunlog.go
+++ plugins/inputs/aliyunlog/aliyunlog.go
@@ -2,10 +2,14 @@ package aliyunlog
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/models"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 
@@ -50,6 +54,8 @@ type runningStore struct {
 	cfg        *LogStoreCfg
 	metricName string
 
+	fieldsInfo map[string]string
+
 	logger *models.Logger
 }
 
@@ -58,7 +64,7 @@ func (_ *AliyunLog) SampleConfig() string {
 }
 
 func (_ *AliyunLog) Description() string {
-	return ""
+	return "Collect logs from aliyun SLS"
 }
 
 func (_ *AliyunLog) Gather(telegraf.Accumulator) error {
@@ -138,10 +144,25 @@ func (r *runningProject) run(ctx context.Context) error {
 
 func (r *runningStore) run(ctx context.Context) error {
 
+	r.fieldsInfo = map[string]string{}
+
+	for _, fitem := range r.cfg.Fields {
+		parts := strings.Split(fitem, ":")
+		if len(parts) != 2 {
+			r.logger.Warnf("invalid field type specification")
+			continue
+		}
+		fieldType := parts[0]
+		fieldNames := strings.Split(parts[1], ",")
+		for _, f := range fieldNames {
+			r.fieldsInfo[f] = fieldType
+		}
+	}
+
 	option := consumerLibrary.LogHubConfig{
 		Endpoint:          r.proj.inst.cfg.Endpoint,
-		AccessKeyID:       r.proj.inst.cfg.AccessKey,
-		AccessKeySecret:   r.proj.inst.cfg.AccessID,
+		AccessKeyID:       r.proj.inst.cfg.AccessKeyID,
+		AccessKeySecret:   r.proj.inst.cfg.AccessKeySecret,
 		Project:           r.proj.cfg.Name,
 		Logstore:          r.cfg.Name,
 		ConsumerGroupName: r.cfg.ConsumerGroupName,
@@ -165,57 +186,116 @@ func (r *runningStore) run(ctx context.Context) error {
 
 }
 
+func (r *runningStore) checkAsTag(key string) bool {
+	for _, k := range r.cfg.Tags {
+		if k == key {
+			return true
+		}
+	}
+	return false
+}
+
+func (r *runningStore) checkFieldType(field string) string {
+	if ftype, ok := r.fieldsInfo[field]; ok {
+		return ftype
+	}
+	return "string"
+}
+
 func (r *runningStore) logProcess(shardId int, logGroupList *sls.LogGroupList) string {
+
 	r.logger.Debugf("shardId:%d, grouplist:%s", shardId, logGroupList.String())
 	for _, lg := range logGroupList.LogGroups {
-
-		tags := map[string]string{}
-		tags["store"] = r.cfg.Name
-		tags["project"] = r.proj.cfg.Name
-
-		for _, lt := range lg.GetLogTags() {
-			k := lt.GetKey()
-			if k == "" {
-				continue
-			}
-			tags[k] = lt.GetValue()
-		}
-
-		if lg.GetSource() != "" {
-			tags["source"] = lg.GetSource()
-		}
-
-		if lg.GetTopic() != "" {
-			tags["topic"] = lg.GetTopic()
-		}
 
 		for _, l := range lg.GetLogs() {
 
 			fields := map[string]interface{}{}
 
+			tags := map[string]string{}
+			tags["store"] = r.cfg.Name
+			tags["project"] = r.proj.cfg.Name
+			tags["__topic__"] = lg.GetTopic()
+
+			for _, lt := range lg.GetLogTags() {
+				k := lt.GetKey()
+				if k == "" || lt.GetValue() == "" {
+					continue
+				}
+				if r.checkAsTag(k) {
+					tags[k] = lt.GetValue()
+				} else {
+					fields[k] = lt.GetValue()
+				}
+			}
+
+			if lg.GetSource() != "" {
+				if r.checkAsTag("__source__") {
+					tags["__source__"] = lg.GetSource()
+				} else {
+					fields["__source__"] = lg.GetSource()
+				}
+			}
+
+			// if lg.GetCategory() != "" {
+			// 	tags["__category__"] = lg.GetCategory()
+			// }
+
 			for _, lc := range l.Contents {
 				k := lc.GetKey()
 				if k != "" {
-					fields[k] = lc.GetValue()
+					if r.checkAsTag(k) {
+						tags[k] = lc.GetValue()
+					} else {
+						strval := lc.GetValue()
+						fieldType := r.checkFieldType(k)
+						if fieldType != "string" {
+							switch fieldType {
+							case "int":
+								nval, err := strconv.ParseInt(strval, 10, 64)
+								if err != nil {
+									r.logger.Warnf("you specify '%s' as int, but fail to convert '%s' to int", k, strval)
+								} else {
+									fields[k] = nval
+								}
+							case "float":
+								fval, err := strconv.ParseFloat(strval, 64)
+								if err != nil {
+									r.logger.Warnf("you specify '%s' as float, but fail to convert '%s' to float", k, strval)
+								} else {
+									fields[k] = fval
+								}
+							}
+						} else {
+							fields[k] = strval
+						}
+					}
 				}
 			}
 
 			tm := time.Unix(int64(l.GetTime()), 0)
 			m, err := metric.New(r.metricName, tags, fields, tm)
 			if err == nil {
-				r.proj.inst.agent.accumulator.AddMetric(m)
+				if r.proj.inst.agent.accumulator != nil {
+					r.proj.inst.agent.accumulator.AddMetric(m)
+				} else {
+					fmt.Printf("%s", internal.Metric2InfluxLine(m))
+				}
 			} else {
-				r.logger.Warnf("%s", err)
+				r.logger.Warnf("fail to generate metric, %s", err)
 			}
 		}
 	}
 	return ""
 }
 
+func NewAgent() *AliyunLog {
+	ac := &AliyunLog{}
+	ac.ctx, ac.cancelFun = context.WithCancel(context.Background())
+	return ac
+}
+
 func init() {
 	inputs.Add("aliyunlog", func() telegraf.Input {
-		ac := &AliyunLog{}
-		ac.ctx, ac.cancelFun = context.WithCancel(context.Background())
-		return ac
+		return NewAgent()
 	})
 }
