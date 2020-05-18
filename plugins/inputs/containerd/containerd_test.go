f656290f97d14e668318a54ee685c7a590550fba
liguozhuang <liguozhuang@jiagouyun.com>
2020-05-18 03:27:22 +0000

    fix bug of containerd stop
    Former-commit-id: ce2d18d242ede28ea55a6849e91d5405d0fe4c3a [formerly d968804ca5e1d54286b1e6def435b896dd1b1b15] [formerly 89356478c9fd1ce312fe69a7c613c058b6f58fa2 [formerly b73a7fbec7230f0fb3f5af2c79743956bbe24db7]]
    Former-commit-id: 48265652fd44ffa4ab7eb77fedb097d45dce7ed0 [formerly 02a4a0e0645e958fb7402952d7d1d544b202c84b]
    Former-commit-id: 2b5bb72a9081a7591926f503cef88a754c677ba1

--- plugins/inputs/containerd/containerd_test.go
+++ plugins/inputs/containerd/containerd_test.go
@@ -3,6 +3,7 @@
 package containerd
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"testing"
@@ -10,6 +11,8 @@ import (
 )
 
 func TestStart(t *testing.T) {
+
+	ctx, cancel := context.WithCancel(context.Background())
 
 	var e = Containerd{
 		Config: Config{
@@ -23,20 +26,24 @@ func TestStart(t *testing.T) {
 				},
 			},
 		},
+		ctx:    ctx,
+		cancel: cancel,
+		wg:     new(sync.WaitGroup),
 	}
-
-	e.wg = new(sync.WaitGroup)
 
 	for _, sub := range e.Config.Subscribes {
 		e.wg.Add(1)
 		s := sub
-		stream := newStream(&s, nil)
+		stream := newStream(&s, &e)
 		fmt.Println(s)
 		if err := stream.processMetrics(); err != nil {
 			panic(err)
 		}
+
 		go stream.start(e.wg)
 	}
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(7 * time.Second)
+
+	e.Stop()
 }
