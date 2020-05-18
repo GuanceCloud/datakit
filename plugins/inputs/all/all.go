b8bc388c4a061af7af8cb97e52cd971f4ac97460
wuqinchao <wuqinchao@jiagouyun.com>
2020-05-18 16:29:41 +0800

    fix目录升级成dataflux
     [formerly 2dd7b009b40dc8b7b00550393e769ac672290f25 [formerly dc788eebeff83c3933cac5fbd1ec1090e47393a1]] [formerly d1663a949d5430b939e56143b0fe6e98ceeaea94 [formerly 5d5e0e0dc535de51f9e0ad22f5571a32f960cc3e] [formerly d1663a949d5430b939e56143b0fe6e98ceeaea94 [formerly 5d5e0e0dc535de51f9e0ad22f5571a32f960cc3e] [formerly d1663a949d5430b939e56143b0fe6e98ceeaea94 [formerly 5d5e0e0dc535de51f9e0ad22f5571a32f960cc3e] [formerly 112d1d71bddda62abb2a976552899f7f38334152 [formerly 2395eefe47cce82aaf00491f0dbff6cdadeaada3]]]] [formerly 2dd7b009b40dc8b7b00550393e769ac672290f25 [formerly dc788eebeff83c3933cac5fbd1ec1090e47393a1] [formerly 2dd7b009b40dc8b7b00550393e769ac672290f25 [formerly dc788eebeff83c3933cac5fbd1ec1090e47393a1] [formerly 2dd7b009b40dc8b7b00550393e769ac672290f25 [formerly dc788eebeff83c3933cac5fbd1ec1090e47393a1] [formerly 050439ef55207a87560e9f227895ba5ae37f8aca [formerly 4b696b2828cb2f1b76e4dd4ef5483f056da19906]]]]]]
    
    
    
    
    Former-commit-id: fd8f4ba280e2cf6e63b2766292d5ada950909d5e [formerly bb586c0a0305854b389cde9681e29ba4ef55182b]
    Former-commit-id: 60319e626a8e4eb6a1f08f06b23804e86443f42b [formerly e7c9136996f5068767574da59bda7fd7a39c634a] [formerly dbdb864a429b017cac4e07d315cabc9a9a478eeb [formerly 0bca5b877c9b7a121e01fd76c14b3973b3e43d5c]]
    Former-commit-id: 34ff2a1cd240ddf12accd4214b0246806b6e74ef [formerly 9c3a640d407281cbf200510876fd6f830109b962]
    Former-commit-id: 645819969fdfa22dbe8960dbd99a5bff9cbadc81

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -13,6 +13,7 @@ import (
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/apachelog"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/awsbill"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/binlog"
+
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/containerd"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/coredns"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/dataclean"
