6cd9ccc93129c0aecc4a8d9ec00e4c491399d41b
wuqinchao <wuqinchao@jiagouyun.com>
2020-05-25 15:55:20 +0800

    .
     [formerly afddba08331d6b1bc0ea2a4852eb6deb92e473d9] [formerly ec1ab60100f36adedfd55f3f950d8888c128eca2 [formerly 4ae4605272296953100e7e56861779cbe516cca0] [formerly ec1ab60100f36adedfd55f3f950d8888c128eca2 [formerly 4ae4605272296953100e7e56861779cbe516cca0] [formerly ec1ab60100f36adedfd55f3f950d8888c128eca2 [formerly 4ae4605272296953100e7e56861779cbe516cca0] [formerly dd21409caa65b950dd09fb71fec7dbc4fdda43e5 [formerly 567ee99612c6340c51d552fa3c545518455a1777]]]] [formerly 1063b1dee40d0de83f063333c40209001be6844b [formerly 45e14b3af9198304389e25a225342bdf171a9b0e] [formerly 1063b1dee40d0de83f063333c40209001be6844b [formerly 45e14b3af9198304389e25a225342bdf171a9b0e] [formerly 1063b1dee40d0de83f063333c40209001be6844b [formerly 45e14b3af9198304389e25a225342bdf171a9b0e] [formerly f9bcb11b15b7618885daadc03ef91d5e50eea4e7 [formerly c375a3e0aabafea785d97f915d47920be2a5698a]]]]]]
    
    
    
    
    Former-commit-id: 3751eaa00fa1c4abe5de08b1055b8de4033be193 [formerly bdaec081285cfa8ef679788fb4b580ee05694d22]
    Former-commit-id: 7b15ecb3c6ff9d4926ccfd628d828234d6486a80 [formerly 329ee5ab5048054d574a4add38ee67a8aeac16c0] [formerly ce809079114f4d66ebade8711b64d1f1bb1bc634 [formerly e8d92764b783c095381d79cc8c561ab2cd68ed50]]
    Former-commit-id: d78d808e417faa3a0b869ee7c522a98c1810d887 [formerly 355128b2093b5f9ad31356b1b1e2bd10d9e8872c]
    Former-commit-id: d93aa4dc50f4e81bbc0778c782bc4d19b163586d

--- plugins/inputs/binlog/rows.go
+++ plugins/inputs/binlog/rows.go
@@ -83,10 +83,7 @@ func (r *RowsEvent) handleUnsigned() {
 
 func (r *RowsEvent) checkIgnoreColumn(c *schema.TableColumn) bool {
 	rtype := strings.ToLower(c.RawType)
-	if strings.HasPrefix(rtype, "blob") {
-		return true
-	}
-	return false
+	return strings.HasPrefix(rtype, "blob")
 }
 
 // String implements fmt.Stringer interface.
