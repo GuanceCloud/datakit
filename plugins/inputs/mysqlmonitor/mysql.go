7c10d24c1f59ffedc48f3675e7e053d00585c784
liushaobo <liushaobo@jiagouyun.com>
2020-05-26 17:03:21 +0800

    提交代码
    Former-commit-id: 7188b12bfbc190e3b19c64d41259530863279448 [formerly 262237f8b2205295650c322159e8f1506ac2c5aa] [formerly bdb62fa6b31b25ec6331c07fc647bb812e5ea29b [formerly c2825eacdae7f5b40c2f2dbb47cbf054813392eb]] [formerly e2f3369fe6fd20ab27724fe931f1153a645bb898 [formerly 2a9662468fea16eda9886450ef07068fb32e474a] [formerly d04cb748a2acb29273401a90c43ebea92c99fcba [formerly eb378d056d5e003ebee5841c36e8754da2eabf5f]]]
    Former-commit-id: e2f3369fe6fd20ab27724fe931f1153a645bb898 [formerly 2a9662468fea16eda9886450ef07068fb32e474a]
    Former-commit-id: 1c37bc274415615fe3af1c370dfdfad501ef4f33 [formerly 74a035565e463d5b8cbf1d56662e065e09626c9d]
    Former-commit-id: bb7d2f304a356f61434280969c3bd17b2b4012ce

--- plugins/inputs/mysqlmonitor/mysql.go
+++ plugins/inputs/mysqlmonitor/mysql.go
@@ -220,63 +220,35 @@ func (r *runningInstance) Query(sql string) ([]map[string]interface{}, error) {
 // 		item := make(map[string]interface{})
 // 		for i, data := range cache {
 // 			key := strings.ToLower(columns[i])
-// 			item[key] = *data.(*interface{})
+// 			val := *data.(*interface{})
+
+// 			if val != nil {
+// 				vType := reflect.TypeOf(val)
+
+// 				switch vType.String() {
+// 				case "int64":
+// 					item[key] = val.(int64)
+// 				case "string":
+// 					var data interface{}
+// 					data, err := strconv.ParseFloat(val.(string), 64)
+// 					if err != nil {
+// 						data = val
+// 					}
+// 					item[key] = data
+// 				case "time.Time":
+// 					item[key] = val.(time.Time)
+// 				case "[]uint8":
+// 					item[key] = string(val.([]uint8))
+// 				default:
+// 					return nil, fmt.Errorf("unsupport data type '%s' now\n", vType)
+// 				}
+// 			}
 // 		}
+
 // 		list = append(list, item)
 // 	}
 // 	return list, nil
 // }
-
-func (r *runningInstance) Query(sql string) ([]map[string]interface{}, error) {
-	rows, err := r.db.Query(sql)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	columns, _ := rows.Columns()
-	columnLength := len(columns)
-	cache := make([]interface{}, columnLength)
-	for idx, _ := range cache {
-		var a interface{}
-		cache[idx] = &a
-	}
-	var list []map[string]interface{}
-	for rows.Next() {
-		_ = rows.Scan(cache...)
-
-		item := make(map[string]interface{})
-		for i, data := range cache {
-			key := strings.ToLower(columns[i])
-			val := *data.(*interface{})
-
-			if val != nil {
-				vType := reflect.TypeOf(val)
-
-				switch vType.String() {
-				case "int64":
-					item[key] = val.(int64)
-				case "string":
-					var data interface{}
-					data, err := strconv.ParseFloat(val.(string), 64)
-					if err != nil {
-						data = val
-					}
-					item[key] = data
-				case "time.Time":
-					item[key] = val.(time.Time)
-				case "[]uint8":
-					item[key] = string(val.([]uint8))
-				default:
-					return nil, fmt.Errorf("unsupport data type '%s' now\n", vType)
-				}
-			}
-		}
-
-		list = append(list, item)
-	}
-	return list, nil
-}
 
 func init() {
 	inputs.Add("mysqlmonitor", func() telegraf.Input {
