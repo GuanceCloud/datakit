9c45f5f1923757c1d06fd8032d93ca8475157b5e
liushaobo <liushaobo@jiagouyun.com>
2020-06-01 14:37:43 +0800

    分类
    Former-commit-id: d8209d8a8a11fd0e906c10d89690391eea2101fc [formerly 66fa2646b347ccbfde221a3f9406d5ef25c31c9e]
    Former-commit-id: 2f0d4b9009f66214b060148e72afe4ac5fba4489

--- plugins/inputs/harborMonitor/conf.go
+++ plugins/inputs/harborMonitor/conf.go
@@ -5,10 +5,17 @@ import "gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
 const (
 	baiduIndexConfigSample = `
 #[[harbor]]
+#  ## 指标集名称
 #  metricName = 'harbor'
+#  ## 镜像仓库域名
 #  domain = ''
-#  interval = '10s'
+#  ## 域名是否支持https
+#  https = true
+#  ## 采集频度
+#  interval = '1h'
+#  ## 帐号
 #  username = ''
+#  ## 密码
 #  password = ''
 `
 )
@@ -16,6 +23,7 @@ const (
 type HarborCfg struct {
 	MetricName string
 	Domain     string
+	Https      bool
 	Username   string
 	Password   string
 	Interval   internal.Duration `toml:"interval"`
