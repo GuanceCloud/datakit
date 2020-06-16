08a131b91add7d2766ed66f4dde1675a7a117bf1
liushaobo <liushaobo@jiagouyun.com>
2020-06-16 17:38:52 +0800

    提交代码
     [formerly 7d6ab868853e4b9fa14abfafd13d5d1ad6eece58 [formerly d7963c3ccacfbfdc1125bae1e06fd10d86618a39] [formerly 7d6ab868853e4b9fa14abfafd13d5d1ad6eece58 [formerly d7963c3ccacfbfdc1125bae1e06fd10d86618a39] [formerly 12b626aa7ba48a10216d7938284add1e3188fa1e [formerly 989d4b39fefd3ff64a282e869c930b43e6f211fb]]]]
    
    
    
    Former-commit-id: f782035b9f9af44fa81ad42a3279aa09d5dd88ed
    Former-commit-id: 1bed9561a5b15043ffd8ec6c294d83b4a514dc57 [formerly ff75209119eb710105110a64761819abfef51feb]
    Former-commit-id: 3a5a11381a621b79d66db5829b230d8a621a61f1

--- config/telegrafSamples.go
+++ config/telegrafSamples.go
@@ -68,6 +68,7 @@ var (
 		"uwsgi":             &telegrafInputs{name: "uwsgi", Catalog: "uwsgi"},
 		"solr":              &telegrafInputs{name: "solr", Catalog: "solr"},
 		`systemd_units`:     &telegrafInputs{name: "systemd_units", Catalog: "systemd_units"},
+		`influxdb`:          &telegrafInputs{name: "influxdb", Catalog: "influxdb"},
 	}
 
 	telegrafCfgSamples = map[string]string{}
@@ -2283,5 +2284,33 @@ func initTelegrafSamples() {
 #  ## values are "socket", "target", "device", "mount", "automount", "swap",
 #  ## "timer", "path", "slice" and "scope ":
 #  # unittype = "service"
+`
+
+	telegrafCfgSamples[`influxdb`] = `
+# Read InfluxDB-formatted JSON metrics from one or more HTTP endpoints
+#[[inputs.influxdb]]
+	## Works with InfluxDB debug endpoints out of the box,
+	## but other services can use this format too.
+	## See the influxdb plugin's README for more details.
+
+	## Multiple URLs from which to read InfluxDB-formatted JSON
+	## Default is "http://localhost:8086/debug/vars".
+	# urls = [
+	#	"http://localhost:8086/debug/vars"
+	# ]
+
+	## Username and password to send using HTTP Basic Authentication.
+	# username = ""
+	# password = ""
+
+	## Optional TLS Config
+	# tls_ca = "/etc/telegraf/ca.pem"
+	# tls_cert = "/etc/telegraf/cert.pem"
+	# tls_key = "/etc/telegraf/key.pem"
+	## Use TLS but skip chain & host verification
+	# insecure_skip_verify = false
+
+	## http request & header timeout
+	# timeout = "5s"
 `
 }
