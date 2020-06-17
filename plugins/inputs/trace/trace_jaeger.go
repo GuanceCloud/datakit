6bc914c2f2fa65e091a41b01b2ad4aa39b0d14fe
root <root@DESKTOP-7BK497S.localdomain>
2020-06-17 11:42:05 +0800

    增加trace采集
    Former-commit-id: 12510d5caee9735753cb46fec872bb717a9b7b49 [formerly 8991cce08a124f8f6289b230672c4c24d7d23c57] [formerly 69b72afd405f9a01f4696dea1a724e566d856194 [formerly c6ce13939e8fc10a957ed353522246e89a53ee5d]] [formerly 132f1d86874fb53f525ac780191470fd6cc698e9 [formerly 188b714cb64223003753d423a0b5f516fe3cf437] [formerly a9e511715d8ab5aa3eeeb72d0640bc24d69d2e4a [formerly 772ad6cf588e30a7b13468f253b32acffe5c2e21]]]
    Former-commit-id: 059e87915448beb5f201480c207aa8a45c01343b
    Former-commit-id: 76db5b134c609f3c3a82555d4c843614985df139 [formerly 0887b06e3dc392c9be397e57e41a33bf5117ec11]
    Former-commit-id: d34e86d7a0c7a9e4750832cd472eae228660e0de

---
+++ plugins/inputs/trace/trace_jaeger.go
@@ -0,0 +1,60 @@
+package trace
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/uber/jaeger-client-go/thrift"
+	j "github.com/uber/jaeger-client-go/thrift-gen/jaeger"
+)
+
+func (z *JaegerTracer) Decode(octets []byte) error {
+	switch z.ContentType {
+	case "application/x-thrift":
+		return z.parseJaegerThrift(octets)
+	default:
+		return fmt.Errorf("Jaeger unsupported Content-Type: %s", z.ContentType)
+	}
+}
+
+func (z *JaegerTracer) parseJaegerThrift(octets []byte) error {
+	buffer := thrift.NewTMemoryBuffer()
+	if _, err := buffer.Write(octets); err != nil {
+		return err
+	}
+	transport := thrift.NewTBinaryProtocolTransport(buffer)
+	batch := &j.Batch{}
+	if err := batch.Read(transport); err != nil {
+		return err
+	}
+
+	for _, s := range batch.Spans {
+		tAdpter := TraceAdapter{}
+		tAdpter.source = "jaeger"
+
+		tAdpter.duration    = s.Duration
+		tAdpter.timestampUs = s.StartTime
+		sJson, err := json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		tAdpter.content = string(sJson)
+
+		tAdpter.class         = "tracing"
+		tAdpter.serviceName   = batch.Process.ServiceName
+		tAdpter.operationName = s.OperationName
+		tAdpter.parentID      = fmt.Sprintf("%d", s.ParentSpanId)
+		tAdpter.traceID       = fmt.Sprintf("%d%d", s.TraceIdHigh, s.TraceIdLow)
+		tAdpter.spanID        = fmt.Sprintf("%d", s.SpanId)
+
+		for _, tag := range s.Tags {
+			if tag.Key == "error" {
+				tAdpter.isError = "true"
+				break
+			}
+		}
+
+		tAdpter.mkLineProto()
+	}
+
+	return nil
+}
