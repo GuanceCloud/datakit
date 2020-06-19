ddd271cf99262d24b66f821e19f90d35a34d0628
coanor <coanor@gmail.com>
2020-06-19 10:35:00 +0000

    update: 	[*] refact extra.oraclemonitor
    Former-commit-id: d3bd4732fbfd157f5c82c15917726251a66233f9 [formerly d9f0eb4f8ff16b88dd835a43ad0f19829e6c0d44]
    Former-commit-id: 988d1b6d1391a3c225e84133ef76ec37fb3a4cfc

--- vendor/github.com/aliyun/aliyun-oss-go-sdk/oss/option.go
+++ vendor/github.com/aliyun/aliyun-oss-go-sdk/oss/option.go
@@ -175,6 +175,26 @@ func ServerSideEncryptionKeyID(value string) Option {
 	return setHeader(HTTPHeaderOssServerSideEncryptionKeyID, value)
 }
 
+// ServerSideDataEncryption is an option to set X-Oss-Server-Side-Data-Encryption header
+func ServerSideDataEncryption(value string) Option {
+	return setHeader(HTTPHeaderOssServerSideDataEncryption, value)
+}
+
+// SSECAlgorithm is an option to set X-Oss-Server-Side-Encryption-Customer-Algorithm header
+func SSECAlgorithm(value string) Option {
+	return setHeader(HTTPHeaderSSECAlgorithm, value)
+}
+
+// SSECKey is an option to set X-Oss-Server-Side-Encryption-Customer-Key header
+func SSECKey(value string) Option {
+	return setHeader(HTTPHeaderSSECKey, value)
+}
+
+// SSECKeyMd5 is an option to set X-Oss-Server-Side-Encryption-Customer-Key-Md5 header
+func SSECKeyMd5(value string) Option {
+	return setHeader(HTTPHeaderSSECKeyMd5, value)
+}
+
 // ObjectACL is an option to set X-Oss-Object-Acl header
 func ObjectACL(acl ACLType) Option {
 	return setHeader(HTTPHeaderOssObjectACL, string(acl))
@@ -249,6 +269,11 @@ func ACReqHeaders(value string) Option {
 // TrafficLimitHeader is an option to set X-Oss-Traffic-Limit
 func TrafficLimitHeader(value int64) Option {
 	return setHeader(HTTPHeaderOssTrafficLimit, strconv.FormatInt(value, 10))
+}
+
+// UserAgentHeader is an option to set HTTPHeaderUserAgent
+func UserAgentHeader(ua string) Option {
+	return setHeader(HTTPHeaderUserAgent, ua)
 }
 
 // ForbidOverWrite  is an option to set X-Oss-Forbid-Overwrite
@@ -490,7 +515,7 @@ func handleOptions(headers map[string]string, options []Option) error {
 	return nil
 }
 
-func getRawParams(options []Option) (map[string]interface{}, error) {
+func GetRawParams(options []Option) (map[string]interface{}, error) {
 	// Option
 	params := map[string]optionValue{}
 	for _, option := range options {
@@ -513,7 +538,7 @@ func getRawParams(options []Option) (map[string]interface{}, error) {
 	return paramsm, nil
 }
 
-func findOption(options []Option, param string, defaultVal interface{}) (interface{}, error) {
+func FindOption(options []Option, param string, defaultVal interface{}) (interface{}, error) {
 	params := map[string]optionValue{}
 	for _, option := range options {
 		if option != nil {
@@ -529,7 +554,7 @@ func findOption(options []Option, param string, defaultVal interface{}) (interfa
 	return defaultVal, nil
 }
 
-func isOptionSet(options []Option, option string) (bool, interface{}, error) {
+func IsOptionSet(options []Option, option string) (bool, interface{}, error) {
 	params := map[string]optionValue{}
 	for _, option := range options {
 		if option != nil {
@@ -545,7 +570,7 @@ func isOptionSet(options []Option, option string) (bool, interface{}, error) {
 	return false, nil, nil
 }
 
-func deleteOption(options []Option, strKey string) []Option {
+func DeleteOption(options []Option, strKey string) []Option {
 	var outOption []Option
 	params := map[string]optionValue{}
 	for _, option := range options {
