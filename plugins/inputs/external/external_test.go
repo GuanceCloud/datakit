f2729d91cf17f1fc9bb0fe41d694bac67ee54fc2
coanor <coanor@gmail.com>
2020-06-23 11:25:02 +0000

    update: 	[*] add gRPC demo 	[*] refact oraclemonitor as pure external command
     [formerly 1da626e2d928115b3c21ac0f7234b4fbdd779e3e] [formerly d32ef2e7b461a2fe5c4fdb385df5ce83cc29f863 [formerly 041a7a78a7292aa5e79c6c461c0b75b4db3509f7] [formerly d32ef2e7b461a2fe5c4fdb385df5ce83cc29f863 [formerly 041a7a78a7292aa5e79c6c461c0b75b4db3509f7] [formerly d32ef2e7b461a2fe5c4fdb385df5ce83cc29f863 [formerly 041a7a78a7292aa5e79c6c461c0b75b4db3509f7] [formerly 27498d05864cf54c96d3abff1baa253e88403b52 [formerly b38165727a1af110c2620c22f42be624a5bcef1f]]]]] [formerly 4a4dee24b856ffff8e20e4367d004ac176a7bb00 [formerly e748c240b26a470f76a6d21e984071f3232b71a4] [formerly 4a4dee24b856ffff8e20e4367d004ac176a7bb00 [formerly e748c240b26a470f76a6d21e984071f3232b71a4] [formerly 4a4dee24b856ffff8e20e4367d004ac176a7bb00 [formerly e748c240b26a470f76a6d21e984071f3232b71a4] [formerly 77fc9d37a429d2b665e23a45dfb72578d1ecc2ed [formerly 0539b99f23d615a01f474b9b8a243d03ad1ede31]]]] [formerly 39c97048532dbd98b593efb5ed329c7033b33bfd] [formerly d32ef2e7b461a2fe5c4fdb385df5ce83cc29f863 [formerly 041a7a78a7292aa5e79c6c461c0b75b4db3509f7] [formerly d32ef2e7b461a2fe5c4fdb385df5ce83cc29f863 [formerly 041a7a78a7292aa5e79c6c461c0b75b4db3509f7] [formerly d32ef2e7b461a2fe5c4fdb385df5ce83cc29f863 [formerly 041a7a78a7292aa5e79c6c461c0b75b4db3509f7] [formerly 27498d05864cf54c96d3abff1baa253e88403b52 [formerly b38165727a1af110c2620c22f42be624a5bcef1f]]]] [formerly dc8e2a443870b2391045dfb5b3cff8db04247a34 [formerly 47c821a5b2e415a926a4102f5d75a0fde1903be5] [formerly dc8e2a443870b2391045dfb5b3cff8db04247a34 [formerly 47c821a5b2e415a926a4102f5d75a0fde1903be5] [formerly dc8e2a443870b2391045dfb5b3cff8db04247a34 [formerly 47c821a5b2e415a926a4102f5d75a0fde1903be5] [formerly 0bbf6b4d457deae49bb03bd8c1898c945e8203ab [formerly 2cc1000889b20398f6897f558b8b64861325fddc]]]]]]]
     [formerly 39c97048532dbd98b593efb5ed329c7033b33bfd]
     [formerly 4a4dee24b856ffff8e20e4367d004ac176a7bb00 [formerly e748c240b26a470f76a6d21e984071f3232b71a4] [formerly 4a4dee24b856ffff8e20e4367d004ac176a7bb00 [formerly e748c240b26a470f76a6d21e984071f3232b71a4] [formerly 4a4dee24b856ffff8e20e4367d004ac176a7bb00 [formerly e748c240b26a470f76a6d21e984071f3232b71a4] [formerly 77fc9d37a429d2b665e23a45dfb72578d1ecc2ed [formerly 0539b99f23d615a01f474b9b8a243d03ad1ede31]]]]]
    
    
    
    Former-commit-id: e53358d30cf3fce6edf226ae5b92f1852f690bfa [formerly 6b94f0187bb7e9ea3eb74721a6233d1af4e923f6]
    Former-commit-id: 29463cac6fd19874bd87a58771b91336065222ca [formerly 91c558b60f6f635306c154227167c525adc8cde5] [formerly c6a754fa420a844848e58ce00c984549f6fdac23 [formerly 97d164c7e88aedad0fa2bd29375b82a1f5d75b82]]
    Former-commit-id: 8084fd2f86119d0049c315c5b94d492c15df0e70 [formerly 56f10fb59904a3b1c143fea76d1090fbb36aae31]
    Former-commit-id: e082df4692a7231fc6279eaf269fa60815b64f88

---
+++ plugins/inputs/external/external_test.go
@@ -0,0 +1,70 @@
+package external
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func TestRPC(t *testing.T) {
+	wg := sync.WaitGroup{}
+
+	s := &Server{
+		Listen: "/tmp/dk.sock",
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		s.Start(nil)
+	}()
+
+	time.Sleep(time.Second)
+
+	conn, err := grpc.Dial("unix://"+s.Listen, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+	c := NewDataKitClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Feed(ctx, &Request{
+		Lines: []byte(strings.Join([]string{
+			`test_a,tag1=val1,tag2=val2 f1=1i,f2=3,f3="abc",f4=T ` + fmt.Sprintf("%d", time.Now().UnixNano()),
+			`test_b,tag1=val1,tag2=val2 f1=1i,f2=3,f3="abc",f4=T ` + fmt.Sprintf("%d", time.Now().UnixNano()),
+			`test_c,tag1=val1,tag2=val2 f1=1i,f2=3,f3="abc",f4=T ` + fmt.Sprintf("%d", time.Now().UnixNano()),
+		}, "\n"))})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("[C] sending %d points ok, err: %s", r.GetPoints(), r.GetErr())
+
+	r, err = c.Feed(ctx, &Request{
+		Lines: []byte(strings.Join([]string{ // bad body
+			`test_a tag1=val1,tag2=val2 f1=1i,f2=3,f3="abc",f4=T ` + fmt.Sprintf("%d", time.Now().UnixNano()),
+			`test_b tag1=val1,tag2=val2 f1=1i,f2=3,f3="abc",f4=T ` + fmt.Sprintf("%d", time.Now().UnixNano()),
+			`test_c tag1=val1,tag2=val2 f1=1i,f2=3,f3="abc",f4=T ` + fmt.Sprintf("%d", time.Now().UnixNano()),
+		}, "\n"))})
+
+	if err != nil {
+		t.Fatal("should not been here")
+	}
+
+	log.Printf("[C] sending points: %d, err: %s", r.GetPoints(), r.GetErr())
+
+	log.Printf("stopping server...")
+	s.Stop()
+
+	wg.Wait()
+}
