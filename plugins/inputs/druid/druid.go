99f0795848e79a9f8f31576a6e1ef4a4f83c8ed8
liguozhuang <liguozhuang@jiagouyun.com>
2020-06-30 18:40:15 +0800

    add httpserver and druid collector
    Former-commit-id: 3c03a008364f40cacf2eb484949e57587b356703 [formerly bcdbb35aaa5fec902b986b70f5783b131dcc68a7] [formerly 62ba392592d3c1caa5e28260ef262cba5081a37a [formerly cf4c533b35c3788d963e11bcf331ed63185f6218]] [formerly dbc8f734af96d09ff4d2ad677b1512a728079dfb [formerly 1f505cd10e65e761d97ff830e596e1f6a6c0cc5b] [formerly 5061d237015b1a6fe6c27b394955d94ba893798f [formerly 437f1afd8a611b289bb3a6f9cbcb9f873ecc2b4f]]]
    Former-commit-id: 8854e34ba7182ce750ba6a2c183871bd96be0af2
    Former-commit-id: 3d4334e499458d288d651bc8f1ac41da0b755a37 [formerly 2f7329edd7049cc1c31dfa8805fe83867970fe2c]
    Former-commit-id: 04477e443d37f3cc7a90fdcc410a49766a14b8e3

---
+++ plugins/inputs/druid/druid.go
@@ -0,0 +1,256 @@
+package druid
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	influxdb "github.com/influxdata/influxdb1-client/v2"
+	"github.com/tidwall/gjson"
+	"go.uber.org/zap"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+)
+
+// druid config
+//
+// druid.monitoring.emissionPeriod=PT10s
+// druid.monitoring.monitors=["com.metamx.metrics.JvmMonitor"]
+// druid.emitter=none
+// druid.emitter.http.flushMillis=10000
+// druid.emitter.http.recipientBaseUrl=http://ADDR_TO_THIS_SERVICE:8424
+
+const configSample = `
+# [druid]
+# 	path = "/druid"
+`
+
+type (
+	MetricType uint8
+
+	Druid struct {
+		Config Config `toml:"druid"`
+	}
+
+	Config struct {
+		Path string `toml:"path"`
+		// Measurement string `toml:"measurement"`
+	}
+)
+
+var l *zap.SugaredLogger
+
+const (
+	Normal MetricType = iota
+	Count
+	ConvertRange
+)
+
+func init() {
+	l = logger.SLogger("druid")
+	io.RegisterRoute("druid", handle)
+}
+
+func (d *Druid) Run() {
+	io.CallRoute("druid", d.Config.Path)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		l.Error("failed of read body, err: %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	if err := extract(body); err != nil {
+		l.Error("failed of extracted metrics err: %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func extract(body []byte) error {
+
+	metrics := gjson.GetBytes(body, "#").Array()
+	if len(metrics) == 0 {
+		return nil
+	}
+
+	template := metricsTemplate()
+
+	var (
+		serviceName  string
+		metricName   string
+		metricKey    string
+		serviceValue float64
+
+		fields = make(map[string]interface{})
+	)
+
+	for _, metric := range metrics {
+
+		serviceName = metric.Get("service").String()
+
+		metricType, ok := template[serviceName]
+		if !ok {
+			continue
+		}
+
+		metricName = metric.Get("metricName").String()
+		if serviceName == "druid/peon" {
+			// Skipping all metrics from peon. These are task specific and need some
+			// thinking before sending to DataDog.
+			continue
+		}
+		metricKey = strings.Replace(serviceName+"."+metricName, "/", ".", -1)
+
+		serviceValue = metric.Get("value").Float()
+
+		switch metricType {
+		case Normal:
+			fields[metricKey] = serviceValue
+		case Count:
+			fields[metricKey] = int64(serviceValue)
+		case ConvertRange:
+			fields[metricKey] = serviceValue * 100
+		default:
+			l.Info("Unknown metric type ", metricType)
+		}
+
+	}
+
+	pt, err := influxdb.NewPoint("druid", nil, fields, time.Now())
+	if err != nil {
+		return err
+	}
+
+	return io.Feed([]byte(pt.PrecisionString("ns")), io.Metric)
+}
+
+func metricsTemplate() map[string]MetricType {
+
+	return map[string]MetricType{
+		"query/time":       Normal,
+		"query/bytes":      Count,
+		"query/node/bytes": Count,
+
+		"query/success/Count":     Count,
+		"query/interrupted/Count": Count,
+		"query/failed/Count":      Count,
+
+		"query/node/time":          Normal,
+		"query/node/ttfb":          Normal,
+		"query/intervalChunk/time": Normal,
+
+		"query/segment/time":         Normal,
+		"query/wait/time":            Normal,
+		"segment/scan/pending":       Normal,
+		"query/segmentAndCache/time": Normal,
+		"query/cpu/time":             Normal,
+
+		"query/cache/delta/numEntries":   Count,
+		"query/cache/delta/sizeBytes":    Count,
+		"query/cache/delta/hits":         Count,
+		"query/cache/delta/misses":       Count,
+		"query/cache/delta/evictions":    Count,
+		"query/cache/delta/hitRate":      ConvertRange,
+		"query/cache/delta/averageBytes": Count,
+		"query/cache/delta/timeouts":     Count,
+		"query/cache/delta/errors":       Count,
+
+		"query/cache/total/numEntries":   Normal,
+		"query/cache/total/sizeBytes":    Normal,
+		"query/cache/total/hits":         Normal,
+		"query/cache/total/misses":       Normal,
+		"query/cache/total/evictions":    Normal,
+		"query/cache/total/hitRate":      ConvertRange,
+		"query/cache/total/averageBytes": Normal,
+		"query/cache/total/timeouts":     Normal,
+		"query/cache/total/errors":       Normal,
+
+		"ingest/events/thrownAway":    Count,
+		"ingest/events/unparseable":   Count,
+		"ingest/events/processed":     Count,
+		"ingest/rows/output":          Count,
+		"ingest/persist/Count":        Count,
+		"ingest/persist/time":         Normal,
+		"ingest/persist/cpu":          Normal,
+		"ingest/persist/backPressure": Normal,
+		"ingest/persist/failed":       Count,
+		"ingest/handoff/failed":       Count,
+		"ingest/merge/time":           Normal,
+		"ingest/merge/cpu":            Normal,
+
+		"task/run/time":       Normal,
+		"segment/added/bytes": Count,
+		"segment/moved/bytes": Count,
+		"segment/nuked/bytes": Count,
+
+		"segment/assigned/Count":        Count,
+		"segment/moved/Count":           Count,
+		"segment/dropped/Count":         Count,
+		"segment/deleted/Count":         Count,
+		"segment/unneeded/Count":        Count,
+		"segment/cost/raw":              Count,
+		"segment/cost/normalization":    Count,
+		"segment/cost/normalized":       Count,
+		"segment/loadQueue/size":        Normal,
+		"segment/loadQueue/failed":      Normal,
+		"segment/loadQueue/Count":       Normal,
+		"segment/dropQueue/Count":       Normal,
+		"segment/size":                  Normal,
+		"segment/overShadowed/Count":    Normal,
+		"segment/underReplicated/Count": Normal,
+		"segment/Count":                 Normal,
+		"segment/unavailable/Count":     Normal,
+
+		"segment/max":         Normal,
+		"segment/used":        Normal,
+		"segment/usedPercent": ConvertRange,
+
+		"jvm/pool/committed":      Normal,
+		"jvm/pool/init":           Normal,
+		"jvm/pool/max":            Normal,
+		"jvm/pool/used":           Normal,
+		"jvm/bufferpool/Count":    Normal,
+		"jvm/bufferpool/used":     Normal,
+		"jvm/bufferpool/capacity": Normal,
+		"jvm/mem/init":            Normal,
+		"jvm/mem/max":             Normal,
+		"jvm/mem/used":            Normal,
+		"jvm/mem/committed":       Normal,
+		"jvm/gc/Count":            Count,
+		"jvm/gc/time":             Normal,
+
+		"ingest/events/buffered": Normal,
+
+		"sys/swap/free":        Normal,
+		"sys/swap/max":         Normal,
+		"sys/swap/pageIn":      Normal,
+		"sys/swap/pageOut":     Normal,
+		"sys/disk/write/Count": Count,
+		"sys/disk/read/Count":  Count,
+		"sys/disk/write/size":  Count,
+		"sys/disk/read/size":   Count,
+		"sys/net/write/size":   Count,
+		"sys/net/read/size":    Count,
+		"sys/fs/used":          Normal,
+		"sys/fs/max":           Normal,
+		"sys/mem/used":         Normal,
+		"sys/mem/max":          Normal,
+		"sys/storage/used":     Normal,
+		"sys/cpu":              Normal,
+
+		"coordinator-segment/Count": Normal,
+		"historical-segment/Count":  Normal,
+
+		"jetty/numopenconnections": Normal,
+	}
+}
