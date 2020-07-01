125eca808887d3f2b535689b1614665386ab7ebb
liguozhuang <liguozhuang@jiagouyun.com>
2020-07-01 13:50:26 +0800

    fix bug
    Former-commit-id: d07ac97b38267200f7889980310f8ef12d80af8d [formerly 55f5b65cc6966e90354d8f7496ebfa7375d2d2f0]
    Former-commit-id: 85e36db87a6e49314d100a12fc5853203852d3af

--- cmd/datakit/main.go
+++ cmd/datakit/main.go
@@ -152,10 +152,15 @@ func exitDatakit() {
 
 func __run() {
 
-	if err := runTelegraf(); err != nil {
-		l.Fatalf("fail to start sub service: %s", err)
-	}
-	l.Info("telegraf process exit ok")
+	config.WG.Add(1)
+	go func() {
+		defer config.WG.Done()
+		if err := runTelegraf(); err != nil {
+			l.Fatalf("fail to start sub service: %s", err)
+		}
+
+		l.Info("telegraf process exit ok")
+	}()
 
 	if err := runDatakit(); err != nil && err != context.Canceled {
 		l.Fatalf("datakit abort: %s", err)
