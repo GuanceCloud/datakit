2f7eb311291389d99c700c891fb64c2afa633464
liguozhuang <liguozhuang@jiagouyun.com>
2020-07-01 18:42:13 +0800

    add httpserver log
     [formerly e04d10755ffa1461c49550ccde8bb54af8f1e1f0] [formerly 7be4ac588cd7895a67899e310d02228faa8a2615 [formerly b5ef8326defaf7d2110e7c74cc91ac0b176556fd] [formerly 7be4ac588cd7895a67899e310d02228faa8a2615 [formerly b5ef8326defaf7d2110e7c74cc91ac0b176556fd] [formerly 7be4ac588cd7895a67899e310d02228faa8a2615 [formerly b5ef8326defaf7d2110e7c74cc91ac0b176556fd] [formerly d9ae4fe876f1e0ced4fb55577b8c03502f1f1a16 [formerly 0c969fc31e12b8fd9a702e72401fecb534268be5]]]]] [formerly 6e80199822587f9a52483595f790429494bd42cb [formerly 4bb4cf483d93afa117b5304fca08acfe7603577b] [formerly 6e80199822587f9a52483595f790429494bd42cb [formerly 4bb4cf483d93afa117b5304fca08acfe7603577b] [formerly 6e80199822587f9a52483595f790429494bd42cb [formerly 4bb4cf483d93afa117b5304fca08acfe7603577b] [formerly 968823d674a64bcf0014451b293f2a31a821b9fc [formerly 787f5319496712dc1c0d9974f349dd3fad300582]]]] [formerly a5e9f6605225820b29d610f303013e26a83a4288] [formerly 7be4ac588cd7895a67899e310d02228faa8a2615 [formerly b5ef8326defaf7d2110e7c74cc91ac0b176556fd] [formerly 7be4ac588cd7895a67899e310d02228faa8a2615 [formerly b5ef8326defaf7d2110e7c74cc91ac0b176556fd] [formerly 7be4ac588cd7895a67899e310d02228faa8a2615 [formerly b5ef8326defaf7d2110e7c74cc91ac0b176556fd] [formerly d9ae4fe876f1e0ced4fb55577b8c03502f1f1a16 [formerly 0c969fc31e12b8fd9a702e72401fecb534268be5]]]] [formerly ed372efc56c35ef7a83d69c824dec7046feab85c [formerly e8914d7e759f3845cfcff23bd5c45e120e252e2b] [formerly ed372efc56c35ef7a83d69c824dec7046feab85c [formerly e8914d7e759f3845cfcff23bd5c45e120e252e2b] [formerly ed372efc56c35ef7a83d69c824dec7046feab85c [formerly e8914d7e759f3845cfcff23bd5c45e120e252e2b] [formerly 44eeb2d4935181bfd91dc81a75b1b6e7ecd69e0a [formerly 48c4e9ac72496a5455e8aa3f238bb2b19f6fb3c6]]]]]]]
     [formerly a5e9f6605225820b29d610f303013e26a83a4288]
    
    
    
    
    Former-commit-id: 8da76105c9b8598982796831ee405c7407c857ac [formerly d0e4a6866420c72bef2bcb61e8dc9b4789542a55]
    Former-commit-id: 662594b8acb0bb8ea13bbcaa880a9d5441ccb327 [formerly 9f6762706e17ef93fc4a8ceb2471c14168efe6ac] [formerly b368a2a32fb5fae47194839f4d6b2a3073da7638 [formerly ae5152f4a3e2890ceb2c08f488a3b5de9b4a2dde]]
    Former-commit-id: 7ab8e1abff6992875d7673f9228efe77412b68f6 [formerly 88b9c2d44180bafb7af18a2d75ca3d993d2beb36]
    Former-commit-id: 20b4262a138c8cd1eaca5ac9e0b696e4769ddd80

--- io/httpserver.go
+++ io/httpserver.go
@@ -2,29 +2,42 @@ package io
 
 import (
 	"context"
+	"log"
 	"net/http"
+	"os"
+	"reflect"
 	"sync"
 	"time"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 )
 
-type handler func(http.ResponseWriter, *http.Request)
 var (
 	routeList = struct {
 		mut sync.Mutex
 		// map["/path"] = handle
-		m map[string]handler
-	}{mut: sync.Mutex{}, m: make(map[string]handler)}
+		m map[string]http.HandlerFunc
+	}{mut: sync.Mutex{}, m: make(map[string]http.HandlerFunc)}
+
+	logout *log.Logger
 )
 
-func RegisterRoute(path string, handle func(http.ResponseWriter, *http.Request)) {
+// RegiRegisterRoute
+// type HandlerFunc func(http.ResponseWriter, *http.Request)
+func RegisterRoute(path string, h http.HandlerFunc) {
 	routeList.mut.Lock()
-	routeList.m[path] = handle
+	routeList.m[path] = h
 	routeList.mut.Unlock()
 }
 
 func HTTPServer() {
+
+	var err error
+	logFile, err := os.OpenFile(config.Cfg.MainCfg.HTTPServerLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		l.Fatalf("failed to open the httpserver log file, err: %s", err.Error())
+	}
+	logout = log.New(logFile, "", 0)
 
 	mux := http.NewServeMux()
 
@@ -60,4 +73,47 @@ func HTTPServer() {
 	}
 
 	return
+}
+
+type logFormatterParams struct {
+	timeStamp  time.Time
+	statusCode int
+	latency    time.Duration
+	clientIP   string
+	method     string
+	path       string
+}
+
+func requestLogger(targetMux http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var param logFormatterParams
+
+		start := time.Now()
+		path := r.URL.Path
+		raw := r.URL.RawQuery
+
+		targetMux.ServeHTTP(w, r)
+
+		param.timeStamp = time.Now()
+		param.latency = param.timeStamp.Sub(start)
+
+		param.clientIP = r.RemoteAddr
+		param.method = r.Method
+		// "Status: 200 OK"
+		param.statusCode = int(reflect.ValueOf(w).Elem().FieldByName("status").Int())
+
+		if raw != "" {
+			path = path + "?" + raw
+		}
+		param.path = path
+
+		logout.Printf("[DataKit-HTTPServer] %v | %3d | %13v | %15s | %-7s  %#v\n",
+			param.timeStamp.Format("2006/01/02 - 15:04:05"),
+			param.statusCode,
+			param.latency,
+			param.clientIP,
+			param.method,
+			param.path,
+		)
+	})
 }
