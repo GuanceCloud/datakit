c6442d544c165268acee7930df78abaae04fab70
coanor <coanor@gmail.com>
2020-07-02 10:28:44 +0000

    update: 	[*] move datakit global settings from config to top level datakit.go 	[+] add external python building
    Former-commit-id: 6624b11473deae4bdb06ebd0d2decf4fa16afae9 [formerly 6f40625cc52c6125a3941f7ab48d4b5882e2a9ce] [formerly 67ecda014c62001829dcf1d61b6f834439fa22b1 [formerly d69bd0a1a3cab2c4d0a24860f9bce3848a3f34af]]
    Former-commit-id: e073c2159d8278641499d18573073689899e3061 [formerly 5537b373fad9a6fa705c472502f63456adbd3618]
    Former-commit-id: b84d5b873d46ea2b86cfd81f7b4fdf6cb0d4c115

--- plugins/inputs/csv/sample.go
+++ plugins/inputs/csv/sample.go
@@ -2,44 +2,45 @@ package csv
 
 var (
 	configSample = `
-start_rows = 10  # 有效数据开始行编号, 编号从 0 开始
-
-[[rules]]
-metric = "test_1"
-
-[[rules.columns]]
-index = 0
-name = "t1"
-na_action = "ignore"  # ignore/drop/abort
-as_tag = true
-
-[[rules.columns]]
-index = 1
-name = "f1"
-na_action = "ignore"
-type = "int"         # int/str/float
-as_field = true
-
-[[rules.columns]]
-index = 2
-name = "f2"
-na_action = "drop"
-type = "float"
-as_field = true
-
-[[rules.columns]]
-index = 3
-name = "f3"
-na_action = "abort"
-type = "str"
-as_field = true
-
-[[rules.columns]]
-index = 4
-name = ""
-na_action = "ignore"
-as_time = true
-time_format = "15/08/27 10:20:06" # csv/excel 中时间格式
-time_precision = "s"              # 时间戳单位: h/m/s/ms/us/ns
+#python_path = '' # 有效的 python 安装路径
+#start_rows = 10  # 有效数据开始行编号, 编号从 0 开始
+#
+#[[rules]]
+#metric = "test_1"
+#
+#[[rules.columns]]
+#index = 0
+#name = "t1"
+#na_action = "ignore"  # ignore/drop/abort
+#as_tag = true
+#
+#[[rules.columns]]
+#index = 1
+#name = "f1"
+#na_action = "ignore"
+#type = "int"         # int/str/float
+#as_field = true
+#
+#[[rules.columns]]
+#index = 2
+#name = "f2"
+#na_action = "drop"
+#type = "float"
+#as_field = true
+#
+#[[rules.columns]]
+#index = 3
+#name = "f3"
+#na_action = "abort"
+#type = "str"
+#as_field = true
+#
+#[[rules.columns]]
+#index = 4
+#name = ""
+#na_action = "ignore"
+#as_time = true
+#time_format = "15/08/27 10:20:06" # csv/excel 中时间格式
+#time_precision = "s"              # 时间戳单位: h/m/s/ms/us/ns
 `
 )
