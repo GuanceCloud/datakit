74a80309f63216ef4298c42edce7c9738558ef7f
coanor <coanor@gmail.com>
2020-07-02 10:28:44 +0000

    update: 	[*] move datakit global settings from config to top level datakit.go 	[+] add external python building
    Former-commit-id: f75cc282315ec80ef40cb6dd5de758e7fe49035f

--- run/run.go
+++ run/run.go
@@ -4,6 +4,7 @@ import (
 	"go.uber.org/zap"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
@@ -27,16 +28,16 @@ func (a *Agent) Run() error {
 
 	io.Init()
 
-	config.WG.Add(1)
+	datakit.WG.Add(1)
 	go func() {
-		defer config.WG.Done()
+		defer datakit.WG.Done()
 		io.Start()
 		l.Info("io goroutine exit")
 	}()
 
-	config.WG.Add(1)
+	datakit.WG.Add(1)
 	go func() {
-		defer config.WG.Done()
+		defer datakit.WG.Done()
 		io.GRPCServer()
 		l.Info("gRPC goroutine exit")
 	}()
@@ -63,9 +64,9 @@ func (a *Agent) runInputs() error {
 
 		case inputs.Input:
 			l.Infof("starting input %s ...", input.Config.Name)
-			config.WG.Add(1)
+			datakit.WG.Add(1)
 			go func(i inputs.Input, name string) {
-				defer config.WG.Done()
+				defer datakit.WG.Done()
 				i.Run()
 				l.Infof("input %s exited", name)
 			}(input.Input.(inputs.Input), input.Config.Name)
