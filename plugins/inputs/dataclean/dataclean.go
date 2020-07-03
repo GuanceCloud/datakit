51faeb92aa1388a2a29065b98c47592bfbedc23d
coanor <coanor@gmail.com>
2020-07-03 10:21:45 +0000

    update: 	[*] update /v1/write/metrics -> /v1/write/metrics
    Former-commit-id: ac1268087339a8d738bc155f3c0d5c4955f01eb8 [formerly 162f09d70c9d1f69a3c1d2bc5050e1f40321732e]
    Former-commit-id: 12f3d3de0cb2780807e536d28e37f4c80ed3c2e5

--- plugins/inputs/dataclean/dataclean.go
+++ plugins/inputs/dataclean/dataclean.go
@@ -132,7 +132,7 @@ func (d *DataClean) Run() {
 }
 
 func (d *DataClean) FakeDataway() string {
-	return fmt.Sprintf("http://%s/v1/write/metrics", d.BindAddr)
+	return fmt.Sprintf("http://%s/v1/write/metric", d.BindAddr)
 }
 
 func NewAgent() *DataClean {
