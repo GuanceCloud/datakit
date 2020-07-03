12f3d3de0cb2780807e536d28e37f4c80ed3c2e5
coanor <coanor@gmail.com>
2020-07-03 10:21:45 +0000

    update: 	[*] update /v1/write/metrics -> /v1/write/metrics
    Former-commit-id: 162f09d70c9d1f69a3c1d2bc5050e1f40321732e

--- cmd/installer/installer.go
+++ cmd/installer/installer.go
@@ -108,7 +108,7 @@ func main() {
 		var dwcfg *config.DataWayCfg
 		if *flagDataway == "" {
 			for {
-				dw := readInput("Please set DataWay request URL(http://IP:Port/v1/write/metrics) > ")
+				dw := readInput("Please set DataWay request URL(http://IP:Port/v1/write/metric) > ")
 				dwcfg, err = parseDataway(dw)
 				if err == nil {
 					break
