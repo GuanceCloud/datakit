4a881b67b251e0d94ceeac2ad634c8b705613bd2
huangbo <huangbo@jiagouyun.com>
2020-07-03 17:44:22 +0800

    traefik采集器按新框架修改
    Former-commit-id: 0d1a7cca2072c2b2091a3a37fbcd272722e9cfb7 [formerly 2b7b78830047a16442418264666b8f94af9ca5af] [formerly 7a8d803569bdd12cf2b5f4e147437b450bee2772 [formerly ee8cb3f3c2cfdff0e4b12e08f566de77ca408ba2]] [formerly c174eb106f759fdf45b33cc30aca2fc7a0c30744 [formerly 6c0d77942f7caf32989ecf2f6c43363965d1d667] [formerly ab69522e4b1a99e81a3f44664ee891f6ae3c08eb [formerly 068041b9369e3df1570623d8fadddb65a94bad12]]]
    Former-commit-id: 9df47a464a574efce70984d67cd33c2c4b916bdf
    Former-commit-id: bdd466a00cecc04d1d7d2cedc9e7d0203129a337 [formerly dee732eb6e798384cab965e02cef8a03b9716f45]
    Former-commit-id: 842c51f3b2ff0e6f017bb5ab1709c88478540faa

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -41,7 +41,7 @@ import (
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/timezone"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/trace"
 
-	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/traefik"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/traefik"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/ucmon"
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/yarn"
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/zabbix"
