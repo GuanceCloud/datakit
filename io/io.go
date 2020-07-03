6982d474a44b397f47064ab98ae0a13c2258f970
coanor <coanor@gmail.com>
2020-07-03 07:56:38 +0000

    update: 	[*] rename path DataFlux to dataflux 	[*] fix nats catalog 	[*] fix http server
    Former-commit-id: b704b16bd2bf2ae54a6e929444743e530435f54e

--- io/io.go
+++ io/io.go
@@ -2,9 +2,11 @@ package io
 
 import (
 	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 
 	"go.uber.org/zap"
@@ -23,6 +25,9 @@ var (
 
 	httpCli      *http.Client
 	categoryURLs map[string]string
+
+	outputFile     *os.File
+	outputFileSize int64
 
 	inited = false
 )
@@ -56,10 +61,15 @@ func Feed(data []byte, category string) error {
 		return fmt.Errorf("invalid category %s", category)
 	}
 
-	input <- &iodata{
+	select {
+	case input <- &iodata{
 		category: category,
 		data:     data,
-	} // XXX: blocking
+	}: // XXX: blocking
+
+	case <-datakit.Exit.Wait():
+		l.Warn("feed skipped on global exit")
+	}
 
 	return nil
 }
@@ -68,6 +78,14 @@ func Feed(data []byte, category string) error {
 func Init() {
 	l = logger.SLogger("io")
 	inited = true
+}
+
+func Stop() {
+	if outputFile != nil {
+		if err := outputFile.Close(); err != nil {
+			l.Error(err)
+		}
+	}
 }
 
 func Start() {
@@ -100,6 +118,8 @@ func Start() {
 		Logging:          nil,
 	}
 
+	defer Stop()
+
 	var f rtpanic.RecoverCallback
 
 	f = func(trace []byte, _ error) {
@@ -125,9 +145,7 @@ func Start() {
 				}
 
 			case <-tick.C:
-				l.Debugf("flushing...")
 				flush(cache)
-				l.Debugf("flush done")
 
 			case <-datakit.Exit.Wait():
 				l.Info("exit")
@@ -161,17 +179,42 @@ func flush(cache map[string][][]byte) {
 	}
 }
 
+func gz(data []byte) ([]byte, error) {
+	var z bytes.Buffer
+	zw := gzip.NewWriter(&z)
+	if _, err := zw.Write(data); err != nil {
+		l.Error(err)
+		return nil, err
+	}
+
+	zw.Flush()
+	zw.Close()
+	return z.Bytes(), nil
+}
+
 func doFlush(bodies [][]byte, url string) error {
 
 	if bodies == nil {
-		l.Debugf("no data, skip %s", url)
 		return nil
 	}
 
 	body := bytes.Join(bodies, []byte("\n"))
 
-	gz := false
-	if len(body) > 1024 { // Gzip ?
+	if datakit.OutputFile != "" {
+		return fileOutput(body)
+	}
+
+	gzOn := false
+	if len(body) > 1024 {
+		gzbody, err := gz(body)
+		if err != nil {
+			return err
+		}
+
+		l.Debugf("gzip %d->%d", len(body), len(gzbody))
+
+		gzOn = true
+		body = gzbody
 	}
 
 	req, err := http.NewRequest("POST", categoryURLs[url], bytes.NewBuffer(body))
@@ -183,7 +226,7 @@ func doFlush(bodies [][]byte, url string) error {
 	req.Header.Set("X-Datakit-UUID", config.Cfg.MainCfg.UUID)
 	req.Header.Set("X-Version", git.Version)
 	req.Header.Set("X-Version", datakit.DKUserAgent)
-	if gz {
+	if gzOn {
 		req.Header.Set("Content-Encoding", "gzip")
 	}
 
@@ -221,6 +264,35 @@ func doFlush(bodies [][]byte, url string) error {
 	case 5:
 		l.Warnf("post to %s failed(HTTP: %d): %s", url, resp.StatusCode, string(respbody))
 		return fmt.Errorf("dataway internal error")
+	}
+
+	return nil
+}
+
+func fileOutput(body []byte) error {
+
+	if outputFile == nil {
+		f, err := os.OpenFile(datakit.OutputFile, os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			l.Error(err)
+			return err
+		}
+
+		outputFile = f
+	}
+
+	if _, err := outputFile.Write(append(body, '\n')); err != nil {
+		l.Error(err)
+		return err
+	}
+
+	outputFileSize += int64(len(body))
+	if outputFileSize > 4*1024*1024 {
+		if err := outputFile.Truncate(0); err != nil {
+			l.Error(err)
+			return err
+		}
+		outputFileSize = 0
 	}
 
 	return nil
