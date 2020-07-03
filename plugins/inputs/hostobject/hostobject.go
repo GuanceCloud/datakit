6bc1e794ddce830a2302e7862703f558bbdec011
wuqinchao <wuqinchao@jiagouyun.com>
2020-07-03 15:32:00 +0800

    .
     [formerly f42d26148d41307ecf57f652fec8d65992a4dd01 [formerly 55810185428f582369ab0720599eabf3691d7cda] [formerly f42d26148d41307ecf57f652fec8d65992a4dd01 [formerly 55810185428f582369ab0720599eabf3691d7cda] [formerly dc5c42310792f5fe14a05c54870ed765a9d2a5f8 [formerly c3bb591d86962eb03365bf6ab70e6a0725f42dd2]]]]
    
    
    
    Former-commit-id: f5be105e3b3dcad05700ad3ab60ceeee895fc29c
    Former-commit-id: 89bf9afd53acc05fd42abad5527df9d3c85610f7 [formerly d4bafb7f23a9c703c437889af7f0ff414a41bf87]
    Former-commit-id: 48107626f1489391846b92f48f03a796b0d9c4de

--- plugins/inputs/hostobject/hostobject.go
+++ plugins/inputs/hostobject/hostobject.go
@@ -6,17 +6,19 @@ import (
 	"os"
 	"time"
 
-	"github.com/influxdata/telegraf"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 type (
 	Collector struct {
-		Name  string
-		Class string
-		Desc  string `toml:"description"`
+		Name     string
+		Class    string
+		Desc     string `toml:"description"`
+		Interval internal.Duration
 	}
 
 	osInfo struct {
@@ -34,83 +36,95 @@ func (_ *Collector) SampleConfig() string {
 	return sampleConfig
 }
 
-func (_ *Collector) Description() string {
-	return "Collect host info and send to Dataflux as object data format."
-}
+// func (_ *Collector) Description() string {
+// 	return "Collect host info and send to Dataflux as object data format."
+// }
 
-func (c *Collector) Gather(acc telegraf.Accumulator) error {
+func (c *Collector) Run() {
 
-	obj := &internal.ObjectData{
-		Name:        c.Name,
-		Description: c.Desc,
+	if err := c.initialize(); err != nil {
+		return
 	}
 
-	tags := map[string]string{
-		"uuid":    config.Cfg.MainCfg.UUID,
-		"__class": c.Class,
-	}
+	tick := time.NewTicker(c.Interval.Duration)
+	defer tick.Stop()
 
-	hostname, err := os.Hostname()
-	if err == nil {
-		tags["host"] = hostname
-	}
+	for {
 
-	ipval := getIP()
-	if mac, err := getMacAddr(ipval); err == nil && mac != "" {
-		tags["mac"] = mac
-	}
-	tags["ip"] = ipval
-
-	oi := getOSInfo()
-	tags["os_type"] = oi.OSType
-	tags["os"] = oi.Release
-
-	//tags["cpu_total"] = fmt.Sprintf("%d", runtime.NumCPU())
-
-	//meminfo, _ := mem.VirtualMemory()
-	//tags["memory_total"] = fmt.Sprintf("%v", meminfo.Total/uint64(1024*1024*1024))
-
-	for _, input := range config.Cfg.Inputs {
-		if input.Config.Name == inputName {
-			for k, v := range input.Config.Tags {
-				tags[k] = v
+		select {
+		case <-datakit.Exit.Wait():
+			return
+		case <-tick.C:
+			obj := &internal.ObjectData{
+				Name:        c.Name,
+				Description: c.Desc,
 			}
-			break
+
+			tags := map[string]string{
+				"uuid":    config.Cfg.MainCfg.UUID,
+				"__class": c.Class,
+			}
+
+			hostname, err := os.Hostname()
+			if err == nil {
+				tags["host"] = hostname
+			}
+
+			ipval := getIP()
+			if mac, err := getMacAddr(ipval); err == nil && mac != "" {
+				tags["mac"] = mac
+			}
+			tags["ip"] = ipval
+
+			oi := getOSInfo()
+			tags["os_type"] = oi.OSType
+			tags["os"] = oi.Release
+
+			//tags["cpu_total"] = fmt.Sprintf("%d", runtime.NumCPU())
+
+			//meminfo, _ := mem.VirtualMemory()
+			//tags["memory_total"] = fmt.Sprintf("%v", meminfo.Total/uint64(1024*1024*1024))
+
+			for _, input := range config.Cfg.Inputs {
+				if input.Config.Name == inputName {
+					for k, v := range input.Config.Tags {
+						tags[k] = v
+					}
+					break
+				}
+			}
+
+			obj.Tags = tags
+
+			switch c.Name {
+			case "__mac":
+				obj.Name = tags["mac"]
+			case "__ip":
+				obj.Name = tags["ip"]
+			case "__uuid":
+				obj.Name = tags["uuid"]
+			case "__host":
+				obj.Name = tags["host"]
+			case "__os":
+				obj.Name = tags["os"]
+			case "__os_type":
+				obj.Name = tags["os_type"]
+			}
+
+			data, err := json.Marshal(&obj)
+			if err == nil {
+				fields := map[string]interface{}{
+					"object": string(data),
+				}
+
+				io.FeedEx(io.Object, inputName, tags, fields)
+			}
 		}
 	}
 
-	obj.Tags = tags
-
-	switch c.Name {
-	case "__mac":
-		obj.Name = tags["mac"]
-	case "__ip":
-		obj.Name = tags["ip"]
-	case "__uuid":
-		obj.Name = tags["uuid"]
-	case "__host":
-		obj.Name = tags["host"]
-	case "__os":
-		obj.Name = tags["os"]
-	case "__os_type":
-		obj.Name = tags["os_type"]
-	}
-
-	data, err := json.Marshal(&obj)
-	if err != nil {
-		return err
-	}
-
-	fields := map[string]interface{}{
-		"object": string(data),
-	}
-
-	acc.AddFields(inputName, fields, nil, time.Now().UTC())
-
-	return nil
 }
 
-func (c *Collector) Init() error {
+func (c *Collector) initialize() error {
 
 	if c.Class == "" {
 		c.Class = "Servers"
@@ -121,6 +135,9 @@ func (c *Collector) Init() error {
 			return err
 		}
 		c.Name = name
+	}
+	if c.Interval.Duration == 0 {
+		c.Interval.Duration = 3 * time.Minute
 	}
 	return nil
 }
