8cbf56d6fefc01f17287e186a3fb9ec114dec143
liguozhuang <liguozhuang@jiagouyun.com>
2020-07-06 18:51:11 +0800

    update code
     [formerly 6e4a71dad4e01492bc663c329646a6b9a0336800] [formerly a55ecd2a7638172b9bf432ed3a4a3642aebb76a7 [formerly d9bd3e0f609bb3361bf8c44f0305ff83d11ec2c4] [formerly a55ecd2a7638172b9bf432ed3a4a3642aebb76a7 [formerly d9bd3e0f609bb3361bf8c44f0305ff83d11ec2c4] [formerly a55ecd2a7638172b9bf432ed3a4a3642aebb76a7 [formerly d9bd3e0f609bb3361bf8c44f0305ff83d11ec2c4] [formerly 05c5c378bca323dc0d069d7651674a8c446a359a [formerly 172e7f4739b2e1783ff034710bc32683208c85db]]]] [formerly d84f2c6c16afbd85c3bb8c67e4641a126c5cab9f [formerly b487bc7595b97f2ac0f51ed1a529304e88120c9e] [formerly d84f2c6c16afbd85c3bb8c67e4641a126c5cab9f [formerly b487bc7595b97f2ac0f51ed1a529304e88120c9e] [formerly d84f2c6c16afbd85c3bb8c67e4641a126c5cab9f [formerly b487bc7595b97f2ac0f51ed1a529304e88120c9e] [formerly 8e1fbd6dc29277c6f1a2903d38d8a20c8c16098d [formerly 73e3af2a7ad5b20da778735c6dbf58c8bc10c307]]]]]]
    
    
    
    
    Former-commit-id: 38dc8292b8c8d2e84f0072a2c0e68dbdda3935d3 [formerly f9e02df11d70796f6d61b04e5af241b6439eb04c]
    Former-commit-id: 3ea08bbb352cb8384db6866b6c469255ea7adf6f [formerly 87947ed33e4c0a0a668a5af49e598817871802ad] [formerly acc4cde574ce0a88e1baa00163eb2dae78da2be5 [formerly 9dcb412ee8f45a23f43d6cd1e9aa26012b6fcafb]]
    Former-commit-id: bfe18aa519f1898b09cb674ef51eb11a846b257e [formerly e28fef2a88047bfa54d119acf3fb9845ad09cd8e]
    Former-commit-id: fccd366e5f1ff1f43ae48b803dc84e2c564df2a6

--- plugins/inputs/tailf/tailf.go
+++ plugins/inputs/tailf/tailf.go
@@ -3,55 +3,66 @@
 package tailf
 
 import (
-	"context"
-	"log"
-	"sync"
+	"strings"
+	"time"
 
+	"github.com/hpcloud/tail"
 	influxdb "github.com/influxdata/influxdb1-client/v2"
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/metric"
+	"go.uber.org/zap"
 
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
-type Tailf struct {
-	Config Config `toml:"tailf"`
+const (
+	inputName = "tailf"
 
-	ctx    context.Context
-	cancel context.CancelFunc
-	acc    telegraf.Accumulator
-	wg     *sync.WaitGroup
-}
+	sampleCfg = `
+# [[tailf]]
+#	## 文件路径名。不能设置为 datakit 日志文件。
+#	filename = ""
+#
+#       ## 是否从文件首部读取
+#	from_beginning = false
+#
+#       ## 是否是一个pipe
+#	pipe = false
+#
+#	## 通知方式，默认是 inotify 即由操作系统进行变动通知
+#       ## 可以设为 poll 改为轮询文件的方式
+#	watch_method = "inotify"
+#
+#       ## 数据源名字，不可为空
+#       source = ""
+`
+)
 
-func init() {
-	inputs.Add(pluginName, func() inputs.Input {
-		t := &Tailf{}
-		return t
-	})
-}
+var (
+	l *zap.SugaredLogger
+)
 
-func (t *Tailf) Start(acc telegraf.Accumulator) error {
-
-	t.ctx, t.cancel = context.WithCancel(context.Background())
-	t.acc = acc
-	t.wg = new(sync.WaitGroup)
-
-	log.Printf("I! [Tailf] start\n")
-	log.Printf("I! [Tailf] load subscribes count %d\n", len(t.Config.Subscribes))
-	for _, sub := range t.Config.Subscribes {
-		t.wg.Add(1)
-		s := sub
-		stream := newStream(&s, t)
-		go stream.start(t.wg)
+type (
+	Tailf struct {
+		C []Impl `toml:"tailf"`
 	}
 
-	return nil
-}
+	Impl struct {
+		File          string `toml:"filename"`
+		FormBeginning bool   `toml:"from_beginning"`
+		Pipe          bool   `toml:"pipe"`
+		WatchMethod   string `toml:"watch_method"`
+		Measurement   string `toml:"source"`
+		offset        int64
+	}
+)
 
-func (t *Tailf) Stop() {
-	t.cancel()
-	t.wg.Wait()
-	log.Printf("I! [Tailf] stop\n")
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		return &Tailf{}
+	})
 }
 
 func (_ *Tailf) Catalog() string {
@@ -59,29 +70,115 @@ func (_ *Tailf) Catalog() string {
 }
 
 func (_ *Tailf) SampleConfig() string {
-	return tailfConfigSample
+	return sampleCfg
 }
 
-func (_ *Tailf) Description() string {
-	return "Convert stream a log file (like the tail -f command) to Dataway"
-}
+func (t *Tailf) Run() {
+	l = logger.SLogger(inputName)
 
-func (_ *Tailf) Gather(telegraf.Accumulator) error {
-	return nil
-}
-
-func (e *Tailf) ProcessPts(pts []*influxdb.Point) error {
-	for _, pt := range pts {
-		fields, err := pt.Fields()
-		if err != nil {
-			return err
-		}
-		pt_metric, err := metric.New(pt.Name(), pt.Tags(), fields, pt.Time())
-		if err != nil {
-			return err
-		}
-		log.Printf("D! [Tailf] metric: %v\n", pt_metric)
-		e.acc.AddMetric(pt_metric)
+	for _, i := range t.C {
+		go i.start()
 	}
-	return nil
+}
+
+func (i *Impl) start() {
+
+	if i.File == config.Cfg.MainCfg.Log {
+		l.Error("cannot collect datakit log!")
+		return
+	}
+
+	if i.Measurement == "" {
+		l.Error("invalid measurement")
+		return
+	}
+
+	var poll bool
+	// const defaultWatchMethod = "inotify"
+	if i.WatchMethod == "poll" {
+		poll = true
+	}
+
+	var seek *tail.SeekInfo
+
+	if !i.Pipe && !i.FormBeginning {
+		seek = &tail.SeekInfo{
+			Whence: 0,
+			Offset: i.offset,
+		}
+		l.Infof("using offset %d", i.offset)
+	} else {
+		seek = &tail.SeekInfo{
+			Whence: 2,
+			Offset: 0,
+		}
+	}
+
+	tailer, err := tail.TailFile(i.File,
+		tail.Config{
+			ReOpen:    true,
+			Follow:    true,
+			Location:  seek,
+			MustExist: true,
+			Poll:      poll,
+			Pipe:      i.Pipe,
+			Logger:    tail.DiscardingLogger,
+		})
+	if err != nil {
+		l.Errorf("failed to open file, err: %s", err.Error())
+		return
+	}
+
+	go i.foreachLines(tailer)
+
+	<-datakit.Exit.Wait()
+
+	if !i.Pipe && !i.FormBeginning {
+		offset, err := tailer.Tell()
+		if err == nil {
+			l.Infof("recording offset %d", offset)
+		} else {
+			l.Errorf("recording offset err:%s", err.Error())
+		}
+		i.offset = offset
+	}
+
+	if err := tailer.Stop(); err != nil {
+		l.Errorf("stop err: %s", err.Error())
+	} else {
+		l.Info("exit")
+	}
+}
+
+func (i *Impl) foreachLines(tailer *tail.Tail) {
+
+	var fields = make(map[string]interface{})
+
+	for line := range tailer.Lines {
+		if line.Err != nil {
+			l.Errorf("tailing error: %s", line.Err.Error())
+			continue
+		}
+
+		text := strings.TrimRight(line.Text, "\r")
+		fields["__content"] = text
+
+		pt, err := influxdb.NewPoint(i.Measurement, nil, fields, time.Now())
+		if err != nil {
+			l.Error(err)
+			continue
+		}
+
+		data := []byte(pt.String())
+		if err := io.Feed(data, io.Logging); err != nil {
+			l.Error(err)
+		} else {
+			l.Debugf("feed %d bytes to io ok", len(data))
+		}
+	}
+
+	l.Info("tailer stop")
+	if err := tailer.Err(); err != nil {
+		l.Errorf("tailing error: %s", err.Error())
+	}
 }
