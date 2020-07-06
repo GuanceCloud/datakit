86348eeb54f6a347b66295c9a4a9515bbd30a73b
huangbo <huangbo@jiagouyun.com>
2020-07-06 16:22:01 +0800

    statsd采集器修改
    Former-commit-id: 37d58504dad88388191414e3859c765b1f6b302e [formerly 531233089dcc6c45a1ba73703f3da808a6ed0605] [formerly a033af521c243fda35277768c4da6ab72cc5f417 [formerly ca4d2a24254a8baba9c437784b1a78c7895fadc7]] [formerly c08782c0607d5913cce9c1681d3d9847ae97b98a [formerly debb49800b1b35366f5e221728787f84e479d311] [formerly 160f30d20e652d6e951fcf99b4f59fe7950796b2 [formerly 3f07fdf8dbf479d995740e2c752e041cc01904d5]]]
    Former-commit-id: 15b5a639c59180d53296d79685e27b0bce184fdc
    Former-commit-id: d12ff587c1d6406ea2a183daaea26288c81c3b4a [formerly 6364b7ca3e2d623e4bdd7f54c76e518193e43969]
    Former-commit-id: bdeef220702c033d9b6b7b4b7c6cab30dd3cbc09

--- plugins/inputs/statsd/statsd_run.go
+++ plugins/inputs/statsd/statsd_run.go
@@ -1,77 +1,79 @@
 package statsd
 
 import (
+	"sync"
 	"bufio"
-	"context"
 	"errors"
-	"log"
 	"net"
 	"regexp"
 	"strings"
 	"time"
 
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/metric"
+	influxdb "github.com/influxdata/influxdb1-client/v2"
 
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
 var (
 	ConnectionReset = errors.New("ConnectionReset")
 )
 
-func (p *StatsdParams) gather(ctx context.Context) {
+func (p *StatsdParams) gather(wg *sync.WaitGroup) {
 	var connectFail bool = true
 	var conn net.Conn
-	var err error
+	var err  error
+	tick := time.NewTicker(time.Duration(p.input.Interval) * time.Second)
+	defer tick.Stop()
 
 	for {
 		select {
-		case <-stopChan:
-			return
-		case <-ctx.Done():
-			return
-		default:
-		}
+		case <-tick.C:
+			if connectFail {
+				conn, err = net.Dial("tcp", p.input.Host)
+				if err != nil {
+					connectFail = true
+				} else {
+					connectFail = false
+				}
+			}
 
-		if connectFail {
-			conn, err = net.Dial("tcp", p.input.Host)
-			if err != nil {
-				connectFail = true
+			if connectFail == false && conn != nil {
+				err = p.getMetrics(conn)
+				if err != nil && err != ConnectionReset {
+					Log.Errorf("getMetrics err: %s", err.Error())
+				}
+
+				if err == ConnectionReset {
+					connectFail = true
+					conn.Close()
+				}
 			} else {
-				connectFail = false
+				err = p.reportNotUp()
+				if err != nil {
+					Log.Errorf("reportNotUp err: %s", err.Error())
+				}
 			}
-		}
 
-		if connectFail == false && conn != nil {
-			err = p.getMetrics(conn)
-			if err != nil {
-				log.Printf("W! [statsd] %s", err.Error())
-			}
-			if err == ConnectionReset {
-				connectFail = true
-				conn.Close()
-			}
-		} else {
-			p.reportNotUp()
-		}
-
-		err = internal.SleepContext(ctx, time.Duration(p.input.Interval)*time.Second)
-		if err != nil {
-			log.Printf("W! [statsd] %s", err.Error())
+		case <-datakit.Exit.Wait():
+			wg.Done()
+			sqlog.Info("input statsd exit")
+			return
 		}
 	}
 }
 
 func (p *StatsdParams) getMetrics(conn net.Conn) error {
-	var metrics telegraf.Metric
+	var pt *influxdb.Point
+	var err error
+
 	tags := make(map[string]string)
 	fields := make(map[string]interface{})
 
 	tags["host"] = p.input.Host
 	fields["is_up"] = true
 
-	err := getMetric(conn, "counters", fields)
+	err = getMetric(conn, "counters", fields)
 	if err != nil {
 		goto ERR
 	}
@@ -87,17 +89,20 @@ func (p *StatsdParams) getMetrics(conn net.Conn) error {
 	}
 
 	fields["can_connect"] = true
-	metrics, err = metric.New(p.input.MetricName, tags, fields, time.Now())
+	pt, err = influxdb.NewPoint(p.input.MetricName, tags, fields, time.Now())
 	if err != nil {
 		return err
 	}
-	p.output.acc.AddMetric(metrics)
-	return nil
+	err = p.output.IoFeed([]byte(pt.String()), io.Metric)
+	if err != nil {
+		return err
+	}
 
 ERR:
 	fields["can_connect"] = false
-	metrics, _ = metric.New(p.input.MetricName, tags, fields, time.Now())
-	p.output.acc.AddMetric(metrics)
+	pt, _ = influxdb.NewPoint(p.input.MetricName, tags, fields, time.Now())
+	err = p.output.IoFeed([]byte(pt.String()), io.Metric)
+
 	return ConnectionReset
 }
 
@@ -126,7 +131,7 @@ func getMetric(conn net.Conn, msg string, fields map[string]interface{}) error {
 	return nil
 }
 
-func (p *StatsdParams) reportNotUp() {
+func (p *StatsdParams) reportNotUp() error {
 	tags := make(map[string]string)
 	fields := make(map[string]interface{})
 
@@ -134,6 +139,10 @@ func (p *StatsdParams) reportNotUp() {
 	fields["is_up"] = false
 	fields["can_connect"] = false
 
-	pointMetric, _ := metric.New(p.input.MetricName, tags, fields, time.Now())
-	p.output.acc.AddMetric(pointMetric)
+	pt, err := influxdb.NewPoint(p.input.MetricName, tags, fields, time.Now())
+	if err != nil {
+		return err
+	}
+	err = p.output.IoFeed([]byte(pt.String()), io.Metric)
+	return err
 }
