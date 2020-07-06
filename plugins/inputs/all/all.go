70dcf38ecd5ea89629cc2157f56c68df5d27cc7c
wuqinchao <wuqinchao@jiagouyun.com>
2020-07-06 18:59:28 +0800

    input配置方式更改
     [formerly c05d1b72f223e68203b161f069af2be5e8527db6] [formerly 864aa11fba528eca71b869982deca917a166c1bc [formerly 4b5e1ead6ef1deb6eeffbbb395433dcd241d5c53] [formerly 864aa11fba528eca71b869982deca917a166c1bc [formerly 4b5e1ead6ef1deb6eeffbbb395433dcd241d5c53] [formerly 864aa11fba528eca71b869982deca917a166c1bc [formerly 4b5e1ead6ef1deb6eeffbbb395433dcd241d5c53] [formerly 32499bde7fb2f34c1dcad13552729bfd13d61c00 [formerly d791d222710ed1e2e0373ecd67849af34a3e526e]]]] [formerly 37f149281b1ab7f85c92ca0a34b63255b20de143 [formerly 8d1927ad0c4d4606c67e0354cf898bf265405f82] [formerly 37f149281b1ab7f85c92ca0a34b63255b20de143 [formerly 8d1927ad0c4d4606c67e0354cf898bf265405f82] [formerly 37f149281b1ab7f85c92ca0a34b63255b20de143 [formerly 8d1927ad0c4d4606c67e0354cf898bf265405f82] [formerly 33d94aae5684919cbcd76f698bf22434c86fcb17 [formerly 81492c9d4b3e616ec358cd3732486567ed5be395]]]]]]
    
    
    
    
    Former-commit-id: c0f65a92034bac7f64dd58f5726191ee07b12acb [formerly d1b0c3ebfb4ad3c4621591aa56f5d31d0797d9d8]
    Former-commit-id: 2f98689e3a94d54d52c941824409dfbc1c545904 [formerly 5be5257f4e9141b6c36aa6fbcfdc53a389288e61] [formerly 460b4eded73623c17c010c9b12d4a049f6b1c6ad [formerly 384b33951c391f6f1ba1449755bf4a58a4abe806]]
    Former-commit-id: fe4cf7c3cc3d431771bf1776e9d5840ca2186f65 [formerly 25cd6b96ead4300501caf4fdf59738f7fde87282]
    Former-commit-id: 1ddbcf631ba423fae5b134a2ff7575bada109fa1

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -31,7 +31,7 @@ import (
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mysqlmonitor"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/prometheus"
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/replication"
-	// _ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/self"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/self"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/squid"
 
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/ssh"
@@ -39,10 +39,12 @@ import (
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/tailf"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/tencentcms"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/timezone"
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/trace"
 
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/traefik"
+	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/trace"
+
+	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/traefik"
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/ucmon"
+
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/yarn"
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/zabbix"
 
@@ -50,7 +52,7 @@ import (
 	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/binlog"
 
 	// external inputs wrap
-	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/oraclemonitor"
+	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/oraclemonitor"
 	// with dll/so dependencies, and also 32bit disabled
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/netPacket"
 	//_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/scanport"
