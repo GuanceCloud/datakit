e66686d28ff57fce48b592aedf769e1be88234f5
liguozhuang <liguozhuang@jiagouyun.com>
2020-07-06 18:51:11 +0800

    update code
    Former-commit-id: 0750b8e1908f12ebee9cbb6e9d282bcaeea4486b [formerly d9bd3e0f609bb3361bf8c44f0305ff83d11ec2c4] [formerly 05c5c378bca323dc0d069d7651674a8c446a359a [formerly 71dd010af3a3606febf968a27b29e3e86afa44b6]] [formerly d80bc43377df439ff466bfc58f01b711075a2569 [formerly 8cdc2cceee2881449ac1bccdedcd06126b7ac921] [formerly 8e1fbd6dc29277c6f1a2903d38d8a20c8c16098d [formerly 44b8da9ee3dc13b1be483615fe46cb21c1f6d010]]]
    Former-commit-id: 6e4a71dad4e01492bc663c329646a6b9a0336800
    Former-commit-id: bd318a24ca9f3dd27d31e94d947adb088766acdd [formerly 5df46d1336b52ce76fcf48686feceba7e35aff17]
    Former-commit-id: 63cbd3dc31315f13b0b68f77a1604cc43ecc147e

--- plugins/inputs/etcd/etcd.go
+++ plugins/inputs/etcd/etcd.go
@@ -1,85 +1,196 @@
 package etcd
 
 import (
-	"context"
-	"log"
-	"sync"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
 
 	influxdb "github.com/influxdata/influxdb1-client/v2"
-	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/metric"
+	"go.uber.org/zap"
 
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
-type Etcd struct {
-	Config Config `toml:"etcd"`
+const (
+	inputName = "etcd"
 
-	ctx    context.Context
-	cancel context.CancelFunc
-	acc    telegraf.Accumulator
-	wg     *sync.WaitGroup
-}
+	defaultMeasurement = "etcd"
+
+	configSample = `
+# [[etcd]]
+#       ## etcd 地址
+#	host = "127.0.0.1"
+#
+#       ## etcd 端口
+#	port = 2379
+#
+#       ## 是否开启 HTTPS TLS，如果开启则需要同时配置下面的3个路径
+#       tls_open = false
+#
+#       ## CA 证书路径
+#       tls_cacert_file = "ca.crt"
+#
+#       ## 客户端证书文件路径
+#	tls_cert_file = "peer.crt"
+#
+#	## 私钥文件路径
+#	tls_key_file = "peer.key"
+#
+#	## 采集周期，时间单位是秒
+#	collect_cycle = 60
+`
+)
+
+var l *zap.SugaredLogger
 
 func init() {
-	inputs.Add(pluginName, func() inputs.Input {
-		e := &Etcd{}
-		return e
+	inputs.Add(inputName, func() inputs.Input {
+		return &Etcd{}
 	})
 }
 
-func (e *Etcd) Start(acc telegraf.Accumulator) error {
-
-	e.ctx, e.cancel = context.WithCancel(context.Background())
-	e.acc = acc
-	e.wg = new(sync.WaitGroup)
-
-	log.Printf("I! [Etcd] start\n")
-	log.Printf("I! [Etcd] load subscribes count %d\n", len(e.Config.Subscribes))
-	for _, sub := range e.Config.Subscribes {
-		e.wg.Add(1)
-		s := sub
-		stream := newStream(&s, e)
-		go stream.start(e.wg)
+type (
+	Etcd struct {
+		C []Impl `toml:"etcd"`
 	}
-
-	return nil
-}
-
-func (e *Etcd) Stop() {
-	e.cancel()
-	e.wg.Wait()
-	log.Printf("I! [Etcd] stop\n")
-}
+	Impl struct {
+		Host       string        `toml:"host"`
+		Port       int           `toml:"port"`
+		TLSOpen    bool          `toml:"tls_open"`
+		CacertFile string        `toml:"tls_cacert_file"`
+		CertFile   string        `toml:"tls_cert_file"`
+		KeyFile    string        `toml:"tls_key_file"`
+		Cycle      time.Duration `toml:"collect_cycle"`
+		address    string
+		// HTTPS TLS
+		tlsConfig *tls.Config
+	}
+)
 
 func (_ *Etcd) SampleConfig() string {
-	return etcdConfigSample
+	return configSample
 }
 
 func (_ *Etcd) Catalog() string {
-	return "etcd"
+	return inputName
 }
 
-func (_ *Etcd) Description() string {
-	return "Convert Etcd collection data to Dataway"
-}
+func (e *Etcd) Run() {
+	l = logger.SLogger(inputName)
 
-func (_ *Etcd) Gather(telegraf.Accumulator) error {
-	return nil
-}
-
-func (e *Etcd) ProcessPts(pts []*influxdb.Point) error {
-	for _, pt := range pts {
-		fields, err := pt.Fields()
-		if err != nil {
-			return err
-		}
-		pt_metric, err := metric.New(pt.Name(), pt.Tags(), fields, pt.Time())
-		if err != nil {
-			return err
-		}
-		log.Printf("D! [Etcd] metric: %v\n", pt_metric)
-		e.acc.AddMetric(pt_metric)
+	for _, c := range e.C {
+		c.start()
 	}
-	return nil
+}
+
+func (i *Impl) start() {
+	// "https" or "http"
+	if i.TLSOpen {
+		i.address = fmt.Sprintf("https://%s:%d/metrics", i.Host, i.Port)
+		tc, err := TLSConfig(i.CacertFile, i.CertFile, i.KeyFile)
+		if err != nil {
+			l.Error(err)
+			return
+		}
+		i.tlsConfig = tc
+
+	} else {
+		i.address = fmt.Sprintf("http://%s:%d/metrics", i.Host, i.Port)
+	}
+
+	ticker := time.NewTicker(time.Second * i.Cycle)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-datakit.Exit.Wait():
+			l.Info("exit")
+			return
+
+		case <-ticker.C:
+			pt, err := i.getMetrics()
+			if err != nil {
+				l.Error(err)
+				continue
+			}
+			io.Feed([]byte(pt.String()), io.Metric)
+		}
+	}
+}
+
+func (i *Impl) getMetrics() (*influxdb.Point, error) {
+
+	client := &http.Client{}
+	client.Timeout = time.Second * 5
+	defer client.CloseIdleConnections()
+
+	if i.tlsConfig != nil {
+		client.Transport = &http.Transport{
+			TLSClientConfig: i.tlsConfig,
+		}
+	}
+
+	resp, err := client.Get(i.address)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	metrics, err := ParseV2(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(metrics) == 0 {
+		return nil, errors.New("metrics is empty")
+	}
+
+	var tags = make(map[string]string)
+	var fields = make(map[string]interface{}, len(metrics))
+
+	// prometheus to point
+	for _, metric := range metrics {
+
+		for k, v := range metric.Tags() {
+
+			// Save 4 fields from prometheus.
+			// It's STUPID!
+
+			// prometheus,action=create etcd_debugging_store_writes_total=1 1586857769285050000
+			// prometheus,action=set etcd_debugging_store_writes_total=4 1586857769285050000
+			// prometheus,action=get etcd_debugging_store_reads_total=1 1586857769285050000
+			// prometheus,action=getRecursive etcd_debugging_store_reads_total=2 1586857769285050000
+			// ============== TO =============
+			// etcd_debugging_store_writes_total_create=1
+			// etcd_debugging_store_writes_total_set=4
+			// etcd_debugging_store_reads_total_get=1
+			// etcd_debugging_store_reads_total_getRecursive=2
+
+			if _, ok := actionList[v]; ok {
+				for _k, _v := range metric.Fields() {
+					fields[_k+"_"+v] = _v
+					goto END_ACTION
+				}
+			}
+
+			if _, ok := collectList[k]; ok {
+				tags[k] = v
+			}
+		}
+
+		for k, v := range metric.Fields() {
+			if _, ok := collectList[k]; ok {
+				fields[k] = v
+			}
+		}
+
+	END_ACTION:
+	}
+
+	return influxdb.NewPoint(defaultMeasurement, tags, fields, time.Now())
 }
