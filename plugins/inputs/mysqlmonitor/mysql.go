b164f1d94bbec11fac079fb1bdb6bb8de6bb19cb
liushaobo <liushaobo@jiagouyun.com>
2020-07-08 14:55:40 +0800

    提交代码
     [formerly d8e9c51be4d6d4be680244679cff7a393ac48ced] [formerly add92e6497d419044a2f51adc159b977733a5711 [formerly 89f8a3151a2dd0eea6128b3ab75019cd566e2e1f] [formerly add92e6497d419044a2f51adc159b977733a5711 [formerly 89f8a3151a2dd0eea6128b3ab75019cd566e2e1f] [formerly add92e6497d419044a2f51adc159b977733a5711 [formerly 89f8a3151a2dd0eea6128b3ab75019cd566e2e1f] [formerly 4da9708bc4c66d2b4d939c7493937c4d09155f3f [formerly 5f60ce0c385038edfcf0fe1fda7e5146efd161ab]]]]] [formerly 6d749fa1e8355d328586c509fe6e3600a9cccd3d [formerly 20deef97315464bd838dae2d10d2aa94bd2a9e0b] [formerly 6d749fa1e8355d328586c509fe6e3600a9cccd3d [formerly 20deef97315464bd838dae2d10d2aa94bd2a9e0b] [formerly 6d749fa1e8355d328586c509fe6e3600a9cccd3d [formerly 20deef97315464bd838dae2d10d2aa94bd2a9e0b] [formerly 36e7d6d835f8cdf410b63f625fe1f7b4ebb99e6c [formerly f803b29a1ce5758e8016784ab1f5ab8bf1f3bc12]]]] [formerly 9f37a77f67f6d2114b0b977a97cf27804cb07d41] [formerly add92e6497d419044a2f51adc159b977733a5711 [formerly 89f8a3151a2dd0eea6128b3ab75019cd566e2e1f] [formerly add92e6497d419044a2f51adc159b977733a5711 [formerly 89f8a3151a2dd0eea6128b3ab75019cd566e2e1f] [formerly add92e6497d419044a2f51adc159b977733a5711 [formerly 89f8a3151a2dd0eea6128b3ab75019cd566e2e1f] [formerly 4da9708bc4c66d2b4d939c7493937c4d09155f3f [formerly 5f60ce0c385038edfcf0fe1fda7e5146efd161ab]]]] [formerly 7cdd69ffb33c960a2bd4e9d97e12aa273e064314 [formerly f5a0d6c6986d632765638d7198208ba4aa1cc384] [formerly 7cdd69ffb33c960a2bd4e9d97e12aa273e064314 [formerly f5a0d6c6986d632765638d7198208ba4aa1cc384] [formerly 7cdd69ffb33c960a2bd4e9d97e12aa273e064314 [formerly f5a0d6c6986d632765638d7198208ba4aa1cc384] [formerly 936cbcdc35cb34d8ec26718dfdfbb9f89152ed6f [formerly 567a6a6f5203752c5f51d872e5f99a6be0482407]]]]]]]
     [formerly 9f37a77f67f6d2114b0b977a97cf27804cb07d41]
    
    
    
    
    Former-commit-id: 81272821a972e1407c5792940afc3f00675ef983 [formerly ff9704e36f7a84fad581c1983ac4635ef769ab30]
    Former-commit-id: 795af1dccb75588b602ed5129891ad92a51ed496 [formerly 57b49be9b858387b6b7a3de6d4a5ea209bbe05f8] [formerly 2713f5d7cf2fc8c198087bb6881588f63ed226f2 [formerly 49ae8110048c95253a8e385e3767fd8302f87834]]
    Former-commit-id: 714aebb743cb5de36bf63bfc48e102bfa50d3235 [formerly f1931bb8db74dbbdd04c47f9c3f0347be6bb3e47]
    Former-commit-id: 1f0088152a25c54d2d9c5100a1b330faba8d495e

--- plugins/inputs/mysqlmonitor/mysql.go
+++ plugins/inputs/mysqlmonitor/mysql.go
@@ -1,7 +1,6 @@
 package mysqlmonitor
 
 import (
-	"context"
 	"fmt"
 	"reflect"
 	"strconv"
@@ -10,30 +9,22 @@ import (
 
 	"database/sql"
 
-	influxdb "github.com/influxdata/influxdb1-client/v2"
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"go.uber.org/zap"
 
 	_ "github.com/go-sql-driver/mysql"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/models"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
+var (
+	l *zap.SugaredLogger
+)
+
 type MysqlMonitor struct {
-	Mysql     []*Mysql
-	ctx       context.Context
-	cancelFun context.CancelFunc
-	logger    *models.Logger
-
-	runningInstances []*runningInstance
-}
-
-type runningInstance struct {
-	cfg        *Mysql
-	agent      *MysqlMonitor
-	logger     *models.Logger
-	db         *sql.DB
-	metricName string
+	cfg *Mysql
+	db  *sql.DB
 }
 
 func (_ *MysqlMonitor) Catalog() string {
@@ -48,113 +39,76 @@ func (_ *MysqlMonitor) Description() string {
 	return ""
 }
 
-func (_ *MysqlMonitor) Gather() error {
-	return nil
-}
+func (mysql *MysqlMonitor) Run() {
+	l = logger.SLogger("harborMonitor")
+	l.Info("harborMonitor input started...")
 
-func (_ *MysqlMonitor) Init() error {
-	return nil
-}
-
-func (o *MysqlMonitor) Run() {
-	o.logger = &models.Logger{
-		Name: `mysqlmonitor`,
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mysql.cfg.Username, mysql.cfg.Password, mysql.cfg.Host, mysql.cfg.Port, mysql.cfg.Database)
+	db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		l.Errorf("mysql connect faild %v", err)
 	}
 
-	if len(o.Mysql) == 0 {
-		o.logger.Warnf("no configuration found")
+	mysql.db = db
+
+	interval, err := time.ParseDuration(mysql.cfg.Interval)
+	if err != nil {
+		l.Error(err)
 	}
 
-	o.logger.Infof("starting...")
-
-	for _, instCfg := range o.Mysql {
-		r := &runningInstance{
-			cfg:    instCfg,
-			agent:  o,
-			logger: o.logger,
-		}
-		r.metricName = instCfg.MetricName
-		if r.metricName == "" {
-			r.metricName = "mysql_monitor"
-		}
-
-		if r.cfg.Interval.Duration == 0 {
-			r.cfg.Interval.Duration = time.Minute * 5
-		}
-
-		connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", instCfg.Username, instCfg.Password, instCfg.Host, instCfg.Port, instCfg.Database)
-		db, err := sql.Open("mysql", connStr)
-		if err != nil {
-			r.logger.Errorf("mysql connect faild %v", err)
-		}
-		r.db = db
-
-		o.runningInstances = append(o.runningInstances, r)
-
-		go r.run(o.ctx)
-	}
-}
-
-func (o *MysqlMonitor) Stop() {
-	o.cancelFun()
-}
-
-func (r *runningInstance) run(ctx context.Context) error {
-	defer func() {
-		if e := recover(); e != nil {
-
-		}
-	}()
+	tick := time.NewTicker(interval)
+	defer tick.Stop()
 
 	for {
 		select {
-		case <-ctx.Done():
-			return context.Canceled
-		default:
+		case <-tick.C:
+			// handle
+			mysql.command()
+		case <-datakit.Exit.Wait():
+			l.Info("exit")
+			return
 		}
-
-		go r.command()
-
-		internal.SleepContext(ctx, r.cfg.Interval.Duration)
-		// internal.SleepContext(ctx, 10*time.Second)
 	}
 }
 
-func (r *runningInstance) command() {
+func (mysql *MysqlMonitor) command() {
 	for key, item := range metricMap {
-		resMap, err := r.Query(item)
+		resMap, err := mysql.Query(item)
 		if err != nil {
-			r.logger.Errorf("mysql query faild %v", err)
+			l.Errorf("mysql query faild %v", err)
 		}
 
-		r.handleResponse(key, resMap)
+		mysql.handleResponse(key, resMap)
 	}
 }
 
-func (r *runningInstance) handleResponse(m string, response []map[string]interface{}) error {
+func (mysql *MysqlMonitor) handleResponse(m string, response []map[string]interface{}) error {
 	for _, item := range response {
 		tags := map[string]string{}
 
-		tags["dbName"] = r.cfg.Database
-		tags["instanceId"] = r.cfg.InstanceId
-		tags["instanceDesc"] = r.cfg.InstanceDesc
-		tags["server"] = r.cfg.Host
-		tags["port"] = r.cfg.Port
-		tags["product"] = r.cfg.Product
+		tags["dbName"] = mysql.cfg.Database
+		tags["instanceId"] = mysql.cfg.InstanceId
+		tags["instanceDesc"] = mysql.cfg.InstanceDesc
+		tags["server"] = mysql.cfg.Host
+		tags["port"] = mysql.cfg.Port
+		tags["product"] = mysql.cfg.Product
 		tags["type"] = m
 
-		pt, err := influxdb.NewPoint(r.metricName, tags, item, time.Now())
+		pt, err := io.MakeMetric(mysql.cfg.MetricName, tags, item, time.Now())
 		if err != nil {
-			return err
+			l.Errorf("make metric point error %v", err)
 		}
 
-		err = io.Feed([]byte(pt.String()), io.Metric)
+		err = io.Feed([]byte(pt), io.Metric)
+		if err != nil {
+			l.Errorf("push metric point error %v", err)
+		}
 	}
 
 	return nil
 }
 
-func (r *runningInstance) Query(sql string) ([]map[string]interface{}, error) {
+func (r *MysqlMonitor) Query(sql string) ([]map[string]interface{}, error) {
 	rows, err := r.db.Query(sql)
 	if err != nil {
 		return nil, err
@@ -205,61 +159,8 @@ func (r *runningInstance) Query(sql string) ([]map[string]interface{}, error) {
 	return list, nil
 }
 
-// func (r *runningInstance) Query(sql string) ([]map[string]interface{}, error) {
-// 	rows, err := r.db.Query(sql)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer rows.Close()
-
-// 	columns, _ := rows.Columns()
-// 	columnLength := len(columns)
-// 	cache := make([]interface{}, columnLength)
-// 	for idx, _ := range cache {
-// 		var a interface{}
-// 		cache[idx] = &a
-// 	}
-// 	var list []map[string]interface{}
-// 	for rows.Next() {
-// 		_ = rows.Scan(cache...)
-
-// 		item := make(map[string]interface{})
-// 		for i, data := range cache {
-// 			key := strings.ToLower(columns[i])
-// 			val := *data.(*interface{})
-
-// 			if val != nil {
-// 				vType := reflect.TypeOf(val)
-
-// 				switch vType.String() {
-// 				case "int64":
-// 					item[key] = val.(int64)
-// 				case "string":
-// 					var data interface{}
-// 					data, err := strconv.ParseFloat(val.(string), 64)
-// 					if err != nil {
-// 						data = val
-// 					}
-// 					item[key] = data
-// 				case "time.Time":
-// 					item[key] = val.(time.Time)
-// 				case "[]uint8":
-// 					item[key] = string(val.([]uint8))
-// 				default:
-// 					return nil, fmt.Errorf("unsupport data type '%s' now\n", vType)
-// 				}
-// 			}
-// 		}
-
-// 		list = append(list, item)
-// 	}
-// 	return list, nil
-// }
-
 func init() {
 	inputs.Add("mysqlmonitor", func() inputs.Input {
-		ac := &MysqlMonitor{}
-		ac.ctx, ac.cancelFun = context.WithCancel(context.Background())
-		return ac
+		return &MysqlMonitor{}
 	})
 }
