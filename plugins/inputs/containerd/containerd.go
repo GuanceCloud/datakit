0b8441e919450d31112b37246cf3734dafb9f257
liguozhuang <liguozhuang@jiagouyun.com>
2020-07-08 17:35:10 +0800

    update code end

--- plugins/inputs/containerd/containerd.go
+++ plugins/inputs/containerd/containerd.go
@@ -3,7 +3,6 @@
 package containerd
 
 import (
-	"bytes"
 	"time"
 
 	"go.uber.org/zap"
@@ -17,42 +16,30 @@ import (
 const (
 	inputName = "containerd"
 
-	configSample = `
+	defaultMeasurement = "containerd"
+
+	sampleCfg = `
 # [[containerd]]
-#       ## containerd 在本机的 sock 地址，一般使用默认即可
-#       host_path = "/run/containerd/containerd.sock"
-#
-#       ## 需要采集的 containerd namespace
-#       ## 可以使 'ps -ef | grep containerd | grep containerd-shim' 查看详情
-#       namespace = "moby"
-#
-#       ## 需要采集的 containerd ID 列表，ID 是一串长度为 64 的字符串
-#       ## 如果该值是 "*" ，会默认采集所有
-#       ID_list = ["*"]
-#
-#	## 采集周期，时间单位秒
-#	collect_cycle = 60
+# 	# containerd sock file, use default
+# 	host_path = "/run/containerd/containerd.sock"
+# 	
+# 	# containerd namespace
+# 	# 'ps -ef | grep containerd | grep containerd-shim' print detail
+# 	namespace = "moby"
+# 	
+# 	# containerd ID list，ID is string and length 64.
+# 	# if value is "*", collect all ID
+# 	ID_list = ["*"]
+# 	
+# 	# second
+# 	collect_cycle = 60
+# 	
+# 	# [inputs.tailf.tags]
+# 	# tags1 = "tags1"
 `
 )
 
 var l *zap.SugaredLogger
-
-type (
-	Containerd struct {
-		C []Impl `toml:"containerd"`
-	}
-
-	Impl struct {
-		HostPath  string        `toml:"host_path"`
-		Namespace string        `toml:"namespace"`
-		IDList    []string      `toml:"ID_list"`
-		Cycle     time.Duration `toml:"collect_cycle"`
-		// get all ids metrics
-		isAll bool
-		// id cache
-		ids map[string]byte
-	}
-)
 
 func init() {
 	inputs.Add(inputName, func() inputs.Input {
@@ -60,33 +47,40 @@ func init() {
 	})
 }
 
+type Containerd struct {
+	HostPath  string            `toml:"host_path"`
+	Namespace string            `toml:"namespace"`
+	IDList    []string          `toml:"ID_list"`
+	Cycle     time.Duration     `toml:"collect_cycle"`
+	Tags      map[string]string `toml:"tags"`
+	// get all ids metrics
+	isAll bool
+	// id cache
+	ids map[string]byte
+}
+
 func (_ *Containerd) Catalog() string {
 	return inputName
 }
 
 func (_ *Containerd) SampleConfig() string {
-	return configSample
+	return sampleCfg
 }
 
 func (c *Containerd) Run() {
 	l = logger.SLogger(inputName)
 
-	for _, i := range c.C {
-		go i.start()
-	}
-}
+	c.isAll = len(c.IDList) == 1 && c.IDList[0] == "*"
 
-func (i *Impl) start() {
-	i.isAll = len(i.IDList) == 1 && i.IDList[0] == "*"
-	i.ids = func() map[string]byte {
+	c.ids = func() map[string]byte {
 		m := make(map[string]byte)
-		for _, v := range i.IDList {
-			m[v] = '0'
+		for _, v := range c.IDList {
+			m[v] = 0
 		}
 		return m
 	}()
 
-	ticker := time.NewTicker(time.Second * i.Cycle)
+	ticker := time.NewTicker(time.Second * c.Cycle)
 	defer ticker.Stop()
 
 	for {
@@ -95,23 +89,16 @@ func (i *Impl) start() {
 			l.Info("exit")
 
 		case <-ticker.C:
-			pts, err := i.collectContainerd()
+			data, err := c.collectContainerd()
 			if err != nil {
 				l.Error(err)
 				continue
 			}
-
-			var data bytes.Buffer
-
-			for _, pt := range pts {
-				if _, err := data.WriteString(pt.String()); err != nil {
-					l.Error(err)
-					continue
-				}
-				data.WriteString("\n")
+			if err := io.Feed(data, io.Metric); err != nil {
+				l.Error(err)
+				continue
 			}
-
-			io.Feed(data.Bytes(), io.Metric)
+			l.Debugf("feed %d bytes to io ok", len(data))
 		}
 	}
 }
--- plugins/inputs/mongodboplog/mongodboplog_test.go
+++ plugins/inputs/mongodboplog/mongodboplog_test.go
@@ -1,42 +1,8 @@
 package mongodboplog
 
 import (
-	"fmt"
-	"sync"
 	"testing"
-	"time"
 )
 
-func TestStart(t *testing.T) {
-
-	var m = Mongodboplog{
-		Config: Config{
-			Subscribes: []Subscribe{
-				Subscribe{
-					MongodbURL:  "mongodb://10.100.64.106:30001",
-					Database:    "db123",
-					Collection:  "tb123",
-					Measurement: "test_mea",
-					Tags:        []string{"/name", "/address/home"},
-					Fields: map[string]string{
-						"/age":            "int",
-						"/address/school": "string",
-						"/ob2\\[0\\]":     "int",
-						"/ob2[1]/name":    "string",
-					},
-				},
-			},
-		},
-	}
-
-	m.wg = new(sync.WaitGroup)
-
-	for _, sub := range m.Config.Subscribes {
-		m.wg.Add(1)
-		fmt.Printf("%#v\n", sub)
-		stream := newStream(&sub, nil)
-		panic(stream.start(m.wg))
-	}
-	time.Sleep(10 * time.Second)
-
+func TestMain(t *testing.T) {
 }
