788e2afc05085adb2a166e3057a0acba0002c4d9
huangbo <huangbo@jiagouyun.com>
2020-07-10 16:34:38 +0800

    zabbix采集器修改
     [formerly e28e9b6e1d7e1d1da6b25caa59b1251dd22eed63] [formerly 8262264b10f275fe8c5f776d96dbab365e7bdda1 [formerly 32f84b708aeda4be658c9d84ef13488621b37dff] [formerly 8262264b10f275fe8c5f776d96dbab365e7bdda1 [formerly 32f84b708aeda4be658c9d84ef13488621b37dff] [formerly 8262264b10f275fe8c5f776d96dbab365e7bdda1 [formerly 32f84b708aeda4be658c9d84ef13488621b37dff] [formerly 6fe80d69e18a1abd94767add14c963ffd279f338 [formerly 5e30ea9d6d64ea038b68bfd38bc68d050eef6f0f]]]]] [formerly 9c1519e22b697f77430a08930c77bc27e2c62df8 [formerly cc73c2135cd8e59075fe11f29441d5157db8e391] [formerly 9c1519e22b697f77430a08930c77bc27e2c62df8 [formerly cc73c2135cd8e59075fe11f29441d5157db8e391] [formerly 9c1519e22b697f77430a08930c77bc27e2c62df8 [formerly cc73c2135cd8e59075fe11f29441d5157db8e391] [formerly 8941aec100e7bfc4cd8628353a1adf8a2fa42a44 [formerly e18003e875fdd94e8e90de557948357fe2379be5]]]] [formerly 2bc467cea8b3a2379c8361084ad3bac2f66a339b] [formerly 8262264b10f275fe8c5f776d96dbab365e7bdda1 [formerly 32f84b708aeda4be658c9d84ef13488621b37dff] [formerly 8262264b10f275fe8c5f776d96dbab365e7bdda1 [formerly 32f84b708aeda4be658c9d84ef13488621b37dff] [formerly 8262264b10f275fe8c5f776d96dbab365e7bdda1 [formerly 32f84b708aeda4be658c9d84ef13488621b37dff] [formerly 6fe80d69e18a1abd94767add14c963ffd279f338 [formerly 5e30ea9d6d64ea038b68bfd38bc68d050eef6f0f]]]] [formerly 117ced618c900e26c5aecb14b41c706ca68f0849 [formerly 911774a57ddd5ebff9cc3e8893ea2af23823500a] [formerly 117ced618c900e26c5aecb14b41c706ca68f0849 [formerly 911774a57ddd5ebff9cc3e8893ea2af23823500a] [formerly 117ced618c900e26c5aecb14b41c706ca68f0849 [formerly 911774a57ddd5ebff9cc3e8893ea2af23823500a] [formerly df8af2716549e65ad7b881a7c36739c8eec0ed28 [formerly c854acc7a7fb56d88939bab3e527e3c1d1a546e8]]]]]]]
     [formerly 2bc467cea8b3a2379c8361084ad3bac2f66a339b]
    
    
    
    
    Former-commit-id: 4e0211fdde61c88adbaec1efd84ddcd4fb18050c [formerly d4ac63fbba5a699b165ad65bd7727d9e5d4e3a56]
    Former-commit-id: b75b723354951f9569a34990c71a5ef1ae57f708 [formerly 3ac9454ca3383d064a7c620a4d9a121ee4e04eb8] [formerly 1438aaaf2e019302ee079d5f11d3b3aaba9c4ce5 [formerly 098321ada72fa09cb3f8b867ca1cec109bd83e8d]]
    Former-commit-id: 168f56c13ca1d2a91bac8419988bb67a5a79ef99 [formerly 473e5aec0d9be18ad9344f001fac56ccc5188d48]
    Former-commit-id: 6805eeaee07f250bb659257c90d4a694072c5218

--- plugins/inputs/zabbix/zabbix_run.go
+++ plugins/inputs/zabbix/zabbix_run.go
@@ -1,159 +1,96 @@
 package zabbix
 
 import (
-	"context"
-	"fmt"
-	influxm "github.com/influxdata/influxdb1-client/models"
-	influxdb "github.com/influxdata/influxdb1-client/v2"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
-	"log"
 	"strconv"
 	"strings"
 	"time"
+
+	influxm "github.com/influxdata/influxdb1-client/models"
+	influxdb "github.com/influxdata/influxdb1-client/v2"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 )
 
 type ZabbixInput struct {
-	provider      string
-	address       string
-	tablename     string
-	interval      int
-	hoursperbatch int
+	Zabbix
 }
 
 type ZabbixParam struct {
 	input  ZabbixInput
 	output ZabbixOutput
+	log    *logger.Logger
 }
 
-func (p *ZabbixParam) gather() error {
+func (z *ZabbixParam) gather() {
+	var start, stop time.Time
+	tbls := []string{"history", "history_uint", "trends", "trends_uint"}
+
+	ticker := time.NewTicker(time.Duration(z.input.Interval) * time.Second)
+	defer ticker.Stop()
+
+	start = z.getStartDate()
 	for {
 		select {
-		case <-stopChan:
-			p.output.cfun()
-			return nil
-		case <-p.output.ctx.Done():
-			p.output.cfun()
-			return context.Canceled
-		default:
-		}
+		case <-ticker.C:
+			stop = z.getStopDate(start)
 
-		err := p.gatherData()
-		if err != nil {
-			log.Printf("W! [Zabbix] %s", err.Error())
-			return err
-		}
+			for _, tbl := range tbls {
+				select {
+				case <-datakit.Exit.Wait():
+					z.log.Info("input zabbix exit")
+					return
+				default:
+				}
 
-		err = internal.SleepContext(p.output.ctx, time.Duration(p.input.interval)*time.Second)
-		if err != nil {
-			log.Printf("W! [Zabbix] %s", err.Error())
-			return err
+				err := z.gatherData(start, stop, tbl)
+				if err != nil {
+					z.log.Errorf("gatherData %s", err.Error())
+				}
+			}
+
+			start = stop
+			z.updateRegistry(stop)
+
+		case <-datakit.Exit.Wait():
+			z.log.Info("input zabbix exit")
+			return
 		}
 	}
 }
 
-func (p *ZabbixParam) gatherData() error {
+func (z *ZabbixParam) gatherData(start, stop time.Time, tblName string) error {
+	starttimestr := strconv.FormatInt(start.Unix(), 10)
+	endtimestr   := strconv.FormatInt(stop.Unix() , 10)
 
-	var infoLogs []string
-	var currTable string = p.input.tablename
-	var currTableForLog string = RightPad(currTable, " ", 12-len(currTable))
-
-	// read registry
-	if err := ReadRegistry(registryPath, &mapTables); err != nil {
-		return err
-	}
-
-	// set times
-	starttimereg := GetValueFromKey(mapTables, currTable)
-	startimerfc, err := time.Parse("2006-01-02T15:04:05", starttimereg)
-	if err != nil {
-		startimerfc, err = time.Parse(time.RFC3339, starttimereg)
-		if err != nil {
-			return err
-		}
-	}
-	var starttimestr string = strconv.FormatInt(startimerfc.Unix(), 10)
-	var endtimetmp time.Time = startimerfc.Add(time.Hour * time.Duration(p.input.hoursperbatch))
-	var endtimestr string = strconv.FormatInt(endtimetmp.Unix(), 10)
-
-	//
-	// <--  Extract
-	//
-	infoLogs = append(infoLogs,
-		fmt.Sprintf(
-			"----------- | %s | [%v --> %v[",
-			currTableForLog,
-			startimerfc.Format("2006-01-02 15:04:00"),
-			endtimetmp.Format("2006-01-02 15:04:00")))
-
-	//start watcher
-	startwatch := time.Now()
-	ext := NewExtracter(
-		p.input.provider,
-		p.input.address,
-		currTable,
-		starttimestr,
-		endtimestr)
-
+	ext := NewExtracter(z.input.DbType, z.input.DbAddress, tblName, starttimestr, endtimestr)
 	if err := ext.Extract(); err != nil {
 		return err
 	}
 
-	// count rows
-	var rowcount int = len(ext.Result)
-	infoLogs = append(infoLogs,
-		fmt.Sprintf(
-			"<-- Extract | %s | %v rows in %s",
-			currTableForLog,
-			rowcount,
-			time.Since(startwatch)))
+	rowcount := len(ext.Result)
+	z.log.Info("table:%s from %s to %s gather %d rows", tblName,
+		start.Format(time.RFC3339), stop.Format(time.RFC3339), rowcount)
 
-	// set max clock time
-	var maxclock time.Time = startimerfc
-	if ext.Maxclock.IsZero() == false {
-		maxclock = ext.Maxclock
-	}
-	// no row
-	if rowcount == 0 {
-		infoLogs = append(infoLogs,
-			fmt.Sprintf(
-				"--> Load    | %s | No data",
-				currTableForLog))
-	} else {
-		startwatch = time.Now()
-		inlineData := ""
-		inlineData = strings.Join(ext.Result[:], "\n")
-
-		pts, _ := ParseInfluxPts([]byte(inlineData))
-		err := p.output.ProcessPts(pts)
+	if rowcount != 0 {
+		inlineData := strings.Join(ext.Result[:], "\n")
+		pts, _, err := ParseInfluxPts([]byte(inlineData))
 		if err != nil {
-			log.Printf("W! [Zabbix] %s", err.Error())
+			return err
 		}
 
-		infoLogs = append(infoLogs,
-			fmt.Sprintf(
-				"--> Load    | %s | %v rows in %s",
-				currTableForLog,
-				rowcount,
-				time.Since(startwatch)))
+		err = z.ProcessPts(pts)
+		if err != nil {
+			return err
+		}
 	}
-
-	infoLogs = append(infoLogs,
-		fmt.Sprintf("--- Waiting | %s | %v sec ",
-			currTableForLog,
-			p.input.interval))
-	// print all log messages
-	log.Printf("I! [%s] %s\n", pluginName, infoLogs)
-
-	// Save in registry
-	saveMaxTime(currTable, startimerfc, maxclock, p.input.hoursperbatch)
-
 	return nil
 }
 
-func ParseInfluxPts(data []byte) ([]*influxdb.Point, int) {
+func ParseInfluxPts(data []byte) ([]*influxdb.Point, int, error) {
 	points, err := influxm.ParsePoints(data)
 	if err != nil {
-		return nil, 0
+		return nil, 0, err
 	}
 
 	bp := []*influxdb.Point{}
@@ -166,32 +103,13 @@ func ParseInfluxPts(data []byte) ([]*influxdb.Point, int) {
 		}
 
 		fields, _ := pt.Fields()
-		pt, err := influxdb.NewPoint(
-			measurement,
-			mapTags,
-			fields,
-			pt.Time(), //job.Ts, //m1,
-		)
-
+		pt, err := influxdb.NewPoint(measurement, mapTags, fields, pt.Time())
 		if err != nil {
 			continue
 		}
 		counts++
 		bp = append(bp, pt)
 	}
-	return bp, counts
-}
 
-func saveMaxTime(tablename string, starttime time.Time, maxtime time.Time, duration int) {
-
-	var timetosave time.Time
-
-	// if maxtime is greater than now, keep the maxclock returned
-	if (starttime.Add(time.Hour * time.Duration(duration))).After(time.Now()) {
-		timetosave = maxtime
-	} else {
-		timetosave = starttime.Add(time.Hour * time.Duration(duration))
-	}
-	//fmt.Printf("|| %s   %s  %s\n", starttime.Format(time.RFC3339), maxtime.Format(time.RFC3339), timetosave.Format(time.RFC3339))
-	SaveRegistry(registryPath, tablename, timetosave.Format(time.RFC3339))
+	return bp, counts, nil
 }
