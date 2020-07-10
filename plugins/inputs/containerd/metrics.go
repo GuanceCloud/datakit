3d10f4664173dd4443d7b8ba9354e23822ffd758
liguozhuang <liguozhuang@jiagouyun.com>
2020-07-10 18:07:13 +0800

    update code of containerd
    Former-commit-id: 86b2da5a2e6e904495eb674194fc5a8de40422f9 [formerly 88f6af4e2cc7e61c76309fa43362c6e4712425c8] [formerly 42ae35c285fdb219271d52d7c0f221f3cbd7b2e7 [formerly eca9d441871ba3d164aac7f95156ec95f6789390]] [formerly c736841ca27b506f1cce2d4f289a984ff9306749 [formerly 1d649839d0f8d4bca21f2c233b53c5c0409b009b] [formerly 8aedfd513160b2ecb1db4b14b4c36e025762789e [formerly 5ad93bfc1a58442054a0119b3e9b1c9399868c5a]]]
    Former-commit-id: b1896d0c901b3f290c2d5b6777d24fb8e33fe4e9
    Former-commit-id: 3a1784a46a20f1a0f130591bf0dfde58cdd2d1b6 [formerly f10f1c9728f258bbfd0e2443ff5f98b73491ec79]
    Former-commit-id: 6df262204794ab189cfab707f85be70c4e18651f

--- plugins/inputs/containerd/metrics.go
+++ plugins/inputs/containerd/metrics.go
@@ -6,8 +6,6 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"reflect"
-	"strings"
 	"time"
 
 	"github.com/containerd/containerd"
@@ -33,57 +31,37 @@ func (con *Containerd) collectContainerd() ([]byte, error) {
 	}
 	defer client.Close()
 
-	cs, err := client.Containers(ctx)
+	containers, err := client.Containers(ctx)
 	if err != nil {
 		return nil, err
 	}
 
+	var pts bytes.Buffer
 	var tags = make(map[string]string)
 	tags["namespace"] = con.Namespace
-	var pts bytes.Buffer
 
-	for _, c := range cs {
-		// IS UGLY !!
+	for _, container := range containers {
 
-		if _, ok := con.ids[c.ID()]; !con.isAll && !ok {
+		if _, ok := con.ids[container.ID()]; !con.isAll && !ok {
 			continue
 		}
 
-		task, err := c.Task(ctx, attach)
+		metrics, err := getMetrics(ctx, container)
 		if err != nil {
 			return nil, err
 		}
 
-		mt, err := task.Metrics(ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		data, err := typeurl.UnmarshalAny(mt.Data)
-		if err != nil {
-			return nil, err
-		}
-
-		meta, ok := data.(*v1.Metrics)
-		if !ok {
-			return nil, errors.New("invalid metrics data")
-		}
-
-		tags["id"] = c.ID()
+		tags["id"] = container.ID()
 		for k, v := range con.Tags {
 			tags[k] = v
 		}
-
-		pt, err := parseMetrics(defaultMeasurement, tags, meta)
+		fields := parseMetrics(metrics)
+		pt, err := io.MakeMetric(defaultMeasurement, tags, fields, time.Now())
 		if err != nil {
 			return nil, err
 		}
 
-		if _, err := pts.Write(pt); err != nil {
-			return nil, err
-		}
-
-		if _, err := pts.WriteString("\n"); err != nil {
+		if err := appendData(&pts, pt); err != nil {
 			return nil, err
 		}
 	}
@@ -91,52 +69,123 @@ func (con *Containerd) collectContainerd() ([]byte, error) {
 	return pts.Bytes(), nil
 }
 
-func parseMetrics(mensurement string, tags map[string]string, mt *v1.Metrics) ([]byte, error) {
-	var fields = make(map[string]interface{})
-
-	rematch(mt, "", fields)
-
-	return io.MakeMetric(defaultMeasurement, tags, fields, time.Now())
+func appendData(pts *bytes.Buffer, pt []byte) error {
+	if _, err := pts.Write(pt); err != nil {
+		return err
+	}
+	if _, err := pts.WriteString("\n"); err != nil {
+		return err
+	}
+	return nil
 }
 
-func rematch(data interface{}, succkey string, m map[string]interface{}) {
-
-	if reflect.ValueOf(data).IsNil() {
-		return
+func getMetrics(ctx context.Context, c containerd.Container) (*v1.Metrics, error) {
+	task, err := c.Task(ctx, attach)
+	if err != nil {
+		return nil, err
 	}
 
-	t := reflect.TypeOf(data).Elem()
-	v := reflect.ValueOf(data).Elem()
+	mt, err := task.Metrics(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-	for i := 0; i < v.NumField(); i++ {
+	data, err := typeurl.UnmarshalAny(mt.Data)
+	if err != nil {
+		return nil, err
+	}
 
-		key := t.Field(i).Name
+	meta, ok := data.(*v1.Metrics)
+	if !ok {
+		return nil, errors.New("invalid metrics data")
+	}
+	return meta, nil
+}
 
-		// filter 'XXX_', example:
-		//   type PidsStat struct {
-		//       Current              uint64
-		//       Limit                uint64
-		//       XXX_NoUnkeyedLiteral struct{}
-		//       XXX_unrecognized     []byte
-		//       XXX_sizecache        int32
-		//   }
+func parseMetrics(mt *v1.Metrics) map[string]interface{} {
+	var fields = make(map[string]interface{})
 
-		if strings.HasPrefix(key, "XXX_") {
-			continue
+	if mt.Pids != nil {
+		fields["pids_limit"] = int64(mt.Pids.Limit)
+		fields["pids_current"] = int64(mt.Pids.Current)
+	}
+
+	if mt.CPU != nil {
+		if mt.CPU.Usage != nil {
+			fields["cpu_usage_total"] = int64(mt.CPU.Usage.Total)
+			fields["cpu_usage_kernal"] = int64(mt.CPU.Usage.Kernel)
+			fields["cpu_usage_user"] = int64(mt.CPU.Usage.User)
 		}
 
-		switch v.Field(i).Kind() {
-
-		case reflect.Ptr:
-			rematch(v.Field(i).Interface(), succkey+key+"_", m)
-
-		case reflect.Uint64:
-			// integer
-			m[succkey+key] = int64(v.Field(i).Uint())
-
-		default:
-			// nil
-
+		if mt.CPU.Throttling != nil {
+			fields["cpu_thorttling_periods"] = int64(mt.CPU.Throttling.Periods)
+			fields["cpu_thorttling_throttled_eriods"] = int64(mt.CPU.Throttling.ThrottledPeriods)
+			fields["cpu_thorttling_throttled_ime"] = int64(mt.CPU.Throttling.ThrottledTime)
 		}
 	}
+
+	if mt.Memory != nil {
+		fields["memory_cache"] = int64(mt.Memory.Cache)
+		fields["memory_rss"] = int64(mt.Memory.RSS)
+		fields["memory_rss_huge"] = int64(mt.Memory.RSSHuge)
+		fields["memory_mapped_file"] = int64(mt.Memory.MappedFile)
+		fields["memory_dirty"] = int64(mt.Memory.Dirty)
+		fields["memory_writeback"] = int64(mt.Memory.Writeback)
+		fields["memory_pg_pg_in"] = int64(mt.Memory.PgPgIn)
+		fields["memory_pg_pg_out"] = int64(mt.Memory.PgPgOut)
+		fields["memory_pg_fault"] = int64(mt.Memory.PgFault)
+		fields["memory_pg_maj_fault"] = int64(mt.Memory.PgMajFault)
+		fields["memory_inactive_anon"] = int64(mt.Memory.InactiveAnon)
+		fields["memory_active_anon"] = int64(mt.Memory.ActiveAnon)
+		fields["memory_inactive_file"] = int64(mt.Memory.InactiveFile)
+		fields["memory_active_file"] = int64(mt.Memory.ActiveFile)
+		fields["memory_unevictable"] = int64(mt.Memory.Unevictable)
+		fields["memory_hierarchical_memory_limit"] = int64(mt.Memory.HierarchicalMemoryLimit)
+		fields["memory_hierarchical_swap_limit"] = int64(mt.Memory.HierarchicalSwapLimit)
+		fields["memory_total_cache"] = int64(mt.Memory.TotalCache)
+		fields["memory_total_rss"] = int64(mt.Memory.TotalRSS)
+		fields["memory_total_rss_huge"] = int64(mt.Memory.TotalRSSHuge)
+		fields["memory_total_mapped_file"] = int64(mt.Memory.TotalMappedFile)
+		fields["memory_total_dirty"] = int64(mt.Memory.TotalDirty)
+		fields["memory_total_writeback"] = int64(mt.Memory.TotalWriteback)
+		fields["memory_total_pg_pg_in"] = int64(mt.Memory.TotalPgPgIn)
+		fields["memory_total_pg_pg_out"] = int64(mt.Memory.TotalPgPgOut)
+		fields["memory_total_pg_fault"] = int64(mt.Memory.TotalPgFault)
+		fields["memory_total_pg_maj_fault"] = int64(mt.Memory.TotalPgMajFault)
+		fields["memory_total_inactive_anon"] = int64(mt.Memory.TotalInactiveAnon)
+		fields["memory_total_active_anon"] = int64(mt.Memory.TotalActiveAnon)
+		fields["memory_total_inactive_file"] = int64(mt.Memory.TotalInactiveFile)
+		fields["memory_total_active_file"] = int64(mt.Memory.TotalActiveFile)
+		fields["memory_total_unevictable"] = int64(mt.Memory.TotalUnevictable)
+
+		if mt.Memory.Usage != nil {
+			fields["memory_usage_limit"] = int64(mt.Memory.Usage.Limit)
+			fields["memory_usage_usage"] = int64(mt.Memory.Usage.Usage)
+			fields["memory_usage_max"] = int64(mt.Memory.Usage.Max)
+			fields["memory_usage_failcnt"] = int64(mt.Memory.Usage.Failcnt)
+		}
+
+		if mt.Memory.Swap != nil {
+			fields["memory_swap_limit"] = int64(mt.Memory.Swap.Limit)
+			fields["memory_swap_usage"] = int64(mt.Memory.Swap.Usage)
+			fields["memory_swap_max"] = int64(mt.Memory.Swap.Max)
+			fields["memory_swap_failcnt"] = int64(mt.Memory.Swap.Failcnt)
+		}
+
+		if mt.Memory.Kernel != nil {
+			fields["memory_kernel_limit"] = int64(mt.Memory.Kernel.Limit)
+			fields["memory_kernel_usage"] = int64(mt.Memory.Kernel.Usage)
+			fields["memory_kernel_max"] = int64(mt.Memory.Kernel.Max)
+			fields["memory_kernel_failcnt"] = int64(mt.Memory.Kernel.Failcnt)
+		}
+		if mt.Memory.KernelTCP != nil {
+
+			fields["memory_kernel_tcp_limit"] = int64(mt.Memory.KernelTCP.Limit)
+			fields["memory_kernel_tcp_usage"] = int64(mt.Memory.KernelTCP.Usage)
+			fields["memory_kernel_tcp_max"] = int64(mt.Memory.KernelTCP.Max)
+			fields["memory_kernel_tcp_failcnt"] = int64(mt.Memory.KernelTCP.Failcnt)
+		}
+	}
+
+	return fields
 }
