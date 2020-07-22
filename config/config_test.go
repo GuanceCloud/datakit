77a3158dd577e91849177046d007273928e4b402
tanbiao <tanbiao@jiagouyun.com>
2020-07-22 11:45:49 +0000

    update: 	[*] refact conf loading
    Former-commit-id: 2a2fe872e6821c0935f53efa2ae0612f6cb87cd9 [formerly 6424e046c593ca90241810aa4022a78660e734d4] [formerly 097a6df45d8fdba7ff35f94a8e4f8e1aaf25c211 [formerly e8aa870d7d59df9823ea3577a3044cd893a50b01]]
    Former-commit-id: da0f3d77966d51a047b18ac2fdcae78c0cd9dc4f [formerly 6c6ec38b0ccacbf5899c9029452bbd5fae21bf9f]
    Former-commit-id: dbff9053caeacca8dcb76a0f618721c899ef58b2

--- config/config_test.go
+++ config/config_test.go
@@ -1,9 +1,10 @@
 package config
 
 import (
-	//"log"
 	"testing"
-	//uuid "github.com/satori/go.uuid"
+
+	"github.com/influxdata/toml"
+	"github.com/influxdata/toml/ast"
 )
 
 func TestLoadMainCfg(t *testing.T) {
@@ -11,6 +12,80 @@ func TestLoadMainCfg(t *testing.T) {
 	c := newDefaultCfg()
 	if err := c.LoadMainConfig(); err != nil {
 		t.Errorf("%s", err)
+	}
+}
+
+func TestTomlParse(t *testing.T) {
+	x := []byte(`
+[[inputs.abc]]
+	key1 = 1
+	key2 = "a"
+	key3 = 3.14
+
+[[inputs.abc]]
+	key1 = 11
+	key2 = "aa"
+	key3 = 6.28
+
+[[inputs.abc]]
+	key1 = 22
+	key2 = "aaa"
+	key3 = 6.18
+
+[[inputs.def]]
+	key1 = 22
+	key2 = "aaa"
+	key3 = 6.18
+
+	`)
+
+	type obj struct {
+		Key1 int     `toml:"key1"`
+		Key2 string  `toml:"key2"`
+		Key3 float64 `toml:"key3"`
+	}
+
+	tbl, err := toml.Parse(x)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if tbl.Fields == nil {
+		t.Fatal("empty data")
+	}
+
+	for f, v := range tbl.Fields {
+		switch f {
+		case "inputs":
+			tbl_ := v.(*ast.Table)
+			t.Logf("tbl_: %+#v", tbl_)
+
+			for k, v_ := range tbl_.Fields {
+				// t.Logf("%s: %+#v", k, v_)
+
+				tbls := []*ast.Table{}
+
+				switch v_.(type) {
+				case []*ast.Table:
+					tbls = v_.([]*ast.Table)
+				case *ast.Table:
+					tbls = append(tbls, v_.(*ast.Table))
+				default:
+					t.Fatal("bad data")
+				}
+
+				t.Logf("elems: %d", len(tbls))
+
+				for idx, elem := range tbls {
+					var o obj
+					toml.UnmarshalTable(elem, &o)
+					t.Logf("[%s] %d: %+#v\n", k, idx, o)
+				}
+			}
+
+		default:
+			t.Fatal("bad data")
+		}
 	}
 }
 
