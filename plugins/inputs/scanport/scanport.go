94c27063a78b4be07792ab4d070c5e6469b9cadd
liushaobo <liushaobo@jiagouyun.com>
2020-07-23 11:21:58 +0800

    提交代码
     [formerly 9aade640d2dba08bddcbdc6e4b5858f28cba0c06 [formerly 7cb9456f6c87b55bc6662b0cdb131faa4b3f86a9] [formerly 28184c45645674e2a3d127b0b8128793b1b8af57 [formerly 3b1b7c723a26c4f5226572c2b14414f9ba4acc19]] [formerly 28184c45645674e2a3d127b0b8128793b1b8af57 [formerly 3b1b7c723a26c4f5226572c2b14414f9ba4acc19] [formerly 86c807cfd756912d5b4cf1d7990f260c1963553b [formerly aca958454f5d84d13e4a8ac9603f3da1e51cecb1]]]]
    
    
    
    Former-commit-id: 325460e1fe514aa99515ef6bf192002d6268fb6e
    Former-commit-id: bed11c7b038615cc90e74485c25f6c419271120e [formerly 577f3eb4a238064938c89cd5811a0f6395fc0e68]
    Former-commit-id: 33144b785ac40bc3ec243c667f6098963add9f99

--- plugins/inputs/scanport/scanport.go
+++ plugins/inputs/scanport/scanport.go
@@ -1,376 +1,246 @@
-// +build ignore
-
-// +build linux
-
 package scanport
 
 import (
-	"context"
+	"bytes"
 	"fmt"
-	"math/rand"
 	"net"
-	"strconv"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
-	"github.com/influxdata/telegraf"
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
-// scan
-type Scan struct {
-	MinPort  int
-	MaxPort  int
-	Target   string
-	lport    int
-	rport    int
-	laddr    net.IP
-	raddr    net.IP
-	ifName   string
-	network  string
-	forceV4  bool
-	forceV6  bool
-	connScan bool
-	ip       gopacket.NetworkLayer
+const (
+	pattern = "^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\/([1-9]|[1-2]\\d|3[0-1])$"
+)
+
+var (
+	l    *logger.Logger
+	name = "scanport"
+)
+
+func (_ *Scanport) Catalog() string {
+	return "scanport"
 }
 
-const sampleConfig = `
-# scan port,
-# minPort = 1
-# maxPort = 1000
-# target  = "www.dataflux.com"
- `
-const description = `scan network port`
-
-func (s *Scan) Description() string {
-	return description
+func (_ *Scanport) SampleConfig() string {
+	return configSample
 }
 
-func (s *Scan) Catalog() string {
-	return "network"
+func (_ *Scanport) Description() string {
+	return ""
 }
 
-func (s *Scan) SampleConfig() string {
-	return sampleConfig
-}
-
-func (s *Scan) Gather(acc telegraf.Accumulator) error {
-	s.exec(acc)
+func (_ *Scanport) Gather() error {
 	return nil
 }
 
-func (s *Scan) exec(acc telegraf.Accumulator) {
-	if s.IsCIDR() {
-		fmt.Errorf("it doesn't support CIDR")
-		return
+func (_ *Scanport) Init() error {
+	return nil
+}
+
+func (s *Scanport) Run() {
+	l = logger.SLogger("scanport")
+
+	l.Info("scanport input started...")
+
+	interval, err := time.ParseDuration(s.Interval)
+	if err != nil {
+		l.Error(err)
 	}
-	s.setIP()
-	openPorts := s.Run()
-	fields := make(map[string]interface{})
-	tags := make(map[string]string)
-	for _, p := range openPorts {
-		fields["port"] = p.port
-		tags["target"] = s.Target       //域名
-		tags["addr"] = s.raddr.String() //ip
-		tags["minPort"] = fmt.Sprintf("%d", s.MinPort)
-		tags["maxPort"] = fmt.Sprintf("%d", s.MaxPort)
-		acc.AddFields("scan_port", fields, tags, p.t)
+
+	tick := time.NewTicker(interval)
+	defer tick.Stop()
+
+	for {
+		select {
+		case <-tick.C:
+			// handle
+			s.handle()
+		case <-datakit.Exit.Wait():
+			l.Info("exit")
+			return
+		}
+	}
+}
+
+func (s *Scanport) checkCfg() {
+	// 采集频度
+	s.IntervalDuration = 10 * time.Minute
+
+	if s.Interval != "" {
+		du, err := time.ParseDuration(s.Interval)
+		if err != nil {
+			l.Errorf("bad interval %s: %s, use default: 10m", s.Interval, err.Error())
+		} else {
+			s.IntervalDuration = du
+		}
+	}
+
+	// 指标集名称
+	if s.Metric != "" {
+		s.Metric = "scanport"
+	}
+
+	// cidr
+	if s.Cidr != "" {
+		matched, err := regexp.MatchString(pattern, s.Cidr)
+		if err != nil {
+			l.Errorf("incorrect input cidr config `%s': %s", s.Cidr, err)
+		}
+
+		if !matched {
+			l.Errorf("incorrect input cidr config `%s': %s", s.Cidr, err)
+		}
+	}
+
+	if len(s.Ips) == 0 {
+		ipList, err := s.walkCIDR()
+		if err != nil {
+			l.Errorf("incorrect input cidr config `%s': %s", s.Cidr, err)
+		}
+
+		s.Ips = append(s.Ips, ipList...)
+	}
+
+	s.Timeout = time.Millisecond
+}
+
+// handle
+func (s *Scanport) handle() error {
+	lines := [][]byte{}
+
+	jobCh := s.genJob()
+	retCh := make(chan []byte, 200)
+
+	wg := new(sync.WaitGroup)
+
+	workPool(50, jobCh, retCh, wg)
+
+	wg.Wait()
+	close(retCh)
+	for ret := range retCh {
+		lines = append(lines, ret)
+	}
+
+	pushLines := bytes.Join(lines, []byte("\n"))
+	err := io.NamedFeed(pushLines, io.Metric, name)
+	if err != nil {
+		l.Errorf("push metric point error %s", err)
+	}
+	return nil
+}
+
+func (s *Scanport) genJob() <-chan task {
+	jobCh := make(chan task, 200)
+
+	go func() {
+		for _, proto := range s.Protocol {
+			for _, ip := range s.Ips {
+				for port := s.PortStart; port <= s.PortEnd; port++ {
+					t := task{
+						proto:   proto,
+						metric:  s.Metric,
+						ip:      ip,
+						port:    port,
+						timeout: s.Timeout,
+					}
+
+					jobCh <- t
+				}
+			}
+		}
+		close(jobCh)
+	}()
+
+	return jobCh
+}
+
+// cidr解析
+func (s *Scanport) walkCIDR() ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(s.Cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	// remove network address and broadcast address
+	return ips[1 : len(ips)-1], nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
+// dial
+func dial(protocol string, ip string, port int, timeout time.Duration) bool {
+	conn, err := net.DialTimeout(protocol, fmt.Sprintf("%s:%d", ip, port), timeout)
+	if err != nil {
+		if strings.Contains(err.Error(), "too many open files") {
+			l.Errorf("too many open files %v", err.Error())
+		}
+		return false
+	}
+
+	if conn != nil {
+		defer conn.Close()
+	}
+
+	return true
+}
+
+// workpool
+func workPool(n int, jobCh <-chan task, retCh chan<- []byte, wg *sync.WaitGroup) {
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go work(i, jobCh, retCh, wg)
+	}
+}
+
+type task struct {
+	metric  string
+	proto   string
+	ip      string
+	port    int
+	timeout time.Duration
+}
+
+func work(id int, jobCh <-chan task, retCh chan<- []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobCh {
+		if dial(job.proto, job.ip, job.port, job.timeout) {
+			fields := make(map[string]interface{})
+			tags := make(map[string]string)
+
+			tags["protocol"] = job.proto
+			fields["port"] = job.port
+			fields["ip"] = job.ip
+
+			ptline, err := io.MakeMetric(job.metric, tags, fields, time.Now())
+			if err != nil {
+				l.Errorf("new point failed: %s", err.Error())
+			}
+
+			retCh <- ptline
+		}
 	}
 }
 
 func init() {
-	inputs.Add("scanport", func() inputs.Input { return &Scan{} })
-}
-
-// IsCIDR checks the target if it's CIDR
-func (s *Scan) IsCIDR() bool {
-	_, _, err := net.ParseCIDR(s.Target)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-func isIPv4(ip net.IP) bool {
-	return len(ip.To4()) == net.IPv4len
-}
-
-func isIPv6(ip net.IP) bool {
-	if r := strings.Index(ip.String(), ":"); r != -1 {
-		return true
-	}
-	return false
-}
-
-func (s *Scan) setIP() error {
-	ips, err := net.LookupIP(s.Target)
-	if err != nil {
-		return err
-	}
-	for _, ip := range ips {
-		if isIPv4(ip) && !s.forceV6 {
-			s.raddr = ip
-			s.network = "ip4"
-			break
-		} else if isIPv6(ip) && !s.forceV4 {
-			s.raddr = ip
-			s.network = "ip6"
-			break
-		}
-	}
-	return nil
-}
-
-// Run tries to scan wide range ports (TCP)
-func (s *Scan) Run() []point {
-	var (
-		openPorts []point
-		err       error
-	)
-	if s.MinPort != s.MaxPort {
-		fmt.Printf("Scan %s (%s) TCP ports %d-%d\n", s.Target, s.raddr, s.MinPort, s.MaxPort)
-	} else {
-		fmt.Printf("Scan %s (%s) TCP port %d\n", s.Target, s.raddr, s.MinPort)
-	}
-	tStart := time.Now()
-	if s.connScan {
-		openPorts = s.tcpConnScan()
-	} else {
-		openPorts, err = s.tcpSYNScan()
-	}
-	if err != nil {
-		println(err.Error())
-		return nil
-	}
-	if len(openPorts) == 0 {
-		println("there isn't any opened port")
-	} else {
-		println("")
-		elapsed := fmt.Sprintf("%.3f seconds", time.Since(tStart).Seconds())
-		println("Scan done:", len(openPorts), "opened port(s) found in", elapsed)
-	}
-	return openPorts
-}
-
-func (s *Scan) packetDataTCP(rport int) (error, []byte) {
-	tcp := &layers.TCP{
-		SrcPort: layers.TCPPort(s.lport),
-		DstPort: layers.TCPPort(rport),
-		Seq:     1,
-		SYN:     true,
-		Window:  15000,
-	}
-	tcp.SetNetworkLayerForChecksum(s.ip)
-	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{
-		ComputeChecksums: true,
-		FixLengths:       true,
-	}
-	if err := gopacket.SerializeLayers(buf, opts, tcp); err != nil {
-		return err, []byte{}
-	}
-	return nil, buf.Bytes()
-}
-
-func (s *Scan) setProto(proto string) error {
-	var netProto = fmt.Sprintf("%s:%s", s.network, proto)
-	switch netProto {
-	case "ip4:tcp":
-		s.ip = &layers.IPv4{
-			SrcIP:    s.laddr,
-			DstIP:    s.raddr,
-			Protocol: layers.IPProtocolTCP,
-		}
-	case "ip4:udp":
-		s.ip = &layers.IPv4{
-			SrcIP:    s.laddr,
-			DstIP:    s.raddr,
-			Protocol: layers.IPProtocolUDP,
-		}
-	case "ip6:tcp":
-		s.ip = &layers.IPv6{
-			SrcIP:      s.laddr,
-			DstIP:      s.raddr,
-			NextHeader: layers.IPProtocolTCP,
-		}
-	case "ip6:udp":
-		s.ip = &layers.IPv6{
-			SrcIP:      s.laddr,
-			DstIP:      s.raddr,
-			NextHeader: layers.IPProtocolUDP,
-		}
-	}
-	return nil
-}
-
-func (s *Scan) setLocalNet() error {
-	conn, err := net.Dial("udp", net.JoinHostPort(s.raddr.String(), "80"))
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	if lAddr, ok := conn.LocalAddr().(*net.UDPAddr); ok {
-		s.laddr = lAddr.IP
-		s.lport = lAddr.Port
-	} else {
-		return fmt.Errorf("can not find local address/port")
-	}
-	ifs, _ := net.Interfaces()
-	for _, i := range ifs {
-		addrs, _ := i.Addrs()
-		for _, addr := range addrs {
-			ip, _, _ := net.ParseCIDR(addr.String())
-			if ip.String() == s.laddr.String() {
-				s.ifName = i.Name
-				break
-			}
-		}
-	}
-	return nil
-}
-
-func (s *Scan) tcpSYNScan() ([]point, error) {
-	var err error
-	ctx, cancel := context.WithCancel(context.Background())
-	if err = s.setLocalNet(); err != nil {
-		return nil, fmt.Errorf("source IP address not configured")
-	}
-	if err = s.setProto("tcp"); err != nil {
-		println(err.Error())
-	}
-	go func() {
-		if err := s.sendTCPSYN(); err != nil {
-			println(err.Error())
-		}
-		cancel()
-	}()
-	openPorts, err := s.pCapture(ctx)
-	return openPorts, err
-}
-
-func (s *Scan) pCapture(ctx context.Context) ([]point, error) {
-	var (
-		tcp       *layers.TCP
-		timeout   = 100 * time.Nanosecond
-		openPorts []point
-		ok        bool
-	)
-	handle, err := pcap.OpenLive(s.ifName, 6*1024, false, timeout)
-	if err != nil {
-		return nil, err
-	}
-	defer handle.Close()
-	filter := "tcp and src host " + s.raddr.String()
-	if err := handle.SetBPFFilter(filter); err != nil {
-		return nil, err
-	}
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-LOOP:
-	for {
-		select {
-		case <-ctx.Done():
-			break LOOP
-		case packet := <-packetSource.Packets():
-			tcpLayer := packet.Layer(layers.LayerTypeTCP)
-			if tcpLayer != nil {
-				if tcp, ok = tcpLayer.(*layers.TCP); !ok {
-					continue
-				}
-				if tcp.SYN && tcp.ACK {
-					p, _ := strconv.Atoi(fmt.Sprintf("%d", tcp.SrcPort))
-					pt := point{
-						port: p,
-						t:    time.Now(),
-					}
-					// ports = append(ports, pt)
-					openPorts = append(openPorts, pt)
-				}
-				continue
-			}
-		}
-	}
-	return openPorts, nil
-}
-
-func (s *Scan) sendTCPSYN() error {
-	var (
-		buf  []byte
-		err  error
-		conn net.PacketConn
-	)
-	if s.network != "ip6" {
-		conn, err = net.ListenPacket(s.network+":tcp", "0.0.0.0")
-	} else {
-		conn, err = net.ListenPacket(s.network+":tcp", "::")
-	}
-	if err != nil {
-		return err
-	}
-	for i := s.MinPort; i <= s.MaxPort; i++ {
-		if err, buf = s.packetDataTCP(i); err != nil {
-			return err
-		}
-		if _, err := conn.WriteTo(buf, &net.IPAddr{IP: s.raddr}); err != nil {
-			println(err.Error())
-		}
-		time.Sleep(5 * time.Millisecond)
-	}
-	time.Sleep(1 * time.Second)
-	return nil
-}
-
-type point struct {
-	port int
-	t    time.Time
-}
-
-// tcpConnScan tries to scan a single host
-func (s *Scan) tcpConnScan() []point {
-	var wg sync.WaitGroup
-	var ports = make([]point, 0)
-	for i := s.MinPort; i <= s.MaxPort; i++ {
-		wg.Add(1)
-		go func(i int) {
-			for {
-				host := net.JoinHostPort(s.raddr.String(), fmt.Sprintf("%d", i))
-				conn, err := net.DialTimeout("tcp", host, 2*time.Second)
-				if err != nil {
-					if strings.Contains(err.Error(), "too many open files") {
-						// random back-off
-						time.Sleep(time.Duration(10+rand.Int31n(30)) * time.Millisecond)
-						continue
-					}
-					wg.Done()
-					return
-				}
-				conn.Close()
-				break
-			}
-			p := point{
-				port: i,
-				t:    time.Now(),
-			}
-			ports = append(ports, p)
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
-	// sort.Ints(ports)
-	return ports
-}
-
-func uniqSlice(s []int) []int {
-	m := map[int]bool{}
-	r := []int{}
-	for _, v := range s {
-		if _, ok := m[v]; !ok {
-			m[v] = true
-			r = append(r, v)
-		}
-	}
-	return r
+	inputs.Add(name, func() inputs.Input {
+		return &Scanport{}
+	})
 }
