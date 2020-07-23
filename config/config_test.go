277c6573329a6d3457dadcb06c6faa6ee03e897e
tanbiao <tanbiao@jiagouyun.com>
2020-07-23 11:28:42 +0000

    update: 	[*] allow 		- config loading within confd dir 		- merging multiple datakit inputs config into 1 conf 		- merging multiple telegraf inputs config into 1 conf 		- DO NOT merging telegraf and datakit inputs config into 1 conf
     [formerly ee179a237530d55502f078158e5ef5925a04285c [formerly 3d10a5e309c4276f897a500de959d6dfb51db4dc]]
    
    
    
    Former-commit-id: 9edc1f5dc676701c7f32662b235a3e8bfa5646df

--- config/config_test.go
+++ config/config_test.go
@@ -15,6 +15,73 @@ func TestLoadMainCfg(t *testing.T) {
 	}
 }
 
+func TestTomlUnmarshal(t *testing.T) {
+	x := []byte(`
+global = "global config"
+[[inputs.abc]]
+	key1 = 1
+	key2 = "a"
+	key3 = 3.14
+
+[[inputs.abc]]
+	key1 = 11
+	key2 = "aa"
+	key3 = 6.28
+
+[[inputs.def]]
+	key1 = 22
+	key2 = "aaa"
+	key3 = 6.18
+
+[inputs.xyz]
+	key1 = 22
+	key2 = "aaa"
+	key3 = 6.18
+
+	[[inputs.xyz.tags]]
+		key1 = 22
+		key2 = "aaa"
+		key3 = 6.18
+		#key4 = 7.18
+	`)
+
+	tbl, err := toml.Parse(x)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("tbl: %+#v", tbl)
+	t.Log(tbl.Source())
+
+	for f, v := range tbl.Fields {
+
+		switch f {
+		case "inputs":
+			switch v.(type) {
+			case *ast.Table:
+				tbl_ := v.(*ast.Table)
+
+				for _, v_ := range tbl_.Fields {
+					switch v_.(type) {
+					case []*ast.Table:
+						for idx, elem := range v_.([]*ast.Table) {
+							t.Logf("[%d] %+#v, source: %s", idx, elem, elem.Source())
+						}
+					case *ast.Table:
+						t.Logf("%+#v, source: %s", v_.(*ast.Table), v_.(*ast.Table).Source())
+					default:
+						t.Log("bad data")
+					}
+				}
+
+			default:
+				t.Log("unknown type")
+			}
+
+		}
+	}
+}
+
 func TestTomlParse(t *testing.T) {
 	x := []byte(`
 [[inputs.abc]]
