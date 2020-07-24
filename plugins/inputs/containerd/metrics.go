f277074b4f1278328a40070d1b75fc59b3aaf7b5
liguozhuang <liguozhuang@jiagouyun.com>
2020-07-24 17:29:02 +0800

    update config
    Former-commit-id: 8dc061c89cdfc8424163128e4a9fd672cf93bc6a [formerly b60091fcd39deb15bd6e6623a647eb8f1660c342]
    Former-commit-id: 546595c33ce58cfd5b8d8e6fb96bf4a8c6984fc2

--- plugins/inputs/containerd/metrics.go
+++ plugins/inputs/containerd/metrics.go
@@ -6,6 +6,8 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"reflect"
+	"strings"
 	"time"
 
 	"github.com/containerd/containerd"
@@ -104,88 +106,47 @@ func getMetrics(ctx context.Context, c containerd.Container) (*v1.Metrics, error
 
 func parseMetrics(mt *v1.Metrics) map[string]interface{} {
 	var fields = make(map[string]interface{})
-
-	if mt.Pids != nil {
-		fields["pids_limit"] = int64(mt.Pids.Limit)
-		fields["pids_current"] = int64(mt.Pids.Current)
-	}
-
-	if mt.CPU != nil {
-		if mt.CPU.Usage != nil {
-			fields["cpu_usage_total"] = int64(mt.CPU.Usage.Total)
-			fields["cpu_usage_kernal"] = int64(mt.CPU.Usage.Kernel)
-			fields["cpu_usage_user"] = int64(mt.CPU.Usage.User)
-		}
-
-		if mt.CPU.Throttling != nil {
-			fields["cpu_thorttling_periods"] = int64(mt.CPU.Throttling.Periods)
-			fields["cpu_thorttling_throttled_eriods"] = int64(mt.CPU.Throttling.ThrottledPeriods)
-			fields["cpu_thorttling_throttled_ime"] = int64(mt.CPU.Throttling.ThrottledTime)
-		}
-	}
-
-	if mt.Memory != nil {
-		fields["memory_cache"] = int64(mt.Memory.Cache)
-		fields["memory_rss"] = int64(mt.Memory.RSS)
-		fields["memory_rss_huge"] = int64(mt.Memory.RSSHuge)
-		fields["memory_mapped_file"] = int64(mt.Memory.MappedFile)
-		fields["memory_dirty"] = int64(mt.Memory.Dirty)
-		fields["memory_writeback"] = int64(mt.Memory.Writeback)
-		fields["memory_pg_pg_in"] = int64(mt.Memory.PgPgIn)
-		fields["memory_pg_pg_out"] = int64(mt.Memory.PgPgOut)
-		fields["memory_pg_fault"] = int64(mt.Memory.PgFault)
-		fields["memory_pg_maj_fault"] = int64(mt.Memory.PgMajFault)
-		fields["memory_inactive_anon"] = int64(mt.Memory.InactiveAnon)
-		fields["memory_active_anon"] = int64(mt.Memory.ActiveAnon)
-		fields["memory_inactive_file"] = int64(mt.Memory.InactiveFile)
-		fields["memory_active_file"] = int64(mt.Memory.ActiveFile)
-		fields["memory_unevictable"] = int64(mt.Memory.Unevictable)
-		fields["memory_hierarchical_memory_limit"] = int64(mt.Memory.HierarchicalMemoryLimit)
-		fields["memory_hierarchical_swap_limit"] = int64(mt.Memory.HierarchicalSwapLimit)
-		fields["memory_total_cache"] = int64(mt.Memory.TotalCache)
-		fields["memory_total_rss"] = int64(mt.Memory.TotalRSS)
-		fields["memory_total_rss_huge"] = int64(mt.Memory.TotalRSSHuge)
-		fields["memory_total_mapped_file"] = int64(mt.Memory.TotalMappedFile)
-		fields["memory_total_dirty"] = int64(mt.Memory.TotalDirty)
-		fields["memory_total_writeback"] = int64(mt.Memory.TotalWriteback)
-		fields["memory_total_pg_pg_in"] = int64(mt.Memory.TotalPgPgIn)
-		fields["memory_total_pg_pg_out"] = int64(mt.Memory.TotalPgPgOut)
-		fields["memory_total_pg_fault"] = int64(mt.Memory.TotalPgFault)
-		fields["memory_total_pg_maj_fault"] = int64(mt.Memory.TotalPgMajFault)
-		fields["memory_total_inactive_anon"] = int64(mt.Memory.TotalInactiveAnon)
-		fields["memory_total_active_anon"] = int64(mt.Memory.TotalActiveAnon)
-		fields["memory_total_inactive_file"] = int64(mt.Memory.TotalInactiveFile)
-		fields["memory_total_active_file"] = int64(mt.Memory.TotalActiveFile)
-		fields["memory_total_unevictable"] = int64(mt.Memory.TotalUnevictable)
-
-		if mt.Memory.Usage != nil {
-			fields["memory_usage_limit"] = int64(mt.Memory.Usage.Limit)
-			fields["memory_usage_usage"] = int64(mt.Memory.Usage.Usage)
-			fields["memory_usage_max"] = int64(mt.Memory.Usage.Max)
-			fields["memory_usage_failcnt"] = int64(mt.Memory.Usage.Failcnt)
-		}
-
-		if mt.Memory.Swap != nil {
-			fields["memory_swap_limit"] = int64(mt.Memory.Swap.Limit)
-			fields["memory_swap_usage"] = int64(mt.Memory.Swap.Usage)
-			fields["memory_swap_max"] = int64(mt.Memory.Swap.Max)
-			fields["memory_swap_failcnt"] = int64(mt.Memory.Swap.Failcnt)
-		}
-
-		if mt.Memory.Kernel != nil {
-			fields["memory_kernel_limit"] = int64(mt.Memory.Kernel.Limit)
-			fields["memory_kernel_usage"] = int64(mt.Memory.Kernel.Usage)
-			fields["memory_kernel_max"] = int64(mt.Memory.Kernel.Max)
-			fields["memory_kernel_failcnt"] = int64(mt.Memory.Kernel.Failcnt)
-		}
-		if mt.Memory.KernelTCP != nil {
-
-			fields["memory_kernel_tcp_limit"] = int64(mt.Memory.KernelTCP.Limit)
-			fields["memory_kernel_tcp_usage"] = int64(mt.Memory.KernelTCP.Usage)
-			fields["memory_kernel_tcp_max"] = int64(mt.Memory.KernelTCP.Max)
-			fields["memory_kernel_tcp_failcnt"] = int64(mt.Memory.KernelTCP.Failcnt)
-		}
-	}
-
+	deepHit(mt, "", fields)
 	return fields
+}
+
+func deepHit(data interface{}, prefix string, m map[string]interface{}) {
+
+	if reflect.ValueOf(data).IsNil() {
+		return
+	}
+
+	t := reflect.TypeOf(data).Elem()
+	v := reflect.ValueOf(data).Elem()
+
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).CanInterface() {
+			key := strings.ToLower(t.Field(i).Name)
+
+			// filter 'XXX_', example:
+			//   type PidsStat struct {
+			//       Current              uint64
+			//       Limit                uint64
+			//       XXX_NoUnkeyedLiteral struct{}
+			//       XXX_unrecognized     []byte
+			//       XXX_sizecache        int32
+			//   }
+			if strings.HasPrefix(key, "XXX_") {
+				continue
+			}
+
+			switch v.Field(i).Kind() {
+
+			case reflect.Ptr:
+				deepHit(v.Field(i).Interface(), prefix+key+"_", m)
+
+			case reflect.Uint64:
+				// integer
+				m[prefix+key] = int64(v.Field(i).Uint())
+
+			default:
+				// nil
+			}
+		}
+	}
 }
