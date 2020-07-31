3281e3272d6bbfb31e2a35f4b207a2de1a9da861
liushaobo <liushaobo@jiagouyun.com>
2020-07-31 10:22:36 +0800

    提交
     [formerly dfa55ff035c90d61d09244a53e1ea8dfcc5623b4 [formerly 9fe4ecbb28ce91a11ea92c35696b9fbf1219635b] [formerly 69a44e14d064b6bf122345e5e40bbcf86688a880 [formerly 760485c949edeb488831b7ea2f8d4f9923677843]] [formerly 69a44e14d064b6bf122345e5e40bbcf86688a880 [formerly 760485c949edeb488831b7ea2f8d4f9923677843] [formerly 0936bf2d0100e8dc1768535c3e3b25d971951635 [formerly 222e3ce3090c809e2a7a194ff6ce75afe087e94b]]]]
    
    
    
    Former-commit-id: 92a1ce8fc596d16fe0070bc209e3d9046661e060
    Former-commit-id: 1b8dc87fde780e7095c25f8cb2990419399428c2 [formerly bcb8518de55b2d49eafc9dd2074e42c2d8122825]
    Former-commit-id: 8016acfa2f6017886b048e38d01e90d1dd426607

--- plugins/inputs/httpstat/conf.go
+++ plugins/inputs/httpstat/conf.go
@@ -1,18 +1,17 @@
 package httpstat
 
-const (
-	pluginName = "httpstat"
+import "time"
 
+const (
 	description = `stat http protocol request time, contain dnsLookup, tcpConnection, tlsHandshake,
 	serverProcessing, contentTransfer, and total time`
 	httpstatConfigSample = `
-#    [[inputs.httpstat]]
-#    ##if empty, use "httpstat"
-#    metricName = ''
-#    timeout = ''
-#    ## default is 10s
-#    interval = '10s'
-#    [[httpstat.action]]
+#[[inputs.httpstat]]
+#  ##if empty, use "httpstat"
+#  metricName = ''
+#  ## default is 10s
+#  interval = '10s'
+#  [[inputs.httpstat.action]]
 #    url = ""
 #    method = ""
 #    playload = ""
@@ -23,11 +22,11 @@ const (
 )
 
 type Httpstat struct {
-	MetricName string    `toml:"metricName"`
-	Timeout    string    `toml:"timeout"`
-	Interval   string    `toml:"interval"`
-	Actions    []*Action `toml:"action"`
-	httpPing   []*httpPing
+	MetricName       string    `toml:"metricName"`
+	Interval         string    `toml:"interval"`
+	Actions          []*Action `toml:"action"`
+	httpPing         []*httpPing
+	IntervalDuration time.Duration `json:"-" toml:"-"`
 }
 
 type Action struct {
