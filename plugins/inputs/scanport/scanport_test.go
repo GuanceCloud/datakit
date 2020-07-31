3d772699b089271a0c0806c6d87257f942c612e2
liushaobo <liushaobo@jiagouyun.com>
2020-07-31 10:22:36 +0800

    提交
     [formerly 920aa582816d724f1d7bcefa23230acd766b8ee2] [formerly a6cb83a5e97fd4e0b1986068adfaa88063a98449 [formerly fd2175142b14105fbdfb104f3afb6e4ec62e90ed] [formerly a6cb83a5e97fd4e0b1986068adfaa88063a98449 [formerly fd2175142b14105fbdfb104f3afb6e4ec62e90ed] [formerly a6cb83a5e97fd4e0b1986068adfaa88063a98449 [formerly fd2175142b14105fbdfb104f3afb6e4ec62e90ed] [formerly 2210698ed6fb8245f8484d16374b73ff27fed78f [formerly 17c6e7757cbe2da9e551ec9c2dd578793a8ce217]]]]] [formerly bb5812b49a4215e3f38a83bf9fe323944ab38ce1 [formerly c84bc0e967eb6dcff3eb43591a17b5ba5b2dca9a] [formerly bb5812b49a4215e3f38a83bf9fe323944ab38ce1 [formerly c84bc0e967eb6dcff3eb43591a17b5ba5b2dca9a] [formerly bb5812b49a4215e3f38a83bf9fe323944ab38ce1 [formerly c84bc0e967eb6dcff3eb43591a17b5ba5b2dca9a] [formerly 7c07486113cfcab3c204467e34b1ea5d1be099c6 [formerly 81aa30c04b6cc51ff78a6205f96b4941147ba33e]]]] [formerly aeeeb61d75fd3f471d27016babc129f8964d07c3] [formerly a6cb83a5e97fd4e0b1986068adfaa88063a98449 [formerly fd2175142b14105fbdfb104f3afb6e4ec62e90ed] [formerly a6cb83a5e97fd4e0b1986068adfaa88063a98449 [formerly fd2175142b14105fbdfb104f3afb6e4ec62e90ed] [formerly a6cb83a5e97fd4e0b1986068adfaa88063a98449 [formerly fd2175142b14105fbdfb104f3afb6e4ec62e90ed] [formerly 2210698ed6fb8245f8484d16374b73ff27fed78f [formerly 17c6e7757cbe2da9e551ec9c2dd578793a8ce217]]]] [formerly 1d31fe56a257057a7ea2ad17982414f4f72111e0 [formerly bd8a332b7fbad77cd4273fc4736216a682c0c6dd] [formerly 1d31fe56a257057a7ea2ad17982414f4f72111e0 [formerly bd8a332b7fbad77cd4273fc4736216a682c0c6dd] [formerly 1d31fe56a257057a7ea2ad17982414f4f72111e0 [formerly bd8a332b7fbad77cd4273fc4736216a682c0c6dd] [formerly 0936bf2d0100e8dc1768535c3e3b25d971951635 [formerly d502135cb62004d3d9a09a46b62b95a272d6ec7b]]]]]]]
     [formerly aeeeb61d75fd3f471d27016babc129f8964d07c3]
    
    
    
    
    Former-commit-id: bb5e1d88e76bc89931f2567ced59701210b7ad6b [formerly 2de21f5eb80b059a853a5d3d9f40bb5bd1228236]
    Former-commit-id: b8a18e465f65f263ef7d4098c2541fa0dc53f601 [formerly 117aa970cd38d7a2a060fa902bbf6e8c9fa455ec] [formerly 87f8cf56278f0cb03f4b36aaff17da050a1c4b53 [formerly 58a5ccbb50af7e2c67afc2eac45d548eefdb0238]]
    Former-commit-id: 53a0635fc81128132c916d6b90734b2d537834de [formerly dc5628d496d676f3fc066e528c29d6ce52f81d9c]
    Former-commit-id: daa54b771d9a7a25d2b90421f0ba0ef7bf1dd53b

--- plugins/inputs/scanport/scanport_test.go
+++ plugins/inputs/scanport/scanport_test.go
@@ -1,16 +1,24 @@
 package scanport
 
 import (
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+
 	"testing"
 )
 
 func TestHandle(t *testing.T) {
+	logger.SetGlobalRootLogger("",
+		"debug",
+		logger.OPT_ENC_CONSOLE|logger.OPT_SHORT_CALLER)
+	l = logger.SLogger("scanport")
+
 	t.Run("case-tracerouter", func(t *testing.T) {
 		scan := Scanport{}
-		scan.Ips = []string{"127.0.0.1"}
-		scan.PortStart = 3000
-		scan.PortEnd = 60000
-		scan.Protocol = []string{"tcp"}
+		scan.Targets = []string{"127.0.0.1"}
+		scan.Port = "88-5000"
+		scan.Process = 100
 		scan.handle()
+
+		t.Log("ok")
 	})
 }
