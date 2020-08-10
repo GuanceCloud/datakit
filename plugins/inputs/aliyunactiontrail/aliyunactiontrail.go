426b7f0a7f28fea365c5d325e457ff47bcc93d27
tanbiao <tanbiao@jiagouyun.com>
2020-08-10 03:05:40 +0000

    update: 	[-] try moving internal module
    Former-commit-id: 1bac075e34885a5910b9473a47cdbf18f2adf891 [formerly a15c0f0fe2ebb21649dfe6a159c46a115e140d59]
    Former-commit-id: 9bcc731243c3bc4c573720c7ccb226cb6f78f97b

--- plugins/inputs/aliyunactiontrail/aliyunactiontrail.go
+++ plugins/inputs/aliyunactiontrail/aliyunactiontrail.go
@@ -12,7 +12,6 @@ import (
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
@@ -161,7 +160,7 @@ func (r *AliyunActiontrail) run() error {
 
 		r.handleResponse(response)
 
-		internal.SleepContext(r.ctx, r.Interval.Duration)
+		datakit.SleepContext(r.ctx, r.Interval.Duration)
 		startTm = startTm.Add(r.Interval.Duration)
 	}
 }
