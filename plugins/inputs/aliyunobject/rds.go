46ee429a89fc2539abe879438bc8172966ac1c88
tanbiao <tanbiao@jiagouyun.com>
2020-08-10 03:05:40 +0000

    update: 	[-] try moving internal module
    Former-commit-id: 58a553ea2af3e6fad024d39e2e775511e1545a0d [formerly d7c5d821f223f5b8182bccfafaaf8209168e754e] [formerly 1046544078dd34c5e64d69c21b47d5c7d2260c8e [formerly c436e7f43e7d9287d587e5aa88bf7d3f4ee9d610]] [formerly ae61124d326736766715bddbb9aeee43321d3669 [formerly 1c589b42e0fc35e297d0c58d3f0b4122e4c574d6] [formerly 4f10825279b21de6a64ebf0d294d367fce7599aa [formerly 944f675e54e2184aa237953124e10fa3f6e1c0d8]]]
    Former-commit-id: a77603efe6e50fa39bc1604cc1a99d61326a23db
    Former-commit-id: 582090092131a45d195d51386fb99c0a7653413f [formerly 4ab8701823b96101ce28897aa6f282c5015dd4d6]
    Former-commit-id: 19f00792b77ed2001338e1cfa43e1e9909a7a120

--- plugins/inputs/aliyunobject/rds.go
+++ plugins/inputs/aliyunobject/rds.go
@@ -7,7 +7,8 @@ import (
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/rds"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
@@ -50,7 +51,7 @@ func (r *Rds) run(ag *objectAgent) {
 			break
 		}
 		moduleLogger.Errorf("%s", err)
-		internal.SleepContext(ag.ctx, time.Second*3)
+		datakit.SleepContext(ag.ctx, time.Second*3)
 	}
 
 	for {
@@ -107,7 +108,7 @@ func (r *Rds) run(ag *objectAgent) {
 			}
 		}
 
-		internal.SleepContext(ag.ctx, ag.Interval.Duration)
+		datakit.SleepContext(ag.ctx, ag.Interval.Duration)
 	}
 }
 
