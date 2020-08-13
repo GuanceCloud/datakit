2ba030f3269ba7ff85060576dd62bb2052ad9407
liguozhuang <liguozhuang@jiagouyun.com>
2020-08-13 18:34:26 +0800

    add cloudflare
     [formerly a77b55f6115acb501a1601e6805aadbbe3f9a41e] [formerly 771efad8ac289b1fe9f4868b08fc3a70d2b80d06 [formerly b4d663ed13dc6ceb81e304caea7a3c91fb07c21f] [formerly 771efad8ac289b1fe9f4868b08fc3a70d2b80d06 [formerly b4d663ed13dc6ceb81e304caea7a3c91fb07c21f] [formerly 771efad8ac289b1fe9f4868b08fc3a70d2b80d06 [formerly b4d663ed13dc6ceb81e304caea7a3c91fb07c21f] [formerly 666a8be170b4643324fce61282d88e6d11b4f3fe [formerly 61512a6e78c6e78e7f12455b2b0a3e67643b30d0]]]]] [formerly d9514996bd3df88499dda86be7a5d0d5e9e407ac [formerly 62361c4f37c4a2425e84a96e847d5d876fa664a7] [formerly d9514996bd3df88499dda86be7a5d0d5e9e407ac [formerly 62361c4f37c4a2425e84a96e847d5d876fa664a7] [formerly d9514996bd3df88499dda86be7a5d0d5e9e407ac [formerly 62361c4f37c4a2425e84a96e847d5d876fa664a7] [formerly 3431494a3258c99ce23ccd6c7088bbd755512a7e [formerly 701ff744a514f8ed1da17689ed9569737f3ebb31]]]] [formerly f2609e6bdb5a0b477c5c1594302885b5f371deb8] [formerly 771efad8ac289b1fe9f4868b08fc3a70d2b80d06 [formerly b4d663ed13dc6ceb81e304caea7a3c91fb07c21f] [formerly 771efad8ac289b1fe9f4868b08fc3a70d2b80d06 [formerly b4d663ed13dc6ceb81e304caea7a3c91fb07c21f] [formerly 771efad8ac289b1fe9f4868b08fc3a70d2b80d06 [formerly b4d663ed13dc6ceb81e304caea7a3c91fb07c21f] [formerly 666a8be170b4643324fce61282d88e6d11b4f3fe [formerly 61512a6e78c6e78e7f12455b2b0a3e67643b30d0]]]] [formerly d049003afe2bf42beb57c10fa85d398d8a062fb0 [formerly 9e5db3ea088bef053f22679937f647113cfe1661] [formerly d049003afe2bf42beb57c10fa85d398d8a062fb0 [formerly 9e5db3ea088bef053f22679937f647113cfe1661] [formerly d049003afe2bf42beb57c10fa85d398d8a062fb0 [formerly 9e5db3ea088bef053f22679937f647113cfe1661] [formerly 8c7071d0c4f00a26eb75a9d49ac85922dfda577e [formerly 514991b03bb82f58550bdf83e704406bf5350516]]]]]]]
     [formerly f2609e6bdb5a0b477c5c1594302885b5f371deb8]
     [formerly d9514996bd3df88499dda86be7a5d0d5e9e407ac [formerly 62361c4f37c4a2425e84a96e847d5d876fa664a7] [formerly d9514996bd3df88499dda86be7a5d0d5e9e407ac [formerly 62361c4f37c4a2425e84a96e847d5d876fa664a7] [formerly d9514996bd3df88499dda86be7a5d0d5e9e407ac [formerly 62361c4f37c4a2425e84a96e847d5d876fa664a7] [formerly 3431494a3258c99ce23ccd6c7088bbd755512a7e [formerly 701ff744a514f8ed1da17689ed9569737f3ebb31]]]]]
    
    
    
    Former-commit-id: 04a3b0689e00601f26f345bf9b9b15f1c0bcbb6c [formerly 82e2dc5ac25e6555c9c502955b6a039cdc1f8f21]
    Former-commit-id: a5aaaa86b55ed7cb000fdd3dcb6509bd4bad76f1 [formerly 44db44377d096e4bc314c2a99f5ce7b0a415fe52] [formerly 0a5124149cd6ab9aef9d44a555edc6c944d351d1 [formerly a2224ff63e53e51931cc3ae0474cd75739573f4c]]
    Former-commit-id: e91e7eb76f63224914d510560b961df30325b8bc [formerly 02fb3e8e94389913f7f8e0b179c31649ae12e384]
    Former-commit-id: b9da76817aff91d2dfd6aed0a832d5f68497e2ec

---
+++ plugins/inputs/cloudflare/cloudflare_test.go
@@ -0,0 +1,38 @@
+package cloudflare
+
+import (
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"testing"
+)
+
+const (
+	email  = "a_ha2008@qq.com"
+	zoneID = "0355d2a9ca65054d3d7f6013d8b237f4"
+	apiKey = "d44e173bf1a3cd849c868a9662ae56d32470a"
+)
+
+func __init() {
+	logger.SetGlobalRootLogger("", logger.DEBUG, logger.OPT_DEFAULT)
+	l = logger.SLogger(inputName)
+}
+
+func TestMain(t *testing.T) {
+	__init()
+
+	var c = Cloudflare{
+		Email:  email,
+		ZoneID: zoneID,
+		APIKey: apiKey,
+	}
+
+	c.laodCfg()
+
+	data, err := c.getMetrics()
+	if err != nil {
+		panic(err)
+	}
+
+	t.Logf("len: %d\n", len(data))
+	t.Logf("%s\n", data)
+
+}
