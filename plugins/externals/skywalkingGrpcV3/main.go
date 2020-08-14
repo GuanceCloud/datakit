da20e869a24a4c9cb8fd357272fe0942799a0c77
huangbo <huangbo@jiagouyun.com>
2020-08-14 16:42:42 +0800

    skywalking V3独立成单独应用
     [formerly edd6b9389eb2471f9752f733b9e24ee50d8d2f78] [formerly 81bb86e9bdf7d96c9d04acc951e3a548d29b27ef [formerly 56f4c5175bd5794c2eed2f8e1f112e17bcbafa79] [formerly 81bb86e9bdf7d96c9d04acc951e3a548d29b27ef [formerly 56f4c5175bd5794c2eed2f8e1f112e17bcbafa79] [formerly 81bb86e9bdf7d96c9d04acc951e3a548d29b27ef [formerly 56f4c5175bd5794c2eed2f8e1f112e17bcbafa79] [formerly c8fbe5e704f4e6245a1714a9b448229aa29dbffb [formerly 2b8051f365018c934d3b12f8b1b7625925e6def4]]]]] [formerly 25c8beb6e6d265b541f350f6e875da8b433b6933 [formerly e82bee06f8c8401fe612445d1a95ef7c6134cc25] [formerly 25c8beb6e6d265b541f350f6e875da8b433b6933 [formerly e82bee06f8c8401fe612445d1a95ef7c6134cc25] [formerly 25c8beb6e6d265b541f350f6e875da8b433b6933 [formerly e82bee06f8c8401fe612445d1a95ef7c6134cc25] [formerly 1d24062487c8b232f36aacfe33d0e24f4231eecb [formerly 2d4afb5d4b6080bd11603af3f6b640d62bdd0300]]]] [formerly 303708fd5f19993dc3247bcadebcb3dc6d520c5e] [formerly 81bb86e9bdf7d96c9d04acc951e3a548d29b27ef [formerly 56f4c5175bd5794c2eed2f8e1f112e17bcbafa79] [formerly 81bb86e9bdf7d96c9d04acc951e3a548d29b27ef [formerly 56f4c5175bd5794c2eed2f8e1f112e17bcbafa79] [formerly 81bb86e9bdf7d96c9d04acc951e3a548d29b27ef [formerly 56f4c5175bd5794c2eed2f8e1f112e17bcbafa79] [formerly c8fbe5e704f4e6245a1714a9b448229aa29dbffb [formerly 2b8051f365018c934d3b12f8b1b7625925e6def4]]]] [formerly 85cd385fe61ec777d084b4dbf96a674758ca4f72 [formerly 4616f92467ec2c9b848ad35efac55c7a450fdab4] [formerly 85cd385fe61ec777d084b4dbf96a674758ca4f72 [formerly 4616f92467ec2c9b848ad35efac55c7a450fdab4] [formerly a9dc6df10b56d3793cad3716bbccb1d744282e5e [formerly 32eb2376a789ee2f91921bdc407d6cab290cbe7d]]] [formerly 85cd385fe61ec777d084b4dbf96a674758ca4f72 [formerly 4616f92467ec2c9b848ad35efac55c7a450fdab4] [formerly a9dc6df10b56d3793cad3716bbccb1d744282e5e [formerly 32eb2376a789ee2f91921bdc407d6cab290cbe7d]] [formerly a9dc6df10b56d3793cad3716bbccb1d744282e5e [formerly 32eb2376a789ee2f91921bdc407d6cab290cbe7d] [formerly abfb321ac1931562078e31ed04d244c0622d7b04 [formerly b7721eee3b35f6cd5670a0c4efab19fdd5ff3013]]]]]]]
     [formerly 303708fd5f19993dc3247bcadebcb3dc6d520c5e]
    
    
    
    
    Former-commit-id: 124aaa9c7d450f4d193b8b57473d1c067e9a6531 [formerly 0e84f3b768ec7af4a62e946bc130bd4ed70a3065]
    Former-commit-id: c9de679b19084bb1f981c45c57598af4573a6479 [formerly 75bfa79b68c760dc0c83c4c3a3421a49af751a41] [formerly eddf362dad9b4964ee2610b74ca43cf058702bec [formerly 048af554dd6a870f487347a67f4950709d99d815]]
    Former-commit-id: 41a3efc50e421549afa763e6bf7200a01452b085 [formerly d120c9ec6600c5e03740b39524367bd9904f0039]
    Former-commit-id: c614405047c159a09039ca346cbeaed5174ad2d4

---
+++ plugins/externals/skywalkingGrpcV3/main.go
@@ -0,0 +1,256 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net"
+	"flag"
+	"encoding/json"
+	"path/filepath"
+	"time"
+	"context"
+
+	"google.golang.org/grpc"
+	"github.com/influxdata/toml"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	dkio "gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+    swV3 "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/externals/skywalkingGrpcV3/v3"
+)
+
+type SkywalkingServerV3 struct {}
+
+type TraceAdapter struct {
+	Source        string
+
+	Duration      int64
+	TimestampUs   int64
+	Content       string
+
+	Class         string
+	ServiceName   string
+	OperationName string
+	ParentID      string
+	TraceID       string
+	SpanID        string
+	IsError       string
+	SpanType      string
+	EndPoint      string
+
+	Tags          map[string]string
+}
+
+type Skywalking struct {
+	GrpcPort int32
+	Tags     map[string]string
+}
+
+const (
+	US_PER_SECOND   int64 = 1000000
+	SPAN_TYPE_ENTRY  = "entry"
+	SPAN_TYPE_LOCAL  = "local"
+	SPAN_TYPE_EXIT   = "exit"
+
+)
+
+var (
+	flagCfgStr    = flag.String("cfg", "", "toml config string")
+	flagRPCServer = flag.String("rpc-server", "unix://"+datakit.GRPCDomainSock, "gRPC server")
+	flagLog       = flag.String("log", filepath.Join(datakit.InstallDir, "externals", "skywalkingGrpcV3.log"), "log file")
+	flagLogLevel  = flag.String("log-level", "info", "log file")
+
+	l      *logger.Logger
+	rpcCli dkio.DataKitClient
+	skywalkingV3 Skywalking
+)
+
+
+func main() {
+	doInit()
+	skywalkingGrpcServRun(fmt.Sprintf(":%d", skywalkingV3.GrpcPort))
+}
+
+func doInit() {
+	flag.Parse()
+
+	cfgdata, err := base64.StdEncoding.DecodeString(*flagCfgStr)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.SetGlobalRootLogger(*flagLog, *flagLogLevel, logger.OPT_ENC_CONSOLE|logger.OPT_SHORT_CALLER)
+	l = logger.SLogger("skywalkingGrpcV3")
+	l.Infof("log level: %s", *flagLogLevel)
+
+	if err := toml.Unmarshal(cfgdata, &skywalkingV3); err != nil {
+		l.Errorf("failed to parse toml `%s': %s", *flagCfgStr, err)
+		return
+	}
+
+	l.Infof("gRPC dial %s...", *flagRPCServer)
+	conn, err := grpc.Dial(*flagRPCServer, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Second*5))
+	if err != nil {
+		l.Fatalf("connect RCP failed: %s", err)
+	}
+
+	l.Infof("gRPC connect %s ok", *flagRPCServer)
+	defer conn.Close()
+
+	rpcCli = dkio.NewDataKitClient(conn)
+}
+
+func skywalkingGrpcServRun(addr string) {
+	l.Infof("skywalking V3 gRPC starting...")
+
+	rpcListener, err := net.Listen("tcp", addr)
+	if err != nil {
+		l.Errorf("start skywalking V3 gRPC server %s failed: %v", addr, err)
+		return
+	}
+
+	l.Infof("start skywalking V3 gRPC server on %s ok", addr)
+
+	rpcServer := grpc.NewServer()
+	swV3.RegisterTraceSegmentReportServiceServer(rpcServer, &SkywalkingServerV3{})
+	if err := rpcServer.Serve(rpcListener); err != nil {
+		l.Error(err)
+	}
+}
+
+func (s *SkywalkingServerV3) Collect(tsc swV3.TraceSegmentReportService_CollectServer) error {
+	for {
+		sgo, err := tsc.Recv()
+		if err == io.EOF {
+			return tsc.SendAndClose(&swV3.Commands{})
+		}
+		if err != nil {
+			return err
+		}
+		err = skywalkGrpcToLineProto(sgo)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func skywalkGrpcToLineProto(sg *swV3.SegmentObject) error {
+	var lines [][]byte
+	for _, span := range sg.Spans {
+		t := TraceAdapter{}
+
+		t.Source = "skywalking"
+
+		t.Duration = (span.EndTime -span.StartTime)*1000
+		t.TimestampUs = span.StartTime * 1000
+		js ,err := json.Marshal(span)
+		if err != nil {
+			return err
+		}
+		t.Content = string(js)
+		t.Class         = "tracing"
+		t.ServiceName   = sg.Service
+		t.OperationName = span.OperationName
+		if span.SpanType == swV3.SpanType_Entry {
+			if len(span.Refs) > 0 {
+				t.ParentID      = fmt.Sprintf("%s%d", span.Refs[0].ParentTraceSegmentId,
+					span.Refs[0].ParentSpanId)
+			}
+		} else {
+			t.ParentID      = fmt.Sprintf("%s%d", sg.TraceSegmentId, span.ParentSpanId)
+		}
+
+		t.TraceID       = sg.TraceId
+		t.SpanID        = fmt.Sprintf("%s%d", sg.TraceSegmentId, span.SpanId)
+		if span.IsError {
+			t.IsError   = "true"
+		}
+		if span.SpanType == swV3.SpanType_Entry {
+			t.SpanType  = SPAN_TYPE_ENTRY
+		} else if span.SpanType == swV3.SpanType_Local {
+			t.SpanType  = SPAN_TYPE_LOCAL
+		} else {
+			t.SpanType  = SPAN_TYPE_EXIT
+		}
+		t.EndPoint      = span.Peer
+
+		t.Tags = skywalkingV3.Tags
+		pt, err := t.MkLineProto()
+		if err != nil {
+			l.Error(err)
+			continue
+		}
+		lines = append(lines, pt)
+		l.Debug(string(pt))
+	}
+
+	if len(lines) == 0 {
+		return nil
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := rpcCli.Send(ctx, &dkio.Request{
+		Lines:     bytes.Join(lines, []byte("\n")),
+		Precision: "ns",
+		Name:      "skywalkingGrpcV3",
+	})
+	if err != nil {
+		l.Errorf("feed error: %s", err.Error())
+		return err
+	}
+	l.Debugf("feed %d points, error: `%s'", r.GetPoints(), r.GetErr())
+	return nil
+}
+
+
+func (tAdpt *TraceAdapter) MkLineProto() ([]byte, error) {
+	tags := make(map[string]string)
+	fields := make(map[string]interface{})
+
+	tags["__class"]         = tAdpt.Class
+	tags["__operationName"] = tAdpt.OperationName
+	tags["__serviceName"]   = tAdpt.ServiceName
+	tags["__parentID"]      = tAdpt.ParentID
+	tags["__traceID"]       = tAdpt.TraceID
+	tags["__spanID"]        = tAdpt.SpanID
+
+	for tag, tagV := range tAdpt.Tags {
+		tags[tag] = tagV
+	}
+	if tAdpt.IsError == "true" {
+		tags["__isError"] = "true"
+	} else {
+		tags["__isError"] = "false"
+	}
+
+	if tAdpt.EndPoint != "" {
+		tags["__endpoint"] = tAdpt.EndPoint
+	} else {
+		tags["__endpoint"] = "null"
+	}
+
+	if tAdpt.SpanType != "" {
+		tags["__spanType"] = tAdpt.SpanType
+	} else {
+		tags["__spanType"] = SPAN_TYPE_ENTRY
+	}
+
+	fields["__duration"] = tAdpt.Duration
+	fields["__content"]  = tAdpt.Content
+
+	ts := time.Unix(tAdpt.TimestampUs/US_PER_SECOND, (tAdpt.TimestampUs%US_PER_SECOND)*1000)
+
+	pt, err := dkio.MakeMetric(tAdpt.Source, tags, fields, ts)
+	if err != nil {
+		return nil, fmt.Errorf("build metric err: %s", err)
+	}
+
+	lineProtoStr := string(pt)
+	l.Debugf(lineProtoStr)
+
+	return pt, nil
+}
