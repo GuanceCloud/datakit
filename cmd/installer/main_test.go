101277eff1c235aca2f2f07becf2d590322e0a4f
tanbiao <tanbiao@jiagouyun.com>
2020-08-24 10:31:44 +0000

    update: 	[*] add lint checking, on the march

--- cmd/installer/main_test.go
+++ cmd/installer/main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestUpdateLagacyConfig(t *testing.T) {
+func TestUpdateLagacyConfig(_ *testing.T) {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	updateLagacyConfig("/usr/local/cloudcare/forethought/datakit")
 }
