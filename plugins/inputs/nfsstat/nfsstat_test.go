42f6cdb1ee0c4e54b11da15853be643362af0fce
liguozhuang <liguozhuang@jiagouyun.com>
2020-08-26 13:44:17 +0800

    fix golint
     [formerly b69c02e88e968999c55c715daa781d41c5e7ae8e [formerly f918031c9e050628d1c1bf3d1a27b06b6a03ef67] [formerly e3e16c17949c208216b74ee70728a3dda4b0a9d3 [formerly 46760ff9c0be95beb4410314dfe627f88a269132]] [formerly e3e16c17949c208216b74ee70728a3dda4b0a9d3 [formerly 46760ff9c0be95beb4410314dfe627f88a269132] [formerly 8687914f9e5fe961d5c2e88cbfc24f727bee277f [formerly 3acf5950b656df072566bcaf8f96ed89a65a2fc8]]]]
    
    
    
    Former-commit-id: d6aa56c957215ddeb0b2a6b6ba7d0fdfa2512dc9
    Former-commit-id: a171cdf43883d2fa8319a93effa2dc9e794e8fb1 [formerly 93e13d7da44d842d6916977341aa7363eca2178e]
    Former-commit-id: b6342e5a9323ee6cbc9c4982adf2c5539cbbd200

--- plugins/inputs/nfsstat/nfsstat_test.go
+++ plugins/inputs/nfsstat/nfsstat_test.go
@@ -4,17 +4,9 @@ package nfsstat
 
 import (
 	"testing"
-
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 )
 
-func __init() {
-	logger.SetGlobalRootLogger("", logger.DEBUG, logger.OPT_DEFAULT)
-	l = logger.SLogger(inputName)
-}
-
 func TestMain(t *testing.T) {
-	__init()
 	testAssert = true
 
 	var nfs = NFSstat{
