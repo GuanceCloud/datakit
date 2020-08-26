24da7795ac6b0efd2ebe8df52a9609881ee3ab3f
liushaobo <liushaobo@jiagouyun.com>
2020-08-26 14:32:38 +0800

    提交代码
    Former-commit-id: 85adc5b286584c9f31d0db95640e998511963be2 [formerly db328de30a8b351bf347cacf9d356f23103c5a42] [formerly 5fe2178103c7806be1812b52df9b8d66be971ad1 [formerly bf7649a68c017e614a8fc3d8464a6179490e8cd0]]
    Former-commit-id: 0118d9070cae4a57a9bae8e846423f135cad553e [formerly 69016b08a4debdeb285961ae362ed91384a88b05]
    Former-commit-id: 9ce5d42959431be9bab6ab11efe464e68c001e2e

--- plugins/inputs/mysqlmonitor/mysql.go
+++ plugins/inputs/mysqlmonitor/mysql.go
@@ -1,7 +1,7 @@
 package mysqlmonitor
 
 import (
-	"fmt"
+	"sync"
 	"time"
 
 	"database/sql"
@@ -26,30 +26,24 @@ var (
 	name = "mysqlMonitor"
 )
 
-func (_ *Mysql) Catalog() string {
+func (_ *MysqlMonitor) Catalog() string {
 	return "db"
 }
 
-func (_ *Mysql) SampleConfig() string {
+func (_ *MysqlMonitor) SampleConfig() string {
 	return configSample
 }
 
-func (mysql *Mysql) Run() {
+func (m *MysqlMonitor) Run() {
 	l = logger.SLogger("mysqlMonitor")
 	l.Info("mysqlMonitor input started...")
 
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mysql.Username, mysql.Password, mysql.Host, mysql.Port, mysql.Database)
-	db, err := sql.Open("mysql", connStr)
-	if err != nil {
-		l.Errorf("mysql connect faild %v", err)
-	}
-
-	mysql.db = db
-
-	interval, err := time.ParseDuration(mysql.Interval)
+	interval, err := time.ParseDuration(m.Interval)
 	if err != nil {
 		l.Error(err)
 	}
+
+	m.MetricName = name
 
 	tick := time.NewTicker(interval)
 	defer tick.Stop()
@@ -58,7 +52,7 @@ func (mysql *Mysql) Run() {
 		select {
 		case <-tick.C:
 			// handle
-			mysql.handle()
+			m.handle()
 		case <-datakit.Exit.Wait():
 			l.Info("exit")
 			return
@@ -66,8 +60,142 @@ func (mysql *Mysql) Run() {
 	}
 }
 
+func (m *MysqlMonitor) handle() {
+	var wg sync.WaitGroup
+
+	// Loop through each server and collect metrics
+	for _, server := range m.Servers {
+		wg.Add(1)
+		go func(s string) {
+			defer wg.Done()
+			m.gatherServer(s)
+		}(server)
+	}
+
+	wg.Wait()
+}
+
+func (m *MysqlMonitor) gatherServer(serv string) error {
+	serv, err := dsnAddTimeout(serv)
+	if err != nil {
+		return err
+	}
+
+	db, err := sql.Open("mysql", serv)
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	err = m.gatherGlobalStatuses(db, serv)
+	if err != nil {
+		return err
+	}
+
+	if m.GatherGlobalVars {
+		// Global Variables may be gathered less often
+		err = m.gatherGlobalVariables(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherBinaryLogs {
+		err = m.gatherBinaryLogs(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherProcessList {
+		err = m.GatherProcessListStatuses(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherUserStatistics {
+		err = m.GatherUserStatisticsStatuses(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherSlaveStatus {
+		err = m.gatherSlaveStatuses(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherInfoSchemaAutoInc {
+		err = m.gatherInfoSchemaAutoIncStatuses(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherInnoDBMetrics {
+		err = m.gatherInnoDBMetrics(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherTableIOWaits {
+		err = m.gatherPerfTableIOWaits(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherIndexIOWaits {
+		err = m.gatherPerfIndexIOWaits(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherTableLockWaits {
+		err = m.gatherPerfTableLockWaits(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherEventWaits {
+		err = m.gatherPerfEventWaits(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherFileEventsStats {
+		err = m.gatherPerfFileEventsStatuses(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherPerfEventsStatements {
+		err = m.gatherPerfEventsStatements(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+
+	if m.GatherTableSchema {
+		err = m.gatherTableSchema(db, serv)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func init() {
 	inputs.Add(name, func() inputs.Input {
-		return &Mysql{}
+		return &MysqlMonitor{}
 	})
 }
