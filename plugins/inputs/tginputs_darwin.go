d28a449e51e57ba4d879aac9362d948f1faf1aaa
tanbiao <tanbiao@jiagouyun.com>
2020-08-31 10:30:33 +0000

    update: 	[*] try import telegraf source code directly
     [formerly fd67b3396943b82988e24839064a44971996f147] [formerly f2e6dcd12e4d332301bc12b6c96b22d574917bc3 [formerly 13e83a915e54b0c87e7951952c4914efe55304ea] [formerly f2e6dcd12e4d332301bc12b6c96b22d574917bc3 [formerly 13e83a915e54b0c87e7951952c4914efe55304ea] [formerly f2e6dcd12e4d332301bc12b6c96b22d574917bc3 [formerly 13e83a915e54b0c87e7951952c4914efe55304ea] [formerly c83187e5b94526c16b8e647742ff530df97ceb02 [formerly a8561e5807a299c7c07fc205e1b0e28a8ec318f2]]]] [formerly 7292998c61d0c5486b9d2a11e840ae3fb927761c [formerly d3d0f59ed8350f6609a73186e03891bb70400139] [formerly 7292998c61d0c5486b9d2a11e840ae3fb927761c [formerly d3d0f59ed8350f6609a73186e03891bb70400139] [formerly 7292998c61d0c5486b9d2a11e840ae3fb927761c [formerly d3d0f59ed8350f6609a73186e03891bb70400139] [formerly 204d7541b9e5e3949b0deb3d9b50e0ba5237c896 [formerly fdbb9835ec9ed457e7eba38484411c12bccff8d9]]]]]]
    
    
    
    
    Former-commit-id: 102f42949835f3a6b257872b30297de5e6055849 [formerly 447e73fbe363d5cf01db6671a55738e770182169]
    Former-commit-id: 57167239e4faff963d53d03bc7e9e97871bde834 [formerly 2e4a6ba875f231192c4b0642d84e618eb13db198] [formerly ad67895cb3907962c097cd7e5e2ee7733a22b98d [formerly 918ad6ee3721c43bb602e034dc62c498fc3547fd]]
    Former-commit-id: f559672c85501a620820eb681b60c4fc57fdc831 [formerly f4fa1c887d70fd963c53ad5728acf7147ac6cff3]
    Former-commit-id: 0de70cde3e1926b0a98314e92bb6d423300ee557

---
+++ plugins/inputs/tginputs_darwin.go
@@ -0,0 +1,24 @@
+// +build darwin
+
+package inputs
+
+import (
+	"github.com/influxdata/telegraf/plugins/inputs/processes"
+	"github.com/influxdata/telegraf/plugins/inputs/procstat"
+	"github.com/influxdata/telegraf/plugins/inputs/varnish"
+)
+
+var (
+	telegrafInputsDarwin = map[string]*TelegrafInput{ // Name: Catalog
+		"varnish":   {name: "varnish", Catalog: "varnish", input: &varnish.Varnish{}},
+		"procstat":  {name: "procstat", Catalog: "processes", input: &procstat.Procstat{}},
+		"processes": {name: "processes", Catalog: "processes", input: &processes.Processes{}},
+	}
+)
+
+func init() {
+	for k, v := range telegrafInputsDarwin {
+		l.Debug("add telegraf plugin %s", k)
+		TelegrafInputs[k] = v
+	}
+}
