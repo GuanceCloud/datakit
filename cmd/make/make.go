0fc763e75c6969a73d2bf49545a1285b2b2aaa0c
liushaobo <liushaobo@jiagouyun.com>
2020-08-31 15:00:47 +0800

    提交
    Former-commit-id: 1143c3d973056ccc365e7b58233f55f1a89f3738 [formerly 77530edd77dd4d5b34f7c166f7367450f9d4f001]
    Former-commit-id: 6635ed6eb80fed4792927b47c670771f3a637d26

--- cmd/make/make.go
+++ cmd/make/make.go
@@ -427,8 +427,8 @@ var (
 
 			entry: "main.go",
 			osarchs: map[string]bool{
-				"linux/amd64": true,
-				"linux/386":   true,
+				"linux/amd64":  true,
+				`darwin/amd64`: true,
 			},
 
 			buildArgs: nil,
@@ -437,22 +437,21 @@ var (
 			},
 		},
 
-		// &dkexternal{
-		// 	name: "tcpdump",
-		// 	lang: "go",
+		&dkexternal{
+			name: "tcpdump",
+			lang: "go",
 
-		// 	entry: "main.go",
-		// 	osarchs: map[string]bool{
-		// 		"linux/amd64":  true,
-		// 		"linux/386":    true,
-		// 		`darwin/amd64`: true,
-		// 	},
+			entry: "main.go",
+			osarchs: map[string]bool{
+				"linux/amd64":  true,
+				`darwin/amd64`: true,
+			},
 
-		// 	buildArgs: nil,
-		// 	envs: []string{
-		// 		"CGO_ENABLED=1",
-		// 	},
-		// },
+			buildArgs: nil,
+			envs: []string{
+				"CGO_ENABLED=1",
+			},
+		},
 
 		&dkexternal{
 			name: "csv",
@@ -483,27 +482,27 @@ var (
 			buildCmd:  "bash",
 		},
 
-		&dkexternal{
-			// requirement: apt-get install gcc-multilib
-			name: "skywalkingGrpcV3",
-			lang: "go",
+		// &dkexternal{
+		// 	// requirement: apt-get install gcc-multilib
+		// 	name: "skywalkingGrpcV3",
+		// 	lang: "go",
 
-			entry: "main.go",
-			osarchs: map[string]bool{
-				`linux/386`:   true,
-				`linux/amd64`: true,
-				//`linux/arm`:     true,
-				//`linux/arm64`:   true,
-				//`darwin/amd64`:  true,
-				`windows/amd64`: true,
-				`windows/386`:   true,
-			},
+		// 	entry: "main.go",
+		// 	osarchs: map[string]bool{
+		// 		// `linux/386`:   true,
+		// 		`linux/amd64`: true,
+		// 		//`linux/arm`:     true,
+		// 		//`linux/arm64`:   true,
+		// 		//`darwin/amd64`:  true,
+		// 		`windows/amd64`: true,
+		// 		// `windows/386`:   true,
+		// 	},
 
-			buildArgs: nil,
-			envs: []string{
-				"CGO_ENABLED=1",
-			},
-		},
+		// 	buildArgs: nil,
+		// 	envs: []string{
+		// 		"CGO_ENABLED=1",
+		// 	},
+		// },
 
 		// others...
 	}
