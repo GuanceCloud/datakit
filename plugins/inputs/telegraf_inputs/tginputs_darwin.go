f3fae2c9fc0cfd36683950482a20b85c0bc5cd72
tanbiao <tanbiao@jiagouyun.com>
2020-09-01 10:17:56 +0000

    update: 	[*] add toml check on datakit & telegraf input
    Former-commit-id: 399bd06ba766fb301981e12e9f1393faf77672ae [formerly aad1e264906c5642917df66dbd613e6699ae823f]
    Former-commit-id: 6cf2d1d91c3a923a94d3d48f68b27e048a2cf2cb

--- plugins/inputs/telegraf_inputs/tginputs_darwin.go
+++ plugins/inputs/telegraf_inputs/tginputs_darwin.go
@@ -1,24 +1,3 @@
 // +build darwin
 
 package telegraf_inputs
-
-import (
-	"github.com/influxdata/telegraf/plugins/inputs/processes"
-	"github.com/influxdata/telegraf/plugins/inputs/procstat"
-	"github.com/influxdata/telegraf/plugins/inputs/varnish"
-)
-
-var (
-	telegrafInputsDarwin = map[string]*TelegrafInput{ // Name: Catalog
-		"varnish":   {name: "varnish", Catalog: "varnish", input: &varnish.Varnish{}},
-		"procstat":  {name: "procstat", Catalog: "processes", input: &procstat.Procstat{}},
-		"processes": {name: "processes", Catalog: "processes", input: &processes.Processes{}},
-	}
-)
-
-func init() {
-	for k, v := range telegrafInputsDarwin {
-		l.Debug("add telegraf plugin %s", k)
-		TelegrafInputs[k] = v
-	}
-}
