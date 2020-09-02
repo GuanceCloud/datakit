c10a4f22028c7d312b74b3cfd49fc5d6c2061e19
liushaobo <liushaobo@jiagouyun.com>
2020-09-02 10:33:27 +0800

    提交代码
     [formerly 9cd95de8206f671b5762de56fce14680d1a58cb1 [formerly 90db630f2af88e47b146aac63e84dd0245e0a847] [formerly ca8ef1e3dc774f7dbcf43232701dc019bf7337f3 [formerly 562b9c72cfc34aae0af24c4e108fda15c0927a5f]] [formerly 5fcbc0b1c449a6f298f4502ca94d293c231e8468] [formerly d32c64407a88336e4ee9836758fc1945c9a1b025 [formerly 3c47496070bdb593596359e9c1a46d4484c83a6b] [formerly d32c64407a88336e4ee9836758fc1945c9a1b025 [formerly 3c47496070bdb593596359e9c1a46d4484c83a6b] [formerly aca474f9f26c9a8cf7dd7f971e1ad8b29b24ccdb [formerly a581b47ca662d6ef3dcb028b355b5440f1c113f2]]]] [formerly 00863edb154be8208e077261d7298f1033640323] [formerly d916e2512db9ca5254bd4978b7bd935fb50616ce [formerly c9d3a92e2b27b681ab40d6fc12835d9e4d34cdf6] [formerly e2d415bdd14975d066bdbe4b8671172f7c476a74 [formerly 4f293e9285602cc15a8cf338d1fa1289a3fd471a] [formerly e2d415bdd14975d066bdbe4b8671172f7c476a74 [formerly 4f293e9285602cc15a8cf338d1fa1289a3fd471a] [formerly e2d415bdd14975d066bdbe4b8671172f7c476a74 [formerly 4f293e9285602cc15a8cf338d1fa1289a3fd471a] [formerly 2e250cc37515361e938f116eb43b1b79065f98e9 [formerly 51f571769d9cdf031e433cd99359f4d17ba665fb]]]]]]]
     [formerly 00863edb154be8208e077261d7298f1033640323]
    
    
    
    
    Former-commit-id: 6b86245fc465930abb91a0e9473b670b5c161b94 [formerly 0f198bb55a22e19f2f06f58af4ef15bac7f1a3b6]
    Former-commit-id: fcbbd8931de07d546f50ea5cfe1e7e5124962c07 [formerly 21a85ca62fb684fff3bdf95e2a2b1b68a77ada04] [formerly 2c3d684ee68ef9230a708b0981fd2a28c5815093 [formerly f5fde24b5cceef757320b3105d34ddea63bac415]]
    Former-commit-id: 7a7c8a894176cea6c10471240954bf52045b9d2b [formerly 7d0149e99757689fc0dedd021e77f63a38f0d514]
    Former-commit-id: b40f2fb515a5ef941ef1db86f48d56d2f0cb72a0

--- plugins/inputs/mysqlmonitor/conf.go
+++ plugins/inputs/mysqlmonitor/conf.go
@@ -17,6 +17,8 @@ const (
 #
 ## If no servers are specified, then localhost is used as the host.
 servers = ["tcp(127.0.0.1:3306)/"]
+## scan interval
+## interval = "10m"
 ## Selects the metric output format.
 ## if the list is empty, then metrics are gathered from all database tables
 # table_schema_databases = []
@@ -60,34 +62,37 @@ servers = ["tcp(127.0.0.1:3306)/"]
 # tls_key = "/etc/telegraf/key.pem"
 ## Use TLS but skip chain & host verification
 # insecure_skip_verify = false
+# [inputs.mysqlmonitor.tags]
+# tags1 = "value1"
 `
 )
 
 type MysqlMonitor struct {
-	Servers                             []string      `toml:"servers"`
-	MetricName                          string        `toml:"metricName"`
-	Interval                            string        `toml:"interval"`
-	IntervalDuration                    time.Duration `json:"-" toml:"-"`
-	PerfEventsStatementsDigestTextLimit int64         `toml:"perf_events_statements_digest_text_limit"`
-	PerfEventsStatementsLimit           int64         `toml:"perf_events_statements_limit"`
-	PerfEventsStatementsTimeLimit       int64         `toml:"perf_events_statements_time_limit"`
-	TableSchemaDatabases                []string      `toml:"table_schema_databases"`
-	GatherProcessList                   bool          `toml:"gather_process_list"`
-	GatherUserStatistics                bool          `toml:"gather_user_statistics"`
-	GatherInfoSchemaAutoInc             bool          `toml:"gather_info_schema_auto_inc"`
-	GatherInnoDBMetrics                 bool          `toml:"gather_innodb_metrics"`
-	GatherSlaveStatus                   bool          `toml:"gather_slave_status"`
-	GatherBinaryLogs                    bool          `toml:"gather_binary_logs"`
-	GatherTableIOWaits                  bool          `toml:"gather_table_io_waits"`
-	GatherTableLockWaits                bool          `toml:"gather_table_lock_waits"`
-	GatherIndexIOWaits                  bool          `toml:"gather_index_io_waits"`
-	GatherEventWaits                    bool          `toml:"gather_event_waits"`
-	GatherTableSchema                   bool          `toml:"gather_table_schema"`
-	GatherFileEventsStats               bool          `toml:"gather_file_events_stats"`
-	GatherPerfEventsStatements          bool          `toml:"gather_perf_events_statements"`
-	GatherGlobalVars                    bool          `toml:"gather_global_variables"`
-	GatherGlobalStatus                  bool          `toml:"gather_global_status"`
-	IntervalSlow                        string        `toml:"interval_slow"`
+	Servers                             []string          `toml:"servers"`
+	MetricName                          string            `toml:"metricName"`
+	Interval                            string            `toml:"interval"`
+	IntervalDuration                    time.Duration     `json:"-" toml:"-"`
+	PerfEventsStatementsDigestTextLimit int64             `toml:"perf_events_statements_digest_text_limit"`
+	PerfEventsStatementsLimit           int64             `toml:"perf_events_statements_limit"`
+	PerfEventsStatementsTimeLimit       int64             `toml:"perf_events_statements_time_limit"`
+	TableSchemaDatabases                []string          `toml:"table_schema_databases"`
+	GatherProcessList                   bool              `toml:"gather_process_list"`
+	GatherUserStatistics                bool              `toml:"gather_user_statistics"`
+	GatherInfoSchemaAutoInc             bool              `toml:"gather_info_schema_auto_inc"`
+	GatherInnoDBMetrics                 bool              `toml:"gather_innodb_metrics"`
+	GatherSlaveStatus                   bool              `toml:"gather_slave_status"`
+	GatherBinaryLogs                    bool              `toml:"gather_binary_logs"`
+	GatherTableIOWaits                  bool              `toml:"gather_table_io_waits"`
+	GatherTableLockWaits                bool              `toml:"gather_table_lock_waits"`
+	GatherIndexIOWaits                  bool              `toml:"gather_index_io_waits"`
+	GatherEventWaits                    bool              `toml:"gather_event_waits"`
+	GatherTableSchema                   bool              `toml:"gather_table_schema"`
+	GatherFileEventsStats               bool              `toml:"gather_file_events_stats"`
+	GatherPerfEventsStatements          bool              `toml:"gather_perf_events_statements"`
+	GatherGlobalVars                    bool              `toml:"gather_global_variables"`
+	GatherGlobalStatus                  bool              `toml:"gather_global_status"`
+	IntervalSlow                        string            `toml:"interval_slow"`
+	Tags                                map[string]string `toml:"tags"`
 
 	lastT            time.Time
 	initDone         bool
