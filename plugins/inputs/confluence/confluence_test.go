9601f18c55347904bc5ca767bee5344d74f69bd9
liguozhuang <liguozhuang@jiagouyun.com>
2020-09-03 17:51:38 +0800

    changed prom data
    Former-commit-id: 9b03dc4f942ad03166d88b53a6bf11517a1071f6 [formerly 192d350de76e15ca10dbcc14279a28e5efd9e883]
    Former-commit-id: cf6309ffe6f1b1f8a8a4cde04c3c666bd494231a

--- plugins/inputs/confluence/confluence_test.go
+++ plugins/inputs/confluence/confluence_test.go
@@ -2,22 +2,17 @@ package confluence
 
 import (
 	"testing"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/prom"
 )
 
 func TestMain(t *testing.T) {
-	testAssert = true
-
-	var co = Confluence{
+	c := Confluence{
 		URL:      "http://127.0.0.1:8090/plugins/servlet/prometheus/metrics",
 		Interval: "5s",
-		Tags:     map[string]string{"TEST_TAGS": "XXXXXXXXX"},
+		Tags:     map[string]string{"TestTags": "TestValue"},
 	}
 
-	data, err := co.getMetrics()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Logf("data: \n%s\n", data)
-
+	prom.TestAssert = true
+	c.Run()
 }
