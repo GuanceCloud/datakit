cbb0414ae2050065797f9f4e7b7ce86497a5c323
liguozhuang <liguozhuang@jiagouyun.com>
2020-09-04 18:54:16 +0800

    one
    Former-commit-id: 863df54a80c61b4130e9cb67eb03f29d9a870957 [formerly c0dcf4e963723819b7be40f6e51517ff13cc080d [formerly 8488ceebfeec2bca96c1855f288fcf7a32a29bf9] [formerly 1b57373ecd1da4ff6df4a2518ac556db5c9b641e [formerly c1ea14125bf821fbdd0e73c1601862f06debf59f]]]
    Former-commit-id: f96afa36b5a1d234ed517bb65dfae73bd18fa1e9
    Former-commit-id: cd78b794a819e3196be3adc27ea727eea301f61a [formerly f070652187150ecb7a8d043cdec1e7203ba23c10]
    Former-commit-id: 99fc745629aedc2fdcdd849140d6dbc076597920

--- plugins/inputs/envoy/envoy.go
+++ plugins/inputs/envoy/envoy.go
@@ -2,16 +2,13 @@ package envoy
 
 import (
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/prom"
 )
 
 const (
 	inputName = "envoy"
 
-	defaultMeasurement = "envoy"
-
 	sampleCfg = `
-[[inputs.envoy]]
+[[inputs.prom]]
     # envoy metrics from http(https)://HOST:PORT/stats/prometheus
     # usually modify host and port
     # required
@@ -27,7 +24,12 @@ const (
     # tls_cert = "/tmp/peer.crt"
     # tls_key = "/tmp/peer.key"
     
-    # [inputs.envoy.tags]
+    ## Internal configuration. Don't modify.
+    name = "envoy"
+    ignore_measurement = ["envoy_http", "envoy_listener"]
+    ignore_fields_key_prefix= ["envoy_server_worker"]
+
+    # [inputs.prom.tags]
     # from = "127.0.0.1:9901"
     # tags1 = "value1"
 `
@@ -40,13 +42,6 @@ func init() {
 }
 
 type Envoy struct {
-	URL        string            `toml:"url"`
-	Interval   string            `toml:"interval"`
-	CacertFile string            `toml:"tls_ca"`
-	CertFile   string            `toml:"tls_cert"`
-	KeyFile    string            `toml:"tls_key"`
-	Tags       map[string]string `toml:"tags"`
-	TLSOpen    bool              `toml:"tls_open"`
 }
 
 func (*Envoy) SampleConfig() string {
@@ -57,21 +52,5 @@ func (*Envoy) Catalog() string {
 	return inputName
 }
 
-func (e *Envoy) Run() {
-	p := prom.Prom{
-		URL:        e.URL,
-		Interval:   e.Interval,
-		TLSOpen:    e.TLSOpen,
-		CacertFile: e.CacertFile,
-		CertFile:   e.CertFile,
-		KeyFile:    e.KeyFile,
-		Tags:       e.Tags,
-
-		InputName:          inputName,
-		DefaultMeasurement: defaultMeasurement,
-
-		IgnoreMeasurement:     []string{"envoy_http", "envoy_listener"},
-		IgnoreFieldsKeyPrefix: []string{"envoy_server_worker"},
-	}
-	p.Start()
+func (*Envoy) Run() {
 }
