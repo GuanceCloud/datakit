dac229c4ed9929d416dc25bde440ba71399ef6b1
liguozhuang <liguozhuang@jiagouyun.com>
2020-09-09 14:45:22 +0800

    dockerContainers done
    Former-commit-id: 6259240eda8ed3f71987c9a45c36ab66961183ce [formerly 4f785e152214261a93c2a91a5d351f2d0f55da07] [formerly 4fba91e5aeaad0077a62bf67de72b3b04cd8ddb8 [formerly 33f6c34cd60500331de9c474f866dd4e6254e331]]
    Former-commit-id: 26e76d08f3c07c8491844a88050b0cc211e1f4a0 [formerly d9ced6065e8ee028f80960bb4b4531fbdaf7ee7d]
    Former-commit-id: a2e9523db9639bf523361d0860df9c92212e4eb9

---
+++ plugins/inputs/dockerContainers/dockerContainers.go
@@ -0,0 +1,221 @@
+package dockerContainers
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/docker/docker/api/types"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+const (
+	inputName = "dockerContainers"
+
+	sampleCfg = `
+[[inputName.dockerContainers]]
+    # Docker Endpoint
+    # To use TCP, set endpoint = "tcp://[ip]:[port]"
+    # To use environment variables (ie, docker-machine), set endpoint = "ENV"
+    endpoint = "unix:///var/run/docker.sock"
+
+    # valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h"
+    interval = "5s"
+
+    # Timeout for Docker API calls.
+    timeout = "5s"
+
+    ## Optional TLS Config
+    # tls_ca = "/etc/telegraf/ca.pem"
+    # tls_cert = "/etc/telegraf/cert.pem"
+    # tls_key = "/etc/telegraf/key.pem"
+    ## Use TLS but skip chain & host verification
+    # insecure_skip_verify = false
+`
+	defaultEndpoint = "unix:///var/run/docker.sock"
+)
+
+var l = logger.DefaultSLogger(inputName)
+
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		return &DockerContainers{
+			Endpoint:     defaultEndpoint,
+			newEnvClient: NewEnvClient,
+			newClient:    NewClient,
+		}
+	})
+}
+
+type DockerContainers struct {
+	Endpoint string `toml:"endpoint"`
+	Interval string `toml:"interval"`
+	Timeout  string `toml:"timeout"`
+
+	timeoutDuration  time.Duration
+	intervalDutation time.Duration
+	ClientConfig
+
+	newEnvClient func() (Client, error)
+	newClient    func(string, *tls.Config) (Client, error)
+
+	client Client
+	opts   types.ContainerListOptions
+
+	objects []*DockerObject
+}
+
+func (*DockerContainers) SampleConfig() string {
+	return sampleCfg
+}
+
+func (*DockerContainers) Catalog() string {
+	return "docker"
+}
+
+func (d *DockerContainers) Run() {
+	l = logger.SLogger(inputName)
+
+	if d.loadCfg() {
+		return
+	}
+
+	ticker := time.NewTicker(d.intervalDutation)
+	defer ticker.Stop()
+
+	l.Info("dockerContainers input start")
+	for {
+		select {
+		case <-datakit.Exit.Wait():
+			l.Info("exit")
+			return
+
+		case <-ticker.C:
+			d.gather()
+		}
+	}
+}
+
+func (d *DockerContainers) loadCfg() bool {
+	var err error
+	for {
+		select {
+		case <-datakit.Exit.Wait():
+			l.Info("exit")
+			return true
+		default:
+			// nil
+		}
+
+		d.intervalDutation, err = time.ParseDuration(d.Interval)
+		if err != nil || d.intervalDutation <= 0 {
+			err = fmt.Errorf("invalid interval, %s", err.Error())
+			goto label
+		}
+
+		d.timeoutDuration, err = time.ParseDuration(d.Timeout)
+		if err != nil || d.timeoutDuration <= 0 {
+			err = fmt.Errorf("invalid timeout, %s", err.Error())
+			goto label
+		}
+
+		if d.Endpoint == "ENV" {
+			d.client, err = d.newEnvClient()
+			if err != nil {
+				goto label
+			}
+		} else {
+			tlsConfig, err := d.ClientConfig.TLSConfig()
+			if err != nil {
+				goto label
+			}
+			d.client, err = d.newClient(d.Endpoint, tlsConfig)
+			if err != nil {
+				goto label
+			}
+		}
+		break
+	label:
+		l.Error(err)
+		time.Sleep(time.Second)
+	}
+	return false
+}
+
+func (d *DockerContainers) gather() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, d.timeoutDuration)
+	defer cancel()
+	containers, err := d.client.ContainerList(ctx, d.opts)
+	if err != nil {
+		l.Error(err)
+		return
+	}
+
+	for _, container := range containers {
+		if err = d.gatherContainer(container); err != nil {
+			l.Error(err)
+		}
+	}
+
+	data, err := json.Marshal(d.objects)
+	if err != nil {
+	}
+
+	if err := io.NamedFeed(data, io.Object, inputName); err != nil {
+	}
+
+	d.objects = d.objects[:0]
+}
+
+func (d *DockerContainers) gatherContainer(container types.Container) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, d.timeoutDuration)
+	defer cancel()
+	containerJSON, err := d.client.ContainerInspect(ctx, container.ID)
+	if err != nil {
+		return err
+	}
+
+	var obj = DockerObject{}
+	obj.Name = containerName(container.Names)
+	obj.Tags = Tags{
+		Class:           "docker_containers",
+		ContainerName:   obj.Name,
+		ContainerID:     container.ID,
+		ContainerImage:  container.Image,
+		ContainerStatue: container.State,
+		Host:            containerJSON.Config.Hostname,
+		PID:             strconv.Itoa(containerJSON.State.Pid),
+	}
+	obj.Carated = containerTime(containerJSON.Created)
+	obj.Started = containerTime(containerJSON.State.StartedAt)
+	obj.Finished = containerTime(containerJSON.State.FinishedAt)
+	obj.Path = containerJSON.Path
+	obj.Inspect = containerJSON
+
+	d.objects = append(d.objects, &obj)
+	return nil
+}
+
+func containerName(names []string) string {
+	if len(names) > 0 {
+		return names[0]
+	}
+	return ""
+}
+
+func containerTime(tim string) int64 {
+	t, err := time.Parse(time.RFC3339Nano, tim)
+	if err != nil {
+		return 0
+	}
+	return t.UnixNano()
+}
