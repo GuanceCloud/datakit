f1126819521120db6bb2439257ff6a190209efc1
tanbiao <tanbiao@jiagouyun.com>
2020-09-10 10:31:51 +0000

    update: 	[*] add more nginx inputs
    Former-commit-id: 89e825fec8a27b7332ab32a32d625b7d558a8ddd
    Former-commit-id: 6e6e88241058796d2e17d240d01038d331c1c7a7

--- vendor/github.com/google/gopacket/layers/sip.go
+++ vendor/github.com/google/gopacket/layers/sip.go
@@ -245,11 +245,14 @@ func (s *SIP) NextLayerType() gopacket.LayerType {
 
 // DecodeFromBytes decodes the slice into the SIP struct.
 func (s *SIP) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error {
+
 	// Init some vars for parsing follow-up
 	var countLines int
 	var line []byte
 	var err error
-	var offset int
+
+	// Clean leading new line
+	data = bytes.Trim(data, "\n")
 
 	// Iterate on all lines of the SIP Headers
 	// and stop when we reach the SDP (aka when the new line
@@ -262,21 +265,19 @@ func (s *SIP) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error {
 		line, err = buffer.ReadBytes(byte('\n'))
 		if err != nil {
 			if err == io.EOF {
-				if len(bytes.Trim(line, "\r\n")) > 0 {
-					df.SetTruncated()
-				}
 				break
 			} else {
 				return err
 			}
 		}
-		offset += len(line)
 
 		// Trim the new line delimiters
 		line = bytes.Trim(line, "\r\n")
 
 		// Empty line, we hit Body
+		// Putting packet remain in Paypload
 		if len(line) == 0 {
+			s.BaseLayer.Payload = buffer.Bytes()
 			break
 		}
 
@@ -297,7 +298,6 @@ func (s *SIP) DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error {
 
 		countLines++
 	}
-	s.BaseLayer = BaseLayer{Contents: data[:offset], Payload: data[offset:]}
 
 	return nil
 }
