fa9fe311af8f9a5ba5c375e329726e56c4273640
liguozhuang <liguozhuang@jiagouyun.com>
2020-09-10 18:01:01 +0800

    update description of docker_containers
    Former-commit-id: a2b1301b6b3263a5f9050b4b3d399d6ccc484989
    Former-commit-id: 6e2d740f22bbcf240e3fae0ed3864bf9556d97a9

--- plugins/inputs/docker_containers/object.go
+++ plugins/inputs/docker_containers/object.go
@@ -4,14 +4,14 @@ package docker_containers
 // 	"github.com/docker/docker/api/types"
 // )
 
-type DockerObject struct {
+type ContainerObject struct {
 	Name        string `json:"__name"`
 	Tags        Tags   `json:"__tags"`
 	Carated     int64  `json:"created"`
 	Started     int64  `json:"started_at"`
 	Finished    int64  `json:"finished_at"`
 	Path        string `json:"path"`
-	Description string `json:"__description"`
+	Description string `json:"__description,omitempty"`
 	//Inspect  types.ContainerJSON `json:"inspect"`
 }
 
