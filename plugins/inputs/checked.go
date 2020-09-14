707c689b2257db590ee3f6c93674454c8ac7939d
liguozhuang <liguozhuang@jiagouyun.com>
2020-09-14 15:46:45 +0800

    update checked
    Former-commit-id: dfce7061f95d876b1e8a3059d0a3e59831ca79b8
    Former-commit-id: 33eca2fb875411a8c87469bc98281ada4dab0711

--- plugins/inputs/checked.go
+++ plugins/inputs/checked.go
@@ -37,8 +37,8 @@ var (
 		"diskio":                 false,
 		"dns_query":              true,
 		"docker":                 true,
-		"docker_containers":      false,
-		"dockerlog":              false,
+		"docker_containers":      true,
+		"dockerlog":              true,
 		"druid":                  true,
 		"elasticsearch":          true,
 		"envoy":                  true,
@@ -127,6 +127,7 @@ var (
 		"tencentcost":            true,
 		"tencentobject":          false, // doc miss
 		"tengine":                true,
+		"tidb":                   false,
 		"timezone":               true,
 		"traceJaeger":            false,
 		"traceSkywalking":        false,
