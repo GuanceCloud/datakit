42c28271f8977e9989ad3ab49ec92f517356d249
liguozhuang <liguozhuang@jiagouyun.com>
2020-09-15 17:34:49 +0800

    delete update file list for tailf
     [formerly b9b92740853f57586259b8386678455b7658bb8a] [formerly 0e1bed5df08fe1adbd932e2513e444143f35f9dc [formerly 46cf8645028e765cfda23ee8efae87fb7e5746b4] [formerly a25983bb492baa1d7ca7fed8c98f9c765619e54e [formerly 5400fee21880c0cd3288939399fd572dd5eb7277] [formerly a25983bb492baa1d7ca7fed8c98f9c765619e54e [formerly 5400fee21880c0cd3288939399fd572dd5eb7277] [formerly a25983bb492baa1d7ca7fed8c98f9c765619e54e [formerly 5400fee21880c0cd3288939399fd572dd5eb7277] [formerly 4edac4e8262d65c1aecdd40a2708a4ff15303c11 [formerly b25b597a6531ab19b162e09af77dabc94519cd2c]]]]]]
     [formerly 2d82c56e1d368e4f78ff5c3750f749a7c552e3ea [formerly 21860c0ec082610dcfd04443383b75c36c31aa97] [formerly 2d82c56e1d368e4f78ff5c3750f749a7c552e3ea [formerly 21860c0ec082610dcfd04443383b75c36c31aa97] [formerly 0ae8f7fa926c7f7c0fa1e302015370028eb4a93d [formerly 6edddb86d8361e5177bad34f08e6b6bb9d7b6f40]]] [formerly 2d82c56e1d368e4f78ff5c3750f749a7c552e3ea [formerly 21860c0ec082610dcfd04443383b75c36c31aa97] [formerly 0ae8f7fa926c7f7c0fa1e302015370028eb4a93d [formerly 6edddb86d8361e5177bad34f08e6b6bb9d7b6f40]] [formerly 0ae8f7fa926c7f7c0fa1e302015370028eb4a93d [formerly 6edddb86d8361e5177bad34f08e6b6bb9d7b6f40] [formerly 9e46badfa2279b68a85a9878b96062850dc47db2 [formerly e3f77f06cd850309ab728bfc18080f6fab94d723]]]]]
    
    
    
    Former-commit-id: 051a6224e8ea4336d2e3fba77cd1ccaa4c9f003c [formerly 271113edd0e2455373351eae5060b51d10c2c26f]
    Former-commit-id: 8368acf61cb1bcab70d5a1c291e3625207677857 [formerly b226f94c30c7afe8c338fbc0c7a206a510b988f0] [formerly c7d8369cd255b8346b9f7f7e64ef4c2ce6e2bb77 [formerly 76a5e6a0da3a8e4a43378ea0d3f85e8ccd9b5ee8]]
    Former-commit-id: 0d6eb63a131ec1a8ecf358fd65ba3751b08d7fc4 [formerly 5f782f4a2dd82b50fb4aa049008b34065e24a1e8]
    Former-commit-id: 710e2ea462939316ad1e0c57399b7812f2630af8

---
+++ plugins/inputs/tailf/files.go
@@ -0,0 +1,57 @@
+package tailf
+
+import (
+	"github.com/gobwas/glob"
+	"github.com/mattn/go-zglob"
+)
+
+func getFileList(filesGlob, ignoreGlob []string) []string {
+	var matches = make(map[string]interface{})
+
+	files := fileList(filesGlob)
+	ignores := ignoreList(ignoreGlob)
+
+	for _, f := range files {
+		for _, g := range ignores {
+			if g.Match(f) {
+				goto next
+			}
+		}
+		matches[f] = nil
+	next:
+	}
+
+	// unique
+	var list []string
+	for matche := range matches {
+		list = append(list, matche)
+	}
+
+	return list
+}
+
+func fileList(filesGlob []string) []string {
+	var matches []string
+	for _, f := range filesGlob {
+		matche, err := zglob.Glob(f)
+		if err != nil {
+			l.Errorf("logfiles glob, %s", err)
+			continue
+		}
+		matches = append(matches, matche...)
+	}
+	return matches
+}
+
+func ignoreList(ignoreGlob []string) []glob.Glob {
+	var globs []glob.Glob
+	for _, ig := range ignoreGlob {
+		g, err := glob.Compile(ig)
+		if err != nil {
+			l.Errorf("ignore glob, %s", err)
+			continue
+		}
+		globs = append(globs, g)
+	}
+	return globs
+}
