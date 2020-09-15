ba46f5c88515d2fbfdef8ef754bc7412dc750204
tanbiao <tanbiao@jiagouyun.com>
2020-09-15 10:51:40 +0000

    update: 	[*] add field uint64 support: convert to string 	[*] add tag value re-write: remove all `\` suffix
     [formerly 2f7e784bd41c81b74c7a6c51e6b05bd21080c6a0 [formerly 0a1faa4306ad5ce735be02e97e72a2d90270b69a] [formerly 5a9d92a3f1e008b149249bd2d9cc4e5380ffdab5 [formerly d1c2913873189f574d7774d8adfd303076a1d487]] [formerly 8dc66b125578a18a3bdf11eebf291b3f36785f68] [formerly 2fc850193210e08257c538a55d57877942c8deae [formerly 426c6960bb98a3e949fb811f74ad7ec368c85d4b] [formerly 2fc850193210e08257c538a55d57877942c8deae [formerly 426c6960bb98a3e949fb811f74ad7ec368c85d4b] [formerly 2b107399491425997522986ecc97b64d3ff57c4d [formerly 0b0d63da817865f868d433bc5cdbf87a44c079c9]]]] [formerly 7be4e31174161388c8baad71a608ee925e36dc8f] [formerly 7abcfbf7a022f52346b4dbf6f8243bfdd691fb31 [formerly 29890e1fbfcd37b1a566908164b345e3a543b934] [formerly d6992bebbbee2ef81f0cf9451592831561a13b67 [formerly 70026db00914210213983b2f45602797501a23b9] [formerly d6992bebbbee2ef81f0cf9451592831561a13b67 [formerly 70026db00914210213983b2f45602797501a23b9] [formerly d6992bebbbee2ef81f0cf9451592831561a13b67 [formerly 70026db00914210213983b2f45602797501a23b9] [formerly 9f0f0554bf929372f6fa861b2d9dc4c10f22d1b7 [formerly 4aa0e32794228626712e1ab57d49b95bd806cbf5]]]]]]]
     [formerly 7be4e31174161388c8baad71a608ee925e36dc8f]
    
    
    
    
    Former-commit-id: 7ff342640f9fcf3bfe99dc3038ccda70b81d52ad [formerly e62839ea8cd92e74a27b6287a6b691c8053656be]
    Former-commit-id: 1be4bc1d468945e8017b5d1c3c308050338d6998 [formerly 46ce6b00df1507269982106abb764ae892e45843] [formerly 367854d6f4ef8342e962a80d973615890a4fa7cb [formerly d80ed6b8a184be00e9a46cd199c09009593f33e4]]
    Former-commit-id: 709429a44a16b684bee7c578f7e1b415237afc67 [formerly 951b2f5fab046f27a1160756ddfc7c959c5ade8b]
    Former-commit-id: 3718b43597907ec28aba0313db3b5ae6abe4f555

--- io/io.go
+++ io/io.go
@@ -176,6 +176,29 @@ func MakeMetric(name string, tags map[string]string, fields map[string]interface
 		}
 	}
 
+	for k, v := range tags { // remove any suffix `\` in all tag values
+		tags[k] = datakit.TrimSuffixAll(v, `\`)
+	}
+
+	for k, v := range fields { // convert uint to int
+		switch v.(type) {
+		case uint64:
+			fields[k] = fmt.Sprintf("%d", v.(uint64)) // convert uint64 to string to avoid overflow
+			l.Warnf("force convert uint64 to string(%d -> %s)", v.(uint64), fields[k])
+		case uint32:
+			fields[k] = int64(v.(uint32))
+			l.Warn("force convert uint32 to int64")
+		case uint16:
+			fields[k] = int64(v.(uint16))
+			l.Warn("force convert uint16 to int64")
+		case uint8:
+			fields[k] = int64(v.(uint8))
+			l.Warn("force convert uint8 to int64")
+		default:
+			// pass
+		}
+	}
+
 	pt, err := ifxcli.NewPoint(name, tags, fields, tm)
 	if err != nil {
 		return nil, err
