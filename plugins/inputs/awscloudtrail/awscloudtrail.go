418dd88717a1d91b4af2c7247300939d8b9a27f3
wuqinchao <wuqinchao@jiagouyun.com>
2020-09-15 16:50:37 +0800

    aws action trail采集器
     [formerly 0ed2e15be8fd658b62a3a630003e45b874461cd0 [formerly 19ec174e59a25ab1c492993b4217db61a954b5a9] [formerly a410c48cefa9544c20574f374c65ce56fc0856f4 [formerly fec0fb0f5860fd37f892f129085de68f22be5412]] [formerly b0634808f2de520787a22a73d801318e01b1c0a3] [formerly bc75a4e74b213911c256cec83e6ad661aa8a5b50 [formerly de20160a253769acbdcc97fc80bdd55ff99b5697] [formerly bc75a4e74b213911c256cec83e6ad661aa8a5b50 [formerly de20160a253769acbdcc97fc80bdd55ff99b5697] [formerly 76311a4d866eaa41851b8880cb5696edc090595c [formerly 2d8912a29e913ed78d1abb36354cc0471facf158]]]] [formerly 6b3dc40eeaa3eea732d9aeea91e27f64c547aa2e] [formerly 213c9fb02a450f9a0ea003b27d97738d7ac134b8 [formerly 1d4f260b0381cc908296be2934f343fbfb04a42d] [formerly ee15734ba36642596922db8b067b1b024b551f63 [formerly a3e002c6b72ea80b4d109a6dc5e5658b16bb6f6c] [formerly ee15734ba36642596922db8b067b1b024b551f63 [formerly a3e002c6b72ea80b4d109a6dc5e5658b16bb6f6c] [formerly ee15734ba36642596922db8b067b1b024b551f63 [formerly a3e002c6b72ea80b4d109a6dc5e5658b16bb6f6c] [formerly b2fb944dca4591e103808d4bc6ecc00a39a1b32f [formerly 71d138cb7ed6365124c32818c01ae095e82c15a9]]]]]]]
     [formerly 6b3dc40eeaa3eea732d9aeea91e27f64c547aa2e]
    
    
    
    
    Former-commit-id: aab3eb2243dff2aba30da3c29bb740a17894b4bf [formerly b28ed5108fe4f32da81d6c1fbbc9f137451ad4f2]
    Former-commit-id: 83f9802fde10021df4a5da3b1bab1febb2be7b99 [formerly b1b1e7415525ea765531de4ac5feddc6033ed500] [formerly d96bf40ce89f1ed42da77df6f82bfc690292ad48 [formerly 28c3386c5be6112752bbdfedaceba7906aaaa593]]
    Former-commit-id: b5978a13fc5758f174ce1f10991d089d8989d36c [formerly 543fe7f6d2ec5ee60cc6b586d9006fa244001199]
    Former-commit-id: cf827fbe5119ccc1bbffdb1dc2bc5d6b57892c91

---
+++ plugins/inputs/awscloudtrail/awscloudtrail.go
@@ -0,0 +1,187 @@
+package awscloudtrail
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"golang.org/x/time/rate"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudtrail"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+var (
+	inputName    = "awscloudtrail"
+	moduleLogger *logger.Logger
+)
+
+func (*AwsInstance) Catalog() string {
+	return "aws"
+}
+
+func (*AwsInstance) SampleConfig() string {
+	return sampleConfig
+}
+
+func (a *AwsInstance) Run() {
+
+	moduleLogger = logger.SLogger(inputName)
+
+	go func() {
+		<-datakit.Exit.Wait()
+		a.cancelFun()
+	}()
+
+	if a.Interval.Duration == 0 {
+		a.Interval.Duration = time.Minute * 5
+	}
+
+	limit := rate.Every(500 * time.Millisecond)
+	a.rateLimiter = rate.NewLimiter(limit, 1)
+
+	a.run(a.ctx)
+}
+
+func (r *AwsInstance) initClient() error {
+	cred := credentials.NewStaticCredentials(r.AccessKey, r.AccessSecret, "")
+
+	cfg := aws.NewConfig()
+	cfg.WithCredentials(cred).WithRegion(r.RegionID)
+
+	sess, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigDisable,
+		Config:            *cfg,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	r.awsClient = cloudtrail.New(sess, aws.NewConfig())
+
+	return nil
+}
+
+func (r *AwsInstance) run(ctx context.Context) {
+
+	defer func() {
+		if e := recover(); e != nil {
+			moduleLogger.Errorf("panic, %v", e)
+		}
+	}()
+
+	for {
+		select {
+		case <-datakit.Exit.Wait():
+			return
+		default:
+		}
+
+		if err := r.initClient(); err != nil {
+			moduleLogger.Errorf("fail to init client, %s", err)
+			time.Sleep(time.Second)
+		} else {
+			break
+		}
+
+	}
+
+	measurement := `awscloudtrail`
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		startTime := time.Now().UTC().Truncate(time.Minute).Add(-r.Interval.Duration - time.Minute)
+
+		input := &cloudtrail.LookupEventsInput{
+			StartTime: aws.Time(startTime),
+		}
+		response, err := r.awsClient.LookupEvents(input)
+
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		if err != nil {
+			moduleLogger.Errorf("fail to LookupEvents, %s", err)
+		} else {
+			for _, evt := range response.Events {
+
+				tags := map[string]string{}
+				fields := map[string]interface{}{}
+
+				if evt.EventSource != nil {
+					tags["EventSource"] = aws.StringValue(evt.EventSource)
+				}
+				if evt.Username != nil {
+					tags["Username"] = aws.StringValue(evt.Username)
+				}
+				if evt.ReadOnly != nil {
+					tags["ReadOnly"] = aws.StringValue(evt.ReadOnly)
+				}
+
+				if evt.EventName != nil {
+					fields["EventName"] = aws.StringValue(evt.EventName)
+				}
+				if evt.EventId != nil {
+					fields["EventId"] = aws.StringValue(evt.EventId)
+				}
+				if evt.AccessKeyId != nil {
+					fields["AccessKeyId"] = aws.StringValue(evt.AccessKeyId)
+				}
+				if evt.CloudTrailEvent != nil {
+					fields["CloudTrailEvent"] = aws.StringValue(evt.CloudTrailEvent)
+				}
+				resources := []string{}
+				for _, res := range evt.Resources {
+					if res.ResourceName != nil {
+						r := aws.StringValue(res.ResourceName)
+						if res.ResourceType != nil {
+							r += "(" + aws.StringValue(res.ResourceType) + ")"
+						}
+						resources = append(resources, r)
+					}
+				}
+				fields["Resources"] = strings.Join(resources, ",")
+
+				if r.debugMode {
+					data, _ := io.MakeMetric(measurement, tags, fields, *evt.EventTime)
+					fmt.Printf("%s\n", string(data))
+				} else {
+					io.NamedFeedEx(inputName, io.Metric, measurement, tags, fields, *evt.EventTime)
+				}
+			}
+
+		}
+
+		datakit.SleepContext(ctx, r.Interval.Duration)
+	}
+
+}
+
+func newInstance() *AwsInstance {
+	ac := &AwsInstance{}
+	ac.ctx, ac.cancelFun = context.WithCancel(context.Background())
+	return ac
+}
+
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		return newInstance()
+	})
+}
