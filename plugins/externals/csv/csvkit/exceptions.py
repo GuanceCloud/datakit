7e6134c25eb70c15d86dd5f7c11f7616d01113d3
huangbo <huangbo@jiagouyun.com>
2020-09-25 17:56:38 +0800

    csvmetric采集器增加
     [formerly f896780412736c691479fd42b6043b3380d2aea4 [formerly 406bf92a68a27d9949746cb15442f6d9888f663a] [formerly 0e3961e5a3c10593f6b30ac5dbc606fbc481e02a [formerly c65461cc2443d9971da9aa7d3b10eb2d40cd74e3]] [formerly f639822a6123c1ec53a1edb72ffbc639c1e2b89d] [formerly b347cba4a4495a2c15d8ed9b1f32611d1bff0259 [formerly dc41159c03d13aecf18e1386cc7912fb0734ba94] [formerly b347cba4a4495a2c15d8ed9b1f32611d1bff0259 [formerly dc41159c03d13aecf18e1386cc7912fb0734ba94] [formerly a4c30f04bed1f618c284fb19a78b4c143f180897 [formerly 47337a624915da765477be99dc56181bd36ee283]]]] [formerly 83925dddaec0a42eee2d631532946e74061ef0cc] [formerly c133a4e11c4888786ddd0393b9e372cc6caebc22 [formerly d2ab41991128d50e47a954cdd0dd2fc64e2ae665] [formerly 19386993b3ac5842ae3aa7b7d51c2447707617d3 [formerly 8ce274f3bd575dc4d3515c8108819a3d1b91792a] [formerly 19386993b3ac5842ae3aa7b7d51c2447707617d3 [formerly 8ce274f3bd575dc4d3515c8108819a3d1b91792a] [formerly 19386993b3ac5842ae3aa7b7d51c2447707617d3 [formerly 8ce274f3bd575dc4d3515c8108819a3d1b91792a] [formerly 7deee46baffbee8c70ddd2b5e03b5dab8feb044e [formerly ba5417bb7fa92c1f0ff6c3b3065cb9f6e1f62306]]]]]]]
     [formerly 83925dddaec0a42eee2d631532946e74061ef0cc]
    
    
    
    
    Former-commit-id: e15e00bafb55a09884185b3d43b4a5fa3dee7589 [formerly 44b77ababd0e60196d9b495526dd3863766843ec]
    Former-commit-id: 2e185094dc8822668bc1cb736d1a84a7ee48a23f [formerly 67f94712fbab8c34e6e8763c53f760368abfae01] [formerly 12d11e8b9fdb7895f48fcbaa2f13d922a6cd2124 [formerly 250df944f6cc875e4753b7dc4cd466a98662e7e6]]
    Former-commit-id: 84ea4137afc359e080409281bf0f18ef13e2eacd [formerly c79442e9e50d89c5e418662841fde8aef6ef6eb6]
    Former-commit-id: 59ecaf6b2b3de8062db95138f0d851ecb351ebe7

--- plugins/externals/csv/csvkit/exceptions.py
+++ plugins/externals/csv/csvkit/exceptions.py
@@ -1,8 +1,11 @@
-class AbortException(Exception):
+class ConfException(BaseException):
     pass
 
-class DropException(Exception):
+class AbortException(BaseException):
     pass
 
-class IgnoreException(Exception):
+class DropException(BaseException):
+    pass
+
+class IgnoreException(BaseException):
     pass
