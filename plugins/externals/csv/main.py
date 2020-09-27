4988650b25c7d0065aaeeca6866b06eecc77edfc
huangbo <huangbo@jiagouyun.com>
2020-09-27 18:07:14 +0800

    csvobject
     [formerly 5b8b5aa1ead192595f48c154c404750cbe51604d [formerly 683f24af7280c30e3455999f955cf7d6edf9408e] [formerly 20f36abe2d12473b2492ef5b065e57758c3e7494 [formerly 4aa621355f16915086414e7edcfd85557d4d7c4f]] [formerly 4acca8d7c74314565a3a9ee5897bbf0e36e2dce7] [formerly 7dbc40121043328ad02edbc210e1c03aa1786577 [formerly 403d922023a5ded1c9247294ae4cb78099070e4e] [formerly 7dbc40121043328ad02edbc210e1c03aa1786577 [formerly 403d922023a5ded1c9247294ae4cb78099070e4e] [formerly 8193718f9889e144c67def451ab29f0ff577ed9c [formerly 2e44b5f8649e3c73708e6413a70f4f9ba9303f13]]]] [formerly 78d995054b4479ab155fece3972db3dd46e7e301] [formerly fe37e8f9c3d3e61dfcce7e60238f847b7e52db8c [formerly 9b0249b3d3ddc61f969e0ecc2f7c263379082972] [formerly b90f6040e48f8cde0d967d2e2495e04a9259cf7d [formerly 75565408b7c47e832a98f93d9244ffbeba8b2947] [formerly b90f6040e48f8cde0d967d2e2495e04a9259cf7d [formerly 75565408b7c47e832a98f93d9244ffbeba8b2947] [formerly b90f6040e48f8cde0d967d2e2495e04a9259cf7d [formerly 75565408b7c47e832a98f93d9244ffbeba8b2947] [formerly a22ac1beb9870ed9a4ee20e2ee3e916631f2c1b8 [formerly c8621d1f68686958392aa5be3e7dede8b1e0b49a]]]]]]]
     [formerly 78d995054b4479ab155fece3972db3dd46e7e301]
    
    
    
    
    Former-commit-id: cd0c792a6619781166b03264a194e2d74e7d12ba [formerly b2b8f62c8025e46bfb51f7526879f10c91d14ffb]
    Former-commit-id: 61aeb8540d668574e1e4b51899811bc5b3ca8107 [formerly 5986abd74f819e60f31693bcb6a150080568a24b] [formerly 7486a63220123cb5ba43f0e8c4181080a035ced9 [formerly 151529b2586f827ee171c04e23b196767e182b56]]
    Former-commit-id: 0b5b746f9bcf43564c40e0b05c62e08bd1012923 [formerly e000cfe15b25e94105a4b43964642b50c1d6c5ef]
    Former-commit-id: c75bc4c1861c727e83f06e780305a090476581e4

--- plugins/externals/csv/main.py
+++ plugins/externals/csv/main.py
@@ -5,6 +5,7 @@ import logging
 from csvkit.network import set_http_addr
 from csvkit.utils import exit, parse_cli
 from csvkit.metric import collect_metric
+from csvkit.object import collect_object
 from csvkit.log import en_logging
 from csvkit.toml_parse import parse_toml_cfg
 
@@ -28,31 +29,9 @@ if args.metric != "":
     collect_metric(parsed_cfg)
 
 if args.object != "":
-    object_cfg = base64.standard_b64decode(args.object)
+    object_cfg = base64.standard_b64decode(args.object).decode()
     logging.info("toml cfg info about metric {}".format(object_cfg))
     
     parsed_cfg = parse_toml_cfg(object_cfg)
     logging.info("toml parsed cfg info about object {}".format(parsed_cfg))
-    pass
-
-# try:
-#     logging.info("start read yaml cfg file")
-#     yaml_cfg = ""
-#     if args.yaml != "":
-#         yaml_cfg =  base64.standard_b64decode(args.yaml)
-#
-#
-#     if yaml_cfg == "":
-#         logging.critical("yaml cfg empty")
-#         exit(0)
-#
-#     logging.info("start parse yaml cfg file")
-#     yaml_data = YamlParser(yaml_cfg).parse()
-#     logging.info("parsed yaml cfg data: {}".format(yaml_data))
-#
-#     succ_csv = get_file(yaml_data[FILE])
-#     succ_csv = csv2excel(succ_csv)
-#     Worker(yaml_data, *[succ_csv]).run()
-#     exit(0)
-# except Exception as e:
-#     logging.critical("{}".format(e))
+    collect_object(parsed_cfg)
