6b3fe156e3e1b166e28f322e457022885095be1d
wuqinchao <wuqinchao@jiagouyun.com>
2020-09-28 13:43:18 +0800

    .
     [formerly 5d982d78828c72081adef0e40feb9edf21e7a23e [formerly 71152fff4b9211f5e45d12eac8dc41be9d5e1ad3] [formerly 5c52c1806e0a9f570c18d8d3ae7d556dfabf80d6 [formerly ef7078a02af23d1d025640abf85958a81e59951a]] [formerly 6a061369bd5ae4960514b7e36f0926a447ca641b] [formerly 4e48426b55e90fb9ede3203370c5b690e6a8bd7e [formerly 5a78d717e5451f80acfc5f222af648cc4afb679e] [formerly 4e48426b55e90fb9ede3203370c5b690e6a8bd7e [formerly 5a78d717e5451f80acfc5f222af648cc4afb679e] [formerly 3299113bfb4e27faec03277666e1394c37d29e01 [formerly b51174e3e8914196b141812b715148a39ee8fbaa]]]] [formerly b1427f84bb5c6261e437eb0e5ab499f24a581de3] [formerly 58902360b10191726aceb5243b840b988e6e10db [formerly 9e2ae69ddf6ad7955c4b90adbd8711ba4a6d9ea6] [formerly dd820b9a1500f7768f86d9840242d83b0352fb4b [formerly b168529eaf1cfa8bb9a2e0ff2b09a086b26170e2] [formerly dd820b9a1500f7768f86d9840242d83b0352fb4b [formerly b168529eaf1cfa8bb9a2e0ff2b09a086b26170e2] [formerly dd820b9a1500f7768f86d9840242d83b0352fb4b [formerly b168529eaf1cfa8bb9a2e0ff2b09a086b26170e2] [formerly 33db63b36a8374dd619ca63e4dcd8709861c0fd8 [formerly 23e4aa341fba4b4e12ea1a2aef4ff46737a69638]]]]]]]
     [formerly b1427f84bb5c6261e437eb0e5ab499f24a581de3]
    
    
    
    
    Former-commit-id: 317713f50d88cd5cf28beea397c5755dca36bcf4 [formerly 7cebf7c0b7123d94c9463d1f62796c4dc97f575f]
    Former-commit-id: 8e639c7cdf8395b1ebfec277ef3f6b9d22c9ac33 [formerly 9158d61fc4a29e2755174487bbec2114b93a1d57] [formerly b9867c5106d1a68590041e0416e9c940f48b693d [formerly 2dbb4bfa2085426f8356cd7078edd0dc4c2f4314]]
    Former-commit-id: f1875b9ded77de72ef6f369eafdf5286f29be57e [formerly 7f914ffec29ea5faee91c3b9c6981a120b457c27]
    Former-commit-id: dedf617a1eec24cbae70630c2baa0dd53604078b

--- plugins/inputs/ucmon/running_instance.go
+++ plugins/inputs/ucmon/running_instance.go
@@ -1,135 +1,123 @@
 package ucmon
 
-import (
-	"context"
-	"time"
+// func (r *ucInstance) run(ctx context.Context) error {
 
-	"github.com/ucloud/ucloud-sdk-go/ucloud"
-	"github.com/ucloud/ucloud-sdk-go/ucloud/auth"
-	"golang.org/x/time/rate"
+// 	uccfg := ucloud.NewConfig()
+// 	credential := auth.NewCredential()
+// 	credential.PrivateKey = r.PrivateKey
+// 	credential.PublicKey = r.PublicKey
+// 	r.ucCli = ucloud.NewClient(&uccfg, &credential)
 
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
-)
+// 	r.queryInfos = r.genQueryInfo()
 
-func (r *ucInstance) run(ctx context.Context) error {
+// 	select {
+// 	case <-ctx.Done():
+// 		return context.Canceled
+// 	default:
+// 	}
 
-	uccfg := ucloud.NewConfig()
-	credential := auth.NewCredential()
-	credential.PrivateKey = r.PrivateKey
-	credential.PublicKey = r.PublicKey
-	r.ucCli = ucloud.NewClient(&uccfg, &credential)
+// 	limit := rate.Every(50 * time.Millisecond)
+// 	rateLimiter := rate.NewLimiter(limit, 1)
 
-	r.queryInfos = r.genQueryInfo()
+// 	for {
 
-	select {
-	case <-ctx.Done():
-		return context.Canceled
-	default:
-	}
+// 		select {
+// 		case <-ctx.Done():
+// 			return context.Canceled
+// 		default:
+// 		}
 
-	limit := rate.Every(50 * time.Millisecond)
-	rateLimiter := rate.NewLimiter(limit, 1)
+// 		t := time.Now()
+// 		for _, req := range r.queryInfos {
 
-	for {
+// 			select {
+// 			case <-ctx.Done():
+// 				return context.Canceled
+// 			default:
+// 			}
 
-		select {
-		case <-ctx.Done():
-			return context.Canceled
-		default:
-		}
+// 			rateLimiter.Wait(ctx)
+// 			if err := r.fetchMetric(ctx, req); err != nil {
+// 				moduleLogger.Errorf(`fail to get metric "%s.%s", %s`, req.resourceID, req.metricname, err)
+// 			}
+// 		}
+// 		useage := time.Now().Sub(t)
+// 		if useage < 5*time.Minute {
+// 			datakit.SleepContext(ctx, 5*time.Minute-useage)
+// 		}
+// 	}
+// }
 
-		t := time.Now()
-		for _, req := range r.queryInfos {
+// func (r *ucInstance) fetchMetric(ctx context.Context, info *queryListInfo) error {
 
-			select {
-			case <-ctx.Done():
-				return context.Canceled
-			default:
-			}
+// 	now := time.Now().Truncate(time.Minute).Add(-time.Minute)
+// 	if now.Sub(info.lastFetchTime) < info.intervalTime {
+// 		return nil
+// 	}
 
-			rateLimiter.Wait(ctx)
-			if err := r.fetchMetric(ctx, req); err != nil {
-				moduleLogger.Errorf(`fail to get metric "%s.%s", %s`, req.resourceID, req.metricname, err)
-			}
-		}
-		useage := time.Now().Sub(t)
-		if useage < 5*time.Minute {
-			datakit.SleepContext(ctx, 5*time.Minute-useage)
-		}
-	}
-}
+// 	var begin, end int64
+// 	if info.lastFetchTime.IsZero() {
+// 		if info.intervalTime < time.Minute*5 {
+// 			begin = now.Add(-5 * time.Minute).Unix()
+// 		} else {
+// 			begin = now.Add(-info.intervalTime).Unix()
+// 		}
+// 	} else {
+// 		begin = info.lastFetchTime.Unix()
+// 	}
+// 	end = now.Unix()
 
-func (r *ucInstance) fetchMetric(ctx context.Context, info *queryListInfo) error {
+// 	moduleLogger.Debugf("query param: resourceID=%s, metric=%s, range=%v-%v, interval=%s", info.resourceID, info.metricname, begin, end, info.intervalTime)
 
-	now := time.Now().Truncate(time.Minute).Add(-time.Minute)
-	if now.Sub(info.lastFetchTime) < info.intervalTime {
-		return nil
-	}
+// 	req := r.ucCli.NewGenericRequest()
+// 	req.SetAction("GetMetric")
+// 	req.SetRegion(r.Region)
+// 	req.SetZone(r.Zone)
+// 	req.SetProjectId(r.ProjectID)
+// 	reqPayload := map[string]interface{}{}
+// 	reqPayload["ResourceType"] = info.resourceType
+// 	reqPayload["ResourceId"] = info.resourceID
+// 	reqPayload["MetricName"] = info.metricname
+// 	reqPayload["BeginTime"] = begin
+// 	reqPayload["EndTime"] = end
+// 	req.SetPayload(reqPayload)
 
-	var begin, end int64
-	if info.lastFetchTime.IsZero() {
-		if info.intervalTime < time.Minute*5 {
-			begin = now.Add(-5 * time.Minute).Unix()
-		} else {
-			begin = now.Add(-info.intervalTime).Unix()
-		}
-	} else {
-		begin = info.lastFetchTime.Unix()
-	}
-	end = now.Unix()
+// 	resp, err := r.ucCli.GenericInvoke(req)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	moduleLogger.Debugf("query param: resourceID=%s, metric=%s, range=%v-%v, interval=%s", info.resourceID, info.metricname, begin, end, info.intervalTime)
+// 	payload := resp.GetPayload()
+// 	if mapData, ok := payload["DataSets"].(map[string]interface{}); ok {
+// 		for _, v := range mapData {
+// 			if series, ok := v.([]interface{}); ok {
+// 				for _, metricItem := range series {
+// 					if metricItemMap, ok := metricItem.(map[string]interface{}); ok {
 
-	req := r.ucCli.NewGenericRequest()
-	req.SetAction("GetMetric")
-	req.SetRegion(r.Region)
-	req.SetZone(r.Zone)
-	req.SetProjectId(r.ProjectID)
-	reqPayload := map[string]interface{}{}
-	reqPayload["ResourceType"] = info.resourceType
-	reqPayload["ResourceId"] = info.resourceID
-	reqPayload["MetricName"] = info.metricname
-	reqPayload["BeginTime"] = begin
-	reqPayload["EndTime"] = end
-	req.SetPayload(reqPayload)
+// 						metricName := "ucmon_" + info.metricname
+// 						tags := map[string]string{
+// 							"Region":       r.Region,
+// 							"Zone":         r.Zone,
+// 							"ProjectID":    r.ProjectID,
+// 							"ResourceID":   info.resourceID,
+// 							"ResourceType": info.resourceType,
+// 						}
+// 						fields := metricItemMap
+// 						metricTime := time.Unix(time.Now().Unix(), 0)
+// 						if tm, ok := fields["Timestamp"].(float64); ok {
+// 							metricTime = time.Unix(int64(tm), 0)
+// 							delete(fields, "Timestamp")
+// 						}
 
-	resp, err := r.ucCli.GenericInvoke(req)
-	if err != nil {
-		return err
-	}
+// 						io.NamedFeedEx(inputName, io.Metric, metricName, tags, fields, metricTime)
 
-	payload := resp.GetPayload()
-	if mapData, ok := payload["DataSets"].(map[string]interface{}); ok {
-		for _, v := range mapData {
-			if series, ok := v.([]interface{}); ok {
-				for _, metricItem := range series {
-					if metricItemMap, ok := metricItem.(map[string]interface{}); ok {
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
 
-						metricName := "ucmon_" + info.metricname
-						tags := map[string]string{
-							"Region":       r.Region,
-							"Zone":         r.Zone,
-							"ProjectID":    r.ProjectID,
-							"ResourceID":   info.resourceID,
-							"ResourceType": info.resourceType,
-						}
-						fields := metricItemMap
-						metricTime := time.Unix(time.Now().Unix(), 0)
-						if tm, ok := fields["Timestamp"].(float64); ok {
-							metricTime = time.Unix(int64(tm), 0)
-							delete(fields, "Timestamp")
-						}
+// 	info.lastFetchTime = now
 
-						io.NamedFeedEx(inputName, io.Metric, metricName, tags, fields, metricTime)
-
-					}
-				}
-			}
-		}
-	}
-
-	info.lastFetchTime = now
-
-	return nil
-}
+// 	return nil
+// }
