e07de562fe9161ee0241c943bb2ab24c4a36d648
wuqinchao <wuqinchao@jiagouyun.com>
2020-09-28 13:43:18 +0800

    .
    Former-commit-id: 9e2ae69ddf6ad7955c4b90adbd8711ba4a6d9ea6 [formerly 901d6d21c5af5579f7b33a10c9de3c061071b88a [formerly b168529eaf1cfa8bb9a2e0ff2b09a086b26170e2] [formerly 33db63b36a8374dd619ca63e4dcd8709861c0fd8 [formerly 23e4aa341fba4b4e12ea1a2aef4ff46737a69638]]]
    Former-commit-id: b1427f84bb5c6261e437eb0e5ab499f24a581de3
    Former-commit-id: 3299113bfb4e27faec03277666e1394c37d29e01 [formerly b51174e3e8914196b141812b715148a39ee8fbaa]
    Former-commit-id: 5a78d717e5451f80acfc5f222af648cc4afb679e

--- plugins/inputs/ucmon/ucmon.go
+++ plugins/inputs/ucmon/ucmon.go
@@ -2,9 +2,15 @@ package ucmon
 
 import (
 	"context"
+	"time"
+
+	"github.com/ucloud/ucloud-sdk-go/ucloud"
+	"github.com/ucloud/ucloud-sdk-go/ucloud/auth"
+	"golang.org/x/time/rate"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
@@ -34,8 +40,124 @@ func (ag *ucInstance) Run() {
 		ag.cancelFun()
 	}()
 
-	ag.run(ag.ctx)
+	uccfg := ucloud.NewConfig()
+	credential := auth.NewCredential()
+	credential.PrivateKey = ag.PrivateKey
+	credential.PublicKey = ag.PublicKey
+	ag.ucCli = ucloud.NewClient(&uccfg, &credential)
 
+	ag.queryInfos = ag.genQueryInfo()
+
+	select {
+	case <-ag.ctx.Done():
+		return
+	default:
+	}
+
+	limit := rate.Every(50 * time.Millisecond)
+	rateLimiter := rate.NewLimiter(limit, 1)
+
+	for {
+
+		select {
+		case <-ag.ctx.Done():
+			return
+		default:
+		}
+
+		t := time.Now()
+		for _, req := range ag.queryInfos {
+
+			select {
+			case <-ag.ctx.Done():
+				return
+			default:
+			}
+
+			rateLimiter.Wait(ag.ctx)
+			if err := ag.fetchMetric(ag.ctx, req); err != nil {
+				moduleLogger.Errorf(`fail to get metric "%s.%s", %s`, req.resourceID, req.metricname, err)
+			}
+		}
+		useage := time.Now().Sub(t)
+		if useage < 5*time.Minute {
+			datakit.SleepContext(ag.ctx, 5*time.Minute-useage)
+		}
+	}
+}
+
+func (ag *ucInstance) fetchMetric(ctx context.Context, info *queryListInfo) error {
+
+	now := time.Now().Truncate(time.Minute)
+	if now.Sub(info.lastFetchTime) < info.intervalTime {
+		return nil
+	}
+
+	var begin, end int64
+	if info.lastFetchTime.IsZero() {
+		if info.intervalTime < time.Minute*5 {
+			begin = now.Add(-5 * time.Minute).Unix()
+		} else {
+			begin = now.Add(-info.intervalTime).Unix()
+		}
+	} else {
+		begin = info.lastFetchTime.Add(-time.Minute).Unix()
+	}
+	end = now.Unix()
+
+	moduleLogger.Debugf("GetMetric: resourceID=%s, metric=%s, range=%v - %v, interval=%s", info.resourceID, info.metricname, begin, end, info.intervalTime)
+
+	req := ag.ucCli.NewGenericRequest()
+	req.SetAction("GetMetric")
+	req.SetRegion(ag.Region)
+	req.SetZone(ag.Zone)
+	req.SetProjectId(ag.ProjectID)
+	reqPayload := map[string]interface{}{}
+	reqPayload["ResourceType"] = info.resourceType
+	reqPayload["ResourceId"] = info.resourceID
+	reqPayload["MetricName"] = info.metricname
+	reqPayload["BeginTime"] = begin
+	reqPayload["EndTime"] = end
+	req.SetPayload(reqPayload)
+
+	resp, err := ag.ucCli.GenericInvoke(req)
+	if err != nil {
+		return err
+	}
+
+	payload := resp.GetPayload()
+	if mapData, ok := payload["DataSets"].(map[string]interface{}); ok {
+		for _, v := range mapData {
+			if series, ok := v.([]interface{}); ok {
+				for _, metricItem := range series {
+					if metricItemMap, ok := metricItem.(map[string]interface{}); ok {
+
+						metricName := "ucmon_" + info.metricname
+						tags := map[string]string{
+							"Region":       ag.Region,
+							"Zone":         ag.Zone,
+							"ProjectID":    ag.ProjectID,
+							"ResourceID":   info.resourceID,
+							"ResourceType": info.resourceType,
+						}
+						fields := metricItemMap
+						metricTime := time.Unix(time.Now().Unix(), 0)
+						if tm, ok := fields["Timestamp"].(float64); ok {
+							metricTime = time.Unix(int64(tm), 0)
+							delete(fields, "Timestamp")
+						}
+
+						io.NamedFeedEx(inputName, io.Metric, metricName, tags, fields, metricTime)
+
+					}
+				}
+			}
+		}
+	}
+
+	info.lastFetchTime = now
+
+	return nil
 }
 
 func init() {
