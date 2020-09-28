555053354f09de4494c72798fd1468f829711c8c
tanbiao <tanbiao@jiagouyun.com>
2020-09-28 06:25:25 +0000

    update: 	[*] add ws client [need testing...]
    Former-commit-id: 967a798a903b686de6ed510c4d5bc8de6c07e423 [formerly 072c923624e6e69d27bc273365e6a574c9580f06]
    Former-commit-id: dc20676a0d9f53d14e4b9f0085838c0e80211e89

---
+++ vendor/gitlab.jiagouyun.com/cloudcare-tools/cliutils/network/ws/epoll.go
@@ -0,0 +1,107 @@
+// +build linux
+
+package ws
+
+// See https://github.com/eranyanay/1m-go-websockets/blob/master/4_optimize_gobwas/epoll.go
+
+import (
+	"net"
+	"reflect"
+	"sync"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+type epoll struct {
+	fd          int
+	connections map[int]net.Conn
+	lock        *sync.RWMutex
+}
+
+func MkEpoll() (*epoll, error) {
+	fd, err := unix.EpollCreate1(0)
+	if err != nil {
+		return nil, err
+	}
+	return &epoll{
+		fd:          fd,
+		lock:        &sync.RWMutex{},
+		connections: make(map[int]net.Conn),
+	}, nil
+}
+
+func (e *epoll) Add(conn net.Conn) error {
+	// Extract file descriptor associated with the connection
+	fd := websocketFD(conn)
+	err := unix.EpollCtl(e.fd, syscall.EPOLL_CTL_ADD, fd, &unix.EpollEvent{Events: unix.POLLIN | unix.POLLHUP, Fd: int32(fd)})
+	if err != nil {
+		return err
+	}
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	e.connections[fd] = conn
+	return nil
+}
+
+func (e *epoll) Remove(conn net.Conn) error {
+	fd := websocketFD(conn)
+	err := unix.EpollCtl(e.fd, syscall.EPOLL_CTL_DEL, fd, nil)
+	if err != nil {
+		return err
+	}
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	delete(e.connections, fd)
+	if len(e.connections)%100 == 0 {
+		l.Debugf("Total number of connections: %v", len(e.connections))
+	}
+	return nil
+}
+
+func (e *epoll) Wait() ([]net.Conn, error) {
+	events := make([]unix.EpollEvent, 100)
+	n, err := unix.EpollWait(e.fd, events, 100)
+	if err != nil {
+		return nil, err
+	}
+	e.lock.RLock()
+	defer e.lock.RUnlock()
+	var connections []net.Conn
+	for i := 0; i < n; i++ {
+		conn := e.connections[int(events[i].Fd)]
+		connections = append(connections, conn)
+	}
+	return connections, nil
+}
+
+func websocketFD(conn net.Conn) int {
+	tcpConn := reflect.Indirect(reflect.ValueOf(conn)).FieldByName("conn")
+	fdVal := tcpConn.FieldByName("fd")
+	pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
+
+	return int(pfdVal.FieldByName("Sysfd").Int())
+}
+
+func (e *epoll) Close() error {
+	for _, c := range e.connections {
+
+		l.Debugf("remove cli %s", c.RemoteAddr().String())
+
+		if err := c.Close(); err != nil {
+			l.Errorf("c.Close(): %s", err.Error())
+			return err
+		}
+
+		//if err := e.Remove(c); err != nil {
+		//	l.Errorf("e.Remove(): %s", err.Error())
+		//	return err
+		//}
+	}
+
+	if err := unix.Close(e.fd); err != nil {
+		l.Errorf("unix.Close(): %s", err.Error())
+	}
+
+	return nil
+}
