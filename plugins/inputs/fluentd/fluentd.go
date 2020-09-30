343ed337b50bc374864491020295987419e40e87
liushaobo <liushaobo@jiagouyun.com>
2020-09-30 15:20:47 +0800

    fluentd插件
     [formerly bb85107f68fa3de2cc20bc31ef15859e0a8cf038 [formerly a8c3c71b355360184a15c95b5b88934c670ed519] [formerly bb85107f68fa3de2cc20bc31ef15859e0a8cf038 [formerly a8c3c71b355360184a15c95b5b88934c670ed519] [formerly f7297783cccf2d7cf1f3e06a4d6519b6031882ca [formerly ded54f7cb810f0aa1fd8ee60e383df80447287c0]]]]
    
    
    
    Former-commit-id: c967692e8e483d389f8ec40f10422c9b3a193246
    Former-commit-id: c0b8e38f766e259fa999452039da129c3c1b5858 [formerly 8fe0ebde219b3b72a18178f82f2bb56c3bc61c6c]
    Former-commit-id: 6a8094d8405968a894dfb2847f671c4cdf495122

---
+++ plugins/inputs/fluentd/fluentd.go
@@ -0,0 +1,116 @@
+package fluentd
+
+import (
+	"bufio"
+	iowrite "io"
+	"net/http"
+	"time"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	httpd "gitlab.jiagouyun.com/cloudcare-tools/datakit/http"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+const (
+	inputName = "fluentd"
+
+	defaultMeasurement = "fluentd"
+
+	sampleCfg = `
+[[inputs.fluentd]]
+    # http server route path
+    # input url(required)
+	path = "/fluentd"
+	# log name(required) 
+	metric = ""
+    # [inputs.fluentd.tags]
+    # tags1 = "value1"
+`
+)
+
+var l = logger.DefaultSLogger(inputName)
+
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		return &Fluentd{}
+	})
+}
+
+type Fluentd struct {
+	Path   string            `toml:"path"`
+	Metric string            `toml:"metric"`
+	Tags   map[string]string `toml:"tags"`
+}
+
+func (*Fluentd) SampleConfig() string {
+	return sampleCfg
+}
+
+func (*Fluentd) Catalog() string {
+	return inputName
+}
+
+func (*Fluentd) Run() {
+	l = logger.SLogger(inputName)
+	l.Infof("Fluentd input started...")
+}
+
+func (f *Fluentd) RegHttpHandler() {
+	httpd.RegHttpHandler("POST", f.Path, f.Handle)
+}
+
+func (f *Fluentd) Handle(w http.ResponseWriter, r *http.Request) {
+	// body, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	l.Errorf("failed to read body, err: %s", err.Error())
+	// 	w.WriteHeader(http.StatusBadRequest)
+	// 	return
+	// }
+	// defer r.Body.Close()
+
+	if err := extract(r.Body, f.Metric, f.Tags); err == nil {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func extract(body iowrite.Reader, metric string, tags map[string]string) error {
+	r := bufio.NewReader(body)
+
+	for {
+		bytes, err := r.ReadBytes(byte('\n'))
+		if err == iowrite.EOF || string(bytes) == "" {
+			break
+		}
+
+		_tags := make(map[string]string)
+		_fields := make(map[string]interface{})
+
+		for key, val := range tags {
+			_tags[key] = val
+		}
+
+		tm := time.Now()
+
+		n := len(bytes)
+		_fields["__content"] = string(bytes[0 : n-1])
+
+		pt, err := io.MakeMetric(metric, _tags, _fields, tm)
+		if err != nil {
+			l.Errorf("make metric point error %v", err)
+			return err
+		}
+
+		l.Debug("point data", string(pt))
+
+		err = io.NamedFeed([]byte(pt), io.Logging, "")
+		if err != nil {
+			l.Errorf("push metric point error %v", err)
+			return err
+		}
+	}
+
+	return nil
+}
