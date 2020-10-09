050e9f617cb3a9d21643df9d9362a27bf77ecc31
liushaobo <liushaobo@jiagouyun.com>
2020-10-09 16:56:16 +0800

    删除ak
    Former-commit-id: 49adfa3ea8a410b3616ad4c7b835cc126f31035e [formerly 2a5cbf90dc6e1890cf8ad1e535d932e4d047c8bd]
    Former-commit-id: b2c8a12406fafa46241513873d5551c5a971928b

--- plugins/inputs/aliyunddos/ddos_test.go
+++ plugins/inputs/aliyunddos/ddos_test.go
@@ -7,8 +7,8 @@ import (
 func TestHandle(t *testing.T) {
 	t.Run("case-gatherGlobalStatuses", func(t *testing.T) {
 		d := DDoS{}
-		d.AccessKeyID = "LTAIqo2UBnC4q78J"
-		d.AccessKeySecret = "t43b4XdKq9Bv50pzSy1yIYiIlwTtvd"
+		d.AccessKeyID = ""
+		d.AccessKeySecret = ""
 		d.Interval = "5s"
 
 		d.Run()
