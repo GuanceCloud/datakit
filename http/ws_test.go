f90eb0b6f78196fde8d11bd372c6cc7e48825052
huangjinjin <huangjinjin@jiagouyun.com>
2020-10-22 14:20:08 +0800

    dk-ws test config
     [formerly 5d694cb335bd313b5789e8e8dc2a8b87d16fe347 [formerly 11d59a63d8ee0dd5c29cb13d4c3ece890fafad87] [formerly 813ce5a5fdca53e7cf071a2970723a9a02d83f3d [formerly b09baa76b8e4bbd5feff3d2f3590c83f12852b1f]] [formerly 27408934e953913d962305ac3172c7c8985e680c] [formerly 4db1782cbff84a8f0f9ae6fd5af960fe2dee5375 [formerly 952d29cedacf42c70950e3bcd7959e6db42517a4] [formerly 4db1782cbff84a8f0f9ae6fd5af960fe2dee5375 [formerly 952d29cedacf42c70950e3bcd7959e6db42517a4] [formerly d186cb0c12354a959e171a53777ad76364fead92 [formerly b51e7b80c5a1d778802b000c112fa4a02dcfcc4a]]]] [formerly df972eff396a34d4416b674ef1d611f7637b3575] [formerly bc0dc43b7ad97956d79c2aeae9cefa9d6ce79f8a [formerly a5e75fb3b4a604eccea46ff024dcce909bc25d03] [formerly 9a14e54c617d55984507dec2a85a814341246a00 [formerly 74ebb1b61370ea7450e6521fa6d3229de03cd3ff] [formerly 9a14e54c617d55984507dec2a85a814341246a00 [formerly 74ebb1b61370ea7450e6521fa6d3229de03cd3ff] [formerly 9a14e54c617d55984507dec2a85a814341246a00 [formerly 74ebb1b61370ea7450e6521fa6d3229de03cd3ff] [formerly e9a9d5a835aadf0b59dbc4a2555e340f2f4acbb5 [formerly 2314c1dc23eb2b54e5f8959d51859c8e8f372793]]]]]]]
     [formerly df972eff396a34d4416b674ef1d611f7637b3575]
    
    
    
    
    Former-commit-id: 2edbd8869c1eb8a48ec4523e7ed7e4555e1f6284 [formerly 5362a8d2a7662dffa727c7e8cbed2cbdb880833f]
    Former-commit-id: 53694533eb6437617fbd64ebbe1a73ff9c48ec7e [formerly 2c14ab4cf3dbbf09bbf60f2d1ef3330d626bebd1] [formerly a46c8ae36171097ef064a68c9da543f15ab7e5e1 [formerly 33bdc7f140e35262ba4c33452c6b988923523813]]
    Former-commit-id: 70ac8050629792f136fa6456e684721785f47024 [formerly b592935a44b5157d4387c5c6dd0f82b57a88f426]
    Former-commit-id: e8a8c9fc809274fd4eddfcd0ef749e4b6d905726

--- http/ws_test.go
+++ http/ws_test.go
@@ -16,31 +16,28 @@ type MsgGetInputConfig struct {
 }
 
 func TestB64(t *testing.T) {
-    //var names []string
+	//var names []string
 	var a MsgGetInputConfig
-	data ,_ := base64.StdEncoding.DecodeString("WyJhbnNpYmxlIiwiY3B1Il0=")
+	data, _ := base64.StdEncoding.DecodeString("WyJhbnNpYmxlIiwiY3B1Il0=")
 
 	//a := string(data)
 	//t.Fatal(string(data),data)
-	_ = json.Unmarshal(data,&a.Names)
+	_ = json.Unmarshal(data, &a.Names)
 
 }
 
 func TestRename(t *testing.T) {
 	path := "/Users/admin/Desktop/test/123.conf"
 	//path := "/usr/local/cloudcare/dataflux/datakit/conf.d"
-	a:= filepath.Dir(path)
-	b,_ := ioutil.ReadFile(path)
+	a := filepath.Dir(path)
+	b, _ := ioutil.ReadFile(path)
 	newName := fmt.Sprintf("%x.conf", md5.Sum(b))
-	newPath := filepath.Join(a,newName)
+	newPath := filepath.Join(a, newName)
 	fmt.Println(newPath)
-	err := os.Rename(path,newPath)
+	err := os.Rename(path, newPath)
 	fmt.Println(err)
 
 }
 
-func TestJson(t *testing.T) {
-	a := "example"
-	b,err := json.Marshal(a)
-	fmt.Println(b,err)
-}
+
+
