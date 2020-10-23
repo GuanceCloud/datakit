308a71648a6adf424065614e08a0b17c68d02be3
huangjinjin <huangjinjin@jiagouyun.com>
2020-10-23 14:18:40 +0800

    dk-ws 更新kodo msg
    Former-commit-id: 182e15eb1fdb4c93934e0334f18faaf7cfbd20f1 [formerly 1b1e309f9735608c128ff51447951f8eadbe9623 [formerly 517951f809f2c5f0df6dfff02096236c52185773] [formerly 898635694f7096ceebc546435c39c3fc498f1dc6 [formerly caa3975df009e9b7ee5afc642c31e568d0c6b6c8]]]
    Former-commit-id: 6c8e918300ec45ee3882b71a8d9e002a8ef6f752
    Former-commit-id: 64bb4e029eb61f1b408b9c3bb1a21812be3e0431 [formerly 721418f90f34aa9ab72c6ad04f69e53a3caed753]
    Former-commit-id: 86df8c0347c4d9d52458a2d6e57d53d0c2d13fd5

--- http/ws_test.go
+++ http/ws_test.go
@@ -3,7 +3,6 @@ package http
 import (
 	"crypto/md5"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -17,12 +16,12 @@ type MsgGetInputConfig struct {
 
 func TestB64(t *testing.T) {
 	//var names []string
-	var a MsgGetInputConfig
-	data, _ := base64.StdEncoding.DecodeString("WyJhbnNpYmxlIiwiY3B1Il0=")
-
+	//var a MsgGetInputConfig
+	data, _ := base64.StdEncoding.DecodeString("W1tpbnB1dHMuY3B1XV0KCiAgIyMgV2hldGhlciB0byByZXBvcnQgcGVyLWNwdSBzdGF0cyBvciBub3QKICBwZXJjcHUgPSB0cnVlCiAgIyMgV2hldGhlciB0byByZXBvcnQgdG90YWwgc3lzdGVtIGNwdSBzdGF0cyBvciBub3QKICB0b3RhbGNwdSA9IHRydWUKICAjIyBJZiB0cnVlLCBjb2xsZWN0IHJhdyBDUFUgdGltZSBtZXRyaWNzLgogIGNvbGxlY3RfY3B1X3RpbWUgPSBmYWxzZQogICMjIElmIHRydWUsIGNvbXB1dGUgYW5kIHJlcG9ydCB0aGUgc3VtIG9mIGFsbCBub24taWRsZSBDUFUgc3RhdGVzLgogIHJlcG9ydF9hY3RpdmUgPSBmYWxzZQ==")
+	fmt.Println(string(data))
 	//a := string(data)
 	//t.Fatal(string(data),data)
-	_ = json.Unmarshal(data, &a.Names)
+	//_ = json.Unmarshal(data, &a.Names)
 
 }
 
