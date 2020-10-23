68cf418db9bffe0e258bda3f582eb95f3d6176f8
liguozhuang <liguozhuang@jiagouyun.com>
2020-10-23 13:50:43 +0800

    add space/return/tab of tailf
     [formerly d4632fb467d49b9ee07f029c426d54a6dd5185e4 [formerly c33cff810b64539a97256a0ca38ac12d43f7f2d9] [formerly 08e5434e46db00a7d5177a957d2eb33449378abd [formerly 1b4cb6884b39acb192c2911ec86c8ee9b109e8e7]] [formerly e05e7447a25b1d9158745e8c155d71801aa8ef84] [formerly f0aa54e8a06edfb47fe4139ec42f734caee64245 [formerly cb11b6cf5273715273499f758ff5b0eb3abacbd1] [formerly f0aa54e8a06edfb47fe4139ec42f734caee64245 [formerly cb11b6cf5273715273499f758ff5b0eb3abacbd1] [formerly 8a647a33e0ca2b33a25f58648feac08ffd08edad [formerly 8b71c64d05ae2723a5fb6cce8c5cd6feef339483]]]] [formerly 9813de08d1284071eb58d919e20a0700b3d543b8] [formerly be41abc107eaf08ab964a517921a2b4b1eb9c833 [formerly 9a7e728afaa54ffd18dcfc81fa65c420fef64a43] [formerly 4deac2efa2e1359371e4badbc68800f50b92b8ba [formerly f83850aa3d679be838b6f4d913f57b22126dabd2] [formerly 4deac2efa2e1359371e4badbc68800f50b92b8ba [formerly f83850aa3d679be838b6f4d913f57b22126dabd2] [formerly 4deac2efa2e1359371e4badbc68800f50b92b8ba [formerly f83850aa3d679be838b6f4d913f57b22126dabd2] [formerly 2c2d2f28837a292dc2831e9096cf87a68664649a [formerly 25eac0dbb5be8bf2e5b0994a1522c7b7e504ab20]]]]]]]
     [formerly 9813de08d1284071eb58d919e20a0700b3d543b8]
    
    
    
    
    Former-commit-id: acea0a8e17204bfe5c8f4beb434d9c0bfc7eb287 [formerly 0247a2a17abdca73585b19fcf5a3281547006a77]
    Former-commit-id: d35bb72d50e47d5c5c54ec73b3ac4d42e39e3368 [formerly 1246ab9a1026261977da6a331c2cbf151e0a4645] [formerly 878de0bb443ad741b9f7afc196aad7004dd042ba [formerly 0083920256f7826a44a2eb1c7b5938ad906134b3]]
    Former-commit-id: 410610f304cefa8dc8444d436f835e9b9dfe05be [formerly 392f3ff80600647d3278af9d95286c8a76aea336]
    Former-commit-id: fa935bc72d2354bf9c30b1bd902be6d96b825f9a

--- plugins/inputs/tailf/tailf_test.go
+++ plugins/inputs/tailf/tailf_test.go
@@ -4,7 +4,8 @@ package tailf
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
+	//"os"
 	"testing"
 	"time"
 
@@ -12,85 +13,48 @@ import (
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
-var (
-	logFiles = []string{
-		"/tmp/tailf_test/**/*.*",
-		// "/tmp/tailf_test/a123/ab.log",
-		// "/tmp/tailf_test/a123/*.txt",
-		// "/tmp/tailf_test/*.no",
+func writeFile() (filepath string) {
+	file, err := ioutil.TempFile("", "example")
+	if err != nil {
+		panic(err)
 	}
 
-	ignore = []string{
-		//"/tmp/tailf_test/a123/ab.log",
-	}
-
-	deepDir = "/tmp/tailf_test/a123/b123/"
-
-	paths = []string{
-		//"/tmp/tailf_test/a123/b123/1234.log",
-		// "/tmp/tailf_test/a123/b123/5678.txt",
-		//"/tmp/tailf_test/a123/ab.log",
-		"/tmp/tailf_test/a123/cd.txt",
-	}
-)
-
-func TestWrite(t *testing.T) {
-	if err := os.MkdirAll(deepDir, os.ModePerm); err != nil {
-		t.Fatal(err)
-	}
-
-	var files []*os.File
-	for _, path := range paths {
-		f, err := os.Create(path)
-		if err != nil {
-			t.Fatal(err)
-		}
-		files = append(files, f)
-	}
-	defer func() {
-		for _, f := range files {
-			fmt.Printf("fd %s close", f.Name())
-			f.Close()
+	count := 0
+	go func() {
+		for {
+			select {
+			case <-datakit.Exit.Wait():
+				// os.Remove(file.Name())
+				return
+			default:
+				str := fmt.Sprintf("%s -- %d\n%s\n%s\n%s\n",
+					time.Now().Format(time.RFC3339Nano), count,
+					"   SPACE", "\rRETURN", "\tTAB")
+				file.WriteString(str)
+				time.Sleep(200 * time.Millisecond)
+				count++
+			}
 		}
 	}()
 
-	count := 0
-	for {
-		select {
-		case <-datakit.Exit.Wait():
-			return
-		default:
-			for index, file := range files {
-				file.WriteString(time.Now().Format(time.RFC3339Nano) +
-					fmt.Sprintf(" -- index: %d -- count: %d\n", index, count))
-				time.Sleep(200 * time.Millisecond)
-			}
-			count++
-		}
-	}
+	return file.Name()
 }
 
 func TestMain(t *testing.T) {
 	io.TestOutput()
-	// go TestWrite(t)
-	// time.Sleep(time.Second)
+
+	w1 := writeFile()
+	time.Sleep(time.Second)
 
 	var tailer = Tailf{
-		LogFiles:      logFiles,
-		Ignore:        ignore,
+		LogFiles:      []string{w1},
 		FromBeginning: true,
-		Source:        "NAXXRAMAS",
-		Tags:          map[string]string{"TestKey": "TestValue"},
+		Source:        "Testing",
 	}
 
-	go func() {
-		time.Sleep(time.Second * 20)
-		datakit.Exit.Close()
-	}()
+	go tailer.Run()
 
-	tailer.Run()
-}
-
-func TestFileList(t *testing.T) {
-	t.Log(getFileList(logFiles, ignore))
+	time.Sleep(time.Second * 15)
+	datakit.Exit.Close()
+	time.Sleep(time.Second)
 }
