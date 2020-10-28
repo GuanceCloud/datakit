7a09f6aa07d246a9875baa2b256219ce57b87bfc
liguozhuang <liguozhuang@jiagouyun.com>
2020-10-28 11:41:21 +0800

    add encoding of tailf
    Former-commit-id: 62efde879ec16ce2d278de0e59e410ab4046044e [formerly 5eb4525f0f1e6beaacbf969cafe57546a2c64bbd [formerly 92899f86aec582c73765e3e4371c7627f4b1c1cc] [formerly d6042c6f36177e68c117cc0bdd04da3de0ead139 [formerly c8159ebd873916932f3bd8befc06fcc9d40b5710]]]
    Former-commit-id: 0b8f7b6d434411e6b1962027940c9dd30caa8c4b
    Former-commit-id: 764cd69157983aa70160571a7397d5a73485f5e6 [formerly 6d76b5aa65f17bcb4a868ffb7a853a0b8a10fa1e]
    Former-commit-id: b410c0e4e6bd56fc1a0f83bb50f2f435b1c7cdf3

---
+++ plugins/inputs/tailf/encoding.go
@@ -0,0 +1,24 @@
+package tailf
+
+import (
+	"errors"
+
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+)
+
+type decoder = *encoding.Decoder
+
+func NewDecoder(enc string) (decoder, error) {
+	switch enc {
+	case "utf-8":
+		return unicode.UTF8.NewDecoder(), nil
+	case "utf-16le":
+		return unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewDecoder(), nil
+	case "utf-16be":
+		return unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewDecoder(), nil
+	case "none", "":
+		return encoding.Nop.NewDecoder(), nil
+	}
+	return nil, errors.New("unknown character encoding")
+}
