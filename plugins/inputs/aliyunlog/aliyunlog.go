cfb06f0e8437d6eb2fdf7035f9ca4835e5bb43d2
谭彪 <tanbiao@jiagouyun.com>
2020-11-02 09:10:41 +0000

    Merge branch 'dev-aliyunlog' into 'dev'
    aliyunlog减少日志打印
    
    See merge request cloudcare-tools/datakit!59
    
    (cherry picked from commit 57d4a2173a9be8a517489f1eacc00d216f5b4219 [formerly 2214779a87e5c09c67a5a258d39c06f5811a1a6f] [formerly 57d4a2173a9be8a517489f1eacc00d216f5b4219 [formerly 2214779a87e5c09c67a5a258d39c06f5811a1a6f] [formerly 4fde33e3173db63231f0d234c89417fba5e25e7e [formerly 555024d73ef48e356746a2fd601c345f47abfba6]]])
    
    02914feb aliyunlog减少日志打印
    
    
    Former-commit-id: c209ccf889f6943f3b4542af666f534d64083ef7
    Former-commit-id: 125fe1cf9b4e3f77807caf4957c72d9d8635ea90 [formerly a2f595aff360b538665d677f8eb84493c689837d]
    Former-commit-id: e9270aa580fa5f5f7fdf94da05e0b0eedcd3feac

--- plugins/inputs/aliyunlog/aliyunlog.go
+++ plugins/inputs/aliyunlog/aliyunlog.go
@@ -294,7 +294,9 @@ func (r *runningStore) logProcess(shardId int, logGroupList *sls.LogGroupList) s
 							if fval, err := strconv.ParseFloat(strval, 64); err == nil {
 								nval = int64(math.Floor(fval))
 							} else {
-								moduleLogger.Warnf("you specify '%s' as int, but fail to convert '%s' to int", k, strval)
+								if strval != "-" {
+									moduleLogger.Warnf("you specify '%s' as int, but fail to convert '%s' to int", k, strval)
+								}
 							}
 						} else {
 							fields[k] = nval
@@ -302,7 +304,9 @@ func (r *runningStore) logProcess(shardId int, logGroupList *sls.LogGroupList) s
 					case "float":
 						fval, err := strconv.ParseFloat(strval, 64)
 						if err != nil {
-							moduleLogger.Warnf("you specify '%s' as float, but fail to convert '%s' to float", k, strval)
+							if strval != "-" {
+								moduleLogger.Warnf("you specify '%s' as float, but fail to convert '%s' to float", k, strval)
+							}
 						} else {
 							fields[k] = fval
 						}
@@ -328,7 +332,7 @@ func (r *runningStore) logProcess(shardId int, logGroupList *sls.LogGroupList) s
 			if err == nil {
 				fields["__content"] = string(contentStr)
 			} else {
-				moduleLogger.Warnf("fail to marshal content, %s", err)
+				moduleLogger.Debugf("fail to marshal content, %s", err)
 			}
 
 			tm := time.Unix(int64(l.GetTime()), 0)
