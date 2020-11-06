41ea0c4a21230a4ca0fd4ddf432c31352d067160
wuqinchao <wuqinchao@jiagouyun.com>
2020-11-06 14:14:20 +0800

    阿里云日志优化
    Former-commit-id: ab443fc74f4bca62565fc83b9e3ee2449f917fb7 [formerly 303ed81cff5cae4a34fe9e1d859c2414adcd3fd0 [formerly e1f0e27f1273a84f54322599a028fa77c646a652] [formerly 99983c3b4a9297b1a2139b758d2e788816d745a9 [formerly b499ec2a10a717c89d44e5a7fd341861f2041c6b]]]
    Former-commit-id: 184b4cd897d1207813d7b3a66fbec7649ca484a0
    Former-commit-id: 35a4b70b5e7566249376f96f17f97bc5e6f44142 [formerly 38c66037613d7282f70b1be123b8d0c44f4245ca]
    Former-commit-id: 9062924d71ed9bc216857555eb488229f0d8c3ae

--- plugins/inputs/aliyunlog/aliyunlog.go
+++ plugins/inputs/aliyunlog/aliyunlog.go
@@ -87,7 +87,7 @@ func (r *runningProject) run() {
 		r.wg.Add(1)
 
 		if c.ConsumerGroupName == "" {
-			c.ConsumerGroupName = "datakit"
+			c.ConsumerGroupName = "datakit-" + datakit.Cfg.MainCfg.UUID
 		}
 
 		if c.ConsumerName == "" {
@@ -121,8 +121,7 @@ type adapterLogWriter struct {
 }
 
 func (al *adapterLogWriter) Write(p []byte) (n int, err error) {
-	fmt.Printf("****\n")
-	moduleLogger.Errorf("%s", string(p))
+	moduleLogger.Debugf("%s", string(p))
 	return len(p), nil
 }
 
@@ -188,13 +187,10 @@ func (r *runningStore) run() error {
 		Logstore:          r.cfg.Name,
 		ConsumerGroupName: r.cfg.ConsumerGroupName,
 		ConsumerName:      r.cfg.ConsumerName,
-		// This options is used for initialization, will be ignored once consumer group is created and each shard has been started to be consumed.
-		// Could be "begin", "end", "specific time format in time stamp", it's log receiving time.
-		CursorPosition: consumerLibrary.BEGIN_CURSOR,
+		CursorPosition:    consumerLibrary.END_CURSOR,
 	}
 
 	consumerWorker := consumerLibrary.InitConsumerWorker(option, sdkLogger(), r.logProcess)
-	//consumerWorker.Logger = sdkLogger()
 	consumerWorker.Start()
 
 	<-datakit.Exit.Wait()
@@ -337,10 +333,12 @@ func (r *runningStore) logProcess(shardId int, logGroupList *sls.LogGroupList) s
 
 			tm := time.Unix(int64(l.GetTime()), 0)
 
-			//mdata, _ := io.MakeMetric(r.metricName, tags, fields, tm)
-			//fmt.Printf("#### %s\n", string(mdata))
-
-			io.NamedFeedEx(inputName, io.Logging, r.metricName, tags, fields, tm)
+			if r.proj.inst.mode == "debug" {
+				mdata, _ := io.MakeMetric(r.metricName, tags, fields, tm)
+				fmt.Printf("%s\n", string(mdata))
+			} else {
+				io.NamedFeedEx(inputName, io.Logging, r.metricName, tags, fields, tm)
+			}
 
 		}
 	}
