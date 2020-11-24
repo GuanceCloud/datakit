8a5a0e7977225820857b64c1f9089fdab606999d
huangjinjin <huangjinjin@jiagouyun.com>
2020-11-24 16:38:51 +0800

    fix input
    Former-commit-id: 0ca561b3a1b81e362c80804c48885dd0d2e3f435 [formerly a29ff5e15a84e6af557d31c667d686602efd4695 [formerly db710058df0d11883e3eb92aa9656e12f0d1f5a0] [formerly ac2719423417215819f693de418d815c7b775fbb [formerly e900e96aebed8bc0cb5eefda1b72a0fb91c6b522]]]
    Former-commit-id: 10e25e08021921fd435416edd05dd311ade2d84f
    Former-commit-id: b2e0e690794c44de685f79a62ac18c1ef1625439 [formerly 37cdd5ce01359033535db93792ec44860b7e2fd9]
    Former-commit-id: e5616896d47bef55ba2537a4befe1626aa25feeb

--- plugins/inputs/telegraf_inputs/tginputs_win.go
+++ plugins/inputs/telegraf_inputs/tginputs_win.go
@@ -9,13 +9,13 @@ import (
 
 var (
 	telegrafInputsWin = map[string]*TelegrafInput{
-		"win_services":      {name: "win_services", Catalog: "windows", input: &win_services.WinServices{}},
-		"win_perf_counters": {name: "win_perf_counters", Catalog: "windows", input: &win_perf_counters.Win_PerfCounters{}},
-		`dotnetclr`:         {name: "dotnetclr", Catalog: "windows", Sample: samples["dotnetclr"], input: &win_perf_counters.Win_PerfCounters{}},
-		`aspdotnet`:         {name: "aspdotnet", Catalog: "windows", Sample: samples["aspdotnet"], input: &win_perf_counters.Win_PerfCounters{}},
-		`msexchange`:        {name: "msexchange", Catalog: "windows", Sample: samples["msexchange"], input: &win_perf_counters.Win_PerfCounters{}},
-		`iis`:               {name: "iis", Catalog: "windows", Sample: samples["iis"], input: &win_perf_counters.Win_PerfCounters{}},
-		`active_directory`:  {name: "active_directory", Catalog: "windows", Sample: samples["active_directory"], input: &win_perf_counters.Win_PerfCounters{}},
+		"win_services":      {name: "win_services", Catalog: "windows", Input: &win_services.WinServices{}},
+		"win_perf_counters": {name: "win_perf_counters", Catalog: "windows", Input: &win_perf_counters.Win_PerfCounters{}},
+		`dotnetclr`:         {name: "dotnetclr", Catalog: "windows", Sample: samples["dotnetclr"], Input: &win_perf_counters.Win_PerfCounters{}},
+		`aspdotnet`:         {name: "aspdotnet", Catalog: "windows", Sample: samples["aspdotnet"], Input: &win_perf_counters.Win_PerfCounters{}},
+		`msexchange`:        {name: "msexchange", Catalog: "windows", Sample: samples["msexchange"], Input: &win_perf_counters.Win_PerfCounters{}},
+		`iis`:               {name: "iis", Catalog: "windows", Sample: samples["iis"], Input: &win_perf_counters.Win_PerfCounters{}},
+		`active_directory`:  {name: "active_directory", Catalog: "windows", Sample: samples["active_directory"], Input: &win_perf_counters.Win_PerfCounters{}},
 	}
 )
 
