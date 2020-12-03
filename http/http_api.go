490c7595a0b3bdc832720275cebd6ae556e867dc
wuqinchao <wuqinchao@jiagouyun.com>
2020-12-03 17:38:41 +0800

    添加rum指标路由
    Former-commit-id: 9c1de689f2737cc2735509a79edc8033423e0d80
    Former-commit-id: 1a9bc116a7372d19e7d6af2b9a20fc3d6405d71f [formerly 7b9f9b2c690af6943848c9532883758b328af526]
    Former-commit-id: 9c763b17225e2474bd275fd3b2773bee37016341

--- http/http_api.go
+++ http/http_api.go
@@ -7,9 +7,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	influxm "github.com/influxdata/influxdb1-client/models"
+	influxdb "github.com/influxdata/influxdb1-client/v2"
 
 	uhttp "gitlab.jiagouyun.com/cloudcare-tools/cliutils/network/http"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/process"
 	"gitlab.jiagouyun.com/cloudcare-tools/ftagent/utils"
 )
 
@@ -86,4 +88,54 @@ func apiWriteObject(c *gin.Context) {
 		l.Errorf("NamedFeed: %s", err.Error())
 		uhttp.HttpErr(c, err)
 	}
+}
+
+func apiWriteRumMetrics(c *gin.Context) {
+	var precision string = DEFAULT_PRECISION
+	var body []byte
+	var err error
+
+	contentEncoding := c.Request.Header.Get("Content-Encoding")
+	name := c.Query(NAME)
+	precision = c.Query(PRECISION)
+
+	body, err = ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		uhttp.HttpErr(c, err)
+		return
+	}
+	defer c.Request.Body.Close()
+
+	if contentEncoding == "gzip" {
+		body, err = utils.ReadCompressed(bytes.NewReader(body), true)
+		if err != nil {
+			uhttp.HttpErr(c, err)
+			return
+		}
+	}
+
+	pts, err := influxm.ParsePointsWithPrecision(body, time.Now().UTC(), precision)
+	if err != nil {
+		l.Errorf("ParsePoints: %s", err.Error())
+		uhttp.HttpErr(c, err)
+		return
+	}
+
+	l.Debugf("received %d points from %s", len(pts), name)
+
+	convertdata := [][]byte{}
+
+	for _, pt := range pts {
+		convertdata = append(convertdata, process.NewProcedure(influxdb.NewPointFrom(pt)).Geo(c.Request.RemoteAddr).GetByte())
+	}
+
+	body = bytes.Join(convertdata, []byte("\n"))
+
+	// TODO: add global tags
+
+	if err = io.NamedFeed(body, io.Metric, name); err != nil {
+		l.Errorf("NamedFeed: %s", err.Error())
+		uhttp.HttpErr(c, err)
+	}
+	return
 }
