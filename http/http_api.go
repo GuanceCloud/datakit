6220d6be7e50c5bc9e5b9dd79e6b98ed07f5ec0f
huangbo <huangbo@jiagouyun.com>
2020-12-08 16:03:38 +0800

    datakit增加logging接口
     [formerly 6add4263d5f549134287c7e66e77ec91a5941c7b] [formerly 94ec93615a6d30de871e29565332ee27bf712b46 [formerly 11d1c1d6bc026a1595b4e829597d03ce1efb63ae] [formerly 9fb3ba6e63b0fa1b298755768665e2df687fbc3c [formerly 1a8df5dd5fe7488cb4a8ff09d88b2985f3feb715] [formerly 9fb3ba6e63b0fa1b298755768665e2df687fbc3c [formerly 1a8df5dd5fe7488cb4a8ff09d88b2985f3feb715] [formerly 9fb3ba6e63b0fa1b298755768665e2df687fbc3c [formerly 1a8df5dd5fe7488cb4a8ff09d88b2985f3feb715] [formerly 6822a2606bebc81c1da39513ef9c890341068124 [formerly 3c6ecb874f8869cf60c01eeee36e405d2a2ba396]]]]]]
     [formerly 02d9d37cdec537061afdd1d97a89fe77730cf0ff [formerly cbd1b1e78ab6bbcce0f5a77b67ab325a97d05d8d] [formerly 02d9d37cdec537061afdd1d97a89fe77730cf0ff [formerly cbd1b1e78ab6bbcce0f5a77b67ab325a97d05d8d] [formerly 02d9d37cdec537061afdd1d97a89fe77730cf0ff [formerly cbd1b1e78ab6bbcce0f5a77b67ab325a97d05d8d] [formerly c985e8f66e7cc66b9c4bc4e2db2206c3d6a67f88 [formerly 71ba524687bd58b90a87b5bb32a83bda906c6770]]]]]
    
    
    
    Former-commit-id: 55714fd267e8be18b8fcef0464638682804514cf [formerly edfd04e2107216b7358e03d05ade0ad4bac1abc7]
    Former-commit-id: 7a0ab35ff1a60f00d28b1dbb2bba133eb8b51b34 [formerly b285c63367086d629b2b5a96495e71f7b06e6451] [formerly c74f9a718ad43724b2fa5e08eeaef551b44c3fd6 [formerly f5de67438189d39376865146eebeac42731fc251]]
    Former-commit-id: 8135273566e45d914c2287f00f8d31a88babec83 [formerly 8f4de3b50f4f1c8e939db5843417578dc26de0c6]
    Former-commit-id: 9ecb97fea976fd24150e387f211000b077569733

--- http/http_api.go
+++ http/http_api.go
@@ -54,11 +54,53 @@ func apiWriteMetric(c *gin.Context) {
 		return
 	}
 
-	l.Debugf("received %d points from %s", len(pts), name)
+	l.Debugf("received metric %d points from %s", len(pts), name)
 
 	// TODO: add global tags
 
 	if err = io.NamedFeed(body, io.Metric, name); err != nil {
+		l.Errorf("NamedFeed: %s", err.Error())
+		uhttp.HttpErr(c, err)
+	}
+	return
+}
+
+func apiWriteLogging(c *gin.Context) {
+	var precision string = DEFAULT_PRECISION
+	var body []byte
+	var err error
+
+	contentEncoding := c.Request.Header.Get("Content-Encoding")
+	name := c.Query(NAME)
+	precision = c.Query(PRECISION)
+
+	body, err = ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		uhttp.HttpErr(c, err)
+		return
+	}
+	defer c.Request.Body.Close()
+
+	if contentEncoding == "gzip" {
+		body, err = utils.ReadCompressed(bytes.NewReader(body), true)
+		if err != nil {
+			uhttp.HttpErr(c, err)
+			return
+		}
+	}
+
+	pts, err := influxm.ParsePointsWithPrecision(body, time.Now().UTC(), precision)
+	if err != nil {
+		l.Errorf("ParsePoints: %s", err.Error())
+		uhttp.HttpErr(c, err)
+		return
+	}
+
+	l.Debugf("received logging %d points from %s", len(pts), name)
+
+	// TODO: add global tags
+
+	if err = io.NamedFeed(body, io.Logging, name); err != nil {
 		l.Errorf("NamedFeed: %s", err.Error())
 		uhttp.HttpErr(c, err)
 	}
