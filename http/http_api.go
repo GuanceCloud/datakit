3487f90d3fed4cf3804ceca948a587351001cda1
huangbo <huangbo@jiagouyun.com>
2020-12-16 18:18:39 +0800

    增加tracing接口
     [formerly bfc26056051fc1338fcd2bd2800c9a4919b9aa43 [formerly b3c49102d741b5ee62bda8caa36b86575527375f] [formerly bfc26056051fc1338fcd2bd2800c9a4919b9aa43 [formerly b3c49102d741b5ee62bda8caa36b86575527375f] [formerly bfc26056051fc1338fcd2bd2800c9a4919b9aa43 [formerly b3c49102d741b5ee62bda8caa36b86575527375f] [formerly d8809188751d4ba72b2814aab30de049b8077c53 [formerly c783aaf732a0ab055a9c06336b0373247e129ca1]]]]]
    
    
    
    Former-commit-id: 471d81d2fa9c79ccc3e7d597ef6f6d6eeceb898b [formerly 70f705c1c3f6edfb60cfbb0bc80df43299628270]
    Former-commit-id: c77c325f675573e7c263d6adb46612e08d8ef2d2 [formerly 3b8a8a6b3c8f0cfca34c3c686957eceb94e66f4a] [formerly 8a5caf7e81909cf8444a2407837ba9bf2a101007 [formerly fb18fc236cee9abedbc32880d970bf74d98fb65c]]
    Former-commit-id: ae8e34907fb726b34ca4dbbe5e50d09657ca5fba [formerly c08a4834a95313df92178e72b304188bc6b9db14]
    Former-commit-id: 168a5c61301cc206bfa479c9f6dcdc6d6f56fd43

--- http/http_api.go
+++ http/http_api.go
@@ -105,6 +105,48 @@ func apiWriteLogging(c *gin.Context) {
 	return
 }
 
+func apiWriteTracing(c *gin.Context) {
+	var precision string = DEFAULT_PRECISION
+	var body []byte
+	var err error
+
+	contentEncoding := c.Request.Header.Get("Content-Encoding")
+	name := c.Query(NAME)
+	precision = c.Query(PRECISION)
+
+	body, err = ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		uhttp.HttpErr(c, err)
+		return
+	}
+	defer c.Request.Body.Close()
+
+	if contentEncoding == "gzip" {
+		body, err = utils.ReadCompressed(bytes.NewReader(body), true)
+		if err != nil {
+			uhttp.HttpErr(c, err)
+			return
+		}
+	}
+
+	pts, err := influxm.ParsePointsWithPrecision(body, time.Now().UTC(), precision)
+	if err != nil {
+		l.Errorf("ParsePoints: %s", err.Error())
+		uhttp.HttpErr(c, err)
+		return
+	}
+
+	l.Debugf("received tracing %d points from %s", len(pts), name)
+
+	// TODO: add global tags
+
+	if err = io.NamedFeed(body, io.Tracing, name); err != nil {
+		l.Errorf("NamedFeed: %s", err.Error())
+		uhttp.HttpErr(c, err)
+	}
+	return
+}
+
 func apiWriteObject(c *gin.Context) {
 	var body []byte
 	var err error
