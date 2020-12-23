70bcebc2d65c2db3f2a9d5cf4dcb402e3213a037
liushaobo <liushaobo@jiagouyun.com>
2020-12-23 10:28:33 +0800

    提交代码
    Former-commit-id: 7b7241daa2c75de74e9f9076df74e3d0480b1b3f [formerly ce5139afa4707387736172fe3014daf02c3aacf6 [formerly e59eb9650e529560324200795abd5988aa7f4085] [formerly cc5dacf4467a9d8c0fb216a693bb659196309402 [formerly 6c9781657da8f0f891a7fadb3697859354c2125a]]]
    Former-commit-id: 9a1372edc200b60d1f31b7eedc936650c3524565
    Former-commit-id: f3b6aa666b42f0191254cc8f99d9c41f7ce5a592 [formerly 2cf62b7f03212e05e7a5f615f08176daeffdd97c]
    Former-commit-id: 1cd49816e4442c1106abca1af2130beb44678c0e

--- vendor/github.com/gcla/termshark/v2/system/extcmds_windows.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2020 Graham Clark. All rights reserved.  Use of this source
-// code is governed by the MIT license that can be found in the LICENSE
-// file.
-
-package system
-
-var CopyToClipboard = []string{"clip"}
-
-var OpenURL = []string{"explorer"}
