475fd60dca7c4291370cdd8e147d4fc7195413d1
wuqinchao <wuqinchao@jiagouyun.com>
2020-12-23 16:54:22 +0800

    优化配置模板
    Former-commit-id: 22f99f872909bdadb289337d6cd38d1062ba6f79

--- configtemplate/configtemplate.go
+++ configtemplate/configtemplate.go
@@ -4,8 +4,6 @@ import (
 	"archive/tar"
 	"bytes"
 	"crypto/tls"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -14,17 +12,24 @@ import (
 	"path/filepath"
 	"strings"
 	"text/template"
+	"unicode"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/ftagent/utils"
 )
 
 const (
 	defaultURL = `https://static.dataflux.cn/datakit/conf/`
+	envFile    = `env.data`
+)
+
+var (
+	moduleLogger *logger.Logger
 )
 
 type CfgTemplate struct {
 	InstallDir string
-	data       map[string]interface{}
 }
 
 func NewCfgTemplate(instDir string) *CfgTemplate {
@@ -34,50 +39,15 @@ func NewCfgTemplate(instDir string) *CfgTemplate {
 	}
 }
 
-func (c *CfgTemplate) InstallConfigs(path string, data []byte) error {
+func (c *CfgTemplate) InstallConfigs(path string) error {
 
 	if path == "" {
 		return nil
 	}
 
+	moduleLogger = logger.SLogger("configtemplate")
+
 	var err error
-
-	if bytes.HasPrefix(data, []byte("file://")) {
-		data, err = ioutil.ReadFile(string(data[7:]))
-		if err != nil {
-			return err
-		}
-	} else if bytes.HasPrefix(data, []byte("http://")) || bytes.HasPrefix(data, []byte("https://")) {
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
-		cli := &http.Client{
-			Transport: tr,
-		}
-		resp, err := cli.Get(string(data))
-		if err != nil {
-			return err
-		}
-		defer resp.Body.Close()
-		if data, err = ioutil.ReadAll(resp.Body); err != nil {
-			return err
-		}
-
-	} else if bytes.HasPrefix(data, []byte("base64://")) {
-		bdata := data[9:]
-		if data, err = base64.StdEncoding.DecodeString(string(bdata)); err != nil {
-			return fmt.Errorf("decode base64 failed, %s", err)
-		}
-	}
-
-	c.data = nil
-	if len(data) > 0 {
-		jsonData := map[string]interface{}{}
-		if err := json.Unmarshal(data, &jsonData); err != nil {
-			return fmt.Errorf("invalid json data: %s, error:%s", string(data), err)
-		}
-		c.data = jsonData
-	}
 
 	var packageData []byte
 	if strings.HasPrefix(path, "file://") {
@@ -121,14 +91,45 @@ func (c *CfgTemplate) InstallConfigs(path string, data []byte) error {
 	return c.parsePackage(packageData)
 }
 
+func (c *CfgTemplate) parseEnvData(tardata []byte) (bindData map[string]string) {
+
+	buf := bytes.NewBuffer(tardata)
+	tr := tar.NewReader(buf)
+	for hdr, err := tr.Next(); err != io.EOF && hdr != nil; hdr, err = tr.Next() {
+
+		if err != nil {
+			moduleLogger.Errorf("invalid tar, %s", err)
+			return nil
+		}
+
+		fi := hdr.FileInfo()
+
+		if !fi.IsDir() && fi.Name() == envFile {
+			var b bytes.Buffer
+			_, err := io.Copy(&b, tr)
+			if err != nil {
+				moduleLogger.Errorf("fail to read values file")
+				return nil
+			}
+
+			bindData = parseKVPairs(b.String())
+			moduleLogger.Debugf("parsed key-value pairs: %v", bindData)
+			break
+		}
+	}
+	return
+}
+
 func (c *CfgTemplate) parsePackage(data []byte) error {
 	if data == nil {
 		return nil
 	}
 	tarData, err := utils.ReadCompressed(bytes.NewReader(data), true)
 	if err != nil {
-		return fmt.Errorf("fail to decompress, %s. Is it a gzip?", err)
+		return fmt.Errorf("fail to decompress, error: %s. Is it a tar.gz?", err)
 	}
+
+	bindData := c.parseEnvData(tarData)
 
 	buf := bytes.NewBuffer(tarData)
 	tr := tar.NewReader(buf)
@@ -139,16 +140,16 @@ func (c *CfgTemplate) parsePackage(data []byte) error {
 		}
 
 		fi := hdr.FileInfo()
-		if !fi.IsDir() {
-			targetDir := filepath.Join(c.InstallDir, "conf.d", filepath.Dir(hdr.Name))
+
+		if !fi.IsDir() && fi.Name() != envFile {
+
+			targetDir := filepath.Join(c.InstallDir, "conf.d", filepath.Base(filepath.Dir(hdr.Name)))
 			if err = os.MkdirAll(targetDir, 0775); err != nil {
 				return fmt.Errorf("fail to create dir '%s', error: %s", targetDir, err)
 			}
 
 			filename := fi.Name()
-			if strings.HasSuffix(filename, ".sample") {
-				filename = filename[:len(filename)-len(".sample")]
-			}
+			filename = strings.TrimSuffix(filename, ".sample")
 			targetFile := filepath.Join(targetDir, filename)
 
 			var tmplData bytes.Buffer
@@ -157,7 +158,7 @@ func (c *CfgTemplate) parsePackage(data []byte) error {
 				return fmt.Errorf("fail to read template file: %s", hdr.Name)
 			}
 
-			err = c.genConfig(targetFile, tmplData.Bytes())
+			err = c.genConfig(targetFile, tmplData.Bytes(), bindData)
 			if err != nil {
 				return err
 			}
@@ -167,17 +168,18 @@ func (c *CfgTemplate) parsePackage(data []byte) error {
 	return nil
 }
 
-func (c *CfgTemplate) genConfig(targetFile string, tmpl []byte) error {
+func (c *CfgTemplate) genConfig(targetFile string, tmpl []byte, bindData map[string]string) error {
 
+	moduleLogger.Debugf("generating config file: %s", targetFile)
 	cfgData := tmpl
 
-	if len(c.data) > 0 {
+	if len(bindData) > 0 {
 		tmp, err := template.New("cfg").Parse(string(tmpl))
 		if err != nil {
 			return fmt.Errorf("fail to parse template: %s, error: %s", string(tmpl), err)
 		}
 		var buf bytes.Buffer
-		err = tmp.Execute(&buf, c.data)
+		err = tmp.Execute(&buf, bindData)
 		if err != nil {
 			return fmt.Errorf("fail to apply template, %s", err)
 		}
@@ -188,4 +190,50 @@ func (c *CfgTemplate) genConfig(targetFile string, tmpl []byte) error {
 		return fmt.Errorf("write file '%s' failed, %s", targetFile, err)
 	}
 	return nil
+}
+
+func parseKVPairs(input string) map[string]string {
+
+	rep := strings.NewReplacer("\n", ";", "\r\n", ";")
+	input = rep.Replace(input)
+
+	if input == "" {
+		return nil
+	}
+
+	lastQuote := rune(0)
+	f := func(c rune) bool {
+		switch {
+		case c == lastQuote:
+			lastQuote = rune(0)
+			return false
+		case lastQuote != rune(0):
+			return false
+		case unicode.In(c, unicode.Quotation_Mark):
+			lastQuote = c
+			return false
+		default:
+			return c == rune(';')
+
+		}
+	}
+
+	items := strings.FieldsFunc(input, f)
+
+	output := make(map[string]string)
+
+	for _, item := range items {
+		npos := strings.IndexByte(item, '=')
+		if npos > 0 {
+			k := strings.Trim(item[:npos], `"`)
+			v := strings.Trim(item[npos+1:], `"`)
+			k = strings.TrimSpace(k)
+			v = strings.TrimSpace(v)
+			if k != "" {
+				output[k] = v
+			}
+		}
+	}
+
+	return output
 }
