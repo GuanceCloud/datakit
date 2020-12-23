6883b34e09b4cfc6078b3d582c3e38de3afcc66c
liushaobo <liushaobo@jiagouyun.com>
2020-12-23 10:28:33 +0800

    提交代码
     [formerly e59eb9650e529560324200795abd5988aa7f4085 [formerly 257823d2aab196da2c055aa54b3b7f7881623573]]
    
    
    
    Former-commit-id: 5f809740003cccab5b02b450e693c26488045ab2

--- plugins/inputs/cshark/config.go
+++ plugins/inputs/cshark/config.go
@@ -3,17 +3,23 @@ package cshark
 const (
 	sharkConfigSample = `
 #[[inputs.cshark]]
+interval = "10s"
+tsharkPath = "/usr/bin/tshark"
 `
 )
 
 type Shark struct {
 	MetricName string
+    Interval   string   `toml:"interval"`
+    TsharkPath string `toml:"tsharkPath"`
 }
 
 type Params struct {
-    Device  []string       `json:"device"`
-    Stream  *Stream        `json:"stream"`
-    Statistics *Statistics `json:"statistics"`
+    Device  []string          `json:"device"`
+    Stream  *Stream           `json:"stream"`
+    Statistics *Statistics    `json:"statistics"`
+    Sync     bool             `json:"sync"`
+    Fin      chan error `json:"-"`
 }
 
 type Stream struct {
