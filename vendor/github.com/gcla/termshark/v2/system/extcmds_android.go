87c0d5a293da328f95043f42b4202422b53e5796
liushaobo <liushaobo@jiagouyun.com>
2020-12-23 10:28:33 +0800

    提交代码
     [formerly 9a1372edc200b60d1f31b7eedc936650c3524565] [formerly 273897495bb986bd9d2b9c9d2b53f5148c4befc7 [formerly 7b7241daa2c75de74e9f9076df74e3d0480b1b3f] [formerly e70d407742a9be997f99fc1509736e67b29c1e41 [formerly e59eb9650e529560324200795abd5988aa7f4085] [formerly e70d407742a9be997f99fc1509736e67b29c1e41 [formerly e59eb9650e529560324200795abd5988aa7f4085] [formerly e70d407742a9be997f99fc1509736e67b29c1e41 [formerly e59eb9650e529560324200795abd5988aa7f4085] [formerly cc5dacf4467a9d8c0fb216a693bb659196309402 [formerly 6c9781657da8f0f891a7fadb3697859354c2125a]]]]]]
     [formerly 8fd5d005e0a53e7ca8834510069f3a0926ea891a [formerly d33c5f75e5dfeb84e2046b6a30c31573a2eb4acb] [formerly 8fd5d005e0a53e7ca8834510069f3a0926ea891a [formerly d33c5f75e5dfeb84e2046b6a30c31573a2eb4acb] [formerly 275946cd99c3eb28cfb9c94f473beeca7624e6fc [formerly 35451dd736dc3d45824c2818242c79372006ded8]]] [formerly 8fd5d005e0a53e7ca8834510069f3a0926ea891a [formerly d33c5f75e5dfeb84e2046b6a30c31573a2eb4acb] [formerly 275946cd99c3eb28cfb9c94f473beeca7624e6fc [formerly 35451dd736dc3d45824c2818242c79372006ded8]] [formerly 275946cd99c3eb28cfb9c94f473beeca7624e6fc [formerly 35451dd736dc3d45824c2818242c79372006ded8] [formerly cbf236565efe18d2e1cc46f4f26184f4563ae344 [formerly 916b7ccfafdf84c252699cd3a65df1e1208b5111]]]]]
    
    
    
    Former-commit-id: afa41a8b1614f759e6cf9bdfe31af7a04cec31ea [formerly e6ba408c2f893b939eb2f7fd5a150df827b411a2]
    Former-commit-id: ea2535711502b9bfccc78e99ce53e149d93ca800 [formerly 723fc390aa0611f1d85e33d806ca4508221bc635] [formerly ef15ccc3be1784e5f1a8d8f8ab8b0bc4b3d2d640 [formerly 4a50d016db8e7d98b65bd73d9054a899112ed1e9]]
    Former-commit-id: 8af0cd410f8320231be95c11256deca8255f0fa9 [formerly 4169b173a4d3ba53db6a239ae1cac34a79d3a4d8]
    Former-commit-id: f8119d20f9789fbb4f060ab403de25f9d92106d8

--- vendor/github.com/gcla/termshark/v2/system/extcmds_android.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2020 Graham Clark. All rights reserved.  Use of this source
-// code is governed by the MIT license that can be found in the LICENSE
-// file.
-
-package system
-
-var CopyToClipboard = []string{"termux-clipboard-set"}
-
-var OpenURL = []string{"am", "start", "-a", "android.intent.action.VIEW", "-d"}
