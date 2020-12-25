1de3ff87b0698a3e29cd2d0953fd6a7583106cd9
tanbiao <tanbiao@jiagouyun.com>
2020-12-25 07:18:54 +0000

    update: 	[*] remove input prom in default input-list 	[*] refact http/ws.go module
    Former-commit-id: af7c1762af54ceee995f8c0d301072403c0b5556 [formerly d1a5e78bf23994e2f4068f5f30b89af71eb3d8d5] [formerly 9778e85ab976acc7327d7d26097161f6e277163e [formerly 5cb39488aa0cd836a0ac8465bdbcc6599f7a8d4c]]
    Former-commit-id: 48a72658f4dcb02263118cc7a7f6c712ff81c49d [formerly eedf5c92f822c15f21ab25c3dee7e43dfdec0edd]
    Former-commit-id: 7b4eb0d76ea534d0b74f9b11aabb4ff042792e95

--- http/ws.go
+++ http/ws.go
@@ -34,9 +34,23 @@ var (
 )
 
 type wscli struct {
-	c    *websocket.Conn
-	id   string
-	exit chan interface{}
+	c     *websocket.Conn
+	id    string
+	reset chan interface{}
+}
+
+func (c *wscli) setup() {
+
+	c.reset = make(chan interface{})
+
+	cli.tryConnect(wsurl.String())
+	go func() {
+		cli.waitMsg() // blocking reader, do not add to datakit.WG
+	}()
+
+	go func() {
+		cli.sendHeartbeat()
+	}()
 }
 
 func StartWS() {
@@ -45,38 +59,36 @@ func StartWS() {
 	l.Infof(wsurl.String())
 
 	cli = &wscli{
-		id:   datakit.Cfg.MainCfg.UUID,
-		exit: make(chan interface{}),
+		id: datakit.Cfg.MainCfg.UUID,
 	}
 
-	if err := cli.tryConnect(wsurl.String());err == nil {
-		go func() {
-			cli.waitMsg()
-		}()
-
-		go func() {
-			cli.sendHeartbeat()
-		}()
-	}
+	cli.setup()
 
 	for {
 		select {
 		case <-datakit.Exit.Wait():
 			l.Info("start ws exit")
+			cli.c.Close()
 			return
-		case <-cli.exit:
-			cli.reset()
+
+		case <-cli.reset:
+
+			l.Info("start ws on reset")
+
+			cli.c.Close()
+			cli.setup()
 		}
 	}
-
 }
 
+// blocking wait ws connect ok
 func (wc *wscli) tryConnect(wsurl string) error {
 
 	for {
 		select {
 		case <-datakit.Exit.Wait():
-			return fmt.Errorf("ws not ready:wsurl:%s",wsurl)
+			return fmt.Errorf("ws not ready:wsurl:%s", wsurl)
+
 		default:
 			c, resp, err := websocket.DefaultDialer.Dial(wsurl, nil)
 			if err != nil {
@@ -84,29 +96,12 @@ func (wc *wscli) tryConnect(wsurl string) error {
 				time.Sleep(time.Second * 3)
 				continue
 			}
-			_ = resp
+
+			l.Infof("ws connect ok, resp: %+#v", resp)
 
 			wc.c = c
-			l.Infof("ws ready")
 			return nil
 		}
-
-	}
-}
-
-func (wc *wscli) reset() {
-	l.Infof("ws reset run")
-	wc.c.Close()
-	wc.exit = make(chan interface{})
-
-	if err := cli.tryConnect(wsurl.String());err == nil {
-		go func() {
-			cli.waitMsg()
-		}()
-
-		go func() {
-			cli.sendHeartbeat()
-		}()
 	}
 }
 
@@ -116,29 +111,31 @@ func (wc *wscli) waitMsg() {
 	f = func(trace []byte, err error) {
 		for {
 
+			// TODO: if panic, fire a key event to dataflux
+
 			defer rtpanic.Recover(f, nil)
 
 			if trace != nil {
-				l.Warn("recover ok: %s err:", string(trace), err.Error())
-
+				l.Warnf("recover ok: %s err:", string(trace), err.Error())
 			}
 
 			_, resp, err := wc.c.ReadMessage()
 			if err != nil {
-				l.Error(err)
-				wc.exit <- make(chan interface{})
+				l.Errorf("ws read message error: %s", err)
+				wc.reset <- nil
 				return
 			}
 
 			wm, err := wsmsg.ParseWrapMsg(resp)
 			if err != nil {
-				l.Error("msg.ParseWrapMsg(): %s", err.Error())
+				l.Errorf("msg.ParseWrapMsg(): %s", err.Error())
 				continue
 			}
-			l.Infof("dk hand message %s", wm)
+
+			l.Debugf("ws hand message %s", wm)
 
 			if err := wc.handle(wm); err != nil {
-				wc.exit <- make(chan interface{})
+				wc.reset <- nil
 				return
 			}
 		}
@@ -177,7 +174,7 @@ func (wc *wscli) sendHeartbeat() {
 
 			err = wc.sendText(wm)
 			if err != nil {
-				wc.exit <- make(chan interface{})
+				wc.reset <- nil
 				return
 			}
 
@@ -242,41 +239,40 @@ func (wc *wscli) EnableInputs(wm *wsmsg.WrapMsg) {
 	var names wsmsg.MsgGetInputConfig
 	err := names.Handle(wm)
 	if err != nil {
-		wc.SetMessage(wm, "bad_request", fmt.Sprintf("parse config err:%s",err.Error()))
+		wc.SetMessage(wm, "bad_request", fmt.Sprintf("parse config err:%s", err.Error()))
 		return
 	}
 	inputsConfMap := SearchFile(names.Names)
-	for _,v := range names.Names {
-		n,_ := inputs.InputEnabled(v)
+	for _, v := range names.Names {
+		n, _ := inputs.InputEnabled(v)
 		if n > 0 {
-			wc.SetMessage(wm, "error", fmt.Sprintf("input:%s is enable",v))
+			wc.SetMessage(wm, "error", fmt.Sprintf("input:%s is enable", v))
 			return
 		}
-		if confPath ,ok := inputsConfMap[v];!ok {
-			wc.SetMessage(wm, "error", fmt.Sprintf("input:%s not exist conf",v))
+		if confPath, ok := inputsConfMap[v]; !ok {
+			wc.SetMessage(wm, "error", fmt.Sprintf("input:%s not exist conf", v))
 			return
-		}else {
+		} else {
 			buf, err := ioutil.ReadFile(confPath)
 			if err != nil {
-				wc.SetMessage(wm, "error", fmt.Sprintf("input:%s read conf err:%s",v,err))
+				wc.SetMessage(wm, "error", fmt.Sprintf("input:%s read conf err:%s", v, err))
 				return
 			}
-			tbl,err := toml.Parse(buf)
+			tbl, err := toml.Parse(buf)
 			if err != nil {
-				wc.SetMessage(wm, "error", fmt.Sprintf("input:%s parse conf err:%s",v,err))
+				wc.SetMessage(wm, "error", fmt.Sprintf("input:%s parse conf err:%s", v, err))
 				return
 			}
 			if len(tbl.Fields) == 0 {
-				datakit.AnnotationConf(confPath,"delete")
+				datakit.AnnotationConf(confPath, "delete")
 			}
 
 		}
 
 	}
-	wc.SetMessage(wm,"ok","")
+	wc.SetMessage(wm, "ok", "")
 
 }
-
 
 func (wc *wscli) TestInput(wm *wsmsg.WrapMsg) {
 	var configs wsmsg.MsgSetInputConfig
@@ -336,7 +332,7 @@ func (wc *wscli) TestInput(wm *wsmsg.WrapMsg) {
 	wc.SetMessage(wm, "ok", returnMap)
 }
 
-func (wc *wscli)ModifyStatus() {
+func (wc *wscli) ModifyStatus() {
 	var wmsg = wsmsg.WrapMsg{}
 	wmsg.Type = wsmsg.MtypeModifyDKStatus
 	wmsg.ID = cliutils.XID("wmsg_")
@@ -361,7 +357,7 @@ func (wc *wscli) Reload(wm *wsmsg.WrapMsg) {
 
 func checkConfig(listInput []string) bool {
 	var Set = map[string]bool{}
-	for _,inp := range listInput {
+	for _, inp := range listInput {
 		Set[inp] = true
 	}
 	if len(Set) != len(listInput) {
@@ -370,7 +366,7 @@ func checkConfig(listInput []string) bool {
 	return true
 }
 
-func marshalToml(sstbl interface{},name string,listMd5 []string)(catelog string,err error)  {
+func marshalToml(sstbl interface{}, name string, listMd5 []string) (catelog string, err error) {
 	tbls := []*ast.Table{}
 	switch t := sstbl.(type) {
 	case []*ast.Table:
@@ -385,7 +381,7 @@ func marshalToml(sstbl interface{},name string,listMd5 []string)(catelog string,
 		if creator, ok := inputs.Inputs[name]; ok {
 			inp := creator()
 			err = toml.UnmarshalTable(tb, inp)
-			listMd5 = append(listMd5, inputs.SetInputsMD5(name,inp))
+			listMd5 = append(listMd5, inputs.SetInputsMD5(name, inp))
 			catelog = inp.Catalog()
 			continue
 		}
@@ -393,7 +389,7 @@ func marshalToml(sstbl interface{},name string,listMd5 []string)(catelog string,
 			catelog = creator.Catalog
 			cr := reflect.ValueOf(creator).Elem().Interface()
 			err = toml.UnmarshalTable(tb, cr.(tgi.TelegrafInput).Input)
-			listMd5 = append(listMd5, inputs.SetInputsMD5(name,cr.(tgi.TelegrafInput)))
+			listMd5 = append(listMd5, inputs.SetInputsMD5(name, cr.(tgi.TelegrafInput)))
 			continue
 		}
 		err = fmt.Errorf("input:%s is not available", name)
@@ -402,8 +398,7 @@ func marshalToml(sstbl interface{},name string,listMd5 []string)(catelog string,
 	return
 }
 
-
-func parseConf(conf string, name string) (listMd5 []string,catelog string, err error) {
+func parseConf(conf string, name string) (listMd5 []string, catelog string, err error) {
 	data, err := base64.StdEncoding.DecodeString(conf)
 	if err != nil {
 		return
@@ -430,7 +425,7 @@ func parseConf(conf string, name string) (listMd5 []string,catelog string, err e
 				}
 			}
 		default:
-			catelog,err = marshalToml(node,name,listMd5)
+			catelog, err = marshalToml(node, name, listMd5)
 			if err != nil {
 				return
 			}
@@ -439,22 +434,20 @@ func parseConf(conf string, name string) (listMd5 []string,catelog string, err e
 	return
 }
 
-
-
 func (wc *wscli) parseTomlToFile(tomlStr, name string) error {
-	listInput,catalog, err := parseConf(tomlStr, name)
+	listInput, catalog, err := parseConf(tomlStr, name)
 	if err != nil {
 		return err
 	}
-	if !checkConfig(listInput){
+	if !checkConfig(listInput) {
 		return fmt.Errorf("cannot set same config")
 	}
-	inputPath := filepath.Join(datakit.ConfdDir, catalog, fmt.Sprintf("%s.conf",name))
+	inputPath := filepath.Join(datakit.ConfdDir, catalog, fmt.Sprintf("%s.conf", name))
 	inputConfMap := SearchFile([]string{name})
-	if confPath,ok  := inputConfMap[name];ok {
+	if confPath, ok := inputConfMap[name]; ok {
 		inputPath = confPath
 	}
-	if err = wc.WriteFile(tomlStr,inputPath); err != nil {
+	if err = wc.WriteFile(tomlStr, inputPath); err != nil {
 		return err
 	}
 	return nil
@@ -464,11 +457,11 @@ func (wc *wscli) SetInput(wm *wsmsg.WrapMsg) {
 	var configs wsmsg.MsgSetInputConfig
 	err := configs.Handle(wm)
 	if err != nil {
-		wc.SetMessage(wm, "bad_request", fmt.Sprintf("%+#v",wm))
+		wc.SetMessage(wm, "bad_request", fmt.Sprintf("%+#v", wm))
 		return
 	}
 	for k, v := range configs.Configs {
-		if err := wc.parseTomlToFile(v["toml"],k); err != nil {
+		if err := wc.parseTomlToFile(v["toml"], k); err != nil {
 			wc.SetMessage(wm, "error", err.Error())
 			return
 		}
@@ -477,7 +470,7 @@ func (wc *wscli) SetInput(wm *wsmsg.WrapMsg) {
 	wc.SetMessage(wm, "ok", "")
 }
 
-func (wc *wscli) WriteFile(tomlStr,cfgPath string) error{
+func (wc *wscli) WriteFile(tomlStr, cfgPath string) error {
 	data, err := base64.StdEncoding.DecodeString(tomlStr)
 	if err != nil {
 		return err
@@ -490,20 +483,20 @@ func (wc *wscli) DisableInput(wm *wsmsg.WrapMsg) {
 	var names wsmsg.MsgGetInputConfig
 	err := names.Handle(wm)
 	if err != nil {
-		wc.SetMessage(wm, "bad_request", fmt.Sprintf("parse config err:%s",err.Error()))
+		wc.SetMessage(wm, "bad_request", fmt.Sprintf("parse config err:%s", err.Error()))
 		return
 	}
-	for _,name := range names.Names {
-		n,cfg := inputs.InputEnabled(name)
+	for _, name := range names.Names {
+		n, cfg := inputs.InputEnabled(name)
 		if n > 0 {
-			for _,fp := range cfg {
-				if err := datakit.AnnotationConf(fp,"add");err!= nil {
-					wc.SetMessage(wm, "error", fmt.Sprintf("input:%s disable err:%s",name,err.Error()))
+			for _, fp := range cfg {
+				if err := datakit.AnnotationConf(fp, "add"); err != nil {
+					wc.SetMessage(wm, "error", fmt.Sprintf("input:%s disable err:%s", name, err.Error()))
 					return
 				}
 			}
-		}else {
-			wc.SetMessage(wm, "error", fmt.Sprintf("input:%s not enable",name))
+		} else {
+			wc.SetMessage(wm, "error", fmt.Sprintf("input:%s not enable", name))
 			return
 		}
 	}
@@ -513,7 +506,7 @@ func (wc *wscli) DisableInput(wm *wsmsg.WrapMsg) {
 func (wc *wscli) GetEnableInputsConfig(wm *wsmsg.WrapMsg) {
 	var names wsmsg.MsgGetInputConfig
 	err := names.Handle(wm)
-	if err != nil || len(names.Names)==0{
+	if err != nil || len(names.Names) == 0 {
 		wc.SetMessage(wm, "bad_request", fmt.Sprintf("params error"))
 		return
 	}
@@ -560,7 +553,7 @@ func (wc *wscli) GetInputsConfig(wm *wsmsg.WrapMsg) {
 	var data []map[string]map[string]string
 	inputConfMap := SearchFile(names.Names)
 	for _, v := range names.Names {
-		if inputConf,ok := inputConfMap[v];!ok{
+		if inputConf, ok := inputConfMap[v]; !ok {
 			sample, err := inputs.GetSample(v)
 			if err != nil {
 				errMessage := fmt.Sprintf("get config error %s", err)
@@ -569,9 +562,9 @@ func (wc *wscli) GetInputsConfig(wm *wsmsg.WrapMsg) {
 				return
 			}
 			data = append(data, map[string]map[string]string{v: {"toml": base64.StdEncoding.EncodeToString([]byte(sample))}})
-		}else {
+		} else {
 
-			buf,err :=  ioutil.ReadFile(inputConf)
+			buf, err := ioutil.ReadFile(inputConf)
 			if err != nil {
 				errMessage := fmt.Sprintf("get config error %s", err)
 				l.Error(errMessage)
@@ -586,19 +579,19 @@ func (wc *wscli) GetInputsConfig(wm *wsmsg.WrapMsg) {
 
 func (wc *wscli) OnlineInfo(wm *wsmsg.WrapMsg) {
 	m := wsmsg.MsgDatakitOnline{
-		UUID:            wc.id,
-		Name:            datakit.Cfg.MainCfg.Name,
-		Version:         git.Version,
-		OS:              runtime.GOOS,
-		Arch:            runtime.GOARCH,
-		Heartbeat:       datakit.Cfg.MainCfg.DataWay.Heartbeat,
-		InputInfo:       map[string]interface{}{},
+		UUID:      wc.id,
+		Name:      datakit.Cfg.MainCfg.Name,
+		Version:   git.Version,
+		OS:        runtime.GOOS,
+		Arch:      runtime.GOARCH,
+		Heartbeat: datakit.Cfg.MainCfg.DataWay.Heartbeat,
+		InputInfo: map[string]interface{}{},
 	}
 	m.InputInfo["availableInputs"] = GetAvailableInputs()
 	m.InputInfo["enabledInputs"] = GetEnableInputs()
-	state,err := io.GetStats()
+	state, err := io.GetStats()
 	if err != nil {
-		l.Errorf("get state err:%s",err.Error())
+		l.Errorf("get state err:%s", err.Error())
 		state = []*io.InputsStat{}
 	}
 	m.InputInfo["state"] = state
@@ -625,28 +618,25 @@ func GetAvailableInputs() []string {
 	return AvailableInputs
 }
 
-func GetEnableInputs()(Enable []string)  {
+func GetEnableInputs() (Enable []string) {
 	for k, _ := range inputs.Inputs {
 		n, _ := inputs.InputEnabled(k)
 		if n > 0 {
-			Enable = append(Enable,k)
+			Enable = append(Enable, k)
 		}
 	}
 
 	for k, _ := range tgi.TelegrafInputs {
 		n, _ := inputs.InputEnabled(k)
 		if n > 0 {
-			Enable = append(Enable,k)
+			Enable = append(Enable, k)
 		}
 	}
 	return Enable
 }
 
-
-
 func SearchFile(inputName []string) map[string]string {
-	confMap := map[string]string{
-	}
+	confMap := map[string]string{}
 
 	if err := filepath.Walk(datakit.ConfdDir, func(fp string, f os.FileInfo, err error) error {
 		if err != nil {
@@ -657,7 +647,7 @@ func SearchFile(inputName []string) map[string]string {
 			l.Debugf("ignore dir %s", fp)
 			return nil
 		}
-		for _,v := range inputName {
+		for _, v := range inputName {
 			if f.Name() == fmt.Sprintf("%s.conf", v) {
 				confMap[v] = fp
 			}
