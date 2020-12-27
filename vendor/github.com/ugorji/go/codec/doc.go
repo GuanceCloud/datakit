2184ce83f47cc8e057fe49d7488096c37681bc66
tanbiao <tanbiao@jiagouyun.com>
2020-12-27 08:58:20 +0000

    update: 	[*] remove dependency on ftagent 	[*] refact HTTP response 	[+] add http/rum test case
     [formerly f409b5a2e6c1365aff1ba408fb467e47aef24384] [formerly 9646bf1c4f7d5f045a20caf8c3961a23ada2e3a5 [formerly d0baf796cd1bc5e202bc7382e7f3d343dda13f15] [formerly 7357f9baf54c3330ab0a9394d76dac54bf25cc96 [formerly b029559dce5143468a1e6e81a7a95cfec2eb3a66] [formerly 7357f9baf54c3330ab0a9394d76dac54bf25cc96 [formerly b029559dce5143468a1e6e81a7a95cfec2eb3a66] [formerly 7357f9baf54c3330ab0a9394d76dac54bf25cc96 [formerly b029559dce5143468a1e6e81a7a95cfec2eb3a66] [formerly 3c32603f42127d52ced53330b1f401a7048c4e61 [formerly e9238962314aa4aa2c60bfa6acbb8a3aed98c2aa]]]]]]
     [formerly 8867473ceb2d392f94514b1fc6bc9161704822b4 [formerly 1c91c15f1752db2b2f685aa6bd5edcd32b727784] [formerly 8867473ceb2d392f94514b1fc6bc9161704822b4 [formerly 1c91c15f1752db2b2f685aa6bd5edcd32b727784] [formerly a79da41aad0415be8e16aeca85219e0ff769e1b0 [formerly 17cd9b956557512c3bfbf3549813736a1ec1a6ea]]] [formerly 8867473ceb2d392f94514b1fc6bc9161704822b4 [formerly 1c91c15f1752db2b2f685aa6bd5edcd32b727784] [formerly a79da41aad0415be8e16aeca85219e0ff769e1b0 [formerly 17cd9b956557512c3bfbf3549813736a1ec1a6ea]] [formerly a79da41aad0415be8e16aeca85219e0ff769e1b0 [formerly 17cd9b956557512c3bfbf3549813736a1ec1a6ea] [formerly acbe862775eada368524202838c42c089c47111e [formerly 749767a5deaeae50d5bcd62068a0d5360ebec3b9]]]]]
    
    
    
    Former-commit-id: 0cecc55560b00a98f17974203e3ca9df421286f0 [formerly 3cc8129e107c5c73f9596b99d753276d1566f640]
    Former-commit-id: 5cc3734e93ccb8f30d73c5058e46821b8980f355 [formerly d35479d7c993231a52d4732a650fa7fdb2b19659] [formerly fc1750bf4be049886abf4f19eee270436e83060b [formerly 5e7907e71bed94c32935c0143380a6fc986d9ce9]]
    Former-commit-id: e082add5420bdde5dfb20ed1ca5235f365575ae6 [formerly b4dfb3109c337b725f10ea742bf4c764d8372323]
    Former-commit-id: e2ef8eaf8a756d088651a3a9f6d69bd2a67616af

--- vendor/github.com/ugorji/go/codec/doc.go
+++ vendor/github.com/ugorji/go/codec/doc.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2018 Ugorji Nwoke. All rights reserved.
+// Copyright (c) 2012-2020 Ugorji Nwoke. All rights reserved.
 // Use of this source code is governed by a MIT license found in the LICENSE file.
 
 /*
@@ -17,6 +17,8 @@ Supported Serialization formats are:
 This package will carefully use 'package unsafe' for performance reasons in specific places.
 You can build without unsafe use by passing the safe or appengine tag
 i.e. 'go install -tags=safe ...'.
+
+This library works with both the standard `gc` and the `gccgo` compilers.
 
 For detailed usage information, read the primer at http://ugorji.net/blog/go-codec-primer .
 
