80273a6cd215c5f974a90f4c11630623a804090a
huangbo <huangbo@jiagouyun.com>
2021-01-04 15:06:34 +0800

    log func增加
     [formerly e1f832630267b8f8ad126dd357d44579816a04a2] [formerly 52809c84f73b6128232ec60995e6890ca72a5e28 [formerly 357b38445f2277a939ef2df8e8812743e831cb9d] [formerly f76959a2423709fcc932342db995093fbec0cceb [formerly 7f2764294e5c686a18d1d30b305b58a2acd3b338] [formerly f76959a2423709fcc932342db995093fbec0cceb [formerly 7f2764294e5c686a18d1d30b305b58a2acd3b338] [formerly f76959a2423709fcc932342db995093fbec0cceb [formerly 7f2764294e5c686a18d1d30b305b58a2acd3b338] [formerly 8b3ad2ef0a8f3db59d7859cf79cafc002a718b81 [formerly 92dad1ba195da1f4808158996651b7c80edae373]]]]]]
     [formerly 2eff46d959d50f434b0caa352adc00f5c0a47450 [formerly acae52c61c39242604c1b07a437b0cb1ba221454] [formerly b92f76c4a84758d8b4e7a6944a6579efb7495e7b [formerly 890550e95fffa38e207da07c733a613eeb2ca932]] [formerly 2eff46d959d50f434b0caa352adc00f5c0a47450 [formerly acae52c61c39242604c1b07a437b0cb1ba221454] [formerly b92f76c4a84758d8b4e7a6944a6579efb7495e7b [formerly 890550e95fffa38e207da07c733a613eeb2ca932]] [formerly b92f76c4a84758d8b4e7a6944a6579efb7495e7b [formerly 890550e95fffa38e207da07c733a613eeb2ca932] [formerly fcfa461c9c3ffa4faa097aeabdfe714f7632861e [formerly 0ac9a5314c4d2946926d2864e8389f022705998f]]]]]
    
    
    
    Former-commit-id: 8e59914c821062612ab1c631e1afa1e2e6437859 [formerly 96e3d8d6e8c42d5577d9080c932e00dd88615b1c]
    Former-commit-id: df288b13e6f587510bd0db3ae9d752087bf16856 [formerly 79ea34d8d49e4f6920e57cd226a888ee7bbded1f] [formerly 5d583ce01ce6ee31ed67fb2a283b239d4146bf45 [formerly e8465207ed529d42625315760dc6d48898ae51f2]]
    Former-commit-id: 73c1892eb85249b45e222f6a37d46eff1e33b15e [formerly 41de7275e9eeece27d58f2b078515ec1e8702c74]
    Former-commit-id: 46912e308b5fd420692977374d99db7fe34951a4

---
+++ process/funcs.go
@@ -0,0 +1,127 @@
+package process
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/process/parser"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/process/grok"
+)
+const (
+	CONTENT = "__content"
+)
+
+type ProFunc func(p *Procedure, node parser.Node) (*Procedure, error)
+
+var (
+	funcsMap = map[string]ProFunc{
+		"grok"       : grok.Grok,
+		"rename"     : Rename   ,
+		"user_agent" : UserAgent,
+		"urldecode"  : UrlDecode,
+		"GeoIp"      : GeoIp    ,
+		"date"       : Date    ,
+	}
+)
+
+func (p *Procedure) ProcessLog(data string, nodes []parser.Node) *Procedure {
+	var err error
+
+	p.Content = logStructed(data)
+
+	for _, node := range nodes {
+		switch v := node.(type) {
+		case *parser.FuncExpr:
+			fn := strings.ToLower(v.Name)
+			f, ok := funcsMap[fn]
+			if !ok {
+				err := fmt.Errorf("unsupported func: %v", v.Name)
+				l.Error(err)
+				p.lastErr = err
+				return p
+			}
+
+			p, err = f(p, node)
+			if err != nil {
+				l.Errorf("ProcessLog %v: %v", v.Name, err)
+				p.lastErr = err
+				return p
+			}
+
+		default:
+			p.lastErr = fmt.Errorf("%v not function", v.String())
+		}
+	}
+	return p
+}
+
+func Rename(p *Procedure, node parser.Node) (*Procedure, error) {
+	return p, nil
+}
+
+func UserAgent(p *Procedure, node parser.Node) (*Procedure, error) {
+	return p, nil
+}
+
+func UrlDecode(p *Procedure, node parser.Node) (*Procedure, error) {
+	return p, nil
+}
+
+func GeoIp(p *Procedure, node parser.Node) (*Procedure, error) {
+	return p, nil
+}
+
+func Date(p *Procedure, node parser.Node) (*Procedure, error) {
+	return p, nil
+}
+
+
+func ParseScript(path string) ([]parser.Node, error) {
+	pNodes := make([]parser.Node, 0)
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s := bufio.NewScanner(strings.NewReader(string(data)))
+	for s.Scan() {
+		str := s.Text()
+		str = strings.TrimSpace(str)
+
+		if len(str) == 0 {
+			continue
+		}
+
+		if str[0] == '#' {
+			continue
+		}
+
+		nodes, err := parser.ParseFuncExpr(str)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(nodes) == 0 {
+			continue
+		}
+
+		pNodes = append(pNodes, nodes...)
+	}
+
+	return pNodes, nil
+}
+
+func logStructed(data string) []byte {
+	m := make(map[string]interface{})
+	err := json.Unmarshal([]byte(data), &m)
+	if err == nil {
+		return []byte(data)
+	}
+
+	m[CONTENT] = data
+	js, _ := json.Marshal(m)
+	return js
+}
