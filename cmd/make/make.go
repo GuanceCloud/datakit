021e90fe2be8134ff2cc05a6476b8d6af52608b5
tanbiao <tanbiao@jiagouyun.com>
2021-01-05 14:43:04 +0800

    update: 	[*] update build-isp

--- cmd/make/make.go
+++ cmd/make/make.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"os"
+	"path/filepath"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
@@ -20,11 +21,22 @@ var (
 	flagEnv          = flag.String(`env`, ``, `build for local/test/preprod/release`)
 	flagPub          = flag.Bool(`pub`, false, `publish binaries to OSS: local/test/release/preprod`)
 	flagPubAgent     = flag.Bool("pub-agent", false, `publish telegraf`)
+	flagBuildISP     = flag.Bool("build-isp", false, "generate ISP data")
 
 	l = logger.DefaultSLogger("make")
 )
 
 func applyFlags() {
+
+	if *flagBuildISP {
+		curDir, _ := os.Getwd()
+		inputDir := filepath.Join(curDir, "china-operator-ip")
+		outputFile := filepath.Join(curDir, "process", "ip2isp", "ip2isp.go")
+		build.GenIspFile(inputDir, outputFile)
+
+		os.Exit(0)
+	}
+
 	build.AppBin = *flagBinary
 	build.BuildDir = *flagBuildDir
 	build.PubDir = *flagPubDir
@@ -52,6 +64,7 @@ func applyFlags() {
 		build.PubDatakit()
 		os.Exit(0)
 	}
+
 }
 
 func main() {
