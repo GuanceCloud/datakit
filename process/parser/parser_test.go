519fea51a406da1896ad717295151a3a6892a2c0
huangbo <huangbo@jiagouyun.com>
2021-01-05 11:29:45 +0800

    log func增加
    Former-commit-id: f4cc5e905c392ea2ad091e42fe649937314d5d58
    Former-commit-id: 57d27fa1aeea10e0ee10fc5fbefaf7bd8d09b7b1 [formerly b55ccc72baa05b47ce64d9abeb925f85b289a63b]
    Former-commit-id: 3dd7ef492951bd999f69bc215a894f38c9f69f76

--- process/parser/parser_test.go
+++ process/parser/parser_test.go
@@ -1,10 +1,6 @@
 package parser
 
-import (
-	"testing"
-
-	//"gitlab.jiagouyun.com/cloudcare-tools/cliutils/testutil"
-)
+import "testing"
 
 type parseCase struct {
 	in       string
@@ -12,78 +8,35 @@ type parseCase struct {
 	err      string
 	fail     bool
 }
-var in = `count(avg(2,5));
-count2(avg2(2,5));
-count3(avg2(2,5));count4(avg2(2,5));
-count6(avg2(2,5))
-count5(avg2(21,51))
-`
-func TestParseFuncExpr(t *testing.T) {
-	var funcExprCases = []*parseCase{
-		{
-			in: "#count(avg(2,5));count1(avg2(2,5))",
-			expected: &Funcs{
-				&FuncExpr{
-					Name: "count",
-					Param: []Node{
-						&FuncExpr{
-							Name: "avg",
-							Param: []Node{
-								&NumberLiteral{IsInt: true, Int: 2},
-								&NumberLiteral{IsInt: true, Int: 5},
-							},
-						},
-					},
-				},
-			},
-		},
+var in = `avg(x.y.z,1,2,3, p68, re("cd"), pqa);
 
+f(g(f("abc"), 123),
+1,2,3);
+
+
+kkk(g(1,2),h(2,3))
+`
+func FuncPrintHelp(f *FuncExpr, prev string, t *testing.T)  {
+	t.Logf("%v%v", prev, f.Name)
+
+	for _, node := range f.Param {
+		switch v := node.(type) {
+		case *FuncExpr:
+			FuncPrintHelp(v, prev+"    ", t)
+		default:
+			t.Logf("%v%v", prev+"    |", node)
+		}
+	}
+}
+func TestParseFuncExpr(t *testing.T) {
+	fexpr, err := ParseFuncExpr(in)
+	if err != nil {
+		t.Error(err)
+		return
 	}
 
-	for idx, _ := range funcExprCases {
-		_ = idx
-
-		fexpr, err := ParseFuncExpr(in)
-		if err != nil {
-			t.Error(err)
-		}
-		t.Logf("%v", len(fexpr))
-
-		for _, fex := range fexpr {
-			t.Logf("----------------------------\n")
-			f, _ := fex.(*FuncExpr)
-			t.Logf("%v", f.Name)
-			ff, _ := f.Param[0].(*FuncExpr)
-			t.Logf("%v", ff.Name)
-			fp0 := ff.Param[0].(*NumberLiteral)
-			t.Logf("%v", fp0.Int)
-			fp1 := ff.Param[1].(*NumberLiteral)
-			t.Logf("%v", fp1.Int)
-
-		}
-
-		//var x, y string
-
-		//if err != nil {
-		//	t.Logf("parse error: %s", err.Error())
-		//} else {
-		//	if tc.expected != nil {
-		//		t.Logf("tc.expected: %+#v", tc.expected)
-		//		expected, ok := tc.expected.(Funcs)
-		//		if ok {
-		//			x = expected.String()
-		//		}
-		//		y = fexpr.String()
-		//	}
-		//}
-
-		//if !tc.fail {
-		//	testutil.Ok(t, err)
-		//	testutil.Equals(t, x, y)
-		//	t.Logf("[%d] ok %s -> %s", idx, tc.in, y)
-		//} else {
-		//	t.Logf("[%d] %s -> expect fail: %v", idx, tc.in, err)
-		//	testutil.NotOk(t, err, "")
-		//}
+	for _, fex := range fexpr {
+		f, _ := fex.(*FuncExpr)
+		FuncPrintHelp(f, "", t)
 	}
 }
