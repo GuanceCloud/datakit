338cbd7980e1ebd63a0bece8fc0d901c51aa7610
liguozhuang <liguozhuang@jiagouyun.com>
2021-01-08 16:30:29 +0800

    update: 	[*]添加telegraf_http采集器，用以接收telegraf数据并区分metric和logging 	[*]telegraf_http采集器需要支持pipeline功能，ongoing
     [formerly f3a7a2266cbd20e38539f63b2c45af6f469ea914 [formerly 9ef0439708c60c073336b5cfd2f0057a738a5615] [formerly 0b14751d00d93dd0ad3b4ec86131bdc9911295cd [formerly 5b4df7bb5ecab12f1106b179644dcc160875629c]] [formerly 1cf447859d8aea0687864232aa7b243e832c64b2] [formerly 21ccecc74bd74807cbc1479fc80fba770198c264 [formerly 8b358c6d7a1c0ea09a3e1577c56a55a6f8d4be9f] [formerly 21ccecc74bd74807cbc1479fc80fba770198c264 [formerly 8b358c6d7a1c0ea09a3e1577c56a55a6f8d4be9f] [formerly 6e497030dfa21a819de458ed43cef47b8a341035 [formerly 9c1626d723961e763850ab0f89d51cfd8dce9341]]]] [formerly 0bff26b589ea2de3b3975a26ff5503112e7f30df] [formerly 59144a5693948180624558d1fee786a24e88a465 [formerly 1c5d06b9788d97af1baa58c2c1f731611d870d04] [formerly 73d2111414ca9dbca3c084216683603727b4b263 [formerly cb5c8a809cee9717f7140326316d01986f5c49f8] [formerly 73d2111414ca9dbca3c084216683603727b4b263 [formerly cb5c8a809cee9717f7140326316d01986f5c49f8] [formerly 73d2111414ca9dbca3c084216683603727b4b263 [formerly cb5c8a809cee9717f7140326316d01986f5c49f8] [formerly ae32970010b0b5d626bf3615ff0d8fc4531d8209 [formerly 6528d01904013f426594bac1f21410906212ba12]]]]]]]
     [formerly 0bff26b589ea2de3b3975a26ff5503112e7f30df]
    
    
    
    
    Former-commit-id: 8d9406c7aaef12df47d498c87c79e08204ce53e4 [formerly 70297fe43cbbd9c93baa2e38cd94f36eca68d9ae]
    Former-commit-id: 411a3bac12edab30c27e97cab3eb6606722f9b22 [formerly be1cf6485e60ad30c599eb1a897bd8a5102a221e] [formerly ad7d673eb778d5f5fab0dcaf7e45ca6fb77e3637 [formerly 19386e6997ca089efe14f7962fa5fb98bc3ad395]]
    Former-commit-id: 0a6edd8752575541635c68302bf3fbbf0ae28a38 [formerly 523a4a96bb734ae9ed9e925b8410d688c4e7242b]
    Former-commit-id: 6bfca211895b72464146ed276cf115b307022307

--- plugins/inputs/telegraf_inputs/tginputs.go
+++ plugins/inputs/telegraf_inputs/tginputs.go
@@ -86,7 +86,8 @@ type TelegrafInput struct {
 	// so we can't get sample from input.SampleConfig(), we just return the Sample field
 	//
 	// Bad news: it's hard to sync upstream telegraf updates on these inputs
-	Sample string
+	Sample   string
+	Pipeline string
 }
 
 func (ti *TelegrafInput) SampleConfig() string {
@@ -110,11 +111,11 @@ func (ti *TelegrafInput) SampleConfig() string {
 
 var (
 	TelegrafInputs = map[string]*TelegrafInput{ // Name: Catalog
-		"disk":     {name: "disk", Catalog: "host", Input: &disk.DiskStats{}},
-		"diskio":   {name: "diskio", Catalog: "host", Input: &diskio.DiskIO{}},
-		"mem":      {name: "mem", Catalog: "host", Input: &mem.MemStats{}},
-		"swap":     {name: "swap", Catalog: "host", Input: &swap.SwapStats{}},
-		"system":   {name: "system", Catalog: "host", Input: &system.SystemStats{}},
+		"disk":   {name: "disk", Catalog: "host", Input: &disk.DiskStats{}},
+		"diskio": {name: "diskio", Catalog: "host", Input: &diskio.DiskIO{}},
+		"mem":    {name: "mem", Catalog: "host", Input: &mem.MemStats{}},
+		"swap":   {name: "swap", Catalog: "host", Input: &swap.SwapStats{}},
+		"system": {name: "system", Catalog: "host", Input: &system.SystemStats{}},
 		//"cpu":      {name: "cpu", Catalog: "host", input: &cpu.CPUStats{}},
 		"cpu":      {name: "cpu", Catalog: "host", Sample: samples["cpu"], Input: nil},
 		"procstat": {name: "procstat", Catalog: "host", Input: &procstat.Procstat{}},
@@ -233,4 +234,3 @@ func CheckTelegrafToml(name string, tomlcfg []byte) error {
 	l.Debugf("toml %+#v", ti.Input)
 	return nil
 }
-
