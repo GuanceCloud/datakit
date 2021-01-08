046a489e46bf3fc7b101def10391dc5095001f64
huangbo <huangbo@jiagouyun.com>
2021-01-08 16:23:42 +0800

    修改函数实现及单元测试
     [formerly dfd6e10a44907c9eb7b48ff8243c7778915abb52 [formerly 219aef4d89b69679613929662d01236a7aa2c5cc] [formerly 32316030dc632d66f575c96f2ffaec0a0728be58 [formerly a2e18a728205de2db0ded037ffd71c607cb975e2]] [formerly 4ba74190f4354d428a2ac48742e9148cd3e28351] [formerly fd4f659dad85ca6c053176d845820ddd81b25025 [formerly 7f29ceb954621285bd7a32bd58b7782c4a60e864] [formerly fd4f659dad85ca6c053176d845820ddd81b25025 [formerly 7f29ceb954621285bd7a32bd58b7782c4a60e864] [formerly ca4bc2fd0e5371a25b1653d5b5815a9afc17b314 [formerly 6f91d43623e50e109976631c460310dde0e0e702]]]] [formerly c574f11cb7232f83784de2d1cb1cf422583fb0a9] [formerly 501f64cdabff37a4a4179e34fa949276aa5d0665 [formerly f0e125938ea60068526af07845bd25b5a00e7e28] [formerly 38835d335fc81db13a1f27079fd8c7ef23e7825e [formerly 9d9b9e7283e5f8adf58ace799f2d41dc5f3c1d8a] [formerly 38835d335fc81db13a1f27079fd8c7ef23e7825e [formerly 9d9b9e7283e5f8adf58ace799f2d41dc5f3c1d8a] [formerly 38835d335fc81db13a1f27079fd8c7ef23e7825e [formerly 9d9b9e7283e5f8adf58ace799f2d41dc5f3c1d8a] [formerly 5ade16aa7a80ac1c9f64f99af0ae15e5ae6fc5fe [formerly b74e209a4f9138352c8d265b49e8815ae25bf2df]]]]]]]
     [formerly c574f11cb7232f83784de2d1cb1cf422583fb0a9]
    
    
    
    
    Former-commit-id: 75162f88310c261d67717f6ad3a8ee2c03998850 [formerly ea01b16f43c51354cb2757f12ec3faf120412907]
    Former-commit-id: d38d363fce4658538b9ee4e9d0ce7e12ce308d61 [formerly 2437242d9157bc58e40720325e4b3ec515cca7c5] [formerly c3557bd096107781640a94223690d5ec846f09e7 [formerly 651fe560d9c5501e1de3aebd5d55d2bccf31415c]]
    Former-commit-id: 4ade9c2f8132a9e6a68bb84f75e4dcd5679b2435 [formerly 7fec9d7e799e5f88e1d0410876a27438412c2c8e]
    Former-commit-id: 93014dd0f84d2cf3ff658a61f761b06bf3251e2d

--- pipeline/cast.go
+++ pipeline/cast.go
@@ -1,6 +1,8 @@
 package pipeline
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -10,19 +12,65 @@ func cast(result interface{}, tInfo string) interface{} {
 		switch v := result.(type) {
 		case bool:
 			return v
-		case int,int32,int64,int8,int16:
+		case int8,int16,int,int32,int64, uint8,uint16,uint,uint32,uint64:
 			return v !=0
-
+		case float32, float64:
+			return v != 0
+		case string:
+            return v != "" && v != "0" && v != "false"
+		default:
+			return nil
 		}
 
 	case "int":
-		return 1
+		switch v := result.(type) {
+		case bool:
+			if v {
+				return 1
+			} else {
+				return 0
+			}
+		case int8,int16,int,int32,int64, uint8,uint16,uint,uint32,uint64:
+			return v
+		case float32:
+			return int64(v)
+		case float64:
+			return int64(v)
+		case string:
+			if intV , err := strconv.ParseInt(v, 64,64); err != nil {
+				l.Error(err)
+				return nil
+			} else {
+				return intV
+			}
+		default:
+			return nil
+		}
 
 	case "float":
-		return 1.1
+		switch v := result.(type) {
+		case bool:
+			if v {
+				return float64(1)
+			} else {
+				return float64(0)
+			}
+		case string:
+			n, _ := strconv.ParseFloat(v, 64)
+			return n
+		case float64,float32:
+			return v
+		case int8,int16,int,int32,int64, uint8,uint16,uint,uint32,uint64:
+			return v
+		}
 
 	case "str":
-		return "result.String()"
+		switch v := result.(type) {
+		case string:
+			return v
+		default:
+			return fmt.Sprintf("%v", v)
+		}
 	}
 
 	return nil
