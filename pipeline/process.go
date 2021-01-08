35d98b3bceb58788ce262b43c1b75f78b47a29b9
huangbo <huangbo@jiagouyun.com>
2021-01-08 18:33:32 +0800

    增加错误返回
    Former-commit-id: 19eced23df16becd79bea17149272d16707b5f43
    Former-commit-id: 3039bc2ba519d4bad790d87ab72987f0e808add6 [formerly d00978d446f6d8d4817ebc9cdd9ce3d1dbdf059e]
    Former-commit-id: 6342d610cb94b62ad7dcf13a7ac322189e22e66a

--- pipeline/process.go
+++ pipeline/process.go
@@ -42,7 +42,7 @@ func (p *Pipeline) Run(data string) *Pipeline {
 			p.lastErr = fmt.Errorf("%v", r)
 		}
 	}()
-	
+
 	var err error
 
 	//防止脚本解析错误
@@ -79,8 +79,8 @@ func (p *Pipeline) Run(data string) *Pipeline {
 	return p
 }
 
-func (p *Pipeline) Result() map[string]interface{} {
-	return p.Output
+func (p *Pipeline) Result() (map[string]interface{}, error) {
+	return p.Output, p.lastErr
 }
 
 func (p *Pipeline) LastError() error {
