1216a06701ad223ae5583ad4da8871638ac9c7fe
huangbo <huangbo@jiagouyun.com>
2021-01-08 17:02:21 +0800

    新增脚本函数
    Former-commit-id: 998d025b15c39d5a24cb7aa17a1abc7c7c35a517 [formerly 8d07fddb42f6ffe6b32cf000a989fc77faa6fdd6]
    Former-commit-id: f507ab2c473b32b8879d884e356df77f4693a72a

--- pipeline/funcs.go
+++ pipeline/funcs.go
@@ -31,6 +31,9 @@ var (
 		"datetime"   :  DateTime,
 		"group"      :  Group,
 		"group_in"   :  GroupIn,
+
+		"uppercase"  :  Uppercase,
+		"lowercase"  :  Lowercase,
 	}
 )
 
@@ -309,6 +312,34 @@ func DebugNodesHelp(f *parser.FuncExpr, prev string) {
 			l.Debugf("%v%v", prev+"    |", node)
 		}
 	}
+}
+
+func Uppercase(p *Pipeline, node parser.Node) (*Pipeline, error) {
+	funcExpr := node.(*parser.FuncExpr)
+	if len(funcExpr.Param) != 1 {
+		return nil, fmt.Errorf("func %s expected 1 args", funcExpr.Name)
+	}
+
+	key := funcExpr.Param[0].(*parser.Identifier).Name
+	v := p.getContentStr(key)
+	v = strings.ToUpper(v)
+	p.setContent(key, v)
+
+	return p, nil
+}
+
+func Lowercase(p *Pipeline, node parser.Node) (*Pipeline, error) {
+	funcExpr := node.(*parser.FuncExpr)
+	if len(funcExpr.Param) != 1 {
+		return nil, fmt.Errorf("func %s expected 1 args", funcExpr.Name)
+	}
+
+	key := funcExpr.Param[0].(*parser.Identifier).Name
+	v := p.getContentStr(key)
+	v = strings.ToLower(v)
+	p.setContent(key, v)
+
+	return p, nil
 }
 
 func getGjsonResult(data, id string) interface{} {
