f3075ce68f6b52eeefba8547f67b721458047716
tanbiao <tanbiao@jiagouyun.com>
2021-01-10 14:11:26 +0800

    update: 	[+] add go fmt
    Former-commit-id: 887cf8251a4197252c2252827ec1b0091d198313
    Former-commit-id: 18007dcb17133d979cdab2d60fcc9bc3f5325e5f [formerly d01dd8ed08c0bd5efdc68ba83c92a7ad7534d7a8]
    Former-commit-id: b83c72dfd9bff2e9fe62fcd7bd8e8c631384a819

--- plugins/inputs/cshark/config.go
+++ plugins/inputs/cshark/config.go
@@ -12,28 +12,28 @@ const (
 
 type Shark struct {
 	MetricName string
-    Interval   string   `toml:"interval"`
-    TsharkPath string `toml:"tsharkPath"`
+	Interval   string `toml:"interval"`
+	TsharkPath string `toml:"tsharkPath"`
 }
 
 type Params struct {
-    Device  string            `json:"device"`
-    Stream  *Stream           `json:"stream"`
-    Statistics *Statistics    `json:"statistics"`
-    Sync     bool             `json:"sync"`
-    Fin      chan error       `json:"-"`
+	Device     string      `json:"device"`
+	Stream     *Stream     `json:"stream"`
+	Statistics *Statistics `json:"statistics"`
+	Sync       bool        `json:"sync"`
+	Fin        chan error  `json:"-"`
 }
 
 type Stream struct {
-	Duration string        `json:"duration"`
-	Count    int           `json:"count"`
-	Protocol string	       `json:"protocol"`
-    Ports     []string     `json:"port"`
-    SrcIPs    []string     `json:"srcIP"`
-    DstIPs    []string     `json:"dstIP"`
-    Filter    string      `json:"filter"`
+	Duration string   `json:"duration"`
+	Count    int      `json:"count"`
+	Protocol string   `json:"protocol"`
+	Ports    []string `json:"port"`
+	SrcIPs   []string `json:"srcIP"`
+	DstIPs   []string `json:"dstIP"`
+	Filter   string   `json:"filter"`
 }
 
 type Statistics struct {
-	Condition  string
+	Condition string
 }
