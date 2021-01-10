a8285ed2757a3a8d0d5ef00e5e086a0dc176f316
huangjinjin <huangjinjin@jiagouyun.com>
2021-01-10 10:51:29 +0800

    aliyun  object
     [formerly 00bf4ece8da3da9342016c1816d039d7a0a2bd83 [formerly 088e4ad27b653dad3605b8ff759a1d04ad374f2b] [formerly 7097bb91f0c528001e96861d95415c208f9d214a [formerly d16aba4cbeb904341607fd8aa1623d293144b5d4]] [formerly b5382709b7d349a4b0945279bff362b7fafbed8f] [formerly 57d275540be399e5b9bdc118a26f57c6d8ba7a25 [formerly 6ec08469e56d85b1bc027236aeab412558791df3] [formerly 57d275540be399e5b9bdc118a26f57c6d8ba7a25 [formerly 6ec08469e56d85b1bc027236aeab412558791df3] [formerly 4824441273c1f1f1b71270f765f1a42f42a4a5d2 [formerly afc862196520b66dd5352b6610dacd045fcf9c0f]]]] [formerly 82c0c91e7d1b2a68ff33ac3276f764e318cc093f] [formerly fa95a8f024f3d4fa24916348e73095c567caf333 [formerly baa1d4f3ad941ce811ccfd7764a4fa744e415dc9] [formerly 3c73fb601e73d446c5088edc0fe3d700f2c45084 [formerly 1eb73114d22a4f8a0c39882ca2c09017c36df2f8] [formerly 3c73fb601e73d446c5088edc0fe3d700f2c45084 [formerly 1eb73114d22a4f8a0c39882ca2c09017c36df2f8] [formerly 3c73fb601e73d446c5088edc0fe3d700f2c45084 [formerly 1eb73114d22a4f8a0c39882ca2c09017c36df2f8] [formerly 72faf6811d1ebdb6b1fa821f26ddd5de1a4f0f19 [formerly 3b10f8fb7fd5991469360a7bb070f41d5221c02d]]]]]]]
     [formerly 82c0c91e7d1b2a68ff33ac3276f764e318cc093f]
    
    
    
    
    Former-commit-id: b4de707c80de63cb2849946590eca5dc23e80f33 [formerly b7a5f1ddfca86af2ccbf68a6ff810fe84f105ffa]
    Former-commit-id: 65888d121a0754f47830032e9a8c5c999c1f7cf9 [formerly 9cbaf0a7b408e2a4b6ed7303727a3cae0dc4eeb9] [formerly 27c6c9075a00ea9d9a024eb12ae08c36070eebe7 [formerly b55f781420a0d32db1ba4afa4ab838a262eb73ae]]
    Former-commit-id: fa88e4a8ed6ff31eb4d1b8421319c900974be83f [formerly 67ddf32a94597cde95d6cca1ebcdf2c1393067a8]
    Former-commit-id: 932b1f8035a9a4b8db5bc985063adaa2c79f2568

--- plugins/inputs/aliyunobject/aliyunobject.go
+++ plugins/inputs/aliyunobject/aliyunobject.go
@@ -9,6 +9,7 @@ import (
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/pipeline"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
@@ -43,15 +44,14 @@ func (_ *objectAgent) Catalog() string {
 	return `aliyun`
 }
 
-func (_ *objectAgent) PipelineConfig() map[string]string{
+func (_ *objectAgent) PipelineConfig() map[string]string {
 	pipelineMap := map[string]string{
-		"aliyun_redis.p":redisPipelineConifg,
-		"aliyun_waf.p":wafPipelineConfig,
-		"aliyun_cdn.p":cdnPipelineConifg,
+		"aliyun_redis.p": redisPipelineConifg,
+		"aliyun_waf.p":   wafPipelineConfig,
+		"aliyun_cdn.p":   cdnPipelineConifg,
 	}
 	return pipelineMap
 }
-
 
 func (ag *objectAgent) Test() (*inputs.TestResult, error) {
 	ag.mode = "test"
@@ -131,23 +131,32 @@ func newAgent() *objectAgent {
 	return ag
 }
 
-
-func parseObject(obj interface{},class,id,pipelinePath string,blacklist,whitelist []string) {
+func (ag *objectAgent) parseObject(obj interface{}, class, id string, pipeline *pipeline.Pipeline, blacklist, whitelist []string, tags map[string]string) {
 	if datakit.CheckExcluded(id, blacklist, whitelist) {
 		return
 	}
-	data,err := json.Marshal(obj)
+	data, err := json.Marshal(obj)
 	if err != nil {
 		moduleLogger.Errorf("[error] json marshal err:%s", err.Error())
 		return
 	}
-	tags := map[string]string{
-		"class":class,
+	if tags == nil {
+		tags = map[string]string{}
 	}
-
-	fields := inputs.RunPipeline(string(data),pipelinePath)
+	for k, v := range ag.Tags {
+		if _, ok := tags[k]; ok {
+			continue
+		} else {
+			tags[k] = v
+		}
+	}
+	fields := pipeline.Run(string(data)).Result()
 	fields["content"] = string(data)
-	io.NamedFeedEx(inputName,io.Object,class,tags,fields,time.Now().UTC())
+
+	tags["class"] = class
+	tags["name"] = fields["name"].(string)
+
+	io.NamedFeedEx(inputName, io.Object, class, tags, fields, time.Now().UTC())
 }
 
 func init() {
