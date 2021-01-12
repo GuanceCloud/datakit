c455b76bcf94d9555d3e35874f9122c6bdf5ed8a
wuqinchao <wuqinchao@jiagouyun.com>
2021-01-12 13:32:52 +0800

    fix huaweiyunces
     [formerly 91c7465ca04ffa789485fec205ccdd2dc19cd537 [formerly 21d579bc92253ddd695b2a9f53bb2d148838362a] [formerly 91c7465ca04ffa789485fec205ccdd2dc19cd537 [formerly 21d579bc92253ddd695b2a9f53bb2d148838362a] [formerly 91c7465ca04ffa789485fec205ccdd2dc19cd537 [formerly 21d579bc92253ddd695b2a9f53bb2d148838362a] [formerly 477a329326a2b44b03efabefe884bae6b1278770 [formerly 04e39c7bda25ce6cbcda31351afbc96759fc1d5f]]]]]
    
    
    
    Former-commit-id: ec37e18a699087893aca9d02f92941ed6105f52b [formerly 88e463a688f9ca55fecc2c47e6bc1ae913e0ba30]
    Former-commit-id: 52a7ab9e057e44b5d36d0cabc3827caf21e0343e [formerly ac93892e97a764ddd69aaeb23e099974fadcfbe8] [formerly b43eec3706ffdf1abb7ed24669e4e3a020175d7a [formerly 33ebe8180764275a52a14cceb9b5df27403511b4]]
    Former-commit-id: 2bdd78fd608ba4d5984a35fd58cf6e8696692a19 [formerly 552c208d5379d7571d8514a1997d4a9d6f24c17b]
    Former-commit-id: 60a37d426cd04239f7f6807f6d1d1375c7870fe3

--- plugins/inputs/huaweiyunces/huaweiyun_test.go
+++ plugins/inputs/huaweiyunces/huaweiyun_test.go
@@ -9,45 +9,55 @@ import (
 	"time"
 
 	"github.com/influxdata/toml"
+	"github.com/influxdata/toml/ast"
 
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/sdk/huaweicloud"
 )
 
-var (
-	ak = ``
-	sk = ``
-
-	endPoint  = `ces.cn-east-3.myhuaweicloud.com`
-	projectID = `0838fedce480f3982f39c0150293ac02`
-
-	testInput = false
-)
+func genAgent() *agent {
+	ag := loadCfg("test.conf").(*agent)
+	return ag
+}
 
 func TestGetMetric(t *testing.T) {
 
 	//https://support.huaweicloud.com/api-ces/ces_03_0033.html
 
-	cli := huaweicloud.NewHWClient(ak, sk, endPoint, projectID, moduleLogger)
+	moduleLogger = logger.DefaultSLogger(inputName)
+
+	ag := genAgent()
+
+	cli := huaweicloud.NewHWClient(ag.AccessKeyID, ag.AccessKeySecret, ag.EndPoint, ag.ProjectID, moduleLogger)
+
 	dims := []*Dimension{
 		{
 			Name:  "instance_id",
-			Value: "b5d7b7a3-681d-4c08-8e32-f14b640b3e12",
+			Value: "9a4d6fb6-4de2-422a-b4d3-5d436b79ef09",
 		},
 	}
 	dms := []string{}
 	for _, d := range dims {
 		dms = append(dms, fmt.Sprintf("%s,%s", d.Name, d.Value))
 	}
-	resp, err := cli.CESGetMetric("SYS.ECS", "cpu_util", "min", 300, time.Now().Add(-5*time.Minute).Unix()*1000, time.Now().Unix()*1000, dms)
+
+	now := time.Now().Truncate(time.Minute)
+	from := now.Add(-30*time.Minute).Unix() * 1000
+	to := now.Unix() * 1000
+	resp, err := cli.CESGetMetric("SYS.ECS", "cpu_util", "min", 300, from, to, dms)
 	if err != nil {
 		t.Error(err)
 	}
-	log.Printf("%v", resp)
+	log.Printf("%s", string(resp))
 }
 
 func TestBatchMetrics(t *testing.T) {
 
-	cli := huaweicloud.NewHWClient(ak, sk, endPoint, projectID, moduleLogger)
+	moduleLogger = logger.DefaultSLogger(inputName)
+	ag := genAgent()
+
+	cli := huaweicloud.NewHWClient(ag.AccessKeyID, ag.AccessKeySecret, ag.EndPoint, ag.ProjectID, moduleLogger)
 
 	dims := []*Dimension{
 		{
@@ -92,15 +102,83 @@ func TestBatchMetrics(t *testing.T) {
 
 func TestInput(t *testing.T) {
 
-	testInput = true
-
-	data, err := ioutil.ReadFile("test.conf")
-	if err != nil {
-		t.Error(err)
-	}
-	ag := newAgent()
-	if err = toml.Unmarshal(data, &ag); err != nil {
-		t.Error(err)
-	}
+	ag := genAgent()
+	ag.mode = "debug"
 	ag.Run()
+}
+
+func loadCfg(file string) inputs.Input {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalf("read config file failed: %s", err.Error())
+		return nil
+	}
+
+	tbl, err := toml.Parse(data)
+	if err != nil {
+		log.Fatalf("parse toml file failed, %s", err)
+		return nil
+	}
+
+	creator := func() inputs.Input {
+		return newAgent()
+	}
+
+	for field, node := range tbl.Fields {
+
+		switch field {
+		case "inputs":
+			stbl, ok := node.(*ast.Table)
+			if !ok {
+				log.Fatalf("ignore bad toml node")
+			} else {
+				for inputName, v := range stbl.Fields {
+					input, err := tryUnmarshal(v, inputName, creator)
+					if err != nil {
+						log.Fatalf("unmarshal input %s failed: %s", inputName, err.Error())
+						return nil
+					}
+					return input
+				}
+			}
+
+		default: // compatible with old version: no [[inputs.xxx]] header
+			input, err := tryUnmarshal(node, "aa", creator)
+			if err != nil {
+				log.Fatalf("unmarshal input failed: %s", err.Error())
+				return nil
+			}
+			return input
+		}
+	}
+
+	return nil
+}
+
+func tryUnmarshal(tbl interface{}, name string, creator inputs.Creator) (inputs.Input, error) {
+
+	tbls := []*ast.Table{}
+
+	switch t := tbl.(type) {
+	case []*ast.Table:
+		tbls = tbl.([]*ast.Table)
+	case *ast.Table:
+		tbls = append(tbls, tbl.(*ast.Table))
+	default:
+		err := fmt.Errorf("invalid toml format: %v", t)
+		return nil, err
+	}
+
+	for _, t := range tbls {
+		input := creator()
+
+		err := toml.UnmarshalTable(t, input)
+		if err != nil {
+			log.Fatalf("toml unmarshal failed: %v", err)
+			return nil, err
+		}
+		return input, nil
+
+	}
+	return nil, nil
 }
