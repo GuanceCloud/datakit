4a85a5dce881bb2454a0d151e648308e70ac2411
wuqinchao <wuqinchao@jiagouyun.com>
2021-01-13 10:13:33 +0800

    update
    Former-commit-id: 0c2f0233e18c3a7e396dc92295b89385a9d4c93c [formerly 0de663812a04009c341adb5447910c0c665baf16] [formerly 9c4889840b22e12c7b742d2460b05b8fe139b629 [formerly 4048c57f647af26cf2d6154b2f894c2b80a68dd5]]
    Former-commit-id: 622ee382a4937d28b9e77ab78e6573e7857efb0d [formerly 93e079ba41fa112f318ce4b9119a7c0c0c4f0b26]
    Former-commit-id: d15c76e4480a090bbfcdddd2f5e78747721b11d0

--- plugins/inputs/hostobject/cfg.go
+++ plugins/inputs/hostobject/cfg.go
@@ -5,21 +5,12 @@ const (
 
 	sampleConfig = `
 #[inputs.hostobject]
-# ##(optional) default use host name
-#name = ''
-
-# ##(optional) default is Servers
-#class = 'Servers'
 
 # ##(optional) collect interval, default is 3 miniutes
 #interval = '3m'
 
 # ##(optional) 
 #pipeline = ''
-
-# ##(optional) custom tags
-#[inputs.hostobject.tags]
-# key1 = 'val1'
 `
 
 	pipelineSample = ``
