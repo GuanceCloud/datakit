91a3a0a98587e5be0f3cb7cc53c866f39f040c97
liguozhuang <liguozhuang@jiagouyun.com>
2021-01-14 18:15:05 +0800

    update: 	[+] 添加nginxlog 	[*] 修复prom潜在问题
    Former-commit-id: 73406d2d1ef677208acc2939f7478db441aa60cc

--- plugins/inputs/expressjs/expressjs.go
+++ plugins/inputs/expressjs/expressjs.go
@@ -1,8 +1,6 @@
 package expressjs
 
 import (
-	ifxcli "github.com/influxdata/influxdb1-client/v2"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/prom"
 )
@@ -11,6 +9,7 @@ const (
 	inputName = "expressjs"
 
 	sampleCfg = `
+[[inputs.prom]]
     # expressjs metrics from http(https)://HOST:PORT/metrics
     # usually modify host and port
     # required
@@ -26,30 +25,14 @@ const (
     # tls_cert = "/tmp/peer.crt"
     # tls_key = "/tmp/peer.key"
 
-    ## Internal configuration. Don't modify.
-    name = "expressjs"
-    ## ignore_measurement = ['nodejs_version_info']
-
     # [inputs.prom.tags]
-    # from = "127.0.0.1:2379"
+    # from = "127.0.0.1:3000"
     # tags1 = "value1"
 `
 )
 
 func init() {
 	inputs.Add(inputName, func() inputs.Input {
-		return &prom.Prom{
-			Interval:       datakit.Cfg.MainCfg.Interval,
-			InputName:      inputName,
-			CatalogStr:     inputName,
-			SampleCfg:      sampleCfg,
-			Tags:           make(map[string]string),
-			IgnoreFunc:     ignore,
-			PromToNameFunc: nil,
-		}
+		return prom.NewProm(inputName, inputName, sampleCfg, nil)
 	})
-}
-
-func ignore(pt *ifxcli.Point) bool {
-	return false
 }
