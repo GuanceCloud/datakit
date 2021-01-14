2a88185f28d0ea56384c20fc19b593606555b57f
liguozhuang <liguozhuang@jiagouyun.com>
2021-01-14 18:15:05 +0800

    update: 	[+] 添加nginxlog 	[*] 修复prom潜在问题
    Former-commit-id: 70807292ad1ba58bf352378328fa0bcbbb8aebfe
    Former-commit-id: 32ab68438a1afc67b09bee14348d9318e039589d [formerly 2d57c0a4bc5ebfe860356e6276f6948bfeb9aee9]
    Former-commit-id: f58b1f19e13bb87e9bd1f8e33962d263201cc1f1

--- plugins/inputs/tailf/files.go
+++ plugins/inputs/tailf/files.go
@@ -5,11 +5,14 @@ import (
 	"github.com/mattn/go-zglob"
 )
 
-func getFileList(filesGlob, ignoreGlob []string) []string {
+// FIXME:
+// 可以不作为 Tailf 成员函数，仅仅是为了打印那两条 Warnning
+
+func (t *Tailf) getFileList(filesGlob, ignoreGlob []string) []string {
 	var matches = make(map[string]interface{})
 
-	files := fileList(filesGlob)
-	ignores := ignoreList(ignoreGlob)
+	files := t.fileList(filesGlob)
+	ignores := t.ignoreList(ignoreGlob)
 
 	for _, f := range files {
 		for _, g := range ignores {
@@ -30,12 +33,12 @@ func getFileList(filesGlob, ignoreGlob []string) []string {
 	return list
 }
 
-func fileList(filesGlob []string) []string {
+func (t *Tailf) fileList(filesGlob []string) []string {
 	var matches []string
 	for _, f := range filesGlob {
 		matche, err := zglob.Glob(f)
 		if err != nil {
-			l.Warnf("logfile %s, %s", f, err)
+			t.log.Warnf("logfile %s, %s", f, err)
 			continue
 		}
 		matches = append(matches, matche...)
@@ -43,12 +46,12 @@ func fileList(filesGlob []string) []string {
 	return matches
 }
 
-func ignoreList(ignoreGlob []string) []glob.Glob {
+func (t *Tailf) ignoreList(ignoreGlob []string) []glob.Glob {
 	var globs []glob.Glob
 	for _, ig := range ignoreGlob {
 		g, err := glob.Compile(ig)
 		if err != nil {
-			l.Errorf("ignore glob, %s", err)
+			t.log.Errorf("ignore glob, %s", err)
 			continue
 		}
 		globs = append(globs, g)
