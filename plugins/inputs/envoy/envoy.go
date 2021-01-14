af02e932cb79212b29a449710da2a3b0b6b40c04
liguozhuang <liguozhuang@jiagouyun.com>
2021-01-14 18:15:05 +0800

    update: 	[+] 添加nginxlog 	[*] 修复prom潜在问题
     [formerly 0c5e8eef99f08776dbe3af9d16a5a05bd30037a6 [formerly 13f512cdd9cf77bd0d667b218b96268de071a58a]]
    
    
    
    Former-commit-id: c2102d0ca6dadf61b4c673684c3882aef6f0758f

--- plugins/inputs/envoy/envoy.go
+++ plugins/inputs/envoy/envoy.go
@@ -5,7 +5,6 @@ import (
 
 	ifxcli "github.com/influxdata/influxdb1-client/v2"
 
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/prom"
 )
@@ -38,15 +37,7 @@ const (
 
 func init() {
 	inputs.Add(inputName, func() inputs.Input {
-		return &prom.Prom{
-			Interval:       datakit.Cfg.MainCfg.Interval,
-			InputName:      inputName,
-			CatalogStr:     inputName,
-			SampleCfg:      sampleCfg,
-			Tags:           make(map[string]string),
-			IgnoreFunc:     ignore,
-			PromToNameFunc: nil,
-		}
+		return prom.NewProm(inputName, inputName, sampleCfg, ignore)
 	})
 }
 
