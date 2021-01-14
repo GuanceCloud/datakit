b0d1429c8c4b96dc489073952ccff16c3ded9234
liushaobo <liushaobo@jiagouyun.com>
2021-01-14 15:00:18 +0800

    语法扩展
     [formerly a4ff08db5f44ba6ae74c273f86ebe473b607f652 [formerly a38dcbf8cb723490665e07b25586dfc25e25935c] [formerly a4ff08db5f44ba6ae74c273f86ebe473b607f652 [formerly a38dcbf8cb723490665e07b25586dfc25e25935c] [formerly a4ff08db5f44ba6ae74c273f86ebe473b607f652 [formerly a38dcbf8cb723490665e07b25586dfc25e25935c] [formerly 97bef319741747fb9d989de5cfdf77f3543f6510 [formerly 6efa3e3b44b01ef82abdcd72434dcb1e018f82e4]]]]]
    
    
    
    Former-commit-id: a334ac76e5e13d5e7586d53539c6630ebd17adc2 [formerly 5831a96a85c190a0e28da46a40b01f161d4e2324]
    Former-commit-id: 9fe6714e78e32731b9b866752f192317de5b2163 [formerly 6ffbb9cc5de868e383d951514eaf8ff1d7dcd370] [formerly d298a270192ef9bf7472de436d8c6c424f3d7762 [formerly 2df2d63c95edba5db5b5022aeabef3181bab8c17]]
    Former-commit-id: 2d0234ada0500fdf8edb0f12db6c8e523102c791 [formerly 2267c86cfd300256b3d3f0339d6c18cdc0e0e1fb]
    Former-commit-id: cf6f332632a37858a06e748f1e96caf04358f554

--- pipeline/funcs_test.go
+++ pipeline/funcs_test.go
@@ -4,10 +4,6 @@ import (
 	"testing"
 )
 
-
-
-
-
 func assertEqual(t *testing.T, a, b interface{}) {
 	t.Helper()
 	if a != b {
@@ -66,47 +62,32 @@ grok(_, "%{time}")`
 	second, _ := p.getContentStr("second")
 	assertEqual(t, second, "14")
 }
-//
-//func TestRenameFunc(t *testing.T) {
-//	script := `
-//add_pattern("_second", "(?:(?:[0-5]?[0-9]|60)(?:[:.,][0-9]+)?)");
-//add_pattern("_minute", "(?:[0-5][0-9])");
-//add_pattern("_hour", "(?:2[0123]|[01]?[0-9])");
-//add_pattern("time", "([^0-9]?)%{_hour:hour}:%{_minute:minute}(?::%{_second:second})([^0-9]?)");
-//grok(_, "%{time}");
-//rename(newhour, hour)`
-//
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run("12:13:14")
-//	assertEqual(t, p.lastErr, nil)
-//
-//	r := p.getContentStr("newhour")
-//	assertEqual(t, r, "12")
-//}
-//
-//func TestExprFunc(t *testing.T) {
-//
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
-//	script := `json(_, a.second);
-//cast(a.second, "int");
-//expr(a.second*10+(2+3)*5, bb);
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//	assertEqual(t, p.lastErr, nil)
-//
-//	assertEqual(t, p.getContentStr("bb"), "45")
-//}
 
-func TestDefaultTimeFunc(t *testing.T) {
+func TestRenameFunc(t *testing.T) {
+	script := `
+add_pattern("_second", "(?:(?:[0-5]?[0-9]|60)(?:[:.,][0-9]+)?)")
+add_pattern("_minute", "(?:[0-5][0-9])")
+add_pattern("_hour", "(?:2[0123]|[01]?[0-9])")
+add_pattern("time", "([^0-9]?)%{_hour:hour}:%{_minute:minute}(?::%{_second:second})([^0-9]?)")
+grok(_, "%{time}")
+rename(newhour, hour)`
 
-	js := `{"a":{"time":"2014/04/08 22:05","second":2,"thrid":"abc","forth":true},"age":47}`
-	script := `json(_, a.time);
-default_time(a.second);
+	p, err := NewPipeline(script)
+	assertEqual(t, err, nil)
+
+	p.Run("12:13:14")
+	assertEqual(t, p.lastErr, nil)
+
+	r, _ := p.getContentStr("newhour")
+	assertEqual(t, r, "12")
+}
+
+func TestExprFunc(t *testing.T) {
+
+	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
+	script := `json(_, a.second)
+cast(a.second, "int")
+expr(a.second*10+(2+3)*5, bb)
 `
 	p, err := NewPipeline(script)
 	assertEqual(t, err, nil)
@@ -114,41 +95,54 @@ default_time(a.second);
 	p.Run(js)
 	assertEqual(t, p.lastErr, nil)
 
-	r, err := p.getContent("a.time")
-	assertEqual(t, err, nil)
-
-	assertEqual(t, r, 1396965900)
+	v, _ := p.getContentStr("bb")
+	assertEqual(t, v, "45")
 }
 
-func TestUrlencodeFunc(t *testing.T) {
-	js := `{"url":"http%3A%2F%2Fwww.baidu.com%2Fs%3Fwd%3D%E8%87%AA%E7%94%B1%E5%BA%A6","second":2}`
-	script := `json(_, url); url_decode(url);`
-
-	p, err := NewPipeline(script)
-	assertEqual(t, err, nil)
-
-	p.Run(js)
-
-	r, err := p.getContentStr("url")
-	assertEqual(t, err, nil)
-
-	assertEqual(t, r, "http://www.baidu.com/s?wd=自由度")
-}
-
-func TestGeoIpFunc(t *testing.T) {
-	js := `{"a":{"ip":"116.228.89.206", "second":2,"thrid":"abc","forth":true},"age":47}`
-	script := `json(_, a.ip); geoip(a.ip);`
-
-	p, err := NewPipeline(script)
-	assertEqual(t, err, nil)
-
-	p.Run(js)
-
-	r, err := p.getContentStr("city")
-	assertEqual(t, err, nil)
-	assertEqual(t, r, "Shanghai")
-}
-
+//func TestDefaultTimeFunc(t *testing.T) {
+//
+//	js := `{"a":{"time":"2014/04/08 22:05","second":2,"thrid":"abc","forth":true},"age":47}`
+//	script := `json(_, a.time);
+//default_time(a.second);
+//`
+//	p, err := NewPipeline(script)
+//	assertEqual(t, err, nil)
+//
+//	p.Run(js)
+//	assertEqual(t, p.lastErr, nil)
+//
+//	r := p.getContent("a.time")
+//
+//	assertEqual(t, r, 1396965900)
+//}
+//
+//func TestUrlencodeFunc(t *testing.T) {
+//	js := `{"url":"http%3A%2F%2Fwww.baidu.com%2Fs%3Fwd%3D%E8%87%AA%E7%94%B1%E5%BA%A6","second":2}`
+//	script := `json(_, url); url_decode(url);`
+//
+//	p, err := NewPipeline(script)
+//	assertEqual(t, err, nil)
+//
+//	p.Run(js)
+//
+//	r := p.getContentStr("url")
+//
+//	assertEqual(t, r, "http://www.baidu.com/s?wd=自由度")
+//}
+//
+//func TestGeoIpFunc(t *testing.T) {
+//	js := `{"a":{"ip":"116.228.89.206", "second":2,"thrid":"abc","forth":true},"age":47}`
+//	script := `json(_, a.ip); geoip(a.ip);`
+//
+//	p, err := NewPipeline(script)
+//	assertEqual(t, err, nil)
+//
+//	p.Run(js)
+//
+//	r := p.getContentStr("city")
+//	assertEqual(t, r, "Shanghai")
+//}
+//
 //func TestUserAgentFunc(t *testing.T) {
 //	js := `{"a":{"userAgent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36","second":2},"age":47}`
 //	script := `json(_, a.userAgent); user_agent(a.userAgent)`
@@ -192,7 +186,7 @@ func TestGeoIpFunc(t *testing.T) {
 //
 //	assertEqual(t, r, "ok")
 //}
-//
+
 //func TestGroupInFunc(t *testing.T) {
 //	js := `{"a":{"status": "test","age":"47"}`
 //	script := `json(_, a.status); group_in(a.status, [200, 47, "test"], "ok", newkey);`
@@ -205,96 +199,107 @@ func TestGeoIpFunc(t *testing.T) {
 //	r := p.getContent("newkey")
 //	assertEqual(t, r, "ok")
 //}
-//
-//func TestCastFloat2IntFunc(t *testing.T) {
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
-//	script := `json(_, a.first);
-//cast(a.first, "int");
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//
-//	assertEqual(t, p.getContentStr("a.first"), "2")
-//}
-//
-//func TestCastInt2FloatFunc(t *testing.T) {
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
-//	script := `json(_, a.second);
-//cast(a.second, "float");
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//	assertEqual(t, p.getContentStr("a.second"), "2")
-//}
-//
-//func TestStringfFunc(t *testing.T) {
-//	//js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
-//	//script := `stringf(bb, "%d %s %v", a.second, a.thrid, a.forth);`
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
-//	script := `json(_, a.second);
-//json(_, a.thrid);
-//json(_, a.forth);
-//strfmt(bb, "%d %s %v", a.second, a.thrid, a.forth);
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//	assertEqual(t, p.getContent("bb"), "2 abc true")
-//}
-//
-//func TestUppercaseFunc(t *testing.T) {
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
-//	script := `json(_, a.thrid);
-//uppercase(a.thrid);
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//	assertEqual(t, p.getContentStr("a.thrid"), "ABC")
-//}
-//
-//func TestLowercaseFunc(t *testing.T) {
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"aBC","forth":true},"age":47}`
-//	script := `json(_, a.thrid);
-//lowercase(a.thrid);
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//	assertEqual(t, p.getContentStr("a.thrid"), "abc")
-//}
-//
-//func TestAddkeyFunc(t *testing.T) {
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"aBC","forth":true},"age":47}`
-//	script := `add_key(aa, 3);
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//	assertEqual(t, p.getContentStr("aa"), "3")
-//}
-//
-//func TestDropkeyFunc(t *testing.T) {
-//	js := `{"a":{"first":2.3,"second":2,"thrid":"aBC","forth":true},"age":47}`
-//	script := `json(_, a.thrid);
-//json(_, a.first);
-//drop_key(a.thrid)
-//`
-//	p, err := NewPipeline(script)
-//	assertEqual(t, err, nil)
-//
-//	p.Run(js)
-//	assertEqual(t, p.getContentStr("a.first"), "2.3")
-//}
-//
+
+func TestCastFloat2IntFunc(t *testing.T) {
+	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
+	script := `json(_, a.first)
+cast(a.first, "int")
+`
+	p, err := NewPipeline(script)
+	assertEqual(t, err, nil)
+
+	p.Run(js)
+	v, _ := p.getContentStr("a.first")
+
+	assertEqual(t, v, "2")
+}
+
+func TestCastInt2FloatFunc(t *testing.T) {
+	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
+	script := `json(_, a.second)
+cast(a.second, "float")
+`
+	p, err := NewPipeline(script)
+	assertEqual(t, err, nil)
+
+	p.Run(js)
+
+	v, _ := p.getContentStr("a.second")
+	assertEqual(t, v, "2")
+}
+
+func TestStringfFunc(t *testing.T) {
+	//js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
+	//script := `stringf(bb, "%d %s %v", a.second, a.thrid, a.forth);`
+	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
+	script := `json(_, a.second)
+json(_, a.thrid)
+cast(a.second, "int")
+json(_, a.forth)
+strfmt(bb, "%d %s %v", a.second, a.thrid, a.forth)
+`
+	p, err := NewPipeline(script)
+	assertEqual(t, err, nil)
+
+	p.Run(js)
+	v, _ := p.getContent("bb")
+	assertEqual(t, v, "2 \"abc\" true")
+}
+
+func TestUppercaseFunc(t *testing.T) {
+	js := `{"a":{"first":2.3,"second":2,"thrid":"abc","forth":true},"age":47}`
+	script := `json(_, a.thrid)
+uppercase(a.thrid)
+`
+	p, err := NewPipeline(script)
+	assertEqual(t, err, nil)
+
+	p.Run(js)
+
+	v, _ := p.getContent("a.thrid")
+	assertEqual(t, v, "\"ABC\"")
+}
+
+func TestLowercaseFunc(t *testing.T) {
+	js := `{"a":{"first":2.3,"second":2,"thrid":"aBC","forth":true},"age":47}`
+	script := `json(_, a.thrid)
+lowercase(a.thrid)
+`
+	p, err := NewPipeline(script)
+	t.Log(err)
+	assertEqual(t, err, nil)
+
+	p.Run(js)
+	v, _ := p.getContentStr("a.thrid")
+	assertEqual(t, v, "\"abc\"")
+}
+
+func TestAddkeyFunc(t *testing.T) {
+	js := `{"a":{"first":2.3,"second":2,"thrid":"aBC","forth":true},"age":47}`
+	script := `add_key(aa, 3)
+`
+	p, err := NewPipeline(script)
+	assertEqual(t, err, nil)
+
+	p.Run(js)
+	v, _ := p.getContentStr("aa")
+	assertEqual(t, v, "3")
+}
+
+func TestDropkeyFunc(t *testing.T) {
+	js := `{"a":{"first":2.3,"second":2,"thrid":"aBC","forth":true},"age":47}`
+	script := `json(_, a.thrid)
+json(_, a.first)
+drop_key(a.thrid)
+`
+	p, err := NewPipeline(script)
+	assertEqual(t, err, nil)
+
+	p.Run(js)
+	v, _ := p.getContentStr("a.first")
+	assertEqual(t, v, "2.3")
+}
+
 //func TestNullIfFunc(t *testing.T) {
 //	js := `{"a":{"first":2.3,"second":2,"thrid":"aBC","forth":true},"age":47}`
 //	script := `json(_, a.first);
