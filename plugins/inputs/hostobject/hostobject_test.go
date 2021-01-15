2a6cf4318915fb0c71c3861c40905fa8f8a24707
wuqinchao <wuqinchao@jiagouyun.com>
2021-01-15 16:14:57 +0800

    hostobject update
     [formerly 038d89096ad51b88a9b7526e96a406e57c95633f] [formerly ad587a81f73f5baac711ac45d03a015b1ac6cf69 [formerly 7babc6250da67655e2f4b90b4519d438704fb503] [formerly ec324594cbf6e981f32ced9c097c848c91cdd1d3 [formerly a4b23020d9b8aed910e37f777478cc250388d533] [formerly ec324594cbf6e981f32ced9c097c848c91cdd1d3 [formerly a4b23020d9b8aed910e37f777478cc250388d533] [formerly ec324594cbf6e981f32ced9c097c848c91cdd1d3 [formerly a4b23020d9b8aed910e37f777478cc250388d533] [formerly b48bfce2a848eaaf02562bd00f524a5c40b5449c [formerly 90f95ed66f2111575bcdf6747f68d5a7ffeb0a5c]]]]]]
     [formerly 9ace9009586814ed7dfc0f03d3493c5216a99144 [formerly d68a1aa06b6ef806052d82a7bf758302336027e4] [formerly 9ace9009586814ed7dfc0f03d3493c5216a99144 [formerly d68a1aa06b6ef806052d82a7bf758302336027e4] [formerly 9ace9009586814ed7dfc0f03d3493c5216a99144 [formerly d68a1aa06b6ef806052d82a7bf758302336027e4] [formerly 0a6fce79beab76d10ec742cbad23df3671f56f00 [formerly 14a9e8b43dfeb63aa3690125d3d9399f93fea3cf]]]]]
    
    
    
    Former-commit-id: 4bcdffb694b458c6dd1bc26ce631980b3f4ebd84 [formerly e03e68ed9e3693b4f016db3253adcad564f447dd]
    Former-commit-id: 9183cb8f360918921439c42e18f02a8ba31e8841 [formerly 32bbab81a5e38e4457e34c4d2e6e2fcd5d4a1464] [formerly 9e3859571d835e1164cdb5547f8bf6febff3be3d [formerly 1b8d50d58c827670df077a348f5e194b520dd5c9]]
    Former-commit-id: 87c35f8df1df4390ff9003236c2c9ebf598d90b5 [formerly 638c87fe9b0dae8f5f1d805b32ff599b563a37b3]
    Former-commit-id: 6b510d9425d094eee406c62ee4616a66b740a9d9

--- plugins/inputs/hostobject/hostobject_test.go
+++ plugins/inputs/hostobject/hostobject_test.go
@@ -1,54 +1,13 @@
 package hostobject
 
 import (
+	"encoding/json"
 	"log"
-	"net"
 	"testing"
 )
 
-func TestOSDetail(t *testing.T) {
-
-	oi := getOSInfo()
-	log.Printf("%s", oi.Release)
-
-}
-
-func TestGetIP(t *testing.T) {
-	conn, err := net.Dial("udp", "114.114.114.114:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	log.Printf("%s", localAddr.IP)
-}
-
-func TestListInterface(t *testing.T) {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		t.Error(err)
-	}
-	for _, i := range ifaces {
-		log.Printf("mac: %s", i.HardwareAddr.String())
-
-		addrs, err := i.Addrs()
-		if err != nil {
-			t.Error(err)
-		}
-
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-				if ip.To4() != nil {
-					log.Printf("%s", ip)
-				}
-			case *net.IPAddr:
-				ip = v.IP
-			}
-		}
-	}
+func TestMessageObj(t *testing.T) {
+	msg := getHostObjectMessage()
+	msgdata, _ := json.Marshal(msg)
+	log.Printf("%s", string(msgdata))
 }
