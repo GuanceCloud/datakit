4438a3582e2dba10f0110ea49919d357554f3659
wuqinchao <wuqinchao@jiagouyun.com>
2021-01-19 10:57:53 +0800

    .
    Former-commit-id: a1d8ffd3bd78fd1e6eab9e3940c364c3c5fe8e12 [formerly 3247f1993b69f5e615b22e39df6886b8c6c3123b [formerly 116fc6edd226019a70615f6f47aba75b9b481b36] [formerly 70e81b828c304c0875b4db857e116a820f0b878f [formerly 2110011f1c14e69b36acf0cf5368482681262175]]]
    Former-commit-id: 32f5b1b0ddb2e415198b0a30650b49f0f945767e
    Former-commit-id: 988df8987ef0b18513df39c7d0bf7082eaa05aa6 [formerly 5447845cf05b81af93654a3a44b77f6944d27491]
    Former-commit-id: b48b532b4d07ee904706df62b373ed0bda0c98d3

---
+++ vendor/github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/monitor/v20180724/client.go
@@ -0,0 +1,527 @@
+// Copyright (c) 2017-2018 THL A29 Limited, a Tencent company. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v20180724
+
+import (
+    "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+    tchttp "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/http"
+    "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+)
+
+const APIVersion = "2018-07-24"
+
+type Client struct {
+    common.Client
+}
+
+// Deprecated
+func NewClientWithSecretId(secretId, secretKey, region string) (client *Client, err error) {
+    cpf := profile.NewClientProfile()
+    client = &Client{}
+    client.Init(region).WithSecretId(secretId, secretKey).WithProfile(cpf)
+    return
+}
+
+func NewClient(credential *common.Credential, region string, clientProfile *profile.ClientProfile) (client *Client, err error) {
+    client = &Client{}
+    client.Init(region).
+        WithCredential(credential).
+        WithProfile(clientProfile)
+    return
+}
+
+
+func NewBindingPolicyObjectRequest() (request *BindingPolicyObjectRequest) {
+    request = &BindingPolicyObjectRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "BindingPolicyObject")
+    return
+}
+
+func NewBindingPolicyObjectResponse() (response *BindingPolicyObjectResponse) {
+    response = &BindingPolicyObjectResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 将告警策略绑定到特定对象
+func (c *Client) BindingPolicyObject(request *BindingPolicyObjectRequest) (response *BindingPolicyObjectResponse, err error) {
+    if request == nil {
+        request = NewBindingPolicyObjectRequest()
+    }
+    response = NewBindingPolicyObjectResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewCreatePolicyGroupRequest() (request *CreatePolicyGroupRequest) {
+    request = &CreatePolicyGroupRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "CreatePolicyGroup")
+    return
+}
+
+func NewCreatePolicyGroupResponse() (response *CreatePolicyGroupResponse) {
+    response = &CreatePolicyGroupResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 增加策略组
+func (c *Client) CreatePolicyGroup(request *CreatePolicyGroupRequest) (response *CreatePolicyGroupResponse, err error) {
+    if request == nil {
+        request = NewCreatePolicyGroupRequest()
+    }
+    response = NewCreatePolicyGroupResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDeletePolicyGroupRequest() (request *DeletePolicyGroupRequest) {
+    request = &DeletePolicyGroupRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DeletePolicyGroup")
+    return
+}
+
+func NewDeletePolicyGroupResponse() (response *DeletePolicyGroupResponse) {
+    response = &DeletePolicyGroupResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 删除告警策略组
+func (c *Client) DeletePolicyGroup(request *DeletePolicyGroupRequest) (response *DeletePolicyGroupResponse, err error) {
+    if request == nil {
+        request = NewDeletePolicyGroupRequest()
+    }
+    response = NewDeletePolicyGroupResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeAccidentEventListRequest() (request *DescribeAccidentEventListRequest) {
+    request = &DescribeAccidentEventListRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribeAccidentEventList")
+    return
+}
+
+func NewDescribeAccidentEventListResponse() (response *DescribeAccidentEventListResponse) {
+    response = &DescribeAccidentEventListResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取平台事件列表
+func (c *Client) DescribeAccidentEventList(request *DescribeAccidentEventListRequest) (response *DescribeAccidentEventListResponse, err error) {
+    if request == nil {
+        request = NewDescribeAccidentEventListRequest()
+    }
+    response = NewDescribeAccidentEventListResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeBaseMetricsRequest() (request *DescribeBaseMetricsRequest) {
+    request = &DescribeBaseMetricsRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribeBaseMetrics")
+    return
+}
+
+func NewDescribeBaseMetricsResponse() (response *DescribeBaseMetricsResponse) {
+    response = &DescribeBaseMetricsResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取基础指标详情
+func (c *Client) DescribeBaseMetrics(request *DescribeBaseMetricsRequest) (response *DescribeBaseMetricsResponse, err error) {
+    if request == nil {
+        request = NewDescribeBaseMetricsRequest()
+    }
+    response = NewDescribeBaseMetricsResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeBasicAlarmListRequest() (request *DescribeBasicAlarmListRequest) {
+    request = &DescribeBasicAlarmListRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribeBasicAlarmList")
+    return
+}
+
+func NewDescribeBasicAlarmListResponse() (response *DescribeBasicAlarmListResponse) {
+    response = &DescribeBasicAlarmListResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取基础告警列表
+func (c *Client) DescribeBasicAlarmList(request *DescribeBasicAlarmListRequest) (response *DescribeBasicAlarmListResponse, err error) {
+    if request == nil {
+        request = NewDescribeBasicAlarmListRequest()
+    }
+    response = NewDescribeBasicAlarmListResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeBindingPolicyObjectListRequest() (request *DescribeBindingPolicyObjectListRequest) {
+    request = &DescribeBindingPolicyObjectListRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribeBindingPolicyObjectList")
+    return
+}
+
+func NewDescribeBindingPolicyObjectListResponse() (response *DescribeBindingPolicyObjectListResponse) {
+    response = &DescribeBindingPolicyObjectListResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取已绑定对象列表
+func (c *Client) DescribeBindingPolicyObjectList(request *DescribeBindingPolicyObjectListRequest) (response *DescribeBindingPolicyObjectListResponse, err error) {
+    if request == nil {
+        request = NewDescribeBindingPolicyObjectListRequest()
+    }
+    response = NewDescribeBindingPolicyObjectListResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribePolicyConditionListRequest() (request *DescribePolicyConditionListRequest) {
+    request = &DescribePolicyConditionListRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribePolicyConditionList")
+    return
+}
+
+func NewDescribePolicyConditionListResponse() (response *DescribePolicyConditionListResponse) {
+    response = &DescribePolicyConditionListResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取基础告警策略条件
+func (c *Client) DescribePolicyConditionList(request *DescribePolicyConditionListRequest) (response *DescribePolicyConditionListResponse, err error) {
+    if request == nil {
+        request = NewDescribePolicyConditionListRequest()
+    }
+    response = NewDescribePolicyConditionListResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribePolicyGroupInfoRequest() (request *DescribePolicyGroupInfoRequest) {
+    request = &DescribePolicyGroupInfoRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribePolicyGroupInfo")
+    return
+}
+
+func NewDescribePolicyGroupInfoResponse() (response *DescribePolicyGroupInfoResponse) {
+    response = &DescribePolicyGroupInfoResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取基础策略组详情
+func (c *Client) DescribePolicyGroupInfo(request *DescribePolicyGroupInfoRequest) (response *DescribePolicyGroupInfoResponse, err error) {
+    if request == nil {
+        request = NewDescribePolicyGroupInfoRequest()
+    }
+    response = NewDescribePolicyGroupInfoResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribePolicyGroupListRequest() (request *DescribePolicyGroupListRequest) {
+    request = &DescribePolicyGroupListRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribePolicyGroupList")
+    return
+}
+
+func NewDescribePolicyGroupListResponse() (response *DescribePolicyGroupListResponse) {
+    response = &DescribePolicyGroupListResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取基础策略告警组列表
+func (c *Client) DescribePolicyGroupList(request *DescribePolicyGroupListRequest) (response *DescribePolicyGroupListResponse, err error) {
+    if request == nil {
+        request = NewDescribePolicyGroupListRequest()
+    }
+    response = NewDescribePolicyGroupListResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeProductEventListRequest() (request *DescribeProductEventListRequest) {
+    request = &DescribeProductEventListRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribeProductEventList")
+    return
+}
+
+func NewDescribeProductEventListResponse() (response *DescribeProductEventListResponse) {
+    response = &DescribeProductEventListResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 分页获取产品事件的列表
+func (c *Client) DescribeProductEventList(request *DescribeProductEventListRequest) (response *DescribeProductEventListResponse, err error) {
+    if request == nil {
+        request = NewDescribeProductEventListRequest()
+    }
+    response = NewDescribeProductEventListResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeProductListRequest() (request *DescribeProductListRequest) {
+    request = &DescribeProductListRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "DescribeProductList")
+    return
+}
+
+func NewDescribeProductListResponse() (response *DescribeProductListResponse) {
+    response = &DescribeProductListResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 查询云监控产品列表
+func (c *Client) DescribeProductList(request *DescribeProductListRequest) (response *DescribeProductListResponse, err error) {
+    if request == nil {
+        request = NewDescribeProductListRequest()
+    }
+    response = NewDescribeProductListResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewGetMonitorDataRequest() (request *GetMonitorDataRequest) {
+    request = &GetMonitorDataRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "GetMonitorData")
+    return
+}
+
+func NewGetMonitorDataResponse() (response *GetMonitorDataResponse) {
+    response = &GetMonitorDataResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 获取云产品的监控数据。传入产品的命名空间、对象维度描述和监控指标即可获得相应的监控数据。
+// 接口调用频率限制为：20次/秒，1200次/分钟。单请求最多可支持批量拉取10个实例的监控数据，单请求的数据点数限制为1440个。
+// 若您需要调用的指标、对象较多，可能存在因限频出现拉取失败的情况，建议尽量将请求按时间维度均摊。
+func (c *Client) GetMonitorData(request *GetMonitorDataRequest) (response *GetMonitorDataResponse, err error) {
+    if request == nil {
+        request = NewGetMonitorDataRequest()
+    }
+    response = NewGetMonitorDataResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewModifyAlarmReceiversRequest() (request *ModifyAlarmReceiversRequest) {
+    request = &ModifyAlarmReceiversRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "ModifyAlarmReceivers")
+    return
+}
+
+func NewModifyAlarmReceiversResponse() (response *ModifyAlarmReceiversResponse) {
+    response = &ModifyAlarmReceiversResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 修改告警接收人
+func (c *Client) ModifyAlarmReceivers(request *ModifyAlarmReceiversRequest) (response *ModifyAlarmReceiversResponse, err error) {
+    if request == nil {
+        request = NewModifyAlarmReceiversRequest()
+    }
+    response = NewModifyAlarmReceiversResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewModifyPolicyGroupRequest() (request *ModifyPolicyGroupRequest) {
+    request = &ModifyPolicyGroupRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "ModifyPolicyGroup")
+    return
+}
+
+func NewModifyPolicyGroupResponse() (response *ModifyPolicyGroupResponse) {
+    response = &ModifyPolicyGroupResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 更新策略组
+func (c *Client) ModifyPolicyGroup(request *ModifyPolicyGroupRequest) (response *ModifyPolicyGroupResponse, err error) {
+    if request == nil {
+        request = NewModifyPolicyGroupRequest()
+    }
+    response = NewModifyPolicyGroupResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewPutMonitorDataRequest() (request *PutMonitorDataRequest) {
+    request = &PutMonitorDataRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "PutMonitorData")
+    return
+}
+
+func NewPutMonitorDataResponse() (response *PutMonitorDataResponse) {
+    response = &PutMonitorDataResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 默认接口请求频率限制：50次/秒。
+// 默认单租户指标上限：100个。
+// 单次上报最多 30 个指标/值对，请求返回错误时，请求中所有的指标/值均不会被保存。
+// 
+// 上报的时间戳为期望保存的时间戳，建议构造整数分钟时刻的时间戳。
+// 时间戳时间范围必须为当前时间到 300 秒前之间。
+// 同一 IP 指标对的数据需按分钟先后顺序上报。
+func (c *Client) PutMonitorData(request *PutMonitorDataRequest) (response *PutMonitorDataResponse, err error) {
+    if request == nil {
+        request = NewPutMonitorDataRequest()
+    }
+    response = NewPutMonitorDataResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewSendCustomAlarmMsgRequest() (request *SendCustomAlarmMsgRequest) {
+    request = &SendCustomAlarmMsgRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "SendCustomAlarmMsg")
+    return
+}
+
+func NewSendCustomAlarmMsgResponse() (response *SendCustomAlarmMsgResponse) {
+    response = &SendCustomAlarmMsgResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 发送自定义消息告警
+func (c *Client) SendCustomAlarmMsg(request *SendCustomAlarmMsgRequest) (response *SendCustomAlarmMsgResponse, err error) {
+    if request == nil {
+        request = NewSendCustomAlarmMsgRequest()
+    }
+    response = NewSendCustomAlarmMsgResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewUnBindingAllPolicyObjectRequest() (request *UnBindingAllPolicyObjectRequest) {
+    request = &UnBindingAllPolicyObjectRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "UnBindingAllPolicyObject")
+    return
+}
+
+func NewUnBindingAllPolicyObjectResponse() (response *UnBindingAllPolicyObjectResponse) {
+    response = &UnBindingAllPolicyObjectResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 删除全部的关联对象
+func (c *Client) UnBindingAllPolicyObject(request *UnBindingAllPolicyObjectRequest) (response *UnBindingAllPolicyObjectResponse, err error) {
+    if request == nil {
+        request = NewUnBindingAllPolicyObjectRequest()
+    }
+    response = NewUnBindingAllPolicyObjectResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewUnBindingPolicyObjectRequest() (request *UnBindingPolicyObjectRequest) {
+    request = &UnBindingPolicyObjectRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("monitor", APIVersion, "UnBindingPolicyObject")
+    return
+}
+
+func NewUnBindingPolicyObjectResponse() (response *UnBindingPolicyObjectResponse) {
+    response = &UnBindingPolicyObjectResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 删除策略的关联对象
+func (c *Client) UnBindingPolicyObject(request *UnBindingPolicyObjectRequest) (response *UnBindingPolicyObjectResponse, err error) {
+    if request == nil {
+        request = NewUnBindingPolicyObjectRequest()
+    }
+    response = NewUnBindingPolicyObjectResponse()
+    err = c.Send(request, response)
+    return
+}
