b876ce6426d7cb8dcfa423b3f6758c6eeb6f204f
wuqinchao <wuqinchao@jiagouyun.com>
2021-01-22 15:19:15 +0800

    hw
    Former-commit-id: 0923c09a193fe1d6636a93450c113c37ddcf3d50 [formerly 13515000b8a1f987d823c28eee3cc023058b4241 [formerly 4cd914c6ebfcf6455966825427d2a022cf60aa4c] [formerly e302376cc9f3dbd8634aa8c0c574fca3fc28724b [formerly 47263bde2d2e62daf294d4e9b957bc8c3fcf1535]]]
    Former-commit-id: b9942a8691afe0ea59897308015112f616ce30c8
    Former-commit-id: c2d83ca6c907e7a2d9064d9c697a37b1b17086e1 [formerly e4b9018bd806b92b4be85a0bad723a8fc482ea12]
    Former-commit-id: f0c9e503bbab11f1f90f2086de37636d18183d5e

--- plugins/inputs/huaweiyunces/cfg.go
+++ plugins/inputs/huaweiyunces/cfg.go
@@ -3,13 +3,15 @@ package huaweiyunces
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"golang.org/x/time/rate"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/sdk/huaweicloud"
+
+	ces "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v1"
 )
 
 const (
@@ -18,7 +20,7 @@ const (
 # ##(required) the following 4 configurations are required for authentication
 #access_key_id = ''
 #access_key_secret = ''
-#endpoint = ''
+#region = ''
 #projectid = ''
 
 # ##(optional) default is 5min
@@ -52,13 +54,13 @@ const (
 # ##(optional) defalt is 300
 #period = 0
 
-# ##(optional) default is average
+# ##(optional) default is average, support values are: max, min, average, sum, variance
 #filter = ''
 
 # ##(optional) if not set, use the global interval
 #interval = '5m'
 
-# ##(required) you can specify up to 3 dimensions
+# ##(required) you can specify up to 4 dimensions
 #[inputs.huaweiyunces.namespace.property.dimensions]
 #instance_id = 'b5d7b7a3-681d-4c08-8e32-f14******'
 `
@@ -85,20 +87,23 @@ type (
 		MetricNames   []string    `toml:"metric_names"`
 		Property      []*Property `toml:"property"`
 
-		globalMetricProperty *Property
+		properties map[string]*Property
 	}
 
 	agent struct {
 		AccessKeyID     string            `toml:"access_key_id"`
 		AccessKeySecret string            `toml:"access_key_secret"`
-		EndPoint        string            `toml:"endpoint"`
+		EndPoint        string            `toml:"endpoint"` //deprated
+		RegionID        string            `toml:"region"`
 		ProjectID       string            `toml:"projectid"`
 		Interval        datakit.Duration  `toml:"interval"`
 		Delay           datakit.Duration  `toml:"delay"`
 		Tags            map[string]string `toml:"tags,omitempty"`
 		Namespace       []*Namespace      `toml:"namespace"`
 
-		client *huaweicloud.HWClient
+		ecsInstanceIDs []string
+
+		client *ces.CesClient
 
 		limiter *rate.Limiter
 
@@ -131,18 +136,17 @@ type (
 		//每个指标可单独配置interval，默认使用全局的配置
 		interval time.Duration
 
-		tryGetMeta int
-
-		//period的配置不支持时调整period
-		tunePeriod bool
-
-		tuneDimension bool
-
 		lastTime time.Time
 	}
 )
 
-func (ag *agent) isTest() bool {
+func (r *metricsRequest) copy() *metricsRequest {
+	var m metricsRequest
+	m = *r
+	return &m
+}
+
+func (ag *agent) isTestOnce() bool {
 	return ag.mode == "test"
 }
 
@@ -150,74 +154,122 @@ func (ag *agent) isDebug() bool {
 	return ag.mode == "debug"
 }
 
-func (p *Namespace) genMetricReq(metric string) (*metricsRequest, error) {
+func (p *Namespace) checkProperties() error {
+	p.properties = make(map[string]*Property)
+
+	for _, prop := range p.Property {
+
+		if prop.Dimensions == nil && p.Name != "SYS.ECS" {
+			err := fmt.Errorf("invalid property, dimensions cannot be empty")
+			moduleLogger.Errorf("%s", err)
+			return err
+		}
+
+		if prop.Name == "" {
+			p.properties["*"] = prop
+		} else {
+			parts := strings.Split(prop.Name, ",")
+			if len(parts) > 1 {
+				for _, sn := range parts {
+					sn = strings.TrimSpace(sn)
+					np := &Property{}
+					np.Dimensions = prop.Dimensions
+					np.Interval = prop.Interval
+					np.Period = prop.Period
+					np.Name = sn
+					np.Tags = prop.Tags
+					np.Filter = prop.Filter
+					p.properties[sn] = np
+				}
+			} else if len(parts) == 1 {
+				p.properties[prop.Name] = prop
+			}
+		}
+
+	}
+
+	return nil
+}
+
+func (p *Namespace) applyProperty(req *metricsRequest, instanceIDs []string) (adds []*metricsRequest) {
+
+	ecs := (p.Name == "SYS.ECS")
+
+	metricName := req.metricname
+
+	var property *Property
+	property = p.properties[metricName]
+	if property == nil {
+		property = p.properties["*"]
+	}
+
+	if property == nil {
+		if ecs {
+			property = &Property{
+				Name: metricName,
+			}
+		} else {
+			return nil
+		}
+	}
+
+	if property.Period > 0 {
+		req.period = property.Period
+	}
+	if property.Filter != "" {
+		req.filter = property.Filter
+	}
+	if property.Interval.Duration != 0 {
+		req.interval = property.Interval.Duration
+	}
+	req.tags = property.Tags
+
+	if len(property.Dimensions) == 0 && ecs {
+		//如果是ecs且没有设置dimension，则默认拿所有instance
+		for idx, inst := range instanceIDs {
+			if idx == 0 {
+				req.dimensoions = []*Dimension{
+					&Dimension{
+						Name:  "instance_id",
+						Value: inst,
+					},
+				}
+			} else {
+				newreq := req.copy()
+				newreq.dimensoions = []*Dimension{
+					&Dimension{
+						Name:  "instance_id",
+						Value: inst,
+					},
+				}
+				adds = append(adds, newreq)
+			}
+		}
+
+	} else {
+		dims := []*Dimension{}
+		for k, v := range property.Dimensions {
+			dim := &Dimension{
+				Name:  k,
+				Value: v,
+			}
+			dims = append(dims, dim)
+		}
+		req.dimensoions = dims
+	}
+
+	return
+}
+
+func (p *Namespace) genMetricReq(metric string) *metricsRequest {
 
 	req := &metricsRequest{
 		namespace:     p.Name,
 		metricname:    metric,
-		tryGetMeta:    5,
 		metricSetName: p.MetricSetName,
+		filter:        "average",
+		period:        300,
 	}
 
-	if p.globalMetricProperty == nil {
-		for _, prop := range p.Property {
-			if prop.Name == "*" {
-				p.globalMetricProperty = prop
-				if prop.Dimensions == nil {
-					return nil, fmt.Errorf("invalid property, dimensions cannot be empty")
-				}
-				break
-			}
-		}
-	}
-
-	var property *Property
-
-	for _, prop := range p.Property {
-		if prop.Name == "*" {
-			continue
-		}
-		if prop.Name == metric {
-			property = prop
-			break
-		}
-	}
-
-	if property == nil {
-		if p.globalMetricProperty == nil {
-			return nil, fmt.Errorf("no property found for %s", metric)
-		}
-		property = p.globalMetricProperty
-	} else {
-		if property.Dimensions == nil {
-			if p.globalMetricProperty != nil && p.globalMetricProperty.Dimensions != nil {
-				property.Dimensions = p.globalMetricProperty.Dimensions
-			} else {
-				return nil, fmt.Errorf("invalid property, dimensions cannot be empty")
-			}
-		}
-	}
-
-	req.period = property.Period
-	if req.period == 0 {
-		req.period = 300
-	}
-	req.filter = property.Filter
-	if req.filter == "" {
-		req.filter = "average"
-	}
-	req.interval = property.Interval.Duration
-	req.tags = property.Tags
-
-	dims := []*Dimension{}
-	for k, v := range property.Dimensions {
-		dim := &Dimension{
-			Name:  k,
-			Value: v,
-		}
-		dims = append(dims, dim)
-	}
-	req.dimensoions = dims
-
-	return req, nil
+	return req
 }
