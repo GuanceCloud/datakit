67c46bf5599f17d0331d10df018a12f3ab1a8c1f
wuqinchao <wuqinchao@jiagouyun.com>
2021-01-28 14:34:19 +0800

    huaweiyunobject update
     [formerly f827f152f73cb342362ae7b340ddf19ec7d354c0] [formerly b40ef039a778358aa6475c5ef1c9799ad016e293 [formerly 639bff1a67e7e9fb45ea95f07c2395f58a56e850] [formerly 37558023184d9fa069c2246c66f5cc6cc719e4b1 [formerly 29e265010da50c86d5028c88ee31d5cbf3551566]]] [formerly aa41ab3a35aab7c2a7af8455ae98f1d396af421b [formerly 830f11262c6ce7d849bb7229fd2687d473bcd5b3] [formerly f827f152f73cb342362ae7b340ddf19ec7d354c0] [formerly b40ef039a778358aa6475c5ef1c9799ad016e293 [formerly 639bff1a67e7e9fb45ea95f07c2395f58a56e850] [formerly 37558023184d9fa069c2246c66f5cc6cc719e4b1 [formerly 29e265010da50c86d5028c88ee31d5cbf3551566]]] [formerly f174c40d357cfa85b59ca00d9961575e7edabfd9 [formerly 25d7a918a563710f7ed3bbeb0dc0edfbd1020aa4] [formerly 180c56007533e61c6f179042041d741c1467cd77] [formerly 567bab31c07a66cb47bfc65144b5189840119426 [formerly baab598380b7c25f92b5de9b0006f54472e973da] [formerly cdb4a83c90a9dd79f360a9e4691148f02abca7d9 [formerly 47a927022ec684e2589396cd8920a9ea428ba38a]]]]]
     [formerly 567bab31c07a66cb47bfc65144b5189840119426 [formerly baab598380b7c25f92b5de9b0006f54472e973da]]
    
    
    
     [formerly abd6d49667bdf4ce99159476c6d7861e37ac1392 [formerly 0db5113e78fbeb92d2d565e4d6f1f2094d9930af]]
    
    
    
    Former-commit-id: fbd43ad46d423cee2d90d4426996da71c6ff5e6d

--- plugins/inputs/hostobject/hostobject_test.go
+++ plugins/inputs/hostobject/hostobject_test.go
@@ -1,108 +1,24 @@
 package hostobject
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"testing"
 
-	"github.com/influxdata/toml"
-	"github.com/influxdata/toml/ast"
-
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 func TestInput(t *testing.T) {
 
-	ag := loadCfg("test.conf").(*objCollector)
-	ag.Pipeline = `json(_,host.meta.os,jjos)
-uppercase(jjos)`
-	ag.mode = "debug"
-	ag.Run()
-}
-
-func TestMessageObj(t *testing.T) {
-	moduleLogger = logger.SLogger(InputName)
-
-	msg := getHostObjectMessage()
-	msgdata, _ := json.Marshal(msg)
-	log.Printf("%s", string(msgdata))
-}
-
-func loadCfg(file string) inputs.Input {
-	data, err := ioutil.ReadFile(file)
+	data, err := ioutil.ReadFile("test.conf")
 	if err != nil {
-		log.Fatalf("read config file failed: %s", err.Error())
-		return nil
+		t.Error(err)
+		return
 	}
-
-	tbl, err := toml.Parse(data)
+	ag, err := config.LoadInputConfig(data, func() inputs.Input { return newInput("debug") })
 	if err != nil {
-		log.Fatalf("parse toml file failed, %s", err)
-		return nil
+		t.Error(err)
+		return
 	}
-
-	creator := func() inputs.Input {
-		return newInput()
-	}
-
-	for field, node := range tbl.Fields {
-
-		switch field {
-		case "inputs":
-			stbl, ok := node.(*ast.Table)
-			if !ok {
-				log.Fatalf("ignore bad toml node")
-			} else {
-				for inputName, v := range stbl.Fields {
-					input, err := tryUnmarshal(v, inputName, creator)
-					if err != nil {
-						log.Fatalf("unmarshal input %s failed: %s", inputName, err.Error())
-						return nil
-					}
-					return input
-				}
-			}
-
-		default: // compatible with old version: no [[inputs.xxx]] header
-			input, err := tryUnmarshal(node, "aa", creator)
-			if err != nil {
-				log.Fatalf("unmarshal input failed: %s", err.Error())
-				return nil
-			}
-			return input
-		}
-	}
-
-	return nil
-}
-
-func tryUnmarshal(tbl interface{}, name string, creator inputs.Creator) (inputs.Input, error) {
-
-	tbls := []*ast.Table{}
-
-	switch t := tbl.(type) {
-	case []*ast.Table:
-		tbls = tbl.([]*ast.Table)
-	case *ast.Table:
-		tbls = append(tbls, tbl.(*ast.Table))
-	default:
-		err := fmt.Errorf("invalid toml format: %v", t)
-		return nil, err
-	}
-
-	for _, t := range tbls {
-		input := creator()
-
-		err := toml.UnmarshalTable(t, input)
-		if err != nil {
-			log.Fatalf("toml unmarshal failed: %v", err)
-			return nil, err
-		}
-		return input, nil
-
-	}
-	return nil, nil
+	ag[0].Run()
 }
