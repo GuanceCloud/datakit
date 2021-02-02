69457bf7ee01bb6c802b03636550b88e1442fca1
tanbiao <tanbiao@jiagouyun.com>
2021-02-02 21:56:44 +0800

    update: 	[+] add io cache 	[*] tailf default `time` field rename: date_access -> time
    Former-commit-id: 446a016a1f3f7baafe6ca8021f71960cb809c0fb
    Former-commit-id: 9abbd101edd5fd51fda56583fe3a5b11c45be321 [formerly e46291d00bc89e1bd7908fd630af400b146b264e]
    Former-commit-id: 21edc6ab374f3c4c2eae5de46460e947636c3501

--- plugins/inputs/nginxlog/nginxlog.go
+++ plugins/inputs/nginxlog/nginxlog.go
@@ -20,7 +20,7 @@ const (
     # read file from beginning
     # if from_begin was false, off auto discovery file
     from_beginning = false
-    
+
     ## characters are replaced using the unicode replacement character
     ## When set to the empty string the data is not decoded to text.
     ## ex: character_encoding = "utf-8"
@@ -38,15 +38,15 @@ const (
 add_pattern("date2", "%{YEAR}[./]%{MONTHNUM}[./]%{MONTHDAY} %{TIME}")
 
 # access log
-grok(_, "%{IPORHOST:client_ip} %{NOTSPACE:http_ident} %{NOTSPACE:http_auth} \\[%{HTTPDATE:date_access}\\] \"%{DATA:http_method} %{GREEDYDATA:http_url} HTTP/%{NUMBER:http_version}\" %{INT:status_code} %{INT:bytes}")
+grok(_, "%{IPORHOST:client_ip} %{NOTSPACE:http_ident} %{NOTSPACE:http_auth} \\[%{HTTPDATE:time}\\] \"%{DATA:http_method} %{GREEDYDATA:http_url} HTTP/%{NUMBER:http_version}\" %{INT:status_code} %{INT:bytes}")
 
 # access log
-add_pattern("access_common", "%{IPORHOST:client_ip} %{NOTSPACE:http_ident} %{NOTSPACE:http_auth} \\[%{HTTPDATE:date_access}\\] \"%{DATA:http_method} %{GREEDYDATA:http_url} HTTP/%{NUMBER:http_version}\" %{INT:status_code} %{INT:bytes}")
+add_pattern("access_common", "%{IPORHOST:client_ip} %{NOTSPACE:http_ident} %{NOTSPACE:http_auth} \\[%{HTTPDATE:time}\\] \"%{DATA:http_method} %{GREEDYDATA:http_url} HTTP/%{NUMBER:http_version}\" %{INT:status_code} %{INT:bytes}")
 grok(_, '%{access_common} "%{NOTSPACE:referrer}" "%{GREEDYDATA:agent}')
 user_agent(agent)
 
 # error log
-grok(_, "%{date2:date_access} \\[%{LOGLEVEL:level}\\] %{GREEDYDATA:msg}, client: %{IPORHOST:client_ip}, server: %{IPORHOST:server}, request: \"%{DATA:http_method} %{GREEDYDATA:http_url} HTTP/%{NUMBER:http_version}\", host: \"%{IPORHOST:host}\"")
+grok(_, "%{date2:time} \\[%{LOGLEVEL:level}\\] %{GREEDYDATA:msg}, client: %{IPORHOST:client_ip}, server: %{IPORHOST:server}, request: \"%{DATA:http_method} %{GREEDYDATA:http_url} HTTP/%{NUMBER:http_version}\", host: \"%{IPORHOST:host}\"")
 
 cast(status_code, "int")
 cast(bytes, "int")
@@ -58,7 +58,7 @@ group_between(status_code, [500,599], "error", status)
 
 nullif(http_ident, "-")
 nullif(http_auth, "-")
-default_time(date_access)
+default_time(time)
 `
 )
 
