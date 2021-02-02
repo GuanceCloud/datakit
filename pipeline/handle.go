61bedd1adb9e71f2fafa6f9928ad35350e7b4407
liushaobo <liushaobo@jiagouyun.com>
2021-02-02 16:15:43 +0800

    暂存
    Former-commit-id: fa004fc5b60a4d42fd2c3d4392b0b1b9d560cb8a [formerly c00e81e5268e65fabed667a81799dd994b80c3fc [formerly 2cc95bed20999f38d9657b6fc5b304dd15369dd6] [formerly bfeef1477e82bdc61b864a5419927a0e2c2ad08c [formerly 315d292d3a5d30510e574f3b4b62c5e5cbf52a60]]]
    Former-commit-id: 138c79074e478bd6fe9c0a12f3353c52355c18b2
    Former-commit-id: 02ac29e3f5515cc02e15c02543620040f876d847 [formerly d91b3949194847d26272699a071d17f3e347e6b4]
    Former-commit-id: fa80f2d36bfe5303bc42fa8444a64662f1b08bad

--- pipeline/handle.go
+++ pipeline/handle.go
@@ -126,9 +126,9 @@ var datePattern = []struct {
 		defaultYear: true,
 	},
 	{
-		desc:        "mysql, 171113 14:14:20",
-		pattern:     `\d{6} \d{2}:\d{2}:\d{2}`,
-		goFmt:       "060102 15:04:05",
+		desc:    "mysql, 171113 14:14:20",
+		pattern: `\d{6} \d{2}:\d{2}:\d{2}`,
+		goFmt:   "060102 15:04:05",
 	},
 }
 
