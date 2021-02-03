b4dc72b3bef6317a8d4692df5d2ac319fd40b3c6
liguozhuang <liguozhuang@jiagouyun.com>
2021-02-03 16:53:32 +0800

    update: 	[*]添加mysqlog日志采集
    Former-commit-id: 90b04c9165a96a1802131bcaa551e605e44d5a04
    Former-commit-id: 181a5209e5e17dd790517df37de754d5a15f306b [formerly 2bfb167f5d6ca3c3e6582657b0c6d72f5c267012]
    Former-commit-id: caffb8127713cf2f193d1afc72ff7435ea79ebcc

---
+++ plugins/inputs/mysqlog/mysql.go
@@ -0,0 +1,89 @@
+package mysqlog
+
+import (
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/tailf"
+)
+
+const (
+	inputName = "mysqlog"
+
+	sampleCfg = `
+[[inputs.tailf]]
+    # glob logfiles
+    # required
+    logfiles = ["/var/log/mysql/*.log"]
+
+    # glob filteer
+    ignore = [""]
+
+    # read file from beginning
+    # if from_begin was false, off auto discovery file
+    from_beginning = false
+
+    ## characters are replaced using the unicode replacement character
+    ## When set to the empty string the data is not decoded to text.
+    ## ex: character_encoding = "utf-8"
+    ##     character_encoding = "utf-16le"
+    ##     character_encoding = "utf-16le"
+    ##     character_encoding = "gbk"
+    ##     character_encoding = "gb18030"
+    ##     character_encoding = ""
+    #character_encoding = ""
+
+    # [inputs.tailf.tags]
+    # tags1 = "value1"
+`
+	pipelineCfg = `
+grok(_, "%{TIMESTAMP_ISO8601:time}\\s+%{INT:thread_id}\\s+%{WORD:operation}\\s+%{GREEDYDATA:raw_query}")
+grok(_, "%{TIMESTAMP_ISO8601:time} %{INT:thread_id} \\[%{NOTSPACE:serverity}\\] %{GREEDYDATA:msg}")
+
+add_pattern("date2", "%{YEAR}%{MONTHNUM2}%{MONTHDAY} %{TIME}")
+grok(_, "%{date2:time} \\s+(InnoDB:|\\[%{NOTSPACE:serverity}\\])\\s+%{GREEDYDATA:msg}")
+
+add_pattern("timeline", "# Time: %{TIMESTAMP_ISO8601:time}")
+add_pattern("userline", "# User@Host: %{NOTSPACE:db_user}\\s+@\\s%{NOTSPACE:db_host}\\s+\\[(%{NOTSPACE:db_ip})?\\](\\s+Id:\\s+%{INT:query_id})?")
+add_pattern("kvline01", "# Query_time: %{NUMBER:query_time}\\s+Lock_time: %{NUMBER:lock_time}\\s+Rows_sent: %{INT:rows_sent}\\s+Rows_examined: %{INT:rows_examined}")
+add_pattern("kvline02", "# Thread_id: %{INT:thread_id}\\s+Killed: %{INT:killed}\\s+Errno: %{INT:errno}")
+add_pattern("kvline03", "# Bytes_sent: %{INT:bytes_sent}\\s+Bytes_received: %{INT:bytes_received}")
+add_pattern("set01", "SET timestamp=%{INT:query_timestamp};")
+add_pattern("set02", "%{GREEDYDATA:db_slow_statement}")
+
+grok(_, "%{timeline}\\n%{userline}\\n%{kvline01}\\n%{kvline02}\\n%{kvline03}\\n%{set01}\\n%{set02}")
+
+cast(thread_id, "int")
+cast(query_id, "int")
+cast(rows_sent, "int")
+cast(rows_examined, "int")
+cast(bytes_sent, "int")
+cast(bytes_received, "int")
+cast(killed, "int")
+cast(errno, "int")
+cast(query_timestamp, "int")
+
+cast(query_time, "float")
+cast(lock_time, "float")
+
+nullif(serverity, "")
+default_time(time)
+`
+)
+
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		t := tailf.NewTailf(
+			inputName,
+			"log",
+			sampleCfg,
+			map[string]string{"mysql": pipelineCfg},
+		)
+		t.Source = inputName
+		t.Pipeline = "mysql.p"
+
+		// # Time: 2019-11-27T10:43:13.460744Z
+		// 2017-12-29T12:04:09.954078Z 0 [Warning] System table 'plugin' is expected to be transactional
+		// 171113 14:14:20  InnoDB: Shutdown completed; log sequence number 1595675
+		t.Match = `^(# Time|\d{4}-\d{2}-\d{2}|\d{6}\s+\d{2}:\d{2}:\d{2}).*`
+		return t
+	})
+}
