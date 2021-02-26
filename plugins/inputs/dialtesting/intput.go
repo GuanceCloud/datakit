ad7f4e8a0c18a8d74626aae4633566ed7e41f2bd
tanbiao <tanbiao@jiagouyun.com>
2021-02-26 12:32:46 +0800

    update: 	[*] update http dialer testing
     [formerly d928f9404a2317a6ac02a1d406bc371028c8837b] [formerly ccf7f6f04b43beb3f66375d8247b600fc3edbbed [formerly d974b00ac661eaf6f9c7a8eed63fbd59b667cb90] [formerly 965c932a0b350f86b9db0a6f36cdf3fbcf0b94d3 [formerly f0809585f54c3f1e87c7328b5f1c33adce5c1987]]]
     [formerly d513428c00ae63ad5fb913ec1a3de8b36daffc73 [formerly 2ba1726a1408532b7f1d05db58b21023474a5c6c]]
    
    
    
    Former-commit-id: ab17b394dec26a450d303e5bdc174e0077c314af

--- plugins/inputs/dialtesting/intput.go
+++ plugins/inputs/dialtesting/intput.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"reflect"
 	"sync"
 	"time"
 
@@ -15,6 +14,7 @@ import (
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/system/rtpanic"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+	dt "gitlab.jiagouyun.com/cloudcare-tools/kodo/dialtesting"
 )
 
 var (
@@ -24,37 +24,27 @@ var (
 
 const (
 	maxCrashCnt = 6
-	StatusStop  = "stop"
 )
 
-type Task interface {
-	ID() string
-	Status() string
-	Run() error
-	Init() error
-	Ticker() *time.Ticker
-	CheckResult() []string
-	Stop() error
-}
-
 type DialTesting struct {
-	Location  string `toml:"location"`
-	Server    string `toml:"server,omitempty"`
-	Frequency string `toml:"frequency,omitempty"`
-
-	Tags map[string]string
+	Region       string `toml:"region"`
+	Server       string `toml:"server,omitempty"`
+	PullInterval string `toml:"pull_interval,omitempty"`
+	Tags         map[string]string
 
 	cli *http.Client
 
 	curTasks map[string]*dialer
 	wg       sync.WaitGroup
+	pos      int64 // current largest-task-update-time
 }
 
 const sample = `[[inputs.dialtesting]]
-	location = "" # required
+	region = "" # required
 
 	server = "dialtesting.dataflux.cn"
-	frequency = "5s"
+
+	pull_interval = "1m" # default 1 min
 
 	[[inputs.net_dial_testing.tags]]
 	# 各种可能的 tag
@@ -76,13 +66,13 @@ func (d *DialTesting) Run() {
 
 	l = logger.SLogger(inputName)
 
-	du, err := time.ParseDuration(d.Frequency)
+	du, err := time.ParseDuration(d.PullInterval)
 	if err != nil {
-		l.Warnf("invalid frequency: %s, use default", d.Frequency)
+		l.Warnf("invalid frequency: %s, use default", d.PullInterval)
 		du = time.Second * 5
 	}
 	if du > 30*time.Second || du < time.Second {
-		l.Warnf("invalid frequency: %s, use default", d.Frequency)
+		l.Warnf("invalid frequency: %s, use default", d.PullInterval)
 		du = time.Second * 5
 	}
 
@@ -125,43 +115,67 @@ func protectedRun(d *dialer) {
 	f(nil, nil)
 }
 
+type taskPullResp struct {
+	Content map[string][]string `json:"content"`
+}
+
 func (d *DialTesting) dispatchTasks(j []byte) error {
-	var tasks []Task
-	if err := json.Unmarshal(j, tasks); err != nil {
+	var resp taskPullResp
+
+	if err := json.Unmarshal(j, &resp); err != nil {
 		l.Error(err)
 		return err
 	}
 
-	for _, task := range tasks {
+	for k, arr := range resp.Content {
 
-		switch t := task.(type) {
+		switch k {
 
-		case *httpTask:
-
-			if dialer, ok := d.curTasks[t.ID()]; ok { // update task
-				if err := dialer.updateTask(t); err != nil {
-					delete(d.curTasks, t.ID())
+		case dt.ClassHTTP:
+			for _, j := range arr {
+				var t dt.HTTPTask
+				if err := json.Unmarshal([]byte(j), &t); err != nil {
+					return err
 				}
-			} else { // create new task
-				if err := t.Init(); err == nil {
-					dialer, err := newDialer(t)
-					if err != nil {
-						return err
+
+				// update dialer pos
+				ts := t.UpdateTimeUs()
+				if d.pos < ts {
+					d.pos = ts
+				}
+
+				if dialer, ok := d.curTasks[t.ID()]; ok { // update task
+					if err := dialer.updateTask(&t); err != nil {
+						delete(d.curTasks, t.ID())
 					}
+				} else { // create new task
+					if err := t.Init(); err == nil {
+						dialer, err := newDialer(&t)
+						if err != nil {
+							return err
+						}
 
-					d.wg.Add(1)
-					go func(id string) {
-						defer d.wg.Done()
-						protectedRun(dialer)
-						l.Infof("input %s exited", id)
-					}(t.ID())
+						d.wg.Add(1)
+						go func(id string) {
+							defer d.wg.Done()
+							protectedRun(dialer)
+							l.Infof("input %s exited", id)
+						}(t.ID())
 
-					d.curTasks[t.ID()] = dialer
+						d.curTasks[t.ID()] = dialer
+					}
 				}
 			}
 
+		case dt.ClassDNS:
+			// TODO
+		case dt.ClassTCP:
+			// TODO
+		case dt.ClassOther:
+			// TODO
+
 		default:
-			return fmt.Errorf("unknown task type: %s", reflect.TypeOf(t).String())
+			return fmt.Errorf("unknown task type: %s", k)
 		}
 	}
 	return nil
@@ -182,16 +196,16 @@ func (d *DialTesting) pullTask() ([]byte, error) {
 		}
 
 	case "http", "https": // task server
-		return d.pullHTTPTask(reqURL)
+		return d.pullHTTPTask(reqURL, d.pos)
 	}
 
 	return nil, fmt.Errorf("unknown scheme: %s", reqURL.Scheme)
 }
 
-func (d *DialTesting) pullHTTPTask(reqURL *url.URL) ([]byte, error) {
+func (d *DialTesting) pullHTTPTask(reqURL *url.URL, sinceUs int64) ([]byte, error) {
 
-	reqURL.Path = "/task"
-	reqURL.RawQuery = "location=" + d.Location
+	reqURL.Path = "/v1/pull"
+	reqURL.RawQuery = fmt.Sprintf("region=%s&since=%d", d.Region, sinceUs)
 
 	req, err := http.NewRequest("GET", reqURL.String(), nil)
 	if err != nil {
