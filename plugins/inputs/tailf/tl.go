9e89dd846b880a42a81258b14692668947b9ab5b
liguozhuang <liguozhuang@jiagouyun.com>
2021-03-01 16:21:09 +0800

    update: 	[*]修复tailf等衍生采集器，因配置文件更新导致新旧不兼容，无法启动的bug 	[*]修复tailf的status字段无法匹配"OK"的bug
    Former-commit-id: 62d3289d6bae3ae2117c258afc7837229f063493 [formerly f43d399f4c1e7a4af781eb7175555a2a90847067 [formerly 58c635c5d2f7cccbd427dfa92bc36f7f02a0bc52] [formerly 2cce9bfb03b50108851d6fc1e86f045927e80808 [formerly 37e966ed8b6a263136068cee34acbbf3aa30853b]]]
    Former-commit-id: 6a63fbdb79fc430678e045cb2de8e6abd26c7476
    Former-commit-id: 67ad1b9d017e336b983043f2c82abf2a4d9b56ce [formerly d0cf98659c5e7bef229b1314d2400928e42f8634]
    Former-commit-id: f9090b751ce330377d1aedf642c5ca3c785bcc70

--- plugins/inputs/tailf/tl.go
+++ plugins/inputs/tailf/tl.go
@@ -238,7 +238,7 @@ var statusMap = map[string]string{
 	"debug":    "debug",
 	"o":        "OK",
 	"s":        "OK",
-	"OK":       "OK",
+	"ok":       "OK",
 }
 
 func addStatus(fields map[string]interface{}) {
