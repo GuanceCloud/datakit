11020ce8a62daefbfb5ce5bc41b467954f139fac
huangjinjin <huangjinjin@jiagouyun.com>
2021-03-02 11:05:56 +0800

    add gevent
     [formerly 8ebfe1439024e6426e859f990cb0fefbc6935c42 [formerly 1e4b8d12b94cf3aa1680d1d383f41725df45ccb2] [formerly dabbac424f913bd430effe632f5e11306a7985ca [formerly 51880dff8cd1cd28ae864c81fddff2948ee359fc]] [formerly fa23980050890e2bc04118a836bd309e74214375] [formerly 523a80b6220ebb3feaf24a46c535bfb3adfa856e [formerly add65f9f5399feda39c083ac73b63d82a12ff21a] [formerly 523a80b6220ebb3feaf24a46c535bfb3adfa856e [formerly add65f9f5399feda39c083ac73b63d82a12ff21a] [formerly a96c15c8c4ce435df27b9db5a7ce907b392d6e20 [formerly 7da841e7ee4d5862ee5d28bc20333c84f7e8a078]]]] [formerly ac19eb09018212006a2fe3ae9ab5e23f60b0838e] [formerly 4e62f78007f48dcefb28f14e2ed375eb803c942b [formerly fadcf1efb6f7e2036abaaa803b1b81568bf4f672] [formerly d905c44aeabb42e15a5b758500b5edd3c1858e45 [formerly 43e521c94f9634988a95e6aeee502a7842e52db6] [formerly d905c44aeabb42e15a5b758500b5edd3c1858e45 [formerly 43e521c94f9634988a95e6aeee502a7842e52db6] [formerly d905c44aeabb42e15a5b758500b5edd3c1858e45 [formerly 43e521c94f9634988a95e6aeee502a7842e52db6] [formerly ab51854e414c29472b75b87e2e7fdd24a72cb247 [formerly 287a006193bf4b098cc48ea6dee7e2f76df33d18]]]]]]]
     [formerly ac19eb09018212006a2fe3ae9ab5e23f60b0838e]
    
    
    
    
    Former-commit-id: e134113b7d213c6caf9fcbbaf1b6b8a3015ca7a4 [formerly ac1236b865f6d5f40d466c359be19247b6c08cbd]
    Former-commit-id: 2cc78d39797538b69873be916622d23b0fdd1189 [formerly 03d9f76d2acee33463ff74ca0e1ff8fef5c5bdc3] [formerly c2487c8dfe6f60d7b99c9f273a4997fa1aac3f90 [formerly b1b87adcfa186765ebf62adcf0aac3f2fb861a8b]]
    Former-commit-id: 8a0a5f549e6e63adf32ffe858c303a9af3566b78 [formerly 6586d0fc3a1e99ef60c287257bbbc7081b12ee02]
    Former-commit-id: 45129e204071203ae4623ad85de4eb9ada1fb21f

--- plugins/externals/qyt_all/es_index.py
+++ plugins/externals/qyt_all/es_index.py
@@ -14,32 +14,35 @@ def get_cluster_info(host, user, password):
 
 
 def run(c):
-    host = c.cfg.get("es_index").get("host")
-    user = c.cfg.get("es_index").get("user")
-    password = c.cfg.get("es_index").get("password")
-    es_url = "{}{}".format(host, "/_stats")
-    response = requests.get(es_url, auth=(user, password))
-    if response.status_code != 200:
-        c.log.error("state_code:%d,resp:%s", response.status_code, response.text)
-        return
-    resp = response.json()
-    lines = []
-    measurement = "es_index"
-    t = datetime.datetime.utcnow()
-    cluster_name, cluster_uuid = get_cluster_info(host, user, password)
-    for k, v in resp.get("indices", {}).items():
-        tags = {
-            "index": k,
-            "cluster_name": cluster_name,
-            "cluster_uuid": cluster_uuid,
-        }
-        fields = {
-            "size": v.get("primaries").get("store", {}).get("size_in_bytes", 0)
-        }
-        line = make_line(measurement, tags, fields, t)
-        lines.append(line)
-    body = '\n'.join(lines) + '\n'
-    dk_url = "http://0.0.0.0:9529/v1/write/metric"
-    result = requests.post(dk_url, data=body.encode("utf-8"))
-    if result.status_code != 200:
-        c.log.error("state_code:%d,resp:%s", result.status_code, result.text)
+    c.log.info("es_index starting...")
+    for es_cfg in c.cfg.get("es_index", {}):
+        host = es_cfg.get("host")
+        user = es_cfg.get("user")
+        password = es_cfg.get("password")
+        es_url = "{}{}".format(host, "/_stats")
+        response = requests.get(es_url, auth=(user, password))
+        if response.status_code != 200:
+            c.log.error("state_code:%d,resp:%s", response.status_code, response.text)
+            continue
+        resp = response.json()
+        lines = []
+        measurement = "es_index"
+        t = datetime.datetime.utcnow()
+        cluster_name, cluster_uuid = get_cluster_info(host, user, password)
+        for k, v in resp.get("indices", {}).items():
+            tags = {
+                "index": k,
+                "cluster_name": cluster_name,
+                "cluster_uuid": cluster_uuid,
+            }
+            fields = {
+                "size": v.get("primaries").get("store", {}).get("size_in_bytes", 0)
+            }
+            line = make_line(measurement, tags, fields, t)
+            lines.append(line)
+        body = '\n'.join(lines) + '\n'
+        dk_url = "http://0.0.0.0:9529/v1/write/metric"
+        result = requests.post(dk_url, data=body.encode("utf-8"))
+        if result.status_code != 200:
+            c.log.error("state_code:%d,resp:%s", result.status_code, result.text)
+    c.log.info("es_index ok")
