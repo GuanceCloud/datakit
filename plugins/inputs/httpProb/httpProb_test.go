99507c56cb0a85a6fcc3cb4814de9515482c9e24
liushaobo <liushaobo@jiagouyun.com>
2021-03-08 18:47:03 +0800

    提交代码
     [formerly 1443d81c43af410c05f54f41c0a6e960dc7044cf [formerly 216a894ec0a29220aaefa9622deb8553dae83dcb] [formerly e0c3d0f228d02e1b7fef4fdf698d2d065f7079e5 [formerly 72b4880428756eb8f0835a703bd85f40ab1e69af]] [formerly 000039a4a4d29db1259dc2ba2c3f78178e627887] [formerly 1acc3325b94e2e1fa082230ad1539294780252ad [formerly b1f2210a8b160896ade052cf6b3b783ae7a09e4c] [formerly 1acc3325b94e2e1fa082230ad1539294780252ad [formerly b1f2210a8b160896ade052cf6b3b783ae7a09e4c] [formerly dba4dbcf872024b3bf3ea235d1dc2e630f2f9dfe [formerly b44d84843971338d963e840c839975add899b3bb]]]] [formerly e61e5c7aeeba21fc703d3aa2a28120755971577f] [formerly a6ee55810781b93624d58bdd80232837c08d88fb [formerly 8c79108f02d79cbdb75c99c509f98a11ccf9093a] [formerly 61a9e3df64b993c5317fccc10abb041c04194728 [formerly 6545648e7529b858a0a63e7a91f589abefe20980] [formerly 61a9e3df64b993c5317fccc10abb041c04194728 [formerly 6545648e7529b858a0a63e7a91f589abefe20980] [formerly 61a9e3df64b993c5317fccc10abb041c04194728 [formerly 6545648e7529b858a0a63e7a91f589abefe20980] [formerly db948ee57ab8e0d0395b9ac5a2b361383a18b730 [formerly 30d8613fe4bdb43c2edb7cf0ae8ecf422d9b52eb]]]]]]]
     [formerly e61e5c7aeeba21fc703d3aa2a28120755971577f]
    
    
    
    
    Former-commit-id: 5f443245928d06b4f174a3581db94972c0d4f1d9 [formerly e8bbde54178c20c0ae74b6fa62eb270f6a6e69d2]
    Former-commit-id: 3ff0d8fc5d2e4a17f50244fb89733b8218cb02af [formerly 5448b28d8adb56f4c1e62466597574020097186c] [formerly 34bc50f141f7215ddabaafb81981441ecf139d03 [formerly 3001e28d35d2a58f36e29e3d8198fe24d4447fe2]]
    Former-commit-id: a73f339d66e2cbb740c63e762938bfea471d4a9b [formerly 3bd9db4fb24562bdd0532a5f422549fa59b9e952]
    Former-commit-id: 39061a4ddb406a58099f9223ea637cbdf4e64f3a

--- plugins/inputs/httpProb/httpProb_test.go
+++ plugins/inputs/httpProb/httpProb_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestMain(t *testing.T) {
@@ -15,23 +16,23 @@ func TestMain(t *testing.T) {
 		Url: []*Url{
 			{
 				Uri: "/",
+				DropBody: false,
 			},
 		},
 	}
 
-	// data := `
-	//    {
-	//    	  "data": "test data",
-	//    }
-	// `
-
-	data := "aaaaaaaaaa"
+	data := `
+	   {
+	   	  "data": "test data",
+	   }
+	`
 
 	request, _ := http.NewRequest(http.MethodPost, "/test?key1=value1&key2=value2&key3=value3", bytes.NewBuffer([]byte(data)))
 	request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 	request.Header.Add("Accept-Encoding", "gzip, deflate")
 	request.Header.Add("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3")
 	request.Header.Add("Connection", "keep-alive")
+	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0")
 
 	response := httptest.NewRecorder()
@@ -44,4 +45,6 @@ func TestMain(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
+
+	time.Sleep(30 * time.Second)
 }
