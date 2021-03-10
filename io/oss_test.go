09aec89228801e47e8a99e530213243e73ef807a
huangjinjin <huangjinjin@jiagouyun.com>
2021-03-10 14:47:29 +0800

    file collector
     [formerly aca31acc495bfff4b20686fb24b5b4dcf4a624f6 [formerly 33ad296ce8af011a849b02af8158446d66c75759] [formerly aca31acc495bfff4b20686fb24b5b4dcf4a624f6 [formerly 33ad296ce8af011a849b02af8158446d66c75759] [formerly 3f24c6434942f3f1ae998422dcc32d4afe35974e [formerly 8560cf54c3bdaa450218aa2ed3ad08aab135da94]]] [formerly aca31acc495bfff4b20686fb24b5b4dcf4a624f6 [formerly 33ad296ce8af011a849b02af8158446d66c75759] [formerly 3f24c6434942f3f1ae998422dcc32d4afe35974e [formerly 8560cf54c3bdaa450218aa2ed3ad08aab135da94]] [formerly 3f24c6434942f3f1ae998422dcc32d4afe35974e [formerly 8560cf54c3bdaa450218aa2ed3ad08aab135da94] [formerly 2dfe3369bc2d8ba5e40b05328aeaf45c45d04e2c [formerly fb2e80af00d5a0c4df7275037a49023dbea1ffe0]]]]]
    
    
    
    Former-commit-id: 32dc5cc94552d4b6d247e8d2d54d8afed558ac46 [formerly f30bc7028f5bb57230593969fbd2febb98aca579]
    Former-commit-id: 1e82f94ef367100119fb3f0d47f02b71a49eb9b9 [formerly e534b093e46266be9b8063b754d1fd5a6792e727] [formerly 3576799477f0219e933604506fb8e706c3490295 [formerly 51002c0fa6d927b08e468c84e8c6b1802fe1d9d5]]
    Former-commit-id: c79356281399a0d672acad2eab631ab0e162b2fa [formerly ec10cce2030c8a1636eec6b6d5130331511db287]
    Former-commit-id: b63dd33af07113a4ace4f96d4ed187269b58dafc

--- io/oss_test.go
+++ io/oss_test.go
@@ -1,8 +1,8 @@
 package io
 
 import (
-	"testing"
 	"os"
+	"testing"
 )
 
 func TestOSSUPLoad(t *testing.T) {
@@ -10,12 +10,12 @@ func TestOSSUPLoad(t *testing.T) {
 	ak := "LTAI4G1E5j5QX5h1S4kT2qfg"
 	sk := "aud5Bwb6tXExMoh5P1XEAinbZCH4kl"
 	bucketName := "test20210223"
-	cli,err := NewOSSClient(region,ak,sk,bucketName)
+	cli, err := NewOSSClient(region, ak, sk, bucketName)
 	if err != nil {
 		l.Fatal(err)
 	}
-	f,_ := os.Open("gen.sh")
-	err = OSSUPLoad(cli,bucketName,"123.sh",f)
+	f, _ := os.Open("gen.sh")
+	err = cli.OSSUPLoad("123", f)
 	if err != nil {
 		l.Fatal(err)
 	}
