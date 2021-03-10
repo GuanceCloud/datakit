02303f1ff53b42e08d4f569a151aaf5337ec2569
huangjinjin <huangjinjin@jiagouyun.com>
2021-03-10 14:47:29 +0800

    file collector
     [formerly 452028e9203b89d6b9f0f9d74699898ebd918b8e] [formerly 088436714d9fcfb0e06208ac62053470252f5e87 [formerly 300ac5c09fa07e8067c849804b03439a6e1c7646] [formerly 942d67553de1549e603ff5d95775325459c11add [formerly 8560cf54c3bdaa450218aa2ed3ad08aab135da94] [formerly 942d67553de1549e603ff5d95775325459c11add [formerly 8560cf54c3bdaa450218aa2ed3ad08aab135da94] [formerly 942d67553de1549e603ff5d95775325459c11add [formerly 8560cf54c3bdaa450218aa2ed3ad08aab135da94] [formerly 2dfe3369bc2d8ba5e40b05328aeaf45c45d04e2c [formerly fb2e80af00d5a0c4df7275037a49023dbea1ffe0]]]]]]
     [formerly 28326ffba839caf1ab281b1e8d43615a1790f8a6 [formerly 464c5b7d5420b766e1fbf1c547d7c338ff891b5d] [formerly 28326ffba839caf1ab281b1e8d43615a1790f8a6 [formerly 464c5b7d5420b766e1fbf1c547d7c338ff891b5d] [formerly 28326ffba839caf1ab281b1e8d43615a1790f8a6 [formerly 464c5b7d5420b766e1fbf1c547d7c338ff891b5d] [formerly e677022440858944cd47af42564204fc5226d151 [formerly 08c15221450291ef4bade11be58ed60afc264cdd]]]]]
    
    
    
    Former-commit-id: 8a80ddaadb49ef5828a161d158b5c7698435b4d1 [formerly cabfd8294c50a9300fd7fc70171b7cb4c0b804dc]
    Former-commit-id: 611713b644bf3b9032d5d6455c60cd5a86b0e639 [formerly b7d26a2d6096d914e0cf86e7ae674e3662888c6a] [formerly d9096dbc680b8de8237faef9d12f8bb04d439686 [formerly 962d3935824e6f7ccb71c45c5395201362a0b1ad]]
    Former-commit-id: d8823627944f605c86b0fcc88e8218cf8bf15e4e [formerly f44cba3af8cc86047b8471543da10854544e176d]
    Former-commit-id: 3fe97bd60f9a49eddc26e912fefb55ef86d05f25

--- plugins/inputs/file_collector/file_collector.go
+++ plugins/inputs/file_collector/file_collector.go
@@ -1,24 +1,25 @@
 package file_collector
 
 import (
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
-	"path/filepath"
-	"os"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
-	"github.com/fsnotify/fsnotify"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
-	"time"
 	"fmt"
-	"strings"
-	"syscall"
+	"github.com/fsnotify/fsnotify"
+	"github.com/nightlyone/lockfile"
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	httpd "gitlab.jiagouyun.com/cloudcare-tools/datakit/http"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 	goIO "io"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 var (
-	inputName  = `file_collector`
-	l          = logger.DefaultSLogger("file_collector")
-	modifyFile = make(map[string]time.Time)
+	inputName = `file_collector`
+	l         = logger.DefaultSLogger("file_collector")
+	fc        = &Fsn{}
 )
 
 func (_ *Fsn) SampleConfig() string {
@@ -42,7 +43,7 @@ func (fsn *Fsn) initFsn() {
 	}
 	fsn.watch = watch
 
-	if fsn.OssClient == nil {
+	if fsn.OssClient != nil {
 		fsn.UploadType = "oss"
 		oc, err := fsn.OssClient.GetOSSCli()
 		if err != nil {
@@ -51,7 +52,7 @@ func (fsn *Fsn) initFsn() {
 		}
 		fsn.OssClient.Cli = oc
 
-	} else if fsn.SftpClient == nil {
+	} else if fsn.SftpClient != nil {
 		fsn.UploadType = "sftp"
 		sc, err := fsn.SftpClient.GetSFTPClient()
 		if err != nil {
@@ -66,6 +67,7 @@ func (fsn *Fsn) initFsn() {
 
 func (fsn *Fsn) Run() {
 	l = logger.SLogger(inputName)
+
 	if !datakit.FileExist(fsn.Path) {
 		l.Errorf("[error] file:%s not exist", fsn.Path)
 		return
@@ -75,6 +77,8 @@ func (fsn *Fsn) Run() {
 		return
 	}
 	fsn.initFsn()
+	fc = fsn
+	httpd.RegHttpHandler("POST", "/"+inputName, Handle)
 
 	filepath.Walk(fsn.Path, func(path string, f os.FileInfo, err error) error {
 		if f.IsDir() {
@@ -93,8 +97,6 @@ func (fsn *Fsn) Run() {
 			switch ev.Op {
 			case fsnotify.Create:
 				fsn.WriteLogByCreate(ev)
-			case fsnotify.Chmod:
-				fsn.WriteLogByChmod(ev)
 			case fsnotify.Remove:
 				fsn.WriteLogByRemove(ev)
 			case fsnotify.Rename:
@@ -118,33 +120,35 @@ func (fsn *Fsn) Run() {
 func (fsn *Fsn) getRemotePath(name string) string {
 	token := datakit.Cfg.MainCfg.DataWay.GetToken()
 	hostName := datakit.Cfg.MainCfg.Hostname
-	name = strings.ReplaceAll(name, "/", "-")
-	name = strings.ReplaceAll(name, "\\", "-")
+	name = strings.ReplaceAll(name, "/", "_")
+	name = strings.ReplaceAll(name, "\\", "_")
+	if fsn.UploadType == "sftp" {
+		return filepath.Join(fsn.SftpClient.UploadPath, token, hostName, name)
+	}
 	return filepath.Join(token, hostName, name)
 }
 
-func (fsn *Fsn) WriteLog(ev fsnotify.Event, fields map[string]interface{}) {
+func (fsn *Fsn) WriteLog(name string, fields map[string]interface{}) {
 	tags := map[string]string{
 		"source":      inputName,
 		"path":        fsn.Path,
-		"filename":    ev.Name,
+		"filename":    name,
 		"upload_type": fsn.UploadType,
 	}
 
-	remotePath := fsn.getRemotePath(ev.Name)
+	remotePath := fsn.getRemotePath(name)
 	switch fsn.UploadType {
-	case "oss", "ft-oss":
+	case "oss":
 		tags["endpoint"] = fsn.OssClient.EndPoint
 		tags["bucket"] = fsn.OssClient.BucketName
 		tags["remote_path"] = remotePath
-		tags["url"] = fmt.Sprintf("https://%s.%s/%s", fsn.OssClient.BucketName, fsn.OssClient.EndPoint, remotePath)
+		tags["url"] = fsn.OssClient.GetOSSUrl(remotePath)
 	case "sftp":
 		tags["user"] = fsn.SftpClient.User
 		tags["remote_path"] = remotePath
 		tags["remote_host"] = fsn.SftpClient.Host
 	default:
 	}
-	fmt.Println(fields["message"])
 	io.NamedFeedEx(inputName, io.Logging, inputName, tags, fields, time.Now())
 }
 
@@ -156,13 +160,13 @@ func (fsn *Fsn) LoadFile(fi os.FileInfo, ev fsnotify.Event) {
 			l.Errorf("[error] fsnotify openfile err:%s", err.Error())
 			return
 		}
-		tmpPath := filepath.Join(datakit.DataDir,remotePath)
-		if err := FileCopy(f, tmpPath);err != nil {
+		tmpPath := filepath.Join(datakit.DataDir, remotePath)
+		if err := FileCopy(f, tmpPath); err != nil {
 			l.Errorf("[error] fileCopy err:%s", err.Error())
 			return
 		}
-		f.Close()
-		copyF,err := os.Open(tmpPath)
+		defer f.Close()
+		copyF, err := os.Open(tmpPath)
 		if err != nil {
 			return
 		}
@@ -173,24 +177,40 @@ func (fsn *Fsn) LoadFile(fi os.FileInfo, ev fsnotify.Event) {
 				l.Errorf("[error] fsnotify ossupload err:%s", err.Error())
 			}
 		case "sftp":
+			fmt.Println("start", remotePath)
 			if err := fsn.SftpClient.SFTPUPLoad(remotePath, copyF); err != nil {
 				l.Errorf("[error] fsnotify sftpupload err:%s", err.Error())
 			}
+			fmt.Println("end")
 		}
-		os.Remove(tmpPath)
+		//os.RemoveAll(tmpPath)
 	}
 }
 
 func FileCopy(f *os.File, tmpPath string) error {
-	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX); err != nil {
-		return err
-	}
-	out := os.NewFile(uintptr(syscall.Stdout), tmpPath)
-	_, err := goIO.Copy(out, f)
+	lock, err := lockfile.New(f.Name())
 	if err != nil {
 		return err
 	}
-	return syscall.Flock(int(f.Fd()), syscall.LOCK_UN)
+	if err = lock.TryLock(); err != nil {
+		return err
+	}
+	defer lock.Unlock()
+
+	err = os.MkdirAll(filepath.Dir(tmpPath), 0777)
+	if err != nil {
+		return err
+	}
+	tmpf, err := os.Create(tmpPath)
+	defer tmpf.Close()
+	if err != nil {
+		return err
+	}
+
+	if _, err := goIO.Copy(tmpf, f); err != nil {
+		return fmt.Errorf("copy err :%s", err.Error())
+	}
+	return nil
 }
 
 func (fsn *Fsn) WriteLogByCreate(ev fsnotify.Event) {
@@ -206,7 +226,7 @@ func (fsn *Fsn) WriteLogByCreate(ev fsnotify.Event) {
 		"message": fmt.Sprintf("文件夹 %s 内创建了新的文件%s", fsn.Path, ev.Name),
 		"size":    fi.Size(),
 	}
-	fsn.WriteLog(ev, fields)
+	fsn.WriteLog(ev.Name, fields)
 	if fsn.UploadType != "" {
 		go fsn.LoadFile(fi, ev)
 	}
@@ -220,23 +240,7 @@ func (fsn *Fsn) WriteLogByRemove(ev fsnotify.Event) {
 	fields := map[string]interface{}{
 		"message": fmt.Sprintf("文件夹 %s 内删除了文件%s", fsn.Path, ev.Name),
 	}
-	fsn.WriteLog(ev, fields)
-}
-
-func (fsn *Fsn) WriteLogByChmod(ev fsnotify.Event) {
-	fi, err := os.Stat(ev.Name)
-	if err != nil {
-		l.Errorf("[error] fsnotify os.stat err:%s", err.Error())
-		return
-	}
-	if fi.IsDir() {
-		return
-	}
-	fields := map[string]interface{}{
-		"message": fmt.Sprintf("文件夹 %s 内文件 %s 变更了权限", fsn.Path, ev.Name),
-		"size":    fi.Size(),
-	}
-	fsn.WriteLog(ev, fields)
+	fsn.WriteLog(ev.Name, fields)
 }
 
 func (fsn *Fsn) WriteLogByWrite(ev fsnotify.Event) {
@@ -249,13 +253,7 @@ func (fsn *Fsn) WriteLogByWrite(ev fsnotify.Event) {
 		"message": fmt.Sprintf("文件夹 %s 中文件 %s 被修改了", fsn.Path, ev.Name),
 		"size":    fi.Size(),
 	}
-	if t, ok := modifyFile[ev.Name]; ok {
-		if time.Since(t) < time.Minute*5 {
-			return
-		}
-	}
-	modifyFile[ev.Name] = time.Now()
-	fsn.WriteLog(ev, fields)
+	fsn.WriteLog(ev.Name, fields)
 	if fsn.UploadType != "" {
 		go fsn.LoadFile(fi, ev)
 	}
@@ -265,13 +263,7 @@ func (fsn *Fsn) WriteLogByRename(ev fsnotify.Event) {
 	fields := map[string]interface{}{
 		"message": fmt.Sprintf("文件夹 %s 中 %s 被重命名了", fsn.Path, ev.Name),
 	}
-	if t, ok := modifyFile[ev.Name]; ok {
-		if time.Since(t) < time.Minute*5 {
-			return
-		}
-	}
-	modifyFile[ev.Name] = time.Now()
-	fsn.WriteLog(ev, fields)
+	fsn.WriteLog(ev.Name, fields)
 }
 
 func init() {
