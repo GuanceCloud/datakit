9e75f9699e0754d525e6ce1db32fe4a004944c4a
huangjinjin <huangjinjin@jiagouyun.com>
2021-03-15 17:22:08 +0800

    file collector
     [formerly 326ce4d8324ed5d7b10d954b49d8e809ff30ef52] [formerly fe13ba4632c9106a6ca76e7725ab60de84e2add0 [formerly 54c9002fa274b93719388a77ade28c02c5012ff2] [formerly 4e3401beb4511013ebd7b282e20e8de27c15fb9b [formerly 65ea6f8c0b0cf3d0cc9a702981fc252700aa8e59] [formerly 4e3401beb4511013ebd7b282e20e8de27c15fb9b [formerly 65ea6f8c0b0cf3d0cc9a702981fc252700aa8e59] [formerly 4e3401beb4511013ebd7b282e20e8de27c15fb9b [formerly 65ea6f8c0b0cf3d0cc9a702981fc252700aa8e59] [formerly a0dde33d7ddb083e19fcb7d5ed30249dbcebb6db [formerly 1f240ecb2296aa70255c9d9f82029c3847323ee7]]]]]]
     [formerly 328ca9e817c2e62e88b43e68219f1a8f966b138f [formerly c837781972584f3d0ab613bfebfb527f20aa4c72] [formerly 328ca9e817c2e62e88b43e68219f1a8f966b138f [formerly c837781972584f3d0ab613bfebfb527f20aa4c72] [formerly 328ca9e817c2e62e88b43e68219f1a8f966b138f [formerly c837781972584f3d0ab613bfebfb527f20aa4c72] [formerly c09e33c1a0e268070d1c1690b4b227daa7745120 [formerly 71168c146f958f1f359a6e43d98b89b665946813]]]]]
    
    
    
    Former-commit-id: eec7ba5db0f738058c9299e2b48c778f77afd2e1 [formerly ce2d6abc2a879e7aa7180ef33360df8b4c47c9a4]
    Former-commit-id: 245e35cb10e296b3fc63faabbc4661bf8870eca8 [formerly 0a5008a835df347527e882f01e731b67b9a420a0] [formerly 3faeee1b9c64afc74a905575b07b92528b8f1b60 [formerly 3b2b0482e054a4d6d810e2cda4375a16a7d72db1]]
    Former-commit-id: 8a498fec06b5c1998aafcab35207bd22f4d9f432 [formerly 980fc50fe25b3e68c492abd640920f7bfb78fda0]
    Former-commit-id: 360bf2fb21ba018cba409840d93de4e5884ea222

--- io/oss_test.go
+++ io/oss_test.go
@@ -1,22 +1,26 @@
 package io
 
 import (
-	"os"
 	"testing"
 )
 
 func TestOSSUPLoad(t *testing.T) {
-	region := "cn-shanghai"
+	endpoint := "oss-cn-shanghai.aliyuncs.com"
 	ak := "LTAI4G1E5j5QX5h1S4kT2qfg"
 	sk := "aud5Bwb6tXExMoh5P1XEAinbZCH4kl"
 	bucketName := "test20210223"
-	cli, err := NewOSSClient(region, ak, sk, bucketName)
+	cli, err := NewOSSClient(endpoint, ak, sk, bucketName)
 	if err != nil {
 		l.Fatal(err)
 	}
-	f, _ := os.Open("gen.sh")
-	err = cli.OSSUPLoad("123", f)
+	//f, _ := os.Open("gen.sh")
+	//err = cli.OSSUPLoad("123", f)
+	//if err != nil {
+	//	l.Fatal(err)
+	//}
+	_, err = cli.ObjectExist("tkn_12595c1a660711ebb18e46cf65a67f12/122.conf")
 	if err != nil {
 		l.Fatal(err)
 	}
+
 }
