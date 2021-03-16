285b19eebfffa629925e05cdb5a02eb6aa872339
huangjinjin <huangjinjin@jiagouyun.com>
2021-03-16 10:54:49 +0800

    http update
    Former-commit-id: e7a775f048ebef5224cdf027399527c595e85012
    Former-commit-id: b70c31c178db3e499b2c2edac1ae85f1abeaa353 [formerly 3fa5891743ba8d370c1874cb778484f96cffaafd]
    Former-commit-id: f423bbe5c06eede35feddac38bbef51762564e43

--- plugins/inputs/file_collector/http_handle.go
+++ plugins/inputs/file_collector/http_handle.go
@@ -1,6 +1,7 @@
 package file_collector
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -11,28 +12,41 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	if handler.Size > F.MaxUploadSize*1024*1024 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("file too large"))
 		return
 	}
 	remotePath := F.getRemotePath(handler.Filename)
 	defer f.Close()
 	switch F.UploadType {
 	case "oss":
-		go func() {
-			err = F.OssClient.OSSUPLoad(remotePath, f)
-			if err != nil {
-				l.Errorf("http oss upload err:%s", err.Error())
-				return
-			}
-		}()
-
+		err = F.OssClient.OSSUPLoad(remotePath, f)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		resp := map[string]string{
+			"url": F.OssClient.GetOSSUrl(remotePath),
+		}
+		body, err := json.MarshalIndent(resp, "", "    ")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write(body)
 	case "sftp":
-		go func() {
-			err = F.SftpClient.SFTPUPLoad(remotePath, f)
-			if err != nil {
-				l.Errorf("http sftp upload err:%s", err.Error())
-				return
-			}
-		}()
+		err = F.SftpClient.SFTPUPLoad(remotePath, f)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		return
