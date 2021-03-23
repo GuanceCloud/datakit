152bfc70abe664575e4d9916f34bce5df7ba0a0a
liushaobo <liushaobo@jiagouyun.com>
2021-03-23 16:19:07 +0800

    提交代码
    Former-commit-id: c40ec982b9ccaa0f41ae88d77295314645f73c3b [formerly 32f25a69a6ce1668b37b5f71e0e30adef81dd462]
    Former-commit-id: 57973a6236362e1f5bc2a573c8ab36c5c04fc6da

--- plugins/inputs/mysqlmonitor/mysql_test.go
+++ plugins/inputs/mysqlmonitor/mysql_test.go
@@ -1,96 +1,133 @@
 package mysqlmonitor
 
 import (
-	"path/filepath"
 	"testing"
-	"time"
-
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
-	// "gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
-	// "gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestRun(t *testing.T) {
-	// t.Run("case-run", func(t *testing.T) {
-	// 	m := MysqlMonitor{}
-	// 	m.Servers = []string{"root:root@tcp(10.200.6.53:3306)/"}
-	// 	m.GatherProcessList = true
-	// 	m.GatherUserStatistics = false
-	// 	m.GatherInfoSchemaAutoInc = false
-	// 	m.GatherInnoDBMetrics = false
-	// 	m.GatherSlaveStatus = false
-	// 	m.GatherBinaryLogs = false
-	// 	m.GatherTableIOWaits = false
-	// 	m.GatherTableLockWaits = false
-	// 	m.GatherIndexIOWaits = false
-	// 	m.GatherEventWaits = false
-	// 	m.GatherTableSchema = false
-	// 	m.GatherFileEventsStats = false
-	// 	m.GatherPerfEventsStatements = false
-	// 	m.GatherGlobalVars = false
-	// 	m.GatherGlobalStatus = false
-	// 	m.Interval = "10s"
-	// 	m.MetricName = name
+func TestGetDsnString(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "127.0.0.1"
+	m.Port = 3306
+	m.User = "root"
+	m.Pass = "test"
 
-	// 	m.Run()
-
-	// 	t.Log("ok")
-	// })
-
-	t.Run("case-push-data", func(t *testing.T) {
-		datakit.InstallDir = "."
-		datakit.DataDir = "."
-		// datakit.OutputFile = "metrics.txt"
-		datakit.GRPCDomainSock = filepath.Join(datakit.InstallDir, "datakit.sock")
-		datakit.Exit = cliutils.NewSem()
-
-		// config.Cfg.MainCfg = &config.MainConfig{}
-		// config.Cfg.MainCfg.DataWay = &config.DataWayCfg{}
-
-		// config.Cfg.MainCfg.DataWay.Host = "preprod-openway.cloudcare.cn"
-		// config.Cfg.MainCfg.DataWay.Token = "tkn_f299ad7b7c0d4acdb8657be8d086f13a"
-		// config.Cfg.MainCfg.DataWay.Scheme = "https"
-		datakit.IntervalDuration = time.Second * 10
-
-		// io.Start()
-
-		m := MysqlMonitor{}
-		m.Servers = []string{"root:123456@tcp(127.0.0.1:3306)/"}
-		// m.GatherProcessList = true
-		// // 无测试数 ok
-		// m.GatherUserStatistics = true
-		// // int64 ok
-		// m.GatherInfoSchemaAutoInc = true
-		// // ok
-		// m.GatherInnoDBMetrics = true
-		// // 无测试数 ok
-		// m.GatherSlaveStatus = true
-		// // ok
-		// m.GatherBinaryLogs = true
-		// //  ok
-		// m.GatherTableIOWaits = true
-		// // 无测试数 ok
-		// m.GatherTableLockWaits = true
-		// // 数据切分(todo) ok
-		// m.GatherIndexIOWaits = true
-		// // ok
-		// m.GatherEventWaits = true
-		// // ok
-		// m.GatherTableSchema = true
-		// // ok
-		// m.GatherFileEventsStats = true
-		// // 无测试数据 ok
-		// m.GatherPerfEventsStatements = true
-		// // fail
-		m.GatherGlobalVars = true
-		// fail
-		// m.GatherGlobalStatus = true
-		m.Interval = "10s"
-		m.MetricName = name
-
-		m.Run()
-
-		t.Log("ok")
-	})
+	expected := "root:test@tcp(127.0.0.1:3306)/"
+	actual := m.getDsnString()
+	assert.Equal(t, expected, actual)
 }
+
+func TestGetStatus(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "127.0.0.1"
+	m.Port = 3306
+	m.User = "root"
+	m.Pass = "test"
+
+	initLog()
+
+	m.initCfg()
+	m.resData = make(map[string]interface{})
+
+	m.getStatus()
+	m.submitMetrics()
+}
+
+func TestGetVariables(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "127.0.0.1"
+	m.Port = 3306
+	m.User = "root"
+	m.Pass = "test"
+
+	initLog()
+
+	m.initCfg()
+	m.resData = make(map[string]interface{})
+
+	m.getVariables()
+	m.submitMetrics()
+}
+
+// todo
+func TestGetInnodbStatus(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "127.0.0.1"
+	m.Port = 3306
+	m.User = "root"
+	m.Pass = "test"
+
+	initLog()
+
+	m.initCfg()
+	m.resData = make(map[string]interface{})
+
+	m.getInnodbStatus()
+	m.submitMetrics()
+}
+
+func TestGetLogStats(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "rm-bp15268nefz6870hg.mysql.rds.aliyuncs.com"
+	m.Port = 3306
+	m.User = "cc_monitor"
+	m.Pass = "Zyadmin123"
+
+	initLog()
+
+	m.initCfg()
+	m.resData = make(map[string]interface{})
+
+	m.getLogStats()
+	m.submitMetrics()
+}
+
+func TestQuerySizePerschema(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "rm-bp15268nefz6870hg.mysql.rds.aliyuncs.com"
+	m.Port = 3306
+	m.User = "cc_monitor"
+	m.Pass = "Zyadmin123"
+
+	initLog()
+
+	m.initCfg()
+	m.resData = make(map[string]interface{})
+
+	m.querySizePerschema()
+	m.submitMetrics()
+}
+
+func TestGetQueryExecTimePerSchema(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "127.0.0.1"
+	m.Port = 3306
+	m.User = "root"
+	m.Pass = "test"
+
+	initLog()
+
+	m.initCfg()
+	m.resData = make(map[string]interface{})
+
+	m.getQueryExecTimePerSchema()
+	m.submitMetrics()
+}
+
+func TestGetQueryExecTime95th(t *testing.T) {
+	m := MysqlMonitor{}
+	m.Host = "127.0.0.1"
+	m.Port = 3306
+	m.User = "root"
+	m.Pass = "test"
+
+	initLog()
+
+	m.initCfg()
+	m.resData = make(map[string]interface{})
+
+	m.getQueryExecTime95th()
+	m.submitMetrics()
+}
+
+
