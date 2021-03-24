0601d1baaf15077af37738d5d723af25b3482caf
tanbiao <tanbiao@jiagouyun.com>
2021-03-24 17:07:08 +0800

    update: 	[*] clean building
     [formerly e40d87da467869f62066a15c4fd09011029cc074] [formerly a03d6e79fdb19d1209a8b5f63fac41db2451571e [formerly 8b92158eb0276791d1a7227da2f2a9829dda9039] [formerly 5b64c52f5617ec9dfc3148aac514e37abc2c3df9 [formerly c46418f42be0dbeb2c507e28b33cde78c9a97142] [formerly 5b64c52f5617ec9dfc3148aac514e37abc2c3df9 [formerly c46418f42be0dbeb2c507e28b33cde78c9a97142] [formerly 5b64c52f5617ec9dfc3148aac514e37abc2c3df9 [formerly c46418f42be0dbeb2c507e28b33cde78c9a97142] [formerly 26c08cb9a59115b5dbaa6cc4418f06114091e28b [formerly a02e7ae4b6b83235bfc81b05d0e2095a60c528b5]]]]]]
     [formerly 0a3e11c9616ae9e6e0ab71e2875b65fd45a78e3b [formerly a741eab980e7244042e5f26a1667371d5a49a976] [formerly 0a3e11c9616ae9e6e0ab71e2875b65fd45a78e3b [formerly a741eab980e7244042e5f26a1667371d5a49a976] [formerly 0a3e11c9616ae9e6e0ab71e2875b65fd45a78e3b [formerly a741eab980e7244042e5f26a1667371d5a49a976] [formerly b5d04ca1d3a2d310284c2f2fc89f885314063565 [formerly 77ff32f8c6286ed3437f931370d94467a465179a]]]]]
    
    
    
    Former-commit-id: 92faa4952380fc3a79d91bb5eb13f57bf37d5ddc [formerly 015dc8b77abde005a6543ff56a6312f5ee5ec051]
    Former-commit-id: ba12b51b746490182879cab163bc81961e71a00f [formerly bddee418f8b700342ba103c7761e64a1a6e0d628] [formerly 9cdf55c22137a99afd1220625c90e1dcd8370232 [formerly 74f713ac8070f878da00969c97b2b0dc266f5c8e]]
    Former-commit-id: 807b5c8430f4cf1fa550967a91a4fc15793f238c [formerly 4b3bd87d1322a2716448ac59db862113b3157d19]
    Former-commit-id: 3a5de7c6760f84d595860c87f76386b1403f17aa

--- vendor/golang.org/x/sys/plan9/asm_plan9_386.s
+++ vendor/golang.org/x/sys/plan9/asm_plan9_386.s
@@ -1,22 +1,12 @@
-// Copyright 2021 The Go Authors. All rights reserved.
+// Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-<<<<<<< HEAD:vendor/golang.org/x/sys/plan9/asm_plan9_386.s
 #include "textflag.h"
 
 //
 // System call support for 386, Plan 9
 //
-=======
-//go:build (darwin || freebsd || netbsd || openbsd) && gc
-// +build darwin freebsd netbsd openbsd
-// +build gc
-
-#include "textflag.h"
-
-// System call support for 386 BSD
->>>>>>> dev:vendor/golang.org/x/sys/unix/asm_bsd_386.s
 
 // Just jump to package syscall's implementation for all these functions.
 // The runtime may know about them.
@@ -27,14 +17,7 @@ TEXT	·Syscall(SB),NOSPLIT,$0-32
 TEXT	·Syscall6(SB),NOSPLIT,$0-44
 	JMP	syscall·Syscall6(SB)
 
-<<<<<<< HEAD:vendor/golang.org/x/sys/plan9/asm_plan9_386.s
 TEXT ·RawSyscall(SB),NOSPLIT,$0-28
-=======
-TEXT	·Syscall9(SB),NOSPLIT,$0-52
-	JMP	syscall·Syscall9(SB)
-
-TEXT	·RawSyscall(SB),NOSPLIT,$0-28
->>>>>>> dev:vendor/golang.org/x/sys/unix/asm_bsd_386.s
 	JMP	syscall·RawSyscall(SB)
 
 TEXT ·RawSyscall6(SB),NOSPLIT,$0-40
