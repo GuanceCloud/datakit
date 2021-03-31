c99b332c6b5119f6350e448e7493473700cf3216
tanbiao <tanbiao@jiagouyun.com>
2021-03-31 11:53:24 +0800

    update: 	[*] refact demo
    Former-commit-id: dfa1c3dd8843715ad7e5c71eb7781236e52efc39 [formerly b658125729777f3450fc4fb7d8caf329dd75f373] [formerly 65f53f2df02fce40001b538449333477e0d5c683 [formerly d84bfadd5482b6b6150f5da69cdc67b1d3d32a37]]
    Former-commit-id: ed8b4c918f134161835d64984dbe05a7d221f5bd [formerly 3ec7fbf648caecc1b39b5d5e73886797fffd69b3]
    Former-commit-id: c3682c09fef49933e84118c0b4dd4dfcaa4cc356

--- plugins/inputs/demo/input.go
+++ plugins/inputs/demo/input.go
@@ -3,7 +3,6 @@ package demo
 import (
 	"time"
 
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils"
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
@@ -15,9 +14,56 @@ var (
 	l         = logger.DefaultSLogger("demo")
 )
 
-type Demo struct{}
+type Input struct {
+	collectCache []inputs.Measurement
+}
 
-func (i *Demo) Run() {
+type demoMeasurement struct {
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+}
+
+func (m *demoMeasurement) LineProto() (io.Point, error) {
+	return io.MakeMetric(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *demoMeasurement) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: "demo",
+		Fields: map[string]*inputs.FieldInfo{
+			"usage":       &inputs.FieldInfo{DataType: inputs.Float, Type: inputs.Gauge, Unit: inputs.Percent, Desc: "this is CPU usage"},
+			"disk_size":   &inputs.FieldInfo{DataType: inputs.Int, Type: inputs.Gauge, Unit: inputs.SizeIByte, Desc: "this is disk size"},
+			"some_string": &inputs.FieldInfo{DataType: inputs.String, Type: inputs.Gauge, Unit: inputs.UnknownUnit, Desc: "some string field"},
+			"ok":          &inputs.FieldInfo{DataType: inputs.Bool, Type: inputs.Gauge, Unit: inputs.UnknownUnit, Desc: "some boolean field"},
+		},
+	}
+}
+
+func (i *Input) Collect() error {
+
+	i.collectCache = []inputs.Measurement{
+		&demoMeasurement{
+			name: "demo",
+			tags: map[string]string{"tag_a": "a", "tag_b": "b"},
+			fields: map[string]interface{}{
+				"usage":       "12.3",
+				"disk_size":   5e9,
+				"some_string": "hello world",
+				"ok":          true,
+			},
+			ts: time.Now(),
+		},
+	}
+
+	// simulate long-time collect..
+	time.Sleep(time.Second)
+
+	return nil
+}
+
+func (i *Input) Run() {
 
 	l = logger.SLogger("demo")
 	tick := time.NewTicker(time.Second * 3)
@@ -33,39 +79,32 @@ func (i *Demo) Run() {
 		case <-tick.C:
 			l.Debugf("demo input gathering...")
 			start := time.Now()
-			pt, err := io.MakeMetric("demo",
-				map[string]string{
-					"tag_a": "a",
-					"tag_b": "b",
-				},
-				map[string]interface{}{
-					"f1": 123,
-					"f2": 456.0,
-					"f3": cliutils.CreateRandomString(1024),
-					"f5": true,
-				},
-			)
-
-			if err != nil {
+			if err := i.Collect(); err != nil {
 				l.Error(err)
 			} else {
-				time.Sleep(time.Second)
-				io.Feed("demo", io.Metric, io.Points{pt},
+
+				inputs.FeedMeasurement("demo", io.Metric, i.collectCache,
 					&io.Option{CollectCost: time.Since(start), HighFreq: (n%2 == 0)})
+
+				i.collectCache = i.collectCache[:] // NOTE: do not forget to clean cache
 			}
 
 		case <-datakit.Exit.Wait():
 			return
 		}
-
 	}
 }
 
-func (i *Demo) Catalog() string      { return "testing" }
-func (i *Demo) SampleConfig() string { return "[inputs.demo]" }
+func (i *Input) Catalog() string      { return "testing" }
+func (i *Input) SampleConfig() string { return "[inputs.demo]" }
+func (i *Input) SampleMeasurement() []inputs.Measurement {
+	return []inputs.Measurement{
+		&demoMeasurement{},
+	}
+}
 
 func init() {
 	inputs.Add(inputName, func() inputs.Input {
-		return &Demo{}
+		return &Input{}
 	})
 }
