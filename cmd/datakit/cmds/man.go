9e797afc9b47f79d66e02110541f1fb82db9752c
tanbiao <tanbiao@jiagouyun.com>
2021-04-01 12:27:49 +0800

    update: 	[*] enable all inputs, build ok
    Former-commit-id: 6449212a1122cbbdda91eb273a604b39c74c0e06 [formerly e568dfb68aade6a3593d0e7aa3276fb4a0d6e8b8]
    Former-commit-id: fd531005885beb55a3aa07f5df22022d3c926424

--- cmd/datakit/cmds/man.go
+++ cmd/datakit/cmds/man.go
@@ -1,13 +1,16 @@
 package cmds
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"strings"
+	"text/template"
 
 	markdown "github.com/MichaelMure/go-term-markdown"
 	"github.com/c-bata/go-prompt"
 
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/git"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/man"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
@@ -49,12 +52,49 @@ func runMan(txt string) {
 }
 
 func getMan(inputName string) {
+	c, ok := inputs.Inputs[inputName]
+	if !ok {
+		fmt.Printf("[E] intput %s not found: %s\n", inputName)
+		return
+	}
+
+	input := c() // construct input
+
+	var sampleMeasurements []inputs.Measurement
+	switch i := input.(type) {
+	case inputs.ManualInput:
+		sampleMeasurements = i.SampleMeasurement()
+	default:
+		fmt.Printf("[E] intput %s not implement SampleMeasurement()\n", inputName)
+		return
+	}
+
 	md, err := man.Get(inputName)
 	if err != nil {
 		fmt.Printf("[E] get manual failed: %s\n", err.Error())
 		return
 	}
 
-	result := markdown.Render(md, 80, 6)
+	temp, err := template.New(inputName).Parse(md)
+	if err != nil {
+		fmt.Printf("[E] invalid markdown template in %s: %s\n", inputName, err.Error())
+		return
+	}
+
+	x := man.Input{
+		InputName:   inputName,
+		InputSample: input.SampleConfig(),
+		Version:     git.Version,
+		ReleaseDate: git.BuildAt,
+	}
+
+	for _, m := range sampleMeasurements {
+		x.Measurements = append(x.Measurements, m.Info())
+	}
+
+	var buf bytes.Buffer
+	temp.Execute(&buf, x)
+
+	result := markdown.Render(buf.String(), 80, 6)
 	fmt.Println(string(result))
 }
