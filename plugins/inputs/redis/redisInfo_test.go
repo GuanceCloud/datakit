9641f03d293e8ed716993c7773542e3955e34721
liushaobo <liushaobo@jiagouyun.com>
2021-04-01 17:36:02 +0800

    redis采集器
    Former-commit-id: 1c9920c3f3c9eadbbc8080eb0361833009fc8b2a [formerly 2d405552bf1eddd7d3b497c37dfcefdce5def432]
    Former-commit-id: f7bca52e0dea61ce0e2bb3ef489239590350d9e8

--- plugins/inputs/redis/redisInfo_test.go
+++ plugins/inputs/redis/redisInfo_test.go
@@ -2,10 +2,18 @@ package redis
 
 import (
 	"testing"
+	"fmt"
+	"github.com/go-redis/redis"
 	// "github.com/stretchr/testify/assert"
 )
 
 func TestCollectInfoMetrics(t *testing.T) {
+	cli := redis.NewClient(&redis.Options{
+        Addr:     "localhost:6379",
+        Password: "dev", // no password set
+        DB:       0,  // use default DB
+    })
+
 	info := CollectInfoMeasurement(cli)
 
 	fmt.Println(info.fields)
