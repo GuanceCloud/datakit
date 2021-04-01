7b3e64e827aa8c0b1a48c18468a998644b0b346c
tanbiao <tanbiao@jiagouyun.com>
2021-04-01 12:27:49 +0800

    update: 	[*] enable all inputs, build ok
    Former-commit-id: 56a334dbf1805b1d6f9b3a2d051e8b108c3d4062
    Former-commit-id: fb1ced5d13477f55bb95bc5d2d9b6853cb67386a [formerly 22b97d8627d7957e083a3ee70b2e531ee4f0a9ac]
    Former-commit-id: 7d68dc6d2a47f7af70726614483a542d53dabbb1

--- plugins/inputs/mock/mock.go
+++ plugins/inputs/mock/mock.go
@@ -40,10 +40,6 @@ func (m *Mock) Catalog() string {
 	return "mock"
 }
 
-func (m *Mock) Test() (result *inputs.TestResult, err error) {
-	return
-}
-
 func (m *Mock) Run() {
 
 	l = logger.SLogger("mock")
