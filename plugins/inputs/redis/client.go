b255126cd04b4cbacfc9d4d5cf12890f60c4f87c
liushaobo <liushaobo@jiagouyun.com>
2021-04-02 16:49:20 +0800

    提交代码
     [formerly 61dc2c0359759fe39a00fff74810c111b3bd96ae [formerly bfd8b082a8d1c82d9792617295e801ecc601c3f2] [formerly a6e3466a412aff1f7fba4455106c1b956a4bfd9b [formerly 8ab8d10be0e1ca1c14dd45764c73c7f490fc911c]] [formerly 2dd4b9494c0a642b2afddbbb79aaf57e7dd0ac13] [formerly 2ecc9449f2276e3684dd80b73a7323fc6ba8733f [formerly 19051385f82a0cd84a71394a578df18af6baba9d] [formerly 2ecc9449f2276e3684dd80b73a7323fc6ba8733f [formerly 19051385f82a0cd84a71394a578df18af6baba9d] [formerly d96f8f7c1b8393e11f3b8ec14c065d2648c34b7d [formerly 154099e3ff951ab2cc8f5913be870d5862649e0f]]]] [formerly 55694a3963cdf1899439408e4efaa3c10355f2d1] [formerly 3b4b9e60f3a2b941397edd97b0bb5dff91e81f22 [formerly d09bce87cf5cb52ffe56f2ef22d8afc6e977ebd7] [formerly f009f48ae18bb97e29e6037d84e3761650dbbe50 [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly f009f48ae18bb97e29e6037d84e3761650dbbe50 [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly f009f48ae18bb97e29e6037d84e3761650dbbe50 [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly 1d8d84c2952b8c94b84f01d9a3e5fa1fb8a0d90e [formerly 0fd2db77f7511b13fb2b5dc3fffa05825fa1530f]]]]]]]
     [formerly 55694a3963cdf1899439408e4efaa3c10355f2d1]
    
    
    
    
    Former-commit-id: 544c057b2056dcd4ce422b01ba87b7ea661cad00 [formerly 00c1f77c65e873b670d8f49f946d239960f8c82e]
    Former-commit-id: a04df4b67904f0597d3be2cccdb9e5c738b0eaa4 [formerly 964bb092c26471b643d9d6659e2e212ad9e0869b] [formerly 6d2b6bc8d27ae76a18e91a0ef326fc7bd871be1b [formerly b77f54003cca74a18f45735f5804c924d52b0099]]
    Former-commit-id: b89f12815b8c7e214cf8ab2b1c9a80bb1475a42b [formerly e35b3bc00f99b594a255f23c76d8006924e5e84a]
    Former-commit-id: 780b34a991bd8c05bcc310a57d6118bc95494b56

---
+++ plugins/inputs/redis/client.go
@@ -0,0 +1,152 @@
+package redis
+
+import (
+	"time"
+    "strings"
+    "bufio"
+
+    "github.com/go-redis/redis"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+type clientMeasurement struct {
+	client *redis.Client
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+	resData map[string]interface{}
+}
+
+func (m *clientMeasurement) LineProto() (io.Point, error) {
+	return io.MakeMetric(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *clientMeasurement) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: "redis_client",
+		Fields: map[string]*inputs.FieldInfo{
+			"id": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"addr": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"fd": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"age": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"idle": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"sub": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"psub": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+		},
+	}
+}
+
+func CollectClientMeasurement(cli *redis.Client, tags map[string]string) *clientMeasurement {
+	m := &clientMeasurement{
+		client: cli,
+		resData: make(map[string]interface{}),
+		tags: make(map[string]string),
+		fields: make(map[string]interface{}),
+	}
+	m.name = "redis_client_list"
+	m.tags = tags
+
+	m.getData()
+	m.submit()
+
+	return m
+}
+
+// 数据源获取数据
+func (m *clientMeasurement) getData() error {
+	list, err := m.client.ClientList().Result()
+	if err != nil {
+		return err
+	}
+	m.parseInfoData(list)
+
+	return nil
+}
+
+// 解析返回结果
+func (m *clientMeasurement) parseInfoData(list string) error {
+	rdr := strings.NewReader(list)
+
+	scanner := bufio.NewScanner(rdr)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+
+		parts := strings.SplitN(line, " ", 18)
+		if len(parts) < 18 {
+			continue
+		}
+
+		for _, part := range parts {
+			item := strings.Split(part, "=")
+
+			key := item[0]
+			val := strings.TrimSpace(item[1])
+
+			if key == "name" {
+				if val == "" {
+					val = "unknown"
+				} else {
+					val = val
+				}
+				m.tags["name"] = val
+			} else {
+				m.resData[key] = val
+			}
+		}
+	}
+
+	return nil
+}
+
+// 提交数据
+func (m *clientMeasurement) submit() error {
+	metricInfo := m.Info()
+	for key, item := range metricInfo.Fields {
+		if value, ok := m.resData[key]; ok {
+			val, err := Conv(value, item.DataType)
+			if err != nil {
+				l.Errorf("infoMeasurement metric %v value %v parse error %v", key, value, err)
+			} else {
+				m.fields[key] = val
+			}
+		}
+	}
+
+	return nil
+}
+
