92f99f49b3a29c757ee612335ac891b7eeb3e75a
liushaobo <liushaobo@jiagouyun.com>
2021-04-02 16:49:20 +0800

    提交代码
     [formerly 61dc2c0359759fe39a00fff74810c111b3bd96ae [formerly bfd8b082a8d1c82d9792617295e801ecc601c3f2] [formerly a6e3466a412aff1f7fba4455106c1b956a4bfd9b [formerly 8ab8d10be0e1ca1c14dd45764c73c7f490fc911c]] [formerly 2dd4b9494c0a642b2afddbbb79aaf57e7dd0ac13] [formerly 2d400d0717834335ce10eeda3df732ea868550db [formerly 19051385f82a0cd84a71394a578df18af6baba9d] [formerly 2d400d0717834335ce10eeda3df732ea868550db [formerly 19051385f82a0cd84a71394a578df18af6baba9d] [formerly d96f8f7c1b8393e11f3b8ec14c065d2648c34b7d [formerly d938974b0bf8c16c805b49f52fabff1a98ac99f4]]]] [formerly 55694a3963cdf1899439408e4efaa3c10355f2d1] [formerly 3b4b9e60f3a2b941397edd97b0bb5dff91e81f22 [formerly d09bce87cf5cb52ffe56f2ef22d8afc6e977ebd7] [formerly 6a073bf00b0083951b5f48825ff1583eeb0c94b3 [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly 6a073bf00b0083951b5f48825ff1583eeb0c94b3 [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly 6a073bf00b0083951b5f48825ff1583eeb0c94b3 [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly 1d8d84c2952b8c94b84f01d9a3e5fa1fb8a0d90e [formerly bde385e98bbde9358d62c37d64af780c13679d99]]]]]]]
     [formerly 55694a3963cdf1899439408e4efaa3c10355f2d1]
    
    
    
    
    Former-commit-id: 544c057b2056dcd4ce422b01ba87b7ea661cad00 [formerly 00c1f77c65e873b670d8f49f946d239960f8c82e]
    Former-commit-id: a04df4b67904f0597d3be2cccdb9e5c738b0eaa4 [formerly 964bb092c26471b643d9d6659e2e212ad9e0869b] [formerly 6d2b6bc8d27ae76a18e91a0ef326fc7bd871be1b [formerly 367002a228d0796212002a777278363fcab15bb8]]
    Former-commit-id: b89f12815b8c7e214cf8ab2b1c9a80bb1475a42b [formerly e35b3bc00f99b594a255f23c76d8006924e5e84a]
    Former-commit-id: 780b34a991bd8c05bcc310a57d6118bc95494b56

---
+++ plugins/inputs/redis/commandStat.go
@@ -0,0 +1,130 @@
+package redis
+
+import (
+	"time"
+    "strings"
+    "bufio"
+
+    "github.com/go-redis/redis"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+type commandMeasurement struct {
+	client *redis.Client
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+	resData map[string]interface{}
+}
+
+func (m *commandMeasurement) LineProto() (io.Point, error) {
+	return io.MakeMetric(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *commandMeasurement) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: "redis_client",
+		Fields: map[string]*inputs.FieldInfo{
+			"calls": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"usec": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+			"usec_per_call": &inputs.FieldInfo{
+				DataType: inputs.Float,
+				Type: inputs.Gauge,
+				Desc: "this is CPU usage",
+			},
+		},
+	}
+}
+
+func CollectCommandMeasurement(cli *redis.Client, tags map[string]string) *commandMeasurement {
+	m := &commandMeasurement{
+		client: cli,
+		resData: make(map[string]interface{}),
+		tags: make(map[string]string),
+		fields: make(map[string]interface{}),
+	}
+
+	m.name = "redis_command_stat"
+	m.tags = tags
+
+	m.getData()
+	m.submit()
+
+	return m
+}
+
+// 数据源获取数据
+func (m *commandMeasurement) getData() error {
+	list, err := m.client.Info("commandstats").Result()
+	if err != nil {
+		return err
+	}
+	m.parseInfoData(list)
+
+	return nil
+}
+
+// 解析返回结果
+func (m *commandMeasurement) parseInfoData(list string) error {
+	rdr := strings.NewReader(list)
+
+	scanner := bufio.NewScanner(rdr)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) < 2 {
+			continue
+		}
+
+		//cmdstat_get:calls=2,usec=16,usec_per_call=8.00
+		method := parts[0]
+
+		m.tags["method"] = method
+
+		itemStrs := strings.Split(parts[1], ",")
+		for _, itemStr := range itemStrs {
+			item := strings.Split(itemStr, "=")
+
+			key := item[0]
+			val := strings.TrimSpace(item[1])
+
+			m.resData[key] = val
+		}
+	}
+
+	return nil
+}
+
+// 提交数据
+func (m *commandMeasurement) submit() error {
+	metricInfo := m.Info()
+	for key, item := range metricInfo.Fields {
+		if value, ok := m.resData[key]; ok {
+			val, err := Conv(value, item.DataType)
+			if err != nil {
+				l.Errorf("infoMeasurement metric %v value %v parse error %v", key, value, err)
+			} else {
+				m.fields[key] = val
+			}
+		}
+	}
+
+	return nil
+}
+
