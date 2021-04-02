5ccb765ac58ba15e444e5f911d70a4b5628f5213
huangjinjin <huangjinjin@jiagouyun.com>
2021-04-02 18:20:35 +0800

    nginx add
     [formerly 5cebc6afebad08995401c48cc70a365b9a6ac7da] [formerly b1abefe420bb78dcaffd54f034b571408d98b14e [formerly 5e9c05a326842698c5f702eb929482b2bc38e598] [formerly f00a5ab2854f2bf6eaa858501ec86bab3a3519d2 [formerly aad6a4eb7de403e5e488d7203d2ad584fd6acab3] [formerly f00a5ab2854f2bf6eaa858501ec86bab3a3519d2 [formerly aad6a4eb7de403e5e488d7203d2ad584fd6acab3] [formerly f00a5ab2854f2bf6eaa858501ec86bab3a3519d2 [formerly aad6a4eb7de403e5e488d7203d2ad584fd6acab3] [formerly cea55de052c84a0437583ea796d43574b7c85923 [formerly 796d4ab72c9af35c9b2e818f5e345bd402dd5ac2]]]]]]
     [formerly f1277370ae7a01c2dae69cef29ba70a0b109e566 [formerly ba841a6985ba95bb17b356b0cac9e7d7b3575288] [formerly f1277370ae7a01c2dae69cef29ba70a0b109e566 [formerly ba841a6985ba95bb17b356b0cac9e7d7b3575288] [formerly f1277370ae7a01c2dae69cef29ba70a0b109e566 [formerly ba841a6985ba95bb17b356b0cac9e7d7b3575288] [formerly 6e8adca709ae35dfaa86c11ac7f71ee1629086ef [formerly 0378f9abe09dc1d6a685af19dbbdd5b66bc8720f]]]]]
    
    
    
    Former-commit-id: a9ae0e1921f948df8eb27d9643549c865dfbd9b9 [formerly 1e709ece854adf60dae180e34cde0fd35e955dd8]
    Former-commit-id: 894138e965a82b9387f67cdbe10519de1e055153 [formerly 6870f58aa52ec7fcd4e0ffb6c33b03672d0f0a1d] [formerly a4670f0bf4e906deb27c80f52228cf751094607f [formerly ff3b671d4119c99bf47433a076626800406b4ff4]]
    Former-commit-id: 996e7e315cf34fb1b834320cf40982d47615e902 [formerly fd89a32cbc42d3aa6734a8a51adce4cfee7ef4f2]
    Former-commit-id: 32dbbaa0717571298b537015a17148393d1bba73

---
+++ plugins/inputs/nginx/input.go
@@ -0,0 +1,116 @@
+package nginx
+
+import (
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+	"net/http"
+	"time"
+)
+
+var (
+	inputName = `nginx`
+	l         = logger.DefaultSLogger(inputName)
+	sample    = `
+[[inputs.nginx]]
+	url = "http://localhost/server_status"
+	# ##(optional) collection interval, default is 30s
+	# interval = "30s"
+	use_vts = false
+	## Optional TLS Config
+	# tls_ca = "/xxx/ca.pem"
+	# tls_cert = "/xxx/cert.cer"
+	# tls_key = "/xxx/key.key"
+	## Use TLS but skip chain & host verification
+	insecure_skip_verify = false
+	
+	# HTTP response timeout (default: 5s)
+	response_timeout = "20s"
+`
+)
+
+func (_ *Input) SampleConfig() string {
+	return sample
+}
+
+func (_ *Input) Catalog() string {
+	return inputName
+}
+
+func (n *Input) Run() {
+	client, err := n.createHttpClient()
+	if err != nil {
+		l.Errorf("[error] nginx init client err:%s", err.Error())
+		return
+	}
+	n.client = client
+	if n.Interval.Duration == 0 {
+		n.Interval.Duration = time.Second * 30
+	}
+
+	tick := time.NewTicker(n.Interval.Duration)
+	defer tick.Stop()
+	cleanCacheTick := time.NewTicker(time.Second * 5)
+	defer cleanCacheTick.Stop()
+
+	for {
+		select {
+		case <-tick.C:
+			n.getMetric()
+		case <-cleanCacheTick.C:
+			if len(n.collectCache) > 0 {
+				inputs.FeedMeasurement(inputName, io.Metric, n.collectCache, &io.Option{CollectCost: time.Since(n.start)})
+				n.collectCache = n.collectCache[:]
+			}
+		case <-datakit.Exit.Wait():
+			l.Info("nginx exit")
+			return
+		}
+	}
+}
+
+func (n *Input) getMetric() {
+	n.start = time.Now()
+	if n.UseVts {
+		n.getVTSMetric()
+	} else {
+		n.getStubStatusModuleMetric()
+	}
+}
+
+func (n *Input) createHttpClient() (*http.Client, error) {
+	tlsCfg, err := n.ClientConfig.TLSConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	if n.ResponseTimeout.Duration < time.Second {
+		n.ResponseTimeout.Duration = time.Second * 5
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsCfg,
+		},
+		Timeout: n.ResponseTimeout.Duration,
+	}
+
+	return client, nil
+}
+
+func (i *Input) SampleMeasurement() []inputs.Measurement {
+	return []inputs.Measurement{
+		&NginxMeasurement{},
+		&ServerZoneMeasurement{},
+		&UpstreamZoneMeasurement{},
+		&CacheZoneMeasurement{},
+	}
+}
+
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		s := &Input{}
+		return s
+	})
+}
