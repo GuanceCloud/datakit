4c5d87d2e8ff3f834807a848a7a95eb2c793acf8
liushaobo <liushaobo@jiagouyun.com>
2021-04-02 16:49:20 +0800

    提交代码
    Former-commit-id: d09bce87cf5cb52ffe56f2ef22d8afc6e977ebd7 [formerly 042cb9f1c6dc422f90646d8436e04b3f31df8830 [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly 1d8d84c2952b8c94b84f01d9a3e5fa1fb8a0d90e [formerly 0fd2db77f7511b13fb2b5dc3fffa05825fa1530f]]]
    Former-commit-id: 55694a3963cdf1899439408e4efaa3c10355f2d1
    Former-commit-id: d96f8f7c1b8393e11f3b8ec14c065d2648c34b7d [formerly 154099e3ff951ab2cc8f5913be870d5862649e0f]
    Former-commit-id: 19051385f82a0cd84a71394a578df18af6baba9d

---
+++ plugins/inputs/redis/input_test.go
@@ -0,0 +1,24 @@
+package redis
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestCollect(t *testing.T) {
+    input := &Input{
+        Host: "127.0.0.1",
+        Port: 6379,
+        Password: "dev",
+        Service: "dev-test",
+        Tags: make(map[string]string),
+    }
+
+    input.initCfg()
+
+    input.Collect()
+
+    for _, obj := range input.collectCache {
+        fmt.Println("obj =====>", obj)
+    }
+}
