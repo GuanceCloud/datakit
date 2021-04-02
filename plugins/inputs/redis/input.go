512cf7ef38391913219544169c7052c41e59e3b0
liushaobo <liushaobo@jiagouyun.com>
2021-04-02 16:49:20 +0800

    提交代码
    Former-commit-id: d02eafa79ef4a4ea578768890c0380e6999557d6
    Former-commit-id: af99ceb9583cc3588471184b22f075dae7c50ea1 [formerly 0c830dadc25c75dfbaf675af063c9d9918e49eff]
    Former-commit-id: 4b4b9f5659f24300322658a9df0e28f42c19e114

--- plugins/inputs/redis/input.go
+++ plugins/inputs/redis/input.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"time"
+	"fmt"
 
 	"github.com/go-redis/redis"
 
@@ -33,46 +34,75 @@ type Input struct {
 	Tags              map[string]string `toml:"tags"`
 	client           *redis.Client      `toml:"-"`
 	collectCache []inputs.Measurement   `toml:"-"`
+	Addr              string            `toml:"-"`
 }
 
-func (i *Input) Init() error {
+func (i *Input) initCfg() {
+	// 采集频度
+	i.IntervalDuration = 1 * time.Minute
+
+	if i.Interval != "" {
+		du, err := time.ParseDuration(i.Interval)
+		if err != nil {
+			l.Errorf("bad interval %s: %s, use default: 1m", i.Interval, err.Error())
+		} else {
+			i.IntervalDuration = du
+		}
+	}
+
+	i.Addr = fmt.Sprintf("%s:%d", i.Host, i.Port)
 	client := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "dev", // no password set
-        DB:       0,  // use default DB
+        Addr:     i.Addr,
+        Password: i.Password, // no password set
+        DB:       i.DB,  // use default DB
     })
 
     i.client = client
 
-    return nil
+    i.globalTag()
+}
+
+func (i *Input) globalTag() {
+	i.Tags["server"] = i.Addr
+	i.Tags["service_name"] = i.Service
 }
 
 func (i *Input) Collect() error {
 	i.collectCache = []inputs.Measurement{}
 
-	// demo
-	demoMeasurement :=CollectDemoMeasurement()
-	i.collectCache = append(i.collectCache, demoMeasurement)
-
 	// 获取info指标
-	infoMeasurement := CollectInfoMeasurement(i.client)
+	infoMeasurement := CollectInfoMeasurement(i.client, i.Tags)
 	i.collectCache = append(i.collectCache, infoMeasurement)
+
+	// 获取客户端信息
+	clientMeasurement := CollectClientMeasurement(i.client, i.Tags)
+	i.collectCache = append(i.collectCache, clientMeasurement)
+
+	// db command
+	commandMeasurement := CollectCommandMeasurement(i.client, i.Tags)
+	i.collectCache = append(i.collectCache, commandMeasurement)
+
+	// slowlog
+	slowlogMeasurement := CollectSlowlogMeasurement(i)
+	i.collectCache = append(i.collectCache, slowlogMeasurement)
+
+	// bigkey
+	bigKeyMeasurement := CollectBigKeyMeasurement(i)
+	i.collectCache = append(i.collectCache, bigKeyMeasurement)
 
 	return nil
 }
 
 func (i *Input) Run() {
-
 	l = logger.SLogger("redis")
-	tick := time.NewTicker(time.Second * 3)
+	tick := time.NewTicker(i.IntervalDuration)
 	defer tick.Stop()
 
-	i.Init()
+	i.initCfg()
 
 	n := 0
 
 	for {
-
 		n++
 		select {
 		case <-tick.C:
@@ -95,12 +125,15 @@ func (i *Input) Run() {
 
 func (i *Input) Catalog() string      { return catalogName }
 
-func (i *Input) SampleConfig() string { return "[inputs.demo]" }
+func (i *Input) SampleConfig() string { return configSample }
 
 func (i *Input) SampleMeasurement() []inputs.Measurement {
 	return []inputs.Measurement{
-		&demoMeasurement{},
 		&infoMeasurement{},
+		&clientMeasurement{},
+		&commandMeasurement{},
+		&slowlogMeasurement{},
+		&bigKeyMeasurement{},
 	}
 }
 
