0e96122dd71ddb4abf3c5a5bcf13a8694f218ba5
liushaobo <liushaobo@jiagouyun.com>
2021-04-02 17:51:47 +0800

    暂存代码
    Former-commit-id: 61d38b2b08e49eecc180c1dede529e0275e7e5a6 [formerly 9783f60882e0193ed2c6cbcaa46d36aa3c11ac03 [formerly aeb2d4054539a5f201a5457a2838869331532219] [formerly 8ee40f17315cdf7a950a2193e57b2c3c4ed407e8 [formerly 94f93f72f9ea2f8958edc6fc28f4278860ab8074]]]
    Former-commit-id: 62e17cb89a65036661221fe0e2bdb10ab474f9c1
    Former-commit-id: a42a2376cff9100296a905c3c767e02b222c9f0b [formerly 16f307fde79b94755b216202a641fbf3e0a7e1b6]
    Former-commit-id: 94685900c1b5c15647e25dd59b407dc5f000a3e8

--- plugins/inputs/redis/db_test.go
+++ plugins/inputs/redis/db_test.go
@@ -1,18 +1,18 @@
 package redis
 
 import (
-	"testing"
 	"fmt"
 	"github.com/go-redis/redis"
+	"testing"
 	// "github.com/stretchr/testify/assert"
 )
 
 func TestDBMeasurement(t *testing.T) {
 	cli := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "dev", // no password set
-        DB:       0,  // use default DB
-    })
+		Addr:     "localhost:6379",
+		Password: "dev", // no password set
+		DB:       0,     // use default DB
+	})
 
 	info := CollectDBMeasurement(cli)
 
