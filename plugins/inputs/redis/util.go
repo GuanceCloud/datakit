c5de66799d9ce77932eb8f5028be79e65b8840db
liushaobo <liushaobo@jiagouyun.com>
2021-04-02 16:49:20 +0800

    提交代码
    Former-commit-id: d09bce87cf5cb52ffe56f2ef22d8afc6e977ebd7 [formerly 1f279a62064b1c6cabff4e056ca4c8446f4ba74b [formerly b15678b8000d3ebae5b9f7c48d07a79338fb3c5b] [formerly 1d8d84c2952b8c94b84f01d9a3e5fa1fb8a0d90e [formerly 0fd2db77f7511b13fb2b5dc3fffa05825fa1530f]]]
    Former-commit-id: 55694a3963cdf1899439408e4efaa3c10355f2d1
    Former-commit-id: d96f8f7c1b8393e11f3b8ec14c065d2648c34b7d [formerly 154099e3ff951ab2cc8f5913be870d5862649e0f]
    Former-commit-id: 19051385f82a0cd84a71394a578df18af6baba9d

--- plugins/inputs/redis/util.go
+++ plugins/inputs/redis/util.go
@@ -2,6 +2,10 @@ package redis
 
 import (
 	"math"
+
+	"github.com/spf13/cast"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 func Round(num float64, precision int) float64 {
@@ -11,4 +15,24 @@ func Round(num float64, precision int) float64 {
 
 func Cast(num float64) int {
 	return int(num + math.Copysign(0.5, num))
+}
+
+func Conv(val interface{}, Datatype int) (interface{}, error) {
+	var (
+		res interface{}
+		err error
+	)
+
+	switch Datatype {
+		case inputs.Float:
+			res, err = cast.ToFloat64E(val)
+		case inputs.Int:
+			res, err = cast.ToInt64E(val)
+		case inputs.Bool:
+			res, err = cast.ToBoolE(val)
+		case inputs.String:
+			res, err = cast.ToStringE(val)
+	}
+
+	return res, err
 }
