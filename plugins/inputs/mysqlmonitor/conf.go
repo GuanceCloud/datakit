7ca5a6d4e87596c47e1b415d8dddc70b9f4bd54b
liushaobo <liushaobo@jiagouyun.com>
2021-04-02 17:52:35 +0800

    暂存
     [formerly d1427fd7347e5a097faf101b5a69125cabfa6d04] [formerly bd65b3f443676a574af39ae842234383a4db1a90 [formerly db0b2068ac0ad32bd7513a675426d60fabee7467] [formerly bf4e5646b54eba37efd7980ecb0131cc26f22d1c [formerly c28d27bee60393f061cb5a330e8897a031b068c3] [formerly bf4e5646b54eba37efd7980ecb0131cc26f22d1c [formerly c28d27bee60393f061cb5a330e8897a031b068c3] [formerly bf4e5646b54eba37efd7980ecb0131cc26f22d1c [formerly c28d27bee60393f061cb5a330e8897a031b068c3] [formerly c608e000a87c2879d4f0b4ae1b86745749b16a10 [formerly 560dafc2924d515362ac83b4850e1832a6faafd4]]]]]]
     [formerly 59806a181775a3a408b11dae65e881dcdc242b5f [formerly 048f03b21bb65239d1b6e9f63f588b11d1526881] [formerly 59806a181775a3a408b11dae65e881dcdc242b5f [formerly 048f03b21bb65239d1b6e9f63f588b11d1526881] [formerly 59806a181775a3a408b11dae65e881dcdc242b5f [formerly 048f03b21bb65239d1b6e9f63f588b11d1526881] [formerly 02f4285a6501e50b063c6fdfc4e1377a29e59ef1 [formerly 90b33e8b2224568e9dbe23dda8ec041b2eeaab28]]]]]
    
    
    
    Former-commit-id: 5d1891f043659268161f6088a5298e663368a6bb [formerly bf1e45b429bc584afe8c4512781b64c69283556d]
    Former-commit-id: ad3689091d76ab1a9445f3e0491064cb08af374d [formerly e42553545e8d357dfb7c6f5982594ca2b7fc55a3] [formerly bd0b5fea5ccbe407a1586d3cab3f41601dc1bf51 [formerly 04309edd115cd3976d6a3e1268f56b37c67a6853]]
    Former-commit-id: ed1217d1ecd700b1a27df8970ca92d3cd0c25f7d [formerly ed068e7c50970d42767ecd1d44772b5d5f060b53]
    Former-commit-id: 4019eb3600906f50d46299d5768cb0582264078e

--- plugins/inputs/mysqlmonitor/conf.go
+++ plugins/inputs/mysqlmonitor/conf.go
@@ -64,40 +64,40 @@ const (
 )
 
 type tls struct {
-	TlsKey                              string			  `toml:"tls_key"`
-	TlsCert                             string			  `toml:"tls_cert"`
-	TlsCA                               string			  `toml:"tls_ca"`
+	TlsKey  string `toml:"tls_key"`
+	TlsCert string `toml:"tls_cert"`
+	TlsCA   string `toml:"tls_ca"`
 }
 
 type options struct {
-	Replication             bool     `toml:"replication"`
-	GaleraCluster           bool	 `toml:"galera_cluster"`
-	ExtraStatusMetrics      bool	 `toml:"extra_status_metrics"`
-	ExtraInnodbMetrics      bool	 `toml:"extra_innodb_metrics"`
-	DisableInnodbMetrics    bool	 `toml:"disable_innodb_metrics"`
-	SchemaSizeMetrics       bool	 `toml:"schema_size_metrics"`
-	ExtraPerformanceMetrics bool	 `toml:"extra_performance_metrics"`
+	Replication             bool `toml:"replication"`
+	GaleraCluster           bool `toml:"galera_cluster"`
+	ExtraStatusMetrics      bool `toml:"extra_status_metrics"`
+	ExtraInnodbMetrics      bool `toml:"extra_innodb_metrics"`
+	DisableInnodbMetrics    bool `toml:"disable_innodb_metrics"`
+	SchemaSizeMetrics       bool `toml:"schema_size_metrics"`
+	ExtraPerformanceMetrics bool `toml:"extra_performance_metrics"`
 }
 
 type MysqlMonitor struct {
 	// 新配置
-	MetricName                          string            `toml:"metricName"`
-	Host                                string			  `toml:"host"`
-	Port                                int			  	  `toml:"port"`
-	User                                string			  `toml:"user"`
-	Pass                                string			  `toml:"pass"`
-	Sock                                string			  `toml:"sock"`
-	Charset                             string			  `toml:"charset"`
-	Timeout                             string			  `toml:"connect_timeout"`
-	TimeoutDuration                     time.Duration     `toml:"-"`
-	Tls									*tls			  `toml:"tls"`
-	Service                             string		  	  `toml:"service"`
-	Interval                            string            `toml:"interval"`
-	IntervalDuration                    time.Duration     `toml:"-"`
-	Tags                                map[string]string `toml:"tags"`
-	options                             *options		  `toml:"options"`
-	db               					*sql.DB
-	resData								map[string]interface{}
+	MetricName       string            `toml:"metricName"`
+	Host             string            `toml:"host"`
+	Port             int               `toml:"port"`
+	User             string            `toml:"user"`
+	Pass             string            `toml:"pass"`
+	Sock             string            `toml:"sock"`
+	Charset          string            `toml:"charset"`
+	Timeout          string            `toml:"connect_timeout"`
+	TimeoutDuration  time.Duration     `toml:"-"`
+	Tls              *tls              `toml:"tls"`
+	Service          string            `toml:"service"`
+	Interval         string            `toml:"interval"`
+	IntervalDuration time.Duration     `toml:"-"`
+	Tags             map[string]string `toml:"tags"`
+	options          *options          `toml:"options"`
+	db               *sql.DB
+	resData          map[string]interface{}
 
 	// 测试相关
 	lastT            time.Time
@@ -107,26 +107,26 @@ type MysqlMonitor struct {
 	testData         []byte `toml:"-, omitempty"`
 
 	// 兼容老版本配置反序列化
-	Product                             string            `toml:"product, omitempty"`
-	Servers                             []string          `toml:"servers, omitempty"`
-	PerfEventsStatementsDigestTextLimit int64             `toml:"perf_events_statements_digest_text_limit, omitempty"`
-	PerfEventsStatementsLimit           int64             `toml:"perf_events_statements_limit, omitempty"`
-	PerfEventsStatementsTimeLimit       int64             `toml:"perf_events_statements_time_limit, omitempty"`
-	TableSchemaDatabases                []string          `toml:"table_schema_databases, omitempty"`
-	GatherProcessList                   bool              `toml:"gather_process_list, omitempty"`
-	GatherUserStatistics                bool              `toml:"gather_user_statistics, omitempty"`
-	GatherInfoSchemaAutoInc             bool              `toml:"gather_info_schema_auto_inc, omitempty"`
-	GatherInnoDBMetrics                 bool              `toml:"gather_innodb_metrics, omitempty"`
-	GatherSlaveStatus                   bool              `toml:"gather_slave_status, omitempty"`
-	GatherBinaryLogs                    bool              `toml:"gather_binary_logs, omitempty"`
-	GatherTableIOWaits                  bool              `toml:"gather_table_io_waits, omitempty"`
-	GatherTableLockWaits                bool              `toml:"gather_table_lock_waits, omitempty"`
-	GatherIndexIOWaits                  bool              `toml:"gather_index_io_waits, omitempty"`
-	GatherEventWaits                    bool              `toml:"gather_event_waits, omitempty"`
-	GatherTableSchema                   bool              `toml:"gather_table_schema, omitempty"`
-	GatherFileEventsStats               bool              `toml:"gather_file_events_stats, omitempty"`
-	GatherPerfEventsStatements          bool              `toml:"gather_perf_events_statements, omitempty"`
-	GatherGlobalVars                    bool              `toml:"gather_global_variables, omitempty"`
-	GatherGlobalStatus                  bool              `toml:"gather_global_status, omitempty"`
-	IntervalSlow                        string            `toml:"interval_slow, omitempty"`
+	Product                             string   `toml:"product, omitempty"`
+	Servers                             []string `toml:"servers, omitempty"`
+	PerfEventsStatementsDigestTextLimit int64    `toml:"perf_events_statements_digest_text_limit, omitempty"`
+	PerfEventsStatementsLimit           int64    `toml:"perf_events_statements_limit, omitempty"`
+	PerfEventsStatementsTimeLimit       int64    `toml:"perf_events_statements_time_limit, omitempty"`
+	TableSchemaDatabases                []string `toml:"table_schema_databases, omitempty"`
+	GatherProcessList                   bool     `toml:"gather_process_list, omitempty"`
+	GatherUserStatistics                bool     `toml:"gather_user_statistics, omitempty"`
+	GatherInfoSchemaAutoInc             bool     `toml:"gather_info_schema_auto_inc, omitempty"`
+	GatherInnoDBMetrics                 bool     `toml:"gather_innodb_metrics, omitempty"`
+	GatherSlaveStatus                   bool     `toml:"gather_slave_status, omitempty"`
+	GatherBinaryLogs                    bool     `toml:"gather_binary_logs, omitempty"`
+	GatherTableIOWaits                  bool     `toml:"gather_table_io_waits, omitempty"`
+	GatherTableLockWaits                bool     `toml:"gather_table_lock_waits, omitempty"`
+	GatherIndexIOWaits                  bool     `toml:"gather_index_io_waits, omitempty"`
+	GatherEventWaits                    bool     `toml:"gather_event_waits, omitempty"`
+	GatherTableSchema                   bool     `toml:"gather_table_schema, omitempty"`
+	GatherFileEventsStats               bool     `toml:"gather_file_events_stats, omitempty"`
+	GatherPerfEventsStatements          bool     `toml:"gather_perf_events_statements, omitempty"`
+	GatherGlobalVars                    bool     `toml:"gather_global_variables, omitempty"`
+	GatherGlobalStatus                  bool     `toml:"gather_global_status, omitempty"`
+	IntervalSlow                        string   `toml:"interval_slow, omitempty"`
 }
