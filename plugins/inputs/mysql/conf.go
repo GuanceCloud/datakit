5efb75b6bb7bfa86160cda938576390a8c897bd7
liushaobo <liushaobo@jiagouyun.com>
2021-04-06 18:19:50 +0800

    oracle采集器完成
    Former-commit-id: 950e5fc0a9785b79abfc0083abe0d3951ff6ecc2 [formerly 93d15da2489c33c834cd60c2c67d3be5a24e1400]
    Former-commit-id: 245ff678488743d8933f41c874a4eca8694b76dc

--- plugins/inputs/mysql/conf.go
+++ plugins/inputs/mysql/conf.go
@@ -3,58 +3,83 @@ package mysql
 const (
 	configSample = `
 [[inputs.mysqlMonitor]]
-# specify servers via a url matching:
-#  [username[:password]@][protocol[(address)]]/[?tls=[true|false|skip-verify|custom]]
-#  see https://github.com/go-sql-driver/mysql#dsn-data-source-name
-#  e.g.
-#    servers = ["user:passwd@tcp(127.0.0.1:3306)/?tls=false"]
-#    servers = ["user@tcp(127.0.0.1:3306)/?tls=false"]
-# product support MySQL and MariaDB, default MySQL
- product = "MySQL"
-# If no servers are specified, then localhost is used as the host.
- servers = ["tcp(127.0.0.1:3306)/"]
-# scan interval
- interval = "10m"
-# Selects the metric output format.
-# if the list is empty, then metrics are gathered from all database tables
- table_schema_databases = []
-# gather metrics from INFORMATION_SCHEMA.TABLES for databases provided above list
- gather_table_schema = true
-# gather thread state counts from INFORMATION_SCHEMA.PROCESSLIST
- gather_process_list = true
-# gather user statistics from INFORMATION_SCHEMA.USER_STATISTICS
- gather_user_statistics = true
-# gather auto_increment columns and max values from information schema
- gather_info_schema_auto_inc = true
-# gather metrics from INFORMATION_SCHEMA.INNODB_METRICS
- gather_innodb_metrics = true
-# gather metrics from SHOW SLAVE STATUS command output
- gather_slave_status = true
-# gather metrics from SHOW BINARY LOGS command output
- gather_binary_logs = true
-# gather metrics from PERFORMANCE_SCHEMA.GLOBAL_VARIABLES
- gather_global_variables = true
-# gather metrics from PERFORMANCE_SCHEMA.GLOBAL_STATUS
- gather_global_status = true
-# gather metrics from PERFORMANCE_SCHEMA.TABLE_IO_WAITS_SUMMARY_BY_TABLE
- gather_table_io_waits = true
-# gather metrics from PERFORMANCE_SCHEMA.TABLE_LOCK_WAITS
- gather_table_lock_waits = true
-# gather metrics from PERFORMANCE_SCHEMA.TABLE_IO_WAITS_SUMMARY_BY_INDEX_USAGE
- gather_index_io_waits = true
-# gather metrics from PERFORMANCE_SCHEMA.EVENT_WAITS
- gather_event_waits = true
-# gather metrics from PERFORMANCE_SCHEMA.FILE_SUMMARY_BY_EVENT_NAME
- gather_file_events_stats = true
-# gather metrics from PERFORMANCE_SCHEMA.EVENTS_STATEMENTS_SUMMARY_BY_DIGEST
- gather_perf_events_statements = true
-# the limits for metrics form perf_events_statements
- perf_events_statements_digest_text_limit = 120
- perf_events_statements_limit = 250
- perf_events_statements_time_limit = 86400
-# Use TLS but skip chain & host verification
- [inputs.mysqlMonitor.tags]
- tags1 = "value1"
+	## @param host - string - optional
+    ## MySQL host to connect to.
+    ## NOTE: Even if the host is "localhost", the agent connects to MySQL using TCP/IP, unless you also
+    ## provide a value for the sock key (below).
+    #
+  	host = "localhost"
+
+    ## @param user - string - optional
+    ## Username used to connect to MySQL.
+    #
+    user = "datakit"
+
+    ## @param pass - string - optional
+    ## Password associated to the MySQL user.
+    #
+    pass = "<PASS>"
+
+    ## @param port - number - optional - default: 3306
+    ## Port to use when connecting to MySQL.
+    #
+    port = 3306
+
+    ## @param sock - string - optional
+    ## Path to a Unix Domain Socket to use when connecting to MySQL (instead of a TCP socket).
+    ## If you specify a socket you dont need to specify a host.
+    #
+    # sock = "<SOCK>"
+
+    ## @param charset - string - optional
+    ## Charset you want to use.
+    #
+    # charset = "utf8"
+
+    ## @param connect_timeout - number - optional - default: 10
+    ## Maximum number of seconds to wait before timing out when connecting to MySQL.
+    #
+    # connect_timeout = 10
+
+   	## @param service - string - optional
+    ## Attach the tag service:<SERVICE> to every metric
+    ##
+    # service = "<SERVICE>"
+
+    ## @param ssl - mapping - optional
+    ## Use this section to configure a TLS connection between the Agent and MySQL.
+    ##
+    ## The following fields are supported:
+    ##
+    ## key: Path to a key file.
+    ## cert: Path to a cert file.
+    ## ca: Path to a CA bundle file.
+    #
+    # ssl:
+    #   key: <KEY_FILE_PATH>
+    #   cert: <CERT_FILE_PATH>
+    #   ca: <CA_PATH_FILE>
+
+
+    ## Enable options to collect extra metrics from your MySQL integration.
+    #
+    options:
+        ## @param disable_innodb_metrics - boolean - optional - default: false
+        ## Set to `true` only if experiencing issues with older (unsupported) versions of MySQL
+        ## that do not run or have InnoDB engine support.
+        ##
+        ## If this flag is enabled, you will only receive a small subset of metrics.
+        ##
+        ## see also the MySQL metrics listing: https://docs.datadoghq.com/integrations/mysql/#metrics
+        #
+        # disable_innodb_metrics = false
+
+    ## @param tags - list of strings - optional
+    ## A list of tags to attach to every metric and service check emitted by this instance.
+    ##
+    [inputs.httpProb.tags]
+    # tag1 = val1
+    # tag2 = val2
 `
 )
 
