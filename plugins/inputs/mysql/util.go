ca7bdb917d05b7fe9169141acb3d3843fc2ddb13
liushaobo <liushaobo@jiagouyun.com>
2021-04-06 14:10:33 +0800

    mysql采集开发
     [formerly c07e58b6d33254ef4fa353024b1bc1be972e34db [formerly 4fd737fb496ae9b23ebffe4c988b0773b9fb7cd0] [formerly e56dbbf0b84d403c87ccf29f9e646e3e4c2723bb [formerly 0f8f3917754f774258de27d32b46bf580cf4db2a]] [formerly 49a12ac92b1d4cff2b91d22d83f506742e744744] [formerly c3ede11dfd9208f8c59f5892f89ee93dd353898b [formerly 822948b193f836fbeb9681ac4d6a8682817317b9] [formerly c3ede11dfd9208f8c59f5892f89ee93dd353898b [formerly 822948b193f836fbeb9681ac4d6a8682817317b9] [formerly ce227a289e150f44c2f4f73f65a7fdd02819db93 [formerly d7172208305d18e3ca50edbe1012379a1358237a]]]] [formerly d7edb540ad2a6e675e05790892d31f693278324d] [formerly abab76ec99b8deedbe643a94452e63ed8743fc4b [formerly 1913121757a7dc2c859b372c40cf658069b07ca8] [formerly 5fca0264d51eadfbe6e78fdd9eef83827773e328 [formerly ce8d91e1a12700f39770560a6897fd222f22dd88] [formerly 5fca0264d51eadfbe6e78fdd9eef83827773e328 [formerly ce8d91e1a12700f39770560a6897fd222f22dd88] [formerly 5fca0264d51eadfbe6e78fdd9eef83827773e328 [formerly ce8d91e1a12700f39770560a6897fd222f22dd88] [formerly 70f73813d0b796584ed1142f4c51038b96b79192 [formerly a0e7dfef28957564b78519b86bd6cd20e99a1e9f]]]]]]]
     [formerly d7edb540ad2a6e675e05790892d31f693278324d]
    
    
    
    
    Former-commit-id: d76654837aefe1122142c400568edd71c097afc2 [formerly 2aa7b87c4a27a02f1552e7331b850cf74ccaa901]
    Former-commit-id: bbee229d53b7e06893fa93ed7d5cc3d8e3e6c904 [formerly b08c7d69ef0e249ff8ba0d78c65ce3494cb997c0] [formerly 675ea47cbecc1390f04c93abcb3e597b913f24c1 [formerly c2a1271091e127945e0489476c551758374bf4f0]]
    Former-commit-id: da477553287eb7a4987283024e7066a17ac4c305 [formerly 01dea1052bc2a5e8d32230bd7ec4dfcc207c13ca]
    Former-commit-id: a95d76f54d02ad9b8f4dc3d2e549aa0d6151be4d

--- plugins/inputs/mysql/util.go
+++ plugins/inputs/mysql/util.go
@@ -1,4 +1,4 @@
-package mysqlmonitor
+package mysql
 
 import (
 	"bytes"
@@ -7,6 +7,8 @@ import (
 	"math"
 	"strconv"
 	"strings"
+	"github.com/spf13/cast"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 type ConversionFunc func(value sql.RawBytes) (interface{}, error)
@@ -140,4 +142,24 @@ func makeBigint(hi string, lo string) int64 {
 	val := hiVal * loVal
 
 	return val
+}
+
+func Conv(val interface{}, Datatype string) (interface{}, error) {
+	var (
+		res interface{}
+		err error
+	)
+
+	switch Datatype {
+	case inputs.Float:
+		res, err = cast.ToFloat64E(val)
+	case inputs.Int:
+		res, err = cast.ToInt64E(val)
+	case inputs.Bool:
+		res, err = cast.ToBoolE(val)
+	case inputs.String:
+		res, err = cast.ToStringE(val)
+	}
+
+	return res, err
 }
