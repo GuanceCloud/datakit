899abc46faa641cf05c1ffb38f22d0a6b31b3260
tanbiao <tanbiao@jiagouyun.com>
2021-04-07 20:24:48 +0800

    update: 	[*] add :9529/man
    Former-commit-id: ef211cbb7b7f7bae4523a5d31120494c68371e43 [formerly 6136f29e57f3daac0db8c785527340f828b4f52e] [formerly 528930ddd559f0c9f63995c308e2be8cbafaa437 [formerly 68454953795e5d81bf5afdf6e850d8cd73eecf1a]]
    Former-commit-id: 90cd47a27553cedf4788c1d8446b1b81b88c8281 [formerly 6854c5ffc31073250af5d936e49533602bdf787e]
    Former-commit-id: e8dab5fbce3dd16f601220938d558943c2c58332

--- man/man.go
+++ man/man.go
@@ -1,24 +1,20 @@
 package man
 
 import (
-	"html/template"
-	"net/http"
+	"bytes"
+	"fmt"
+	"text/template"
 
-	"github.com/gin-gonic/gin"
 	"github.com/gobuffalo/packr/v2"
-	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/html"
-	"github.com/gomarkdown/markdown/parser"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
-	dkhttp "gitlab.jiagouyun.com/cloudcare-tools/datakit/http"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/git"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 var (
-	ManualBox   = packr.New("manulas", "./manuals")
-	tocTemplate *template.Template
-	l           = logger.DefaultSLogger("man")
+	ManualBox = packr.New("manulas", "./manuals")
+	l         = logger.DefaultSLogger("man")
 )
 
 type Input struct {
@@ -33,52 +29,42 @@ func Get(name string) (string, error) {
 	return ManualBox.FindString(name + ".md")
 }
 
-type tocPage struct {
-	PageTitle  string
-	InputNames []string
-}
+func BuildMarkdownManual(name string) ([]byte, error) {
 
-func InitManPage() error {
-	// table of content template
-	//data, err := ManualBox.FindString("toc.html")
-	//if err != nil {
-	//	return err
-	//}
+	x, ok := inputs.Inputs[name]
+	if !ok {
+		return nil, fmt.Errorf("intput %s not found", name)
+	}
 
-	l = logger.SLogger("man")
-
-	tocTemplate = template.Must(template.ParseFiles("toc.html"))
-
-	dkhttp.RegGinHandler("GET", "/man", func(c *gin.Context) {
-
-		v := c.Query("input")
-		if v == "" { // TOC page
-			data := tocPage{
-				PageTitle:  "DataKit Manuals",
-				InputNames: []string{},
-			}
-
-			if err := tocTemplate.Execute(c.Writer, data); err != nil {
-				l.Error(err)
-			}
-		} else { // input detail page
-			data, err := Get(v)
-			if err != nil {
-				l.Warn(err)
-				http.Redirect(c.Writer, c.Request, "/man", 302)
-			}
-
-			mdext := parser.CommonExtensions
-			psr := parser.NewWithExtensions(mdext)
-
-			htmlFlags := html.CommonFlags | html.HrefTargetBlank | html.TOC
-			opts := html.RendererOptions{Flags: htmlFlags}
-			renderer := html.NewRenderer(opts)
-
-			out := markdown.ToHTML([]byte(data), psr, renderer)
-			c.Data(http.StatusOK, "text/html; charset=UTF-8", out)
+	input := x()
+	switch i := input.(type) {
+	case inputs.ManualInput: // pass
+		sampleMeasurements := i.SampleMeasurement()
+		x := Input{
+			InputName:   name,
+			InputSample: i.SampleConfig(),
+			Version:     git.Version,
+			ReleaseDate: git.BuildAt,
 		}
-	})
+		for _, m := range sampleMeasurements {
+			x.Measurements = append(x.Measurements, m.Info())
+		}
 
-	return nil
+		md, err := Get(name)
+		if err != nil {
+			return nil, err
+		}
+		temp, err := template.New(name).Parse(md)
+		if err != nil {
+			return nil, err
+		}
+
+		var buf bytes.Buffer
+		if err := temp.Execute(&buf, x); err != nil {
+			return nil, err
+		}
+		return buf.Bytes(), nil
+	default:
+		return nil, nil
+	}
 }
