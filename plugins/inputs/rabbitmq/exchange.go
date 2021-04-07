34ae4cbddcfca359e2dcc47dd7e10ff3a30a6e3d
huangjinjin <huangjinjin@jiagouyun.com>
2021-04-07 18:59:40 +0800

    add rabbitmq solve issue https://gitlab.jiagouyun.com/cloudcare-tools/datakit/-/issues/31
     [formerly b55f116ed1db1fba2349096b413d36af0d8340e3 [formerly 76b6cdd5619eee690f30940956e49711eb1391d7] [formerly b55f116ed1db1fba2349096b413d36af0d8340e3 [formerly 76b6cdd5619eee690f30940956e49711eb1391d7] [formerly b55f116ed1db1fba2349096b413d36af0d8340e3 [formerly 76b6cdd5619eee690f30940956e49711eb1391d7] [formerly 73c7075861dc4613948958746d94e74c3d687af5 [formerly 623f3ffaffa67b2e40ed97094d388aafd4223235]]]]]
    
    
    
    Former-commit-id: 10da3a3271305468f691fa4664c788f013230d1f [formerly f51a64facfb15590a9ce51aaf66c80f150684992]
    Former-commit-id: 4a3acb8981e060714e80ae45ef5f8042fb40b347 [formerly 6551d11a91ca568e3313e2c806b37f23dbea2c8d] [formerly ff77d780af987f82546062048dc868553a89e087 [formerly ac468a0f32ee559ca0228f6cee123adb1a1eb01b]]
    Former-commit-id: 474b39b59b0a726d67e258b842e03e55738f6b0f [formerly 28bdc1937d7514dc3d4ff46f06c7d6b27c54a186]
    Former-commit-id: cbc4b370a6ea49886f0d06f1937d293dc08d8f7a

--- plugins/inputs/rabbitmq/exchange.go
+++ plugins/inputs/rabbitmq/exchange.go
@@ -1,28 +1,33 @@
 package rabbitmq
 
 import (
-	"time"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 	"strconv"
+	"time"
 )
 
 func getExchange(n *Input) {
-	exchanges := make([]Exchange, 0)
+	var exchanges []Exchange
 	err := n.requestJSON("/api/exchanges", &exchanges)
 	if err != nil {
 		l.Error(err.Error())
 		return
 	}
+	ts := time.Now()
 	for _, exchange := range exchanges {
+		if exchange.Name == "" {
+			exchange.Name = "(AMQP default)"
+		}
+
 		tags := map[string]string{
-			"url":      n.Url,
-			"exchange": exchange.Name,
-			"type":     exchange.Type,
-			"vhost":    exchange.Vhost,
-			"internal":    strconv.FormatBool(exchange.Internal),
-			"durable":     strconv.FormatBool(exchange.Durable),
-			"auto_delete": strconv.FormatBool(exchange.AutoDelete),
+			"url":           n.Url,
+			"exchange_name": exchange.Name,
+			"type":          exchange.Type,
+			"vhost":         exchange.Vhost,
+			"internal":      strconv.FormatBool(exchange.Internal),
+			"durable":       strconv.FormatBool(exchange.Durable),
+			"auto_delete":   strconv.FormatBool(exchange.AutoDelete),
 		}
 		fields := map[string]interface{}{
 			"message_ack_count":                    exchange.MessageStats.Ack,
@@ -40,6 +45,13 @@ func getExchange(n *Input) {
 			"message_return_unroutable_count":      exchange.MessageStats.ReturnUnroutable,
 			"message_return_unroutable_count_rate": exchange.MessageStats.ReturnUnroutableDetails.Rate,
 		}
+		metric := &ExchangeMeasurement{
+			name:   ExchangeMetric,
+			tags:   tags,
+			fields: fields,
+			ts:     ts,
+		}
+		metricAppend(metric)
 	}
 }
 
@@ -56,39 +68,32 @@ func (m *ExchangeMeasurement) LineProto() (*io.Point, error) {
 
 func (m *ExchangeMeasurement) Info() *inputs.MeasurementInfo {
 	return &inputs.MeasurementInfo{
-		Name: "",
+		Name: ExchangeMetric,
 		Fields: map[string]*inputs.FieldInfo{
-			"object_totals_channels":    newCountFieldInfo("Total number of channels"),
-			"object_totals_connections": newCountFieldInfo("Total number of connections"),
-			"object_totals_consumers":   newCountFieldInfo("Total number of consumers"),
-			"object_totals_queues":      newCountFieldInfo("Total number of queues"),
-
-			"message_ack_count":                    newCountFieldInfo("Number of messages delivered to clients and acknowledged"),
-			"message_ack_rate":                     newRateFieldInfo("Rate of messages delivered to clients and acknowledged per second"),
-			"message_deliver_get_count":            newCountFieldInfo("Sum of messages delivered in acknowledgement mode to consumers, in no-acknowledgement mode to consumers, in acknowledgement mode in response to basic.get, and in no-acknowledgement mode in response to basic.get"),
-			"message_deliver_get_rate":             newRateFieldInfo("Rate per second of the sum of messages delivered in acknowledgement mode to consumers, in no-acknowledgement mode to consumers, in acknowledgement mode in response to basic.get, and in no-acknowledgement mode in response to basic.get "),
-			"message_publish_count":                newCountFieldInfo("Count of messages published"),
-			"message_publish_rate":                 newRateFieldInfo("Rate of messages published per second"),
-			"message_publish_in_count":             newCountFieldInfo("Count of messages published from channels into this overview"),
-			"message_publish_in_rate":              newRateFieldInfo("Rate of messages published from channels into this overview per sec "),
-			"message_publish_out_count":            newCountFieldInfo("Count of messages published from this overview into queues"),
-			"message_publish_out_rate":             newRateFieldInfo("Rate of messages published from this overview into queues per second"),
-			"message_redeliver_count":              newCountFieldInfo("Count of subset of messages in deliver_get which had the redelivered flag set"),
-			"message_redeliver_rate":               newRateFieldInfo("Rate of subset of messages in deliver_get which had the redelivered flag set per second"),
-			"message_return_unroutable_count_rate": newRateFieldInfo("Rate of messages returned to publisher as unroutable per second"),
-			"message_return_unroutable_count":      newCountFieldInfo("Count of messages returned to publisher as unroutable "),
-
-			"queue_totals_messages_count":                newCountFieldInfo("Total number of messages (ready plus unacknowledged)"),
-			"queue_totals_messages_rate":                 newRateFieldInfo("Total rate of messages (ready plus unacknowledged)"),
-			"queue_totals_messages_ready_count":          newCountFieldInfo("Number of messages ready for delivery "),
-			"queue_totals_messages_ready_rate":           newRateFieldInfo("Rate of number of messages ready for delivery"),
-			"queue_totals_messages_unacknowledged_count": newCountFieldInfo("Number of unacknowledged messages"),
-			"queue_totals_messages_unacknowledged_rate":  newRateFieldInfo("Rate of number of unacknowledged messages"),
+			"message_ack_count":                    newCountFieldInfo("Number of messages in exchanges delivered to clients and acknowledged"),
+			"message_ack_rate":                     newRateFieldInfo("Rate of messages in exchanges delivered to clients and acknowledged per second"),
+			"message_deliver_get_count":            newCountFieldInfo("Sum of messages in exchanges delivered in acknowledgement mode to consumers, in no-acknowledgement mode to consumers, in acknowledgement mode in response to basic.get, and in no-acknowledgement mode in response to basic.get"),
+			"message_deliver_get_rate":             newRateFieldInfo("Rate per second of the sum of exchange messages delivered in acknowledgement mode to consumers, in no-acknowledgement mode to consumers, in acknowledgement mode in response to basic.get, and in no-acknowledgement mode in response to basic.get"),
+			"message_publish_count":                newCountFieldInfo("Count of messages in exchanges published"),
+			"message_publish_rate":                 newRateFieldInfo("Rate of messages in exchanges published per second"),
+			"message_publish_in_count":             newCountFieldInfo("Count of messages published from channels into this exchange"),
+			"message_publish_in_rate":              newRateFieldInfo("Rate of messages published from channels into this exchange per sec"),
+			"message_publish_out_count":            newCountFieldInfo("Count of messages published from this exchange into queues"),
+			"message_publish_out_rate":             newRateFieldInfo("Rate of messages published from this exchange into queues per second"),
+			"message_redeliver_count":              newCountFieldInfo("Count of subset of messages in exchanges in deliver_get which had the redelivered flag set"),
+			"message_redeliver_rate":               newRateFieldInfo("Rate of subset of messages in exchanges in deliver_get which had the redelivered flag set per second"),
+			"message_return_unroutable_count_rate": newRateFieldInfo("Rate of messages in exchanges returned to publisher as unroutable per second"),
+			"message_return_unroutable_count":      newCountFieldInfo("Count of messages in exchanges returned to publisher as unroutable"),
 		},
+
 		Tags: map[string]*inputs.TagInfo{
-			"url":              inputs.NewTagInfo("rabbitmq url"),
-			"rabbitmq_version": inputs.NewTagInfo("rabbitmq version"),
-			"cluster_name":     inputs.NewTagInfo("rabbitmq cluster name"),
+			"url":           inputs.NewTagInfo("rabbitmq url"),
+			"exchange_name": inputs.NewTagInfo("rabbitmq exchange name"),
+			"type":          inputs.NewTagInfo("rabbitmq exchange type"),
+			"vhost":         inputs.NewTagInfo("rabbitmq exchange virtual hosts"),
+			"internal":      inputs.NewTagInfo("If set, the exchange may not be used directly by publishers, but only when bound to other exchanges. Internal exchanges are used to construct wiring that is not visible to applications"),
+			"durable":       inputs.NewTagInfo("If set when creating a new exchange, the exchange will be marked as durable. Durable exchanges remain active when a server restarts. Non-durable exchanges (transient exchanges) are purged if/when a server restarts."),
+			"auto_delete":   inputs.NewTagInfo("If set, the exchange is deleted when all queues have finished using it"),
 		},
 	}
 }
