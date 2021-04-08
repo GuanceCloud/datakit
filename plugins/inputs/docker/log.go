421a4897ec5cc5b458bb49f5f92fe9d406476fd5
liguozhuang <liguozhuang@jiagouyun.com>
2021-04-08 18:35:11 +0800

    docker容器相关日志采集已编译通过，正在完善测试和文档(ongoing)
     [formerly 1c1166afab88ba22056b3568c1621cc3bf5747f1 [formerly 0949a36fd6d9259f7eccc61a4e7b3b9549e22593] [formerly 1c1166afab88ba22056b3568c1621cc3bf5747f1 [formerly 0949a36fd6d9259f7eccc61a4e7b3b9549e22593] [formerly 1c1166afab88ba22056b3568c1621cc3bf5747f1 [formerly 0949a36fd6d9259f7eccc61a4e7b3b9549e22593] [formerly 7f9c90acb3d0c5e31dd643044a067f9453eda031 [formerly bb991e060e2624041b3db828cb54fda592d8c311]]]]]
    
    
    
    Former-commit-id: f5a44c815479b85ae3b45451591c0990fd6d9366 [formerly f8e2228bb4a3808e3ef851e1253537d42a955c8c]
    Former-commit-id: b6879193060b091717f3b225e5a2accfd4174eb1 [formerly 51e41591aade2ccd23f24710e64b073014a9294b] [formerly b1f37a44c9124e331a9df342fdafebe7d7da1b6d [formerly 134cf599c4a3092a4f042dfeaf46dcfec4b5ac3a]]
    Former-commit-id: 7ceff50fcb9566b5a4ad7d4e6e619e150ee3e2ce [formerly 01d1dc4316db421ed5d90c5afd41bec20ec8c315]
    Former-commit-id: ee4c6a911be44153fdfba37f2b02d1ce0cb17d25

--- plugins/inputs/docker/log.go
+++ plugins/inputs/docker/log.go
@@ -4,11 +4,8 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"crypto/tls"
 	"fmt"
 	"io"
-	"os"
-	"path/filepath"
 	"reflect"
 	"strings"
 	"sync"
@@ -16,125 +13,37 @@ import (
 	"unicode"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/pkg/stdcopy"
 
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	iod "gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/pipeline"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 const (
-
 	// Maximum bytes of a log line before it will be split, size is mirroring
 	// docker code:
 	// https://github.com/moby/moby/blob/master/daemon/logger/copier.go#L21
 	maxLineBytes = 16 * 1024
-
-	updateInterval = 5 * time.Second
 )
 
-var (
-	containerStates = []string{"created", "restarting", "running", "removing", "paused", "exited", "dead"}
-)
-
-type DockerLogs struct {
-	Endpoint              string            `toml:"endpoint"`
-	FromBeginning         bool              `toml:"from_beginning"`
-	Timeout               string            `toml:"timeout"`
-	LabelInclude          []string          `toml:"docker_label_include"`
-	LabelExclude          []string          `toml:"docker_label_exclude"`
-	ContainerInclude      []string          `toml:"container_name_include"`
-	ContainerExclude      []string          `toml:"container_name_exclude"`
-	ContainerStateInclude []string          `toml:"container_state_include"`
-	ContainerStateExclude []string          `toml:"container_state_exclude"`
-	Source                string            `toml:"source"`
-	PipelinePath          string            `toml:"pipeline_path"`
-	IncludeSourceTag      bool              `toml:"source_tag"`
-	Tags                  map[string]string `toml:"tags"`
-
-	timeoutDuration time.Duration
-	ClientConfig
-
-	newEnvClient func() (Client, error)
-	newClient    func(string, *tls.Config) (Client, error)
-
-	client          Client
-	labelFilter     Filter
-	containerFilter Filter
-	stateFilter     Filter
-	opts            types.ContainerListOptions
-	wg              sync.WaitGroup
-	mu              sync.Mutex
-	containerList   map[string]context.CancelFunc
-
-	pipe *pipeline.Pipeline
-}
-
-func (*DockerLogs) SampleConfig() string {
-	return sampleCfg
-}
-
-func (*DockerLogs) Catalog() string {
-	return "docker"
-}
-
-func (d *DockerLogs) Run() {
-	l = logger.SLogger(inputName)
-
-	if d.initCfg() {
-		return
-	}
-
-	ticker := time.NewTicker(updateInterval)
-	defer ticker.Stop()
-
-	l.Info("dockerlog input start")
-	for {
-		select {
-		case <-datakit.Exit.Wait():
-			d.Stop()
-			l.Info("exit")
-			return
-
-		case <-ticker.C:
-			d.gather()
-		}
-	}
-}
-
-func (d *DockerLogs) gather() {
+func (this *Inputs) gatherLog() {
 	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, d.timeoutDuration)
+	ctx, cancel := context.WithTimeout(ctx, this.timeoutDuration)
 	defer cancel()
-	containers, err := d.client.ContainerList(ctx, d.opts)
+
+	cList, err := this.client.ContainerList(ctx, this.opts)
 	if err != nil {
 		l.Error(err)
 		return
 	}
 
-	for _, container := range containers {
-		if d.containerInContainerList(container.ID) {
-			continue
-		}
-
-		containerName := d.matchedContainerName(container.Names)
-		if containerName == "" {
-			continue
-		}
-
-		ctx, cancel := context.WithCancel(context.Background())
-		d.addToContainerList(container.ID, cancel)
-
+	for _, container := range cList {
 		// Start a new goroutine for every new container that has logs to collect
-		d.wg.Add(1)
+		this.wg.Add(1)
 		go func(container types.Container) {
-			defer d.wg.Done()
-			defer d.removeFromContainerList(container.ID)
+			defer this.wg.Done()
 
-			err = d.tailContainerLogs(ctx, container, containerName)
+			err = this.tailContainerLogs(ctx, container)
 			if err != nil && err != context.Canceled {
 				l.Error(err)
 			}
@@ -142,51 +51,26 @@ func (d *DockerLogs) gather() {
 	}
 }
 
-func (d *DockerUtil) tailContainerLogs(ctx context.Context, container types.Container, containerName string) error {
+func (this *Inputs) tailContainerLogs(ctx context.Context, container types.Container) error {
 	imageName, imageVersion := ParseImage(container.Image)
+	containerName := getContainerName(container.Names)
+
 	tags := map[string]string{
 		"container_name":    containerName,
 		"container_image":   imageName,
 		"container_version": imageVersion,
-		"endpoint":          d.Endpoint,
+		"endpoint":          this.Endpoint,
 	}
-
-	for k, v := range d.Tags {
+	for k, v := range this.Tags {
 		tags[k] = v
 	}
 
-	measurement := containerName
-	if d.Source != "" {
-		measurement = d.Source
-	}
-
-	// Add matching container labels as tags
-	for k, label := range container.Labels {
-		if d.labelFilter.Match(k) {
-			tags[k] = label
-		}
-	}
-
-	hasTTY, err := d.hasTTY(ctx, container)
+	hasTTY, err := this.hasTTY(ctx, container)
 	if err != nil {
 		return err
 	}
 
-	tail := "0"
-	if d.FromBeginning {
-		tail = "all"
-	}
-
-	logOptions := types.ContainerLogsOptions{
-		ShowStdout: true,
-		ShowStderr: true,
-		Timestamps: true,
-		Details:    false,
-		Follow:     true,
-		Tail:       tail,
-	}
-
-	logReader, err := d.client.ContainerLogs(ctx, container.ID, logOptions)
+	logReader, err := this.client.ContainerLogs(ctx, container.ID, this.containerLogsOptions)
 	if err != nil {
 		return err
 	}
@@ -197,11 +81,132 @@ func (d *DockerUtil) tailContainerLogs(ctx context.Context, container types.Cont
 	//
 	// If the container is *not* using a TTY, streams for stdout and stderr are
 	// multiplexed.
-	if hasTTY {
-		return tailStream(measurement, tags, container.ID, logReader, "tty", d.pipe)
-	} else {
-		return tailMultiplexed(measurement, tags, container.ID, logReader, d.pipe)
+
+	for _, opt := range this.LogOption {
+		if opt.nameCompile != nil && opt.nameCompile.MatchString(containerName) {
+			if hasTTY {
+				return tailStream(logReader, "tty", container, opt, tags)
+			} else {
+				return tailMultiplexed(logReader, container, opt, tags)
+			}
+		}
 	}
+
+	if hasTTY {
+		return tailStream(logReader, "tty", container, nil, tags)
+	} else {
+		return tailMultiplexed(logReader, container, nil, tags)
+	}
+
+}
+
+func (this *Inputs) hasTTY(ctx context.Context, container types.Container) (bool, error) {
+	ctx, cancel := context.WithTimeout(ctx, this.timeoutDuration)
+	defer cancel()
+	c, err := this.client.ContainerInspect(ctx, container.ID)
+	if err != nil {
+		return false, err
+	}
+	return c.Config.Tty, nil
+}
+
+func tailStream(reader io.ReadCloser, stream string, container types.Container, opt *LogOption, baseTags map[string]string) error {
+	defer reader.Close()
+
+	tags := make(map[string]string, len(baseTags)+1)
+	for k, v := range baseTags {
+		tags[k] = v
+	}
+	tags["stream"] = stream
+
+	r := bufio.NewReaderSize(reader, maxLineBytes)
+
+	for {
+		line, err := r.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+
+		if len(line) == 0 {
+			continue
+		}
+
+		ts, message, err := parseLine(line)
+		if err != nil {
+			l.Error(err)
+			continue
+		}
+
+		var fields = make(map[string]interface{})
+
+		measurement := getContainerName(container.Names)
+		if opt != nil {
+			if pipe := opt.pipelinePool.Get(); pipe != nil {
+				fields, err = pipe.(*pipeline.Pipeline).Run(message).Result()
+				if err != nil {
+					l.Errorf("run pipeline error, %s", err)
+					continue
+				}
+			}
+			fields["service"] = opt.Service
+		} else {
+			fields["message"] = message
+		}
+
+		if v, ok := fields["time"]; ok { // time should be nano-second
+			nanots, ok := v.(int64)
+			if !ok {
+				l.Warn("filed `time' should be nano-second, but got `%s'", reflect.TypeOf(v).String())
+				continue
+			}
+
+			ts = time.Unix(nanots/int64(time.Second), nanots%int64(time.Second))
+			delete(fields, "time")
+		}
+
+		pt, err := iod.MakePoint(measurement, tags, fields, ts)
+		if err != nil {
+			l.Error(err)
+		} else {
+			if err := iod.Feed(inputName, iod.Logging, []*iod.Point{pt}, &iod.Option{HighFreq: true}); err != nil {
+				l.Error(err)
+			}
+		}
+	}
+}
+
+func tailMultiplexed(src io.ReadCloser, container types.Container, opt *LogOption, baseTags map[string]string) error {
+	outReader, outWriter := io.Pipe()
+	errReader, errWriter := io.Pipe()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := tailStream(outReader, "stdout", container, opt, baseTags)
+		if err != nil {
+			l.Error(err)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := tailStream(errReader, "stderr", container, opt, baseTags)
+		if err != nil {
+			l.Error(err)
+		}
+	}()
+
+	_, err := stdcopy.StdCopy(outWriter, errWriter, src)
+	outWriter.Close()
+	errWriter.Close()
+	src.Close()
+	wg.Wait()
+	return err
 }
 
 func parseLine(line []byte) (time.Time, string, error) {
@@ -226,134 +231,6 @@ func parseLine(line []byte) (time.Time, string, error) {
 	}
 
 	return ts, string(message), nil
-}
-
-func tailStream(measurement string, baseTags map[string]string, containerID string, reader io.ReadCloser, stream string, pipe *pipeline.Pipeline) error {
-	defer reader.Close()
-
-	tags := make(map[string]string, len(baseTags)+1)
-	for k, v := range baseTags {
-		tags[k] = v
-	}
-	tags["stream"] = stream
-
-	r := bufio.NewReaderSize(reader, maxLineBytes)
-
-	var pts []*iod.Point
-
-	for {
-		line, err := r.ReadBytes('\n')
-		if err != nil {
-			if err == io.EOF {
-				return nil
-			}
-			return err
-		}
-
-		if len(line) == 0 {
-			continue
-		}
-
-		ts, message, err := parseLine(line)
-		if err != nil {
-			l.Error(err)
-			continue
-		}
-
-		var fields = make(map[string]interface{})
-
-		if pipe != nil {
-			fields, err = pipe.Run(message).Result()
-			if err != nil {
-				l.Errorf("run pipeline error, %s", err)
-				continue
-			}
-		} else {
-			fields["message"] = message
-		}
-
-		if _, ok := fields["container_id"]; !ok {
-			fields["container_id"] = containerID
-		}
-
-		if v, ok := fields["time"]; ok { // time should be nano-second
-			nanots, ok := v.(int64)
-			if !ok {
-				l.Warn("filed `time' should be nano-second, but got `%s'", reflect.TypeOf(v).String())
-				continue
-			}
-
-			ts = time.Unix(nanots/int64(time.Second), nanots%int64(time.Second))
-			delete(fields, "time")
-		}
-
-		pt, err := iod.MakePoint(measurement, tags, fields, ts)
-		if err != nil {
-			l.Error(err)
-		} else {
-			pts = append(pts, pt)
-		}
-	}
-
-	if err := iod.Feed(inputName, iod.Logging, pts, &iod.Option{HighFreq: true}); err != nil {
-		l.Error(err)
-		return err
-	}
-	return nil
-}
-
-func tailMultiplexed(measurement string, tags map[string]string, containerID string, src io.ReadCloser, pipe *pipeline.Pipeline) error {
-	outReader, outWriter := io.Pipe()
-	errReader, errWriter := io.Pipe()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err := tailStream(measurement, tags, containerID, outReader, "stdout", pipe)
-		if err != nil {
-			l.Error(err)
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err := tailStream(measurement, tags, containerID, errReader, "stderr", pipe)
-		if err != nil {
-			l.Error(err)
-		}
-	}()
-
-	_, err := stdcopy.StdCopy(outWriter, errWriter, src)
-	outWriter.Close()
-	errWriter.Close()
-	src.Close()
-	wg.Wait()
-	return err
-}
-
-func (d *DockerLogs) matchedContainerName(names []string) string {
-	// Check if all container names are filtered; in practice I believe
-	// this array is always of length 1.
-	for _, name := range names {
-		trimmedName := strings.TrimPrefix(name, "/")
-		match := d.containerFilter.Match(trimmedName)
-		if match {
-			return trimmedName
-		}
-	}
-	return ""
-}
-
-func (d *DockerLogs) hasTTY(ctx context.Context, container types.Container) (bool, error) {
-	ctx, cancel := context.WithTimeout(ctx, d.timeoutDuration)
-	defer cancel()
-	c, err := d.client.ContainerInspect(ctx, container.ID)
-	if err != nil {
-		return false, err
-	}
-	return c.Config.Tty, nil
 }
 
 // Adapts some of the logic from the actual Docker library's image parsing
