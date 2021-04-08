802b56f5aefff8399c83157967ab2b0f7323745f
liguozhuang <liguozhuang@jiagouyun.com>
2021-04-08 18:35:11 +0800

    docker容器相关日志采集已编译通过，正在完善测试和文档(ongoing)
    Former-commit-id: 7b48537bc26b274f7d1fc4df64ec7521f65471a8
    Former-commit-id: 6accc04e48143ee9e48d99a15b9c928d592174f5 [formerly 5e13f439766dd1ab13130be5c12f6a93cbc041ba]
    Former-commit-id: 691c42909c5737db84ab5bbb530811a57d420808

--- plugins/inputs/docker/config.go
+++ plugins/inputs/docker/config.go
@@ -1,9 +1,14 @@
 package docker
 
 import (
+	"regexp"
+	"sync"
 	"time"
 
+	"github.com/docker/docker/api/types"
+
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/pipeline"
 )
 
 const (
@@ -21,6 +26,7 @@ const (
     collect_object_interval = "5m"
 
     collect_logging = true
+    collect_logging_from_beginning = false
 
     # Is all containers, Return all containers. By default, only running containers are shown.
     include_exited = false
@@ -32,7 +38,7 @@ const (
     ## Use TLS but skip chain & host verification
     # insecure_skip_verify = false
 
-    [[inputs.docker.log_pipeline]]
+    [[inputs.docker.log_option]]
 	# container_name_match = "<regexp-container-name>"
         # source = "<your-source>"
         # service = "<your-service>"
@@ -49,56 +55,90 @@ const (
 
 var l = logger.DefaultSLogger(inputName)
 
-type DockerLogPipeline struct {
-	ContainerNameMatch string `toml:"container_name_match"`
-	Source             string `toml:"source"`
-	Service            string `toml:"service"`
-	Pipeline           string `toml:"pipeline"`
+type LogOption struct {
+	NameMatch string `toml:"container_name_match"`
+	Source    string `toml:"source"`
+	Service   string `toml:"service"`
+	Pipeline  string `toml:"pipeline"`
+
+	pipelinePool sync.Pool
+	nameCompile  *regexp.Regexp
 }
 
-func (d *DockerUtil) loadCfg() (err error) {
+func (this *Inputs) loadCfg() (err error) {
 	// new docker client
-	if d.Endpoint == "ENV" {
-		d.client, err = d.newEnvClient()
+	if this.Endpoint == "ENV" {
+		this.client, err = this.newEnvClient()
 		return
 	}
 	// tlsConfig 可以为空指针，即没有配置tls
-	tlsConfig, _err := d.ClientConfig.TLSConfig()
+	tlsConfig, _err := this.ClientConfig.TLSConfig()
 	if _err != nil {
 		return _err
 	}
 
-	d.client, err = d.newClient(d.Endpoint, tlsConfig)
+	this.client, err = this.newClient(this.Endpoint, tlsConfig)
 	if err != nil {
 		return
 	}
 
-	d.collectMetricDuration, err = time.ParseDuration(d.CollectMetricInterval)
+	this.collectMetricDuration, err = time.ParseDuration(this.CollectMetricInterval)
 	if err != nil {
 		return
 	}
 
-	d.collectObjectDuration, err = time.ParseDuration(d.CollectObjectInterval)
+	this.collectObjectDuration, err = time.ParseDuration(this.CollectObjectInterval)
 	if err != nil {
 		return
 	}
 
 	// 限制最小采集间隔
-	if 0 < d.collectMetricDuration &&
-		d.collectMetricDuration < minimumCollectMetricDuration {
+	if 0 < this.collectMetricDuration &&
+		this.collectMetricDuration < minimumCollectMetricDuration {
 		l.Warn("invalid collect_metric_interval, cannot be less than 5s. Use default interval 5s")
-		d.collectMetricDuration = minimumCollectMetricDuration
+		this.collectMetricDuration = minimumCollectMetricDuration
 	}
 
-	if 0 < d.collectObjectDuration &&
-		d.collectObjectDuration < minimumCollectObjectDuration {
+	if 0 < this.collectObjectDuration &&
+		this.collectObjectDuration < minimumCollectObjectDuration {
 		l.Warn("invalid collect_object_interval, cannot be less than 5m. Use default interval 5m")
-		d.collectObjectDuration = minimumCollectObjectDuration
+		this.collectObjectDuration = minimumCollectObjectDuration
 	}
 
-	if d.Tags == nil {
-		d.Tags = make(map[string]string)
+	if this.Tags == nil {
+		this.Tags = make(map[string]string)
 	}
+
+	for _, opt := range this.LogOption {
+		// FIXME:
+		//   source 为空时，应该使用 defalut，还是 container_name ?
+		//   偏向后者。此处使用 default
+		if opt.Source == "" {
+			opt.Source = "default"
+		}
+
+		if opt.Service == "" {
+			opt.Service = opt.Source
+		}
+
+		opt.nameCompile, err = regexp.Compile(opt.NameMatch)
+		if err != nil {
+			return
+		}
+		opt.pipelinePool = sync.Pool{
+			New: func() interface{} {
+				p, err := pipeline.NewPipelineFromFile(opt.Pipeline)
+				if err != nil {
+					l.Error(err)
+					return nil
+				}
+				return p
+			},
+		}
+	}
+
+	this.timeoutDuration = defaultAPITimeout
+	this.opts = types.ContainerListOptions{All: this.IncludeExited}
 
 	return
 }
