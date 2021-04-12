cf120b830ed289d2e21008e39f65b134f6a19562
vircoys <vircoys@gmail.com>
2021-04-12 11:39:26 +0800

    swap 采集器
    Former-commit-id: d01aa60d80318bfdd6d15c13899be70bae2fb3d1 [formerly 19cbeb56391247f8f0f83e2d53794b5e1ac8e35a] [formerly 27c052b227a9a3156f79e90ba7d6a843f5baf259 [formerly d3cfd8746ad932f32646095d17d3f3af3b30e9e3]]
    Former-commit-id: 569536a39e59294919364e7a99a9a2d6aaaf18c0 [formerly 070af6114087153a72cecabf09e352b1c023e66b]
    Former-commit-id: 5a7a75123ad09bf10120e7346f62b37bb127c91e

---
+++ plugins/inputs/swap/swap_test.go
@@ -0,0 +1,56 @@
+package swap
+
+import (
+	"testing"
+
+	"github.com/shirou/gopsutil/mem"
+)
+
+func TestCollec(t *testing.T) {
+	i := &Input{swapStat: PSSwapStat4Test}
+	if err := i.Collect(); err != nil {
+		t.Errorf("collect swap stat error:%s", err)
+	}
+
+	expected := map[string]interface{}{
+		"total":        uint64(111),
+		"used":         uint64(222),
+		"free":         uint(333),
+		"used_percent": float64(50),
+		"sin":          uint64(100),
+		"sout":         uint64(100),
+	}
+
+	actual := i.collectCacheLast1Ptr.(*swapMeasurement).fields
+	AssertFields(t, actual, expected)
+
+}
+
+func PSSwapStat4Test() (*mem.SwapMemoryStat, error) {
+	return &mem.SwapMemoryStat{
+		Total:       111,
+		Used:        222,
+		Free:        333,
+		UsedPercent: 50,
+		Sin:         100,
+		Sout:        100,
+	}, nil
+}
+
+func AssertFields(t *testing.T, actual, expected map[string]interface{}) {
+	for k := range expected {
+		ed := expected[k]
+		if al, ok := actual[k]; ok {
+			switch ed.(type) {
+			case uint64:
+				if ed.(uint64) != al.(uint64) {
+					t.Errorf("error: "+k+" expected: %f \t actual %f", ed.(uint64), al.(uint64))
+				}
+			case float64:
+				if ed.(float64) != al.(float64) {
+					t.Errorf("error: "+k+" expected: %f \t actual %f", ed.(float64), al.(float64))
+				}
+			}
+		}
+	}
+}
