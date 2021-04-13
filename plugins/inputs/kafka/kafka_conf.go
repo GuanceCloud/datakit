21aa598c53f03c0bb5f24a666e4bc34905cd5cdc
huangbo <huangbo@jiagouyun.com>
2021-04-13 17:57:43 +0800

    kafka采集器使用telegraf方式
    Former-commit-id: 8cca6f030bf758be4d9ddf9dad22449f61e77d63 [formerly fdc21576d999205d5f40100176cb4541c6943a64] [formerly 0c20f83f9177889b979ace7b9f9cb22d84c88dab [formerly 3ba9df9f1392c8d480cf2a04c842eccf29396009]]
    Former-commit-id: f3ea72dbde750138670d67cf93d8df611950f16e [formerly 2033f7cec62cbad713ae7ee15cbebeaf82981aa2]
    Former-commit-id: 4037bdc466218bb70cdb6c1e3d4a8c4793d06aa3

---
+++ plugins/inputs/kafka/kafka_conf.go
@@ -0,0 +1,76 @@
+package kafka
+
+const (
+	kafkaConfSample = `#[[inputs.kafka]]
+  # default_tag_prefix      = ""
+  # default_field_prefix    = ""
+  # default_field_separator = "."
+ 
+  # username = ""
+  # password = ""
+  # response_timeout = "5s"
+
+  ## Optional TLS config
+  # tls_ca   = "/var/private/ca.pem"
+  # tls_cert = "/var/private/client.pem"
+  # tls_key  = "/var/private/client-key.pem"
+  # insecure_skip_verify = false
+
+  ## Monitor Intreval
+  # interval   = "60s"
+  
+  # Add agents URLs to query
+  #urls = ["http://localhost:8080/jolokia"]
+
+  ## Add metrics to read
+  #[[inputs.kafka.metric]]
+  #  name         = "controller"
+  #  mbean        = "kafka.controller:name=*,type=*"
+  #  field_prefix = "$1."
+
+  #[[inputs.kafka.metric]]
+  #  name         = "replica_manager"
+  #  mbean        = "kafka.server:name=*,type=ReplicaManager"
+  #  field_prefix = "$1."
+
+  #[[inputs.kafka.metric]]
+  #  name         = "purgatory"
+  #  mbean        = "kafka.server:delayedOperation=*,name=*,type=DelayedOperationPurgatory"
+  #  field_prefix = "$1."
+  #  field_name   = "$2"
+
+  #[[inputs.kafka.metric]]
+  #  name     = "client"
+  #  mbean    = "kafka.server:client-id=*,type=*"
+  #  tag_keys = ["client-id", "type"]
+
+  #[[inputs.kafka.metric]]
+  #  name         = "request"
+  #  mbean        = "kafka.network:name=*,request=*,type=RequestMetrics"
+  #  field_prefix = "$1."
+  #  tag_keys     = ["request"]
+
+  #[[inputs.kafka.metric]]
+  #  name         = "topics"
+  #  mbean        = "kafka.server:name=*,type=BrokerTopicMetrics"
+  #  field_prefix = "$1."
+
+  #[[inputs.kafka.metric]]
+  #  name         = "topic"
+  #  mbean        = "kafka.server:name=*,topic=*,type=BrokerTopicMetrics"
+  #  field_prefix = "$1."
+  #  tag_keys     = ["topic"]
+
+  #[[inputs.kafka.metric]]
+  #  name       = "partition"
+  #  mbean      = "kafka.log:name=*,partition=*,topic=*,type=Log"
+  #  field_name = "$1"
+  #  tag_keys   = ["topic", "partition"]
+
+  #[[inputs.kafka.metric]]
+  #  name       = "partition"
+  #  mbean      = "kafka.cluster:name=UnderReplicated,partition=*,topic=*,type=Partition"
+  #  field_name = "UnderReplicatedPartitions"
+  #  tag_keys   = ["topic", "partition"]
+`
+)
