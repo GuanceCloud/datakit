d82fb8cc317735501f30c1f1449f3c529bed4fb1
liushaobo <liushaobo@jiagouyun.com>
2021-04-13 18:29:14 +0800

    添加日志
    Former-commit-id: a047e9269b625d7a476b83975a29c01bd8612e70 [formerly 3c0ece26513691230785b5526f5e5cc0ea923fad [formerly 76b78f5a4053e763a2ac7396c1fedfb72f882865] [formerly 54436f2850f9eb67367c61f3d5d38da476f491aa [formerly bd7094d6a6efef36279ad8b2492a9b80493440da]]]
    Former-commit-id: 18cbd2b05b7fa96bc3ceab71b51f87b79686e0b1
    Former-commit-id: 917610e2aa37e9a3dd39dfdf1d9b01b4c638ce72 [formerly 459c4a3cbb595827ef8f01019d7c93a8995bb45c]
    Former-commit-id: 8da658ba0f5d597a2249ed47c1803328cf8862d7

--- plugins/inputs/redis/conf.go
+++ plugins/inputs/redis/conf.go
@@ -38,8 +38,52 @@ const (
     ## Collect INFO COMMANDSTATS output as metrics.
     # command_stats = false
 
+    [inputs.redis.log]
+    # required, glob logfiles
+    files = ["/var/log/redis/*.log"]
+
+    # glob filteer
+    ignore = [""]
+
+    source = "redislog"
+
+    # add service tag, if it's empty, use $source.
+    service = "redislog"
+
+    # grok pipeline script path
+    pipeline = "redis.p"
+
+    # read file from beginning
+    # if from_begin was false, off auto discovery file
+    from_beginning = false
+
+    # optional encodings:
+    #    "utf-8", "utf-16le", "utf-16le", "gbk", "gb18030" or ""
+    character_encoding = ""
+
+    # The pattern should be a regexp. Note the use of '''this regexp'''
+    # regexp link: https://golang.org/pkg/regexp/syntax/#hdr-Syntax
+    match = '''^\S.*'''
+
+    [inputs.tailf.tags]
+    # tags1 = "value1"
+
     [inputs.redis.tags]
     # tag1 = val1
     # tag2 = val2
+`
+
+	pipelineCfg = `
+add_pattern("date2", "%{MONTHDAY} %{MONTH} %{YEAR}?%{TIME}")
+
+grok(_, "%{INT:pid}:%{WORD:role} %{date2:time} %{NOTSPACE:serverity} %{GREEDYDATA:msg}")
+
+group_in(serverity, ["."], "debug", status)
+group_in(serverity, ["-"], "verbose", status)
+group_in(serverity, ["*"], "notice", status)
+group_in(serverity, ["#"], "warning", status)
+
+cast(pid, "int")
+default_time(time)
 `
 )
