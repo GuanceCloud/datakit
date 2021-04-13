f859f7498efc7eccea749f86127312c5558fb06b
liushaobo <liushaobo@jiagouyun.com>
2021-04-13 18:29:14 +0800

    添加日志
    Former-commit-id: a047e9269b625d7a476b83975a29c01bd8612e70 [formerly 0a9b4ee31be1e440f1dba0883fc3b5b8883d6a10 [formerly 76b78f5a4053e763a2ac7396c1fedfb72f882865] [formerly 54436f2850f9eb67367c61f3d5d38da476f491aa [formerly 77de49347ae0bdd28b2bf3f7bdb0b75234c5cd0a]]]
    Former-commit-id: 18cbd2b05b7fa96bc3ceab71b51f87b79686e0b1
    Former-commit-id: 917610e2aa37e9a3dd39dfdf1d9b01b4c638ce72 [formerly 748f872d8b9e6b35ed1288c4372a41db85675a06]
    Former-commit-id: 8da658ba0f5d597a2249ed47c1803328cf8862d7

--- plugins/inputs/mysql/conf.go
+++ plugins/inputs/mysql/conf.go
@@ -18,6 +18,33 @@ const (
 
     interval = "10s"
 
+    [inputs.mysql.log]
+    # required, glob logfiles
+    files = ["/var/log/mysql/*.log"]
+
+    # glob filteer
+    ignore = [""]
+
+    source = "mysqlog"
+
+    # add service tag, if it's empty, use $source.
+    service = "mysqlog"
+
+    # grok pipeline script path
+    pipeline = "mysql.p"
+
+    # read file from beginning
+    # if from_begin was false, off auto discovery file
+    from_beginning = false
+
+    # optional encodings:
+    #    "utf-8", "utf-16le", "utf-16le", "gbk", "gb18030" or ""
+    character_encoding = ""
+
+    # The pattern should be a regexp. Note the use of '''this regexp'''
+    # regexp link: https://golang.org/pkg/regexp/syntax/#hdr-Syntax
+    match = '''^(# Time|\d{4}-\d{2}-\d{2}|\d{6}\s+\d{2}:\d{2}:\d{2}).*'''
+
     # [[inputs.mysql.custom_queries]]
     #     sql = "SELECT foo, COUNT(*) FROM table.events GROUP BY foo"
     #     metric = "xxxx"
@@ -27,5 +54,47 @@ const (
     [inputs.mysql.tags]
     # tag1 = val1
     # tag2 = val2
+`
+
+	pipelineCfg = `
+grok(_, "%{TIMESTAMP_ISO8601:time}\\s+%{INT:thread_id}\\s+%{WORD:operation}\\s+%{GREEDYDATA:raw_query}")
+grok(_, "%{TIMESTAMP_ISO8601:time} %{INT:thread_id} \\[%{NOTSPACE:status}\\] %{GREEDYDATA:msg}")
+
+add_pattern("date2", "%{YEAR}%{MONTHNUM2}%{MONTHDAY} %{TIME}")
+grok(_, "%{date2:time} \\s+(InnoDB:|\\[%{NOTSPACE:status}\\])\\s+%{GREEDYDATA:msg}")
+
+add_pattern("timeline", "# Time: %{TIMESTAMP_ISO8601:time}")
+add_pattern("userline", "# User@Host: %{NOTSPACE:db_user}\\s+@\\s(%{NOTSPACE:db_host})?(\\s+)?\\[(%{NOTSPACE:db_ip})?\\](\\s+Id:\\s+%{INT:query_id})?")
+add_pattern("kvline01", "# Query_time: %{NUMBER:query_time}\\s+Lock_time: %{NUMBER:lock_time}\\s+Rows_sent: %{INT:rows_sent}\\s+Rows_examined: %{INT:rows_examined}")
+
+add_pattern("kvline02", "# Thread_id: %{INT:thread_id}\\s+Killed: %{INT:killed}\\s+Errno: %{INT:errno}")
+add_pattern("kvline03", "# Bytes_sent: %{INT:bytes_sent}\\s+Bytes_received: %{INT:bytes_received}")
+
+# multi-line SQLs
+add_pattern("sqls", "(?s)(.*)")
+
+grok(_, "%{timeline}\\n%{userline}\\n%{kvline01}(\\n)?(%{kvline02})?(\\n)?(%{kvline03})?\\n%{sqls:db_slow_statement}")
+
+cast(thread_id, "int")
+cast(query_id, "int")
+cast(rows_sent, "int")
+cast(rows_examined, "int")
+
+cast(bytes_sent, "int")
+cast(bytes_received, "int")
+cast(killed, "int")
+cast(errno, "int")
+cast(query_timestamp, "int")
+cast(query_time, "float")
+cast(lock_time, "float")
+
+nullif(thread_id, 0)
+nullif(db_host, "")
+nullif(killed, 0)
+nullif(bytes_sent, 0)
+nullif(bytes_received, 0)
+nullif(errno, 0)
+
+default_time(time)
 `
 )
