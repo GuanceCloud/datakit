d20c89faf8a209a22c00facac6fb3cbfe5fe0eea
huangjinjin <huangjinjin@jiagouyun.com>
2021-04-14 17:14:01 +0800

    modify host_process metric name, delete processes
    Former-commit-id: e648093b575b135a2a92ef39f74f564c3d18ca0b [formerly 262c1b6d87872188d4471f513c19d33914f7f199] [formerly 062c3e360e65fe42304ef715e03e1d2521366cb4 [formerly 8f713191e6b1183aceccd57f96295c09842fa1ba]]
    Former-commit-id: 95828c532ffd1dd14a7447bf5596ce485254704f [formerly a0282ab228794f5b9bd20d2d2221ec1e4f71b29c]
    Former-commit-id: e516c5d84dd083d70d78732dec6ccb3b17be34f4

--- plugins/inputs/telegraf_inputs/tginputs_nonwin.go
+++ plugins/inputs/telegraf_inputs/tginputs_nonwin.go
@@ -3,14 +3,12 @@
 package telegraf_inputs
 
 import (
-	"github.com/influxdata/telegraf/plugins/inputs/processes"
 	"github.com/influxdata/telegraf/plugins/inputs/varnish"
 )
 
 var (
 	telegrafInputsNonWin = map[string]*TelegrafInput{ // Name: Catalog
 		"varnish":   {name: "varnish", Catalog: "varnish", Input: &varnish.Varnish{}},
-		"processes": {name: "processes", Catalog: "host", Input: &processes.Processes{}},
 	}
 )
 
