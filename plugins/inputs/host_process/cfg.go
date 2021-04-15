0e6ed229213b9b6aec73eb8fec8995baf2afb368
huangjinjin <huangjinjin@jiagouyun.com>
2021-04-15 16:35:47 +0800

    io add time cost
     [formerly c141b8635cf1c80ae6b361c8f631d91234c04b94 [formerly 0e32b95c2360bb7bf9b7495e919d59aded2f4f84] [formerly 63b67879debfcf1cc7ab8cab04e3a7e48939e983 [formerly ba8f8fd3df9304d2e96c0d7c909bffcbfc2d3871]] [formerly 63b67879debfcf1cc7ab8cab04e3a7e48939e983 [formerly ba8f8fd3df9304d2e96c0d7c909bffcbfc2d3871] [formerly 63b67879debfcf1cc7ab8cab04e3a7e48939e983 [formerly ba8f8fd3df9304d2e96c0d7c909bffcbfc2d3871] [formerly ce44d8f5d037d5d1badd850c9cfb8151e3f9751a [formerly 68957a3b06a0d4a92d3d4d548c446bc1ae04c3e2]]]]]
    
    
    
    Former-commit-id: 8fdd33d3f10bbea65fa7a52e3967815f57ba318b [formerly a8137212d10fee8ee4e21b77a2d0f79c2b1db8c6]
    Former-commit-id: 7eefed0e4c81107e8c1742afc77a28f1ec9d7474 [formerly ba7b079bc114c2d9226e733d5f511295cae655a1] [formerly 4710ed8823d7df56615e045e419f80270e262f34 [formerly e8e7f7c919b8c3a08089571881dc30a13a4d2904]]
    Former-commit-id: b31364ea420b6e106a8fa90536f4df4fb9995335 [formerly 943fccc2867c1bd5b391d8fc100baa482949a79a]
    Former-commit-id: 686017de1cf29fea7206f1b76307ad0489142402

--- plugins/inputs/host_process/cfg.go
+++ plugins/inputs/host_process/cfg.go
@@ -1,7 +1,11 @@
 package host_process
 
 import (
+	"time"
+
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 const (
@@ -23,7 +27,7 @@ const (
 	pipelineSample = ``
 )
 
-type Processes struct {
+type Input struct {
 	ProcessName    []string         `toml:"process_name,omitempty"`
 	ObjectInterval datakit.Duration `toml:"object_interval,omitempty"`
 	RunTime        datakit.Duration `toml:"min_run_time,omitempty"`
@@ -33,4 +37,42 @@ type Processes struct {
 
 	re     string
 	isTest bool
+}
+
+type ProcessMetric struct {
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+}
+
+func (m *ProcessMetric) LineProto() (*io.Point, error) {
+	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *ProcessMetric) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name:   "process",
+		Fields: map[string]interface{}{},
+		Tags:   map[string]interface{}{},
+	}
+}
+
+type ProcessObject struct {
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+}
+
+func (m *ProcessObject) LineProto() (*io.Point, error) {
+	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *ProcessObject) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name:   "process",
+		Fields: map[string]interface{}{},
+		Tags:   map[string]interface{}{},
+	}
 }
