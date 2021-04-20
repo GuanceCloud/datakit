152410114ff4f466e54dc5d40f2d262625c56240
tanbiao <tanbiao@jiagouyun.com>
2021-04-20 09:09:52 +0800

    upadte: fix /v1/write/metric API error; updadte http test case
     [formerly 33a8fdcc7080419ad3a7c5f038b6cbf987ac4b3b [formerly 934897dcb2d14c9c0c5ab6c1b06fa5bd12209620] [formerly d212d32fc4b0157dfa3a79945d4a09c5403bfad1] [formerly a5b81ed377fb5597432a94327b5479cd92ae50a3 [formerly 4e8e9228d1e9c11db468430bf315966f73b19c89] [formerly 4846d542a5f7f9947eb781293a2ef8c4110a4631 [formerly 1d20f00e471cf0379a6721ed1baf879281cae604]]]]
     [formerly d212d32fc4b0157dfa3a79945d4a09c5403bfad1]
    
    
    
    
    Former-commit-id: 057fa6447dd52f47486296027fac82ed75098dcf

--- http/http_test.go
+++ http/http_test.go
@@ -1,16 +1,17 @@
 package http
 
 import (
-	//"bytes"
-	//"encoding/json"
-	//"fmt"
-	//"io/ioutil"
-	//"net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	nhttp "net/http"
 	"testing"
 	"time"
-	//"gitlab.jiagouyun.com/cloudcare-tools/cliutils/testutil"
-	//"gitlab.jiagouyun.com/cloudcare-tools/datakit"
-	//"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/testutil"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
 var (
@@ -40,17 +41,15 @@ func TestReload(t *testing.T) {
 	t.Log("HTTP server stop ok")
 }
 
-/*
 func TestAPI(t *testing.T) {
-	type dkAPICase struct {
+
+	var cases = []struct {
 		api           string
 		body          []byte
 		method        string
 		gz            bool
 		expectErrCode string
-	}
-
-	var dkApiCases = []*dkAPICase{
+	}{
 		{
 			api:    "/v1/write/metric?name=test",
 			body:   []byte(`test,t1=abc f1=1i,f2=2,f3="str"`),
@@ -67,64 +66,62 @@ func TestAPI(t *testing.T) {
 		},
 	}
 
+	httpBind = __bind
 	io.SetTest()
-	datakit.Cfg.MainCfg.GinLog = "./gin.log"
+	ginLog = "./gin.log"
 
 	go func() {
-		HttpStart(__bind)
+		HttpStart()
 	}()
 
 	time.Sleep(time.Second)
-	runCases = func(t *testing.T, cases []*dkAPICase) {
 
-		httpCli := &http.Client{}
-		var err error
+	httpCli := &nhttp.Client{}
+	var err error
 
-		for i := len(cases) - 1; i >= 0; i-- {
-			tc := cases[i]
-			if tc.gz {
-				tc.body, err = datakit.GZip(tc.body)
-				if err != nil {
-					t.Fatal(err)
-				}
-			}
-
-			req, err := http.NewRequest(tc.method,
-				fmt.Sprintf("http://0.0.0.0%s%s", __bind, tc.api),
-				bytes.NewBuffer([]byte(tc.body)))
+	for i := len(cases) - 1; i >= 0; i-- {
+		tc := cases[i]
+		if tc.gz {
+			tc.body, err = datakit.GZip(tc.body)
 			if err != nil {
 				t.Fatal(err)
 			}
-
-			if tc.gz {
-				req.Header.Set("Content-Encoding", "gzip")
-			}
-
-			resp, err := httpCli.Do(req)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			respbody, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			var x struct {
-				ErrCode string `json:"error_code"`
-				Msg     string `json:"message"`
-			}
-
-			if len(respbody) > 0 {
-				if err := json.Unmarshal(respbody, &x); err != nil {
-					t.Fatal(err)
-				}
-
-				l.Debugf("x: %v, body: %s", x, string(respbody))
-			}
-			testutil.Equals(t, string(tc.expectErrCode), string(x.ErrCode))
-			t.Logf("[%d] ok", i)
 		}
+
+		req, err := nhttp.NewRequest(tc.method,
+			fmt.Sprintf("http://0.0.0.0%s%s", __bind, tc.api),
+			bytes.NewBuffer([]byte(tc.body)))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if tc.gz {
+			req.Header.Set("Content-Encoding", "gzip")
+		}
+
+		resp, err := httpCli.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		respbody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var x struct {
+			ErrCode string `json:"error_code"`
+			Msg     string `json:"message"`
+		}
+
+		if len(respbody) > 0 {
+			if err := json.Unmarshal(respbody, &x); err != nil {
+				t.Fatal(err)
+			}
+
+			l.Debugf("x: %v, body: %s", x, string(respbody))
+		}
+		testutil.Equals(t, string(tc.expectErrCode), string(x.ErrCode))
+		t.Logf("[%d] ok", i)
 	}
-	runCases(t, dkApiCases)
-} */
+}
