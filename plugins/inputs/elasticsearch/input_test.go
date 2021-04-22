3c36f832334c2be421eded22cb63c2d1f390c485
zheng bo <zhengb@jiagouyun.com>
2021-04-22 13:18:00 +0800

    update: elasticsearch test
    Former-commit-id: 008039c1bb919bf8288d2782cbade3d7f4a5abcb [formerly 033554b857c76f90b149b5370a84ef66a7655f37] [formerly 3477f042e541590b61543e71ec4a110b6308f9e1 [formerly a2c50464cfa2f1978ed8d97f732f1ab9e6e126e6]]
    Former-commit-id: 56e9499063e4ec2d09212a6c0b6bef8cc6f25de9 [formerly 753f9e2884e04fecccc291187bbc6a2d427e72f0]
    Former-commit-id: 0cc4715ca2ff3c4faf2ce6630c50f671e91a8336

--- plugins/inputs/elasticsearch/input_test.go
+++ plugins/inputs/elasticsearch/input_test.go
@@ -7,8 +7,10 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
@@ -74,6 +76,45 @@ func TestGatherNodeStats(t *testing.T) {
 	checkIsMaster(es, es.Servers[0], false, t)
 
 	AssertContainsTaggedFields(t, "elasticsearch_node_stats", nodestatsExpected, tags, es.collectCache)
+}
+
+func TestUtilDuration(t *testing.T) {
+	d := Duration{Duration: time.Second}
+	err := d.UnmarshalTOML([]byte("1s"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = d.UnmarshalTOML([]byte("1619059234299"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = d.UnmarshalTOML([]byte("\"1619059234299\""))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = d.UnmarshalTOML([]byte("1619059234299.001"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCollect(t *testing.T) {
+	es := newElasticsearchWithClient()
+	es.Servers = []string{url}
+	es.ClusterHealth = true
+	es.ClusterStats = true
+	es.ClusterHealthLevel = ""
+	es.client.Transport = newTransportMock(clusterHealthResponse)
+	es.serverInfo = make(map[string]serverInfo)
+	es.serverInfo[url] = defaultServerInfo()
+
+	err := es.Collect()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestGatherClusterHealthEmptyClusterHealth(t *testing.T) {
@@ -158,6 +199,78 @@ func TestGatherClusterHealthAlsoIndicesHealth(t *testing.T) {
 	AssertContainsTaggedFields(t, "elasticsearch_cluster_health_indices",
 		v2IndexExpected,
 		map[string]string{"index": "v2", "name": clusterName}, es.collectCache)
+}
+
+func TestGatherClusterIndicesStats(t *testing.T) {
+	es := newElasticsearchWithClient()
+	es.IndicesInclude = []string{"_all"}
+	es.Servers = []string{url}
+	es.client.Transport = newTransportMock(clusterIndicesResponse)
+	es.serverInfo = make(map[string]serverInfo)
+	es.serverInfo[url] = defaultServerInfo()
+
+	if err := es.gatherIndicesStats(""); err != nil {
+		t.Fatal(err)
+	}
+
+	AssertContainsTaggedFields(t, "elasticsearch_indices_stats",
+		clusterIndicesPrimariesExpected,
+		map[string]string{"index_name": "es"}, es.collectCache)
+}
+
+func TestGatherClusterIndiceShardsStats(t *testing.T) {
+	es := newElasticsearchWithClient()
+	es.IndicesLevel = "shards"
+	es.Servers = []string{url}
+	es.client.Transport = newTransportMock(clusterIndicesShardsResponse)
+	es.serverInfo = make(map[string]serverInfo)
+	es.serverInfo[url] = defaultServerInfo()
+
+	if err := es.gatherIndicesStats(""); err != nil {
+		t.Fatal(err)
+	}
+
+	AssertContainsTaggedFields(t, "elasticsearch_indices_stats_shards",
+		clusterIndicesPrimaryShardsExpected,
+		map[string]string{"index_name": "es", "node_id": "oqvR8I1dTpONvwRM30etww", "shard_name": "0", "type": "primary"},
+		es.collectCache)
+}
+
+func TestMapHealthStatusToCode(t *testing.T) {
+	assert.Equal(t, mapHealthStatusToCode("GREEN"), 1)
+	assert.Equal(t, mapHealthStatusToCode("YELLOW"), 2)
+	assert.Equal(t, mapHealthStatusToCode("RED"), 3)
+	assert.Equal(t, mapHealthStatusToCode("NULL"), 0)
+}
+
+func TestInput(t *testing.T) {
+	es := newElasticsearchWithClient()
+	assert.Equal(t, es.Catalog(), "elasticsearch")
+	assert.Equal(t, es.SampleConfig(), sampleConfig)
+
+	pipelineMap := es.PipelineConfig()
+	assert.Equal(t, pipelineMap["elasticsearch"], pipelineCfg)
+
+	assert.Equal(t, es.AvailableArchs(), datakit.AllArch)
+
+	samples := es.SampleMeasurement()
+	assert.Greater(t, len(samples), 0)
+}
+
+func TestMapShardStatusToCode(t *testing.T) {
+	assert.Equal(t, mapShardStatusToCode("UNASSIGNED"), 1)
+	assert.Equal(t, mapShardStatusToCode("INITIALIZING"), 2)
+	assert.Equal(t, mapShardStatusToCode("STARTED"), 3)
+	assert.Equal(t, mapShardStatusToCode("RELOCATING"), 4)
+	assert.Equal(t, mapShardStatusToCode("NULL"), 0)
+}
+
+func TestTlsConfig(t *testing.T) {
+	_, err := TLSConfig("", "", "")
+	if err == nil {
+		t.Fail()
+	}
+
 }
 
 func TestGatherClusterStatsMaster(t *testing.T) {
