684257a94e70c9dd7a4851610e1a0ac6ac2d291c
liguozhuang <liguozhuang@jiagouyun.com>
2021-04-26 19:34:20 +0800

    dk-election 01
     [formerly 83f368d9b78b4795a447bbcb8177792eaa878ea7 [formerly 781817b747fe01e04914390b52c21c22d2274f34 [formerly da9f620f8955d7cc920b971b81946567ec9212c3]] [formerly 781817b747fe01e04914390b52c21c22d2274f34 [formerly da9f620f8955d7cc920b971b81946567ec9212c3] [formerly 781817b747fe01e04914390b52c21c22d2274f34 [formerly da9f620f8955d7cc920b971b81946567ec9212c3] [formerly 50626a6d52850e73c956ab6e12b1eac63162ac94 [formerly b7d9a7d5e76ab0e69facb9272ecffdc888a0f4e4]]]]]
    
    
    
    Former-commit-id: 43c91db809051f7dc2da1fee61fcffffdae9297a [formerly 01a815ff73e061ef336639107748ecdb6569b77d]
    Former-commit-id: 2cd19c19a1602ba0a2309d8b312cd43b672e6225 [formerly 00345ef5c78ef349edfb1681b01c6a3e0ad95f48] [formerly e727ef29493191f920eb3f4fae49bc6e1fb44fab [formerly 87df8f870dc32c4b6494d36f7ea1704efe156364]]
    Former-commit-id: 6cf52fd19fc2fe147a67cc3ec089b90bbc1b0cf1 [formerly 6a7bd7785c3bf3b7b029b325cd03e21431e7f98e]
    Former-commit-id: d9f9488791121578451e0a26ec414af8947a1ada

--- cfg.go
+++ cfg.go
@@ -127,6 +127,8 @@ type MainConfig struct {
 	BlackList []*InputHostList `toml:"black_lists,omitempty"`
 	WhiteList []*InputHostList `toml:"white_lists,omitempty"`
 
+	EnableElection bool `toml:"enable_election,omitempty"`
+
 	EnableUncheckedInputs bool `toml:"enable_unchecked_inputs,omitempty"`
 }
 
