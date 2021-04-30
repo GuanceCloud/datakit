efb75f3eeca05cd949e4d562b34f75c1b7adf72d
huangbo <huangbo@jiagouyun.com>
2021-04-30 16:43:58 +0800

    datakit细节优化
    Former-commit-id: 22052e2a7b5b9b4e8875e530034c6abe6be56a48 [formerly df1f2e4f489101afa36eeca779601c94cec0df52]
    Former-commit-id: 1bc4d8e9c157f4f3bfdf9ce57e51f311175763cc

--- cmd/datakit/cmds/cmds.go
+++ cmd/datakit/cmds/cmds.go
@@ -1,7 +1,12 @@
 package cmds
 
 import (
+	"fmt"
 	"github.com/c-bata/go-prompt"
+	"github.com/kardianos/service"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	nhttp "net/http"
+	"runtime"
 )
 
 var (
@@ -24,5 +29,69 @@ func (c *completer) Complete(d prompt.Document) []prompt.Suggest {
 		return []prompt.Suggest{}
 	default:
 		return prompt.FilterFuzzy(suggestions, w, true)
+	}
+}
+
+func StopDatakit() {
+	svc, err := datakit.NewService()
+	if err != nil {
+		l.Errorf("new %s service failed: %s", runtime.GOOS, err.Error())
+		return
+	}
+
+	status, err := svc.Status()
+	if err != nil {
+		l.Warnf("get datakit service status: %s, ignored", err.Error())
+	}
+	if status == service.StatusStopped {
+		return
+	}
+
+	l.Info("stoping datakit...")
+	if err := service.Control(svc, "stop"); err != nil {
+		l.Warnf("stop service: %s, ignored", err.Error())
+	}
+	l.Info("stop datakit successful")
+}
+
+func StartDatakit() {
+	svc, err := datakit.NewService()
+	if err != nil {
+		l.Errorf("new %s service failed: %s", runtime.GOOS, err.Error())
+		return
+	}
+
+	status, err := svc.Status()
+	if err != nil {
+		l.Warnf("get datakit service status: %s, ignored", err.Error())
+	}
+	if status == service.StatusRunning {
+		l.Info("datakit service is already running")
+		return
+	}
+
+	if err := service.Control(svc, "start"); err != nil {
+		l.Warnf("start datakit service: %s, ignored", err.Error())
+	}
+
+	l.Info("start datakit successful")
+}
+
+func RestartDatakit() {
+	StopDatakit()
+	StartDatakit()
+}
+
+func ReloadDatakit(port int) {
+	resp, err := nhttp.Get(fmt.Sprintf("http://127.0.0.1:%d/reload", port))
+	if err != nil {
+		l.Warn(err)
+		return
+	}
+
+	if resp.StatusCode == 200 {
+		l.Info("datakit reload successful")
+	} else {
+		l.Warn("datakit reload failed: %d", resp.StatusCode)
 	}
 }
