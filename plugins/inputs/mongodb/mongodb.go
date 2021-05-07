21cedc5fea4a3794e9713a3ca147bb5490f91368
CodapeWild <codape.wild@gmail.com>
2021-05-07 10:48:08 +0800

    add mongodb measurement struct
    Former-commit-id: f83bc2d1edf330909e88c3bb330cc3273e28dc05
    Former-commit-id: 4d0c5427bad2a8ccd3827bdee561eabf463666c7 [formerly 61b86428db7b8b90f8727a1225c64a33ce8099ec]
    Former-commit-id: f73db0d241fc7c1474b11ecdb7df6842e7c172bc

---
+++ plugins/inputs/mongodb/mongodb.go
@@ -0,0 +1,224 @@
+package mongodb
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"net"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+	"gopkg.in/mgo.v2"
+)
+
+var (
+	inputName    = "mongodb"
+	sampleConfig = `
+[[inputs.mongodb]]
+  ## An array of URLs of the form:
+  ##   "mongodb://" [user ":" pass "@"] host [ ":" port]
+  ## For example:
+  ##   mongodb://user:auth_key@10.10.3.30:27017,
+  ##   mongodb://10.10.3.33:18832,
+  servers = ["mongodb://127.0.0.1:27017"]
+	## gather interval
+	interval = "10s"
+  ## When true, collect cluster status
+  ## Note that the query that counts jumbo chunks triggers a COLLSCAN, which
+  ## may have an impact on performance.
+  gather_cluster_status = true
+  ## When true, collect per database stats
+  gather_perdb_stats = false
+  ## When true, collect per collection stats
+  gather_col_stats = false
+  ## List of db where collections stats are collected
+  ## If empty, all db are concerned
+  col_stats_dbs = ["local"]
+  ## Optional TLS Config
+  tls_ca = "/etc/telegraf/ca.pem"
+  tls_cert = "/etc/telegraf/cert.pem"
+  tls_key = "/etc/telegraf/key.pem"
+  ## Use TLS but skip chain & host verification
+  insecure_skip_verify = false
+`
+	localhost = &url.URL{Host: "mongodb://127.0.0.1:27017"}
+	l         = logger.SLogger(inputName)
+)
+
+type Ssl struct {
+	Enabled bool
+	CaCerts []string `toml:"cacerts"`
+}
+
+type Input struct {
+	Interval            time.Duration
+	Servers             []string
+	Ssl                 Ssl
+	mongos              map[string]*Server
+	GatherClusterStatus bool
+	GatherPerdbStats    bool
+	GatherColStats      bool
+	GatherTopStat       bool
+	ColStatsDbs         []string
+	//  tlsint.ClientConfig
+	collectCache []inputs.Measurement
+}
+
+func (m *Input) Catalog() string {
+	return inputName
+}
+
+func (m *Input) SampleConfig() string {
+	return sampleConfig
+}
+
+func (m *Input) Run() {
+	l.Info("mongodb input started")
+
+	tick := time.NewTicker(m.Interval)
+	for {
+		select {
+		case <-tick.C:
+			// var lastErr error
+			if err := m.Gather(); err != nil {
+				// lastErr = err
+				l.Errorf(err.Error())
+				continue
+			}
+		case <-datakit.Exit.Wait():
+			l.Info("mongodb input exits")
+
+			return
+		}
+	}
+}
+
+// Reads stats from all configured servers accumulates stats.
+// Returns one of the errors encountered while gather stats (if any).
+func (m *Input) Gather() error {
+	if len(m.Servers) == 0 {
+		m.gatherServer(m.getMongoServer(localhost))
+
+		return nil
+	}
+
+	var wg sync.WaitGroup
+	for i, serv := range m.Servers {
+		if !strings.HasPrefix(serv, "mongodb://") {
+			// Preserve backwards compatibility for hostnames without a
+			// scheme, broken in go 1.8. Remove in Telegraf 2.0
+			serv = "mongodb://" + serv
+			l.Warnf("Using %q as connection URL; please update your configuration to use an URL", serv)
+			m.Servers[i] = serv
+		}
+
+		u, err := url.Parse(serv)
+		if err != nil {
+			l.Errorf("Unable to parse address %q: %s", serv, err.Error())
+			continue
+		}
+		if u.Host == "" {
+			l.Errorf("Unable to parse address %q", serv)
+			continue
+		}
+
+		wg.Add(1)
+		go func(srv *Server) {
+			defer wg.Done()
+
+			if err := m.gatherServer(srv); err != nil {
+				l.Errorf("Error in plugin: %v", err)
+			}
+		}(m.getMongoServer(u))
+	}
+	wg.Wait()
+
+	return nil
+}
+
+func (m *Input) getMongoServer(url *url.URL) *Server {
+	if _, ok := m.mongos[url.Host]; !ok {
+		m.mongos[url.Host] = &Server{URL: url}
+	}
+
+	return m.mongos[url.Host]
+}
+
+func (m *Input) gatherServer(server *Server) error {
+	if server.Session == nil {
+		var dialAddrs []string
+		if server.URL.User != nil {
+			dialAddrs = []string{server.URL.String()}
+		} else {
+			dialAddrs = []string{server.URL.Host}
+		}
+		dialInfo, err := mgo.ParseURL(dialAddrs[0])
+		if err != nil {
+			return fmt.Errorf("unable to parse URL %q: %s", dialAddrs[0], err.Error())
+		}
+		dialInfo.Direct = true
+		dialInfo.Timeout = 5 * time.Second
+
+		var tlsConfig *tls.Config
+
+		if m.Ssl.Enabled {
+			// Deprecated TLS config
+			tlsConfig = &tls.Config{}
+			if len(m.Ssl.CaCerts) > 0 {
+				roots := x509.NewCertPool()
+				for _, caCert := range m.Ssl.CaCerts {
+					ok := roots.AppendCertsFromPEM([]byte(caCert))
+					if !ok {
+						return fmt.Errorf("failed to parse root certificate")
+					}
+				}
+				tlsConfig.RootCAs = roots
+			} else {
+				tlsConfig.InsecureSkipVerify = true
+			}
+		} else {
+			tlsConfig, err = m.ClientConfig.TLSConfig()
+			if err != nil {
+				return err
+			}
+		}
+
+		// If configured to use TLS, add a dial function
+		if tlsConfig != nil {
+			dialInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+				conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+				if err != nil {
+					fmt.Printf("error in Dial, %s\n", err.Error())
+				}
+
+				return conn, err
+			}
+		}
+
+		sess, err := mgo.DialWithInfo(dialInfo)
+		if err != nil {
+			return fmt.Errorf("unable to connect to MongoDB: %s", err.Error())
+		}
+		server.Session = sess
+	}
+
+	return server.gatherData(m.GatherClusterStatus, m.GatherPerdbStats, m.GatherColStats, m.GatherTopStat, m.ColStatsDbs)
+}
+
+func init() {
+	inputs.Add(inputName, func() inputs.Input {
+		return &Input{
+			Interval:            10 * time.Second,
+			mongos:              make(map[string]*Server),
+			GatherClusterStatus: true,
+			GatherPerdbStats:    false,
+			GatherColStats:      false,
+			ColStatsDbs:         []string{"local"},
+		}
+	})
+}
