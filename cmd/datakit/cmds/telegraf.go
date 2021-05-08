644e2c7decd2f92b82aa7618aa7b236d59915921
huangbo <huangbo@jiagouyun.com>
2021-05-08 15:27:49 +0800

    优化telegraf安装
    Former-commit-id: 3d240050c70852e75604d1a6fbb22d101e0b77a7

--- cmd/datakit/cmds/telegraf.go
+++ cmd/datakit/cmds/telegraf.go
@@ -2,10 +2,17 @@ package cmds
 
 import (
 	"fmt"
+	"io/ioutil"
 	"path/filepath"
 	"runtime"
 
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/cmd/installer/install"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
+)
+
+const (
+	DIR_NAME = "telegraf"
 )
 
 func InstallTelegraf(installDir string) error {
@@ -16,14 +23,27 @@ func InstallTelegraf(installDir string) error {
 		return err
 	}
 
+	if err := writeTelegrafSample(installDir); err != nil {
+		return err
+	}
+
 	fmt.Printf("Install Telegraf successfully!\n")
 	if runtime.GOOS == "windows" {
-		fmt.Printf("Start telegraf by `cd %v`, and `telegraf.exe --config <file>`\n", filepath.Join(installDir, "telegraf"))
+		fmt.Printf("Start telegraf by `cd %v`, `copy telegraf.conf.sample telegraf.conf`, and `telegraf.exe --config <file>`\n", filepath.Join(installDir, DIR_NAME))
 	} else {
-		fmt.Printf("Start telegraf by `cd %v`, and ` ./usr/bin/telegraf --config <file>`\n", filepath.Join(installDir, "telegraf"))
+		fmt.Printf("Start telegraf by `cd %v`, `cp telegraf.conf.sample telegraf.conf`, and `./usr/bin/telegraf --config telegraf.conf`\n", filepath.Join(installDir, DIR_NAME))
 	}
 
 	fmt.Printf("Vist https://www.influxdata.com/time-series-platform/telegraf/ for more infomation.\n")
 
 	return nil
+}
+
+func writeTelegrafSample(installDir string) error {
+	if err := config.LoadCfg(datakit.Cfg, datakit.MainConfPath); err != nil {
+		return err
+	}
+
+	file := filepath.Join(installDir, DIR_NAME, "telegraf.conf.sample")
+	return ioutil.WriteFile(file, []byte(TelegrafConfTemplate), 0x666)
 }
