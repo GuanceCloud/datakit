17960586944a97ffa3a8d005a8a4d490e80e0954
tanbiao <tanbiao@jiagouyun.com>
2021-05-08 16:15:05 +0800

    update: 文档更新
     [formerly ca4d7e8119048826c73731315155e31276d00e19] [formerly 16c18017940ac77f9d22e9eeb2b20b85df7ae7cd [formerly d4aa97d1fda5a01755bf3fb5e6805adfc55899c5] [formerly c4f7b3aa7d6dec6b5a6730f6b4a749e13197cf1f [formerly d4fd1cfac8eafe93ff8705aa314733e78f300e03] [formerly c4f7b3aa7d6dec6b5a6730f6b4a749e13197cf1f [formerly d4fd1cfac8eafe93ff8705aa314733e78f300e03] [formerly c4f7b3aa7d6dec6b5a6730f6b4a749e13197cf1f [formerly d4fd1cfac8eafe93ff8705aa314733e78f300e03] [formerly b89363c084d6330715b7a54674820e9b20fb7948 [formerly 42de187e1ae27f84edc62b9a860a7423368412c6]]]]]]
     [formerly 73f59bd04aea777c784b9c672ada5c4b722d4b6e [formerly 8d7e42eac9285738f50314c1f5aeda76e75e56dc] [formerly 73f59bd04aea777c784b9c672ada5c4b722d4b6e [formerly 8d7e42eac9285738f50314c1f5aeda76e75e56dc] [formerly 73f59bd04aea777c784b9c672ada5c4b722d4b6e [formerly 8d7e42eac9285738f50314c1f5aeda76e75e56dc] [formerly 532e0125d566c266df9eba51e2b6e8f7884793b1 [formerly 8853e6f80c7e374216080cfe03261e8e8f221cca]]]]]
    
    
    
    Former-commit-id: cbf2517a2d1716199fd2ddc550adf816e853be0a [formerly 0e78ebb27efde05fe152b464953049cd89fd6f25]
    Former-commit-id: d9e03b106d3c65751ce21f2e32287f31eeaa9e15 [formerly 28a317c0d2043a69e561f12babb5bbe3b3205cae] [formerly c2cf08902e4254cfaffc0a758b731355932df462 [formerly ae9472c810a1b49f6a813348e84934f9f847ab4c]]
    Former-commit-id: 85acfccc7025e3dcc366a39cd47838f48d03468d [formerly d3c15772482d7887b102d6954ab6e360a7b9a4d3]
    Former-commit-id: adf8c1c52fad72e0fdd47a44504a8e79da758130

--- man/man.go
+++ man/man.go
@@ -17,19 +17,20 @@ var (
 	OtherDocs = map[string]interface{}{
 		// value not used, just document the markdown relative path
 		// all manuals under man/manuals/
-		"pipeline":             "man/manuals/pipeline.md",
-		"telegraf":             "man/manuals/telegraf.md",
-		"changelog":            "man/manuals/changelog.md",
-		"datatypes":            "man/manuals/datatypes.md",
-		"apis":                 "man/manuals/apis.md",
-		"sec-checker":          "man/manuals/sec-checker.md",
-		"datakit-how-to":       "man/manuals/datakit-how-to.md",
-		"datakit-arch":         "man/manuals/datakit-arch.md",
-		"nginx-proxy":          "man/manuals/nginx-proxy.md",
-		"dataway":              "man/manuals/dataway.md",
-		"datakit-batch-deploy": "man/manuals/datakit-batch-deploy.md",
-		"prometheus":           "man/manuals/prometheus.md",
-		"datakit-on-public":    "man/manuals/datakit-on-public.md",
+		"pipeline":                "man/manuals/pipeline.md",
+		"telegraf":                "man/manuals/telegraf.md",
+		"changelog":               "man/manuals/changelog.md",
+		"datatypes":               "man/manuals/datatypes.md",
+		"apis":                    "man/manuals/apis.md",
+		"sec-checker":             "man/manuals/sec-checker.md",
+		"datakit-how-to":          "man/manuals/datakit-how-to.md",
+		"datakit-arch":            "man/manuals/datakit-arch.md",
+		"nginx-proxy":             "man/manuals/nginx-proxy.md",
+		"dataway":                 "man/manuals/dataway.md",
+		"datakit-batch-deploy":    "man/manuals/datakit-batch-deploy.md",
+		"prometheus":              "man/manuals/prometheus.md",
+		"datakit-on-public":       "man/manuals/datakit-on-public.md",
+		"datakit-offline-install": "man/manuals/datakit-offline-install.md",
 	}
 
 	l = logger.DefaultSLogger("man")
