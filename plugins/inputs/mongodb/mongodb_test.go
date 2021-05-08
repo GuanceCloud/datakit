2113bbc2d38f026062a1a687d4a0224e12979115
CodapeWild <codape.wild@gmail.com>
2021-05-08 19:53:02 +0800

    add mongo input test
    Former-commit-id: 3112516a1feff876b272932c6aa03d0e2a49b857 [formerly 84e5af34276ffd4af5edbea3fa1ca9f07e15da19] [formerly d61c98acc0e8d39f2e0afaa2149c319140cbac9e [formerly f5e3712957ef9a15cf686322e91abc206d2b519f]]
    Former-commit-id: 9a19e642c163773d85b0f067a57166d172094c58 [formerly fca31ac09929ea9e2ebc87cacdfd5ec8290b73d2]
    Former-commit-id: 328193e69cdbcbb675753a388300573ead8aeca2

--- plugins/inputs/mongodb/mongodb_test.go
+++ plugins/inputs/mongodb/mongodb_test.go
@@ -1,7 +1,47 @@
 package mongodb
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func TestMongodb(t *testing.T) {
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+)
 
+func TestGather(t *testing.T) {
+	input := &Input{
+		Interval:               datakit.Duration{Duration: 3 * time.Second},
+		Servers:                []string{"mongodb://127.0.0.1:27017"},
+		GatherReplicaSetStatus: false,
+		GatherClusterStatus:    false,
+		GatherPerdbStats:       false,
+		GatherColStats:         false,
+		GatherTopStat:          false,
+		ColStatsDbs:            []string{"local"},
+		EnableTls:              false,
+		mongos:                 make(map[string]*Server),
+	}
+	err := input.Gather()
+	if err != nil {
+		l.Panic(err.Error())
+	}
+
+	for _, srv := range input.mongos {
+		if srv.lastResult != nil {
+			data := NewMongodbData(NewStatLine(*srv.lastResult, *srv.lastResult, srv.URL.Host, true, 1), map[string]string{"hostname": srv.URL.Host}, 1)
+			data.AddDefaultStats()
+			data.AddDbStats()
+			data.AddColStats()
+			data.AddShardHostStats()
+			data.AddTopStats()
+			data.append()
+			// data.flush()
+			for _, data := range data.collectCache {
+				if point, err := data.LineProto(); err != nil {
+					l.Error(err.Error())
+				} else {
+					l.Info(point.String())
+				}
+			}
+		}
+	}
 }
