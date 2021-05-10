129921d0e7f1b1d1bdd8b2377284762e206b41cc
CodapeWild <codape.wild@gmail.com>
2021-05-10 11:47:57 +0800

    fix
    Former-commit-id: a165b02949d867c694cd3020822882767d1028c9
    Former-commit-id: 5c1b41cedbc17f8fd06ef9e3fd4e242923deaefc [formerly 8716a67b9a522877c34d999a54803801efbb0b63]
    Former-commit-id: 8ba9a1ff714087eea2dc6e36789b5d53d1e629e0

--- plugins/inputs/mongodb/mongodb_test.go
+++ plugins/inputs/mongodb/mongodb_test.go
@@ -9,16 +9,16 @@ import (
 
 func TestGatherMongoDb(t *testing.T) {
 	input := &Input{
-		Interval:               datakit.Duration{Duration: 3 * time.Second},
-		Servers:                []string{"mongodb://127.0.0.1:27017"},
-		GatherReplicaSetStatus: false,
-		GatherClusterStatus:    false,
-		GatherPerdbStats:       false,
-		GatherColStats:         false,
-		GatherTopStat:          false,
-		ColStatsDbs:            []string{"local"},
-		EnableTls:              false,
-		mongos:                 make(map[string]*Server),
+		Interval:              datakit.Duration{Duration: 3 * time.Second},
+		Servers:               []string{"mongodb://127.0.0.1:27017"},
+		GatherReplicaSetStats: false,
+		GatherClusterStats:    false,
+		GatherPerDbStats:      false,
+		GatherPerColStats:     false,
+		ColStatsDbs:           []string{"local"},
+		GatherTopStat:         false,
+		EnableTls:             false,
+		mongos:                make(map[string]*Server),
 	}
 	err := input.Gather()
 	if err != nil {
@@ -48,16 +48,16 @@ func TestGatherMongoDb(t *testing.T) {
 
 func TestGatherMongoDbPerDbStat(t *testing.T) {
 	input := &Input{
-		Interval:               datakit.Duration{Duration: 3 * time.Second},
-		Servers:                []string{"mongodb://127.0.0.1:27017"},
-		GatherReplicaSetStatus: false,
-		GatherClusterStatus:    false,
-		GatherPerdbStats:       false,
-		GatherColStats:         true,
-		GatherTopStat:          false,
-		ColStatsDbs:            []string{"local", "config"},
-		EnableTls:              false,
-		mongos:                 make(map[string]*Server),
+		Interval:              datakit.Duration{Duration: 3 * time.Second},
+		Servers:               []string{"mongodb://127.0.0.1:27017"},
+		GatherReplicaSetStats: false,
+		GatherClusterStats:    false,
+		GatherPerDbStats:      true,
+		GatherPerColStats:     false,
+		ColStatsDbs:           []string{"local"},
+		GatherTopStat:         false,
+		EnableTls:             false,
+		mongos:                make(map[string]*Server),
 	}
 	err := input.Gather()
 	if err != nil {
