546a89ba87efa652bfbcd043ab846a6363f87a39
liguozhuang <liguozhuang@jiagouyun.com>
2021-05-10 19:08:43 +0800

    done
     [formerly 6c3c533990d5a766ac6edb18e07d39d4ac66f58c [formerly e90c90658e252d79d46468b5957dd1c9870e649b] [formerly 6c3c533990d5a766ac6edb18e07d39d4ac66f58c [formerly e90c90658e252d79d46468b5957dd1c9870e649b] [formerly 6c3c533990d5a766ac6edb18e07d39d4ac66f58c [formerly e90c90658e252d79d46468b5957dd1c9870e649b] [formerly 9dcd8c6c73887f6e55794f72d07702212d02963a [formerly 7042ea76e1b593a6c3f7cbf8745acdc5cd2caa08]]]]]
    
    
    
    Former-commit-id: 26757dc714b3c64e994eac8f70b69aa761a21adb [formerly 0b3096149e8c39db20f9773be08aa962b2108bc5]
    Former-commit-id: 576ca010a8cafb38a7487a6d0a3e63e33a124055 [formerly 28ca0aa01ca904b2d2d3ea7c80676127c8459f9f] [formerly 27e6d8a6655c4b0375d8e6e2a245390cc0e4b32f [formerly afbee30b6c35da19a6f0cdf8b9d31b6c311db093]]
    Former-commit-id: afc773c258c1a095eeba6324c22d52acb0bbe382 [formerly 1cf810f243c4caa01a2917ea1c10f941a1c03de1]
    Former-commit-id: 653780069fd7dc0c4d148eb3e3efab5dc5b9f37f

--- io/io.go
+++ io/io.go
@@ -309,7 +309,7 @@ func (x *IO) StartIO(recoverable bool) {
 				x.cleanHighFreqIOData()
 
 			case <-heartBeatTick.C:
-				x.dw.HeartBeat()
+				// x.dw.HeartBeat()
 
 			case <-tick.C:
 				l.Debugf("chan stat: %s", ChanStat())
