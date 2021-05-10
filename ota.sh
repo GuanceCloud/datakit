17381da1d52122523cf8649adb16cbb080ed6289
tanbiao <tanbiao@jiagouyun.com>
2021-05-10 15:23:41 +0800

    update:
    	[*] 调整集成 tags 输出形式
    	[-] 在 Windows 下禁用 --man 文档功能
    	[*] 调整更新检测逻辑: 只检测，不更新，由单独的外部脚本来实现更新
    	[*] 调整安装程序命令行参数
    	[+] 增加更多文档h
    
    
    Former-commit-id: e8b72f8a1891e3fd6366db23bd4ac617757d785e [formerly ce2abd7fc6aea282d7933775547bd7e8af5ac149] [formerly c100780e5cdf948efe96a81f952e851d9c1f25bc [formerly 64d384f4bfe4cce2c7bd8499fa77eb141edb457c]]
    Former-commit-id: df73a39f6f9a121616e875111256bda5dceabe01 [formerly c7db69c183b1206f530822e0b5f0c1582ca3be46]
    Former-commit-id: 233257dbf0bd80f0b711df77d89210008d8d2c88

--- ota.sh
+++ ota.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Update DataKit if new version available
-# Date: Sun Apr 11 16:17:21 CST 2021
-# Author: tan
 
-if [ !`/usr/local/cloudcare/dataflux/datakit/datakit -check-update -accept-rc-version` ]; then
-	sudo -- sh -c "curl https://static.dataflux.cn/datakit/installer-linux-amd64 -o dk-installer &&
+update_log=/usr/local/cloudcare/dataflux/datakit/update.log
+
+datakit --check-update --accept-rc-version --update-log $update_log
+
+if [[ $? == 42 ]]; then
+	echo "update now..."
+	sudo -- sh -c "curl https://static.dataflux.cn/datakit/installer-darwin-amd64 -o dk-installer &&
 		chmod +x ./dk-installer &&
-		./dk-installer -upgrade &&
+		./dk-installer -upgrade -install-log "${update_log}" &&
 		rm -rf ./dk-installer"
 fi
