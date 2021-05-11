3e86604de5a9a289110d52ae321aac08b996e1b1
tanbiao <tanbiao@jiagouyun.com>
2021-05-11 13:50:07 +0800

    update: 调整 API 代码目录
    Former-commit-id: 8aa2936e2416f5a76f045637b5d3de138eefd764

--- http/http_api.go
+++ http/http_api.go
@@ -19,77 +19,6 @@ const (
 	DEFAULT_PRECISION = "ns"
 )
 
-func apiWriteMetric(c *gin.Context) {
-	var precision string = DEFAULT_PRECISION
-	var body []byte
-	var err error
-
-	name := c.Query(NAME)
-	precision = c.Query(PRECISION)
-
-	body, err = uhttp.GinRead(c)
-	if err != nil {
-		uhttp.HttpErr(c, uhttp.Error(ErrHttpReadErr, err.Error()))
-		return
-	}
-
-	pts, err := lp.ParsePoints(body, &lp.Option{
-		ExtraTags: datakit.Cfg.MainCfg.GlobalTags,
-		Strict:    true,
-		Precision: precision})
-
-	if err != nil {
-		uhttp.HttpErr(c, uhttp.Error(ErrBadReq, err.Error()))
-		return
-	}
-
-	l.Debugf("received metric %d points from %s", len(pts), name)
-
-	var x []*io.Point
-	for _, pt := range pts {
-		x = append(x, &io.Point{pt})
-	}
-
-	if err = io.Feed(name, datakit.Metric, x, nil); err != nil {
-		uhttp.HttpErr(c, uhttp.Error(ErrBadReq, err.Error()))
-		return
-	}
-
-	ErrOK.HttpBody(c, nil)
-}
-
-func apiWriteLogging(c *gin.Context) {
-	var precision string = DEFAULT_PRECISION
-	var body []byte
-	var err error
-
-	name := c.Query(NAME)
-	precision = c.Query(PRECISION)
-
-	body, err = uhttp.GinRead(c)
-	if err != nil {
-		uhttp.HttpErr(c, uhttp.Error(ErrHttpReadErr, err.Error()))
-		return
-	}
-
-	pts, err := influxm.ParsePointsWithPrecision(body, time.Now().UTC(), precision)
-	if err != nil {
-		uhttp.HttpErr(c, uhttp.Error(ErrBadReq, err.Error()))
-		return
-	}
-
-	l.Debugf("received logging %d points from %s", len(pts), name)
-
-	// TODO: add global tags
-
-	if err = io.NamedFeed(body, datakit.Logging, name); err != nil {
-		uhttp.HttpErr(c, uhttp.Error(ErrBadReq, err.Error()))
-		return
-	}
-
-	ErrOK.HttpBody(c, nil)
-}
-
 func apiWriteTracing(c *gin.Context) {
 	var precision string = DEFAULT_PRECISION
 	var body []byte
@@ -224,5 +153,4 @@ func apiWriteTelegraf(c *gin.Context) {
 
 		return
 	}
-
 }
