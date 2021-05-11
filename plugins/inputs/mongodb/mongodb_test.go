52780975ede18af41df54b7d9e0df75b0282c8fb
CodapeWild <codape.wild@gmail.com>
2021-05-11 10:31:17 +0800

    fix tls connection
     [formerly 5012f507f2b5fab6ad2a92da76084da76dce472d [formerly dacd285b7e9d8c1e6e70029bfd5b422756027775] [formerly b1106808d9c447ff2c9f2521ff45c67b66cfdcd9 [formerly 27341b306c20805a458ff61ebbb7e04a58aab8b6]] [formerly b1106808d9c447ff2c9f2521ff45c67b66cfdcd9 [formerly 27341b306c20805a458ff61ebbb7e04a58aab8b6] [formerly b1106808d9c447ff2c9f2521ff45c67b66cfdcd9 [formerly 27341b306c20805a458ff61ebbb7e04a58aab8b6] [formerly 7d54e7c59b21a85dc13482ddc96a9cc90c62f9fb [formerly 8eb427155bb41092f98e332420a26bc2544cca54]]]]]
    
    
    
    Former-commit-id: c4e3ae58f5efaba573c38db75619bf91573ee4d9 [formerly f71ef856fe2f7968881b556d20bcf4b8b65b339d]
    Former-commit-id: 9d33b6d0f6ef04e4d9ca3bfc99c27dbffaef0d2c [formerly 4e5c88f853b1f56f2d94218130946ea1069b90c4] [formerly 17f5e4384ff4ecc6add71a5436df0242d77991a9 [formerly 04ac6457244b0df3fc36b552e67077c68363b14c]]
    Former-commit-id: c95f4ca675d82c65c740a8e8df435aff1368b9b7 [formerly 9465db0da7983ab74daad85ced73bf99c2a9cc64]
    Former-commit-id: d03e253a07284a864d084b3382fe6483d8e3bb70

--- plugins/inputs/mongodb/mongodb_test.go
+++ plugins/inputs/mongodb/mongodb_test.go
@@ -46,6 +46,45 @@ func TestGatherServerStats(t *testing.T) {
 	}
 }
 
+func TestGatherCluster(t *testing.T) {
+	input := &Input{
+		Interval:              datakit.Duration{Duration: 3 * time.Second},
+		Servers:               []string{"mongodb://127.0.0.1:27017"},
+		GatherReplicaSetStats: false,
+		GatherClusterStats:    true,
+		GatherPerDbStats:      false,
+		GatherPerColStats:     false,
+		ColStatsDbs:           []string{"local"},
+		GatherTopStat:         false,
+		EnableTls:             false,
+		mongos:                make(map[string]*Server),
+	}
+	err := input.Gather()
+	if err != nil {
+		l.Panic(err.Error())
+	}
+
+	for _, srv := range input.mongos {
+		if srv.lastResult != nil {
+			data := NewMongodbData(NewStatLine(*srv.lastResult, *srv.lastResult, srv.URL.Host, true, 1), map[string]string{"hostname": srv.URL.Host}, 1)
+			// data.AddDefaultStats()
+			data.AddDbStats()
+			data.AddColStats()
+			data.AddShardHostStats()
+			data.AddTopStats()
+			data.append()
+			// data.flush()
+			for _, data := range data.collectCache {
+				if point, err := data.LineProto(); err != nil {
+					l.Error(err.Error())
+				} else {
+					l.Info(point.String())
+				}
+			}
+		}
+	}
+}
+
 func TestGatherPerDbStats(t *testing.T) {
 	input := &Input{
 		Interval:              datakit.Duration{Duration: 3 * time.Second},
@@ -83,4 +122,9 @@ func TestGatherPerDbStats(t *testing.T) {
 			}
 		}
 	}
+}
+
+// TODO: add tls connection collection
+func TestTlsConnectCollect(t *testing.T) {
+
 }
