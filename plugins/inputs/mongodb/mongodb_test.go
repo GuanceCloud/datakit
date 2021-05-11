d8198fcef76048926eeb2096d684da84881cf19a
CodapeWild <codape.wild@gmail.com>
2021-05-11 14:10:17 +0800

    add inputs measurement
    Former-commit-id: d358c002021294adef3bcc439decd88d783d8c31
    Former-commit-id: 11e3f11bc3beda9df6e729218e5b4aeebd096588 [formerly 99911d112250ed6e13ca98ad4271601675ea38d7]
    Former-commit-id: 9e5f60b709b16ddc56a940f5329762431780ff54

--- plugins/inputs/mongodb/mongodb_test.go
+++ plugins/inputs/mongodb/mongodb_test.go
@@ -54,7 +54,46 @@ func TestGatherCluster(t *testing.T) {
 		GatherClusterStats:    true,
 		GatherPerDbStats:      false,
 		GatherPerColStats:     false,
-		ColStatsDbs:           []string{"local"},
+		ColStatsDbs:           []string{""},
+		GatherTopStat:         false,
+		EnableTls:             false,
+		mongos:                make(map[string]*Server),
+	}
+	err := input.Gather()
+	if err != nil {
+		l.Panic(err.Error())
+	}
+
+	for _, srv := range input.mongos {
+		if srv.lastResult != nil {
+			data := NewMongodbData(NewStatLine(*srv.lastResult, *srv.lastResult, srv.URL.Host, true, 1), map[string]string{"hostname": srv.URL.Host}, 1)
+			data.AddDefaultStats()
+			data.AddDbStats()
+			data.AddColStats()
+			data.AddShardHostStats()
+			data.AddTopStats()
+			data.append()
+			// data.flush()
+			for _, data := range data.collectCache {
+				if point, err := data.LineProto(); err != nil {
+					l.Error(err.Error())
+				} else {
+					l.Info(point.String())
+				}
+			}
+		}
+	}
+}
+
+func TestGatherPerDbStats(t *testing.T) {
+	input := &Input{
+		Interval:              datakit.Duration{Duration: 3 * time.Second},
+		Servers:               []string{"mongodb://127.0.0.1:27017"},
+		GatherReplicaSetStats: false,
+		GatherClusterStats:    false,
+		GatherPerDbStats:      true,
+		GatherPerColStats:     false,
+		ColStatsDbs:           []string{},
 		GatherTopStat:         false,
 		EnableTls:             false,
 		mongos:                make(map[string]*Server),
@@ -85,14 +124,18 @@ func TestGatherCluster(t *testing.T) {
 	}
 }
 
-func TestGatherPerDbStats(t *testing.T) {
+// TODO: add testing gathering sharded conn pool
+func TestGathertShard(t *testing.T) {
+}
+
+func TestGatherCollection(t *testing.T) {
 	input := &Input{
 		Interval:              datakit.Duration{Duration: 3 * time.Second},
 		Servers:               []string{"mongodb://127.0.0.1:27017"},
 		GatherReplicaSetStats: false,
 		GatherClusterStats:    false,
-		GatherPerDbStats:      true,
-		GatherPerColStats:     false,
+		GatherPerDbStats:      false,
+		GatherPerColStats:     true,
 		ColStatsDbs:           []string{"admin", "local", "config"},
 		GatherTopStat:         false,
 		EnableTls:             false,
@@ -124,7 +167,68 @@ func TestGatherPerDbStats(t *testing.T) {
 	}
 }
 
-// TODO: add tls connection collection
-func TestTlsConnectCollect(t *testing.T) {
+func TestGatherTop(t *testing.T) {
+	input := &Input{
+		Interval:              datakit.Duration{Duration: 3 * time.Second},
+		Servers:               []string{"mongodb://127.0.0.1:27017"},
+		GatherReplicaSetStats: false,
+		GatherClusterStats:    false,
+		GatherPerDbStats:      false,
+		GatherPerColStats:     false,
+		ColStatsDbs:           []string{"admin", "local", "config"},
+		GatherTopStat:         true,
+		EnableTls:             false,
+		mongos:                make(map[string]*Server),
+	}
+	err := input.Gather()
+	if err != nil {
+		l.Panic(err.Error())
+	}
 
+	for _, srv := range input.mongos {
+		if srv.lastResult != nil {
+			data := NewMongodbData(NewStatLine(*srv.lastResult, *srv.lastResult, srv.URL.Host, true, 1), map[string]string{"hostname": srv.URL.Host}, 1)
+			// data.AddDefaultStats()
+			data.AddDbStats()
+			data.AddColStats()
+			data.AddShardHostStats()
+			data.AddTopStats()
+			data.append()
+			// data.flush()
+			for _, data := range data.collectCache {
+				if point, err := data.LineProto(); err != nil {
+					l.Error(err.Error())
+				} else {
+					l.Info(point.String())
+				}
+			}
+		}
+	}
+}
+
+// TODO: add tls dial and connection test
+func TestTlsConnectCollect(t *testing.T) {
+	input := &Input{
+		Interval:              datakit.Duration{Duration: 3 * time.Second},
+		Servers:               []string{"mongodb://127.0.0.1:27017"},
+		GatherReplicaSetStats: false,
+		GatherClusterStats:    false,
+		GatherPerDbStats:      false,
+		GatherPerColStats:     false,
+		ColStatsDbs:           []string{"admin"},
+		GatherTopStat:         false,
+		EnableTls:             true,
+		TlsConf: &TlsClientConfig{
+			CaCerts:            []string{},
+			Cert:               "",
+			CertKey:            "",
+			InsecureSkipVerify: false,
+			ServerName:         "",
+		},
+		mongos: make(map[string]*Server),
+	}
+	err := input.Gather()
+	if err != nil {
+		l.Panic(err.Error())
+	}
 }
