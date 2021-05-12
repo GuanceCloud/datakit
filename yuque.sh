4fbbfffcc08f65f9eb13023f3acbad3dc3749914
tanbiao <tanbiao@jiagouyun.com>
2021-05-12 13:52:30 +0800

    update: 支持文档导出语雀时指定文档版本(#118)
    Former-commit-id: 5a94f3eaca74867359988fe733e1202e6c79325c [formerly 3b729129669e6a1dac8acfdbb7a794fc8a50fb7d]
    Former-commit-id: bb72cca3104d5f8dd79f2251ce63d8f529be7796

--- yuque.sh
+++ yuque.sh
@@ -7,5 +7,6 @@
 rm -rf .docs
 mkdir -p .docs
 cp man/summary.md .docs/
-dist/datakit-darwin-amd64/datakit --ignore demo --export-manuals .docs
+man_version=`git tag -l | sort -nr | head -n 1` # use latest tag version
+dist/datakit-darwin-amd64/datakit --ignore demo --export-manuals .docs --man-version $man_version
 waque upload .docs/*.md
