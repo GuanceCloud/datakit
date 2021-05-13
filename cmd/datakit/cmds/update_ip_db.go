5360eda2a2ccd27e80fefd56b7f3e3d82f6ed92f
huangbo <huangbo@jiagouyun.com>
2021-05-13 14:43:54 +0800

    支持命令行升级data目录
    Former-commit-id: 6a765db2fe66ea86c61b471bfe6e4810fcacfaae [formerly e0b3f900f1673df706d9c756c45c7c1927733aad [formerly 2ce167ca267c3e4841d70390a9c134339082691c] [formerly 50bc00d62bb9a6f007b1d247d1247324cfd898f9 [formerly e11b87df95a3f2abe481a0e4f48ba7ad62f63c29]]]
    Former-commit-id: e32d2f0fd50becc5504e2852e4456ea6b9b1ed92
    Former-commit-id: 0464cead4ca43de39245aa96bac00eaf953854b8 [formerly 7e4dcc17aafff2becccdaf99c349c95c149edafc]
    Former-commit-id: 5fab22638c4a27c6a838098be0f977d1ef3e6554

--- cmd/datakit/cmds/update_ip_db.go
+++ cmd/datakit/cmds/update_ip_db.go
@@ -1,5 +1,33 @@
 package cmds
 
-func UpdateIpDB() error {
+import (
+	"fmt"
+	nhttp "net/http"
 
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/cmd/installer/install"
+)
+
+const (
+	dataUrl = "https://zhuyun-static-files-production.oss-cn-hangzhou.aliyuncs.com/datakit/data.tar.gz"
+)
+
+func UpdateIpDB(port int, addr string) error {
+	if addr == "" {
+		addr = dataUrl
+	}
+
+	fmt.Printf("Start downloading data.tar.gz...\n")
+	if err := install.Download(addr, datakit.InstallDir, true, false); err != nil {
+		return err
+	}
+	fmt.Printf("Download and decompress data.tar.gz successfully.\n")
+	fmt.Printf("Update Ip DB successfully.\n")
+
+	_, err := nhttp.Get(fmt.Sprintf("http://127.0.0.1:%d/reload", port))
+	if err == nil {
+		fmt.Printf("Datakit reload successfully\n")
+	}
+
+	return nil
 }
