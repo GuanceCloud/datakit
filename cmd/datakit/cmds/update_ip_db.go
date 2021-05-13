79af37f7f3c5e60adc6e70fd4c083355cf1745d2
huangbo <huangbo@jiagouyun.com>
2021-05-13 18:16:18 +0800

    增加isp文件备份
    Former-commit-id: c0f0a26652313e20fa4fdd2f08fc84651d1619a7
    Former-commit-id: 870f467435f5a4f9da815d622ebc639c8851ff01 [formerly cb4590df3fc21d2bee2db93a1d991c59a6ef0462]
    Former-commit-id: eabbe45fe826e204b274eef52005187441cdf2c1

--- cmd/datakit/cmds/update_ip_db.go
+++ cmd/datakit/cmds/update_ip_db.go
@@ -19,19 +19,36 @@ func UpdateIpDB(port int, addr string) error {
 		addr = dataUrl
 	}
 
-	os.RemoveAll(filepath.Join(datakit.InstallDir, "data", "ip2isp"))
+	ispDir := filepath.Join(datakit.InstallDir, "data", "ip2isp")
+	ispDirBack := filepath.Join(datakit.InstallDir, "data", "ip2isp_backup")
+
+	if err := os.Rename(ispDir, ispDirBack); err != nil {
+		l.Errorf("rename %s to %s failed: %v", ispDir, ispDirBack, err)
+	}
 
 	fmt.Printf("Start downloading data.tar.gz...\n")
 
 	if err := install.Download(addr, datakit.InstallDir, true, false); err != nil {
+		if e := os.Rename(ispDirBack, ispDir); e != nil {
+			l.Errorf("rename %s to %s failed: %v", ispDirBack, ispDir, e)
+		}
 		return err
+	} else {
+		if e := os.RemoveAll(ispDirBack); e != nil {
+			l.Errorf("remove %s failed: %v", ispDirBack, e)
+		}
 	}
 
 	fmt.Printf("Download and decompress data.tar.gz successfully.\n")
 
-	nhttp.Get(fmt.Sprintf("http://127.0.0.1:%d/reload", port))
-
-	fmt.Printf("Update successfully.\n")
+	_, err := nhttp.Get(fmt.Sprintf("http://127.0.0.1:%d/reload", port))
+	if err != nil {
+		fmt.Printf("Reload datakit fail!\n")
+		fmt.Printf("You need restart datakit by `datakit --restart` to make effect.\n")
+		return nil
+	} else {
+		fmt.Printf("Update successfully.\n")
+	}
 
 	return nil
 }
