37b34bdc6da7d501ed74a963852a651c908dca38
zheng bo <zhengb@jiagouyun.com>
2021-05-19 09:37:24 +0800

    feat: postgresql metric collect
    Former-commit-id: 2b0cbe3772ad0156071bbb57d34a932ec14d5dd1
    Former-commit-id: 4127f5d428184f7e408248eb19e1a9665dd17768 [formerly ffbe9f249de96fb10c19c1db3c58dd34f954ff4f]
    Former-commit-id: 03c6abd8208bdd6dfe6a8f1134e8da8258765013

---
+++ plugins/inputs/postgresql/service.go
@@ -0,0 +1,78 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type SqlService struct {
+	Address     string
+	MaxIdle     int
+	MaxOpen     int
+	MaxLifetime time.Duration
+	DB          *sql.DB
+}
+
+func (p *SqlService) Start() (err error) {
+	const localhost = "host=localhost sslmode=disable"
+
+	if p.Address == "" || p.Address == "localhost" {
+		p.Address = localhost
+	}
+
+	connectionString := p.Address
+
+	if p.DB, err = sql.Open("postgres", connectionString); err != nil {
+		l.Error("connect error: ", connectionString)
+		fmt.Println(connectionString)
+		return err
+	}
+
+	p.DB.SetMaxOpenConns(p.MaxIdle)
+	p.DB.SetMaxIdleConns(p.MaxIdle)
+	p.DB.SetConnMaxLifetime(time.Duration(p.MaxLifetime))
+
+	return nil
+}
+
+func (p *SqlService) Stop() {
+	if p.DB != nil {
+		p.DB.Close()
+	}
+}
+
+func (p *SqlService) Query(query string) (Rows, error) {
+	rows, err := p.DB.Query(query)
+	if err != nil {
+		return nil, err
+	} else {
+		return rows, nil
+	}
+}
+
+func (p *SqlService) SetAddress(address string) {
+	p.Address = address
+}
+
+func (p *SqlService) GetColumnMap(row scanner, columns []string) (map[string]*interface{}, error) {
+	var columnVars []interface{}
+
+	columnMap := make(map[string]*interface{})
+
+	for _, column := range columns {
+		columnMap[column] = new(interface{})
+	}
+
+	for i := 0; i < len(columnMap); i++ {
+		columnVars = append(columnVars, columnMap[columns[i]])
+	}
+
+	err := row.Scan(columnVars...)
+	if err != nil {
+		return nil, err
+	}
+	return columnMap, nil
+}
