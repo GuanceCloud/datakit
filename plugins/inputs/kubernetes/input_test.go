9cc890d5544b01a77df48a29b8d6d956d5ba023d
liushaobo <liushaobo@jiagouyun.com>
2021-05-19 17:41:16 +0800

    暂存

--- plugins/inputs/kubernetes/input_test.go
+++ plugins/inputs/kubernetes/input_test.go
@@ -5,14 +5,4 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	var k = newInput()
-
-	k.KubeAPIServerURL = "http://127.0.0.1:8080/metrics"
-
-	pt, err := k.gatherMetrics()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Logf(pt.String())
 }
