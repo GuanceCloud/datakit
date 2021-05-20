a8556d8a7ba5682746d4b5ce2bd3fc21004ffe0b
liguozhuang <liguozhuang@jiagouyun.com>
2021-05-20 11:32:17 +0800

    修复几处编译错误
     [formerly dfec39956f60fb54ef9301bbcad3f2bb6cc1452d [formerly 5cf34d4ad318609c995ce2800c98580ffdaffca2] [formerly dfec39956f60fb54ef9301bbcad3f2bb6cc1452d [formerly 5cf34d4ad318609c995ce2800c98580ffdaffca2] [formerly dfec39956f60fb54ef9301bbcad3f2bb6cc1452d [formerly 5cf34d4ad318609c995ce2800c98580ffdaffca2] [formerly ab5682bddd57eeee116bd2e42f1d981dff4b0b30 [formerly b8c9cce53d507de0d1a5ce39398ab58712141ebc]]]]]
    
    
    
    Former-commit-id: a464a12ccde0e052f7972426b2d9447db8fd0eee [formerly b2a0be0da6132c994152d02eeb3e3c44e05c0ad9]
    Former-commit-id: ae0dba59030d8d99ecd6579cf15301e074ecc691 [formerly f9c918a50a601dd108f70094b1b95ddc139d390c] [formerly cffb5274872c53bcfd8c11943d27c4f6cbf6d7fe [formerly 502c28806324866efceca742736d9451a7b0547e]]
    Former-commit-id: 5282077779374057a438f37799f998586f2f656f [formerly d8c27449fbc0d4f0365bf5c06fa046eb5c442d6d]
    Former-commit-id: 09f215800f20aa8a03ff8420baf6e3eeaf16e856

--- plugins/inputs/docker/log.go
+++ plugins/inputs/docker/log.go
@@ -65,7 +65,7 @@ func (this *Input) cancelTails() error {
 
 func (this *Input) gatherLog() {
 	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, this.timeoutDuration)
+	ctx, cancel := context.WithTimeout(ctx, this.apiTimeoutDuration)
 	defer cancel()
 
 	cList, err := this.client.ContainerList(ctx, this.opts)
@@ -152,7 +152,7 @@ func (this *Input) tailContainerLogs(ctx context.Context, container types.Contai
 }
 
 func (this *Input) hasTTY(ctx context.Context, container types.Container) (bool, error) {
-	ctx, cancel := context.WithTimeout(ctx, this.timeoutDuration)
+	ctx, cancel := context.WithTimeout(ctx, this.apiTimeoutDuration)
 	defer cancel()
 	c, err := this.client.ContainerInspect(ctx, container.ID)
 	if err != nil {
