fedc9a53435148f4e263459c6e882fca959142d1
CodapeWild <codape.wild@gmail.com>
2021-05-21 13:59:52 +0800

    add 'sensors' command input
     [formerly 75ea793c7dd3ebf8b7eccc69e528428e69b6cc3b [formerly 2f7ca45a29f58d109bf1f074d8d560fa03031fe0] [formerly 04bde88b2f75f3bb0ad8a6347dd3ec777a14ae76 [formerly 20b18e8f997f45de95d79b58e67abee2ba87881f]] [formerly 4082f92b33198c1d530df510ed2aff85224f8ec0] [formerly df974fe9250d2c7976b49fd7943bf91ce63c4b13 [formerly d11b920ad027e01114d8db9bab443fe595cc6f25] [formerly df974fe9250d2c7976b49fd7943bf91ce63c4b13 [formerly d11b920ad027e01114d8db9bab443fe595cc6f25] [formerly ef80c154b8285bac122b295119e8722f1adc757a [formerly a021cf87e9002ccffbc490c68c96bcb3c8a9f228]]]] [formerly abc3d4e5c3baac8b0250ed6addc5290f46ce1adb] [formerly 34fa727714e4f6554bbd4f7b8443187bb36ff6f4 [formerly 0d28d2baeaff616b04ff00a6ae59b541f4f8c8b1] [formerly 8192aea8fc635b1e3b7488ae8d0dbe7b6fab9d6d [formerly 1e09546829e98f7ad00d6a5b8fbe14880a5155ad] [formerly 8192aea8fc635b1e3b7488ae8d0dbe7b6fab9d6d [formerly 1e09546829e98f7ad00d6a5b8fbe14880a5155ad] [formerly 8192aea8fc635b1e3b7488ae8d0dbe7b6fab9d6d [formerly 1e09546829e98f7ad00d6a5b8fbe14880a5155ad] [formerly 362c93eaa8979e717005d6a201570136a15e4ae2 [formerly 81c85668b22c0abe3e18ecfc13b7308165c0449b]]]]]]]
     [formerly abc3d4e5c3baac8b0250ed6addc5290f46ce1adb]
    
    
    
    
    Former-commit-id: d7ef693fcb900b7f5626b97c83d8e7b618733592 [formerly 5376752adbbf20fc95a9918d65e39ccb18b45484]
    Former-commit-id: a8e5eadaf4af6f944229eec0c4d448cc2173b417 [formerly aaabde3baef0634e55d3ea2340c9dbf46d1d1b0b] [formerly 11868ea2b43a31356e22175229b810a6e306b038 [formerly 1513190ba2b28ac7ee9e68d9691824499365d586]]
    Former-commit-id: 2e5c24fb90d1bb8fb8818eea3226a758c6c32c82 [formerly 383586ac6c0cd8661e5cafa0a631455c5b435198]
    Former-commit-id: 6faa1419e9ded44e42fe60afd7d00d63cbe2990b

---
+++ plugins/inputs/sensors/sensors.go
@@ -0,0 +1,192 @@
+// +build linux
+
+package sensors
+
+import (
+	"bytes"
+	"errors"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+var (
+	ErrSensorsTimeout = errors.New("Command 'sensors' process timeout")
+)
+
+var (
+	command      = "sensors"
+	defPath      = "/usr/bin/sensors"
+	defInterval  = datakit.Duration{Duration: 10 * time.Second}
+	defTimeout   = datakit.Duration{Duration: 3 * time.Second}
+	inputName    = "sensors"
+	sampleConfig = `
+[[inputs.sensors]]
+	## Command path of 'senssor' usually under /usr/bin/sensors
+	path = "/usr/bin/senssors"
+
+	## Gathering interval
+	interval = "10s"
+
+	## Command timeout
+	timeout = "3s"
+`
+	l = logger.SLogger(inputName)
+)
+
+type Input struct {
+	Path     string           `toml:"path"`
+	Interval datakit.Duration `toml:"interval"`
+	Timeout  datakit.Duration `toml:timeout`
+}
+
+func (*Input) Catalog() string {
+	return inputName
+}
+
+func (*Input) SampleConfig() string {
+	return sampleConfig
+}
+
+func (*Input) AvailableArchs() []string {
+	return []string{datakit.OSLinux}
+}
+
+func (*Input) SampleMeasurement() []inputs.Measurement {
+	return []inputs.Measurement{}
+}
+
+func (i *Input) Run() {
+	l.Info("sensors input started")
+
+	tick := time.NewTicker(i.Interval.Duration)
+	for {
+		select {
+		case <-tick.C:
+			if err := i.Gather(); err != nil {
+				l.Error(err.Error())
+				io.FeedLastError(inputName, err.Error())
+				continue
+			}
+		case <-datakit.Exit.Wait():
+			l.Info("sensors input exits")
+
+			return
+		}
+	}
+}
+
+func (i *Input) Gather() error {
+	var (
+		cmd   = exec.Command(i.Path, "-u")
+		buf   bytes.Buffer
+		start = time.Now()
+	)
+	cmd.Stdout = &buf
+	cmd.Stderr = nil
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	timeout := time.AfterFunc(i.Timeout.Duration, func() {
+		if err := cmd.Process.Kill(); err != nil {
+			l.Error(err.Error())
+		}
+	})
+
+	err = cmd.Wait()
+
+	if !timeout.Stop() && err == nil {
+		err = ErrSensorsTimeout
+	}
+	if err != nil {
+		return err
+	}
+
+	if cache, err := parse(string(buf.Bytes())); err != nil {
+		return err
+	} else {
+		return inputs.FeedMeasurement(inputName, datakit.Metric, cache, &io.Option{CollectCost: time.Now().Sub(start)})
+	}
+}
+
+func parse(output string) ([]inputs.Measurement, error) {
+	var (
+		lines  = strings.Split(strings.TrimSpace(output), "\n")
+		tags   = make(map[string]string)
+		fields = make(map[string]interface{})
+		cache  []inputs.Measurement
+	)
+	for _, line := range lines {
+		if line == "" {
+			cache = append(cache, &sensorsMeasurement{
+				name:   inputName,
+				tags:   tags,
+				fields: fields,
+				ts:     time.Now(),
+			})
+			tags = make(map[string]string)
+			fields = make(map[string]interface{})
+		} else {
+			if strings.Contains(line, ":") {
+				parts := strings.Split(line, ":")
+				if strings.HasSuffix(line, ":") {
+					if len(fields) != 0 {
+						cache = append(cache, &sensorsMeasurement{
+							name:   inputName,
+							tags:   tags,
+							fields: fields,
+							ts:     time.Now(),
+						})
+
+						tmp := make(map[string]string)
+						for k, v := range tags {
+							tmp[k] = v
+						}
+						tags = tmp
+						fields = make(map[string]interface{})
+					}
+					tags["feature"] = strings.ToLower(strings.Replace(strings.TrimSpace(parts[0]), " ", "_", -1))
+				} else if strings.HasPrefix(parts[0], " ") {
+					if value, err := strconv.ParseFloat(strings.TrimSpace(parts[1]), 64); err != nil {
+						log.Println(err.Error())
+
+						return nil, err
+					} else {
+						fields[strings.ToLower(strings.TrimSpace(parts[0]))] = value
+					}
+				} else {
+					tags[strings.ToLower(parts[0])] = strings.TrimSpace(parts[1])
+				}
+			} else {
+				tags["chip"] = line
+			}
+		}
+	}
+	cache = append(cache, &sensorsMeasurement{
+		name:   inputName,
+		tags:   tags,
+		fields: fields,
+		ts:     time.Now(),
+	})
+
+	return cache, nil
+}
+
+func init() {
+	inputs.Add("sensors", func() inputs.Input {
+		return &Input{
+			Path:     defPath,
+			Interval: defInterval,
+			Timeout:  defTimeout,
+		}
+	})
+}
