a8924cb4b3ee669732cf5068851cb6d14cd340bd
tanbiao <tanbiao@jiagouyun.com>
2021-05-24 13:40:15 +0800

    update: fix inputs tag samples
     [formerly ef3b320097fd664e93ddc278ead3171d9b4cc28c] [formerly 231b5f4ccac36b82e4b9b06f4b8ab8007d852b96 [formerly bcb98acba0f8b8a3ee0654f290354770d376986c] [formerly 02a710c6d708231f13cfd56924a2bcdbb437c69b [formerly 37e453be874800d84323837cc956b76cd8d9d42d]]]
     [formerly 19c047e4e0126b5e16de5beca3500d9e0f77c585 [formerly aa3cc9be665abf7d9d39b674e0bd0ba7b77963ee]]
    
    
    
    Former-commit-id: 179d0d66caa34f37762d1a4c739320dfafe6752c

--- plugins/inputs/diskio/input.go
+++ plugins/inputs/diskio/input.go
@@ -57,11 +57,9 @@ var (
   # name_templates = ["$ID_FS_LABEL","$DM_VG_NAME/$DM_LV_NAME", "$device:$ID_FS_TYPE"]
   #
 
-[[inputs.diskio.tags]]
-  # some_tag = "some_tag"
-  # more_tag = "more_tag"
-
-`
+[inputs.diskio.tags]
+  # some_tag = "some_value"
+  # more_tag = "some_other_value"`
 )
 
 type diskioMeasurement measurement
