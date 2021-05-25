cede20eea7edc1a4d919883584b60d943ed5bbca
liushaobo <liushaobo@jiagouyun.com>
2021-05-25 23:54:50 +0800

    k8s监控开发
     [formerly 3e50700b789889a9d543fffedb7de0f68983a1f1] [formerly f7d69fa473c18bd573b4b296b1c06dbfecd58707 [formerly 8cdf5eb8860084068363d1aec1c7f253f69dd1da] [formerly 8d5707a75de7868aad5043d4de1a96579a7e5415 [formerly d5da5400cb4d53183498d39eb758750cd8ef1d8b] [formerly 8d5707a75de7868aad5043d4de1a96579a7e5415 [formerly d5da5400cb4d53183498d39eb758750cd8ef1d8b] [formerly 8d5707a75de7868aad5043d4de1a96579a7e5415 [formerly d5da5400cb4d53183498d39eb758750cd8ef1d8b] [formerly dbb74740707b8f1a8147cc1372b49f13f4b419b9 [formerly d675f6f756d69a9d9e5d2ba214261a985d365533]]]]]]
     [formerly 2836a60afbcf715e1d2247d246bdb09b650c9f38 [formerly 445e087c58e3680bbee7213707a27324fc32cd7e] [formerly 2836a60afbcf715e1d2247d246bdb09b650c9f38 [formerly 445e087c58e3680bbee7213707a27324fc32cd7e] [formerly 2836a60afbcf715e1d2247d246bdb09b650c9f38 [formerly 445e087c58e3680bbee7213707a27324fc32cd7e] [formerly 417d42a83d44fa76b64e16304d437dfcc64bef02 [formerly c6320b091867d40c110b21ea81b6a5a287f8cf7c]]]]]
    
    
    
    Former-commit-id: 8478848a5ff883fd098e04b07d69c9f508c78480 [formerly 3be7a9a66e984ee19a992c6d0fa25d645b9dde96]
    Former-commit-id: 570dbd61e660ce28b17fc4cde481bc1ef3286ee2 [formerly 6f502938ce8f8a9dc93b667b58f80c43cf95fa79] [formerly d6adaf37efdfdec0be9c22db351a0f9810b5eb86 [formerly 343b3c4e623fc82782575465f79a9a500e188ff8]]
    Former-commit-id: b2aafc056116c0b907398a656965b8e5a58ba102 [formerly c728f7dbe038f6ead9a868f77f11cd6fba554a03]
    Former-commit-id: d0a910ea18292b32399b815d98a5c1e5f2d70132

---
+++ plugins/inputs/kubernetes/ingress_test.go
@@ -0,0 +1,29 @@
+package kubernetes
+
+// import (
+// 	"testing"
+// 	"context"
+// )
+
+// func TestCollectIngress(t *testing.T) {
+// 	i := &Input{
+// 		Tags: make(map[string]string),
+// 		KubeConfigPath: "/Users/liushaobo/.kube/config",
+// 	}
+
+// 	i.lastErr = i.initCfg()
+// 	ctx := context.Background()
+// 	err := i.collectIngress(ctx)
+
+// 	t.Log("error ---->", err)
+
+// 	for _, m := range i.collectCache {
+// 		point, err := m.LineProto()
+// 		if err != nil {
+// 			t.Log("error ->", err)
+// 		} else {
+// 			t.Log("point ->", point.String())
+// 		}
+// 	}
+// }
+
