4507493790cccd358b65596c1f9a260c2a418375
liushaobo <liushaobo@jiagouyun.com>
2021-05-25 13:33:21 +0800

    daemonset实现
     [formerly 9078adc6e3ccc9e4844690adbec079e54b9a165e [formerly 4ec3da18e6d2127fc29773b686b25f9455b0b2ad] [formerly 3d1767e26107b6b786eadcf8d575c58286b50b00 [formerly b7381818073e8713e6fd7eb2e347fdf21bd6e852]] [formerly 735e88b8ba4903df880f283f71aa2bd241fe3a00] [formerly e75dacf5a4c0c9cb5090313f1b969316dd5011ce [formerly 762156e19cd1d3fb7bf27f593c8f2b3e6868acb8] [formerly e75dacf5a4c0c9cb5090313f1b969316dd5011ce [formerly 762156e19cd1d3fb7bf27f593c8f2b3e6868acb8] [formerly 2553f03dcec28e94e3572d8578541ac083c017c7 [formerly 661b2b1ecafa4f425c05ca5a8c051dd03fd350a3]]]] [formerly b44c4d85dd3915bb03ca212b9bbcba7b27ea07b1] [formerly c331413d6133de0f700b1c77037858b7cb767d1e [formerly 876c89a6d1933856dbb5e79f7984f9fe9bffbb39] [formerly 884ac8375d0dfa9da694680d9cf0bcfc64923c89 [formerly 22243af55a54f3b4af994721aa49116109c8d712] [formerly 884ac8375d0dfa9da694680d9cf0bcfc64923c89 [formerly 22243af55a54f3b4af994721aa49116109c8d712] [formerly 884ac8375d0dfa9da694680d9cf0bcfc64923c89 [formerly 22243af55a54f3b4af994721aa49116109c8d712] [formerly fe44aaaf998bf5bfb23f189c81bbbe800187792d [formerly cf732e16be742a46e8e6a8c5052d427faa20afa1]]]]]]]
     [formerly b44c4d85dd3915bb03ca212b9bbcba7b27ea07b1]
    
    
    
    
    Former-commit-id: 50941fd16a6edf07503dd28ed1c4e766b8d6af3b [formerly 89d37a0a2a9f90e5bc0c16c4c191fe199c887486]
    Former-commit-id: e7bba47ebf22ff1985c7073ab026beb8eb670886 [formerly 688a65b5fa82e216d3a036fe2c745a4320d06bdf] [formerly e5c47dd08e71c1b32e66ecb2c9c13923965aeffb [formerly 98746f0aa13bd1647afdd4deece14fbbcacaacc6]]
    Former-commit-id: 4d18f83f166647b5512d3db17a527e7d337ce003 [formerly 4d815c0eefe3cdc95f88cbea6cefb9d426a976bf]
    Former-commit-id: 0768de42b31a17af2d1ab87c16508ce01f70ce8a

---
+++ plugins/inputs/kubernetes/daemonset.go
@@ -0,0 +1,127 @@
+package kubernetes
+
+import (
+	"time"
+	"context"
+	v1 "k8s.io/api/apps/v1"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+var daemonSetMeasurement = "kube_daemonset"
+
+type daemonSet struct {
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+}
+
+func (m *daemonSet) LineProto() (*io.Point, error) {
+	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *daemonSet) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: daemonSetMeasurement,
+		Desc: "kubernet daemonSet 对象",
+		Tags: map[string]interface{}{
+			"name":      &inputs.TagInfo{Desc: "pod name"},
+			"namespace": &inputs.TagInfo{Desc: "namespace"},
+			"nodeName":  &inputs.TagInfo{Desc: "node name"},
+		},
+		Fields: map[string]interface{}{
+			"ready": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "容器ready数/总数",
+			},
+			"status": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod 状态",
+			},
+			"restarts": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "重启次数",
+			},
+			"age": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod存活时长",
+			},
+			"podIp": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod ip",
+			},
+			"createTime": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod 创建时间",
+			},
+			"label_xxx": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod lable",
+			},
+		},
+	}
+}
+
+func (i *Input) collectDaemonSets(ctx context.Context) error {
+	list, err := i.client.getDaemonSets(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, d := range list.Items {
+		i.gatherDaemonSet(d)
+	}
+
+	return nil
+}
+
+func (i *Input) gatherDaemonSet(d v1.DaemonSet) {
+	fields := map[string]interface{}{
+		"generation":               d.Generation,
+		"current_number_scheduled": d.Status.CurrentNumberScheduled,
+		"desired_number_scheduled": d.Status.DesiredNumberScheduled,
+		"number_available":         d.Status.NumberAvailable,
+		"number_misscheduled":      d.Status.NumberMisscheduled,
+		"number_ready":             d.Status.NumberReady,
+		"number_unavailable":       d.Status.NumberUnavailable,
+		"updated_number_scheduled": d.Status.UpdatedNumberScheduled,
+	}
+
+	tags := map[string]string{
+		"daemonset_name": d.Name,
+		"namespace":      d.Namespace,
+	}
+	// for key, val := range d.Spec.Selector.MatchLabels {
+	// 	if i.selectorFilter.Match(key) {
+	// 		tags["selector_"+key] = val
+	// 	}
+	// }
+
+	if d.GetCreationTimestamp().Second() != 0 {
+		fields["created"] = d.GetCreationTimestamp().UnixNano()
+	}
+
+    m := &daemonSet{
+		name:   daemonSetMeasurement,
+		tags:   tags,
+		fields: fields,
+		ts:     time.Now(),
+	}
+
+	i.collectCache = append(i.collectCache, m)
+}
