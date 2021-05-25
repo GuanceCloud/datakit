74f661026a7afa3c5559d96f95184f32248a7763
liushaobo <liushaobo@jiagouyun.com>
2021-05-25 20:44:39 +0800

    redis测试
    Former-commit-id: 4f763b628d49fd611b4b8660d949c8c1ba21718c

--- plugins/inputs/redis/input_test.go
+++ plugins/inputs/redis/input_test.go
@@ -2,19 +2,20 @@ package redis
 
 import (
 	"fmt"
-	"testing"
 	"github.com/stretchr/testify/assert"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
+	"testing"
+	// "gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
 func TestCollectInfoMeasurement(t *testing.T) {
 	input := &Input{
-		Host:         "127.0.0.1",
-		Port:         6379,
-		Password:     "dev",
-		Service:      "dev-test",
-		Tags:         make(map[string]string),
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "dev",
+		Service:  "dev-test",
+		Tags:     make(map[string]string),
+		Log:      &inputs.TailerOption{},
 	}
 
 	err := input.initCfg()
@@ -40,11 +41,11 @@ func TestCollectInfoMeasurement(t *testing.T) {
 
 func TestCollectClientMeasurement(t *testing.T) {
 	input := &Input{
-		Host:         "127.0.0.1",
-		Port:         6379,
-		Password:     "dev",
-		Service:      "dev-test",
-		Tags:         make(map[string]string),
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "dev",
+		Service:  "dev-test",
+		Tags:     make(map[string]string),
 	}
 
 	input.initCfg()
@@ -66,11 +67,11 @@ func TestCollectClientMeasurement(t *testing.T) {
 
 func TestCollectCommandMeasurement(t *testing.T) {
 	input := &Input{
-		Host:         "127.0.0.1",
-		Port:         6379,
-		Password:     "dev",
-		Service:      "dev-test",
-		Tags:         make(map[string]string),
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "dev",
+		Service:  "dev-test",
+		Tags:     make(map[string]string),
 	}
 
 	input.initCfg()
@@ -92,11 +93,11 @@ func TestCollectCommandMeasurement(t *testing.T) {
 
 func TestCollectSlowlogMeasurement(t *testing.T) {
 	input := &Input{
-		Host:         "127.0.0.1",
-		Port:         6379,
-		Password:     "dev",
-		Service:      "dev-test",
-		Tags:         make(map[string]string),
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "dev",
+		Service:  "dev-test",
+		Tags:     make(map[string]string),
 	}
 
 	err := input.initCfg()
@@ -122,13 +123,13 @@ func TestCollectSlowlogMeasurement(t *testing.T) {
 
 func TestCollectBigKeyMeasurement(t *testing.T) {
 	input := &Input{
-		Host:         "127.0.0.1",
-		Port:         6379,
-		Password:     "dev",
-		Service:      "dev-test",
-		Tags:         make(map[string]string),
-		Keys:         []string{"queue"},
-		DB:           1,
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "dev",
+		Service:  "dev-test",
+		Tags:     make(map[string]string),
+		Keys:     []string{"queue"},
+		DB:       1,
 	}
 
 	input.initCfg()
@@ -148,14 +149,70 @@ func TestCollectBigKeyMeasurement(t *testing.T) {
 	}
 }
 
-func TestLoadCfg(t *testing.T) {
-	arr, err := config.LoadInputConfigFile("./redis.conf", func() inputs.Input {
-		return &Input{}
-	})
-
-	if err != nil {
-		t.Fatalf("%s", err)
+func TestCollectDBMeasurement(t *testing.T) {
+	input := &Input{
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "dev",
+		Service:  "dev-test",
+		Tags:     make(map[string]string),
+		Keys:     []string{"queue"},
+		DB:       1,
 	}
 
-	arr[0].(*Input).Run()
+	input.initCfg()
+
+	resData, err := input.collectDBMeasurement()
+	if err != nil {
+		assert.Error(t, err, "collect data err")
+	}
+
+	for _, pt := range resData {
+		point, err := pt.LineProto()
+		if err != nil {
+			fmt.Println("error =======>", err)
+		} else {
+			fmt.Println("point line =====>", point.String())
+		}
+	}
 }
+
+func TestCollectReplicaMeasurement(t *testing.T) {
+	input := &Input{
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "dev",
+		Service:  "dev-test",
+		Tags:     make(map[string]string),
+		Keys:     []string{"queue"},
+		DB:       1,
+	}
+
+	input.initCfg()
+
+	resData, err := input.collectReplicaMeasurement()
+	if err != nil {
+		assert.Error(t, err, "collect data err")
+	}
+
+	for _, pt := range resData {
+		point, err := pt.LineProto()
+		if err != nil {
+			fmt.Println("error =======>", err)
+		} else {
+			fmt.Println("point line =====>", point.String())
+		}
+	}
+}
+
+// func TestLoadCfg(t *testing.T) {
+// 	arr, err := config.LoadInputConfigFile("./redis.conf", func() inputs.Input {
+// 		return &Input{}
+// 	})
+
+// 	if err != nil {
+// 		t.Fatalf("%s", err)
+// 	}
+
+// 	arr[0].(*Input).Run()
+// }
