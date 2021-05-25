8294fbe8114bdf24f8d6f7aa11a5c674c019610b
liushaobo <liushaobo@jiagouyun.com>
2021-05-25 23:54:50 +0800

    k8s监控开发
    Former-commit-id: fb631dc26b44afcb7079c980893f86a34cbad262 [formerly d5da5400cb4d53183498d39eb758750cd8ef1d8b] [formerly dbb74740707b8f1a8147cc1372b49f13f4b419b9 [formerly d675f6f756d69a9d9e5d2ba214261a985d365533]]
    Former-commit-id: 417d42a83d44fa76b64e16304d437dfcc64bef02 [formerly c6320b091867d40c110b21ea81b6a5a287f8cf7c]
    Former-commit-id: 445e087c58e3680bbee7213707a27324fc32cd7e

---
+++ plugins/inputs/kubernetes/deployment.go
@@ -0,0 +1,123 @@
+package kubernetes
+
+import (
+	"context"
+	"time"
+
+	v1 "k8s.io/api/apps/v1"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+var deploymentMeasurement = "kube_deployment"
+
+type deployment struct {
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+}
+
+func (m *deployment) LineProto() (*io.Point, error) {
+	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *deployment) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: deploymentMeasurement,
+		Desc: "kubernet daemonSet 对象",
+		Tags: map[string]interface{}{
+			"name":      &inputs.TagInfo{Desc: "pod name"},
+			"namespace": &inputs.TagInfo{Desc: "namespace"},
+			"nodeName":  &inputs.TagInfo{Desc: "node name"},
+		},
+		Fields: map[string]interface{}{
+			"ready": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "容器ready数/总数",
+			},
+			"status": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod 状态",
+			},
+			"restarts": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "重启次数",
+			},
+			"age": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod存活时长",
+			},
+			"podIp": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod ip",
+			},
+			"createTime": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod 创建时间",
+			},
+			"label_xxx": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod lable",
+			},
+		},
+	}
+}
+
+func (i *Input) collectDeployments(ctx context.Context) error {
+	list, err := i.client.getDeployments(ctx)
+	if err != nil {
+		return err
+	}
+	for _, d := range list.Items {
+		i.gatherDeployment(d)
+	}
+
+	return nil
+}
+
+func (i *Input) gatherDeployment(d v1.Deployment) {
+	fields := map[string]interface{}{
+		"replicas_available":   d.Status.AvailableReplicas,
+		"replicas_unavailable": d.Status.UnavailableReplicas,
+		"created":              d.GetCreationTimestamp().UnixNano(),
+	}
+	tags := map[string]string{
+		"deployment_name": d.Name,
+		"namespace":       d.Namespace,
+	}
+
+	// for key, val := range d.Spec.Selector.MatchLabels {
+	// 	if ki.selectorFilter.Match(key) {
+	// 		tags["selector_"+key] = val
+	// 	}
+	// }
+
+	if d.GetCreationTimestamp().Second() != 0 {
+		fields["created"] = d.GetCreationTimestamp().UnixNano()
+	}
+
+    m := &deployment{
+		name:   deploymentMeasurement,
+		tags:   tags,
+		fields: fields,
+		ts:     time.Now(),
+	}
+
+	i.collectCache = append(i.collectCache, m)
+
+}
