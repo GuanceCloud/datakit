60f58973cac4ab5628f4d9bbc6fa9fa750fbd333
liushaobo <liushaobo@jiagouyun.com>
2021-05-25 23:54:50 +0800

    k8s监控开发
     [formerly fc55442f82664e56c2c9ce1fc4b9f778b576c432 [formerly 3e22f454b595cdc20bbe04d07d66a5dea38ff235] [formerly 4ef145a2da953e758e6f97d0b02e8fea001d0137 [formerly 4304ec709a9f36b449686a03df6f64762da1c3f8]] [formerly 9d553603b8363819215e80d129f380db1dfc0eaa] [formerly c02337982a47bf2a1c39b8f9ab0ada2e7e941607 [formerly c1e9b3e0e091cdc05fef69608925896615ebcd4f] [formerly c02337982a47bf2a1c39b8f9ab0ada2e7e941607 [formerly c1e9b3e0e091cdc05fef69608925896615ebcd4f] [formerly 19ab6b89612ed7eac4a48fb651dd5c8e30a66c3e [formerly cbd80c76cebe19a22787e62e049a5c34d2f9f476]]]] [formerly 3e50700b789889a9d543fffedb7de0f68983a1f1] [formerly f7d69fa473c18bd573b4b296b1c06dbfecd58707 [formerly 8cdf5eb8860084068363d1aec1c7f253f69dd1da] [formerly fb631dc26b44afcb7079c980893f86a34cbad262 [formerly d5da5400cb4d53183498d39eb758750cd8ef1d8b] [formerly fb631dc26b44afcb7079c980893f86a34cbad262 [formerly d5da5400cb4d53183498d39eb758750cd8ef1d8b] [formerly fb631dc26b44afcb7079c980893f86a34cbad262 [formerly d5da5400cb4d53183498d39eb758750cd8ef1d8b] [formerly dbb74740707b8f1a8147cc1372b49f13f4b419b9 [formerly d675f6f756d69a9d9e5d2ba214261a985d365533]]]]]]]
     [formerly 3e50700b789889a9d543fffedb7de0f68983a1f1]
    
    
    
    
    Former-commit-id: a0fe94bba734ee0487124f5da6964623c0b663fa [formerly 7bf60e2d5db401d1c2185f311f13d47e3f35eb0a]
    Former-commit-id: fb2407448d5de09513bf9a44c55263522ff40af4 [formerly d5a61c016c0a2dfd731a0fda3f1f2f6c1f5dfa26] [formerly 28e7d914b9798f324f030fcaef6082f36bb146fa [formerly e1a868969008264034201309c8540cd19092a527]]
    Former-commit-id: 74047c1c9aac8b357055e46f09a26ff720360579 [formerly 7858892740548b4a4ffc78203ed3652e6ffaf56c]
    Former-commit-id: ba9f45ec1125f05e58cba1765dc1fdd4178d7ca6

---
+++ plugins/inputs/kubernetes/pvc.go
@@ -0,0 +1,127 @@
+package kubernetes
+
+import (
+	"context"
+	"time"
+	"strings"
+	corev1 "k8s.io/api/core/v1"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+)
+
+var pvcMeasurement = "kube_pvc"
+
+type pvcM struct {
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+}
+
+func (m *pvcM) LineProto() (*io.Point, error) {
+	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *pvcM) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: deploymentMeasurement,
+		Desc: "kubernet daemonSet 对象",
+		Tags: map[string]interface{}{
+			"name":      &inputs.TagInfo{Desc: "pod name"},
+			"namespace": &inputs.TagInfo{Desc: "namespace"},
+			"nodeName":  &inputs.TagInfo{Desc: "node name"},
+		},
+		Fields: map[string]interface{}{
+			"ready": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "容器ready数/总数",
+			},
+			"status": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod 状态",
+			},
+			"restarts": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "重启次数",
+			},
+			"age": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod存活时长",
+			},
+			"podIp": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod ip",
+			},
+			"createTime": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod 创建时间",
+			},
+			"label_xxx": &inputs.FieldInfo{
+				DataType: inputs.String,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "pod lable",
+			},
+		},
+	}
+}
+
+func (i *Input) collectPersistentVolumeClaims(ctx context.Context) error {
+	list, err := i.client.getPersistentVolumeClaims(ctx)
+	if err != nil {
+		return err
+	}
+	for _, pvc := range list.Items {
+		i.gatherPersistentVolumeClaim(pvc)
+	}
+
+	return err
+}
+
+func (i *Input) gatherPersistentVolumeClaim(pvc corev1.PersistentVolumeClaim) {
+	phaseType := 3
+	switch strings.ToLower(string(pvc.Status.Phase)) {
+	case "bound":
+		phaseType = 0
+	case "lost":
+		phaseType = 1
+	case "pending":
+		phaseType = 2
+	}
+	fields := map[string]interface{}{
+		"phase_type": phaseType,
+	}
+	tags := map[string]string{
+		"pvc_name":     pvc.Name,
+		"namespace":    pvc.Namespace,
+		"phase":        string(pvc.Status.Phase),
+		"storageclass": *pvc.Spec.StorageClassName,
+	}
+
+	// for key, val := range pvc.Spec.Selector.MatchLabels {
+	// 	if i.selectorFilter.Match(key) {
+	// 		tags["selector_"+key] = val
+	// 	}
+	// }
+
+	m := &pvcM{
+		name:   daemonSetMeasurement,
+		tags:   tags,
+		fields: fields,
+		ts:     time.Now(),
+	}
+
+	i.collectCache = append(i.collectCache, m)
+}
