827813006824db0e183a6017e6b36005f6dad22d
liushaobo <liushaobo@jiagouyun.com>
2021-05-26 17:58:20 +0800

    完善测试
    Former-commit-id: 6cac8133ba97fcccc9cd5b3ab9061ca6117de5eb

--- plugins/inputs/mysql/mysql_test.go
+++ plugins/inputs/mysql/mysql_test.go
@@ -2,9 +2,37 @@ package mysql
 
 import (
 	"fmt"
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
+
+func TestCollect(t *testing.T) {
+	t.Run("true", func(t *testing.T) {
+		input := &Input{
+			Host: "127.0.0.1",
+			Port: 3306,
+			User: "root",
+			Pass: "test",
+			Tags: make(map[string]string),
+		}
+
+		input.initCfg()
+		input.Collect()
+	})
+
+	t.Run("error", func(t *testing.T) {
+		input := &Input{
+			Host: "127.0.0.2",
+			Port: 3306,
+			User: "root",
+			Pass: "test",
+			Tags: make(map[string]string),
+		}
+
+		input.initCfg()
+		input.Collect()
+	})
+}
 
 // func TestGetDsnString(t *testing.T) {
 // 	m := MysqlMonitor{}
@@ -18,104 +46,7 @@ import (
 // 	assert.Equal(t, expected, actual)
 // }
 
-// func TestGetStatus(t *testing.T) {
-// 	m := MysqlMonitor{}
-// 	m.Host = "127.0.0.1"
-// 	m.Port = 3306
-// 	m.User = "root"
-// 	m.Pass = "test"
-
-// 	initLog()
-
-// 	m.initCfg()
-// 	m.resData = make(map[string]interface{})
-
-// 	m.getStatus()
-// 	m.submitMetrics()
-// }
-
-// func TestGetVariables(t *testing.T) {
-// 	m := MysqlMonitor{}
-// 	m.Host = "127.0.0.1"
-// 	m.Port = 3306
-// 	m.User = "root"
-// 	m.Pass = "test"
-
-// 	initLog()
-
-// 	m.initCfg()
-// 	m.resData = make(map[string]interface{})
-
-// 	m.getVariables()
-// 	m.submitMetrics()
-// }
-
-// // todo
-// func TestGetInnodbStatus(t *testing.T) {
-// 	m := MysqlMonitor{}
-// 	m.Host = "127.0.0.1"
-// 	m.Port = 3306
-// 	m.User = "root"
-// 	m.Pass = "test"
-
-// 	initLog()
-
-// 	m.initCfg()
-// 	m.resData = make(map[string]interface{})
-
-// 	m.getInnodbStatus()
-// 	m.submitMetrics()
-// }
-
-// func TestGetLogStats(t *testing.T) {
-// 	m := MysqlMonitor{}
-// 	m.Host = "rm-bp15268nefz6870hg.mysql.rds.aliyuncs.com"
-// 	m.Port = 3306
-// 	m.User = "cc_monitor"
-// 	m.Pass = "Zyadmin123"
-
-// 	initLog()
-
-// 	m.initCfg()
-// 	m.resData = make(map[string]interface{})
-
-// 	m.getLogStats()
-// 	m.submitMetrics()
-// }
-
-// func TestQuerySizePerschema(t *testing.T) {
-// 	m := MysqlMonitor{}
-// 	m.Host = "rm-bp15268nefz6870hg.mysql.rds.aliyuncs.com"
-// 	m.Port = 3306
-// 	m.User = "cc_monitor"
-// 	m.Pass = "Zyadmin123"
-
-// 	initLog()
-
-// 	m.initCfg()
-// 	m.resData = make(map[string]interface{})
-
-// 	m.querySizePerschema()
-// 	m.submitMetrics()
-// }
-
-// func TestGetQueryExecTimePerSchema(t *testing.T) {
-// 	m := MysqlMonitor{}
-// 	m.Host = "127.0.0.1"
-// 	m.Port = 3306
-// 	m.User = "root"
-// 	m.Pass = "test"
-
-// 	initLog()
-
-// 	m.initCfg()
-// 	m.resData = make(map[string]interface{})
-
-// 	m.getQueryExecTimePerSchema()
-// 	m.submitMetrics()
-// }
-
-func TestCollect(t *testing.T) {
+func TestRun(t *testing.T) {
 	input := &Input{
 		Host: "127.0.0.1",
 		Port: 3306,
@@ -124,45 +55,46 @@ func TestCollect(t *testing.T) {
 		Tags: make(map[string]string),
 	}
 
-	input.Query = []*customQuery{
-		&customQuery{
-			sql:    "select id, namespace,email, username, value from core_stone.biz_main_account",
-			metric: "cutomer-metric",
-			tags:   []string{"id"},
-			fields: []string{},
-		},
+	if err := input.initCfg(); err != nil {
+		assert.Error(t, err, "collect data err")
 	}
-
-	input.initCfg()
 
 	input.Collect()
 }
 
 func TestInnodbCollect(t *testing.T) {
-	input := &Input{
-		Host: "127.0.0.1",
-		Port: 3306,
-		User: "root",
-		Pass: "test",
-		Tags: make(map[string]string),
-	}
+	t.Run("bin log off", func(t *testing.T) {
+		input := &Input{
+			Host: "127.0.0.1",
+			Port: 3306,
+			User: "root",
+			Pass: "test",
+			Tags: make(map[string]string),
+		}
 
-	input.Query = []*customQuery{
-		&customQuery{
-			sql:    "select id, namespace,email, username, value from core_stone.biz_main_account",
-			metric: "cutomer-metric",
-			tags:   []string{"id"},
-			fields: []string{},
-		},
-	}
+		err := input.initCfg()
+		if err != nil {
+			assert.Error(t, err, "collect data err")
+		}
 
-	input.initCfg()
+		resData, err := input.collectInnodbMeasurement()
+		if err != nil {
+			assert.Error(t, err, "collect data err")
+		}
 
-	input.collectInnodbMeasurement()
+		for _, pt := range resData {
+			point, err := pt.LineProto()
+			if err != nil {
+				fmt.Println("error =======>", err)
+			} else {
+				fmt.Println("point line =====>", point.String())
+			}
+		}
+	})
 }
 
 func TestBaseCollect(t *testing.T) {
-	t.Run("bin log off", func(t *testing.T){
+	t.Run("bin log off", func(t *testing.T) {
 		input := &Input{
 			Host: "127.0.0.1",
 			Port: 3306,
@@ -191,7 +123,7 @@ func TestBaseCollect(t *testing.T) {
 		}
 	})
 
-	t.Run("bin log on", func(t *testing.T){
+	t.Run("bin log on", func(t *testing.T) {
 		input := &Input{
 			Host: "rm-bp15268nefz6870hg.mysql.rds.aliyuncs.com",
 			Port: 3306,
@@ -236,6 +168,44 @@ func TestSchemaCollect(t *testing.T) {
 	}
 
 	resData, err := input.collectSchemaMeasurement()
+	if err != nil {
+		assert.Error(t, err, "collect data err")
+	}
+
+	for _, pt := range resData {
+		point, err := pt.LineProto()
+		if err != nil {
+			fmt.Println("error =======>", err)
+		} else {
+			fmt.Println("point line =====>", point.String())
+		}
+	}
+}
+
+func TestCustomSchemaMeasurement(t *testing.T) {
+	input := &Input{
+		Host: "127.0.0.1",
+		Port: 3306,
+		User: "root",
+		Pass: "test",
+		Tags: make(map[string]string),
+	}
+
+	input.Query = []*customQuery{
+		&customQuery{
+			sql:    "select id, namespace,email, username, value from core_stone.biz_main_account",
+			metric: "cutomer-metric",
+			tags:   []string{"id"},
+			fields: []string{},
+		},
+	}
+
+	err := input.initCfg()
+	if err != nil {
+		assert.Error(t, err, "collect data err")
+	}
+
+	resData, err := input.customSchemaMeasurement()
 	if err != nil {
 		assert.Error(t, err, "collect data err")
 	}
