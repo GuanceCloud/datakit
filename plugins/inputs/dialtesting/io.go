2b006945a6ef2bfea71cf7b75a6ab4293766283a
dlj <denglijun@jiagouyun.com>
2021-05-26 17:54:50 +0800

    [+] add dialtesting headless

--- plugins/inputs/dialtesting/io.go
+++ plugins/inputs/dialtesting/io.go
@@ -1,6 +1,7 @@
 package dialtesting
 
 import (
+	lp "gitlab.jiagouyun.com/cloudcare-tools/cliutils/lineproto"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
@@ -10,4 +11,18 @@ func Feed(name, category string, pt *io.Point, opt *io.Option) error {
 	pts = append(pts, pt)
 
 	return io.Feed(name, category, pts, opt)
+}
+
+func LineDataFeed(data string, name, category string, opt *io.Option) error {
+	pts, err := lp.ParsePoints([]byte(data), nil)
+	if err != nil {
+		return err
+	}
+
+	x := []*io.Point{}
+	for _, pt := range pts {
+		x = append(x, &io.Point{Point: pt})
+	}
+
+	return io.Feed(name, category, x, opt)
 }
