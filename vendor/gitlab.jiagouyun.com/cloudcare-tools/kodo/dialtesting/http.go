b495c457cfc8ed1bd8178453f29ed04ef9ad4d1f
dlj <denglijun@jiagouyun.com>
2021-05-27 13:29:59 +0800

    [*] update dialtesting vendor
     [formerly d90bc848c7ff047a6f7a28d0bc091a8d33167770] [formerly 5e07a0b6ad5705aa841b10ab891ecccbeb520ea6 [formerly df6d2395d3dbbf8757c4fee2067ad3790136209f] [formerly 5ef9ea6f6aa0f23d362d9d4634ce17325b9c6fd6 [formerly 35e80ea38a7701f00428192029cde409cc79b8dd]]]
     [formerly 428dccadbd8ef3907693348a12936c2448fe4a1e [formerly 98f96f6625fac7021a8a76c1f4f8c591fec49138]]
    
    
    
    Former-commit-id: ef57566a7cba1861d28ee25aeeccb57386964eb1

--- vendor/gitlab.jiagouyun.com/cloudcare-tools/kodo/dialtesting/http.go
+++ vendor/gitlab.jiagouyun.com/cloudcare-tools/kodo/dialtesting/http.go
@@ -84,6 +84,26 @@ func (t *HTTPTask) GetOwnerExternalID() string {
 	return t.OwnerExternalID
 }
 
+func (t *HTTPTask) SetOwnerExternalID(exid string) {
+	t.OwnerExternalID = exid
+}
+
+func (t *HTTPTask) SetRegionId(regionId string) {
+	t.Region = regionId
+}
+
+func (t *HTTPTask) SetAk(ak string) {
+	t.AK = ak
+}
+
+func (t *HTTPTask) SetStatus(status string) {
+	t.CurStatus = status
+}
+
+func (t *HTTPTask) SetUpdateTime(ts int64) {
+	t.UpdateTime = ts
+}
+
 func (t *HTTPTask) Stop() error {
 	t.cli.CloseIdleConnections()
 	return nil
