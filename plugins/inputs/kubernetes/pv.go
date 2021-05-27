2b96a608e8002f7842e33e81141d2af869018982
liushaobo <liushaobo@jiagouyun.com>
2021-05-27 14:43:40 +0800

    k8s监控开发
    Former-commit-id: 7b79c2ce3ea5023ea443ce8ed0b64e122b668db6 [formerly 84d1eed3a33eef0a385b61355588889bfd79a207 [formerly dcc1c29cc08178da754cdbe2d30fd40a347b46ab] [formerly 0c70233e18d1ca6988a52bced22ace349a5b241d [formerly 440107999deee69835aa71124d65b8caff1128ef]]]
    Former-commit-id: a3138c5ee158bd4ca85f52d261830b8abdfe2c82
    Former-commit-id: 5cac1c0f08caa0e3877ab55dfca04d6c584b3a1d [formerly 99bf8271333e997c883cee0046fcb3fea2fc5e8d]
    Former-commit-id: e75aa4a001e1c0d9ea88c81caa9a63bd47db3a6d

--- plugins/inputs/kubernetes/pv.go
+++ plugins/inputs/kubernetes/pv.go
@@ -1,13 +1,13 @@
 package kubernetes
 
 import (
-	"time"
 	"context"
 	"strings"
+	"time"
 
-	corev1 "k8s.io/api/core/v1"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+	corev1 "k8s.io/api/core/v1"
 )
 
 var pvMeasurement = "kube_daemonset"
