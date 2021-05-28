7823c4f9f28d2888a687c69ee6dd4e0721514dd4
liushaobo <liushaobo@jiagouyun.com>
2021-05-28 14:31:34 +0800

    提交测试
    Former-commit-id: 4b9ff9ebfad1800b54cfe176063069eadd7ba782 [formerly 4da4f0dfa0020bdfdd9b98dfc5d8d187f388ae78] [formerly 907ead344f75e8a948c14a6591d8cad8de2293ce [formerly f2ac1b5a44299c954c8f797d0bd9ca3dddf9257a]]
    Former-commit-id: d59443c91ffda7f98355e5019ae0f5e2ec731b29 [formerly 9ce50e0acf29d33932c65e22aead83e462204b4b]
    Former-commit-id: 9fa5f696c54a3acd3441132d7d1abace8935ff96

--- plugins/inputs/kubernetes/input_test.go
+++ plugins/inputs/kubernetes/input_test.go
@@ -1,26 +1,110 @@
 package kubernetes
 
 import (
+	"github.com/influxdata/telegraf/plugins/common/tls"
+	"github.com/stretchr/testify/assert"
 	"testing"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
-func TestMain(t *testing.T) {
+func TestInitCfg(t *testing.T) {
 	i := &Input{
-		Tags:           make(map[string]string),
-		KubeConfigPath: "/Users/liushaobo/.kube/config",
+		Tags:              make(map[string]string),
+		URL:               "https://172.16.2.41:6443",
+		BearerTokenString: "eyJhbGciOiJSUzI1NiIsImtpZCI6InFWNzd1LTNDNEdEd0FlTjdPQzF1NXBGVnYxU2JrTlVJQ3RUUnZlbXRGZ1EifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4ta3F4NzUiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjM5ZmQxOTQ4LTY5YTAtNDZlZi1hZjc3LWYxYzUwMmFmZDdiMiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.f4oPuQ0fuY1jZI7o7CeGr-FtfQbnYlxzphtZAeKo31HjQAG5ynl4rYLRt1PK7lpCoMiMrAw5xDSMlG2DN9bTF3OYQJbfC4Mq3olPGxHHjxoTSotrfGrMK779NZ_JzRw6OQ9mKEgG8vadFpd4nGRi4KuD-7w8ysOzm_j6Z78eVTxhKrOuU11a6WEUh_LGnJSNLjAdN8xKqim90qcWy5jvdYl2s9N2tRPvkSJ22xwJ9Icts0HHZfvAywG7Rb69WyN13ct37N1_bICwjVrWuONyXOgNSiV7JvUFI2ZFpKfpDrDhpGRwwmVCR5a8BjP0S1kNjjckK9ma4ubYyvLIDS86Xw",
+		ClientConfig: tls.ClientConfig{
+			TLSCA: "/Users/liushaobo/go/src/gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/kubernetes/pki/cacert.pem",
+		},
 	}
 
 	err := i.initCfg()
 	t.Log("error ---->", err)
 
-	i.Collect()
-
-	for _, obj := range i.collectCache {
-		point, err := obj.LineProto()
-		if err != nil {
-			t.Log("error ->", err)
-		} else {
-			t.Log("point ->", point.String())
-		}
+	// 通过 ServerVersion 方法来获取版本号
+	versionInfo, err := i.client.ServerVersion()
+	if err != nil {
+		assert.Error(t, err, "")
 	}
+
+	t.Log("version ==>", versionInfo.String())
+}
+
+func TestMain(t *testing.T) {
+	// t.Run("config", func(t *testing.T) {
+	// 	i := &Input{
+	// 		Tags:           make(map[string]string),
+	// 		KubeConfigPath: "/Users/liushaobo/.kube/config",
+	// 	}
+
+	// 	err := i.initCfg()
+	// 	t.Log("error ---->", err)
+
+	// 	i.Collect()
+
+	// 	for _, obj := range i.collectCache {
+	// 		point, err := obj.LineProto()
+	// 		if err != nil {
+	// 			t.Log("error ->", err)
+	// 		} else {
+	// 			t.Log("point ->", point.String())
+	// 		}
+	// 	}
+	// })
+
+	t.Run("bear token", func(t *testing.T) {
+		i := &Input{
+			Tags:              make(map[string]string),
+			URL:               "https://172.16.2.41:6443",
+			BearerTokenString: "eyJhbGciOiJSUzI1NiIsImtpZCI6InFWNzd1LTNDNEdEd0FlTjdPQzF1NXBGVnYxU2JrTlVJQ3RUUnZlbXRGZ1EifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4ta3F4NzUiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjM5ZmQxOTQ4LTY5YTAtNDZlZi1hZjc3LWYxYzUwMmFmZDdiMiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.f4oPuQ0fuY1jZI7o7CeGr-FtfQbnYlxzphtZAeKo31HjQAG5ynl4rYLRt1PK7lpCoMiMrAw5xDSMlG2DN9bTF3OYQJbfC4Mq3olPGxHHjxoTSotrfGrMK779NZ_JzRw6OQ9mKEgG8vadFpd4nGRi4KuD-7w8ysOzm_j6Z78eVTxhKrOuU11a6WEUh_LGnJSNLjAdN8xKqim90qcWy5jvdYl2s9N2tRPvkSJ22xwJ9Icts0HHZfvAywG7Rb69WyN13ct37N1_bICwjVrWuONyXOgNSiV7JvUFI2ZFpKfpDrDhpGRwwmVCR5a8BjP0S1kNjjckK9ma4ubYyvLIDS86Xw",
+			ClientConfig: tls.ClientConfig{
+				TLSCA: "/Users/liushaobo/go/src/gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/kubernetes/pki/cacert.pem",
+			},
+		}
+
+		err := i.initCfg()
+		t.Log("error ---->", err)
+
+		i.Collect()
+
+		t.Log("======>", i.collectCache)
+
+		for _, obj := range i.collectCache {
+			point, err := obj.LineProto()
+			if err != nil {
+				t.Log("error ->", err)
+			} else {
+				t.Log("point ->", point.String())
+			}
+		}
+	})
+}
+
+func TestLoadCfg(t *testing.T) {
+	arr, err := config.LoadInputConfigFile("./cfg.conf", func() inputs.Input {
+		return &Input{}
+	})
+
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	kube := arr[0].(*Input)
+
+    t.Log("url ---->", kube.URL)
+	t.Log("token ---->", kube.BearerTokenString)
+	t.Log("ca ---->", kube.TLSCA)
+}
+
+func TestRun(t *testing.T) {
+	arr, err := config.LoadInputConfigFile("./cfg.conf", func() inputs.Input {
+		return &Input{}
+	})
+
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	kube := arr[0].(*Input)
+	kube.Run()
 }
