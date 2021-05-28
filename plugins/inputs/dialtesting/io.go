a1fde57127c422fff70791a02452e40a35f4f18b
dlj <denglijun@jiagouyun.com>
2021-05-28 10:54:54 +0800

    [*] update headless dialtesting tags
    Former-commit-id: f0fbc9f401fe8040722af460bdb65a7df8c5c569
    Former-commit-id: da46af73c40e3bed39ecb89f492385597468c04d [formerly 992b4b2373d37b7f8c41b47364d2e032f54a5a8d]
    Former-commit-id: e498696913f939349c5759a1e6da672abb141222

--- plugins/inputs/dialtesting/io.go
+++ plugins/inputs/dialtesting/io.go
@@ -1,20 +1,26 @@
 package dialtesting
 
 import (
+	"time"
+
 	lp "gitlab.jiagouyun.com/cloudcare-tools/cliutils/lineproto"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
-func Feed(name, category string, pt *io.Point, opt *io.Option) error {
+func (d *dialer) linedataFeed(urlStr string, precision string) error {
+	data := d.task.GetLineData()
+	if len(data) == 0 {
+		l.Warnf("no any data for task %s", d.task.ID())
+		return nil
+	}
 
-	pts := []*io.Point{}
-	pts = append(pts, pt)
-
-	return io.Feed(name, category, pts, opt)
-}
-
-func LineDataFeed(data string, name, category string, opt *io.Option) error {
-	pts, err := lp.ParsePoints([]byte(data), nil)
+	pts, err := lp.ParsePoints([]byte(data), &lp.Option{
+		Time:      time.Now(),
+		Precision: precision,
+		ExtraTags: d.tags,
+		Strict:    true,
+	})
 	if err != nil {
 		return err
 	}
@@ -24,5 +30,41 @@ func LineDataFeed(data string, name, category string, opt *io.Option) error {
 		x = append(x, &io.Point{Point: pt})
 	}
 
-	return io.Feed(name, category, x, opt)
+	return Feed(inputName, datakit.Logging, x, &io.Option{
+		HTTPHost: urlStr,
+	})
+
+}
+
+func (d *dialer) pointsFeed(urlStr string) error {
+	// 获取此次任务执行的基本信息
+	tags := map[string]string{}
+	fields := map[string]interface{}{}
+	tags, fields = d.task.GetResults()
+
+	for k, v := range d.tags {
+		tags[k] = v
+	}
+
+	data, err := io.MakePoint(d.task.MetricName(), tags, fields, time.Now())
+	if err != nil {
+		l.Warnf("make metric failed: %s", err.Error)
+		return err
+	}
+
+	pts := []*io.Point{}
+	pts = append(pts, data)
+
+	err = Feed(inputName, datakit.Logging, pts, &io.Option{
+		HTTPHost: urlStr,
+	})
+
+	l.Debugf(`url:%s, tags: %+#v, fs: %+#v`, urlStr, tags, fields)
+
+	return err
+}
+
+func Feed(name, category string, pts []*io.Point, opt *io.Option) error {
+
+	return io.Feed(name, category, pts, opt)
 }
