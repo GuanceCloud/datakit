484b4d9c72c6d9a9941c7c5790cbbf5998d8e315
dlj <denglijun@jiagouyun.com>
2021-05-28 10:54:54 +0800

    [*] update headless dialtesting tags
     [formerly 6466cae1d2df6fdef0aff2d90158d30f588256e6 [formerly 4c9f78911ab4d5e3deeab4e88f7cbff7de18dab2] [formerly a3b8735c4b606742a9d2f740d5a3b1f63bbb6fe7 [formerly 994134591445a191a35e3a6e9dd378b0389146a5]] [formerly d0942cbbea85bb2b063bd8f3f1e01d7a5ea817cc] [formerly 549360595ae8c0ebe0e6adeafaa9846919a90319 [formerly c83831e235db4ce8bf0875a76255f1e1ac0271d9] [formerly b4e2558233d3a0601d24f0c4c46043cabdfaebf9 [formerly 9b4caf28f36d93ecd75216630e10faecdc741788]] [formerly b4e2558233d3a0601d24f0c4c46043cabdfaebf9 [formerly 9b4caf28f36d93ecd75216630e10faecdc741788] [formerly cf13496cb0435a42375a4731678ca5b097c974de [formerly 101934fa3afd6ecd190129b49226095449d1c004]]]] [formerly 24d211f776be965366e49787656b6fbe69f70b51] [formerly 0cb7b44f7d888175edf986164a6983b790acc68a [formerly 116495dad02d5eeefa7e151a76a634f4a5b483c8] [formerly ad09a8e1232388227f5bc788d50e50ee006e09d7 [formerly 30c4b050ed24d4af1cec3ec3af4a227c0e6f935f] [formerly ad09a8e1232388227f5bc788d50e50ee006e09d7 [formerly 30c4b050ed24d4af1cec3ec3af4a227c0e6f935f] [formerly ad09a8e1232388227f5bc788d50e50ee006e09d7 [formerly 30c4b050ed24d4af1cec3ec3af4a227c0e6f935f] [formerly 00d296c77ec34cbb4ab87d59d8c8dce22d201dd0 [formerly 9d25405b1f8533b3ebfbb550f9b5a03c982801ff]]]]]]]
     [formerly 24d211f776be965366e49787656b6fbe69f70b51]
    
    
    
    
    Former-commit-id: 94cd0f4f6b422b31bc460273787486c2256dc62c [formerly e37308933efaff659b5e20683bd41cd5114a05c3]
    Former-commit-id: cd976443572e46f5ad39d3e973c671e9dbfeef70 [formerly bce4d514fd1383c280a4cc41cb4a00313a9e2153] [formerly cc07cbde9d2ff752473c9ce8139f815fd6f619de [formerly 64e6cbbe6feff810da06d497a2aaedfdb37926f3]]
    Former-commit-id: 2793166acdfad4baeec5dc180b595a069941b139 [formerly ba2298e1175f222e21594998bf536eac455a4a0e]
    Former-commit-id: 1bfdc5f43d2f6d56b76d7d93d41e794529988064

--- plugins/inputs/dialtesting/dialer.go
+++ plugins/inputs/dialtesting/dialer.go
@@ -113,50 +113,12 @@ func (d *dialer) feedIo() error {
 	case dt.ClassHTTP:
 		return d.pointsFeed(urlStr)
 	case dt.ClassHeadless:
-		return d.linedataFeed(urlStr)
+		return d.linedataFeed(urlStr, `ms`)
 	//TODO other class
 	default:
 	}
 
 	return nil
-}
-
-func (d *dialer) linedataFeed(urlStr string) error {
-	data := d.task.GetLineData()
-	if len(data) == 0 {
-		l.Warnf("no any data for task %s", d.task.ID())
-		return nil
-	}
-
-	return LineDataFeed(data, inputName, datakit.Logging, &io.Option{
-		HTTPHost: urlStr,
-	})
-
-}
-
-func (d *dialer) pointsFeed(urlStr string) error {
-	// 获取此次任务执行的基本信息
-	tags := map[string]string{}
-	fields := map[string]interface{}{}
-	tags, fields = d.task.GetResults()
-
-	for k, v := range d.tags {
-		tags[k] = v
-	}
-
-	data, err := io.MakePoint(d.task.MetricName(), tags, fields, time.Now())
-	if err != nil {
-		l.Warnf("make metric failed: %s", err.Error)
-		return err
-	}
-
-	err = Feed(inputName, datakit.Logging, data, &io.Option{
-		HTTPHost: urlStr,
-	})
-
-	l.Debugf(`url:%s, tags: %+#v, fs: %+#v`, urlStr, tags, fields)
-
-	return err
 }
 
 func (d *dialer) doUpdateTask(t dt.Task) {
