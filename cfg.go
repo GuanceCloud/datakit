34cf18d7a91a73ed778f8f8a30fb8d83f5062634
tanbiao <tanbiao@jiagouyun.com>
2021-05-31 20:06:47 +0800

    update: 移除 datakit UUID 逻辑,直接使用 datakit hostname 作为 uuid(为兼容中心端现有计费逻辑)
    Former-commit-id: 6e18e140e8dd168160d701ab2f17aba45e06cba3 [formerly c40d47b024da92aa2fe08806a033fd66d73a460e [formerly 522c95a41c040ba8b7abf7f3c52389636fb4dbb6] [formerly 8f3bd1fceeb849608cbdd03f2b60e13e125c7147 [formerly 8d9436754b99536b808ecac52a21814c7125c1e7]]]
    Former-commit-id: 1c6b4be3bd25ac90bfc9fdfc36ab9a4db0debae0
    Former-commit-id: 4a9a68607df06af2d9076004c3b68b55cee6752a [formerly a010b765c51fc743b890f5409f303650a53ea6bc]
    Former-commit-id: 64a58a28292f2cf903c4ea219306147d51a719c4

--- cfg.go
+++ cfg.go
@@ -2,7 +2,6 @@ package datakit
 
 import (
 	"bytes"
-	"crypto/sha1"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,9 +11,7 @@ import (
 	"time"
 
 	bstoml "github.com/BurntSushi/toml"
-	"github.com/denisbrodbeck/machineid"
 
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils"
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/git"
 )
@@ -88,7 +85,7 @@ type Config struct {
 	GlobalTags map[string]string `toml:"global_tags"`
 
 	EnablePProf bool `toml:"enable_pprof,omitempty"`
-	ProtectMode bool `toml:"protect_mode,omitempty"`
+	ProtectMode bool `toml:"protect_mode"`
 
 	IntervalDeprecated string `toml:"interval,omitempty"`
 
@@ -115,7 +112,22 @@ func (c *Config) String() string {
 	return buf.String()
 }
 
-func (c *Config) LoadMainTOML(p, idFile string) error {
+func (c *Config) SetUUID() error {
+	if c.Hostname == "" {
+		hn, err := os.Hostname()
+		if err != nil {
+			l.Errorf("get hostname failed: %s", err.Error())
+			return err
+		}
+
+		c.UUID = hn
+	} else {
+		c.UUID = c.Hostname
+	}
+	return nil
+}
+
+func (c *Config) LoadMainTOML(p string) error {
 	cfgdata, err := ioutil.ReadFile(p)
 	if err != nil {
 		l.Errorf("read main cfg %s failed: %s", p, err.Error())
@@ -128,13 +140,8 @@ func (c *Config) LoadMainTOML(p, idFile string) error {
 		return err
 	}
 
-	// .id 文件此时应该已经存在。安装阶段已将 .id 文件创建好
-	dkid, err := LoadUUID(idFile)
-	if err != nil {
-		l.Errorf("load id failed: %s", err.Error())
-		return err
-	}
-	c.UUID = dkid
+	// 由于 datakit UUID 不再重要, 出错也不管了
+	_ = c.SetUUID()
 
 	return nil
 }
@@ -279,8 +286,10 @@ func (c *Config) ApplyMainConfig() error {
 		OutputFile = c.OutputFile
 	}
 
-	if err := c.setHostname(); err != nil {
-		return err
+	if c.Hostname == "" {
+		if err := c.setHostname(); err != nil {
+			return err
+		}
 	}
 
 	if err := c.setupDataway(); err != nil {
@@ -472,16 +481,4 @@ func ProtectedInterval(min, max, cur time.Duration) time.Duration {
 	}
 
 	return cur
-}
-
-func GenerateDatakitID() string {
-	id, err := machineid.ID()
-	if err != nil {
-		xid := cliutils.XID("dkid_")
-		l.Warnf("failed to get machineid: %s, use XID(%s) instead", err.Error(), xid)
-		return xid
-	}
-
-	l.Infof("machine ID: %s", id)
-	return "dkid_" + fmt.Sprintf("%x", sha1.Sum([]byte(id)))
 }
