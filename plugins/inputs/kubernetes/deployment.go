1600d94ac6c802097e47f8e277425eb93a76a72b
刘少波 <liushaobo@jiagouyun.com>
2021-06-01 22:12:14 +0800

    Liushaobo dev
     [formerly cbf8f9a4040b1df3723cfc5c98dd8d1a65a20ef1 [formerly 10b98323838e558469710e7971dd1865c1ea6c5a] [formerly 5b9d9d5170cbe5e7477b50c3b44c11214905d4c7 [formerly 3b0605feae3ef6fa2771626aa1ca30e952cc2cab]] [formerly 3b0cddf731deef22b0df84784fc7f55fdc088464] [formerly 0cc70bb313a370c28fced3df682ea012c67c5ec7 [formerly 7b272705731b9f13b0fb466fb795e92013bf43f0] [formerly 0cc70bb313a370c28fced3df682ea012c67c5ec7 [formerly 7b272705731b9f13b0fb466fb795e92013bf43f0] [formerly ad626312501ca7efa82102fc17855c87c771d9b0 [formerly c01ad5fe5d54250ce7e1e6582e28752ccc210577]]]] [formerly e13cf327762e792f5a7b98fa1da5d409cc053fa8] [formerly 519a1243100003d537c7b8eedb3bac1cd8666362 [formerly c00dbed06d020ed24c4bfa160d75dcef9e3d1db4] [formerly 612e669ad7aad36c5e477d7c5fff61a4211b9f5a [formerly aaa617e986cb774ab5e38a8a1fcce578e64fa0df] [formerly 612e669ad7aad36c5e477d7c5fff61a4211b9f5a [formerly aaa617e986cb774ab5e38a8a1fcce578e64fa0df] [formerly 612e669ad7aad36c5e477d7c5fff61a4211b9f5a [formerly aaa617e986cb774ab5e38a8a1fcce578e64fa0df] [formerly fa7f9ed87a0550545285607915dc58130f81c270 [formerly a642f0045ede831b63eb0595c42a890249b4f466]]]]]]]
     [formerly e13cf327762e792f5a7b98fa1da5d409cc053fa8]
    
    
    
    
    Former-commit-id: b63dc618da8000b332944d424ab1776e8f0f4fa5 [formerly 5ca373ad83965f1852fb0c34a781a54da0d23d55]
    Former-commit-id: a28f656e7a3b063ca64474255dee6ae5f16a9884 [formerly 0c9c78be4f6e90f21836df98aef3dd58d42e5879] [formerly ff5f95fe0dc6c6e744635801815f6c19afe667f7 [formerly 97c2fbaa7eb8eded1032084df8cd0746eb6bc339]]
    Former-commit-id: 6888d2eae383e5772aa8a60ef0a530f003898b4c [formerly 2c7db92252ff3b314bd5d4ffd68fd6359861dbf7]
    Former-commit-id: abf7301bfea2709d8dd384f2c6a5e526e839efab

---
+++ plugins/inputs/kubernetes/deployment.go
@@ -0,0 +1,95 @@
+package kubernetes
+
+import (
+	"time"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
+	v1 "k8s.io/api/apps/v1"
+)
+
+var deploymentMeasurement = "kube_deployment"
+
+type deployment struct {
+	name   string
+	tags   map[string]string
+	fields map[string]interface{}
+	ts     time.Time
+}
+
+func (m *deployment) LineProto() (*io.Point, error) {
+	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *deployment) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: deploymentMeasurement,
+		Desc: "kubernet deployment",
+		Tags: map[string]interface{}{
+			"deployment_name": &inputs.TagInfo{Desc: "deployment name"},
+			"namespace":       &inputs.TagInfo{Desc: "namespace"},
+			"selector_*":      &inputs.TagInfo{Desc: "lab"},
+		},
+		Fields: map[string]interface{}{
+			"replicas_available": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "Total number of available pods (ready for at least minReadySeconds) targeted by this deployment",
+			},
+			"replicas_unavailable": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "Total number of unavailable pods targeted by this deployment",
+			},
+			"created": &inputs.FieldInfo{
+				DataType: inputs.Int,
+				Type:     inputs.Gauge,
+				Unit:     inputs.UnknownUnit,
+				Desc:     "create time",
+			},
+		},
+	}
+}
+
+func (i *Input) collectDeployments() error {
+	list, err := i.client.getDeployments()
+	if err != nil {
+		return err
+	}
+	for _, d := range list.Items {
+		i.gatherDeployment(d)
+	}
+
+	return nil
+}
+
+func (i *Input) gatherDeployment(d v1.Deployment) {
+	fields := map[string]interface{}{
+		"replicas_available":   d.Status.AvailableReplicas,
+		"replicas_unavailable": d.Status.UnavailableReplicas,
+		"created":              d.GetCreationTimestamp().UnixNano(),
+	}
+	tags := map[string]string{
+		"deployment_name": d.Name,
+		"namespace":       d.Namespace,
+	}
+
+	for key, val := range d.Spec.Selector.MatchLabels {
+		tags["selector_"+key] = val
+	}
+
+	if d.GetCreationTimestamp().Second() != 0 {
+		fields["created"] = d.GetCreationTimestamp().UnixNano()
+	}
+
+	m := &deployment{
+		name:   deploymentMeasurement,
+		tags:   tags,
+		fields: fields,
+		ts:     time.Now(),
+	}
+
+	i.collectCache = append(i.collectCache, m)
+}
