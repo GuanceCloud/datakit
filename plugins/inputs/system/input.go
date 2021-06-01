d38d67fb77de2c87783a32e79990d85859856afd
tanbiao <tanbiao@jiagouyun.com>
2021-06-02 02:09:35 +0800

    update: 整体调整 datakit 模块结构，削减顶层模块 	renamed:    cfg.go -> config/cfg.go 	renamed:    cfg_test.go -> config/cfg_test.go 	renamed:    dataway.go -> io/dataway/dataway.go 	renamed:    dataway_test.go -> io/dataway/dataway_test.go 	renamed:    service.go -> service/service.go
    Former-commit-id: 6f0da6ab54fc187f0c2868e91c4b5bd6fd20b6d5 [formerly 2adfe9baeb5719654af397d9e1993c3678bd16a8 [formerly d0079158e28135d61f350f5acb34f746c3dd1864] [formerly 9ef406f595841f20c4cebf862c352698121f60d0 [formerly 5dee102210917999df657dd08b4297a177d0c552]]]
    Former-commit-id: 556c205bfd6b034fc5e4524e92d6801646dce5a6
    Former-commit-id: 34061e9ac04f84e41c970a3b120e68297fbb645e [formerly 6bf9350141b7d1a5805397475a4650c53347cd31]
    Former-commit-id: 0b852257b84400ee78bfae17e426c6de2bdb8b28

--- plugins/inputs/system/input.go
+++ plugins/inputs/system/input.go
@@ -11,6 +11,7 @@ import (
 	"github.com/shirou/gopsutil/load"
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
@@ -161,7 +162,7 @@ func (i *Input) Collect() error {
 func (i *Input) Run() {
 	l = logger.SLogger(inputName)
 	l.Infof("system input started")
-	i.Interval.Duration = datakit.ProtectedInterval(minInterval, maxInterval, i.Interval.Duration)
+	i.Interval.Duration = config.ProtectedInterval(minInterval, maxInterval, i.Interval.Duration)
 	tick := time.NewTicker(i.Interval.Duration)
 	defer tick.Stop()
 	for {
