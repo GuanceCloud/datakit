8c2a789b08c1d5f52b8b9c2c01184991d47ff8c5
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-02 18:03:26 +0800

    testing
    Former-commit-id: e1c973c4f1ee824fe134efabfc7a2c8c71687019 [formerly 428f83734e06701f032a125a280c3e8ea87b0520] [formerly 6d826e0f01b74e90de04cc9751976ee2b7540dc9 [formerly 08f32a093900b7ad0444e1238e64462a079ce260]]
    Former-commit-id: 6fb268bc0a5819c2b33cd79fb190fa1fb9d343f6 [formerly 5de0b29347af13dcf6f1b06304582badc58d99fa]
    Former-commit-id: 4af570055752150718b6d2fb0a1c54c39f400c72

--- plugins/inputs/container/input_test.go
+++ plugins/inputs/container/input_test.go
@@ -1,11 +1,7 @@
 package container
 
 import (
-	"fmt"
 	"testing"
-	"time"
-
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
 /*
@@ -13,44 +9,4 @@ import (
  */
 
 func TestMain(t *testing.T) {
-	var err error
-	var d = newInput()
-
-	d.client, err = d.newEnvClient()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	pts, err := d.gather(&gatherOption{IsObjectCategory: true})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, pt := range pts {
-		fmt.Println(pt.String())
-	}
-}
-
-func TestGatherLog(t *testing.T) {
-	io.SetTest()
-	var err error
-	var d = newInput()
-
-	d.client, err = d.newEnvClient()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err = d.initLoggingConf(); err != nil {
-		t.Fatal(err)
-	}
-
-	// 开始采集日志数据，非阻塞，会fork多个goroutine
-	d.gatherLog()
-
-	// 等待5秒用以采集
-	time.Sleep(time.Second * 5)
-
-	// 关闭所有采集资源
-	d.cancelTails()
 }
