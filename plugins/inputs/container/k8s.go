d1a18cdc1bc7ec01dd891259b900c8bfa03e2655
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-02 18:03:26 +0800

    testing
     [formerly 613b62f4f5835d912fe3db1f065fdf51c6502a87 [formerly 428f83734e06701f032a125a280c3e8ea87b0520] [formerly 613b62f4f5835d912fe3db1f065fdf51c6502a87 [formerly 428f83734e06701f032a125a280c3e8ea87b0520] [formerly 613b62f4f5835d912fe3db1f065fdf51c6502a87 [formerly 428f83734e06701f032a125a280c3e8ea87b0520] [formerly 6d826e0f01b74e90de04cc9751976ee2b7540dc9 [formerly bc011078884a225dbca4cada0b3d37e53fea24d4]]]]]
    
    
    
    Former-commit-id: 97b37d3e0da3174b68fde563313e9cb220d98e63 [formerly 9462a056fbc42994c350a017324f2349ece11973]
    Former-commit-id: 8c9bd93481046609d9dba480568df5c5b7f29fd3 [formerly f92b12fc9e60723b3f787c323b3c8ee907292c17] [formerly 6abb6929234d909d47cb430b46756a9d15b0e895 [formerly caf36487eaea027d6c871e1494a22ec0c647af85]]
    Former-commit-id: 754dab06a03f93e4404596a4a27f78350d2c6d9c [formerly 8c8773fb4d16c554188d0b7019f91aaae16ab7cc]
    Former-commit-id: 0a8c67414d5a045af5f78f0ccd9e419f550d605d

--- plugins/inputs/container/k8s.go
+++ plugins/inputs/container/k8s.go
@@ -5,15 +5,80 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 	"time"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 )
 
 const (
 	defaultKubernetesURL      = "http://%s:10255"
 	defaultServiceAccountPath = "/run/secrets/kubernetes.io/serviceaccount/token"
 )
+
+func buildNodeMetrics(summaryApi *SummaryMetrics) (*io.Point, error) {
+	tags := map[string]string{
+		"node_name": summaryApi.Node.NodeName,
+	}
+	fields := make(map[string]interface{})
+	fields["cpu_usage_nanocores"] = summaryApi.Node.CPU.UsageNanoCores
+	fields["cpu_usage_core_nanoseconds"] = summaryApi.Node.CPU.UsageCoreNanoSeconds
+	fields["memory_available_bytes"] = summaryApi.Node.Memory.AvailableBytes
+	fields["memory_usage_bytes"] = summaryApi.Node.Memory.UsageBytes
+	fields["memory_working_set_bytes"] = summaryApi.Node.Memory.WorkingSetBytes
+	fields["memory_rss_bytes"] = summaryApi.Node.Memory.RSSBytes
+	fields["memory_page_faults"] = summaryApi.Node.Memory.PageFaults
+	fields["memory_major_page_faults"] = summaryApi.Node.Memory.MajorPageFaults
+	fields["network_rx_bytes"] = summaryApi.Node.Network.RXBytes()
+	fields["network_rx_errors"] = summaryApi.Node.Network.RXErrors()
+	fields["network_tx_bytes"] = summaryApi.Node.Network.TXBytes()
+	fields["network_tx_errors"] = summaryApi.Node.Network.TXErrors()
+	fields["fs_available_bytes"] = summaryApi.Node.FileSystem.AvailableBytes
+	fields["fs_capacity_bytes"] = summaryApi.Node.FileSystem.CapacityBytes
+	fields["fs_used_bytes"] = summaryApi.Node.FileSystem.UsedBytes
+	fields["runtime_image_fs_available_bytes"] = summaryApi.Node.Runtime.ImageFileSystem.AvailableBytes
+	fields["runtime_image_fs_capacity_bytes"] = summaryApi.Node.Runtime.ImageFileSystem.CapacityBytes
+	fields["runtime_image_fs_used_bytes"] = summaryApi.Node.Runtime.ImageFileSystem.UsedBytes
+
+	return io.MakePoint("kubernetes", tags, fields, time.Now())
+}
+
+func buildPodMetrics(summaryApi *SummaryMetrics) ([]*io.Point, error) {
+	var pts []*io.Point
+
+	for _, pod := range summaryApi.Pods {
+		if len(pod.Containers) == 0 {
+			continue
+		}
+		tags := map[string]string{
+			"node_name": summaryApi.Node.NodeName,
+			"pod_name":  pod.PodRef.Name,
+			"namespace": pod.PodRef.Namespace,
+			"uid":       pod.PodRef.UID,
+		}
+
+		fields := make(map[string]interface{})
+		fields["cpu_usage_nanocores"] = pod.CPU.UsageNanoCores
+		fields["cpu_usage_core_nanoseconds"] = pod.CPU.UsageCoreNanoSeconds
+		fields["memory_usage_bytes"] = pod.Memory.UsageBytes
+		fields["memory_working_set_bytes"] = pod.Memory.WorkingSetBytes
+		fields["memory_rss_bytes"] = pod.Memory.RSSBytes
+		fields["memory_page_faults"] = pod.Memory.PageFaults
+		fields["memory_major_page_faults"] = pod.Memory.MajorPageFaults
+		fields["network_rx_bytes"] = pod.Network.RXBytes()
+		fields["network_rx_errors"] = pod.Network.RXErrors()
+		fields["network_tx_bytes"] = pod.Network.TXBytes()
+		fields["network_tx_errors"] = pod.Network.TXErrors()
+
+		pt, err := io.MakePoint("kubernetes_pod", tags, fields, time.Now())
+		if err != nil {
+			return nil, err
+		}
+		pts = append(pts, pt)
+	}
+
+	return pts, nil
+}
 
 // Kubernetes represents the config object for the plugin
 type Kubernetes struct {
@@ -43,24 +108,29 @@ func (k *Kubernetes) Init() error {
 	return nil
 }
 
-func buildURL(endpoint string, base string) (*url.URL, error) {
-	u := fmt.Sprintf(endpoint, base)
-	addr, err := url.Parse(u)
+func (k *Kubernetes) GatherNodeMetrics() (*io.Point, error) {
+	summaryApi, err := k.GetSummaryMetrics()
 	if err != nil {
-		return nil, fmt.Errorf("Unable to parse address '%s': %s", u, err)
+		return nil, err
 	}
-	return addr, nil
+	return buildNodeMetrics(summaryApi)
+}
+
+func (k *Kubernetes) GatherPodMetrics() ([]*io.Point, error) {
+	summaryApi, err := k.GetSummaryMetrics()
+	if err != nil {
+		return nil, err
+	}
+	return buildPodMetrics(summaryApi)
 }
 
 func (k *Kubernetes) GatherPodInfo(containerID string) (map[string]string, error) {
-	var podApi Pods
-	err := k.LoadJson(fmt.Sprintf("%s/pods", k.URL), &podApi)
+	podApi, err := k.GetPods()
 	if err != nil {
 		return nil, err
 	}
 
 	containerID = fmt.Sprintf("docker://%s", containerID)
-
 	var m = make(map[string]string)
 
 	for _, podMetadata := range podApi.Items {
@@ -80,8 +150,7 @@ func (k *Kubernetes) GatherPodInfo(containerID string) (map[string]string, error
 }
 
 func (k *Kubernetes) GatherPodUID(containerID string) (string, error) {
-	var podApi Pods
-	err := k.LoadJson(fmt.Sprintf("%s/pods", k.URL), &podApi)
+	podApi, err := k.GetPods()
 	if err != nil {
 		return "", err
 	}
@@ -103,13 +172,12 @@ func (k *Kubernetes) GatherPodUID(containerID string) (string, error) {
 }
 
 func (k *Kubernetes) GatherWorkName(uid string) (string, error) {
-	var nodeApi Node
-	err := k.LoadJson(fmt.Sprintf("%s/stats/summary", k.URL), &nodeApi)
+	statsSummaryApi, err := k.GetSummaryMetrics()
 	if err != nil {
 		return "", err
 	}
 
-	for _, podMetadata := range nodeApi.Pods {
+	for _, podMetadata := range statsSummaryApi.Pods {
 		if len(podMetadata.Containers) == 0 {
 			continue
 		}
@@ -120,6 +188,28 @@ func (k *Kubernetes) GatherWorkName(uid string) (string, error) {
 	}
 
 	return "", nil
+}
+
+func (k *Kubernetes) GatherNodeName(stats *SummaryMetrics) string {
+	return stats.Node.NodeName
+}
+
+func (k *Kubernetes) GetPods() (*Pods, error) {
+	var podApi Pods
+	err := k.LoadJson(fmt.Sprintf("%s/pods", k.URL), &podApi)
+	if err != nil {
+		return nil, err
+	}
+	return &podApi, nil
+}
+
+func (k *Kubernetes) GetSummaryMetrics() (*SummaryMetrics, error) {
+	var summaryApi SummaryMetrics
+	err := k.LoadJson(fmt.Sprintf("%s/stats/summary", k.URL), &summaryApi)
+	if err != nil {
+		return nil, err
+	}
+	return &summaryApi, err
 }
 
 func (k *Kubernetes) LoadJson(url string, v interface{}) error {
@@ -161,32 +251,138 @@ func (k *Kubernetes) LoadJson(url string, v interface{}) error {
 }
 
 type Pods struct {
-	Kind       string `json:"kind"`
-	ApiVersion string `json:"apiVersion"`
-	Items      []struct {
-		Metadata struct {
-			Name      string            `json:"name"`
-			Namespace string            `json:"namespace"`
-			UID       string            `json:"uid"`
-			Labels    map[string]string `json:"labels"`
-		} `json:"metadata"`
-
-		Status struct {
-			ContainerStatuses []struct {
-				ContainerID string `json:"containerID"`
-			} `json:"containerStatuses"`
-		} `json:"status"`
-	} `json:"items"`
+	Kind       string    `json:"kind"`
+	ApiVersion string    `json:"apiVersion"`
+	Items      []PodItem `json:"items"`
 }
 
-type Node struct {
-	Pods []struct {
-		PodRef struct {
-			UID string `json:"uid"`
-		} `json:"podRef"`
+type PodItem struct {
+	Metadata PodItemMetadata `json:"metadata"`
+	Status   PodItemStatus   `json:"status"`
+}
 
-		Containers []struct {
-			Name string `json:"name"`
-		} `json:"containers"`
-	} `json:"pods"`
+type PodItemMetadata struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	UID       string `json:"uid"`
+}
+
+type PodItemStatus struct {
+	ContainerStatuses []struct {
+		ContainerID  string `json:"containerID"`
+		RestartCount int64  `json:"restartCount"`
+	} `json:"containerStatuses"`
+}
+
+type SummaryMetrics struct {
+	Node NodeMetrics  `json:"node"`
+	Pods []PodMetrics `json:"pods"`
+}
+
+type NodeMetrics struct {
+	NodeName         string             `json:"nodeName"`
+	SystemContainers []ContainerMetrics `json:"systemContainers"`
+	CPU              CPUMetrics         `json:"cpu"`
+	Memory           MemoryMetrics      `json:"memory"`
+	Network          NetworkMetrics     `json:"network"`
+	FileSystem       FileSystemMetrics  `json:"fs"`
+	Runtime          RuntimeMetrics     `json:"runtime"`
+}
+
+type ContainerMetrics struct {
+	Name   string            `json:"name"`
+	CPU    CPUMetrics        `json:"cpu"`
+	Memory MemoryMetrics     `json:"memory"`
+	RootFS FileSystemMetrics `json:"rootfs"`
+	LogsFS FileSystemMetrics `json:"logs"`
+}
+
+type RuntimeMetrics struct {
+	ImageFileSystem FileSystemMetrics `json:"imageFs"`
+}
+
+type CPUMetrics struct {
+	Time                 time.Time `json:"time"`
+	UsageNanoCores       int64     `json:"usageNanoCores"`
+	UsageCoreNanoSeconds int64     `json:"usageCoreNanoSeconds"`
+}
+
+type PodMetrics struct {
+	PodRef     PodReference       `json:"podRef"`
+	StartTime  *time.Time         `json:"startTime"`
+	Containers []ContainerMetrics `json:"containers"`
+	CPU        CPUMetrics         `json:"cpu"`
+	Memory     MemoryMetrics      `json:"memory"`
+	Network    NetworkMetrics     `json:"network"`
+}
+
+type PodReference struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	UID       string `json:"uid"`
+}
+
+type MemoryMetrics struct {
+	Time            time.Time `json:"time"`
+	AvailableBytes  int64     `json:"availableBytes"`
+	UsageBytes      int64     `json:"usageBytes"`
+	WorkingSetBytes int64     `json:"workingSetBytes"`
+	RSSBytes        int64     `json:"rssBytes"`
+	PageFaults      int64     `json:"pageFaults"`
+	MajorPageFaults int64     `json:"majorPageFaults"`
+}
+
+type FileSystemMetrics struct {
+	AvailableBytes int64 `json:"availableBytes"`
+	CapacityBytes  int64 `json:"capacityBytes"`
+	UsedBytes      int64 `json:"usedBytes"`
+}
+
+type NetworkMetrics struct {
+	Time       time.Time `json:"time"`
+	Interfaces []struct {
+		RXBytes  int64 `json:"rxBytes"`
+		RXErrors int64 `json:"rxErrors"`
+		TXBytes  int64 `json:"txBytes"`
+		TXErrors int64 `json:"txErrors"`
+	} `json:"interfaces"`
+}
+
+func (n NetworkMetrics) RXBytes() int64 {
+	var sum int64
+	for _, i := range n.Interfaces {
+		sum += i.RXBytes
+	}
+	return sum
+}
+
+func (n NetworkMetrics) RXErrors() int64 {
+	var sum int64
+	for _, i := range n.Interfaces {
+		sum += i.RXErrors
+	}
+	return sum
+}
+
+func (n NetworkMetrics) TXBytes() int64 {
+	var sum int64
+	for _, i := range n.Interfaces {
+		sum += i.TXBytes
+	}
+	return sum
+}
+
+func (n NetworkMetrics) TXErrors() int64 {
+	var sum int64
+	for _, i := range n.Interfaces {
+		sum += i.TXErrors
+	}
+	return sum
+}
+
+type VolumeMetrics struct {
+	Name           string `json:"name"`
+	AvailableBytes int64  `json:"availableBytes"`
+	CapacityBytes  int64  `json:"capacityBytes"`
+	UsedBytes      int64  `json:"usedBytes"`
 }
