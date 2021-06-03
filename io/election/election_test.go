874730388084d543f8b329fa60c146471e0d5408
tanbiao <tanbiao@jiagouyun.com>
2021-06-03 16:22:36 +0800

    update: refact election
     [formerly 0560d4aa6932330fb9302da89e83dcc5647bc2c8 [formerly 57c46d367bb6c88e3d040f6cab24b190dcb82447] [formerly 814e42caab2a09d9f1e171886b6be5399d5246de] [formerly 67dc4e937a4589e7fb7c5367e473119bde62df2b [formerly 04f6880a255582959168c32ce2f693f2b9a06ea5] [formerly 5ba589cf36878729cccda3fffed4a1aa6e279f09 [formerly da2098b5ad281c4c81b6c250c511609994afc095]]]]
     [formerly 814e42caab2a09d9f1e171886b6be5399d5246de]
    
    
    
    
    Former-commit-id: 418049825b6107f13ecdcab22b005c83f7d13bc7

--- io/election/election_test.go
+++ io/election/election_test.go
@@ -2,72 +2,156 @@ package election
 
 import (
 	"fmt"
-	"net/http"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	tu "gitlab.jiagouyun.com/cloudcare-tools/cliutils/testutil"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io/dataway"
 )
 
 func TestElection(t *testing.T) {
-	const (
-		electionURL  = "http://127.0.0.1:36080/election?token=tkn_test&id=dkid_test"
-		heartbeatURL = "http://127.0.0.1:36080/election/heartbeat?token=tkn_test&id=dkid_test"
 
-		successBody = `{"content":{"status":"success"}}`
-		failBody    = `{"content":{"status":"fail"}}`
-	)
+	ok := []byte(`{"content": {"status": "success"}}`)
+	fail := []byte(`{"content": {"status": "fail"}}`)
 
-	go func() {
-		electionCount := 0
-		http.HandleFunc("/election", func(resp http.ResponseWriter, req *http.Request) {
-			// 只有当调用此 API 为偶数时，才 pass
-			electionCount++
-			if electionCount&1 == 0 {
-				resp.Write([]byte(successBody))
-			} else {
-				resp.Write([]byte(failBody))
-			}
-			// resp.WriteHeader(http.StatusOK)
-		})
+	bind := ":12345"
+	cases := []struct {
+		dataways   []string
+		candidates map[int]*candidate
+	}{
+		{
+			dataways: []string{fmt.Sprintf("http://%s?token=tkn_123", bind)},
+			candidates: map[int]*candidate{
+				1: &candidate{status: statusFail},
+				2: &candidate{status: statusFail},
+				3: &candidate{status: statusFail},
+				4: &candidate{status: statusFail},
+				5: &candidate{status: statusFail},
+				//&candidate{status: statusFail, ttl: 5 * time.Second},
+			},
+		},
+	}
 
-		heartbeatCount := 0
-		http.HandleFunc("/election/heartbeat", func(resp http.ResponseWriter, req *http.Request) {
-			// 5 次心跳，自动结束
-			heartbeatCount++
-			if heartbeatCount%5 == 0 {
-				resp.Write([]byte(failBody))
-			} else {
-				resp.Write([]byte(successBody))
-			}
-			// resp.WriteHeader(http.StatusOK)
-		})
+	elected := []string{}
+	var heartbeat time.Time
 
-		fmt.Println("start http server")
-		http.ListenAndServe("0.0.0.0:36080", nil)
-	}()
+	setElected := func(id string) bool {
 
-	time.Sleep(time.Second * 2)
-
-	csm := NewConsensusModule(electionURL, heartbeatURL)
-	fmt.Printf("start, election stauts: %s\n", csm.CurrentStats())
-
-	go csm.StartElection()
-
-	go func() {
-		index := 0
-		for {
-			select {
-			case <-datakit.Exit.Wait():
-				return
-			case <-time.After(time.Second * 1):
-				index++
-				fmt.Printf("index: %d, election_status: %s\n", index, csm.CurrentStats())
+		if heartbeat.UnixNano() > 0 {
+			if time.Since(heartbeat) > 2*electionInterval {
+				l.Debugf("%s clear elected candidates", id)
+				elected = elected[:0] //clear
 			}
 		}
-	}()
 
-	time.Sleep(time.Second * 30)
-	datakit.Exit.Close()
-	time.Sleep(time.Second * 1)
+		if len(elected) == 0 {
+			elected = append(elected, id)
+			heartbeat = time.Now()
+			return true
+		}
+		return false
+	}
+
+	setHeartbeat := func(id string) bool {
+
+		if len(elected) == 1 {
+			if elected[0] == id {
+				return false
+			} else {
+				heartbeat = time.Now()
+				return true
+			}
+		} else {
+			l.Errorf("should not been here")
+			return false
+		}
+	}
+
+	opt := &tu.HTTPServerOptions{
+		Bind: bind,
+		Exit: make(chan interface{}),
+		Routes: map[string]func(*gin.Context){
+			datakit.Election: func(c *gin.Context) {
+				tkn := c.Query("token")
+
+				if len(tkn) == 0 {
+					c.Data(403, "", []byte(`token missing`))
+					return
+				}
+
+				id := c.Query("id")
+				if len(id) == 0 {
+					c.Data(403, "", []byte(`id missing`))
+					return
+				}
+
+				//l.Debugf("%s electing...", id)
+
+				if setElected(id) {
+					l.Debugf("%s elected", id)
+					c.Writer.Write(ok)
+				} else {
+					c.Writer.Write(fail)
+				}
+			},
+
+			datakit.ElectionHeartbeat: func(c *gin.Context) {
+				tkn := c.Query("token")
+
+				if len(tkn) == 0 {
+					c.Data(403, "", []byte(`token missing`))
+					return
+				}
+
+				id := c.Query("id")
+				if len(id) == 0 {
+					c.Data(403, "", []byte(`id missing`))
+					return
+				}
+
+				if setHeartbeat(id) {
+					l.Debugf("candidate %s heartbeat ok", id)
+					c.Writer.Write(ok)
+				} else {
+					l.Debugf("candidate %s heartbeat failed", id)
+					c.Writer.Write(fail)
+				}
+			},
+		},
+	}
+
+	wg := sync.WaitGroup{}
+	go func() {
+		defer wg.Done()
+		tu.NewHTTPServer(t, opt)
+	}()
+	time.Sleep(time.Second)
+
+	electionInterval = 30 * time.Millisecond // reduce election interval
+
+	for _, tc := range cases {
+		dw := &dataway.DataWayCfg{URLs: tc.dataways}
+		dw.Apply()
+
+		for n := 0; n < 10; n++ {
+			l.Debug("-------------------------")
+			for k, c := range tc.candidates {
+				id := fmt.Sprintf("id-%d", k)
+				c.id = id
+				c.dw = dw
+				c.runOnce()
+				time.Sleep(time.Millisecond * 500)
+			}
+		}
+
+		for _, c := range tc.candidates {
+			t.Logf("%+#v", c)
+		}
+	}
+
+	close(opt.Exit)
+	wg.Wait()
 }
