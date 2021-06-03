885cfd48b171c899d27bba67f3af12ae3c77e42c
刘少波 <liushaobo@jiagouyun.com>
2021-06-03 10:22:40 +0800

    Liushaobo dev
     [formerly 39b21bfbe76251e95fffc4b2f7ba4803396f05bd [formerly 51d9daf90106404684a219bcae18e5b7abd325fc] [formerly 39b21bfbe76251e95fffc4b2f7ba4803396f05bd [formerly 51d9daf90106404684a219bcae18e5b7abd325fc] [formerly 39b21bfbe76251e95fffc4b2f7ba4803396f05bd [formerly 51d9daf90106404684a219bcae18e5b7abd325fc] [formerly 71e40b92904f525ec3e34655a77bb06150beab31 [formerly fccc079e9b5eeb09d56392c06bf67866387fbd6e]]]]]
    
    
    
    Former-commit-id: e374cd43f0283c10e4621a6794d80c18b13b77a7 [formerly 47b00596420b465a567f7b51da98e79a3718681a]
    Former-commit-id: 00830aa4d4d68540cf13c01427c49eb7191a4448 [formerly 252e6098fbe124b6c9cad8421eeb19891d0ed8c3] [formerly 853a1da0b6d4b66ad3258efbb8ec6f10b8c963c3 [formerly 1c1f9a248275a33d3377f26fcafa0fd4df058247]]
    Former-commit-id: ccc580b17ea4eaf0d99342f4468864fc6790b59f [formerly 66be8c073d56a23d2a014c7c7cd158d8e8e99bec]
    Former-commit-id: e6435b05a8b91ab968fd1d40c545f34fcaebd152

--- plugins/inputs/kubernetes/collect_test.go
+++ plugins/inputs/kubernetes/collect_test.go
@@ -2,6 +2,7 @@ package kubernetes
 
 import (
 	"github.com/influxdata/telegraf/plugins/common/tls"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 	"testing"
 )
 
@@ -17,6 +18,7 @@ func TestCollect(t *testing.T) {
 			input: &Input{
 				Tags:           make(map[string]string),
 				KubeConfigPath: "/Users/liushaobo/.kube/config",
+				collectCache:   make(map[string][]inputs.Measurement),
 			},
 			fail: false,
 		},
@@ -29,41 +31,58 @@ func TestCollect(t *testing.T) {
 				ClientConfig: tls.ClientConfig{
 					TLSCA: "/Users/liushaobo/go/src/gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/kubernetes/pki/ca_crt.pem",
 				},
+				collectCache: make(map[string][]inputs.Measurement),
 			},
 			fail: false,
+		},
+		{
+			desc: "auth by token",
+			input: &Input{
+				Tags:        make(map[string]string),
+				URL:         "https://127.0.0.0:6443",
+				BearerToken: "/Users/liushaobo/go/src/gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/kubernetes/pki/token",
+				ClientConfig: tls.ClientConfig{
+					TLSCA: "/Users/liushaobo/go/src/gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/kubernetes/pki/ca_crt.pem",
+				},
+				collectCache: make(map[string][]inputs.Measurement),
+			},
+			fail: true,
 		},
 	}
 
 	for _, tc := range testCase {
-		var testFuncs = map[string]func() error{
-			// "daemonsets":             tc.input.collectDaemonSets,
-			// "deployments":            tc.input.collectDeployments,
-			// "endpoints":              tc.input.collectEndpoints,
-			"ingress": tc.input.collectIngress,
-			// "services":               tc.input.collectServices,
-			// "statefulsets":           tc.input.collectStatefulSets,
-			// "persistentvolumes":      tc.input.collectPersistentVolumes,
-			// "persistentvolumeclaims": tc.input.collectPersistentVolumeClaims,
-		}
-
 		err := tc.input.initCfg()
 		if err != nil {
 			t.Log("init config err ---->", err)
 			return
 		}
 
-		for collector, fn := range testFuncs {
-			err := fn()
+		collectors := map[string]func(collector string) error{
+			"daemonsets":             tc.input.collectDaemonSets,
+			"deployments":            tc.input.collectDeployments,
+			"endpoints":              tc.input.collectEndpoints,
+			"ingress":                tc.input.collectIngress,
+			"services":               tc.input.collectServices,
+			"statefulsets":           tc.input.collectStatefulSets,
+			"persistentvolumes":      tc.input.collectPersistentVolumes,
+			"persistentvolumeclaims": tc.input.collectPersistentVolumeClaims,
+		}
+
+		for collector, fn := range collectors {
+			err := fn(collector)
 			if err != nil {
 				l.Errorf("%s exec error %v", collector, err)
 			}
 
-			for _, m := range tc.input.collectCache {
-				point, err := m.LineProto()
-				if err != nil {
-					t.Log("error ->", err)
-				} else {
-					t.Log("point ->", point.String())
+			for k, ms := range tc.input.collectCache {
+				t.Log("collect resource type", k)
+				for _, m := range ms {
+					point, err := m.LineProto()
+					if err != nil {
+						t.Log("error ->", err)
+					} else {
+						t.Log("point ->", point.String())
+					}
 				}
 			}
 		}
