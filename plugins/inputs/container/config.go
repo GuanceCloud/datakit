6f3d0e38c1a8e1aa26d7dc9de06232a491ff2244
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-03 18:15:18 +0800

    container容器done(#125)
     [formerly 83fc2be976827b829573291b7901833d0438dbaa] [formerly 5eaf4529a8cf3a031a52af58b6e4690d55083f33 [formerly bd454a36b2a6155e23e6e4faad5a15923ab00123] [formerly 62c276c7ec7413934837ba23588fb0c8a0f3e449 [formerly 206244dbf08bd58f2131843748d15d905ade7ae1]]] [formerly 1d228e0f4a0d22513585b4c39a2edcae5b825577 [formerly 558ddf773aae15ce18372437a492ced2aae2de23] [formerly 83fc2be976827b829573291b7901833d0438dbaa] [formerly 5eaf4529a8cf3a031a52af58b6e4690d55083f33 [formerly bd454a36b2a6155e23e6e4faad5a15923ab00123] [formerly 62c276c7ec7413934837ba23588fb0c8a0f3e449 [formerly 206244dbf08bd58f2131843748d15d905ade7ae1]]] [formerly 6ac2aadc5113bdc012fb0eaea7602f6528c5fcb3 [formerly ccd85db4ca06e7b2299483d54495c2c4a674361e] [formerly fa8b2dc9f5f018617b657c71888c5d0589f0c528] [formerly 43e90cd225408378e0875ec195c546fb5bb9484c [formerly d2b72dc69412ec70f8656b0c6d4ac2b95174e38f] [formerly 78fbae3def6188a9972846530c43b28bcc72d0f9 [formerly 8615e962c56560c40ca46e34e474f90411ff8c55]]]]]
     [formerly 43e90cd225408378e0875ec195c546fb5bb9484c [formerly d2b72dc69412ec70f8656b0c6d4ac2b95174e38f]]
    
    
    
     [formerly 43735fd58bfa9e0f3091fb23874a6b32afc57249 [formerly a86fa48518fd175584ea20b62e841da0e362792f]]
    
    
    
    Former-commit-id: c8d072e7e77674f1eb90808967cd48e145e39a0f

---
+++ plugins/inputs/container/config.go
@@ -0,0 +1,169 @@
+package container
+
+import (
+	"net"
+	"net/url"
+	"time"
+
+	"github.com/docker/docker/api/types"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
+)
+
+// ## To use environment variables (ie, docker-machine), set endpoint = "ENV"
+
+const (
+	inputName = "container"
+
+	sampleCfg = `
+[inputs.container]
+  endpoint = "unix:///var/run/docker.sock"
+  
+  enable_metric = false  
+  enable_object = true   
+  enable_logging = true  
+  
+  metric_interval = "10s"
+  
+  ## TLS Config
+  # tls_ca = "/path/to/ca.pem"
+  # tls_cert = "/path/to/cert.pem"
+  # tls_key = "/path/to/key.pem"
+  ## Use TLS but skip chain & host verification
+  # insecure_skip_verify = false
+  
+  
+  [inputs.container.kubelet]
+    kubelet_url = "http://127.0.0.1:10255"
+
+    ## Use bearer token for authorization. ('bearer_token' takes priority)
+    ## If both of these are empty, we'll use the default serviceaccount:
+    ## at: /run/secrets/kubernetes.io/serviceaccount/token
+    # bearer_token = "/path/to/bearer/token"
+    ## OR
+    # bearer_token_string = "abc_123"
+
+    ## Optional TLS Config
+    # tls_ca = /path/to/ca.pem
+    # tls_cert = /path/to/cert.pem
+    # tls_key = /path/to/key.pem
+    ## Use TLS but skip chain & host verification
+    # insecure_skip_verify = false
+  
+  #[[inputs.container.logfilter]]
+  #  filter_message = [
+  #    '''<this-is-message-regexp''',
+  #    '''<this-is-another-message-regexp''',
+  #  ]
+  #  source = "<your-source-name>"
+  #  service = "<your-service-name>"
+  #  pipeline = "<pipeline.p>"
+  
+  [inputs.container.tags]
+    # some_tag = "some_value"
+    # more_tag = "some_other_value"
+`
+	// docker endpoint
+	dockerEndpoint = "unix:///var/run/docker.sock"
+
+	// docker sock 文件路径，用以判断主机是否已安装 docker 服务
+	dockerEndpointPath = "/var/run/docker.sock"
+
+	// Docker API 超时时间
+	apiTimeoutDuration = time.Second * 5
+
+	// 最小指标采集间隔
+	minMetricDuration = time.Second * 10
+
+	// 最大指标采集间隔
+	maxMetricDuration = time.Second * 60
+
+	// 对象采集间隔
+	objectDuration = time.Minute * 5
+
+	// 定时发现新日志源
+	loggingHitDuration = time.Second * 5
+)
+
+var (
+	l = logger.DefaultSLogger(inputName)
+
+	// 容器日志的连接参数
+	containerLogsOptions = types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+		Follow:     true,
+		Tail:       "0", // 默认关闭FromBeginning，避免数据量巨大。开启为 'all'
+	}
+
+	// 容器获取列表的连接参数
+	containerListOptions = types.ContainerListOptions{}
+)
+
+func (this *Input) loadCfg() (err error) {
+	// tlsConfig 可以为空指针，即没有配置tls
+	tlsConfig, _err := this.ClientConfig.TLSConfig()
+	if _err != nil {
+		return _err
+	}
+
+	this.client, err = this.newClient(this.Endpoint, tlsConfig)
+	if err != nil {
+		return
+	}
+
+	if err = this.initLoggingConf(); err != nil {
+		return
+	}
+
+	if this.Kubernetes != nil {
+		l.Debugf("use kubelet_url %s", this.Kubernetes.URL)
+
+		u, err := url.Parse(this.Kubernetes.URL)
+		if err != nil {
+			return err
+		}
+
+		func() {
+			// kubelet API 没有提供 ping 功能
+			// 此处手动检查该端口是否可以连接
+			if err = rawConnect(u.Hostname(), u.Port()); err != nil {
+				l.Errorf("kubelet_url connecting error(not collect kubelet): %s", err)
+				// 如果检测到该端口尚未监听，或无法连接，则将 this.Kubernetes 置为 空指针
+				// 后续将不再采用 kubelet 相关数据
+				this.Kubernetes = nil
+				return
+			}
+
+			if err := this.Kubernetes.Init(); err != nil {
+				l.Debugf("read kubelet token error (use empty tokne): %s", err)
+				// use empty token
+				this.Kubernetes.BearerTokenString = ""
+			}
+		}()
+	}
+
+	return
+}
+
+func (this *Input) initLoggingConf() error {
+	for _, lf := range this.LogFilters {
+		if err := lf.Init(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func rawConnect(host string, port string) error {
+	timeout := time.Second
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+	if err != nil {
+		return err
+	}
+	if conn != nil {
+		defer conn.Close()
+		return nil
+	}
+	return nil
+}
