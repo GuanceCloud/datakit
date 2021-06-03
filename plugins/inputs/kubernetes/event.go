88dd5317977b5c52025685fd628c451ba8e02e53
刘少波 <liushaobo@jiagouyun.com>
2021-06-03 10:22:40 +0800

    Liushaobo dev
    Former-commit-id: aa91c380873de99dca133b114730f522038afb83
    Former-commit-id: 8cd631a6afc97fc2440f59bdd0f61507aca911a6 [formerly 3608f3e80e4548408c322c515e580a026f4a92ff]
    Former-commit-id: 422897ca0d50a7f93c13d38bc7e1e469519acc11

--- plugins/inputs/kubernetes/event.go
+++ plugins/inputs/kubernetes/event.go
@@ -1,34 +1,34 @@
 package kubernetes
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
-	// "k8s.io/client-go/pkg/api/v1"
-)
+// 	corev1 "k8s.io/api/core/v1"
+// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+// 	"k8s.io/apimachinery/pkg/watch"
+// 	// "k8s.io/client-go/pkg/api/v1"
+// )
 
-func (i *Input) collectEvents() error {
-	watchers, err := i.client.CoreV1().Services(i.client.namespace).Watch(metav1.ListOptions{})
-	if err != nil {
-		return err
-	}
+// func (i *Input) collectEvents() error {
+// 	watchers, err := i.client.CoreV1().Services(i.client.namespace).Watch(metav1.ListOptions{})
+// 	if err != nil {
+// 		return err
+// 	}
 
-	for event := range watchers.ResultChan() {
-		svc := event.Object.(*corev1.Service)
+// 	for event := range watchers.ResultChan() {
+// 		svc := event.Object.(*corev1.Service)
 
-		switch event.Type {
-		case watch.Added:
-			fmt.Printf("Service %s/%s added\n", svc.ObjectMeta.Namespace, svc.ObjectMeta.Name)
-		case watch.Modified:
-			fmt.Printf("Service %s/%s modified\n", svc.ObjectMeta.Namespace, svc.ObjectMeta.Name)
-		case watch.Deleted:
-			fmt.Printf("Service %s/%s deleted\n", svc.ObjectMeta.Namespace, svc.ObjectMeta.Name)
-		default:
-			fmt.Println("service add =======>")
-		}
-	}
+// 		switch event.Type {
+// 		case watch.Added:
+// 			fmt.Printf("Service %s/%s added\n", svc.ObjectMeta.Namespace, svc.ObjectMeta.Name)
+// 		case watch.Modified:
+// 			fmt.Printf("Service %s/%s modified\n", svc.ObjectMeta.Namespace, svc.ObjectMeta.Name)
+// 		case watch.Deleted:
+// 			fmt.Printf("Service %s/%s deleted\n", svc.ObjectMeta.Namespace, svc.ObjectMeta.Name)
+// 		default:
+// 			fmt.Println("service add =======>")
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
