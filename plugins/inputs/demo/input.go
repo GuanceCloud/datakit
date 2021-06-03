e3619004ffb4dd643ab86173603370364521fe0f
tanbiao <tanbiao@jiagouyun.com>
2021-06-03 16:22:36 +0800

    update: refact election
    Former-commit-id: bbb94da7d5df17337fb541b6d567721b6580bc0b [formerly 5ba589cf36878729cccda3fffed4a1aa6e279f09] [formerly af9a85cdffb02b1c0841a2acbb55754ba3b18641 [formerly 0a78d8a2e3fd251c7fcd4bcf33902bcd82a61fb0]]
    Former-commit-id: 4bbb36d5c6be9a1994e755c2d5005b9c09a38760 [formerly d3ac3676e68f5f45418df04b136c02a0b3022925]
    Former-commit-id: 6762c990ddac90020474a75cf8a6b4983f8ec478

--- plugins/inputs/demo/input.go
+++ plugins/inputs/demo/input.go
@@ -5,7 +5,6 @@ import (
 
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/election"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
@@ -110,10 +109,6 @@ func (i *Input) Run() {
 
 		select {
 		case <-tick.C:
-			if !election.CurrentStats().IsLeader() {
-				l.Debugf("demo input not leader, skip doing gather")
-				continue
-			}
 
 			l.Debugf("demo input gathering...")
 			start := time.Now()
