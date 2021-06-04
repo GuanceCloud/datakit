cf46f1f8942c89c754776bb40239ca7a6dc5e3f2
tanbiao <tanbiao@jiagouyun.com>
2021-06-04 18:29:46 +0800

    update: add debug info on dataway sending
    Former-commit-id: 9a5f4553c57979211cb8db95e99c844751ce3a77 [formerly a2b70a2950cf2c42b6f382869f32c94b1342ae9c [formerly e85dd820e9fb68c16ba2eb6a50ad7bf0ccf50d88] [formerly 4d95009340f8522e083e2f8a6cce8f486f829cf4 [formerly ce721b74275b7a67479a5b2061373d3d2083f888]]]
    Former-commit-id: 96710e388fee01370b515d956bf8980afcbe0ba0
    Former-commit-id: 34bd16962f3fdfb9691b5161aa8b47abe57b3c7c [formerly d96657229af7689e077a9cce79a1f9ae15c5c172]
    Former-commit-id: 11331cf251c788f743a15b3d53cae2921483ccb9

--- dataway.go
+++ dataway.go
@@ -134,7 +134,8 @@ func (dw *DataWayCfg) Send(category string, data []byte, gz bool) error {
 
 	dw.initHttp()
 
-	for _, dc := range dw.dataWayClients {
+	for idx, dc := range dw.dataWayClients {
+		l.Debugf("post to %d dataway...", idx)
 		if err := dc.send(dw.httpCli, category, data, gz); err != nil {
 			return err
 		}
