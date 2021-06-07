fef4680cae8bc6d1efdd9fffaef327ca316c128d
liushaobo <liushaobo@jiagouyun.com>
2021-06-07 13:54:59 +0800

    暂存代码
    Former-commit-id: cb9cd6077c34a459aa004ab951f50ed05b6851a8
    Former-commit-id: f154d7a533faec84e15bc9af757465bba52a0d6f [formerly 77c7403ea5f61f51e981a45fd76f3cd4fa4513ef]
    Former-commit-id: 90a79bc0223557c29b73aefa0cb204ce70864591

---
+++ cmd/datakit/cmds/kubernetes_test.go
@@ -0,0 +1,102 @@
+package cmds
+
+import (
+	"testing"
+	"bytes"
+	"fmt"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/redis"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mysql"
+	_ "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/mongodb"
+)
+
+var expects = []string{
+		"redis",
+		"mysql",
+		"mongodb",
+	}
+
+func TestBuildK8sConfig(t *testing.T) {
+	BuildK8sConfig("datakit-k8s", true)
+}
+
+func TestRunBuild(t *testing.T) {
+	k := &KubeDeploy{
+		DeployName: "deploy.yaml",
+		Inputs: make(map[string]string),
+	}
+
+	var testCases = []struct{
+        in string
+        expect string
+        fail bool
+	}{
+		{
+			in: "Q",
+			expect: "Bye!",
+		},
+		{
+			in: "q",
+			expect: "Bye!",
+		},
+		{
+			in: "exit",
+			expect: "Bye!",
+		},
+		{
+			in: "render",
+			expect: "start render kubernetes deploy config...",
+		},
+	}
+
+	for _, item := range testCases {
+		buf := bytes.Buffer{}
+		k.runCmd(&buf, item.in)
+		fmt.Println("hello world!")
+		got := buf.String()
+
+		if got != item.expect {
+	        t.Errorf("got '%s' want '%s'", got, item.expect)
+	    }
+	}
+}
+
+func TestBuildConfig(t *testing.T) {
+	k := &KubeDeploy{
+		DeployName: "deploy.yaml",
+		Inputs: make(map[string]string),
+	}
+
+	for _, expect := range expects {
+		if err := k.buildConfig(expect, &Option{}); err != nil {
+			t.Errorf("got error %v", err)
+			return
+		}
+	}
+
+	for k, cfg := range k.Inputs {
+		t.Log(k, "input config", cfg)
+	}
+
+	t.Log("ok")
+}
+
+func TestRender(t *testing.T) {
+	k := &KubeDeploy{
+		DeployName: "deploy",
+		Inputs: make(map[string]string),
+	}
+
+	for _, expect := range expects {
+		if err := k.buildConfig(expect, &Option{}); err != nil {
+			t.Errorf("got error %v", err)
+			return
+		}
+	}
+
+	if err := k.render(); err != nil {
+		t.Errorf("render error %v", err)
+		return
+	}
+
+	t.Log("ok")
+}
