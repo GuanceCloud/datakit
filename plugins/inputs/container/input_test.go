9fe79db61a3e4713e6f5e7283f249b933feb07a9
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-07 19:55:39 +0800

    容器采集器添加drop_tags和pod_name_rewrite配置，详见文档
     [formerly 8c306a2a93a37359c0723f3f0f1c9aac9f9c317e] [formerly c5db178656685dee6b5ae15ed0c27df087280a7c [formerly 9fab64d16b2a393b3ae7e9a7eb4fb459fa98e1ae] [formerly fdcff2aaed037c34e422fcb8404e8300f099ba35 [formerly 70772983ddd75b7c6d32abf514bf7ad37c469bb6]]] [formerly 9f888e33bededb8acba1ccd6d76cca446b635e64 [formerly 1365f24810ab29377ab12c76a26b0b9cd5e1f9cd] [formerly 8c306a2a93a37359c0723f3f0f1c9aac9f9c317e] [formerly c5db178656685dee6b5ae15ed0c27df087280a7c [formerly 9fab64d16b2a393b3ae7e9a7eb4fb459fa98e1ae] [formerly fdcff2aaed037c34e422fcb8404e8300f099ba35 [formerly 70772983ddd75b7c6d32abf514bf7ad37c469bb6]]] [formerly dabbb9b759abd1db70b2ebe3ab73e3156e985452 [formerly 443af9f392ece6f14bca064adaea51b375697cc0] [formerly 328ade60ecc08182946d5d83d0fcb15c73f19f54] [formerly a337246baf087c0e879a2e2f9c14893b17681097 [formerly 7e36cfdba1be71083373c4877747560fb29f5086] [formerly eb24dbae01b830877351da508493cc7b812fb3bc [formerly 2e070893fda2670c71fc9f45d74ed271818203c7]]]]]
     [formerly a337246baf087c0e879a2e2f9c14893b17681097 [formerly 7e36cfdba1be71083373c4877747560fb29f5086]]
    
    
    
     [formerly aeea60917c60cc074cb3dbcd9cbd907b6ad6b418 [formerly f99b3d74557f432d9a6acd0e31dab6f7c0273a09]]
    
    
    
    Former-commit-id: ce03a6cf0889b53d12a104980640c5474a1b8eea

--- plugins/inputs/container/input_test.go
+++ plugins/inputs/container/input_test.go
@@ -8,5 +8,82 @@ import (
  * go test -v -c && sudo ./docker.test -test.v -test.run=TestMain
  */
 
-func TestMain(t *testing.T) {
+func TestTrimPodName(t *testing.T) {
+	var cases = []struct {
+		podName []string
+		name    string
+		result  string
+	}{
+		{
+			podName: []string{"kube"},
+			name:    "kube_proxy_123",
+			result:  "kube_proxy",
+		},
+		{
+			podName: []string{"kube_proxy"},
+			name:    "kube_proxy_123",
+			result:  "kube_proxy",
+		},
+		{
+			podName: []string{"kube_proxy", "kube_proxy_pro"},
+			name:    "kube_proxy_123",
+			result:  "kube_proxy",
+		},
+		{
+			podName: []string{"kube_proxy_pro"},
+			name:    "kube_proxy_123",
+			result:  "kube_proxy_123",
+		},
+		{
+			podName: []string{"kube_proxy_pro"},
+			name:    "kube_proxy_pro_123",
+			result:  "kube_proxy_pro",
+		},
+		{
+			podName: []string{"kube_proxy_123", "kube_proxy_pro"},
+			name:    "kube_proxy_123",
+			// result:  "kube_proxy_123",
+			result: "kube_proxy",
+		},
+		{
+			podName: []string{"kube_proxy"},
+			name:    "kube_123",
+			result:  "kube_123",
+		},
+		{
+			podName: []string{"kube_proxy_pro", "kube_proxy"},
+			name:    "kube_proxy_123",
+			result:  "kube_proxy",
+		},
+		{
+			podName: []string{"proxy_kuber"},
+			name:    "kube_proxy_123",
+			result:  "kube_proxy_123",
+		},
+		{
+			podName: []string{"kube_proxy_123"},
+			name:    "kube_proxy_123_456_789",
+			result:  "kube_proxy_123_456",
+		},
+		{
+			podName: []string{"kube_proxy"},
+			name:    "kube$proxy",
+			result:  "kube$proxy",
+		},
+		{
+			podName: []string{"kube$proxy"},
+			name:    "kube_proxy_123",
+			result:  "kube_proxy_123",
+		},
+	}
+
+	for idx, tc := range cases {
+
+		result := TrimPodName(tc.podName, tc.name)
+		if result != tc.result {
+			t.Logf("[ ERROR ] index: %d, case.result: %s, got:%s\n", idx, tc.result, result)
+		} else {
+			t.Logf("[ OK ] index: %d, case.result: %s, got:%s\n", idx, tc.result, result)
+		}
+	}
 }
