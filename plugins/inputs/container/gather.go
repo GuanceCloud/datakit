0b8c924d79330facc57af10eeca8d5900ded5fcb
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-10 15:47:20 +0800

    支持container对image/container/pod name的数据过滤(#192)pod对象添加message字段
     [formerly 48cda84581966420355dee14cacc82fb4c514faf] [formerly 7ca4f21b066c7e7e97c79a352e9779e520b254f8 [formerly aa90627220b4cc89bd9c5b59469545227b563a3e] [formerly 4b6dd2fe24bc042631a8e209e9c891b41e45f558 [formerly b3f655564e60abc3486815d2ddd61df4af7eb8d2] [formerly 4b6dd2fe24bc042631a8e209e9c891b41e45f558 [formerly b3f655564e60abc3486815d2ddd61df4af7eb8d2] [formerly 4b6dd2fe24bc042631a8e209e9c891b41e45f558 [formerly b3f655564e60abc3486815d2ddd61df4af7eb8d2] [formerly 67ea237a785d1bc2eeb7685927bc7893920d3539 [formerly c24bd200dc45c5ea75feb4160a72a37f4db7f549]]]]]]
     [formerly 96ca42e0dac2643776501de961a46f1eb6ee2ec6 [formerly ede62edb21064fddeb06d22994936b39e55b2bf1] [formerly 96ca42e0dac2643776501de961a46f1eb6ee2ec6 [formerly ede62edb21064fddeb06d22994936b39e55b2bf1] [formerly 96ca42e0dac2643776501de961a46f1eb6ee2ec6 [formerly ede62edb21064fddeb06d22994936b39e55b2bf1] [formerly 7cbacd742b456591e4246b8fc76df7a14e2ab623 [formerly 119ad6fd3d91a2e3589bc6e5a6307b7716619c69]]]]]
    
    
    
    Former-commit-id: a62bc91cd64ec6630acf115bb3ddd91b3820cfbe [formerly ab5e2d17aedba22ec1be15a0364d72c9d83671f1]
    Former-commit-id: 018a09b640006a63b4850f6bdf9804dcb05cca9b [formerly 10d9db8085f4f84a1ec1b0ef2155a1c9e51d4ae5] [formerly f896592de783688d85dcc61b6ff0a84261ba1722 [formerly 4f75ef52e21384c355c63b2c5f633adbebbce39e]]
    Former-commit-id: 4e50da8786336614054b5ad2e651a5f1032c9472 [formerly 6ef20a1de466d78afbf3ef349c73f712b55b3dc2]
    Former-commit-id: f086e87cb6fedacf987eb85c7af0b69220fa7585

--- plugins/inputs/container/gather.go
+++ plugins/inputs/container/gather.go
@@ -3,7 +3,6 @@ package container
 import (
 	"context"
 	"encoding/json"
-	"strings"
 	"sync"
 	"time"
 
@@ -40,6 +39,12 @@ func (this *Input) gather(category category) ([]*io.Point, error) {
 
 		go func(c types.Container) {
 			defer wg.Done()
+
+			if RegexpMatchString(this.ignoreImageNameRegexps, c.Image) ||
+				RegexpMatchString(this.ignoreContainerNameRegexps, getContainerName(c.Names)) {
+				l.Debugf("ignore this container, image_name:%s container_name:%s", c.Image, getContainerName(c.Names))
+				return
+			}
 
 			var pt *io.Point
 			var err error
@@ -140,7 +145,7 @@ func (this *Input) gatherContainerInfo(container types.Container) map[string]str
 		"container_id":   container.ID,
 		"container_name": getContainerName(container.Names),
 		"docker_image":   container.ImageID,
-		"images_name":    container.Image,
+		"image_name":     container.Image,
 		"state":          container.State,
 		"container_type": func() string {
 			if contianerIsFromKubernetes(getContainerName(container.Names)) {
@@ -162,12 +167,7 @@ func (this *Input) gatherContainerInfo(container types.Container) map[string]str
 			l.Debugf("gather k8s pod error, %s", err)
 		}
 		for k, v := range podInfo {
-			switch k {
-			case "pod_name":
-				tags["pod_name"] = TrimPodName(this.PodNameRewrite, v)
-			default:
-				tags[k] = v
-			}
+			tags[k] = v
 		}
 	}
 
@@ -215,111 +215,4 @@ func (this *Input) gatherStats(container types.Container) (map[string]interface{
 		"block_read_byte":    blkRead,
 		"block_write_byte":   blkWrite,
 	}, nil
-}
-
-func getContainerName(names []string) string {
-	if len(names) > 0 {
-		return strings.TrimPrefix(names[0], "/")
-	}
-	return "invalidContainerName"
-}
-
-// contianerIsFromKubernetes 判断该容器是否由kubernetes创建
-// 所有kubernetes启动的容器的containerNamePrefix都是k8s，依据链接如下
-// https://github.com/rootsongjc/kubernetes-handbook/blob/master/practice/monitor.md#%E5%AE%B9%E5%99%A8%E7%9A%84%E5%91%BD%E5%90%8D%E8%A7%84%E5%88%99
-func contianerIsFromKubernetes(containerName string) bool {
-	const kubernetesContainerNamePrefix = "k8s"
-	return strings.HasPrefix(containerName, kubernetesContainerNamePrefix)
-}
-
-// https://docs.docker.com/engine/api/v1.41/#operation/ContainerStats
-
-func calculateCPUDelta(v *types.StatsJSON) int64 {
-	return int64(v.CPUStats.CPUUsage.TotalUsage - v.PreCPUStats.CPUUsage.TotalUsage)
-}
-
-func calculateCPUSystemDelta(v *types.StatsJSON) int64 {
-	return int64(v.CPUStats.SystemUsage - v.PreCPUStats.SystemUsage)
-}
-
-func calculateCPUNumbers(v *types.StatsJSON) int64 {
-	return int64(v.CPUStats.OnlineCPUs)
-}
-
-func calculateCPUPercentUnix(previousCPU, previousSystem uint64, v *types.StatsJSON) float64 {
-	var (
-		cpuPercent = 0.0
-		// calculate the change for the cpu usage of the container in between readings
-		cpuDelta = float64(v.CPUStats.CPUUsage.TotalUsage) - float64(previousCPU)
-		// calculate the change for the entire system between readings
-		systemDelta = float64(v.CPUStats.SystemUsage) - float64(previousSystem)
-		onlineCPUs  = float64(v.CPUStats.OnlineCPUs)
-	)
-
-	if onlineCPUs == 0.0 {
-		onlineCPUs = float64(len(v.CPUStats.CPUUsage.PercpuUsage))
-	}
-	if systemDelta > 0.0 && cpuDelta > 0.0 {
-		cpuPercent = (cpuDelta / systemDelta) * onlineCPUs * 100.0
-	}
-	return cpuPercent
-}
-
-func calculateBlockIO(blkio types.BlkioStats) (int64, int64) {
-	var blkRead, blkWrite int64
-	for _, bioEntry := range blkio.IoServiceBytesRecursive {
-		if len(bioEntry.Op) == 0 {
-			continue
-		}
-		switch bioEntry.Op[0] {
-		case 'r', 'R':
-			blkRead = blkRead + int64(bioEntry.Value)
-		case 'w', 'W':
-			blkWrite = blkWrite + int64(bioEntry.Value)
-		}
-	}
-	return blkRead, blkWrite
-}
-
-func calculateNetwork(network map[string]types.NetworkStats) (int64, int64) {
-	var rx, tx int64
-
-	for _, v := range network {
-		rx += int64(v.RxBytes)
-		tx += int64(v.TxBytes)
-	}
-	return rx, tx
-}
-
-// calculateMemUsageUnixNoCache calculate memory usage of the container.
-// Cache is intentionally excluded to avoid misinterpretation of the output.
-//
-// On cgroup v1 host, the result is `mem.Usage - mem.Stats["total_inactive_file"]` .
-// On cgroup v2 host, the result is `mem.Usage - mem.Stats["inactive_file"] `.
-//
-// This definition is consistent with cadvisor and containerd/CRI.
-// * https://github.com/google/cadvisor/commit/307d1b1cb320fef66fab02db749f07a459245451
-// * https://github.com/containerd/cri/commit/6b8846cdf8b8c98c1d965313d66bc8489166059a
-//
-// On Docker 19.03 and older, the result was `mem.Usage - mem.Stats["cache"]`.
-// See https://github.com/moby/moby/issues/40727 for the background.
-func calculateMemUsageUnixNoCache(mem types.MemoryStats) int64 {
-	// cgroup v1
-	if v, isCgroup1 := mem.Stats["total_inactive_file"]; isCgroup1 && v < mem.Usage {
-		return int64(mem.Usage - v)
-	}
-	// cgroup v2
-	if v := mem.Stats["inactive_file"]; v < mem.Usage {
-		return int64(mem.Usage - v)
-	}
-	return int64(mem.Usage)
-}
-
-func calculateMemPercentUnixNoCache(limit float64, usedNoCache float64) float64 {
-	// MemoryStats.Limit will never be 0 unless the container is not running and we haven't
-	// got any data from cgroup
-	if limit != 0 {
-		return usedNoCache / limit * 100.0
-	}
-	return 0
 }
