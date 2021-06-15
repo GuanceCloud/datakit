0f28f25fd4fb9ec8e2c158bc35aa4d88c0e7f1fc
tanbiao <tanbiao@jiagouyun.com>
2021-06-15 18:44:18 +0800

    update: add demo for logfilter
    Former-commit-id: d4691db5ed14b9c91ddb72a31f2a02f34611e348 [formerly d4d987fe80e962cc7c84fb5203c2ce18091828f5] [formerly 01c76e01014629f95bb90d8a5171d30ea8691483 [formerly 41ac6e5d4e04834ede0b4d4b88ebf4d00aa41d17]]
    Former-commit-id: df71834e3302c47d8b7b1c76d084215870b9defc [formerly 2e5fa7caaa59053abc8d0a89b35c09e9dd189823]
    Former-commit-id: a508d46638b6f05972232a95ff2ac2af35d5d3ff

---
+++ io/logfilter/parse_test.go
@@ -0,0 +1,74 @@
+package logfilter
+
+import (
+	"reflect"
+	"testing"
+
+	tu "gitlab.jiagouyun.com/cloudcare-tools/cliutils/testutil"
+)
+
+func TestParse(t *testing.T) {
+	cases := []struct {
+		in       string
+		expected interface{}
+		fail     bool
+	}{
+		{
+			in: `{
+				a > 0, c < 5 || d < 2}`,
+			//in: "{a > 0}",
+			expected: WhereCondition{
+				conditions: []Node{
+					&BinaryExpr{
+						Op: AND,
+						LHS: &BinaryExpr{
+							Op:  GT,
+							LHS: &Identifier{Name: "a"},
+							RHS: &NumberLiteral{IsInt: true, Int: 0},
+						},
+
+						RHS: &BinaryExpr{
+							Op: OR,
+							LHS: &BinaryExpr{
+
+								Op:  LT,
+								LHS: &Identifier{Name: "c"},
+								RHS: &NumberLiteral{IsInt: true, Int: 5},
+							},
+
+							RHS: &BinaryExpr{
+								Op:  LT,
+								LHS: &Identifier{Name: "d"},
+								RHS: &NumberLiteral{IsInt: true, Int: 2},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		var err error
+		p := newParser(tc.in)
+		defer parserPool.Put(p)
+		defer p.recover(&err)
+
+		p.doParse()
+
+		if tc.fail {
+			tu.Assert(t, len(p.errs) > 0, "")
+			continue
+		}
+
+		switch w := p.parseResult.(type) {
+		case *WhereCondition:
+			exp := tc.expected.(WhereCondition)
+			x, y := exp.String(), w.String()
+			tu.Equals(t, x, y)
+			t.Logf("in: %s, exp: %s", x, y)
+		default:
+			t.Fatalf("should not been here: %s", reflect.TypeOf(p.parseResult).String())
+		}
+	}
+}
