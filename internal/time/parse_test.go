1650252b0aec331b95a6f6c3f0049406a2d6e359
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-16 10:35:56 +0800

    重构container代码，删除docker容器，适配新的指标集名，添加新字段
     [formerly e0b79df4feb5c06ca214ad1f4dedfc668142e1ad [formerly 161f5c1f35b0af6585e31e28f0252e28810b7dec] [formerly ebf727982178e8eeb9675895ca5c526bcdc95818 [formerly 2540629fca7313741bb79f600a10331aa77eee54]] [formerly 6c6a6526cef601dddfb204658778f88d166d66d1] [formerly d8a5abe265752e69410dbfcbefc0ebc9e8ab8639 [formerly ddce3917fc3325d65b8d035e5e12527364c86950] [formerly 98fdf88186438677eec30977cbaf0085637a8926 [formerly f473d49941db30aa466c53cbe1b23869e9a96b8c]] [formerly 98fdf88186438677eec30977cbaf0085637a8926 [formerly f473d49941db30aa466c53cbe1b23869e9a96b8c] [formerly d638ad21b2c693128f9777e3edce20885f1f5253 [formerly d5e59a95dfb35f7bf307936bf6dad9a57705e9f7]]]] [formerly ce1c0ab237d635de4cf7ab15311a174bce5c6a4c] [formerly 5c99505268537193b82f6e74850a7cfb6718f91c [formerly 37480c2f754400eb7041f90689fe809d00e638bd] [formerly 87b451ed6e3be6dede0376d2ccc86d80f7206efc [formerly 80c715f4e978c50ea54f9b93f735babb34ec75cf] [formerly 87b451ed6e3be6dede0376d2ccc86d80f7206efc [formerly 80c715f4e978c50ea54f9b93f735babb34ec75cf] [formerly 87b451ed6e3be6dede0376d2ccc86d80f7206efc [formerly 80c715f4e978c50ea54f9b93f735babb34ec75cf] [formerly a1d5ff1730e6c2131a44390220401eb623d67218 [formerly a43afc394e2f3eb31b3abb747dfd7378a27d1a78]]]]]]]
     [formerly ce1c0ab237d635de4cf7ab15311a174bce5c6a4c]
    
    
    
    
    Former-commit-id: 9510bc6ca6d8c404ce59a5c73d5966377b69fec1 [formerly 013a51c8d9229b544eca63cfcae13e9933d4bf48]
    Former-commit-id: 8e770afee0e7e8aea67ca536dc05c3be1af61790 [formerly e23cea93be20ce08e02ef685ac0082b88b7dd6ff] [formerly 5665025169d075135dc0cda33bb1fe0ff5c5a42a [formerly d533492419d2d70ef1b4752ba9d4036aab73de04]]
    Former-commit-id: 3878eabc3d4b11113a96732e12cd07b78fe86e93 [formerly be0e5e959d81a2e55f8db61310637391814c89e5]
    Former-commit-id: 93c7f3bb7f8a362910c6d1cbf9154e2c38f5e38e

---
+++ internal/time/parse_test.go
@@ -0,0 +1,41 @@
+package time
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParseDuration(t *testing.T) {
+	var cases = []struct {
+		du     string
+		expect time.Duration
+		fail   bool
+	}{
+		{du: `1ns`, expect: time.Duration(1)},
+		{du: `1us`, expect: time.Microsecond},
+		{du: `1ms`, expect: time.Millisecond},
+		{du: `1s`, expect: time.Second},
+		{du: `1h`, expect: time.Hour},
+		{du: `1d`, expect: time.Hour * 24},
+		{du: `1w`, expect: time.Hour * 24 * 7},
+		{du: `1y`, expect: time.Hour * 24 * 365},
+		{du: `1ym`, fail: true},
+	}
+
+	for _, c := range cases {
+		du, err := ParseDuration(c.du)
+		if err != nil && !c.fail {
+			t.Fatalf("%s should ok", c.du)
+		}
+
+		if c.fail && err == nil {
+			t.Fatalf("%s should fail", c.du)
+		}
+
+		if du != c.expect {
+			t.Fatalf("got %q, expect %q", du, c.du)
+		}
+
+		t.Logf("%s is %v", c.du, du)
+	}
+}
