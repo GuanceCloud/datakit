c9218414f3d360c15c2d34fa68ade36fd873514d
CodapeWild <codape.wild@gmail.com>
2021-06-17 11:49:54 +0800

    add mock data
    Former-commit-id: 1554af9549d8e8a7dea91a3a4a5380d421c39005 [formerly 15a22680fd78809eda4ae1169e6caa76f85372af] [formerly f2df5918cc8344be13c0264d1f21b3766d4b910a [formerly 38fc7ad59f7cfe4027430e5f499fc03c45a484ad]]
    Former-commit-id: 272a6d6b899419fb0e617a6f076a40bbfb27053c [formerly 3a447a9377d438a0edfc1a16e3d0012509477ead]
    Former-commit-id: 1c0fce319bdeba56a233d99cff1d55f840dea829

--- io/logfilter_test.go
+++ io/logfilter_test.go
@@ -1,10 +1,15 @@
 package io
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	lp "gitlab.jiagouyun.com/cloudcare-tools/cliutils/lineproto"
+)
 
 type debugLogFilterMock struct{}
 
-func (*debugLogFilterMock) GetLogFilter() ([]byte, error) {
+func (*debugLogFilterMock) getLogFilter() ([]byte, error) {
 	return []byte(`
 {
 	"content": ["{source == mongodb}"]
@@ -12,6 +17,48 @@ func (*debugLogFilterMock) GetLogFilter() ([]byte, error) {
 `), nil
 }
 
+func (*debugLogFilterMock) preparePoints(pts []*Point) []*Point {
+	influxPts, err := lp.ParsePoints([]byte(`
+mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="NETWORK",context="listener",message="{\"t\":{\"$date\":\"2021-06-17T02:35:48.279+00:00\"},\"s\":\"I\",  \"c\":\"NETWORK\",  \"id\":22943,   \"ctx\":\"listener\",\"msg\":\"Connection accepted\",\"attr\":{\"remote\":\"172.17.0.1:58008\",\"connectionId\":1,\"connectionCount\":1}}",msg="Connection accepted",status="info" 1623897348290344000
+mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="COMMAND",context="conn1",message="{\"t\":{\"$date\":\"2021-06-17T02:35:48.297+00:00\"},\"s\":\"W\",  \"c\":\"COMMAND\",  \"id\":4718707, \"ctx\":\"conn1\",\"msg\":\"The shardConnPoolStats command is deprecated. Use instead the connPoolStats command.\"}",msg="The shardConnPoolStats command is deprecated. Use instead the connPoolStats command.",status="warning" 1623897378234503000
+mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="NETWORK",context="listener",message="{\"t\":{\"$date\":\"2021-06-17T02:36:18.242+00:00\"},\"s\":\"I\",  \"c\":\"NETWORK\",  \"id\":22943,   \"ctx\":\"listener\",\"msg\":\"Connection accepted\",\"attr\":{\"remote\":\"172.17.0.1:58012\",\"connectionId\":2,\"connectionCount\":2}}",msg="Connection accepted",status="info" 1623897386561770000
+mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="STORAGE",context="WTCheckpointThread",message="{\"t\":{\"$date\":\"2021-06-17T02:36:26.579+00:00\"},\"s\":\"I\",  \"c\":\"STORAGE\",  \"id\":22430,   \"ctx\":\"WTCheckpointThread\",\"msg\":\"WiredTiger message\",\"attr\":{\"message\":\"[1623897386:579717][1:0x7fb2db747700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 15, snapshot max: 15 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)\"}}",msg="WiredTiger message",status="info" 1623897446578633000
+mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="STORAGE",context="WTCheckpointThread",message="{\"t\":{\"$date\":\"2021-06-17T02:37:26.596+00:00\"},\"s\":\"I\",  \"c\":\"STORAGE\",  \"id\":22430,   \"ctx\":\"WTCheckpointThread\",\"msg\":\"WiredTiger message\",\"attr\":{\"message\":\"[1623897446:596429][1:0x7fb2db747700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 16, snapshot max: 16 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)\"}}",msg="WiredTiger message",status="info" 1623897506592459000
+mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="STORAGE",context="WTCheckpointThread",message="{\"t\":{\"$date\":\"2021-06-17T02:38:26.610+00:00\"},\"s\":\"I\",  \"c\":\"STORAGE\",  \"id\":22430,   \"ctx\":\"WTCheckpointThread\",\"msg\":\"WiredTiger message\",\"attr\":{\"message\":\"[1623897506:610362][1:0x7fb2db747700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 17, snapshot max: 17 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)\"}}",msg="WiredTiger message",status="info" 1623897566610844000
+	`), nil)
+	if err != nil {
+		log.Error(err)
+		pts = nil
+	}
+
+	pts = []*Point{}
+	for _, pt := range influxPts {
+		pts = append(pts, &Point{Point: pt})
+	}
+
+	return pts
+}
+
 func TestLogFilter(t *testing.T) {
+	time.Sleep(3 * time.Second)
+
+	if defLogfilter.status == filter_released {
+		log.Info("log filter released")
+	} else if defLogfilter.status == filter_refreshed {
+		log.Info("log filter refreshed")
+	} else {
+		log.Info("log filter status unknow")
+	}
+
+	log.Infof("log filter current rules: %q", defLogfilter.rules)
+
+	after := defLogfilter.filter(nil)
+	for _, pt := range after {
+		log.Info(pt)
+	}
+}
+
+func init() {
+	defInterval = time.Second
 	defLogFilterMock = &debugLogFilterMock{}
 }
