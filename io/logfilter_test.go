8b8e4526710474b22ba0d9c19c543085b83a0c12
CodapeWild <codape.wild@gmail.com>
2021-06-17 14:22:56 +0800

    fix
    Former-commit-id: 2875c3ecdc0c5597dfae4f91d863c6d5fb2c65e5 [formerly ea834dd802fd218e24fdd79bd9a76ce6d4ecd606]
    Former-commit-id: e75116e07492d5744054bae469f595002033471e

--- io/logfilter_test.go
+++ io/logfilter_test.go
@@ -34,14 +34,14 @@ func (*debugLogFilterMock) preparePoints(pts []*Point) []*Point {
 }
 
 func TestLogFilter(t *testing.T) {
-	debugFilterRules = []byte(`{"content": ["{source == mongodb}"]}`)
+	debugFilterRules = []byte(`{"content": ["{component = 'NETWORK' || context = 'conn1'}", "{a >= 123}"]}`)
 	debugPoints = []byte(`
 mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="NETWORK",context="listener",message="{\"t\":{\"$date\":\"2021-06-17T02:35:48.279+00:00\"},\"s\":\"I\",  \"c\":\"NETWORK\",  \"id\":22943,   \"ctx\":\"listener\",\"msg\":\"Connection accepted\",\"attr\":{\"remote\":\"172.17.0.1:58008\",\"connectionId\":1,\"connectionCount\":1}}",msg="Connection accepted",status="info" 1623897348290344000
 mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="COMMAND",context="conn1",message="{\"t\":{\"$date\":\"2021-06-17T02:35:48.297+00:00\"},\"s\":\"W\",  \"c\":\"COMMAND\",  \"id\":4718707, \"ctx\":\"conn1\",\"msg\":\"The shardConnPoolStats command is deprecated. Use instead the connPoolStats command.\"}",msg="The shardConnPoolStats command is deprecated. Use instead the connPoolStats command.",status="warning" 1623897378234503000
 mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="NETWORK",context="listener",message="{\"t\":{\"$date\":\"2021-06-17T02:36:18.242+00:00\"},\"s\":\"I\",  \"c\":\"NETWORK\",  \"id\":22943,   \"ctx\":\"listener\",\"msg\":\"Connection accepted\",\"attr\":{\"remote\":\"172.17.0.1:58012\",\"connectionId\":2,\"connectionCount\":2}}",msg="Connection accepted",status="info" 1623897386561770000
 mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="STORAGE",context="WTCheckpointThread",message="{\"t\":{\"$date\":\"2021-06-17T02:36:26.579+00:00\"},\"s\":\"I\",  \"c\":\"STORAGE\",  \"id\":22430,   \"ctx\":\"WTCheckpointThread\",\"msg\":\"WiredTiger message\",\"attr\":{\"message\":\"[1623897386:579717][1:0x7fb2db747700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 15, snapshot max: 15 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)\"}}",msg="WiredTiger message",status="info" 1623897446578633000
 mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="STORAGE",context="WTCheckpointThread",message="{\"t\":{\"$date\":\"2021-06-17T02:37:26.596+00:00\"},\"s\":\"I\",  \"c\":\"STORAGE\",  \"id\":22430,   \"ctx\":\"WTCheckpointThread\",\"msg\":\"WiredTiger message\",\"attr\":{\"message\":\"[1623897446:596429][1:0x7fb2db747700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 16, snapshot max: 16 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)\"}}",msg="WiredTiger message",status="info" 1623897506592459000
-mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="STORAGE",context="WTCheckpointThread",message="{\"t\":{\"$date\":\"2021-06-17T02:38:26.610+00:00\"},\"s\":\"I\",  \"c\":\"STORAGE\",  \"id\":22430,   \"ctx\":\"WTCheckpointThread\",\"msg\":\"WiredTiger message\",\"attr\":{\"message\":\"[1623897506:610362][1:0x7fb2db747700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 17, snapshot max: 17 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)\"}}",msg="WiredTiger message",status="info" 1623897566610844000
+mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb a=123i,component="STORAGE",context="WTCheckpointThread",message="{\"t\":{\"$date\":\"2021-06-17T02:38:26.610+00:00\"},\"s\":\"I\",  \"c\":\"STORAGE\",  \"id\":22430,   \"ctx\":\"WTCheckpointThread\",\"msg\":\"WiredTiger message\",\"attr\":{\"message\":\"[1623897506:610362][1:0x7fb2db747700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 17, snapshot max: 17 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)\"}}",msg="WiredTiger message",status="info" 1623897566610844000
 	`)
 
 	time.Sleep(3 * time.Second)
@@ -62,8 +62,8 @@ mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb c
 	}
 }
 
-func TestLogFilterWithEmptyRule(t *testing.T) {
-	debugFilterRules = []byte(`{"content": ["{source == mongodb}"]}`)
+func TestEmptyRule(t *testing.T) {
+	debugFilterRules = []byte(`{"content": ["{component = 'NETWORK'}"]}`)
 	debugPoints = []byte(`
 mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="NETWORK",context="listener",message="{\"t\":{\"$date\":\"2021-06-17T02:35:48.279+00:00\"},\"s\":\"I\",  \"c\":\"NETWORK\",  \"id\":22943,   \"ctx\":\"listener\",\"msg\":\"Connection accepted\",\"attr\":{\"remote\":\"172.17.0.1:58008\",\"connectionId\":1,\"connectionCount\":1}}",msg="Connection accepted",status="info" 1623897348290344000
 mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb component="COMMAND",context="conn1",message="{\"t\":{\"$date\":\"2021-06-17T02:35:48.297+00:00\"},\"s\":\"W\",  \"c\":\"COMMAND\",  \"id\":4718707, \"ctx\":\"conn1\",\"msg\":\"The shardConnPoolStats command is deprecated. Use instead the connPoolStats command.\"}",msg="The shardConnPoolStats command is deprecated. Use instead the connPoolStats command.",status="warning" 1623897378234503000
@@ -83,14 +83,14 @@ mongodb,filename=mongod.log,host=CodapeWilds-MacBook-Pro.local,service=mongodb c
 		log.Info("log filter status unknow")
 	}
 
-	log.Infof("log filter current rules: %q", defLogfilter.rules)
+	log.Debug("log filter current rules: %q", defLogfilter.rules)
 
 	after := defLogfilter.filter(nil)
 	for _, pt := range after {
 		log.Info(pt)
 	}
 
-	debugFilterRules = []byte("")
+	debugFilterRules = []byte(`{"content": []}`)
 	time.Sleep(3 * time.Second)
 	if defLogfilter.status == filter_released {
 		log.Info("log filter released")
