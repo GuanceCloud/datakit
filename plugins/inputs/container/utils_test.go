db1027decaee2a588e86daa338ea90d273c3620f
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-22 14:38:41 +0800

    优化了container采集对k8s的连接处理，现在使用默认k8s地址时将不会报错
    Former-commit-id: 5ac67d07152e603cccd10ee85b62bfc7bcbdd839 [formerly 468f9c3378060c82ca4d3b236bdb05c4e577f492] [formerly abd8fdb3a446f493a582a598ba4321d64f3ef421 [formerly cd6640209b905ecb462905a9670287a7c0ca8a47]]
    Former-commit-id: cc8a08395fbfc45d2b725098e05abde238264756 [formerly 24ad0f479a8ff6a34f67b2db68d0c41f1ff2aad0]
    Former-commit-id: 853c4b847f430dfdc380a77e57e57b0c43d04870

--- plugins/inputs/container/utils_test.go
+++ plugins/inputs/container/utils_test.go
@@ -1,165 +1,89 @@
 package container
 
 import (
-	"regexp"
 	"testing"
 )
 
+func TestParseImage(t *testing.T) {
+	var cases = []struct {
+		image          string
+		imageName      string
+		imageShortName string
+		imageTag       string
+	}{
+		{
+			"influxdb:1.7.9",
+			"influxdb",
+			"influxdb",
+			"1.7.9",
+		},
+		{
+			"gitlab/gitlab-ee:latest",
+			"gitlab/gitlab-ee",
+			"gitlab-ee",
+			"latest",
+		},
+		{
+			"registry.aliyuncs.com/google_containers/pause:3.1",
+			"registry.aliyuncs.com/google_containers/pause",
+			"pause",
+			"3.1",
+		},
+		{
+			"testing-none",
+			"testing-none",
+			"testing-none",
+			"unknown",
+		},
+	}
+
+	for idx, tc := range cases {
+		name, short, version := ParseImage(tc.image)
+		if name != tc.imageName {
+			t.Errorf("[%d][ERROR] should be %s, got %s\n", idx, tc.imageName, name)
+		}
+		if short != tc.imageShortName {
+			t.Errorf("[%d][ERROR] should be %s, got %s\n", idx, tc.imageShortName, short)
+		}
+		if version != tc.imageTag {
+			t.Errorf("[%d][ERROR] should be %s, got %s\n", idx, tc.imageTag, version)
+		}
+
+		t.Logf("[%d][OK] %v\n", idx, tc)
+	}
+}
+
 func TestRegexpMatchString(t *testing.T) {
 	var cases = []struct {
-		regexps []*regexp.Regexp
+		regexps []string
 		target  string
 		fail    bool
 	}{
 		{
-			func() (res []*regexp.Regexp) {
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`hello-*`); return re }())
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`hello1-*`); return re }())
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`hello2-*`); return re }())
-				return
-			}(),
+			[]string{`hello-*`, `hello1-*`, `hello2-*`},
 			"hello2-image",
 			false,
 		},
 		{
-			func() (res []*regexp.Regexp) {
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`registry*`); return re }())
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`registry.aliyuncs*`); return re }())
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`registry.aliyuncs.com*`); return re }())
-				return
-			}(),
+			[]string{`registry*`, `registry.aliyuncs*`, `registry.aliyuncs.com*`},
 			"registry.aliyuncs.com/google_containers",
 			false,
 		},
 		{
-			func() (res []*regexp.Regexp) {
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`hello-*`); return re }())
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`hello1-*`); return re }())
-				res = append(res, func() *regexp.Regexp { re, _ := regexp.Compile(`hello2-*`); return re }())
-				return
-			}(),
+			[]string{`hello-*`, `hello1-*`, `hello2-*`},
 			"registry.aliyuncs.com/google_containers/kube-apiserver",
 			true,
 		},
 	}
 
 	for idx, tc := range cases {
-		matchRes := RegexpMatchString(tc.regexps, tc.target)
+		matchRes := regexpMatchString(tc.regexps, tc.target)
 		if matchRes && !tc.fail {
 			t.Logf("[ OK ] index: %d \n", idx)
 		} else if matchRes {
 			t.Logf("[ ERROR ] index: %d\n", idx)
 		} else {
 			t.Logf("[ OK ] index: %d\n", idx)
-		}
-	}
-}
-
-func TestRawConnect(t *testing.T) {
-	var cases = []struct {
-		host string
-		port string
-		fail bool
-	}{
-		{
-			"127.0.0.1",
-			"22", // 假设本机开启 sshd
-			false,
-		},
-		{
-			"127.0.0.1",
-			"65534",
-			true,
-		},
-	}
-
-	for idx, tc := range cases {
-		err := RawConnect(tc.host, tc.port)
-		if err != nil && tc.fail {
-			t.Logf("[ OK ] index: %d failed of connect: %s\n", idx, err)
-		} else if err != nil {
-			t.Logf("[ ERROR ] index: %d failed of connect: %s\n", idx, err)
-		} else {
-			t.Logf("[ OK ] index: %d connect success %s:%s\n", idx, tc.host, tc.port)
-		}
-	}
-}
-
-func TestTrimPodName(t *testing.T) {
-	var cases = []struct {
-		podName []string
-		name    string
-		result  string
-	}{
-		{
-			podName: []string{"kube"},
-			name:    "kube_proxy_123",
-			result:  "kube_proxy",
-		},
-		{
-			podName: []string{"kube_proxy"},
-			name:    "kube_proxy_123",
-			result:  "kube_proxy",
-		},
-		{
-			podName: []string{"kube_proxy", "kube_proxy_pro"},
-			name:    "kube_proxy_123",
-			result:  "kube_proxy",
-		},
-		{
-			podName: []string{"kube_proxy_pro"},
-			name:    "kube_proxy_123",
-			result:  "kube_proxy_123",
-		},
-		{
-			podName: []string{"kube_proxy_pro"},
-			name:    "kube_proxy_pro_123",
-			result:  "kube_proxy_pro",
-		},
-		{
-			podName: []string{"kube_proxy_123", "kube_proxy_pro"},
-			name:    "kube_proxy_123",
-			// result:  "kube_proxy_123",
-			result: "kube_proxy",
-		},
-		{
-			podName: []string{"kube_proxy"},
-			name:    "kube_123",
-			result:  "kube_123",
-		},
-		{
-			podName: []string{"kube_proxy_pro", "kube_proxy"},
-			name:    "kube_proxy_123",
-			result:  "kube_proxy",
-		},
-		{
-			podName: []string{"proxy_kuber"},
-			name:    "kube_proxy_123",
-			result:  "kube_proxy_123",
-		},
-		{
-			podName: []string{"kube_proxy_123"},
-			name:    "kube_proxy_123_456_789",
-			result:  "kube_proxy_123_456",
-		},
-		{
-			podName: []string{"kube_proxy"},
-			name:    "kube$proxy",
-			result:  "kube$proxy",
-		},
-		{
-			podName: []string{"kube$proxy"},
-			name:    "kube_proxy_123",
-			result:  "kube_proxy_123",
-		},
-	}
-
-	for idx, tc := range cases {
-		result := TrimPodName(tc.podName, tc.name)
-		if result != tc.result {
-			t.Logf("[ ERROR ] index: %d, case.result: %s, got:%s\n", idx, tc.result, result)
-		} else {
-			t.Logf("[ OK ] index: %d, case.result: %s, got:%s\n", idx, tc.result, result)
 		}
 	}
 }
