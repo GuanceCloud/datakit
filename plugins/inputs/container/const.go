413db8da99158525e309404cc1b9beac6545e4d2
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-22 14:38:41 +0800

    优化了container采集对k8s的连接处理，现在使用默认k8s地址时将不会报错
     [formerly dcaf98e3f71e5a6187cfa4dd8facd699c317b732 [formerly b93ae93dfeb4d289555a6963e3f2f4dde835d6be] [formerly 2783f0ec29c4d547ea18c0dcb39cd835b2c779d2 [formerly 6f6ade20073921cfb94f014db1f264e24e23fb17]] [formerly b51997d230f7ca8545469a479fb1e0629a6843ce] [formerly 13e9355d5ec2afa90de885c6682efcb5d6a2f9e1 [formerly 92e86d2b1d5c23d5f322b010310afddae32f4152] [formerly 13e9355d5ec2afa90de885c6682efcb5d6a2f9e1 [formerly 92e86d2b1d5c23d5f322b010310afddae32f4152] [formerly c15acee0f8b62078af57298c81e43365f28b30d3 [formerly 2ea1f7ec0898b177e4fd3064cda659e823e03468]]]] [formerly 015dd9a688151012f344201dc142137e7dc0a238] [formerly 7ffb9597d2d705a804a5d2b62eb27f054c203e38 [formerly c658ccd6dd6a36543c51df4b37be5612bfae681f] [formerly 4e2830940ebac50775aa172647d527b28ea7d693 [formerly 468f9c3378060c82ca4d3b236bdb05c4e577f492] [formerly 4e2830940ebac50775aa172647d527b28ea7d693 [formerly 468f9c3378060c82ca4d3b236bdb05c4e577f492] [formerly 4e2830940ebac50775aa172647d527b28ea7d693 [formerly 468f9c3378060c82ca4d3b236bdb05c4e577f492] [formerly abd8fdb3a446f493a582a598ba4321d64f3ef421 [formerly 8fc0d6a2acb46ead300688fdef88e6b8fe0f43fd]]]]]]]
     [formerly 015dd9a688151012f344201dc142137e7dc0a238]
    
    
    
    
    Former-commit-id: 1375ddefee3e4e3745d79a6e1689adca97784c98 [formerly f668114242f2bb4d468ee7a4598f067a909769d9]
    Former-commit-id: 50db2baa5f774f0fe79d6b8dc78cc34dc6450f3e [formerly bcee4e776edc6ef6dc76cfc4efde95549bab8740] [formerly f76e658d992cd8206057571d28982432daf89c35 [formerly 3ed070999a011cdaf6a2264a8f2f0234e9637323]]
    Former-commit-id: 8ea5c165a13ae5db0ff5a6ace6679275f166b204 [formerly 45055a20133b2ca4b3eea67411c6bce2ffd654f0]
    Former-commit-id: e30cb8586c8a59faec558291acce1ea1bb02b875

---
+++ plugins/inputs/container/const.go
@@ -0,0 +1,92 @@
+package container
+
+import (
+	"time"
+)
+
+const (
+	inputName = "container"
+	catelog   = "container"
+
+	// docker endpoint
+	dockerEndpoint = "unix:///var/run/docker.sock"
+
+	// docker sock 文件路径，用以判断主机是否已安装 docker 服务
+	dockerEndpointPath = "/var/run/docker.sock"
+
+	// Docker API 超时时间
+	apiTimeoutDuration = time.Second * 5
+
+	// 最小指标采集间隔
+	minMetricDuration = time.Second * 10
+
+	// 最大指标采集间隔
+	maxMetricDuration = time.Second * 60
+
+	// 对象采集间隔
+	objectDuration = time.Minute * 5
+
+	// 定时发现新日志源
+	loggingHitDuration = time.Second * 5
+
+	// 是否采集全部容器，包括未在运行的
+	containerAllForMetric  = false
+	containerAllForObject  = true
+	containerAllForLogging = false
+)
+
+const sampleCfg = `
+[inputs.container]
+  endpoint = "unix:///var/run/docker.sock"
+  
+  enable_metric = false  
+  enable_object = true   
+  enable_logging = true  
+  
+  metric_interval = "10s"
+
+  drop_tags = ["contaienr_id"]
+
+  ## Examples:
+  ##    '''nginx*'''
+  ignore_image_name = []
+  ignore_container_name = []
+  
+  ## TLS Config
+  # tls_ca = "/path/to/ca.pem"
+  # tls_cert = "/path/to/cert.pem"
+  # tls_key = "/path/to/key.pem"
+  ## Use TLS but skip chain & host verification
+  # insecure_skip_verify = false
+  
+  [inputs.container.kubelet]
+    kubelet_url = "http://127.0.0.1:10255"
+    ignore_pod_name = []
+
+    ## Use bearer token for authorization. ('bearer_token' takes priority)
+    ## If both of these are empty, we'll use the default serviceaccount:
+    ## at: /run/secrets/kubernetes.io/serviceaccount/token
+    # bearer_token = "/path/to/bearer/token"
+    ## OR
+    # bearer_token_string = "abc_123"
+
+    ## Optional TLS Config
+    # tls_ca = /path/to/ca.pem
+    # tls_cert = /path/to/cert.pem
+    # tls_key = /path/to/key.pem
+    ## Use TLS but skip chain & host verification
+    # insecure_skip_verify = false
+  
+  #[[inputs.container.logfilter]]
+  #  filter_message = [
+  #    '''<this-is-message-regexp''',
+  #    '''<this-is-another-message-regexp''',
+  #  ]
+  #  source = "<your-source-name>"
+  #  service = "<your-service-name>"
+  #  pipeline = "<pipeline.p>"
+  
+  [inputs.container.tags]
+    # some_tag = "some_value"
+    # more_tag = "some_other_value"
+`
