845f5ea67f2da00844071e3d45fe3e06cde80a26
liguozhuang <liguozhuang@jiagouyun.com>
2021-06-22 14:38:41 +0800

    优化了container采集对k8s的连接处理，现在使用默认k8s地址时将不会报错
     [formerly 415091a2100e24f3ad6eae2e13374ff885a31d35 [formerly b93ae93dfeb4d289555a6963e3f2f4dde835d6be] [formerly 2783f0ec29c4d547ea18c0dcb39cd835b2c779d2 [formerly 6f6ade20073921cfb94f014db1f264e24e23fb17]] [formerly b51997d230f7ca8545469a479fb1e0629a6843ce] [formerly d1f147ff29788441cf911a23a81c58b14754c469 [formerly 92e86d2b1d5c23d5f322b010310afddae32f4152] [formerly d1f147ff29788441cf911a23a81c58b14754c469 [formerly 92e86d2b1d5c23d5f322b010310afddae32f4152] [formerly c15acee0f8b62078af57298c81e43365f28b30d3 [formerly 15caf5b132ef30fc8dd3efd551f54bffe5ed7958]]]] [formerly 015dd9a688151012f344201dc142137e7dc0a238] [formerly 7ffb9597d2d705a804a5d2b62eb27f054c203e38 [formerly c658ccd6dd6a36543c51df4b37be5612bfae681f] [formerly 77df180d94d34ddbd1c4df5559ef29a577605c99 [formerly 468f9c3378060c82ca4d3b236bdb05c4e577f492] [formerly 77df180d94d34ddbd1c4df5559ef29a577605c99 [formerly 468f9c3378060c82ca4d3b236bdb05c4e577f492] [formerly 77df180d94d34ddbd1c4df5559ef29a577605c99 [formerly 468f9c3378060c82ca4d3b236bdb05c4e577f492] [formerly abd8fdb3a446f493a582a598ba4321d64f3ef421 [formerly cd6640209b905ecb462905a9670287a7c0ca8a47]]]]]]]
     [formerly 015dd9a688151012f344201dc142137e7dc0a238]
    
    
    
    
    Former-commit-id: 1375ddefee3e4e3745d79a6e1689adca97784c98 [formerly f668114242f2bb4d468ee7a4598f067a909769d9]
    Former-commit-id: 50db2baa5f774f0fe79d6b8dc78cc34dc6450f3e [formerly bcee4e776edc6ef6dc76cfc4efde95549bab8740] [formerly f76e658d992cd8206057571d28982432daf89c35 [formerly ca9a4b1740649cd67547587c3a77e665be4c4455]]
    Former-commit-id: 8ea5c165a13ae5db0ff5a6ace6679275f166b204 [formerly 45055a20133b2ca4b3eea67411c6bce2ffd654f0]
    Former-commit-id: e30cb8586c8a59faec558291acce1ea1bb02b875

---
+++ io/parser/eval_test.go
@@ -0,0 +1,364 @@
+package parser
+
+import (
+	//"reflect"
+	"testing"
+
+	tu "gitlab.jiagouyun.com/cloudcare-tools/cliutils/testutil"
+)
+
+func TestExprConditions(t *testing.T) {
+	cases := []struct {
+		in     string
+		source string
+		tags   map[string]string
+		fields map[string]interface{}
+		pass   bool
+	}{
+
+		{
+			in:     "{abc notin [1.1,1.2,1.3] and (a > 1 || c< 0)}",
+			fields: map[string]interface{}{"abc": int64(4), "a": int64(-1), "c": int64(-2)},
+			pass:   true,
+		},
+
+		{
+			in:     "{a notin [1,2,3,4]}",
+			fields: map[string]interface{}{"a": int64(4)},
+			pass:   false,
+		},
+
+		{
+			in:     "{abc notin [1,2,3]}",
+			fields: map[string]interface{}{"abc": int64(4)},
+			pass:   true,
+		},
+
+		{
+			in:     ";;;{a > 1, b > 1 or c > 1, xx != 123 };;;; {xyz > 1};;;",
+			fields: map[string]interface{}{"a": int64(2), "c": "xyz"},
+			pass:   false,
+		},
+
+		{
+			in:     "{a > 1, b > 1 or c > 1}",
+			fields: map[string]interface{}{"a": int64(2), "c": "xyz"},
+			pass:   false,
+		},
+
+		{
+			in:     "{a > 1, b > 1 or c = 'xyz'}",
+			fields: map[string]interface{}{"a": int64(2), "c": "xyz", "b": false},
+			pass:   true,
+		},
+
+		{
+			in:     "{xxx < 111}; {a > 1, b > 1 or c = 'xyz'}",
+			fields: map[string]interface{}{"a": int64(2), "c": "xyz", "b": false},
+			pass:   true,
+		},
+	}
+
+	for _, tc := range cases {
+		conditions := GetConds(tc.in)
+		tu.Assert(t, conditions != nil, "conditions should not nil")
+
+		tu.Equals(t, tc.pass, conditions.Eval(tc.source, tc.tags, tc.fields))
+
+		t.Logf("[ok] %s => %v, source: %s, tags: %+#v, fields: %+#v", tc.in, tc.pass, tc.source, tc.tags, tc.fields)
+	}
+}
+
+func TestConditions(t *testing.T) {
+	cases := []struct {
+		in     WhereConditions
+		source string
+		tags   map[string]string
+		fields map[string]interface{}
+		pass   bool
+	}{
+
+		{ // multi conditions
+			fields: map[string]interface{}{"a": int64(2), "c": "xyz"},
+			in: WhereConditions{
+				&WhereCondition{
+					conditions: []Node{
+						&BinaryExpr{
+							Op:  GT,
+							LHS: &Identifier{Name: "b"},
+							RHS: &NumberLiteral{IsInt: true, Int: int64(1)},
+						},
+					},
+				},
+
+				&WhereCondition{
+					conditions: []Node{
+						&BinaryExpr{
+							Op:  GT,
+							LHS: &Identifier{Name: "d"},
+							RHS: &NumberLiteral{IsInt: true, Int: int64(1)},
+						},
+					},
+				},
+			},
+			pass: false,
+		},
+
+		{
+			fields: map[string]interface{}{"a": int64(2)},
+			in: WhereConditions{
+				&WhereCondition{
+					conditions: []Node{
+						&BinaryExpr{
+							Op:  GT,
+							LHS: &Identifier{Name: "a"},
+							RHS: &NumberLiteral{IsInt: true, Int: int64(1)},
+						},
+					},
+				},
+			},
+			pass: true,
+		},
+
+		{
+			pass:   true,
+			fields: map[string]interface{}{"a": "abc"},
+			in: WhereConditions{
+				&WhereCondition{
+					conditions: []Node{
+						&BinaryExpr{
+							Op:  IN,
+							LHS: &Identifier{Name: "a"},
+							RHS: NodeList{
+								&StringLiteral{Val: "123"},
+								&StringLiteral{Val: "abc"},
+								&NumberLiteral{Float: 123.0},
+							},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			pass:   true,
+			source: "abc",
+			in: WhereConditions{
+				&WhereCondition{
+					conditions: []Node{
+						&BinaryExpr{
+							Op:  IN,
+							LHS: &Identifier{Name: "source"},
+							RHS: NodeList{
+								&StringLiteral{Val: "xyz"},
+								&StringLiteral{Val: "abc"},
+								&NumberLiteral{Float: 123.0},
+							},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			pass:   true,
+			source: "abc",
+			in: WhereConditions{
+				&WhereCondition{
+					conditions: []Node{
+						&BinaryExpr{
+							Op:  NEQ,
+							LHS: &Identifier{Name: "source"},
+							RHS: &StringLiteral{Val: "xyz"},
+						},
+					},
+				},
+			},
+		},
+
+		{
+			pass:   false,
+			source: "abc",
+			in: WhereConditions{
+				&WhereCondition{
+					conditions: []Node{
+						&BinaryExpr{
+							Op:  EQ,
+							LHS: &Identifier{Name: "source"},
+							RHS: &StringLiteral{Val: "xyz"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		tu.Equals(t, tc.pass, tc.in.Eval(tc.source, tc.tags, tc.fields))
+		t.Logf("[ok] %s => %v, source: %s, tags: %+#v, fields: %+#v", tc.in, tc.pass, tc.source, tc.tags, tc.fields)
+	}
+}
+
+func TestBinEval(t *testing.T) {
+	cases := []struct {
+		op   ItemType
+		lhs  interface{}
+		rhs  interface{}
+		pass bool
+	}{
+		{
+			op:   GT,
+			lhs:  int64(4),
+			rhs:  int64(3),
+			pass: true,
+		},
+
+		{
+			op:   GTE,
+			lhs:  int64(4),
+			rhs:  int64(3),
+			pass: true,
+		},
+
+		{
+			op:   GTE,
+			lhs:  int64(4),
+			rhs:  int64(4),
+			pass: true,
+		},
+
+		{
+			op:   EQ,
+			lhs:  int64(3),
+			rhs:  int64(3),
+			pass: true,
+		},
+
+		{
+			op:   EQ,
+			lhs:  "abc",
+			rhs:  "def",
+			pass: false,
+		},
+
+		{
+			op:   LT,
+			lhs:  "abc",
+			rhs:  "def",
+			pass: true,
+		},
+
+		{
+			op:   LT,
+			lhs:  "abc",
+			rhs:  123.4,
+			pass: false,
+		},
+
+		{
+			op:   LT,
+			lhs:  123.4,
+			rhs:  "abc",
+			pass: false,
+		},
+
+		{
+			op:   EQ,
+			lhs:  123.4,
+			rhs:  "abc",
+			pass: false,
+		},
+
+		{
+			op:   NEQ,
+			lhs:  123.4,
+			rhs:  "abc",
+			pass: false,
+		},
+
+		{
+			op:   IN,
+			lhs:  123.4,
+			rhs:  "abc",
+			pass: false,
+		},
+	}
+
+	for _, tc := range cases {
+		tu.Equals(t, tc.pass, binEval(tc.op, tc.lhs, tc.rhs))
+		t.Logf("[ok] %v %s %v => %v", tc.lhs, tc.op, tc.rhs, tc.pass)
+	}
+}
+
+func TestEval(t *testing.T) {
+	cases := []struct {
+		cond   *BinaryExpr
+		source string
+		tags   map[string]string
+		fields map[string]interface{}
+		pass   bool
+	}{
+		{
+			fields: map[string]interface{}{"a": int64(3)},
+			cond: &BinaryExpr{
+				Op:  GTE,
+				LHS: &Identifier{Name: "a"},
+				RHS: &NumberLiteral{IsInt: true, Int: int64(3)},
+			},
+			pass: true,
+		},
+
+		{
+			fields: map[string]interface{}{"a": float64(3.14)},
+			cond: &BinaryExpr{
+				Op:  GT,
+				LHS: &Identifier{Name: "a"},
+				RHS: &NumberLiteral{Float: float64(1.1)},
+			},
+			pass: true,
+		},
+
+		{
+			source: "abc",
+			cond: &BinaryExpr{
+				Op:  EQ,
+				LHS: &Identifier{Name: "source"},
+				RHS: &StringLiteral{Val: "abc"},
+			},
+			pass: true,
+		},
+
+		{
+			source: "abc",
+			cond: &BinaryExpr{
+				Op:  IN,
+				LHS: &Identifier{Name: "source"},
+				RHS: NodeList{
+					&StringLiteral{Val: "abc123"},
+					&NumberLiteral{Float: 3.14},
+					&StringLiteral{Val: "xyz"},
+				},
+			},
+			pass: false,
+		},
+
+		{
+			tags: map[string]string{"a": "xyz"},
+			cond: &BinaryExpr{
+				Op:  IN,
+				LHS: &Identifier{Name: "a"},
+				RHS: NodeList{
+					&StringLiteral{Val: "abc123"},
+					&NumberLiteral{Float: 3.14},
+					&StringLiteral{Val: "xyz"},
+				},
+			},
+			pass: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Logf("[ok] %s => %v", tc.cond, tc.pass)
+		tu.Equals(t, tc.pass, tc.cond.Eval(tc.source, tc.tags, tc.fields))
+	}
+}
