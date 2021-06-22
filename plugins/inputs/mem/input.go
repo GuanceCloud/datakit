4b6aa6b220ebe73d853b3336a0fd86603554b044
denglijun <denglijun@jiagouyun.com>
2021-06-22 10:50:36 +0800

    update: 整体调整 datakit 模块结构，削减顶层模块 	renamed:    cfg.go -> config/cfg.go 	renamed:    cfg_test.go -> config/cfg_test.go 	renamed:    dataway.go -> io/dataway/dataway.go 	renamed:    dataway_test.go -> io/dataway/dataway_test.go 	renamed:    service.go -> service/service.go
    Former-commit-id: 2d9ea7957956332e04f5210d3fbd12020fd1bbe2
    Former-commit-id: 843c8c7354a596e72ce4acefa0895e067f05dc91 [formerly 024e2a0a2bd56323475c069cf137b95ca39c3849]
    Former-commit-id: c4f52663305e3837c4388ea139723f6be5e3985e

--- plugins/inputs/mem/input.go
+++ plugins/inputs/mem/input.go
@@ -7,6 +7,7 @@ import (
 
 	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
@@ -168,7 +169,7 @@ func (i *Input) Collect() error {
 func (i *Input) Run() {
 	l = logger.SLogger(inputName)
 	l.Infof("memory input started")
-	i.Interval.Duration = datakit.ProtectedInterval(minInterval, maxInterval, i.Interval.Duration)
+	i.Interval.Duration = config.ProtectedInterval(minInterval, maxInterval, i.Interval.Duration)
 	tick := time.NewTicker(i.Interval.Duration)
 	defer tick.Stop()
 	for {
