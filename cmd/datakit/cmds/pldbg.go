29c88f7b22da51a12bc5c2b811b7ba9bd5d23c0d
tanbiao <tanbiao@jiagouyun.com>
2021-06-23 15:56:05 +0800

    update: 完善 DQL 支持
     [formerly bddc5a96d1fd6778e1985c731597d84b5882b8d3 [formerly 37553dbe09339a67aa8c5911003931925f5d35e6] [formerly c5c5c14e9ce872508504841a5eec680ff53034cf [formerly 391db7f3f1e8761927a3349e4500e63f1d5767e7]] [formerly 53993c2f8c97f96a40451676f328f1ae1b098cd5] [formerly 5c5bd614a74a8307ae278cc48b55b572d4b6a140 [formerly 695bcbfaa056bb22d58af17b70d4ee74423576fb] [formerly 5c5bd614a74a8307ae278cc48b55b572d4b6a140 [formerly 695bcbfaa056bb22d58af17b70d4ee74423576fb] [formerly d8d758b7503870a49abc71d3dcc26db5883ef961 [formerly dd4b0e503bdc7e56ef3886ebc98b1d08b9f2cf1c]]]] [formerly 6ba77b36a17a9cdb5f5e0866ba1be1db7b2f2b2f] [formerly e1cc3f3d999b6b8d6833b91298262aa39b0923b9 [formerly b43ab15d7904d4fa42ea42da66aab31e0a84b35b] [formerly 6669353fb99bd8131fc249dfed4ef2be49de7ece [formerly 097975297687a5894ee1b30cca7ee7867df1eadd] [formerly 6669353fb99bd8131fc249dfed4ef2be49de7ece [formerly 097975297687a5894ee1b30cca7ee7867df1eadd] [formerly 6669353fb99bd8131fc249dfed4ef2be49de7ece [formerly 097975297687a5894ee1b30cca7ee7867df1eadd] [formerly ce8a2e0d53f96c35790844707c82c268bb916a1b [formerly c581c2a81d0e579c09d8e3c35fd5d8c37dfeeec2]]]]]]]
     [formerly 6ba77b36a17a9cdb5f5e0866ba1be1db7b2f2b2f]
    
    
    
    
    Former-commit-id: 1ddecb21f2a95b28449d7ba0c7b5894313ab5972 [formerly 6b53a85e66be21d49cf5b6168277e8be0c197920]
    Former-commit-id: cb7deae8bd353cd8a6515cd383b82e5854a570fc [formerly 902dce3977c78ef98f0531319cd75911c84bade8] [formerly ba3010c43a690f182f53001b5e9b6c08a2622258 [formerly e9a52b3fac497048be67077c675ca6963e24c76e]]
    Former-commit-id: 3a407f0de597ffbd250dffa527684e31ab122eb5 [formerly fa6da4dbfce45e1f37033fe4b035ee0e52c10b8c]
    Former-commit-id: b0f39e9ec22824d180f6d5b14c7d01787da177ed

--- cmd/datakit/cmds/pldbg.go
+++ cmd/datakit/cmds/pldbg.go
@@ -6,13 +6,8 @@ import (
 	"path/filepath"
 	"time"
 
-	"gitlab.jiagouyun.com/cloudcare-tools/cliutils/logger"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/pipeline"
-)
-
-var (
-	l = logger.DefaultSLogger("cmds")
 )
 
 func PipelineDebugger(plname, txt string) {
