9b37f656c47c8a80236837ab46f8fc93b32b813d
tanbiao <tanbiao@jiagouyun.com>
2021-06-25 11:06:38 +0800

    update: 	- 修复配置不加载问题 	- 代码日志调整
     [formerly 55b4bc8a9a55999276e99ee34d61d22ae66849bc] [formerly e7f4144af12aba6e236b84aab02b492f463b5eb9 [formerly d4019011ffc8e74993e1ee1b8f1e803952b38453] [formerly 905b8d28704cdbaef87a6a905198d6d973129ca9 [formerly d8beae8a63f8b22ecda33d147aa2d9a8b53b3726] [formerly 905b8d28704cdbaef87a6a905198d6d973129ca9 [formerly d8beae8a63f8b22ecda33d147aa2d9a8b53b3726] [formerly 905b8d28704cdbaef87a6a905198d6d973129ca9 [formerly d8beae8a63f8b22ecda33d147aa2d9a8b53b3726] [formerly 0db7200d5f2cd5562e1b3d1bf29fa56b391441bf [formerly 3555568fd37b001de0c41349fb24241396da7dde]]]]]]
     [formerly 8893ff4a6fc7799783986cde76aedc683f847649 [formerly 0fb26cff0ce382f3f27a3862db0a44972a71f9c0] [formerly 8893ff4a6fc7799783986cde76aedc683f847649 [formerly 0fb26cff0ce382f3f27a3862db0a44972a71f9c0] [formerly 8893ff4a6fc7799783986cde76aedc683f847649 [formerly 0fb26cff0ce382f3f27a3862db0a44972a71f9c0] [formerly c0240d664a152801c494663c683ea56ef613aafb [formerly c2171dc62106d059e6c50668ab9f92391c974684]]]]]
    
    
    
    Former-commit-id: 707ea4f0b398d1871f86d3d14a16c15cfccec452 [formerly e259f581a91b6bd1bcd9ed80d09c6fef6a504b1b]
    Former-commit-id: 8a7f68a52e5d20ca5b09c2b72bc6f3ad4d340e12 [formerly 5ae96d1784481a16043acbd480dca8ed96ce9c1b] [formerly e33d919a7c8e9eccfe14cfb481aefa0fed394f11 [formerly 97be562dc6cc18a59e47a3ca605bed1e8a0a6d5c]]
    Former-commit-id: e830d4e1ba86b6bcedcfc9a9188d7af173d9e09d [formerly 0c16e7e8e9a6ee4c0b719f25e45738144cd7efe7]
    Former-commit-id: be14549073e5d03b838d831138de72c8fc044291

--- config/inputcfg.go
+++ config/inputcfg.go
@@ -28,12 +28,11 @@ func SearchDir(dir string, suffix string) []string {
 			return nil
 		}
 
-		if !strings.HasSuffix(f.Name(), suffix) {
-			return nil
+		if strings.HasSuffix(f.Name(), suffix) {
+			fps = append(fps, fp)
 		}
-
-		fps = append(fps, fp)
 		return nil
+
 	}); err != nil {
 		l.Error(err)
 	}
@@ -46,7 +45,12 @@ func LoadInputsConfig(c *Config) error {
 
 	availableInputCfgs := map[string]*ast.Table{}
 	confs := SearchDir(datakit.ConfdDir, ".conf")
+
+	l.Debugf("loading %d conf...", len(confs))
+
 	for _, fp := range confs {
+
+		l.Debugf("loading conf %s...", fp)
 
 		if filepath.Base(fp) == "datakit.conf" {
 			continue
@@ -54,8 +58,8 @@ func LoadInputsConfig(c *Config) error {
 
 		tbl, err := ParseCfgFile(fp)
 		if err != nil {
-			l.Warnf("[error] parse conf %s failed: %s, ignored", fp, err)
-			return nil
+			l.Warnf("parse conf %s failed: %s, ignored", fp, err)
+			continue
 		}
 
 		deprecates := checkDepercatedInputs(tbl, deprecatedInputs)
@@ -66,13 +70,12 @@ func LoadInputsConfig(c *Config) error {
 		}
 
 		if len(tbl.Fields) == 0 {
-			l.Debugf("no conf available on %s", fp)
-			return nil
+			l.Warnf("no conf available on %s", fp)
+			continue
 		}
 
 		l.Debugf("parse %s ok", fp)
 		availableInputCfgs[fp] = tbl
-		return nil
 	}
 
 	// reset inputs(for reloading)
@@ -81,7 +84,7 @@ func LoadInputsConfig(c *Config) error {
 
 	for name, creator := range inputs.Inputs {
 		if !datakit.Enabled(name) {
-			l.Debugf("LoadInputsConfig: ignore unchecked input %s", name)
+			l.Debugf("ignore unchecked input %s", name)
 			continue
 		}
 
@@ -237,7 +240,7 @@ func initDatakitConfSample(name string, c inputs.Creator) error {
 }
 
 // Creata datakit input plugin's configures if not exists
-func initPluginSamples() {
+func initPluginSamples() error {
 	for name, create := range inputs.Inputs {
 
 		if !datakit.Enabled(name) {
@@ -246,18 +249,23 @@ func initPluginSamples() {
 		}
 
 		if err := initDatakitConfSample(name, create); err != nil {
-			l.Fatal(err)
+			return err
 		}
 	}
+	return nil
 }
 
 func initDefaultEnabledPlugins(c *Config) {
 
 	if len(c.DefaultEnabledInputs) == 0 {
+		l.Debug("no default inputs enabled")
 		return
 	}
 
 	for _, name := range c.DefaultEnabledInputs {
+
+		l.Debugf("init default input %s conf...", name)
+
 		var fpath, sample string
 
 		if c, ok := inputs.Inputs[name]; ok {
