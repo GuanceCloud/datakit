629f5e4b991a8bdb4f94e50db7f174d05a106912
CodapeWild <codape.wild@gmail.com>
2021-06-25 16:02:30 +0800

    fix
     [formerly 976e3c0a21080e8e2c8eac6e236c358b7e2bf395 [formerly 212e5472d59e0788af65b56fc53b56090760bc1a] [formerly 976e3c0a21080e8e2c8eac6e236c358b7e2bf395 [formerly 212e5472d59e0788af65b56fc53b56090760bc1a] [formerly 976e3c0a21080e8e2c8eac6e236c358b7e2bf395 [formerly 212e5472d59e0788af65b56fc53b56090760bc1a] [formerly 4e7699950096456e2e3d9f103cd7f0f56e9425ea [formerly 9b7f01e43d205091469eeae444b6eab217328cfb]]]]]
    
    
    
    Former-commit-id: c02135a4c8cd606be11e3ec0a4f45970df2d223f [formerly 0d5e39f169c9fd7761b1de0b471e6170a71cb9a3]
    Former-commit-id: 0102fe8ce5bd164bd1ac003b7a397b58f53d197c [formerly 94b8e49b9039c5764d3b62d8d04c12b2a93581c1] [formerly 3092c6ac28d1ef4e4c7ccac869e805eecfca8e8f [formerly 69cbb3fe1a74eb950089e3f49f0268f674581539]]
    Former-commit-id: bd848b6f955f711bd14d7dab658792a47cfd58b6 [formerly b9d9b02571b8ff83a73ee1109ac3a4f8ac8e391e]
    Former-commit-id: 7b7c11905ef22fc3a7f351f1730108ef93d82315

---
+++ plugins/inputs/trace/sample.go
@@ -0,0 +1,31 @@
+package trace
+
+var (
+	DefSampleHandler = func(traceId uint64, rate, scope int) bool {
+		return (traceId % uint64(scope)) < uint64(rate)
+	}
+	ErrMapper = map[string]int32{
+		// STATUS_INFO:     1,
+		STATUS_WARN:     2,
+		STATUS_ERR:      3,
+		STATUS_CRITICAL: 4,
+	}
+	DefErrCheckHandler = func(err int32) bool {
+		return err == 0
+	}
+	DefIgnoreTagsHandler = func(source map[string]string, ignoreList []string) bool {
+		for _, v := range ignoreList {
+			if _, ok := source[v]; ok {
+				return true
+			}
+		}
+
+		return false
+	}
+)
+
+type TraceSampleConfig struct {
+	Rate           int      `toml:"rate"`
+	Scope          int      `toml:"scope"`
+	IgnoreTagsList []string `toml:ignore_tags_list`
+}
