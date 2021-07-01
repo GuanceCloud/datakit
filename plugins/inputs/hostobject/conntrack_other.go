eb7152d784bc8b455036c01e285a4a2a254ce470
zheng bo <zhengb@jiagouyun.com>
2021-07-01 11:45:15 +0800

    feat(hostobj): add filefd
    Former-commit-id: c6736c7ab3c8459605904b6f29626031f25384dd [formerly 6ca604fc98207203e262489ceec7dc54255e5281]
    Former-commit-id: eee8d909254335064929ec6fb0eac5103000dee4

--- plugins/inputs/hostobject/conntrack_other.go
+++ plugins/inputs/hostobject/conntrack_other.go
@@ -3,7 +3,18 @@
 package hostobject
 
 func ConntrackCollect() *ConntrackInfo {
-	info := &ConntrackInfo{}
+	info := &ConntrackInfo{
+		Current:       -1,
+		Limit:         -1,
+		Found:         -1,
+		Invalid:       -1,
+		Ignore:        -1,
+		Insert:        -1,
+		InsertFailed:  -1,
+		Drop:          -1,
+		EarlyDrop:     -1,
+		SearchRestart: -1,
+	}
 
 	return info
 }
