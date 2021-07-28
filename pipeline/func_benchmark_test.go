363868c975174d49dcea12d0e9a9d027c1b9280e
vircoys <vircoys@gmail.com>
2021-07-28 17:07:40 +0800

    优化部分 pattern ，使用非捕获组

--- pipeline/func_benchmark_test.go
+++ pipeline/func_benchmark_test.go
@@ -186,6 +186,26 @@ func BenchmarkParseLog_tz(b *testing.B) {
 
 }
 
+func BenchmarkGrok(b *testing.B) {
+	script := `	
+#grok(_, "%{IPV6:client_ip}")
+#grok(_, "%{IPV6:client_ip} %{NOTSPACE:http_ident} %{NOTSPACE:http_auth} \\[%{HTTPDATE:time}\\] \"%{DATA:http_method} %{URIPATHPARAM:http_url} HTTP/%{NUMBER:http_version}\" %{INT:status_code} %{INT:bytes}")
+grok(_, "%{IPORHOST:client_ip} %{NOTSPACE:http_ident} %{NOTSPACE:http_auth} \\[%{HTTPDATE:time}\\] \"%{DATA:http_method} %{GREEDYDATA:http_url} HTTP/%{NUMBER:http_version}\" %{INT:status_code} %{INT:bytes}")
+`
+	pl, err := NewPipeline(script)
+	if err != nil {
+		b.Error(err)
+	}
+	data := `127.0.0.1 - - [21/Jul/2021:14:14:38 +0800] "GET /?1 HTTP/1.1" 200 2178 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"`
+	// data := `fe80:d::127.0.0.1 - - [21/Jul/2021:14:14:38 +0800] "GET /?1 HTTP/1.1" 200 2178 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"`
+
+	for n := 0; n < b.N; n++ {
+		pl.Run(data)
+		// b.Error(pl.Output)
+	}
+
+}
+
 func BenchmarkParseLogNginx(b *testing.B) {
 	script := `
 add_pattern("date2", "%{YEAR}[./]%{MONTHNUM}[./]%{MONTHDAY} %{TIME}")
