86ef345cac816c66c8b56716c782783323a30c76
liguozhuang <liguozhuang@jiagouyun.com>
2021-09-01 17:18:51 +0800

    NSQ采集器完成

--- plugins/inputs/gitlab/input.go
+++ plugins/inputs/gitlab/input.go
@@ -13,6 +13,7 @@ import (
 
 const (
 	inputName = "gitlab"
+	catalog   = "gitlab"
 
 	sampleCfg = `
 [[inputs.gitlab]]
@@ -44,27 +45,17 @@ type Input struct {
 	httpClient *http.Client
 	duration   time.Duration
 
-	paused     bool
-	electionCh chan interface{}
+	pause   bool
+	pauseCh chan bool
 }
 
 func newInput() *Input {
 	return &Input{
-		Tags:     make(map[string]string),
-		duration: time.Second * 10,
-		httpClient: &http.Client{
-			Timeout: 5 * time.Second,
-		},
-		electionCh: make(chan interface{}),
+		Tags:       make(map[string]string),
+		pauseCh:    make(chan bool, 1),
+		duration:   time.Second * 10,
+		httpClient: &http.Client{Timeout: 5 * time.Second},
 	}
-}
-
-func (*Input) SampleConfig() string {
-	return sampleCfg
-}
-
-func (*Input) Catalog() string {
-	return "gitlab"
 }
 
 func (this *Input) Run() {
@@ -75,40 +66,30 @@ func (this *Input) Run() {
 	ticker := time.NewTicker(this.duration)
 	defer ticker.Stop()
 
-	defer close(this.electionCh)
-
 	for {
 		select {
 		case <-datakit.Exit.Wait():
 			l.Info("exit")
 			return
 
-		case x := <-this.electionCh:
-			if x == PAUSE {
-				this.paused = true
-			} else {
-				this.paused = false
-			}
-
 		case <-ticker.C:
-			if !this.paused {
-				this.gather()
+			if this.pause {
+				l.Debugf("not leader, skipped")
+				continue
 			}
+			this.gather()
+
+		case this.pause = <-this.pauseCh:
+			// nil
 		}
 	}
 }
 
-const (
-	PAUSE  = 0
-	RESUME = 1
-)
-
 func (this *Input) Pause() error {
-	tick := time.NewTicker(time.Second * 3)
+	tick := time.NewTicker(time.Second * 5)
 	defer tick.Stop()
-
 	select {
-	case this.electionCh <- PAUSE:
+	case this.pauseCh <- true:
 		return nil
 	case <-tick.C:
 		return fmt.Errorf("pause %s failed", inputName)
@@ -116,11 +97,10 @@ func (this *Input) Pause() error {
 }
 
 func (this *Input) Resume() error {
-	tick := time.NewTicker(time.Second * 3)
+	tick := time.NewTicker(time.Second * 5)
 	defer tick.Stop()
-
 	select {
-	case this.electionCh <- RESUME:
+	case this.pauseCh <- false:
 		return nil
 	case <-tick.C:
 		return fmt.Errorf("resume %s failed", inputName)
@@ -190,85 +170,16 @@ func (this *Input) gatherMetrics() ([]*io.Point, error) {
 	return points, nil
 }
 
-func (*Input) AvailableArchs() []string {
-	return datakit.AllArch
-}
+func (*Input) SampleConfig() string { return sampleCfg }
+
+func (*Input) Catalog() string { return catalog }
+
+func (*Input) AvailableArchs() []string { return datakit.AllArch }
 
 func (*Input) SampleMeasurement() []inputs.Measurement {
 	return []inputs.Measurement{
 		&gitlabMeasurement{},
 		&gitlabBaseMeasurement{},
 		&gitlabHTTPMeasurement{},
-	}
-}
-
-type gitlabMeasurement struct{}
-type gitlabBaseMeasurement struct{}
-type gitlabHTTPMeasurement struct{}
-
-func (*gitlabMeasurement) LineProto() (*io.Point, error)     { return nil, nil }
-func (*gitlabBaseMeasurement) LineProto() (*io.Point, error) { return nil, nil }
-func (*gitlabHTTPMeasurement) LineProto() (*io.Point, error) { return nil, nil }
-
-func (*gitlabMeasurement) Info() *inputs.MeasurementInfo {
-	return &inputs.MeasurementInfo{Name: "gitlab",
-		Desc: "GitLab 运行指标",
-		Tags: map[string]interface{}{
-			"action":           inputs.NewTagInfo("行为"),
-			"controller":       inputs.NewTagInfo("管理"),
-			"feature_category": inputs.NewTagInfo("类型特征"),
-			"storage":          inputs.NewTagInfo("存储"),
-		},
-		Fields: map[string]interface{}{
-			"transaction_cache_read_miss_count_total":             &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.NCount, Desc: "The counter for cache misses for Rails cache calls"},
-			"transaction_cache_read_hit_count_total":              &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.NCount, Desc: "The counter for cache hits for Rails cache calls"},
-			"transaction_db_count_total":                          &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.NCount, Desc: "The counter for db"},
-			"transaction_db_cached_count_total":                   &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.NCount, Desc: "The counter for db cache"},
-			"rack_requests_total":                                 &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.UnknownUnit, Desc: "The rack request count"},
-			"cache_operations_total":                              &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.UnknownUnit, Desc: "The count of cache access time"},
-			"cache_operation_duration_seconds_count":              &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The count of cache access time"},
-			"cache_operation_duration_seconds_sum":                &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The count of cache access time"},
-			"transaction_view_duration_total":                     &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.UnknownUnit, Desc: "The duration for views"},
-			"transaction_new_redis_connections_total":             &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.UnknownUnit, Desc: "The counter for new Redis connections"},
-			"sql_duration_seconds_count":                          &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The total SQL execution time, excluding SCHEMA operations and BEGIN / COMMIT"},
-			"sql_duration_seconds_sum":                            &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The sum of SQL execution time, excluding SCHEMA operations and BEGIN / COMMIT"},
-			"transaction_duration_seconds_count":                  &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The count of duration for all transactions (gitlab_transaction_* metrics)"},
-			"transaction_duration_seconds_sum":                    &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The sum of duration for all transactions (gitlab_transaction_* metrics)"},
-			"banzai_cacheless_render_real_duration_seconds_count": &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The count of duration of rendering Markdown into HTML when cached output exists"},
-			"banzai_cacheless_render_real_duration_seconds_sum":   &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The sum of duration of rendering Markdown into HTML when cached output exists"},
-			"cache_misses_total":                                  &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.UnknownUnit, Desc: "The cache read miss count"},
-			"redis_client_requests_total":                         &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.UnknownUnit, Desc: "Number of Redis client requests"},
-			"redis_client_requests_duration_seconds_count":        &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The count of redis request latency, excluding blocking commands"},
-			"redis_client_requests_duration_seconds_sum":          &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The sum of redis request latency, excluding blocking commands"},
-		},
-	}
-}
-
-func (*gitlabBaseMeasurement) Info() *inputs.MeasurementInfo {
-	return &inputs.MeasurementInfo{Name: "gitlab_base",
-		Desc: "GitLab 编程语言层面指标",
-		Tags: nil,
-		Fields: map[string]interface{}{
-			"ruby_sampler_duration_seconds_total": &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The time spent collecting stats"},
-			"ruby_gc_duration_seconds_sum":        &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The sum of time spent by Ruby in GC"},
-			"ruby_gc_duration_seconds_count":      &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The count of time spent by Ruby in GC"},
-			"rails_queue_duration_seconds_sum":    &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The sum for latency between GitLab Workhorse forwarding a request to Rails"},
-			"rails_queue_duration_seconds_count":  &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The counter for latency between GitLab Workhorse forwarding a request to Rails"},
-		},
-	}
-}
-
-func (*gitlabHTTPMeasurement) Info() *inputs.MeasurementInfo {
-	return &inputs.MeasurementInfo{Name: "gitlab_http",
-		Desc: "GitLab HTTP 相关指标",
-		Tags: map[string]interface{}{
-			"method": inputs.NewTagInfo("方法"),
-			"status": inputs.NewTagInfo("状态码"),
-		},
-		Fields: map[string]interface{}{
-			"http_request_duration_seconds_count": &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The counter for request duration"},
-			"http_request_duration_seconds_sum":   &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.DurationSecond, Desc: "The sum for request duration"},
-			"http_health_requests_total":          &inputs.FieldInfo{DataType: inputs.Float, Unit: inputs.UnknownUnit, Desc: "Number of health requests"},
-		},
 	}
 }
---
+++ plugins/inputs/nsq/input_test.go
@@ -0,0 +1,33 @@
+package nsq
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+	cases := []struct {
+		body string
+	}{
+		{
+			`{"version":"1.2.0","health":"OK","start_time":1630393108,"topics":[{"topic_name":"df-billing","channels":[{"channel_name":"df-billing-channel","depth":0,"backend_depth":0,"in_flight_count":0,"deferred_count":0,"message_count":0,"requeue_count":0,"timeout_count":0,"client_count":0,"clients":[],"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}}],"depth":0,"backend_depth":0,"message_count":0,"message_bytes":0,"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}},{"topic_name":"df-calculate-metering","channels":[{"channel_name":"df-calculate-metering-channel","depth":0,"backend_depth":0,"in_flight_count":0,"deferred_count":0,"message_count":0,"requeue_count":0,"timeout_count":0,"client_count":0,"clients":[],"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}}],"depth":0,"backend_depth":0,"message_count":0,"message_bytes":0,"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}},{"topic_name":"df-trigger-metering","channels":[{"channel_name":"df-trigger-metering-channel","depth":0,"backend_depth":0,"in_flight_count":0,"deferred_count":0,"message_count":0,"requeue_count":0,"timeout_count":0,"client_count":0,"clients":[],"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}}],"depth":0,"backend_depth":0,"message_count":0,"message_bytes":0,"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}},{"topic_name":"influx-data","channels":[{"channel_name":"influx-data-channel","depth":0,"backend_depth":0,"in_flight_count":0,"deferred_count":0,"message_count":0,"requeue_count":0,"timeout_count":0,"client_count":0,"clients":[],"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}}],"depth":0,"backend_depth":0,"message_count":0,"message_bytes":0,"paused":false,"e2e_processing_latency":{"count":0,"percentiles":null}}],"memory":{"heap_objects":5781,"heap_idle_bytes":63447040,"heap_in_use_bytes":2842624,"heap_released_bytes":0,"gc_pause_usec_100":0,"gc_pause_usec_99":0,"gc_pause_usec_95":0,"next_gc_bytes":4473924,"gc_total_runs":0},"producers":[]}`,
+		},
+	}
+
+	for _, tc := range cases {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, tc.body)
+		}))
+
+		pts, err := newInput().gatherEndpoint(ts.URL)
+		assert.NoError(t, err)
+
+		for _, pt := range pts {
+			t.Log(pt.String())
+		}
+	}
+}
