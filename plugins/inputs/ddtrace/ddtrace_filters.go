927a659bf52d6a741df0a1aa3a3fdc27205d10fc
CodapeWild <codape.wild@gmail.com>
2021-09-15 17:14:37 +0800

    fix

--- plugins/inputs/ddtrace/ddtrace_filters.go
+++ plugins/inputs/ddtrace/ddtrace_filters.go
@@ -1,57 +1,80 @@
 package ddtrace
 
-type traceFilter func(Trace) Trace
+import (
+	"time"
 
-func filterOutResource(trace Trace) Trace {
-	for i := range trace {
-		for k := range ignoreResources {
-			if ignoreResources[k].MatchString(trace[i].Resource) {
-				return nil
-			}
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/hashcode"
+	itrace "gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs/trace"
+)
+
+// traceFilter will determine whether a trace should be drop or not,
+// return nil Trace if the trace data dropped,
+// return true will tell runFilterWithBreak to break out loop early
+type traceFilter func(Trace) (Trace, bool)
+
+func runFiltersWithBreak(trace Trace, filters ...traceFilter) Trace {
+	if len(trace) == 0 {
+		log.Debug("empty trace")
+
+		return nil
+	}
+
+	var abort bool
+	for _, filter := range filters {
+		if trace, abort = filter(trace); abort || trace == nil {
+			return trace
 		}
 	}
 
 	return trace
 }
 
-func sample(trace Trace) Trace {
-	if len(trace) == 0 {
-		return nil
+var present = make(map[string]time.Time)
+
+// service resource env
+func rare(trace Trace) (Trace, bool) {
+	var rootSpan *Span
+	for i := range trace {
+		if trace[i].ParentID == 0 {
+			rootSpan = trace[i]
+			break
+		}
+	}
+	if rootSpan == nil {
+		return trace, false
 	}
 
+	checksum := hashcode.GenMapHash(map[string]string{"service": rootSpan.Service, "resource": rootSpan.Resource, "env": rootSpan.Meta[itrace.ENV]})
+	if then, ok := present[checksum]; !ok || time.Since(then) >= time.Hour {
+		present[checksum] = time.Now()
+
+		return trace, true
+	}
+
+	return trace, false
+}
+
+func checkResource(trace Trace) (Trace, bool) {
+	for i := range trace {
+		for k := range ignoreResources {
+			if ignoreResources[k].MatchString(trace[i].Resource) {
+				return nil, false
+			}
+		}
+	}
+
+	return trace, false
+}
+
+func sample(trace Trace) (Trace, bool) {
 	for i := range trace {
 		if trace[i].ParentID == 0 {
 			if priority, ok := trace[i].Metrics["_sampling_priority_v1"]; ok && priority < 1 {
-				return nil
+				return nil, false
 			}
 			break
 		}
 	}
 
-	return trace
-
-	// for i := range trace {
-	// 	// bypass error
-	// 	var status string = itrace.STATUS_OK
-	// 	if trace[i].Error != 0 {
-	// 		status = itrace.STATUS_ERR
-	// 	}
-	// 	if itrace.SampleIgnoreErrStatus(status) {
-	// 		return trace
-	// 	}
-	// 	// bypass ingnore keys
-	// 	if itrace.SampleIgnoreKeys(trace[i].Meta, sampleConf.IgnoreTagsList) {
-	// 		return trace
-	// 	}
-	// 	// bypass tags
-	// 	if itrace.SampleIgnoreTags(itrace.MergeTags(trace[i].Meta, ddTags), defIgnoreTags) {
-	// 		return trace
-	// 	}
-	// }
-	// // do sample
-	// if itrace.DefSampleFunc(trace[0].TraceID, sampleConf.Rate, sampleConf.Scope) {
-	// 	return trace
-	// } else {
-	// 	return nil
-	// }
+	return trace, false
 }
