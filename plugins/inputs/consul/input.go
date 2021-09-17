fac65213c8169068781051bbac9d35232767e0bc
derrickball <971893141@qq.com>
2021-09-17 21:54:22 +0800

    commit

--- plugins/inputs/consul/input.go
+++ plugins/inputs/consul/input.go
@@ -8,8 +8,43 @@ import (
 const (
 	inputName = "consul"
 	configSample = `
-[[inputs.mysql]]
-	pipeline = "consul.p"
+[[inputs.prom]]
+  ## Exporter 地址
+  url = "http://127.0.0.1:9107/metrics"
+
+  ## 采集器别名
+  source = "consul"
+
+  ## 指标类型过滤, 可选值为 counter, gauge, histogram, summary
+  # 默认只采集 counter 和 gauge 类型的指标
+  # 如果为空，则不进行过滤
+  metric_types = ["counter", "gauge"]
+
+  ## 指标名称过滤
+  # 支持正则，可以配置多个，即满足其中之一即可
+  # 如果为空，则不进行过滤
+    metric_name_filter = ["consul_raft_leader", "consul_raft_peers", "consul_serf_lan_members", "consul_catalog_service", "consul_catalog_service_node_healthy", "consul_health_node_status", "consul_serf_lan_member_status"]
+
+
+  ## 指标集名称前缀
+  # 配置此项，可以给指标集名称添加前缀
+  measurement_prefix = ""
+
+  ## 过滤tags, 可配置多个tag
+  # 匹配的tag将被忽略
+  tags_ignore = ["check"]
+
+  ## 采集间隔 "ns", "us" (or "µs"), "ms", "s", "m", "h"
+  interval = "10s"
+
+  pipeline = "consul.p"
+
+  ## 自定义指标集名称
+  # 可以将包含前缀prefix的指标归为一类指标集
+  # 自定义指标集名称配置优先measurement_name配置项
+  [[inputs.prom.measurements]]
+  	prefix = "consul_"
+	name = "consul"
 `
 	pipelineCfg = `
 add_pattern("_clog_date", "%{YEAR}-%{MONTHNUM}-%{MONTHDAY}T%{HOUR}:%{MINUTE}:%{SECOND}%{INT}")
@@ -24,6 +59,7 @@ drop_origin_data()
 type Input struct {
 
 }
+
 
 func (i *Input) Catalog() string {
 	return "consul"
@@ -42,7 +78,12 @@ func (i *Input) AvailableArchs() []string {
 }
 
 func (i *Input) SampleMeasurement() []inputs.Measurement {
-	return []inputs.Measurement{}
+	return []inputs.Measurement{
+		&HostMeasurement{},
+		&ServiceMeasurement{},
+		&HealthMeasurement{},
+		&MemberMeasurement{},
+	}
 }
 
 func (i *Input) PipelineConfig() map[string]string {
