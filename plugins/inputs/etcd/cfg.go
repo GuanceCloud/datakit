38e81c3937430a7f264fabb13a01cd75c369cf44
wanglei03 <390-wanglei03@users.noreply.gitlab.jiagouyun.com>
2021-10-11 09:32:35 +0800

    Iss 377and378

--- plugins/inputs/etcd/cfg.go
+++ plugins/inputs/etcd/cfg.go
@@ -16,6 +16,11 @@ var (
 	sample      = ``
 )
 
+type (
+	ServerMeasurement  Measurement
+	NetworkMeasurement Measurement
+)
+
 type Measurement struct {
 	name   string
 	tags   map[string]string
@@ -23,24 +28,41 @@ type Measurement struct {
 	ts     time.Time
 }
 
-func (m *Measurement) LineProto() (*io.Point, error) {
+func (m *ServerMeasurement) LineProto() (*io.Point, error) {
 	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
 }
 
-func (m *Measurement) Info() *inputs.MeasurementInfo {
+func (m *NetworkMeasurement) LineProto() (*io.Point, error) {
+	return io.MakePoint(m.name, m.tags, m.fields, m.ts)
+}
+
+func (m *NetworkMeasurement) Info() *inputs.MeasurementInfo {
 	return &inputs.MeasurementInfo{
 		Name: inputName,
 		Fields: map[string]interface{}{
-			"server_has_leader":                        newCountFieldInfo("领导者是否存在。1是存在，0不是"),
-			"server_leader_changes_seen_total":         newCountFieldInfo("解释到的领导者变更次数"),
-			"server_proposals_committed_total":         newCountFieldInfo("提交的共识提案总数"),
-			"server_proposals_applied_total":           newCountFieldInfo("已应用的共识提案总数"),
-			"server_proposals_pending":                 newCountFieldInfo("当前待处理提案的数量"),
-			"server_proposals_failed_total":            newCountFieldInfo("看到的失败提案总数"),
 			"network_client_grpc_sent_bytes_total":     newCountFieldInfo("发送到 grpc 客户端的总字节数"),
 			"network_client_grpc_received_bytes_total": newCountFieldInfo("接收到 grpc 客户端的总字节数"),
 		},
-		Tags: map[string]interface{}{},
+		Tags: map[string]interface{}{
+			"host": inputs.NewTagInfo("主机名称"),
+		},
+	}
+}
+
+func (m *ServerMeasurement) Info() *inputs.MeasurementInfo {
+	return &inputs.MeasurementInfo{
+		Name: inputName,
+		Fields: map[string]interface{}{
+			"server_leader_changes_seen_total": newCountFieldInfo("解释到的领导者变更次数"),
+			"server_proposals_committed_total": newCountFieldInfo("提交的共识提案总数"),
+			"server_proposals_applied_total":   newCountFieldInfo("已应用的共识提案总数"),
+			"server_proposals_pending":         newCountFieldInfo("当前待处理提案的数量"),
+			"server_proposals_failed_total":    newCountFieldInfo("看到的失败提案总数"),
+		},
+		Tags: map[string]interface{}{
+			"host":              inputs.NewTagInfo("主机名称"),
+			"server_has_leader": inputs.NewTagInfo("领导者是否存在。1是存在。0是不存在"),
+		},
 	}
 }
 
