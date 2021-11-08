0f805e0f80881a2dedae61a55b97718c43ffb706
wanchuan <wanchuan853@jiagouyun.com>
2021-11-08 13:41:06 +0800

    GitHasEnabled

--- gitrepo/gitrepo.go
+++ gitrepo/gitrepo.go
@@ -41,18 +41,6 @@ const (
 
 func StartPull() error {
 	runGit.Do(func() {
-		hasEnable := false
-		for _, v := range config.Cfg.GitRepos.Repos {
-			if v.Enable {
-				hasEnable = true
-				break
-			}
-		}
-
-		if !hasEnable {
-			return
-		}
-
 		l = logger.SLogger("gitrepo")
 		g := datakit.G("gitrepo")
 
