f383c0af25f5cb39d2fcb8c11e32efbc221f473a
郑波 <zhengb@jiagouyun.com>
2021-11-29 10:37:33 +0800

    Testing upload log #405

--- cmd/datakit/cmds/upload_log_test.go
+++ cmd/datakit/cmds/upload_log_test.go
@@ -2,24 +2,45 @@ package cmds
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/config"
 )
 
 func TestGetLogFile(t *testing.T) {
-	_, err := getLogFile()
+	logFile, err := ioutil.TempFile(os.TempDir(), "log")
+	assert.NoError(t, err, "create temp log file failed")
+	defer os.Remove(logFile.Name()) //nolint: errcheck
+	config.Cfg.Logging.Log = logFile.Name()
+	logFileName, err := getLogFile()
 	assert.NoError(t, err)
+	assert.Contains(t, logFileName, os.TempDir())
+	os.Remove(logFileName) //nolint: errcheck
 }
 
 func TestUploadLog(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "ok")
+		fmt.Fprint(w, "{\"msg\": \"ok\"}")
 	}))
-
 	defer ts.Close()
-	err := uploadLog([]string{ts.URL})
+
+	logFile, err := ioutil.TempFile(os.TempDir(), "log")
+	assert.NoError(t, err, "create temp log file failed")
+	defer os.Remove(logFile.Name()) //nolint: errcheck
+	config.Cfg.Logging.Log = logFile.Name()
+
+	err = uploadLog([]string{ts.URL})
 	assert.NoError(t, err)
+
+	err = uploadLog([]string{})
+	assert.Error(t, err)
+
+	config.Cfg.Logging.Log = ""
+	err = uploadLog([]string{ts.URL})
+	assert.Error(t, err, "should be an error, as log file name is empty")
 }
