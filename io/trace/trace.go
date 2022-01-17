3a7d79e57ff298289c6f5d950f977fe0e5d34dcd
CodapeWild <codape.wild@gmail.com>
2022-01-17 15:25:23 +0800

    fix

--- io/trace/trace.go
+++ io/trace/trace.go
@@ -116,62 +116,62 @@ func FindStringIDSpanType(spanID, parentID string, spanIDs, parentIDs map[string
 	return SPAN_TYPE_ENTRY
 }
 
-func BuildLineProto(tAdapter *DatakitSpan) (*dkio.Point, error) {
+func BuildLineProto(dkspan *DatakitSpan) (*dkio.Point, error) {
 	var (
 		tags   = make(map[string]string)
 		fields = make(map[string]interface{})
 	)
 
-	tags[TAG_PROJECT] = tAdapter.Project
-	tags[TAG_OPERATION] = tAdapter.Operation
-	tags[TAG_SERVICE] = tAdapter.Service
-	tags[TAG_VERSION] = tAdapter.Version
-	tags[TAG_ENV] = tAdapter.Env
-	tags[TAG_HTTP_METHOD] = tAdapter.HTTPMethod
-	tags[TAG_HTTP_CODE] = tAdapter.HTTPStatusCode
+	tags[TAG_PROJECT] = dkspan.Project
+	tags[TAG_OPERATION] = dkspan.Operation
+	tags[TAG_SERVICE] = dkspan.Service
+	tags[TAG_VERSION] = dkspan.Version
+	tags[TAG_ENV] = dkspan.Env
+	tags[TAG_HTTP_METHOD] = dkspan.HTTPMethod
+	tags[TAG_HTTP_CODE] = dkspan.HTTPStatusCode
 
-	if tAdapter.Type != "" {
-		tags[TAG_TYPE] = tAdapter.Type
+	if dkspan.Type != "" {
+		tags[TAG_TYPE] = dkspan.Type
 	} else {
 		tags[TAG_TYPE] = SPAN_SERVICE_CUSTOM
 	}
 
-	for k, v := range tAdapter.Tags {
+	for k, v := range dkspan.Tags {
 		tags[k] = v
 	}
 
-	tags[TAG_SPAN_STATUS] = tAdapter.Status
+	tags[TAG_SPAN_STATUS] = dkspan.Status
 
-	if tAdapter.EndPoint != "" {
-		tags[TAG_ENDPOINT] = tAdapter.EndPoint
+	if dkspan.EndPoint != "" {
+		tags[TAG_ENDPOINT] = dkspan.EndPoint
 	} else {
 		tags[TAG_ENDPOINT] = "null"
 	}
 
-	if tAdapter.SpanType != "" {
-		tags[TAG_SPAN_TYPE] = tAdapter.SpanType
+	if dkspan.SpanType != "" {
+		tags[TAG_SPAN_TYPE] = dkspan.SpanType
 	} else {
 		tags[TAG_SPAN_TYPE] = SPAN_TYPE_ENTRY
 	}
 
-	if tAdapter.ContainerHost != "" {
-		tags[TAG_CONTAINER_HOST] = tAdapter.ContainerHost
+	if dkspan.ContainerHost != "" {
+		tags[TAG_CONTAINER_HOST] = dkspan.ContainerHost
 	}
 
-	if tAdapter.ParentID == "" {
-		tAdapter.ParentID = "0"
+	if dkspan.ParentID == "" {
+		dkspan.ParentID = "0"
 	}
 
-	fields[FIELD_DURATION] = tAdapter.Duration / int64(time.Microsecond)
-	fields[FIELD_START] = tAdapter.Start / int64(time.Microsecond)
-	fields[FIELD_MSG] = tAdapter.Content
-	fields[FIELD_RESOURCE] = tAdapter.Resource
-	fields[FIELD_PARENTID] = tAdapter.ParentID
-	fields[FIELD_TRACEID] = tAdapter.TraceID
-	fields[FIELD_SPANID] = tAdapter.SpanID
+	fields[FIELD_START] = dkspan.Start / int64(time.Microsecond)
+	fields[FIELD_DURATION] = dkspan.Duration / int64(time.Microsecond)
+	fields[FIELD_MSG] = dkspan.Content
+	fields[FIELD_RESOURCE] = dkspan.Resource
+	fields[FIELD_PARENTID] = dkspan.ParentID
+	fields[FIELD_TRACEID] = dkspan.TraceID
+	fields[FIELD_SPANID] = dkspan.SpanID
 
-	ts := time.Unix(tAdapter.Start/int64(time.Second), tAdapter.Start%int64(time.Second))
-	pt, err := dkio.MakePoint(tAdapter.Source, tags, fields, ts)
+	ts := time.Unix(dkspan.Start/int64(time.Second), dkspan.Start%int64(time.Second))
+	pt, err := dkio.MakePoint(dkspan.Source, tags, fields, ts)
 	if err != nil {
 		log.Errorf("build metric err: %s", err)
 	}
