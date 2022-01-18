8f93df4132be8856ffb4328809c5fdc9db83a0c6
CodapeWild <codape.wild@gmail.com>
2022-01-18 13:36:53 +0800

    amend TraceMeasurement

--- io/trace/trace_meausrement.go
+++ io/trace/trace_meausrement.go
@@ -4,7 +4,7 @@ package trace
 import (
 	"time"
 
-	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
+	dkio "gitlab.jiagouyun.com/cloudcare-tools/datakit/io"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/plugins/inputs"
 )
 
@@ -15,26 +15,14 @@ type TraceMeasurement struct {
 	TS     time.Time
 }
 
-func (t *TraceMeasurement) LineProto() (*io.Point, error) {
-	data, err := io.MakePoint(t.Name, t.Tags, t.Fields, t.TS)
-
-	return data, err
+func (tm *TraceMeasurement) LineProto() (*dkio.Point, error) {
+	return dkio.MakePoint(tm.Name, tm.Tags, tm.Fields, tm.TS)
 }
 
-func (t *TraceMeasurement) Info() *inputs.MeasurementInfo {
+func (tm *TraceMeasurement) Info() *inputs.MeasurementInfo {
 	return &inputs.MeasurementInfo{
-		Name: "",
+		Name: tm.Name,
 		Type: "tracing",
-		Fields: map[string]interface{}{
-			FIELD_PARENTID: &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The parent id of current span."},
-			FIELD_TRACEID:  &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "Trace id."},
-			FIELD_SPANID:   &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "Span id."},
-			FIELD_DURATION: &inputs.FieldInfo{DataType: inputs.Int, Type: inputs.Gauge, Unit: inputs.DurationUS, Desc: "Duration of span."},
-			FIELD_START:    &inputs.FieldInfo{DataType: inputs.Int, Type: inputs.Gauge, Unit: inputs.TimestampUS, Desc: "start time of span."},
-			FIELD_MSG:      &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The origin content of span."},
-			FIELD_RESOURCE: &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The resource name."},
-			FIELD_PID:      &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The application process id."},
-		},
 		Tags: map[string]interface{}{
 			TAG_PROJECT:        &inputs.TagInfo{Desc: "project name"},
 			TAG_OPERATION:      &inputs.TagInfo{Desc: "span name"},
@@ -48,6 +36,16 @@ func (t *TraceMeasurement) Info() *inputs.MeasurementInfo {
 			TAG_SPAN_STATUS:    &inputs.TagInfo{Desc: "span status"},
 			TAG_SPAN_TYPE:      &inputs.TagInfo{Desc: "span type"},
 			TAG_CONTAINER_HOST: &inputs.TagInfo{Desc: "container hostname"},
+		},
+		Fields: map[string]interface{}{
+			FIELD_TRACEID:  &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "Trace ID."},
+			FIELD_PARENTID: &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The parent span ID of current span."},
+			FIELD_SPANID:   &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "Span ID."},
+			FIELD_START:    &inputs.FieldInfo{DataType: inputs.Int, Type: inputs.Gauge, Unit: inputs.TimestampUS, Desc: "The point of start time of span."},
+			FIELD_DURATION: &inputs.FieldInfo{DataType: inputs.Int, Type: inputs.Gauge, Unit: inputs.DurationUS, Desc: "Duration of span."},
+			FIELD_MSG:      &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The origin content of span."},
+			FIELD_RESOURCE: &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The resource name."},
+			FIELD_PID:      &inputs.FieldInfo{DataType: inputs.String, Unit: inputs.UnknownUnit, Desc: "The application process id."},
 		},
 	}
 }
