4055b0672ada5f7380da7435a18930cab1841fb4
CodapeWild <codape.wild@gmail.com>
2022-02-08 11:59:00 +0800

    fix

--- io/trace/aftergather.go
+++ io/trace/aftergather.go
@@ -98,30 +98,26 @@ func BuildPointsBatch(inputName string, dktrace DatakitTrace, strict bool) []*dk
 
 // BuildPoint builds point from DatakitSpan.
 func BuildPoint(dkspan *DatakitSpan, strict bool) (*dkio.Point, error) {
-	var (
-		tags   = make(map[string]string)
-		fields = make(map[string]interface{})
-	)
+	tags := map[string]string{
+		TAG_PROJECT:     dkspan.Project,
+		TAG_OPERATION:   dkspan.Operation,
+		TAG_SERVICE:     dkspan.Service,
+		TAG_VERSION:     dkspan.Version,
+		TAG_ENV:         dkspan.Env,
+		TAG_HTTP_METHOD: dkspan.HTTPMethod,
+		TAG_HTTP_CODE:   dkspan.HTTPStatusCode,
+		TAG_SPAN_STATUS: dkspan.Status,
+	}
 
-	tags[TAG_PROJECT] = dkspan.Project
-	tags[TAG_OPERATION] = dkspan.Operation
-	tags[TAG_SERVICE] = dkspan.Service
-	tags[TAG_VERSION] = dkspan.Version
-	tags[TAG_ENV] = dkspan.Env
-	tags[TAG_HTTP_METHOD] = dkspan.HTTPMethod
-	tags[TAG_HTTP_CODE] = dkspan.HTTPStatusCode
+	if dkspan.ParentID == "" {
+		dkspan.ParentID = "0"
+	}
 
 	if dkspan.SourceType != "" {
 		tags[TAG_TYPE] = dkspan.SourceType
 	} else {
 		tags[TAG_TYPE] = SPAN_SERVICE_CUSTOM
 	}
-
-	for k, v := range dkspan.Tags {
-		tags[k] = v
-	}
-
-	tags[TAG_SPAN_STATUS] = dkspan.Status
 
 	if dkspan.EndPoint != "" {
 		tags[TAG_ENDPOINT] = dkspan.EndPoint
@@ -139,17 +135,19 @@ func BuildPoint(dkspan *DatakitSpan, strict bool) (*dkio.Point, error) {
 		tags[TAG_CONTAINER_HOST] = dkspan.ContainerHost
 	}
 
-	if dkspan.ParentID == "" {
-		dkspan.ParentID = "0"
+	for k, v := range dkspan.Tags {
+		tags[k] = v
 	}
 
-	fields[FIELD_START] = dkspan.Start / int64(time.Microsecond)
-	fields[FIELD_DURATION] = dkspan.Duration / int64(time.Microsecond)
-	fields[FIELD_MSG] = dkspan.Content
-	fields[FIELD_RESOURCE] = dkspan.Resource
-	fields[FIELD_PARENTID] = dkspan.ParentID
-	fields[FIELD_TRACEID] = dkspan.TraceID
-	fields[FIELD_SPANID] = dkspan.SpanID
+	fields := map[string]interface{}{
+		FIELD_START:    dkspan.Start / int64(time.Microsecond),
+		FIELD_DURATION: dkspan.Duration / int64(time.Microsecond),
+		FIELD_MSG:      dkspan.Content,
+		FIELD_RESOURCE: dkspan.Resource,
+		FIELD_PARENTID: dkspan.ParentID,
+		FIELD_TRACEID:  dkspan.TraceID,
+		FIELD_SPANID:   dkspan.SpanID,
+	}
 
 	return dkio.NewPoint(dkspan.Source, tags, fields, &dkio.PointOption{
 		Time:     time.Unix(0, dkspan.Start),
