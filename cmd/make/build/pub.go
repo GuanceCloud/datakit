fe290d18e1c9c14833d1c873de7a4377e08dfd8b
pengyonghui <397-pengyonghui@users.noreply.gitlab.jiagouyun.com>
2022-02-15 17:19:41 +0800

    Ppl doc

--- cmd/make/build/pub.go
+++ cmd/make/build/pub.go
@@ -102,9 +102,28 @@ func generateMetaInfo() error {
 
 func generatePipelineDoc() error {
 	encoding := base64.StdEncoding
+	protoPrefix, descPrefix := "函数原型：", "函数说明："
+	// Write function description & prototype.
+	for _, plDoc := range funcs.PipelineFunctionDocs {
+		lines := strings.Split(plDoc.Doc, "\n")
+		for _, line := range lines {
+			if strings.HasPrefix(line, protoPrefix) {
+				proto := strings.TrimPrefix(line, protoPrefix)
+				// Prototype line contains starting and trailing ` only.
+				if strings.Index(proto, "`") == 0 && strings.Index(proto[1:], "`") == len(proto[1:])-1 {
+					proto = proto[1 : len(proto)-1]
+				}
+				plDoc.Prototype = proto
+			} else if strings.HasPrefix(line, descPrefix) {
+				plDoc.Description = strings.TrimPrefix(line, descPrefix)
+			}
+		}
+	}
 	// Encode Markdown docs with base64.
 	for _, plDoc := range funcs.PipelineFunctionDocs {
 		plDoc.Doc = encoding.EncodeToString([]byte(plDoc.Doc))
+		plDoc.Prototype = encoding.EncodeToString([]byte(plDoc.Prototype))
+		plDoc.Description = encoding.EncodeToString([]byte(plDoc.Description))
 	}
 	exportPLDocs := struct {
 		Version   string                  `json:"version"`
@@ -112,7 +131,7 @@ func generatePipelineDoc() error {
 		Functions map[string]*funcs.PLDoc `json:"functions"`
 	}{
 		Version:   git.Version,
-		Docs:      "经过 base64 编码的 pipeline 函数文档，包括各函数原型、说明、使用示例",
+		Docs:      "经过 base64 编码的 pipeline 函数文档，包括各函数原型、函数说明、使用示例",
 		Functions: funcs.PipelineFunctionDocs,
 	}
 	data, err := json.Marshal(exportPLDocs)
