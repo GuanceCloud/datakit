ce2fbfbd5940fb5c052ba2b20f92a826c8fc3231
CodapeWild <codape.wild@gmail.com>
2022-02-16 18:47:06 +0800

    add new rand data type

--- internal/testutils/random.go
+++ internal/testutils/random.go
@@ -4,11 +4,34 @@ package testutils
 
 import (
 	"encoding/base64"
+	"fmt"
 	"math/rand"
 	"time"
 
 	influxdb "github.com/influxdata/influxdb1-client/v2"
 )
+
+func RandInt64(n int) int64 {
+	if n < 1 {
+		return 0
+	}
+
+	i := 1 + rand.Int63n(9)
+	for j := 1; j < n; j++ {
+		i *= 10
+		i += rand.Int63n(10)
+	}
+
+	return i
+}
+
+func RandStrID(n int) string {
+	return fmt.Sprintf("%d", RandInt64(n))
+}
+
+func RandTime() time.Time {
+	return time.Unix(0, RandInt64(13))
+}
 
 func RandString(maxLen int) string {
 	if maxLen <= 0 {
@@ -21,7 +44,20 @@ func RandString(maxLen int) string {
 	return base64.RawStdEncoding.EncodeToString(bts)
 }
 
-func RandTags(entries int, maxKeyLen, maxValueLen int) map[string]string {
+func RandStrings(length, lenPerLine int) []string {
+	ss := make([]string, length)
+	for i := 0; i < length; i++ {
+		ss[i] = RandString(lenPerLine)
+	}
+
+	return ss
+}
+
+func RandWithinStrings(emun []string) string {
+	return emun[rand.Intn(len(emun))]
+}
+
+func RandTags(entries, maxKeyLen, maxValueLen int) map[string]string {
 	tags := make(map[string]string, entries)
 	for i := 0; i < entries; i++ {
 		tags[RandString(maxKeyLen)] = RandString(maxValueLen)
@@ -30,15 +66,14 @@ func RandTags(entries int, maxKeyLen, maxValueLen int) map[string]string {
 	return tags
 }
 
-func RandFields(entries int, maxKeyLen int) map[string]interface{} {
+func RandFields(entries, maxKeyLen int) map[string]interface{} {
 	fields := make(map[string]interface{}, entries)
-
 	for i := 0; i < entries; i++ {
 		switch rand.Int() % 4 {
 		case 0:
 			fields[RandString(maxKeyLen)] = RandString(3 * maxKeyLen)
 		case 1:
-			fields[RandString(maxKeyLen)] = rand.Int()
+			fields[RandString(maxKeyLen)] = rand.Int63n(999999999)
 		case 2:
 			fields[RandString(maxKeyLen)] = rand.Float64()
 		case 3:
@@ -47,6 +82,15 @@ func RandFields(entries int, maxKeyLen int) map[string]interface{} {
 	}
 
 	return fields
+}
+
+func RandMetrics(entries, maxKeyLen int) map[string]float64 {
+	metrics := make(map[string]float64)
+	for i := 0; i < entries; i++ {
+		metrics[RandString(maxKeyLen)] = rand.Float64()
+	}
+
+	return metrics
 }
 
 func RandPoint(name string, maxTags, maxFields int) *influxdb.Point {
--- plugins/inputs/ddtrace/ddtrace_test.go
+++ plugins/inputs/ddtrace/ddtrace_test.go
@@ -2,20 +2,37 @@ package ddtrace
 
 import (
 	"testing"
+
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/testutils"
 )
 
-type httpReq struct {
-	header map[string][]string
-	body   DDTraces
+var contentTypes = []string{
+	"application/msgpack",
+	"application/json",
 }
 
 func TestDDTraceHandlers(t *testing.T) {
 }
 
-func msgpackDataSource() {
+func randomDDSpan() *DDSpan {
+	return &DDSpan{
+		Service:  testutils.RandString(10),
+		Name:     testutils.RandString(10),
+		Resource: testutils.RandString(10),
+		TraceID:  uint64(testutils.RandInt64(10)),
+		SpanID:   uint64(testutils.RandInt64(10)),
+		ParentID: uint64(testutils.RandInt64(10)),
+		Start:    testutils.RandTime().UnixNano(),
+		Duration: testutils.RandInt64(6),
+		Meta:     testutils.RandTags(10, 10, 20),
+		Metrics:  testutils.RandMetrics(10, 10),
+		Type: testutils.RandWithinStrings([]string{"consul", "cache", "memcached", "redis", "aerospike", "cassandra", "db", "elasticsearch", "leveldb",
+			"", "mongodb", "sql", "http", "web", "benchmark", "build", "custom", "datanucleus", "dns", "graphql", "grpc", "hibernate", "queue", "rpc", "soap", "template", "test", "worker"}),
+	}
+}
 
+func msgpackDataSource() {
 }
 
 func jsonDataSource() {
-
 }
