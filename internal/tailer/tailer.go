da3ff84780137c36f16a23b2a9fa22f5ab36b6ea
宋龙奇 <songlongqi827@jiagouyun.com>
2022-02-22 18:34:29 +0800

    Socket line max #623

--- internal/tailer/tailer.go
+++ internal/tailer/tailer.go
@@ -18,6 +18,11 @@ const (
 
 	defaultSource   = "default"
 	defaultMaxLines = 1000
+	LineMaxLen      = 32 * 1024 * 1024
+)
+
+var (
+	maxFieldsLength = 32766
 )
 
 type Option struct {
@@ -44,6 +49,10 @@ type Option struct {
 	//     "none"
 	//     ""
 	CharacterEncoding string
+
+	// datakit 默认的单行最大长度为32k 同时用户也可以配置
+	// 但是 允许最大配置的是32M
+	MaximumLength int
 	// 匹配正则表达式
 	// 符合此正则匹配的数据，将被认定为有效数据。否则会累积追加到上一条有效数据的末尾
 	// 例如 ^\d{4}-\d{2}-\d{2} 行首匹配 YYYY-MM-DD 时间格式
@@ -91,6 +100,10 @@ func (opt *Option) init() error {
 
 	opt.GlobalTags["service"] = opt.Service
 	opt.log = logger.SLogger(opt.InputName)
+
+	if opt.MaximumLength > 0 && opt.MaximumLength < LineMaxLen {
+		maxFieldsLength = opt.MaximumLength
+	}
 
 	if _, err := encoding.NewDecoder(opt.CharacterEncoding); err != nil {
 		return err
