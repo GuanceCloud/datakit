ddfd71bada32c7f91c8b148dc0cb085ff27ab241
wanchuan <wanchuan853@jiagouyun.com>
2022-03-10 14:34:50 +0800

    TestAll

--- io/sink/sinkinfluxdb/sink_influxdb_test.go
+++ io/sink/sinkinfluxdb/sink_influxdb_test.go
@@ -10,6 +10,8 @@ import (
 	// this is important because of the bug in go mod
 	_ "github.com/influxdata/influxdb1-client"
 	client "github.com/influxdata/influxdb1-client/v2"
+	"github.com/stretchr/testify/assert"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io/sink/sinkcommon"
 )
 
 // 检查是不是开发机，如果不是开发机，则直接退出。开发机上需要定义 LOCAL_UNIT_TEST 环境变量。
@@ -22,50 +24,64 @@ func checkDevHost() bool {
 
 //------------------------------------------------------------------------------
 
-func ExampleClient_query() {
-	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: "http://localhost:8086",
-	})
-	if err != nil {
-		fmt.Println("Error creating InfluxDB Client: ", err.Error())
-	}
-	defer c.Close()
+// how to use influxdb v2 SDK:
+// https://github.com/influxdata/influxdb1-client/blob/master/v2/example_test.go
 
-	q := client.NewQuery("SELECT count(value) FROM cpu_load", "mydb", "")
-	if response, err := c.Query(q); err == nil && response.Error() == nil {
-		fmt.Println(response.Results)
-	}
-}
-
-// go test -v -timeout 30s -run ^TestWrite$ gitlab.jiagouyun.com/cloudcare-tools/datakit/io
-func TestWrite(t *testing.T) {
+func TestAll(t *testing.T) {
 	if !checkDevHost() {
 		return
 	}
 
-	sampleSize := 1000
-
-	// Make client
-	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: "http://172.16.239.130:8086",
-	})
-	if err != nil {
-		fmt.Println("Error creating InfluxDB Client: ", err.Error())
-		return
-	}
-	defer c.Close()
-
-	rand.Seed(42)
-
-	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  "db0",
-		Precision: "ns",
-	})
-	if err != nil {
-		fmt.Println("NewBatchPoints: failed: ", err.Error())
-		return
+	cases := []struct {
+		name                  string
+		in                    map[string]interface{}
+		expectLoadConfigError error
+		expectWriteError      error
+	}{
+		{
+			name: "required",
+			in: map[string]interface{}{
+				"id":        "influxdb_1",
+				"addr":      "http://10.200.7.21:8086",
+				"precision": "ns",
+				"database":  "db0",
+			},
+		},
 	}
 
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			si := &SinkInfluxDB{}
+			err := si.LoadConfig(tc.in)
+			assert.Equal(t, tc.expectLoadConfigError, err)
+
+			pts := getTestPoints(t, 1000, 42)
+			var newPts []sinkcommon.ISinkPoint
+			for _, v := range pts {
+				newPts = append(newPts, sinkcommon.ISinkPoint(v))
+			}
+			err = si.Write(newPts)
+			assert.Equal(t, tc.expectWriteError, err)
+		})
+	}
+}
+
+//------------------------------------------------------------------------------
+
+type testPoint struct {
+	*client.Point
+}
+
+var _ sinkcommon.ISinkPoint = new(testPoint)
+
+func (p *testPoint) ToPoint() *client.Point {
+	return p.Point
+}
+
+func getTestPoints(t *testing.T, sampleSize int, seed int64) []*testPoint {
+	rand.Seed(seed)
+
+	var pts []*testPoint
 	for i := 0; i < sampleSize; i++ {
 		regions := []string{"us-west1", "us-west2", "us-west3", "us-east1"}
 		tags := map[string]string{
@@ -86,15 +102,8 @@ func TestWrite(t *testing.T) {
 			fields,
 			time.Now(),
 		)
-		if err != nil {
-			fmt.Printf("Error: %s\n", err.Error())
-			continue
-		}
-		bp.AddPoint(pt)
+		assert.NoError(t, err, fmt.Sprintf("client.NewPoint failed: %v", err))
+		pts = append(pts, &testPoint{pt})
 	}
-
-	err = c.Write(bp)
-	if err != nil {
-		fmt.Println("Error: ", err.Error())
-	}
+	return pts
 }
