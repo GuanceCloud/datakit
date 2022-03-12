bda4578c8dfedc8f3bd44e02636fb9610417e4b6
pengyonghui <421640644@qq.com>
2022-03-12 23:17:20 +0800

    adjust exported json format

--- cmd/make/build/pub.go
+++ cmd/make/build/pub.go
@@ -149,17 +149,86 @@ func generatePipelineDoc() error {
 	return nil
 }
 
+//nolint:lll
 func generatePipelineScripts() error {
+	encoding := base64.StdEncoding
+	type pipelineDemo struct {
+		Pipeline string   `json:"pipeline"`
+		Examples []string `json:"examples"`
+	}
+	logExamples := map[string][]string{
+		"apache.p": {
+			`[Tue May 19 18:39:45.272121 2021] [access_compat:error] [pid 9802] [client ::1:50547] AH01797: client denied by server configuration: /Library/WebServer/Documents/server-status`,
+			`127.0.0.1 - - [17/May/2021:14:51:09 +0800] "GET /server-status?auto HTTP/1.1" 200 917`,
+		},
+		"consul.p": {
+			`Sep 18 19:30:23 derrick-ThinkPad-X230 consul[11803]: 2021-09-18T19:30:23.522+0800 [INFO]  agent.server.connect: initialized primary datacenter CA with provider: provider=consul`,
+		},
+		"elasticsearch.p": {
+			`[2021-06-01T11:45:15,927][WARN ][o.e.c.r.a.DiskThresholdMonitor] [master] high disk watermark [90%] exceeded on [A2kEFgMLQ1-vhMdZMJV3Iw][master][/tmp/elasticsearch-cluster/nodes/0] free: 17.1gb[7.3%], shards will be relocated away from this node; currently relocating away shards totalling [0] bytes; the node is expected to continue to exceed the high disk watermark when these relocations are complete`,
+			`[2021-06-01T11:56:06,712][WARN ][i.s.s.query              ] [master] [shopping][0] took[36.3ms], took_millis[36], total_hits[5 hits], types[], stats[], search_type[QUERY_THEN_FETCH], total_shards[1], source[{"query":{"match":{"name":{"query":"Nariko","operator":"OR","prefix_length":0,"max_expansions":50,"fuzzy_transpositions":true,"lenient":false,"zero_terms_query":"NONE","auto_generate_synonyms_phrase_query":true,"boost":1.0}}},"sort":[{"price":{"order":"desc"}}]}], id[],`,
+			`[2021-06-01T11:56:19,084][WARN ][i.i.s.index              ] [master] [shopping/X17jbNZ4SoS65zKTU9ZAJg] took[34.1ms], took_millis[34], type[_doc], id[LgC3xXkBLT9WrDT1Dovp], routing[], source[{"price":222,"name":"hello"}]`,
+		},
+		"jenkins.p": {
+			`2021-05-18 03:08:58.053+0000 [id=32]	INFO	jenkins.InitReactorRunner$1#onAttained: Started all plugins`,
+		},
+		"kafka.p": {},
+		"mongodb.p": {
+			`{"t":{"$date":"2021-06-03T09:12:19.977+00:00"},"s":"I",  "c":"STORAGE",  "id":22430,   "ctx":"WTCheckpointThread","msg":"WiredTiger message","attr":{"message":"[1622711539:977142][1:0x7f1b9f159700], WT_SESSION.checkpoint: [WT_VERB_CHECKPOINT_PROGRESS] saving checkpoint snapshot min: 653, snapshot max: 653 snapshot count: 0, oldest timestamp: (0, 0) , meta checkpoint timestamp: (0, 0)"}}`,
+		},
+		"mysql.p": {
+			`2017-12-29T12:33:33.095243Z         2 Query     SELECT TABLE_SCHEMA, TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE CREATE_OPTIONS LIKE '%partitioned%';`,
+			`# Time: 2019-11-27T10:43:13.460744Z
+# User@Host: root[root] @ localhost [1.2.3.4]  Id:    35
+# Query_time: 0.214922  Lock_time: 0.000184 Rows_sent: 248832  Rows_examined: 72
+# Thread_id: 55   Killed: 0  Errno: 0
+# Bytes_sent: 123456   Bytes_received: 0
+SET timestamp=1574851393;
+SELECT * FROM fruit f1, fruit f2, fruit f3, fruit f4, fruit f5`,
+		},
+		"nginx.p": {
+			`2021/04/21 09:24:04 [alert] 7#7: *168 write() to "/var/log/nginx/access.log" failed (28: No space left on device) while logging request, client: 120.204.196.129, server: localhost, request: "GET / HTTP/1.1", host: "47.98.103.73"`,
+			`2021/04/29 16:24:38 [emerg] 50102#0: unexpected ";" in /usr/local/etc/nginx/nginx.conf:23`,
+			`127.0.0.1 - - [24/Mar/2021:13:54:19 +0800] "GET /basic_status HTTP/1.1" 200 97 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36"`,
+		},
+		"postgresql.p": {
+			`2021-05-31 15:23:45.110 CST [74305] test [pgAdmin 4 - DB:postgres] postgres [127.0.0.1] 60b48f01.12241 LOG: statement: 		SELECT psd.*, 2^31 - age(datfrozenxid) as wraparound, pg_database_size(psd.datname) as pg_database_size 		FROM pg_stat_database psd 		JOIN pg_database pd ON psd.datname = pd.datname 		WHERE psd.datname not ilike 'template%' AND psd.datname not ilike 'rdsadmin' 		AND psd.datname not ilike 'azure_maintenance' AND psd.datname not ilike 'postgres'`,
+		},
+		"rabbitmq.p": {
+			`2021-05-26 14:20:06.105 [warning] <0.12897.46> rabbitmqctl node_health_check and its HTTP API counterpart are DEPRECATED. See https://www.rabbitmq.com/monitoring.html#health-checks for replacement options.`,
+		},
+		"redis.p": {
+			`122:M 14 May 2019 19:11:40.164 * Background saving terminated with success`,
+		},
+		"solr.p": {},
+		"sqlserver.p": {
+			`2021-05-28 10:46:07.78 spid10s     0 transactions rolled back in database 'msdb' (4:0). This is an informational message only. No user action is required`,
+		},
+		"tomcat.p": {},
+	}
+	// Encode log examples with base64.
+	for _, examples := range logExamples {
+		for i := 0; i < len(examples); i++ {
+			examples[i] = encoding.EncodeToString([]byte(examples[i]))
+		}
+	}
 	scriptMap, err := config.GetScriptMap(false)
 	if err != nil {
 		return err
 	}
-	encoding := base64.StdEncoding
 	// Encode Markdown docs with base64.
 	for name, script := range scriptMap {
 		scriptMap[name] = encoding.EncodeToString([]byte(script))
 	}
-	data, err := json.Marshal(scriptMap)
+	scriptAndExamples := map[string]pipelineDemo{}
+	for name, script := range scriptMap {
+		scriptAndExamples[name] = pipelineDemo{
+			Pipeline: script,
+			Examples: logExamples[name],
+		}
+	}
+
+	data, err := json.Marshal(scriptAndExamples)
 	if err != nil {
 		return err
 	}
@@ -167,6 +236,7 @@ func generatePipelineScripts() error {
 	if err != nil {
 		return err
 	}
+	defer f.Close() //nolint:errcheck,gosec
 	if _, err := f.Write(data); err != nil {
 		return err
 	}
