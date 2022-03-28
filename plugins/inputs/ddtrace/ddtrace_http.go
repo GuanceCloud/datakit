13d3baa1be51d9b6c80110b9c9787ebf8b7970b1
tnt <crypton.from@gmail.com>
2022-03-28 20:17:39 +0800

    fix

--- plugins/inputs/ddtrace/ddtrace_http.go
+++ plugins/inputs/ddtrace/ddtrace_http.go
@@ -57,11 +57,11 @@ func handleDDTraceWithVersion(v string) http.HandlerFunc {
 
 		switch v {
 		case v1, v2, v3:
-			io.WriteString(resp, "OK\n")
+			io.WriteString(resp, "OK\n") // nolint: errcheck,gosec
 		default:
 			resp.Header().Set("Content-Type", "application/json")
 			resp.Header().Set(headerRatesPayloadVersion, req.Header.Get(headerRatesPayloadVersion))
-			resp.Write([]byte("{}"))
+			resp.Write([]byte("{}")) // nolint: errcheck,gosec
 		}
 	}
 }
