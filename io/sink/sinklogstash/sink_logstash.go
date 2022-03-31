9ee48e4065c616046d56cdd2a3b6c9c69808950a
wanchuan <wanchuan853@jiagouyun.com>
2022-03-31 11:57:24 +0800

    sink logstash

--- io/sink/sinklogstash/sink_logstash.go
+++ io/sink/sinklogstash/sink_logstash.go
@@ -2,14 +2,20 @@
 package sinklogstash
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/dkstring"
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io/sink/sinkcommon"
 )
 
 const (
-	creatorID = "logstash"
+	creatorID      = "logstash"
+	defaultTimeout = 10 * time.Second
 )
 
 var (
@@ -18,11 +24,57 @@ var (
 )
 
 type SinkLogstash struct {
-	ID string // sink config identity, unique, automatically generated.
+	ID      string        // sink config identity, unique, automatically generated.
+	addr    string        // required. eg. http://172.16.239.130:8080
+	timeout time.Duration // option.
 }
 
 func (s *SinkLogstash) LoadConfig(mConf map[string]interface{}) error {
-	//
+	if id, err := dkstring.GetMapMD5String(mConf); err != nil {
+		return err
+	} else {
+		s.ID = id
+	}
+
+	if host, err := dkstring.GetMapAssertString("host", mConf); err != nil {
+		return err
+	} else {
+		hostNew, err := dkstring.CheckNotEmpty(host, "host")
+		if err != nil {
+			return err
+		}
+
+		var protocolNew string
+		if protocol, err := dkstring.GetMapAssertString("protocol", mConf); err != nil {
+			return err
+		} else {
+			protocolNew, err = dkstring.CheckNotEmpty(protocol, "protocol")
+			if err != nil {
+				return err
+			}
+		}
+
+		requestPath, err := dkstring.GetMapAssertString("request_path", mConf)
+		if err != nil {
+			return err
+		}
+
+		s.addr = fmt.Sprintf("%s://%s%s", protocolNew, hostNew, requestPath)
+	}
+
+	if timeout, err := dkstring.GetMapAssertString("timeout", mConf); err != nil {
+		return err
+	} else {
+		if timeout != "" {
+			td, err := time.ParseDuration(timeout)
+			if err != nil {
+				return err
+			}
+			s.timeout = td
+		} else {
+			s.timeout = defaultTimeout
+		}
+	}
 
 	initSucceeded = true
 	sinkcommon.AddImpl(s)
@@ -46,6 +98,38 @@ func (s *SinkLogstash) GetInfo() *sinkcommon.SinkInfo {
 }
 
 func (s *SinkLogstash) writeLogstash(pts []sinkcommon.ISinkPoint) error {
+	for _, v := range pts {
+		jp, err := v.ToJSON()
+		if err != nil {
+			return err
+		}
+
+		jsn, err := json.Marshal(jp)
+		if err != nil {
+			return err
+		}
+
+		client := &http.Client{
+			Timeout: s.timeout,
+		}
+
+		req, err := http.NewRequest(http.MethodPut, s.addr, bytes.NewBuffer(jsn))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+
+		if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+			return fmt.Errorf("invalid status code: %d", resp.StatusCode)
+		}
+	}
+
 	return nil
 }
 
--- io/sink/sinklogstash/sink_logstash_test.go
+++ io/sink/sinklogstash/sink_logstash_test.go
@@ -1,13 +1,15 @@
 package sinklogstash
 
 import (
-	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
 	"testing"
 	"time"
 
-	"github.com/telkomdev/go-stash"
+	client "github.com/influxdata/influxdb1-client/v2"
+	"github.com/stretchr/testify/assert"
+	"gitlab.jiagouyun.com/cloudcare-tools/datakit/io/sink/sinkcommon"
 )
 
 // 检查是不是开发机，如果不是开发机，则直接退出。开发机上需要定义 LOCAL_UNIT_TEST 环境变量。
@@ -30,73 +32,100 @@ type Log struct {
 }
 
 func TestAll(t *testing.T) {
-	// if !checkDevHost() {
-	// 	return
-	// }
-
-	// cases := []struct {
-	// 	name                  string
-	// 	in                    map[string]interface{}
-	// 	expectLoadConfigError error
-	// 	expectWriteError      error
-	// }{
-	// 	{
-	// 		name: "required",
-	// 		in: map[string]interface{}{
-	// 			"host":      "10.200.7.21:8086",
-	// 			"protocol":  "http",
-	// 			"precision": "ns",
-	// 			"database":  "db0",
-	// 			"timeout":   "6s",
-	// 		},
-	// 	},
-	// }
-
-	// for _, tc := range cases {
-	// 	t.Run(tc.name, func(t *testing.T) {
-	// 		si := &SinkLogstash{}
-	// 		err := si.LoadConfig(tc.in)
-	// 		assert.Equal(t, tc.expectLoadConfigError, err)
-
-	// 		// pts := getTestPoints(t, 1000, 42)
-	// 		// var newPts []sinkcommon.ISinkPoint
-	// 		// for _, v := range pts {
-	// 		// 	newPts = append(newPts, sinkcommon.ISinkPoint(v))
-	// 		// }
-	// 		// err = si.Write(newPts)
-
-	// 		assert.Equal(t, tc.expectWriteError, err)
-	// 	})
-	// }
-
-	var host string = "localhost"
-	var port uint64 = 5000
-	s, err := stash.Connect(host, port)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if !checkDevHost() {
+		return
 	}
 
-	defer func() {
-		s.Close()
-	}()
-
-	logData := Log{
-		Action: "get_me",
-		Time:   time.Now(),
-		Message: Message{
-			Data: "get me for me",
+	cases := []struct {
+		name                  string
+		in                    map[string]interface{}
+		expectLoadConfigError error
+		expectWriteError      error
+	}{
+		{
+			name: "required",
+			in: map[string]interface{}{
+				"host":         "10.200.7.21:8080",
+				"protocol":     "http",
+				"request_path": "/twitter/tweet/1",
+				"timeout":      "5s",
+			},
 		},
 	}
 
-	logDataJSON, _ := json.Marshal(logData)
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			si := &SinkLogstash{}
+			err := si.LoadConfig(tc.in)
+			assert.Equal(t, tc.expectLoadConfigError, err)
 
-	_, err = s.Write(logDataJSON)
-	if err != nil {
-		// fmt.Fprintf(w, err.Error())
-		fmt.Println(err)
-		return
+			pts := getTestPoints(t, 41)
+			var newPts []sinkcommon.ISinkPoint
+			for _, v := range pts {
+				newPts = append(newPts, sinkcommon.ISinkPoint(v))
+			}
+
+			err = si.Write(newPts)
+			assert.Equal(t, tc.expectWriteError, err)
+		})
 	}
 }
 
 //------------------------------------------------------------------------------
+
+type testPoint struct {
+	measurement string
+	tags        map[string]string
+	fields      map[string]interface{}
+	time        time.Time
+}
+
+var _ sinkcommon.ISinkPoint = new(testPoint)
+
+func (p *testPoint) ToPoint() *client.Point {
+	return nil
+}
+
+func (p *testPoint) String() string {
+	return ""
+}
+
+func (p *testPoint) ToJSON() (*sinkcommon.JSONPoint, error) {
+	return &sinkcommon.JSONPoint{
+		Measurement: p.measurement,
+		Tags:        p.tags,
+		Fields:      p.fields,
+		Time:        p.time,
+	}, nil
+}
+
+func getTestPoints(t *testing.T, seed int64) []*testPoint {
+	t.Helper()
+
+	rand.Seed(seed)
+
+	mms := []string{"mm1", "mm2", "mm3", "mm4"}
+	var pts []*testPoint
+	for i := 0; i < 4; i++ {
+		regions := []string{"us-west1", "us-west2", "us-west3", "us-east1"}
+		tags := map[string]string{
+			"cpu":    "cpu-total",
+			"host":   fmt.Sprintf("host%d", rand.Intn(1000)),
+			"region": regions[rand.Intn(len(regions))],
+		}
+
+		idle := rand.Float64() * 100.0
+		fields := map[string]interface{}{
+			"idle": idle,
+			"busy": 100.0 - idle,
+		}
+		pts = append(pts, &testPoint{
+			measurement: mms[i],
+			tags:        tags,
+			fields:      fields,
+			time:        time.Now(),
+		})
+	}
+
+	return pts
+}
