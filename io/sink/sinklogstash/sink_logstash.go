70fb8929f7ac7453b18283c4efa3067053059649
wanchuan <wanchuan853@jiagouyun.com>
2022-04-01 09:24:46 +0800

    python copyright

--- io/sink/sinklogstash/sink_logstash.go
+++ io/sink/sinklogstash/sink_logstash.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the MIT License.
+// This product includes software developed at Guance Cloud (https://www.guance.com/).
+// Copyright 2021-present Guance, Inc.
+
 // Package sinklogstash contains Logstash sink implement
 package sinklogstash
 
