34e5cabdbadae6aa6ac4b7344e622d231b67402a
tanbiao <tanbiao@jiagouyun.com>
2022-04-19 03:01:32 +0800

    update: fix #690

--- cmd/make/build/build.go
+++ cmd/make/build/build.go
@@ -171,10 +171,7 @@ func Compile() error {
 		}
 
 		goos, goarch := parts[0], parts[1]
-		if goos == datakit.OSDarwin && runtime.GOOS != datakit.OSDarwin {
-			l.Warnf("skip build datakit under %s", arch)
-			continue
-		}
+
 		dir := fmt.Sprintf("%s/%s-%s-%s", BuildDir, AppName, goos, goarch)
 
 		err := os.MkdirAll(dir, os.ModePerm)
@@ -217,8 +214,9 @@ func compileArch(bin, goos, goarch, dir string) error {
 	if goos == datakit.OSWindows {
 		output += winBinSuffix
 	}
+
 	cgoEnabled := "0"
-	if goos == datakit.OSDarwin {
+	if goos == datakit.OSDarwin && runtime.GOOS == datakit.OSDarwin { // darwin verison need CGO to build inputs CPU
 		cgoEnabled = "1"
 	}
 
