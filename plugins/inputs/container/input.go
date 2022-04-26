6e7c537a39562844bbd2e398d644e8a5e9f9b481
liguozhuang <liguozhuang@jiagouyun.com>
2022-04-26 13:09:17 +0800

    修改k8s指标采集时间

--- plugins/inputs/container/input.go
+++ plugins/inputs/container/input.go
@@ -20,7 +20,7 @@ var _ inputs.ReadEnv = (*Input)(nil)
 
 const (
 	objectInterval = time.Minute * 5
-	metricInterval = time.Second * 20
+	metricInterval = time.Second * 60
 )
 
 type Input struct {
@@ -173,9 +173,11 @@ func (i *Input) collectObject() {
 		return
 	}
 
-	l.Debug("collect k8s resource")
+	l.Debug("collect k8s resource object")
 
-	i.gatherK8sResource()
+	if err := i.gatherK8sResourceObject(); err != nil {
+		l.Errorf("failed to collect resource object: %w", err)
+	}
 }
 
 func (i *Input) collectMetric() {
@@ -207,6 +209,10 @@ func (i *Input) collectMetric() {
 	}
 
 	l.Debug("collect k8s-pod metric")
+
+	if err := i.gatherK8sResourceMetric(); err != nil {
+		l.Errorf("failed to collect resource metric: %w", err)
+	}
 
 	if err := i.gatherK8sPodMetrics(); err != nil {
 		l.Errorf("failed to collect pod metric: %w", err)
@@ -289,28 +295,28 @@ func (i *Input) gatherContainerdObject() error {
 		&io.Option{CollectCost: time.Since(start)})
 }
 
-func (i *Input) gatherK8sResource() {
+func (i *Input) gatherK8sResourceMetric() error {
 	start := time.Now()
 
 	metricMeas, err := i.k8sInput.gatherResourceMetric()
-	if err == nil {
-		if err := inputs.FeedMeasurement("k8s-metric", datakit.Metric, metricMeas,
-			&io.Option{CollectCost: time.Since(start)}); err != nil {
-			l.Errorf("failed to feed k8s metrics: %w", err)
-		}
-	} else {
-		l.Errorf("k8s gather metric error: %w", err)
+	if err != nil {
+		return err
 	}
 
+	return inputs.FeedMeasurement("k8s-metric", datakit.Metric, metricMeas,
+		&io.Option{CollectCost: time.Since(start)})
+}
+
+func (i *Input) gatherK8sResourceObject() error {
+	start := time.Now()
+
 	objectMeas, err := i.k8sInput.gatherResourceObject()
-	if err == nil {
-		if err := inputs.FeedMeasurement("k8s-object", datakit.Object, objectMeas,
-			&io.Option{CollectCost: time.Since(start)}); err != nil {
-			l.Errorf("failed to feed k8s objects: %w", err)
-		}
-	} else {
-		l.Errorf("k8s gather object error: %w", err)
+	if err != nil {
+		return err
 	}
+
+	return inputs.FeedMeasurement("k8s-object", datakit.Object, objectMeas,
+		&io.Option{CollectCost: time.Since(start)})
 }
 
 func (i *Input) gatherK8sPodMetrics() error {
