2ab99e7a5263c4a8ffd357cefd0f7aecf2550c2c
郑波 <zhengb@jiagouyun.com>
2022-06-22 18:29:56 +0800

    Iss 836 remove debug log

--- internal/tailer/tailer.go
+++ internal/tailer/tailer.go
@@ -106,8 +106,6 @@ func (opt *Option) Init() error {
 		opt.MultilineMaxLines = defaultMaxLines
 	}
 
-	l.Infof("log_log MultilineMaxLines: %d", opt.MultilineMaxLines)
-
 	opt.GlobalTags["service"] = opt.Service
 	opt.log = logger.SLogger(opt.InputName)
 
