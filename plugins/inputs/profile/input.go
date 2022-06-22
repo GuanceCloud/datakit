d6235b691b3f97306a02f15a48d17eb05a5548d9
tanbiao <tanbiao@jiagouyun.com>
2022-06-22 21:02:05 +0800

    add copyright header

--- plugins/inputs/profile/input.go
+++ plugins/inputs/profile/input.go
@@ -1,4 +1,7 @@
-// Copyright 2022 The guance.com
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the MIT License.
+// This product includes software developed at Guance Cloud (https://www.guance.com/).
+// Copyright 2021-present Guance, Inc.
 
 // Package profile datakit collector
 package profile
