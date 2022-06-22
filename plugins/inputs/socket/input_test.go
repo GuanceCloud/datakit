c75de0d5ceaacfdca0c94c3fad5d2fee98397678
tanbiao <tanbiao@jiagouyun.com>
2022-06-22 18:54:44 +0800

    Iss 799 and 854

--- plugins/inputs/socket/input_test.go
+++ plugins/inputs/socket/input_test.go
@@ -8,6 +8,7 @@ package socket
 import (
 	"fmt"
 	"net"
+	"sync"
 	"testing"
 	"time"
 
@@ -17,9 +18,11 @@ import (
 func TestInput_Run(t *testing.T) {
 	go startTCP()
 	i := &Input{
-		DestURL:    []string{"tcp://127.0.0.1:22", "tcp://127.0.0.1:20000", "udp://1.1.1.1:5555"},
-		UDPTimeOut: datakit.Duration{Duration: time.Second * 10},
-		TCPTimeOut: datakit.Duration{Duration: time.Second * 10},
+		DestURL:    []string{"tcp://47.110.144.10:443", "tcp://497.110.144.10:443", "udp://1.1.1.1:5555", "udp://1.1.1.6541:5555", "tcp://47.96.11.196:443"},
+		curTasks:   map[string]*dialer{},
+		wg:         sync.WaitGroup{},
+		UDPTimeOut: datakit.Duration{Duration: time.Second * 20},
+		TCPTimeOut: datakit.Duration{Duration: time.Second * 20},
 	}
 
 	if err := i.Collect(); err != nil {
