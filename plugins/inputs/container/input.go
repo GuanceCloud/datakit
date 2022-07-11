00ccfda5b62b8fc1aafd47bd16f39c3e6d43b333
liguozhuang <liguozhuang@jiagouyun.com>
2022-07-12 01:47:32 +0800

    修改global_extra_source_map到采集器级别logging_extra_source_map, merge dev

--- plugins/inputs/container/input.go
+++ plugins/inputs/container/input.go
@@ -372,6 +372,7 @@ func (i *Input) setup() bool {
 			containerIncludeLog:    i.ContainerIncludeLog,
 			containerExcludeLog:    i.ContainerExcludeLog,
 			extraTags:              i.Tags,
+			extraSourceMap:         i.LoggingExtraSourceMap,
 		}); err != nil {
 			l.Warnf("create docker input err: %s", err)
 		} else {
@@ -382,6 +383,7 @@ func (i *Input) setup() bool {
 			if c, err := newContainerdInput(&containerdInputConfig{
 				endpoint:            i.ContainerdAddress,
 				extraTags:           i.Tags,
+				extraSourceMap:      i.LoggingExtraSourceMap,
 				containerIncludeLog: i.ContainerIncludeLog,
 				containerExcludeLog: i.ContainerExcludeLog,
 			}); err != nil {
