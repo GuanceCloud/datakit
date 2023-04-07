8f194e82d35bb5829f2f9d5dfd80c3321000c8ea
coanor <coanor@gmail.com>
2023-04-07 16:47:04 +0800

    update .goreleaser.yaml

---
+++ git/git.go
@@ -0,0 +1,17 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the MIT License.
+// This product includes software developed at Guance Cloud (https://www.guance.com/).
+// Copyright 2021-present Guance, Inc.
+
+// Package git used to define various IDs.
+
+package git
+
+var (
+	BuildAt  string = "not-set"
+	Version  string = "not-set"
+	Golang   string = "1.18.3" // cost
+	Commit   string = "not-set"
+	Branch   string = "not-net"
+	Uploader string = "not-set"
+)
