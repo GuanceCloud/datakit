55f75d46092458bc1f7d0df1179c0f4f2b954968
coanor <coanor@gmail.com>
2023-05-11 15:11:18 +0800

    fix lint errors

--- io/dataway/endpoint_test.go
+++ io/dataway/endpoint_test.go
@@ -27,7 +27,6 @@ import (
 )
 
 func TestEndpointRetry(t *T.T) {
-
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 	}))
