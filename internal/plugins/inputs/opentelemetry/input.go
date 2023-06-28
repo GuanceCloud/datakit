9eef31ac9f14543ef88fc790d1424314f971a31a
songlq <songlongqi827@jiagouyun.com>
2023-06-28 15:40:39 +0800

    OTEL exporter http request path
      1. custom request path
      2. zh/en docs
      3. how to use otel.exporter.otlp.protocol=http/protobuf
    
    Close issue: #1718

--- internal/plugins/inputs/opentelemetry/input.go
+++ internal/plugins/inputs/opentelemetry/input.go
@@ -87,15 +87,18 @@ const (
     # capacity = 5120
 
   ## OTEL agent HTTP config for trace and metrics
-  ## If enable set to be true, trace and metrics will be received on path respectively:
+  ## If enable set to be true, trace and metrics will be received on path respectively, by default is:
   ## trace : /otel/v1/trace
   ## metric: /otel/v1/metric
-  ## and the client side should be configured properly with Datakit listening port(default: 9529)
+  ## and the client side should be configured properly with Datakit listening port(default: 9529),
+  ## or custom HTTP request path.
   ## for example http://127.0.0.1:9529/otel/v1/trace
   ## The acceptable http_status_ok values will be 200 or 202.
   [inputs.opentelemetry.http]
    enable = true
    http_status_ok = 200
+   trace_api = "/otel/v1/trace"
+   metric_api = "/otel/v1/metric"
 
   ## OTEL agent GRPC config for trace and metrics.
   ## GRPC services for trace and metrics can be enabled respectively as setting either to be true.
@@ -118,6 +121,8 @@ const (
 var (
 	log               = logger.DefaultSLogger(inputName)
 	statusOK          = 200
+	defaultTraceAPI   = "/otel/v1/trace"
+	defaultMetricAPI  = "/otel/v1/metric"
 	afterGatherRun    itrace.AfterGatherHandler
 	ignoreKeyRegExps  []*regexp.Regexp
 	getAttribute      getAttributeFunc
@@ -129,8 +134,10 @@ var (
 )
 
 type httpConfig struct {
-	Enabled      bool `toml:"enable"`
-	StatusCodeOK int  `toml:"http_status_ok"`
+	Enabled      bool   `toml:"enable"`
+	StatusCodeOK int    `toml:"http_status_ok"`
+	TraceAPI     string `toml:"trace_api"`
+	MetricAPI    string `toml:"metric_api"`
 }
 
 type grpcConfig struct {
@@ -261,16 +268,24 @@ func (ipt *Input) RegHTTPHandler() {
 
 		return
 	}
+	// 路由可能为空，为版本兼容设置默认值。
+	if ipt.HTTPConfig.TraceAPI == "" {
+		ipt.HTTPConfig.TraceAPI = defaultTraceAPI
+	}
 
-	log.Debugf("### register handler for /otel/v1/trace of agent %s", inputName)
+	if ipt.HTTPConfig.MetricAPI == "" {
+		ipt.HTTPConfig.MetricAPI = defaultMetricAPI
+	}
+
+	log.Debugf("### register handler for %s of agent %s", ipt.HTTPConfig.TraceAPI, inputName)
 	statusOK = ipt.HTTPConfig.StatusCodeOK
-	httpapi.RegHTTPHandler("POST", "/otel/v1/trace",
+	httpapi.RegHTTPHandler("POST", ipt.HTTPConfig.TraceAPI,
 		httpapi.CheckExpectedHeaders(
 			workerpool.HTTPWrapper(httpStatusRespFunc, wkpool,
 				httpapi.HTTPStorageWrapper(storage.HTTP_KEY, httpStatusRespFunc, localCache, handleOTELTrace)), log, expectedHeaders))
 
-	log.Debugf("### register handler for /otel/v1/metric of agent %s", inputName)
-	httpapi.RegHTTPHandler("POST", "/otel/v1/metric", httpapi.CheckExpectedHeaders(handleOTElMetrics, log, expectedHeaders))
+	log.Debugf("### register handler for %s of agent %s", ipt.HTTPConfig.MetricAPI, inputName)
+	httpapi.RegHTTPHandler("POST", ipt.HTTPConfig.MetricAPI, httpapi.CheckExpectedHeaders(handleOTElMetrics, log, expectedHeaders))
 }
 
 func (ipt *Input) Run() {
