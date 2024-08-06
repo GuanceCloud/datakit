f186fcb5335bb4f8f93af77d12016c5b5ef8f069
liguozhuang <liguozhuang@jiagouyun.com>
2024-08-06 14:32:00 +0800

    修复进程采集器在 centos 系统下无法解析 container id 的问题

--- internal/plugins/inputs/process/info_linux.go
+++ internal/plugins/inputs/process/info_linux.go
@@ -12,11 +12,16 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 
 	pr "github.com/shirou/gopsutil/v3/process"
 )
+
+// Container IDs should not have a length limit, see this issue https://github.com/opencontainers/runc/issues/1429
+// But there is a limit of 64 here.
+var containerIDregex = regexp.MustCompile(`^[\w+-\.]{64}$`)
 
 func getContainerID(ps *pr.Process) string {
 	s, err := readCgroupFile(int(ps.Pid))
@@ -46,9 +51,24 @@ func parseScopePathForCgroup(s string) (string, error) {
 		return "", fmt.Errorf("cannot parse cgroup items")
 	}
 
+	content := items[2]
+
+	if id := parseContainerIDForUbuntuCgroup(content); id != "" && containerIDregex.MatchString(id) {
+		return id, nil
+	}
+
+	if id := parseContainerIDForCentOSCgroup(content); id != "" && containerIDregex.MatchString(id) {
+		return id, nil
+	}
+
+	// This process was not created by the container and does not return an error.
+	return "", nil
+}
+
+func parseContainerIDForUbuntuCgroup(content string) string {
 	const scopeSuffix = ".scope"
 
-	paths := strings.Split(items[2], "/")
+	paths := strings.Split(content, "/")
 	for _, path := range paths {
 		if !strings.HasSuffix(path, scopeSuffix) {
 			continue
@@ -57,13 +77,18 @@ func parseScopePathForCgroup(s string) (string, error) {
 
 		lastHyphenIndex := strings.LastIndex(scope, "-")
 		if lastHyphenIndex == -1 || lastHyphenIndex == len(scope)-1 {
-			return "", fmt.Errorf("unexpected scope path")
+			return ""
 		}
 
-		return scope[lastHyphenIndex+1:], nil
+		return scope[lastHyphenIndex+1:]
 	}
 
-	return "", nil
+	return ""
+}
+
+func parseContainerIDForCentOSCgroup(content string) string {
+	paths := strings.Split(content, "/")
+	return paths[len(paths)-1]
 }
 
 // Get cgroup from /proc/(pid)/cgroup.
