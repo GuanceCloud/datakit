287bdc0c170ff75d67839e4fcf0585e9eaca6b36
liurui <liurui_wuhan@163.com>
2025-04-18 09:40:34 +0800

    update neo4j dashboard & monitor

--- internal/plugins/inputs/neo4j/export.go
+++ internal/plugins/inputs/neo4j/export.go
@@ -12,55 +12,89 @@ func (*Input) Dashboard(lang inputs.I18n) map[string]string {
 	switch lang {
 	case inputs.I18nZh:
 		return map[string]string{
-			"dbms_page_cache_usage_ratio_note":                     "已用页面数与可用页面总数的比率",
-			"dbms_page_cache_usage_ratio":                          "已用页面数比率",
-			"database_cypher_replan_events_total_note":             "Cypher 决定重新计划查询的总次数",
-			"database_cypher_replan_events_total":                  "Cypher 重新计划时间",
-			"dbms_page_cache_page_faults_total_note":               "页面缓存中发生的页面错误总数",
-			"dbms_page_cache_page_faults_total":                    "页缓存错误",
-			"dbms_page_cache_flushes_total_note":                   "页缓存执行的页刷新总数",
-			"dbms_page_cache_flushes_total":                        "页缓存刷新",
-			"dbms_bolt_connections_note":                           "打开的 Bolt 连接总数<br>关闭的 Bolt 连接总数<br>正在执行`Cypher`并返回结果的 Bolt 连接总数<br>Bolt 连接空闲数",
-			"dbms_bolt_connections":                                "Bolt 连接",
-			"dbms_vm_file_descriptors_note":                        "打开的文件描述符数<br>打开的文件描述符的上限",
-			"dbms_vm_file_descriptors":                             "虚拟机文件描述符",
-			"database_ids_in_use_note":                             "不同关系类型的总数<br>不同属性名称的总数<br>ID 总数<br>节点总数",
-			"database_ids_in_use":                                  "在用数据库 ID",
-			"database_db_query_execution_latency_millis_note":      "成功执行查询的执行时间(毫秒)",
-			"database_db_query_execution_latency_millis":           "查询执行延迟(毫秒)",
-			"database_transaction_last_committed_tx_id_total_note": "",
-			"database_transaction_last_committed_tx_id_total":      "上次提交的事务的ID",
-			"dbms_vm_heap_committed_note":                          "保证可供 JVM 使用的内存量(字节)",
-			"dbms_vm_heap_committed":                               "保证 JVM 内存量",
-			"dbms_vm_heap_used_note":                               "当前使用的内存量(字节)",
-			"dbms_vm_heap_used":                                    "使用的内存量",
+			"var_instance_name":                                    "实例名称",
+			"group_database_performance":                           "数据库性能指标",
+			"group_storage_memory":                                 "存储和内存指标",
+			"group_transaction_query":                              "事务和查询指标",
+			"group_gc_system":                                      "垃圾回收和系统指标",
+			"group_cache_io":                                       "页面缓存和I/O指标",
+			"database_query_duration":                              "数据库查询执行时间",
+			"active_read_transaction_count":                        "当前活跃的读事务数",
+			"active_write_transaction_count":                       "当前活跃的写事务数",
+			"transaction_peak_count":                               "最高并发事务数",
+			"page_cache_hit_ratio":                                 "页面缓存命中率",
+			"database_storage_size":                                "数据库存储大小",
+			"database_pool_used_heap":                              "数据库池已使用的堆大小",
+			"database_pool_used_native":                            "数据库池已使用本地大小",
+			"dbms_vm_heap_used":                                    "已使用 JVM 堆内存",
+			"transaction_committed_total":                          "事务已提交的总数",
+			"transaction_rollbacks_total":                          "事务回滚总数",
+			"db_query_execution_success_total":                     "数据库查询执行成功总数",
+			"db_query_execution_success_total_desc":                "该图表展示了 Neo4j 数据库中查询执行失败总数的最新值随时间的变化趋势。通过 last 函数获取最新的 database_db_query_execution_failure_total 指标值，并以折线图的形式展示其变化情况。",
+			"db_query_execution_failure_total":                     "数据库查询执行失败总数",
+			"vm_gc_time_total":                                     "数据库VM垃圾回收总时间",
+			"vm_file_descriptors_count":                            "当前打开文件描述符数量",
+			"cypher_replan_events_total":                           "Cypher 重规划事件总数",
+			"page_cache_hit_total":                                 "页面缓存命中总数",
+
 		}
 	case inputs.I18nEn:
 		return map[string]string{
-			"dbms_page_cache_usage_ratio_note":                     "The ratio of number of used pages to total number of available pages",
-			"dbms_page_cache_usage_ratio":                          "Page Cache Usage Ratio",
-			"database_cypher_replan_events_total_note":             "The total number of times Cypher has decided to re-plan a query",
-			"database_cypher_replan_events_total":                  "Cypher Replan Events",
-			"dbms_page_cache_page_faults_total_note":               "The total number of page faults happened in the page cache",
-			"dbms_page_cache_page_faults_total":                    "Page Cache Faults",
-			"dbms_page_cache_flushes_total_note":                   "The total number of page merges executed by the page cache",
-			"dbms_page_cache_flushes_total":                        "Page Cache Flushes",
-			"dbms_bolt_connections_note":                           "Number of Bolt connections opened<br>Number of Bolt connections closed<br>Number of Bolt connections running<br>Number of Bolt connections idle",
-			"dbms_bolt_connections":                                "Bolt Connections",
-			"dbms_vm_file_descriptors_note":                        "Number of open file descriptors<br>Maximum number of open file descriptors",
-			"dbms_vm_file_descriptors":                             "Vm File Descriptors",
-			"database_ids_in_use_note":                             "Number of different relationship types<br>Number of different property names<br>Number of relationships<br>Number of nodes",
-			"database_ids_in_use":                                  "IDs In use",
-			"database_db_query_execution_latency_millis_note":      "Execution time in milliseconds of queries executed successfully",
-			"database_db_query_execution_latency_millis":           "Query Execution Latency(ms)",
-			"database_transaction_last_committed_tx_id_total_note": "The ID of the last committed transaction",
-			"database_transaction_last_committed_tx_id_total":      "Transaction  Last Committed ID",
-			"dbms_vm_heap_committed_note":                          "Amount of memory (in bytes) guaranteed to be available for use by the JVM",
-			"dbms_vm_heap_committed":                               "JVM Heap Committed",
-			"dbms_vm_heap_used_note":                               "Amount of memory (in bytes) currently used",
-			"dbms_vm_heap_used":                                    "JVM Heap Used",
+			"var_instance_name":                                    "Instance Name",
+			"group_database_performance":                           "Database Performance",
+			"group_storage_memory":                                 "Storage Memory",
+			"group_transaction_query":                              "Transaction Query",
+			"group_gc_system":                                      "GC System",
+			"group_cache_io":                                       "Cache IO",
+			"database_query_duration":                              "Database Query Duration",
+			"active_read_transaction_count":                        "Active Read Transaction Count",
+			"active_write_transaction_count":                       "Active Write Transaction Count",
+			"transaction_peak_count":                               "Transaction Peak Count",
+			"page_cache_hit_ratio":                                 "Page Cache Hit Ratio",
+			"database_storage_size":                                "Database Storage Size",
+			"database_pool_used_heap":                              "Database Pool Used Heap",
+			"database_pool_used_native":                            "Database Pool Used Native",
+			"dbms_vm_heap_used":                                    "DBMS VM Heap Used",
+			"transaction_committed_total":                          "Transaction Committed Total",
+			"transaction_rollbacks_total":                          "Transaction Rollbacks Total",
+			"db_query_execution_success_total":                     "DB Query Execution Success Total",
+			"db_query_execution_success_total_desc":                "This chart shows the latest value of database_db_query_execution_failure_total, which represents the total number of failed database queries, as a line graph over time. The last function is used to retrieve the latest value of this metric, and the data is presented as a line graph.",
+			"db_query_execution_failure_total":                     "DB Query Execution Failure Total",
+			"vm_gc_time_total":                                     "VM GC Time Total",
+			"vm_file_descriptors_count":                            "VM File Descriptors Count",
+			"cypher_replan_events_total":                           "Cypher Replan Events Total",
+			"page_cache_hit_total":                                 "Page Cache Hit Total",
 		}
 	default:
 		return nil
 	}
 }
+
+func (*Input) Monitor(lang inputs.I18n) map[string]string {
+	switch lang {
+	case inputs.I18nZh:
+		//nolint:lll
+		return map[string]string{
+			"database_db_query_execution_title":                `查询的执行时间过长`,
+			"database_db_query_execution_message":              `查询的执行时间过长，请关注：\n{{instance}}的{{db}}的查询时间超过{{Result}}请关注`,
+			"page_cache_hit_ratio_title":                       `页面缓存的命中率过低`,
+			"page_cache_hit_ratio_message":                     `{{instance}}的页面缓存的命中率低于{{df_status}},请关注`,
+			"vm_gc_time_total_title":                           `垃圾回收的时间过长`,
+			"vm_gc_time_total_message":                         `{{instance}}的垃圾回收时间超过{{df_status}}`,
+
+		}
+	case inputs.I18nEn:
+		//nolint:lll
+		return map[string]string{
+		    "database_db_query_execution_title":                `Query execution time is too long`,
+		    "database_db_query_execution_message":              `Query execution time is too long, please pay attention to: \n{{instance}} of {{db}} query time exceeds {{Result}}, please pay attention to`,
+		    "page_cache_hit_ratio_title":                       `Page cache hit ratio is too low`,
+		    "page_cache_hit_ratio_message":                     `{{instance}} page cache hit ratio is below {{df_status}}, please pay attention to`,
+		    "vm_gc_time_total_title":                           `GC time is too long`,
+		    "vm_gc_time_total_message":                         `{{instance}} GC time exceeds {{df_status}}`,
+		}
+	default:
+		return nil
+	}
+}
+
