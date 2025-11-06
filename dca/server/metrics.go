c8e9ac4858698b481ecf121159d3b98526ec1a1c
郑波 <zhengb@guance.com>
2025-11-06 14:47:56 +0800

    Resolve "DCA 指标集功能完善以及内置视图导出等"

--- dca/server/metrics.go
+++ dca/server/metrics.go
@@ -7,17 +7,71 @@
 package server
 
 import (
+	"time"
+
 	"github.com/GuanceCloud/cliutils/metrics"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var connectionTotalGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-	Name: "connection_number",
-	Help: "The total number of connections.",
-})
+var startTime = time.Now()
+
+var (
+	datakitTotalGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "datakit_number",
+		Help: "The total number of datakits.",
+	}, []string{"host_name", "os"})
+
+	uptimeGauge = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "uptime_seconds",
+		Help: "The uptime of the server.",
+	}, func() float64 {
+		return time.Since(startTime).Seconds()
+	})
+
+	apiElapsedVec = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Name: "api_elapsed_seconds",
+		Help: "API request cost",
+
+		Objectives: map[float64]float64{
+			0.5:  0.05,
+			0.9:  0.01,
+			0.99: 0.001,
+		},
+
+		MaxAge:     prometheus.DefMaxAge,
+		AgeBuckets: prometheus.DefAgeBuckets,
+	}, []string{
+		"api",
+		"method",
+		"status",
+	})
+
+	websocketElapsedVec = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Name: "websocket_elapsed_seconds",
+		Help: "Websocket request cost",
+
+		Objectives: map[float64]float64{
+			0.5:  0.05,
+			0.9:  0.01,
+			0.99: 0.001,
+		},
+
+		MaxAge:     prometheus.DefMaxAge,
+		AgeBuckets: prometheus.DefAgeBuckets,
+	}, []string{
+		"host_name",
+		"action",
+		"code",
+	})
+)
 
 //nolint:gochecknoinits
 func init() {
-	metrics.MustRegister([]prometheus.Collector{connectionTotalGauge}...)
+	metrics.MustRegister([]prometheus.Collector{
+		datakitTotalGauge,
+		uptimeGauge,
+		apiElapsedVec,
+		websocketElapsedVec,
+	}...)
 }
