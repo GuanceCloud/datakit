2b4456385fd815c71a5314fd6639a1337347b197
谭彪 <tanbiao@jiagouyun.com>
2025-11-06 14:45:41 +0800

    fix: ignore release default input's conf if legacy conf exists

--- internal/config/inputcfg_test.go
+++ internal/config/inputcfg_test.go
@@ -14,7 +14,10 @@ import (
 	"gitlab.jiagouyun.com/cloudcare-tools/datakit/internal/plugins/inputs"
 )
 
-type mockedInput struct{}
+type mockedInput struct {
+	sample,
+	catalog string
+}
 
 /*
 
@@ -23,9 +26,22 @@ type mockedInput struct{}
 	SampleConfig() string
 */
 
-func (*mockedInput) Catalog() string      { return "samples" }
-func (*mockedInput) Run()                 {}
-func (*mockedInput) SampleConfig() string { return "test-sample" }
+func (i *mockedInput) Catalog() string {
+	if i.catalog == "" {
+		return "samples"
+	}
+	return i.catalog
+}
+
+func (i *mockedInput) Run() {}
+
+func (i *mockedInput) SampleConfig() string {
+	if i.sample == "" {
+		return "test-sample"
+	}
+
+	return i.sample
+}
 
 func Test_initDefaultEnabledPlugins(t *T.T) {
 	t.Run(`basic`, func(t *T.T) {
@@ -85,6 +101,42 @@ func Test_initDefaultEnabledPlugins(t *T.T) {
 		assert.FileExists(t, filepath.Join(dir, "cpu.conf"))
 
 		data, err := os.ReadFile(filepath.Join(dir, "cpu.conf"))
+		assert.NoError(t, err)
+		assert.Equal(t, []byte(`123`), data)
+
+		assert.FileExists(t, filepath.Join(dir, "mem.conf"))
+
+		data, err = os.ReadFile(filepath.Join(dir, "mem.conf"))
+		assert.NoError(t, err)
+		assert.Equal(t, []byte(`test-sample`), data)
+	})
+
+	t.Run(`conf-legacy-exist-and-skip`, func(t *T.T) {
+		c := DefaultConfig()
+		c.DefaultEnabledInputs = []string{"cpu", "mem"}
+		catalogHost := "host"
+
+		dir := t.TempDir()
+		assert.NoError(t, os.MkdirAll(dir, os.ModePerm))
+		assert.NoError(t, os.MkdirAll(filepath.Join(dir, catalogHost), os.ModePerm))
+
+		// create legacy conf: should not overwrite on it
+		assert.NoError(t, os.WriteFile(filepath.Join(dir,
+			catalogHost,
+			"cpu.conf"), []byte(`123`), os.ModePerm))
+
+		ipts := map[string]inputs.Creator{
+			"cpu": func() inputs.Input { return &mockedInput{catalog: catalogHost} },
+			"mem": func() inputs.Input { return &mockedInput{catalog: "not-exist-cataglog"} },
+		}
+
+		c.initDefaultEnabledPlugins(dir, ipts)
+
+		assert.NoFileExists(t, filepath.Join(dir, "cpu.conf"))
+		assert.NoFileExists(t, filepath.Join(dir, catalogHost, "mem.conf"))
+		assert.FileExists(t, filepath.Join(dir, "mem.conf")) // mem.conf legacy conf not created
+
+		data, err := os.ReadFile(filepath.Join(dir, catalogHost, "cpu.conf"))
 		assert.NoError(t, err)
 		assert.Equal(t, []byte(`123`), data)
 
